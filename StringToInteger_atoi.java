4e40f51732692a62a083344c1ee0864d395311c9
Kai Wang <charles.wangkai@gmail.com>
2014-10-24 18:59:59 +0800

    StringToInteger_atoi

---
+++ StringToInteger_atoi.java
@@ -0,0 +1,25 @@
+public class StringToInteger_atoi {
+	public int atoi(String str) {
+		str = str.trim();
+
+		long number = 0;
+		if (!str.isEmpty()) {
+			char firstCh = str.charAt(0);
+			boolean negative = firstCh == '-';
+			int sign = negative ? -1 : 1;
+			for (int i = (negative || firstCh == '+') ? 1 : 0; i < str.length(); i++) {
+				char ch = str.charAt(i);
+				if (!Character.isDigit(ch)) {
+					break;
+				}
+				number = number * 10 + (str.charAt(i) - '0') * sign;
+				if (number > Integer.MAX_VALUE) {
+					return Integer.MAX_VALUE;
+				} else if (number < Integer.MIN_VALUE) {
+					return Integer.MIN_VALUE;
+				}
+			}
+		}
+		return (int) number;
+	}
+}
