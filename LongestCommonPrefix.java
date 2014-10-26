180953501e396606b30956ba9f1c1f22e68f7589
Kai Wang <charles.wangkai@gmail.com>
2014-10-26 15:42:33 +0800

    LongestCommonPrefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+public class LongestCommonPrefix {
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0) {
+			return "";
+		}
+
+		for (int length = 1;; length++) {
+			Character ch = null;
+			for (String str : strs) {
+				if (length > str.length()
+						|| (ch != null && ch != str.charAt(length - 1))) {
+					return strs[0].substring(0, length - 1);
+				}
+				ch = str.charAt(length - 1);
+			}
+		}
+	}
+}
