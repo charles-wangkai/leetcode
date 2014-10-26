6a2f489366d8e7be6aa6f3758c5e792127000169
Kai Wang <charles.wangkai@gmail.com>
2014-10-26 10:39:51 +0800

    RegularExpressionMatching

---
+++ RegularExpressionMatching.java
@@ -0,0 +1,35 @@
+public class RegularExpressionMatching {
+	public boolean isMatch(String s, String p) {
+		return checkMatch(s, 0, p, 0);
+	}
+
+	boolean checkMatch(String s, int indexS, String p, int indexP) {
+		if (indexS == s.length()) {
+			if (indexP == p.length()) {
+				return true;
+			}
+			return isNextZeroOrMoreMatch(p, indexP)
+					&& checkMatch(s, indexS, p, indexP + 2);
+		}
+
+		if (indexP == p.length()) {
+			return false;
+		}
+
+		boolean nextMatch = isCharMatch(s.charAt(indexS), p.charAt(indexP));
+		if (isNextZeroOrMoreMatch(p, indexP)) {
+			return checkMatch(s, indexS, p, indexP + 2)
+					|| (nextMatch && checkMatch(s, indexS + 1, p, indexP));
+		} else {
+			return nextMatch && checkMatch(s, indexS + 1, p, indexP + 1);
+		}
+	}
+
+	boolean isNextZeroOrMoreMatch(String p, int indexP) {
+		return indexP + 1 < p.length() && p.charAt(indexP + 1) == '*';
+	}
+
+	boolean isCharMatch(char chS, char chP) {
+		return chP == '.' || chS == chP;
+	}
+}
