6a4e2761939dc14d12d64ecd0c211ec7ab810f3a
Kai Wang <charles.wangkai@gmail.com>
2014-10-29 02:25:00 +0800

    GenerateParentheses

---
+++ GenerateParentheses.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+	public List<String> generateParenthesis(int n) {
+		List<String> result = new ArrayList<String>();
+		search(result, new char[n + n], 0, n, n);
+		return result;
+	}
+
+	void search(List<String> result, char[] current, int index, int leftRemain,
+			int rightRemain) {
+		if (leftRemain == 0 && rightRemain == 0) {
+			result.add(new String(current));
+			return;
+		}
+
+		if (leftRemain > 0) {
+			current[index] = '(';
+			search(result, current, index + 1, leftRemain - 1, rightRemain);
+		}
+		if (rightRemain > leftRemain) {
+			current[index] = ')';
+			search(result, current, index + 1, leftRemain, rightRemain - 1);
+		}
+	}
+}
