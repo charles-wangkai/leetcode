45ca017cc774b5698bbfa561767a9fce9f509f9a
Kai Wang <charles.wangkai@gmail.com>
2014-11-11 18:47:36 +0800

    JumpGame_II

---
+++ JumpGame_II.java
@@ -0,0 +1,15 @@
+public class JumpGame_II {
+	public int jump(int[] A) {
+		int step = 0;
+		int maxIndex = 0;
+		int nextMaxIndex = -1;
+		for (int i = 0; maxIndex < A.length - 1; i++) {
+			if (i == maxIndex + 1) {
+				maxIndex = nextMaxIndex;
+				step++;
+			}
+			nextMaxIndex = Math.max(nextMaxIndex, i + A[i]);
+		}
+		return step;
+	}
+}
