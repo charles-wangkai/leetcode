729745482fd5929f4834fa0cdb08318ff6fb03a8
Kai Wang <charles.wangkai@gmail.com>
2014-11-24 02:43:55 +0800

    Combinations

---
+++ Combinations.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		search(result, n, k, new ArrayList<Integer>(), 1);
+		return result;
+	}
+
+	void search(List<List<Integer>> result, int n, int k,
+			List<Integer> combination, int number) {
+		if (combination.size() == k) {
+			result.add(new ArrayList<Integer>(combination));
+			return;
+		}
+		if (combination.size() + (n - number + 1) < k) {
+			return;
+		}
+		search(result, n, k, combination, number + 1);
+		combination.add(number);
+		search(result, n, k, combination, number + 1);
+		combination.remove(combination.size() - 1);
+	}
+}
