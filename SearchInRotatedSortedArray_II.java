6ae1ebedd3b9554cfa60256009113475a4be331b
Kai Wang <charles.wangkai@gmail.com>
2014-11-24 03:12:22 +0800

    SearchInRotatedSortedArray_II

---
+++ SearchInRotatedSortedArray_II.java
@@ -0,0 +1,20 @@
+public class SearchInRotatedSortedArray_II {
+	public boolean search(int[] A, int target) {
+		return search(A, target, 0, A.length - 1);
+	}
+
+	boolean search(int[] A, int target, int lower, int upper) {
+		if (lower > upper) {
+			return false;
+		}
+		if (A[lower] < A[upper] && !(target >= A[lower] && target <= A[upper])) {
+			return false;
+		}
+		int middle = (lower + upper) / 2;
+		if (A[middle] == target) {
+			return true;
+		}
+		return search(A, target, middle + 1, upper)
+				|| search(A, target, lower, middle - 1);
+	}
+}
