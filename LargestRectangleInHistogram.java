d0808f8acabd50b0260fc3ae243f59b12d96837b
Kai Wang <charles.wangkai@gmail.com>
2014-11-25 11:04:07 +0800

    LargestRectangleInHistogram

---
+++ LargestRectangleInHistogram.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+	public int largestRectangleArea(int[] height) {
+		height = Arrays.copyOf(height, height.length + 1);
+		Stack<Integer> indices = new Stack<Integer>();
+		int maxArea = 0;
+		for (int i = 0; i < height.length; i++) {
+			while (!indices.empty() && height[i] <= height[indices.peek()]) {
+				int h = height[indices.pop()];
+				maxArea = Math.max(maxArea, h
+						* (i - (indices.empty() ? 0 : (indices.peek() + 1))));
+			}
+			indices.push(i);
+		}
+		return maxArea;
+	}
+}
