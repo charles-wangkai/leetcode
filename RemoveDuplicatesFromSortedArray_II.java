93b40170ebec858d89e37cf9895e7cb4c40a5a1d
Kai Wang <charles.wangkai@gmail.com>
2014-11-25 10:21:50 +0800

    RemoveDuplicatesFromSortedArray_II

---
+++ RemoveDuplicatesFromSortedArray_II.java
@@ -0,0 +1,12 @@
+public class RemoveDuplicatesFromSortedArray_II {
+	public int removeDuplicates(int[] A) {
+		int length = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (length <= 1 || A[i] != A[length - 2]) {
+				A[length] = A[i];
+				length++;
+			}
+		}
+		return length;
+	}
+}
