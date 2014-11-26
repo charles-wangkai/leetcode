c49b00a5600bc88fcde718aab847ecade4948bf5
Kai Wang <charles.wangkai@gmail.com>
2014-11-26 13:57:28 +0800

    MergeSortedArray

---
+++ MergeSortedArray.java
@@ -0,0 +1,15 @@
+public class MergeSortedArray {
+	public void merge(int A[], int m, int B[], int n) {
+		int indexA = m - 1;
+		int indexB = n - 1;
+		for (int i = m + n - 1; i >= 0; i--) {
+			if (indexA >= 0 && (indexB < 0 || A[indexA] >= B[indexB])) {
+				A[i] = A[indexA];
+				indexA--;
+			} else {
+				A[i] = B[indexB];
+				indexB--;
+			}
+		}
+	}
+}
