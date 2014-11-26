1c66a4d5c510f40d89091e7e7b0070dd74ac63e8
Kai Wang <charles.wangkai@gmail.com>
2014-11-26 13:51:01 +0800

    GrayCode

---
+++ GrayCode.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrayCode {
+	public List<Integer> grayCode(int n) {
+		List<Integer> sequence = new ArrayList<Integer>();
+		search(sequence, new int[n], 0);
+		return sequence;
+	}
+
+	void search(List<Integer> sequence, int[] bits, int index) {
+		if (index == bits.length) {
+			sequence.add(convertToInt(bits));
+			return;
+		}
+
+		search(sequence, bits, index + 1);
+		bits[index] = 1 - bits[index];
+		search(sequence, bits, index + 1);
+	}
+
+	int convertToInt(int[] bits) {
+		int number = 0;
+		for (int bit : bits) {
+			number = number * 2 + bit;
+		}
+		return number;
+	}
+}
