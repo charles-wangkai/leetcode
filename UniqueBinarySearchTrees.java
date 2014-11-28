e23633f91ab6f5b75951e3c298d87d099db21b99
Kai Wang <charles.wangkai@gmail.com>
2014-11-28 21:58:06 +0800

    UniqueBinarySearchTrees

---
+++ UniqueBinarySearchTrees.java
@@ -0,0 +1,12 @@
+public class UniqueBinarySearchTrees {
+	public int numTrees(int n) {
+		int[] treeNums = new int[n + 1];
+		treeNums[0] = 1;
+		for (int i = 1; i < treeNums.length; i++) {
+			for (int j = 0; j < i; j++) {
+				treeNums[i] += treeNums[j] * treeNums[i - 1 - j];
+			}
+		}
+		return treeNums[n];
+	}
+}
