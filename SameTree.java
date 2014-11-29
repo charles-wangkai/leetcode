3856f8c9480ddaf7e6d918effbe296bede65251e
Kai Wang <charles.wangkai@gmail.com>
2014-11-29 19:23:59 +0800

    SameTree

---
+++ SameTree.java
@@ -0,0 +1,25 @@
+// Definition for binary tree
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == q) {
+			return true;
+		}
+		if (p == null || q == null) {
+			return false;
+		}
+		if (p.val != q.val) {
+			return false;
+		}
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+}
