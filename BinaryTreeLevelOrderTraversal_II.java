c2e288ee4e39baa47f513307fa38956318747423
Kai Wang <charles.wangkai@gmail.com>
2014-11-30 16:34:21 +0800

    BinaryTreeLevelOrderTraversal_II

---
+++ BinaryTreeLevelOrderTraversal_II.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Definition for binary tree
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class BinaryTreeLevelOrderTraversal_II {
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> levels = new ArrayList<List<Integer>>();
+		search(levels, root, 0);
+		reverse(levels);
+		return levels;
+	}
+
+	void search(List<List<Integer>> levels, TreeNode node, int depth) {
+		if (node == null) {
+			return;
+		}
+		if (depth == levels.size()) {
+			levels.add(new ArrayList<Integer>());
+		}
+		levels.get(depth).add(node.val);
+		search(levels, node.left, depth + 1);
+		search(levels, node.right, depth + 1);
+	}
+
+	void reverse(List<List<Integer>> levels) {
+		for (int i = 0, j = levels.size() - 1; i < j; i++, j--) {
+			List<Integer> temp = levels.get(i);
+			levels.set(i, levels.get(j));
+			levels.set(j, temp);
+		}
+	}
+}
