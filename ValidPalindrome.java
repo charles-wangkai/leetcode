f67b06743f2a42aeadd34db569aeb7d1c44b589f
Kai Wang <charles.wangkai@gmail.com>
2014-12-04 16:38:01 +0800

    ValidPalindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,30 @@
+public class ValidPalindrome {
+	public boolean isPalindrome(String s) {
+		int i = 0;
+		int j = s.length() - 1;
+		while (true) {
+			while (i < s.length() && !isCheckableChar(s.charAt(i))) {
+				i++;
+			}
+			while (j >= 0 && !isCheckableChar(s.charAt(j))) {
+				j--;
+			}
+			if (i >= j) {
+				return true;
+			}
+			if (!isSameIgnoringCase(s.charAt(i), s.charAt(j))) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+	}
+
+	boolean isCheckableChar(char ch) {
+		return Character.isLetterOrDigit(ch);
+	}
+
+	boolean isSameIgnoringCase(char ch1, char ch2) {
+		return Character.toLowerCase(ch1) == Character.toLowerCase(ch2);
+	}
+}
