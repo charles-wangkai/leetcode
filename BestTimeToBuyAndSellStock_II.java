0b10974d44a073638ec7eb765603e033c8a9cf4b
Kai Wang <charles.wangkai@gmail.com>
2014-12-04 15:39:41 +0800

    BestTimeToBuyAndSellStock_II

---
+++ BestTimeToBuyAndSellStock_II.java
@@ -0,0 +1,9 @@
+public class BestTimeToBuyAndSellStock_II {
+	public int maxProfit(int[] prices) {
+		int maxEarn = 0;
+		for (int i = 0; i < prices.length - 1; i++) {
+			maxEarn += Math.max(0, prices[i + 1] - prices[i]);
+		}
+		return maxEarn;
+	}
+}
