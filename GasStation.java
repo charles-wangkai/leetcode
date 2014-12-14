102a49f3d2573a6576a2e15432964892dd8cd69e
Kai Wang <charles.wangkai@gmail.com>
2014-12-14 12:52:35 +0800

    GasStation

---
+++ GasStation.java
@@ -0,0 +1,16 @@
+public class GasStation {
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		int total = 0;
+		int current = 0;
+		int startingIndex = 0;
+		for (int i = 0; i < gas.length; i++) {
+			total += gas[i] - cost[i];
+			current += gas[i] - cost[i];
+			if (current < 0) {
+				startingIndex = i + 1;
+				current = 0;
+			}
+		}
+		return total >= 0 ? startingIndex : -1;
+	}
+}
