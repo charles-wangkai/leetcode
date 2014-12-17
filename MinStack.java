319133ec3df02028d113f395a8777a807e273145
Kai Wang <charles.wangkai@gmail.com>
2014-12-17 14:18:36 +0800

    MinStack

---
+++ MinStack.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class MinStack {
+	Stack<Integer> numbers = new Stack<Integer>();
+	Stack<Integer> mins = new Stack<Integer>();
+
+	public void push(int x) {
+		numbers.push(x);
+		if (mins.empty() || x <= mins.peek()) {
+			mins.push(x);
+		}
+	}
+
+	public void pop() {
+		int number = numbers.pop();
+		if (mins.peek() == number) {
+			mins.pop();
+		}
+	}
+
+	public int top() {
+		return numbers.peek();
+	}
+
+	public int getMin() {
+		return mins.peek();
+	}
+}
