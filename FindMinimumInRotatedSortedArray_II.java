6019c4b17d627c91f8a7891649fbf239ebd6be1c
Kai Wang <charles.wangkai@gmail.com>
2014-12-17 14:03:15 +0800

    FindMinimumInRotatedSortedArray_II

---
+++ FindMinimumInRotatedSortedArray_II.java
@@ -0,0 +1,14 @@
+public class FindMinimumInRotatedSortedArray_II {
+	public int findMin(int[] num) {
+		return search(num, 0, num.length - 1);
+	}
+
+	int search(int[] num, int lower, int upper) {
+		if (lower == upper || num[lower] < num[upper]) {
+			return num[lower];
+		}
+		int middle = (lower + upper) / 2;
+		return Math.min(search(num, lower, middle),
+				search(num, middle + 1, upper));
+	}
+}
