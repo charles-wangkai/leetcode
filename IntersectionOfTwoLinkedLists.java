9473f4e44f77a5bef6f5db6ee940994f794d2c24
Kai Wang <charles.wangkai@gmail.com>
2014-12-18 21:21:21 +0800

    IntersectionOfTwoLinkedLists

---
+++ IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,43 @@
+// Definition for singly-linked list.
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+public class IntersectionOfTwoLinkedLists {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		int lengthA = findLength(headA);
+		int lengthB = findLength(headB);
+		ListNode nodeA = move(headA, lengthA > lengthB ? (lengthA - lengthB)
+				: 0);
+		ListNode nodeB = move(headB, lengthB > lengthA ? (lengthB - lengthA)
+				: 0);
+		while (nodeA != nodeB) {
+			nodeA = nodeA.next;
+			nodeB = nodeB.next;
+		}
+		return nodeA;
+	}
+
+	int findLength(ListNode head) {
+		int length = 0;
+		while (head != null) {
+			length++;
+			head = head.next;
+		}
+		return length;
+	}
+
+	ListNode move(ListNode head, int step) {
+		ListNode node = head;
+		for (int i = 0; i < step; i++) {
+			node = node.next;
+		}
+		return node;
+	}
+}
