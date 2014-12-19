a0c9e0f91b52ac9db11aae6770660fd340b26950
Kai Wang <charles.wangkai@gmail.com>
2014-12-19 14:25:12 +0800

    MissingRanges

---
+++ MissingRanges.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingRanges {
+	public List<String> findMissingRanges(int[] A, int lower, int upper) {
+		List<String> missingRanges = new ArrayList<String>();
+		int begin = lower;
+		for (int number : A) {
+			if (number != begin) {
+				missingRanges.add(generateRange(begin, number - 1));
+			}
+			begin = number + 1;
+		}
+		if (begin <= upper) {
+			missingRanges.add(generateRange(begin, upper));
+		}
+		return missingRanges;
+	}
+
+	String generateRange(int begin, int end) {
+		return (begin == end) ? (begin + "") : (begin + "->" + end);
+	}
+}
