e7f13499ad329391ada8fe6b9f7e678bf94d3f83
Kai Wang <charles.wangkai@gmail.com>
2014-12-19 14:07:48 +0800

    OneEditDistance

---
+++ OneEditDistance.java
@@ -0,0 +1,15 @@
+public class OneEditDistance {
+	public boolean isOneEditDistance(String s, String t) {
+		int index = 0;
+		while (index < s.length() && index < t.length()
+				&& s.charAt(index) == t.charAt(index)) {
+			index++;
+		}
+		return (index < s.length() && index <= t.length() && s.substring(
+				index + 1).equals(t.substring(index)))
+				|| (index < s.length() && index < t.length() && s.substring(
+						index + 1).equals(t.substring(index + 1)))
+				|| (index <= s.length() && index < t.length() && s.substring(
+						index).equals(t.substring(index + 1)));
+	}
+}
