edf0bd6eac7052b9cdb425a34ee6344bac410240
Kai Wang <charles.wangkai@gmail.com>
2014-12-20 18:10:12 +0800

    ExcelSheetColumnTitle

---
+++ ExcelSheetColumnTitle.java
@@ -0,0 +1,11 @@
+public class ExcelSheetColumnTitle {
+	public String convertToTitle(int n) {
+		StringBuilder sb = new StringBuilder();
+		while (n != 0) {
+			char ch = (char) ('A' + (n - 1) % 26);
+			sb.append(ch);
+			n = (n - 1) / 26;
+		}
+		return sb.reverse().toString();
+	}
+}
