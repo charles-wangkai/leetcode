433002cf224a454a6db0dbd8228726b4d9a92ff4
Kai Wang <charles.wangkai@gmail.com>
2014-12-28 18:04:48 +0800

    ExcelSheetColumnNumber

---
+++ ExcelSheetColumnNumber.java
@@ -0,0 +1,9 @@
+public class ExcelSheetColumnNumber {
+	public int titleToNumber(String s) {
+		int number = 0;
+		for (int i = 0; i < s.length(); i++) {
+			number = number * 26 + (s.charAt(i) - 'A' + 1);
+		}
+		return number;
+	}
+}
