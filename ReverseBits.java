a8287499865c07c35b268916bb42a0180975404b
Kai Wang <charles.wangkai@gmail.com>
2015-03-09 00:30:36 +0800

    ReverseBits

---
+++ ReverseBits.java
@@ -0,0 +1,13 @@
+public class ReverseBits {
+	public int reverseBits(int n) {
+		int reversed = 0;
+		for (int i = 0; i < 32; i++) {
+			reversed <<= 1;
+			if ((n & 1) != 0) {
+				reversed++;
+			}
+			n >>= 1;
+		}
+		return reversed;
+	}
+}
