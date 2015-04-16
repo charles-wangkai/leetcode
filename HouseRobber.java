ffb7a169ac72d7602bc1677c838c1dd1bd204e61
Kai Wang <charles.wangkai@gmail.com>
2015-04-16 14:11:49 +0800

    HouseRobber

---
+++ HouseRobber.java
@@ -0,0 +1,14 @@
+public class HouseRobber {
+	public int rob(int[] num) {
+		int prev3 = 0;
+		int prev2 = 0;
+		int prev1 = 0;
+		for (int number : num) {
+			int current = Math.max(prev3, prev2) + number;
+			prev3 = prev2;
+			prev2 = prev1;
+			prev1 = current;
+		}
+		return Math.max(prev2, prev1);
+	}
+}
