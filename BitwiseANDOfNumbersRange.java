e696b88773c0f407a551237d7e3c079034af25d0
Kai Wang <charles.wangkai@gmail.com>
2015-04-17 09:37:56 +0800

    BitwiseANDOfNumbersRange

---
+++ BitwiseANDOfNumbersRange.java
@@ -0,0 +1,16 @@
+public class BitwiseANDOfNumbersRange {
+	public int rangeBitwiseAnd(int m, int n) {
+		int base = 1;
+		int result = 0;
+		while (n != 0) {
+			if (m == n && (m & 1) != 0) {
+				result += base;
+			}
+
+			m >>= 1;
+			n >>= 1;
+			base <<= 1;
+		}
+		return result;
+	}
+}
