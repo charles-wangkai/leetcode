1230f4a7208b31a332f9add953889f8256ef561f
Kai Wang <charles.wangkai@gmail.com>
2015-04-22 16:43:34 +0800

    HappyNumber

---
+++ HappyNumber.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+	public boolean isHappy(int n) {
+		Set<Integer> history = new HashSet<Integer>();
+		while (!history.contains(n)) {
+			history.add(n);
+			n = computeNext(n);
+		}
+		return n == 1;
+	}
+
+	int computeNext(int number) {
+		int next = 0;
+		while (number != 0) {
+			int digit = number % 10;
+			next += digit * digit;
+			number /= 10;
+		}
+		return next;
+	}
+}
