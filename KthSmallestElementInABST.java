64d82da18a2424cee16f5fd3b5695600b8d3398b
Kai Wang <charles.wangkai@gmail.com>
2015-07-02 18:22:15 +0800

    KthSmallestElementInABST

---
+++ KthSmallestElementInABST.java
@@ -0,0 +1,34 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class KthSmallestElementInABST {
+	static int result;
+	static int sequence;
+
+	public int kthSmallest(TreeNode root, int k) {
+		sequence = 0;
+		search(root, k);
+		return result;
+	}
+
+	void search(TreeNode node, int k) {
+		if (node == null || sequence == k) {
+			return;
+		}
+
+		search(node.left, k);
+		sequence++;
+		if (sequence == k) {
+			result = node.val;
+		}
+		search(node.right, k);
+	}
+}
