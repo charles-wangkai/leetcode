7cc56dce1a640b85703b027cb106521c33b0596f
Kai Wang <charles.wangkai@gmail.com>
2015-08-18 03:47:28 +0800

    P_3SumSmaller

---
+++ P_3SumSmaller.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class P_3SumSmaller {
+	public int threeSumSmaller(int[] nums, int target) {
+		Arrays.sort(nums);
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			int remain = target - nums[i];
+			for (int j = i + 1, k = nums.length - 1; j < k;) {
+				if (nums[j] + nums[k] < remain) {
+					count += k - j;
+					j++;
+				} else {
+					k--;
+				}
+			}
+		}
+		return count;
+	}
+}
