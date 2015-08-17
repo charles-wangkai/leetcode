2341c0bdb15fdfe8664f0610aa825eaa12db8137
Kai Wang <charles.wangkai@gmail.com>
2015-08-18 03:15:53 +0800

    P_3SumClosest

--- P_3SumClosest.java
+++ P_3SumClosest.java
@@ -6,15 +6,7 @@ public class P_3SumClosest {
 		Integer closest = null;
 		for (int i = 0; i < num.length; i++) {
 			int remain = target - num[i];
-			for (int j = 0, k = num.length - 1; j < k;) {
-				if (j == i) {
-					j++;
-					continue;
-				}
-				if (k == i) {
-					k--;
-					continue;
-				}
+			for (int j = i + 1, k = num.length - 1; j < k;) {
 				int sum = num[i] + num[j] + num[k];
 				if (closest == null
 						|| Math.abs(sum - target) < Math.abs(closest - target)) {
