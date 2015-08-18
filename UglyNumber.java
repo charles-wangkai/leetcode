70be8fab3b3e97872271228b88ed8fc3a06b8cf2
Kai Wang <charles.wangkai@gmail.com>
2015-08-19 03:52:22 +0800

    UglyNumber

---
+++ UglyNumber.java
@@ -0,0 +1,14 @@
+public class UglyNumber {
+	public boolean isUgly(int num) {
+		if (num <= 0) {
+			return false;
+		}
+
+		for (int factor : new int[] { 2, 3, 5 }) {
+			while (num % factor == 0) {
+				num /= factor;
+			}
+		}
+		return num == 1;
+	}
+}
