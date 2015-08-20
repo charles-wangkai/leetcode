8efc31df43cb6e3e33e18905407b5f416bc22c42
Kai Wang <charles.wangkai@gmail.com>
2015-08-20 14:55:22 +0800

    PaintHouse

--- PaintHouse.java
+++ PaintHouse.java
@@ -4,9 +4,8 @@ public class PaintHouse {
 		for (int i = 1; i < minCosts.length; i++) {
 			for (int j = 0; j < 3; j++) {
 				minCosts[i][j] = Math.min(minCosts[i - 1][(j + 1) % 3]
-						+ costs[i - 1][(j + 1) % 3],
-						minCosts[i - 1][(j + 2) % 3]
-								+ costs[i - 1][(j + 2) % 3]);
+						+ costs[i - 1][j], minCosts[i - 1][(j + 2) % 3]
+						+ costs[i - 1][j]);
 			}
 		}
 		return Math.min(Math.min(minCosts[minCosts.length - 1][0],
