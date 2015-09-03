0131ffe67a4c27b42aff460299cd55f52f9e461a
Kai Wang <charles.wangkai@gmail.com>
2015-09-04 01:45:48 +0800

    HIndex

---
+++ HIndex.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class HIndex {
+	public int hIndex(int[] citations) {
+		Arrays.sort(citations);
+
+		for (int h = citations.length;; h--) {
+			if ((h == 0 || citations[citations.length - h] >= h)
+					&& (h == citations.length || citations[citations.length - h
+							- 1] <= h)) {
+				return h;
+			}
+		}
+	}
+}
