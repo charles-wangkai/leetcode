045c0908056c94f5d49f51693dcb70c8191033fe
Kai Wang <charles.wangkai@gmail.com>
2015-09-05 17:18:52 +0800

    HIndex_II

---
+++ HIndex_II.java
@@ -0,0 +1,22 @@
+public class HIndex_II {
+	public int hIndex(int[] citations) {
+		int result = -1;
+		int lower = 0;
+		int upper = citations.length;
+		while (lower <= upper) {
+			int middle = (lower + upper) / 2;
+
+			if (isValid(citations, middle)) {
+				lower = middle + 1;
+				result = middle;
+			} else {
+				upper = middle - 1;
+			}
+		}
+		return result;
+	}
+
+	boolean isValid(int[] citations, int h) {
+		return (h == 0 || citations[citations.length - h] >= h);
+	}
+}
