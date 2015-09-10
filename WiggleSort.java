d1cb08a16ccef856a34b570a82836a661f196cf0
Kai Wang <charles.wangkai@gmail.com>
2015-09-10 11:50:12 +0800

    WiggleSort

---
+++ WiggleSort.java
@@ -0,0 +1,13 @@
+public class WiggleSort {
+	public void wiggleSort(int[] nums) {
+		boolean greaterOrEqual = true;
+		for (int i = 1; i < nums.length; i++) {
+			if ((nums[i] >= nums[i - 1]) != greaterOrEqual) {
+				int temp = nums[i];
+				nums[i] = nums[i - 1];
+				nums[i - 1] = temp;
+			}
+			greaterOrEqual = !greaterOrEqual;
+		}
+	}
+}
