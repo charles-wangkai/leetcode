6d535c708f12ab11d9f02e5cc71408286f5868dc
Kai Wang <charles.wangkai@gmail.com>
2015-09-22 15:12:27 +0800

    InorderSuccessorInBST

---
+++ InorderSuccessorInBST.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class InorderSuccessorInBST {
+	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		TreeNode successor;
+		if (p.right == null) {
+			Stack<TreeNode> path = new Stack<TreeNode>();
+			searchPath(root, p, path);
+			TreeNode node = path.pop();
+			while (!path.empty() && path.peek().left != node) {
+				node = path.pop();
+			}
+			successor = path.empty() ? null : path.peek();
+		} else {
+			successor = p.right;
+			while (successor.left != null) {
+				successor = successor.left;
+			}
+		}
+		return successor;
+	}
+
+	void searchPath(TreeNode node, TreeNode p, Stack<TreeNode> path) {
+		path.push(node);
+		if (path.peek() != p && node.left != null) {
+			searchPath(node.left, p, path);
+		}
+		if (path.peek() != p && node.right != null) {
+			searchPath(node.right, p, path);
+		}
+		if (path.peek() != p) {
+			path.pop();
+		}
+	}
+}
