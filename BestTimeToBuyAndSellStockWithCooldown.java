b8fd706fa902ac0bd2b684bfa6cd44ae021366b0
Kai Wang <charles.wangkai@gmail.com>
2015-11-24 12:37:16 +0800

    BestTimeToBuyAndSellStockWithCooldown

---
+++ BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,16 @@
+public class BestTimeToBuyAndSellStockWithCooldown {
+	public int maxProfit(int[] prices) {
+		if (prices.length == 0) {
+			return 0;
+		}
+
+		int[] profits = new int[prices.length];
+		for (int i = 1; i < profits.length; i++) {
+			profits[i] = profits[i - 1];
+			for (int j = 0; j < i; j++) {
+				profits[i] = Math.max(profits[i], (prices[i] - prices[j]) + (j >= 2 ? profits[j - 2] : 0));
+			}
+		}
+		return profits[profits.length - 1];
+	}
+}
