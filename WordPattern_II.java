eaf7bd77f42571e8fa6887df44a0248dfd39f02e
Kai Wang <charles.wangkai@gmail.com>
2015-11-27 20:42:57 +0800

    WordPattern_II

---
+++ WordPattern_II.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class WordPattern_II {
+	public boolean wordPatternMatch(String pattern, String str) {
+		if (pattern.length() > str.length()) {
+			return false;
+		}
+		if (pattern.isEmpty()) {
+			return str.isEmpty();
+		}
+
+		Map<Character, Integer> letter2count = new HashMap<Character, Integer>();
+		for (char letter : pattern.toCharArray()) {
+			if (!letter2count.containsKey(letter)) {
+				letter2count.put(letter, 0);
+			}
+			letter2count.put(letter, letter2count.get(letter) + 1);
+		}
+
+		return isMatched(pattern, str, new ArrayList<Character>(letter2count.keySet()), letter2count,
+				new HashMap<Character, Integer>(), 0, pattern.length(), str.length());
+	}
+
+	boolean isMatched(String pattern, String str, List<Character> letters, Map<Character, Integer> letter2count,
+			Map<Character, Integer> letter2length, int index, int remainPatternLength, int remainStrLength) {
+		if (index == letters.size()) {
+			String[] words = split(pattern, str, letter2length);
+			return isWordsMatched(pattern, words);
+		}
+
+		char letter = letters.get(index);
+		int count = letter2count.get(letter);
+
+		int beginLength;
+		if (index == letters.size() - 1) {
+			if (remainStrLength % remainPatternLength != 0) {
+				return false;
+			}
+
+			beginLength = remainStrLength / remainPatternLength;
+		} else {
+			beginLength = 1;
+		}
+
+		for (int length = beginLength; remainPatternLength - count <= remainStrLength - length * count; length++) {
+			letter2length.put(letter, length);
+
+			if (isMatched(pattern, str, letters, letter2count, letter2length, index + 1, remainPatternLength - count,
+					remainStrLength - length * count)) {
+				return true;
+			}
+
+			letter2length.remove(letter);
+		}
+		return false;
+	}
+
+	String[] split(String pattern, String str, Map<Character, Integer> letter2length) {
+		String[] words = new String[pattern.length()];
+		int beginIndex = 0;
+		for (int i = 0; i < words.length; i++) {
+			int endIndex = beginIndex + letter2length.get(pattern.charAt(i));
+			words[i] = str.substring(beginIndex, endIndex);
+			beginIndex = endIndex;
+		}
+		return words;
+	}
+
+	boolean isWordsMatched(String pattern, String[] words) {
+		Map<Character, String> letter2word = new HashMap<Character, String>();
+		for (int i = 0; i < pattern.length(); i++) {
+			char letter = pattern.charAt(i);
+			String word = words[i];
+			if (letter2word.containsKey(letter)) {
+				if (!letter2word.get(letter).equals(word)) {
+					return false;
+				}
+			} else {
+				letter2word.put(letter, word);
+			}
+		}
+
+		return letter2word.size() == new HashSet<String>(letter2word.values()).size();
+	}
+}
