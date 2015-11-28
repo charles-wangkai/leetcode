505255e4574c7e877408388d82e53b73277118ea
Kai Wang <charles.wangkai@gmail.com>
2015-11-28 15:50:54 +0800

    BestMeetingPoint

---
+++ BestMeetingPoint.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BestMeetingPoint {
+	public int minTotalDistance(int[][] grid) {
+		List<Integer> homeXs = new ArrayList<Integer>();
+		List<Integer> homeYs = new ArrayList<Integer>();
+
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid[x].length; y++) {
+				if (grid[x][y] == 1) {
+					homeXs.add(x);
+					homeYs.add(y);
+				}
+			}
+		}
+
+		return findMinTotalDistance(homeXs) + findMinTotalDistance(homeYs);
+	}
+
+	int findMinTotalDistance(List<Integer> coordinates) {
+		Collections.sort(coordinates);
+
+		int minTotalDistance = 0;
+		for (int i = 0, j = coordinates.size() - 1; i < j; i++, j--) {
+			minTotalDistance += coordinates.get(j) - coordinates.get(i);
+		}
+		return minTotalDistance;
+	}
+}
