ad5b3ca20d562e10bbb56fc6b110e2f099a53411
Kai Wang <charles.wangkai@gmail.com>
2016-03-26 21:14:37 +0800

    HouseRobber_III

---
+++ HouseRobber_III.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class HouseRobber_III {
+	Map<TreeNode, Integer> node2chosenMaxMoney = new HashMap<TreeNode, Integer>();
+	Map<TreeNode, Integer> node2notChosenMaxMoney = new HashMap<TreeNode, Integer>();
+
+	public int rob(TreeNode root) {
+		if (!node2chosenMaxMoney.containsKey(root)) {
+			search(root);
+		}
+
+		return Math.max(node2chosenMaxMoney.get(root), node2notChosenMaxMoney.get(root));
+	}
+
+	void search(TreeNode node) {
+		if (node == null) {
+			node2chosenMaxMoney.put(node, 0);
+			node2notChosenMaxMoney.put(node, 0);
+		} else {
+			node2chosenMaxMoney.put(node,
+					node.val + (node.left == null ? 0 : rob(node.left.left) + rob(node.left.right))
+							+ (node.right == null ? 0 : rob(node.right.left) + rob(node.right.right)));
+			node2notChosenMaxMoney.put(node, rob(node.left) + rob(node.right));
+		}
+	}
+}
