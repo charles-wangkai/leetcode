5d6b099167a3cef4418395832966d567963916f0
Kai Wang <charles.wangkai@gmail.com>
2016-04-03 11:34:43 +0800

    LongestSubstringWithAtMostKDistinctCharacters

---
+++ LongestSubstringWithAtMostKDistinctCharacters.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithAtMostKDistinctCharacters {
+	public int lengthOfLongestSubstringKDistinct(String s, int k) {
+		int maxLength = 0;
+		int back = 0;
+		Map<Character, Integer> letter2count = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			char letter = s.charAt(i);
+			if (!letter2count.containsKey(letter)) {
+				letter2count.put(letter, 0);
+			}
+			letter2count.put(letter, letter2count.get(letter) + 1);
+			while (letter2count.size() > k) {
+				char backLetter = s.charAt(back);
+				letter2count.put(backLetter, letter2count.get(backLetter) - 1);
+				if (letter2count.get(backLetter) == 0) {
+					letter2count.remove(backLetter);
+				}
+				back++;
+			}
+			maxLength = Math.max(maxLength, i - back + 1);
+		}
+		return maxLength;
+	}
+}
