3af9ef8261ef4344edb9c4641afc39cbbfaabed1
Kai Wang <charles.wangkai@gmail.com>
2016-05-18 14:54:35 +0800

    IntersectionOfTwoArrays

---
+++ IntersectionOfTwoArrays.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+	public int[] intersection(int[] nums1, int[] nums2) {
+		Set<Integer> numSet1 = buildSet(nums1);
+		Set<Integer> numSet2 = buildSet(nums2);
+
+		List<Integer> merged = new ArrayList<Integer>();
+		for (int num1 : numSet1) {
+			if (numSet2.contains(num1)) {
+				merged.add(num1);
+			}
+		}
+
+		int[] result = new int[merged.size()];
+		for (int i = 0; i < merged.size(); i++) {
+			result[i] = merged.get(i);
+		}
+		return result;
+	}
+
+	Set<Integer> buildSet(int[] nums) {
+		return Arrays.stream(nums).collect(HashSet::new, Set::add, Set::addAll);
+	}
+}
