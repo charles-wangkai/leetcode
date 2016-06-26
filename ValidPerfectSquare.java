5b29d5f868ef13fa70947e20c93b6820131e6b11
Kai Wang <charles.wangkai@gmail.com>
2016-06-26 16:45:47 +0800

    ValidPerfectSquare

---
+++ ValidPerfectSquare.java
@@ -0,0 +1,18 @@
+public class ValidPerfectSquare {
+	public boolean isPerfectSquare(int num) {
+		int lower = 1;
+		int upper = 1 << 16;
+		while (lower <= upper) {
+			int middle = (lower + upper) / 2;
+			long square = (long) middle * middle;
+			if (num == square) {
+				return true;
+			} else if (num < square) {
+				upper = middle - 1;
+			} else {
+				lower = middle + 1;
+			}
+		}
+		return false;
+	}
+}
