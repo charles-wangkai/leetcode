6e96802ea86993286745e574a568e70459e854b9
Kai Wang <charles.wangkai@gmail.com>
2016-07-21 22:19:18 +0800

    WiggleSubsequence

---
+++ WiggleSubsequence.java
@@ -0,0 +1,20 @@
+public class WiggleSubsequence {
+	public int wiggleMaxLength(int[] nums) {
+		if (nums.length < 2) {
+			return nums.length;
+		}
+
+		return Math.max(search(nums, true), search(nums, false));
+	}
+
+	private int search(int[] nums, boolean upOrDown) {
+		int maxLength = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1] && (nums[i] > nums[i - 1]) == upOrDown) {
+				maxLength++;
+				upOrDown = !upOrDown;
+			}
+		}
+		return maxLength;
+	}
+}
