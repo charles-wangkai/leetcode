a17f42790e6d7090859d4785be83fc2a882cf51a
Kai Wang <charles.wangkai@gmail.com>
2016-09-18 00:43:00 +0800

    EliminationGame

---
+++ EliminationGame.java
@@ -0,0 +1,9 @@
+public class EliminationGame {
+	public int lastRemaining(int n) {
+		return search(n, true);
+	}
+
+	int search(int n, boolean leftOrRight) {
+		return n == 1 ? 1 : search(n / 2, !leftOrRight) * 2 - (!leftOrRight && n % 2 == 0 ? 1 : 0);
+	}
+}
