bf6fd1e36c6916606ab5f0c979d1abb7408c2791
Kai Wang <charles.wangkai@gmail.com>
2016-10-10 14:01:15 +0800

    ThirdMaximumNumber

---
+++ ThirdMaximumNumber.java
@@ -0,0 +1,26 @@
+public class ThirdMaximumNumber {
+	public int thirdMax(int[] nums) {
+		Integer first = null;
+		Integer second = null;
+		Integer third = null;
+
+		for (int num : nums) {
+			if (first == null || num > first) {
+				third = second;
+				second = first;
+				first = num;
+			} else if (num < first) {
+				if (second == null | num > second) {
+					third = second;
+					second = num;
+				} else if (num < second) {
+					if (third == null || num > third) {
+						third = num;
+					}
+				}
+			}
+		}
+
+		return third == null ? first : third;
+	}
+}
