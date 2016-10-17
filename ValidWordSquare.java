70bf761b32dae0442ef6cf13a2d0c863f0ac0947
Kai Wang <charles.wangkai@gmail.com>
2016-10-17 11:10:24 +0800

    ValidWordSquare

---
+++ ValidWordSquare.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class ValidWordSquare {
+	public boolean validWordSquare(List<String> words) {
+		int row = words.size();
+		int col = words.stream().mapToInt(String::length).max().getAsInt();
+
+		if (row != col) {
+			return false;
+		}
+
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < col; c++) {
+				if (getChar(words, r, c) != getChar(words, c, r)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	Character getChar(List<String> words, int r, int c) {
+		String word = words.get(r);
+		return (c < word.length()) ? word.charAt(c) : null;
+	}
+}
