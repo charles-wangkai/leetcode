a92f7f1e86e01d74f68b054cd249a2442767e5cd
Kai Wang <charles.wangkai@gmail.com>
2016-12-19 14:55:20 +0800

    MatchsticksToSquare

---
+++ MatchsticksToSquare.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class MatchsticksToSquare {
+	public boolean makesquare(int[] nums) {
+		Arrays.sort(nums);
+
+		long perimeter = Arrays.stream(nums).asLongStream().sum();
+
+		if (perimeter % 4 != 0) {
+			return false;
+		}
+
+		long size = perimeter / 4;
+		return search(nums, size, new boolean[nums.length], 0, 0, nums.length - 1);
+	}
+
+	boolean search(int[] nums, long size, boolean[] used, int side, int length, int index) {
+		if (side == 3) {
+			return true;
+		}
+		if (index < 0) {
+			return false;
+		}
+
+		if (search(nums, size, used, side, length, index - 1)) {
+			return true;
+		}
+
+		if (!used[index] && length + nums[index] <= size) {
+			int nextSide = side;
+			int nextLength = length + nums[index];
+			int nextIndex = index;
+			if (nextLength == size) {
+				nextSide++;
+				nextLength = 0;
+				nextIndex = nums.length - 1;
+			}
+
+			used[index] = true;
+			if (search(nums, size, used, nextSide, nextLength, nextIndex)) {
+				return true;
+			}
+			used[index] = false;
+		}
+
+		return false;
+	}
+}
