37c2cf7d2309eccc0919d3ca58898ac1d9409c4e
Kai Wang <charles.wangkai@gmail.com>
2016-12-19 13:39:25 +0800

    HammingDistance

---
+++ HammingDistance.java
@@ -0,0 +1,14 @@
+public class HammingDistance {
+	public int hammingDistance(int x, int y) {
+		int distance = 0;
+		while (x != y) {
+			if ((x & 1) != (y & 1)) {
+				distance++;
+			}
+
+			x >>= 1;
+			y >>= 1;
+		}
+		return distance;
+	}
+}
