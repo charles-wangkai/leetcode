c5b5f6166ab3fdfa68e2856b81ca59ae0b5ed501
Kai Wang <charles.wangkai@gmail.com>
2017-01-22 17:54:40 +0800

    ConstructTheRectangle

---
+++ ConstructTheRectangle.java
@@ -0,0 +1,11 @@
+public class ConstructTheRectangle {
+	public int[] constructRectangle(int area) {
+		int[] result = null;
+		for (int W = 1; W * W <= area; W++) {
+			if (area % W == 0) {
+				result = new int[] { area / W, W };
+			}
+		}
+		return result;
+	}
+}
