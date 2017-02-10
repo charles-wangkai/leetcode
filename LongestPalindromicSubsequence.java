7f630ddafe999a7d0c879d2ec7b10e981e8d94dd
Kai Wang <charles.wangkai@gmail.com>
2017-02-10 18:04:54 +0800

    LongestPalindromicSubsequence

---
+++ LongestPalindromicSubsequence.java
@@ -0,0 +1,23 @@
+public class LongestPalindromicSubsequence {
+	public int longestPalindromeSubseq(String s) {
+		int length = s.length();
+
+		int[][] maxLengths = new int[length][length + 1];
+		for (int i = 0; i < length; i++) {
+			maxLengths[i][i + 1] = 1;
+		}
+
+		for (int i = 2; i <= length; i++) {
+			for (int j = 0; j + i <= length; j++) {
+				if (s.charAt(j) == s.charAt(j + i - 1)) {
+					maxLengths[j][j + i] = maxLengths[j + 1][j + i - 1] + 2;
+				} else {
+					maxLengths[j][j + i] = Math.max(Math.max(maxLengths[j + 1][j + i], maxLengths[j][j + i - 1]),
+							maxLengths[j + 1][j + i - 1]);
+				}
+			}
+		}
+
+		return maxLengths[0][length];
+	}
+}
