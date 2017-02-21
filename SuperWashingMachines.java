c7ed9e2bfd342288f98a79f55e2b5a66f2f5912c
Kai Wang <charles.wangkai@gmail.com>
2017-02-21 17:25:37 +0800

    SuperWashingMachines

---
+++ SuperWashingMachines.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class SuperWashingMachines {
+	public int findMinMoves(int[] machines) {
+		int total = Arrays.stream(machines).sum();
+		if (total % machines.length != 0) {
+			return -1;
+		}
+
+		int average = total / machines.length;
+		int minMove = 0;
+		int accumulation = 0;
+		for (int machine : machines) {
+			int delta = machine - average;
+			accumulation += delta;
+			minMove = Math.max(minMove, Math.max(Math.abs(accumulation), delta));
+		}
+		return minMove;
+	}
+}
