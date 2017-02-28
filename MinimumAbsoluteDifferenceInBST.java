e354f5ea03276469081cf5a77e41be6e97ab236c
Kai Wang <charles.wangkai@gmail.com>
2017-02-28 11:04:34 +0800

    MinimumAbsoluteDifferenceInBST

---
+++ MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,39 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class MinimumAbsoluteDifferenceInBST {
+	Integer prevValue;
+	int minDiff;
+
+	public int getMinimumDifference(TreeNode root) {
+		prevValue = null;
+		minDiff = Integer.MAX_VALUE;
+
+		search(root);
+
+		return minDiff;
+	}
+
+	void search(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		search(node.left);
+
+		if (prevValue != null) {
+			minDiff = Math.min(minDiff, node.val - prevValue);
+		}
+		prevValue = node.val;
+
+		search(node.right);
+	}
+}
