2e303e85bb42c00b6af5e80d364a1bd86cc405a8
Kai Wang <charles.wangkai@gmail.com>
2017-04-12 12:11:53 +0800

    BrickWall

---
+++ BrickWall.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BrickWall {
+	public int leastBricks(List<List<Integer>> wall) {
+		Map<Integer, Integer> length2count = new HashMap<Integer, Integer>();
+		for (List<Integer> row : wall) {
+			int length = 0;
+			for (int i = 0; i < row.size() - 1; i++) {
+				length += row.get(i);
+
+				if (!length2count.containsKey(length)) {
+					length2count.put(length, 0);
+				}
+				length2count.put(length, length2count.get(length) + 1);
+			}
+		}
+
+		return wall.size()
+				- (length2count.isEmpty() ? 0 : length2count.values().stream().mapToInt(x -> x).max().getAsInt());
+	}
+}
