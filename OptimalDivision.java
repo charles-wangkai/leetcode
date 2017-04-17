2de254b3e1d4fe0b5444e7215fac4606f86f2d01
Kai Wang <charles.wangkai@gmail.com>
2017-04-17 15:20:23 +0800

    OptimalDivision

---
+++ OptimalDivision.java
@@ -0,0 +1,19 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class OptimalDivision {
+	public String optimalDivision(int[] nums) {
+		if (nums.length == 1) {
+			return String.valueOf(nums[0]);
+		} else if (nums.length == 2) {
+			return toDivisions(nums, 0, 1);
+		} else {
+			return String.format("%s/(%s)", nums[0], toDivisions(nums, 1, nums.length - 1));
+		}
+	}
+
+	String toDivisions(int[] nums, int beginIndex, int endIndex) {
+		return String.join("/", IntStream.rangeClosed(beginIndex, endIndex).mapToObj(i -> String.valueOf(nums[i]))
+				.collect(Collectors.toList()));
+	}
+}
