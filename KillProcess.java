ebb74c6bbaa0e71c0ce5bd866fdc4f30f651f95b
Kai Wang <charles.wangkai@gmail.com>
2017-05-15 11:15:52 +0800

    KillProcess

---
+++ KillProcess.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KillProcess {
+	public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+		Map<Integer, List<Integer>> ppid2pids = new HashMap<Integer, List<Integer>>();
+		for (int i = 0; i < pid.size(); i++) {
+			int onePpid = ppid.get(i);
+
+			if (!ppid2pids.containsKey(onePpid)) {
+				ppid2pids.put(onePpid, new ArrayList<Integer>());
+			}
+			ppid2pids.get(onePpid).add(pid.get(i));
+		}
+
+		List<Integer> killed = new ArrayList<Integer>();
+		search(killed, ppid2pids, kill);
+		return killed;
+	}
+
+	void search(List<Integer> killed, Map<Integer, List<Integer>> ppid2pids, int ppid) {
+		killed.add(ppid);
+
+		if (ppid2pids.containsKey(ppid)) {
+			for (int pid : ppid2pids.get(ppid)) {
+				search(killed, ppid2pids, pid);
+			}
+		}
+	}
+}
