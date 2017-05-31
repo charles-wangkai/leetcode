e6e44e69b156de3417d683ce401ec96b03fc2aaa
Kai Wang <charles.wangkai@gmail.com>
2017-05-31 13:19:34 +0800

    RangeAddition_II

---
+++ RangeAddition_II.java
@@ -0,0 +1,11 @@
+public class RangeAddition_II {
+	public int maxCount(int m, int n, int[][] ops) {
+		int minR = m;
+		int minC = n;
+		for (int[] op : ops) {
+			minR = Math.min(minR, op[0]);
+			minC = Math.min(minC, op[1]);
+		}
+		return minR * minC;
+	}
+}
