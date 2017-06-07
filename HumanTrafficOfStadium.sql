be0ab054114eeb25a4ba7cc823322da19b691425
Kai Wang <charles.wangkai@gmail.com>
2017-06-08 01:26:52 +0800

    HumanTrafficOfStadium

---
+++ HumanTrafficOfStadium.sql
@@ -0,0 +1,13 @@
+SELECT id, date, people
+  FROM stadium s1
+ WHERE people >= 100
+   AND (
+        (EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id - 2)
+     AND EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id - 1))
+    OR
+        (EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id - 1)
+     AND EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id + 1))
+    OR
+        (EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id + 1)
+     AND EXISTS (SELECT 1 FROM stadium s2 WHERE s2.people >= 100 AND s2.id = s1.id + 2))
+   )
