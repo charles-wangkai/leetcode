4f67267c5b5db36d25bfd9b336a2806e4863661c
Kai Wang <charles.wangkai@gmail.com>
2017-07-25 12:24:03 +0800

    SetMismatch

---
+++ SetMismatch.java
@@ -0,0 +1,18 @@
+public class SetMismatch {
+	public int[] findErrorNums(int[] nums) {
+		int[] counts = new int[nums.length + 1];
+		for (int num : nums) {
+			counts[num]++;
+		}
+
+		int[] result = new int[2];
+		for (int i = 1; i < counts.length; i++) {
+			if (counts[i] == 2) {
+				result[0] = i;
+			} else if (counts[i] == 0) {
+				result[1] = i;
+			}
+		}
+		return result;
+	}
+}
