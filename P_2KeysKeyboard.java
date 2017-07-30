6929ed960ed2d962c9d0f2b0ec674db6251ef966
Kai Wang <charles.wangkai@gmail.com>
2017-07-30 16:48:49 +0800

    P_2KeysKeyboard

---
+++ P_2KeysKeyboard.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class P_2KeysKeyboard {
+	public int minSteps(int n) {
+		int[] steps = new int[n + 1];
+		Arrays.fill(steps, Integer.MAX_VALUE);
+		steps[1] = 0;
+		for (int i = 1; i < steps.length; i++) {
+			int step = steps[i] + 1;
+			for (int j = i + i; j < steps.length; j += i) {
+				step++;
+				steps[j] = Math.min(steps[j], step);
+			}
+		}
+		return steps[n];
+	}
+}
