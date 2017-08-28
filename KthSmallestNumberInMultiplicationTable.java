87187693fe5e48a278232233abda5ee0f4c2c76a
Kai Wang <charles.wangkai@gmail.com>
2017-08-28 12:58:35 +0800

    KthSmallestNumberInMultiplicationTable

---
+++ KthSmallestNumberInMultiplicationTable.java
@@ -0,0 +1,27 @@
+public class KthSmallestNumberInMultiplicationTable {
+	public int findKthNumber(int m, int n, int k) {
+		int lower = 1;
+		int upper = m * n;
+		int result = -1;
+		while (lower <= upper) {
+			int middle = (lower + upper) / 2;
+			int notGreaterNum = countNotGreaterNum(m, n, middle);
+			if (notGreaterNum >= k) {
+				result = middle;
+
+				upper = middle - 1;
+			} else {
+				lower = middle + 1;
+			}
+		}
+		return result;
+	}
+
+	int countNotGreaterNum(int m, int n, int value) {
+		int notGreaterNum = 0;
+		for (int i = 1; i <= m; i++) {
+			notGreaterNum += Math.min(n, value / i);
+		}
+		return notGreaterNum;
+	}
+}
