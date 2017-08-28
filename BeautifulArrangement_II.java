64970e35a63673e9a8cb8e56bba070e7d84ff155
Kai Wang <charles.wangkai@gmail.com>
2017-08-28 11:56:03 +0800

    BeautifulArrangement_II

---
+++ BeautifulArrangement_II.java
@@ -0,0 +1,21 @@
+public class BeautifulArrangement_II {
+	public int[] constructArray(int n, int k) {
+		int[] result = new int[n];
+		int index = 0;
+		for (int i = 1; i <= n - k; i++) {
+			result[index] = i;
+			index++;
+		}
+
+		int number = n - k;
+		int sign = 1;
+		for (int step = k; step >= 1; step--) {
+			number += step * sign;
+			result[index] = number;
+			index++;
+
+			sign *= -1;
+		}
+		return result;
+	}
+}
