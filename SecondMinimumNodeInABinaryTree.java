ba6bf1b54a0c052df99511ad991a6e1f978d2aa5
Kai Wang <charles.wangkai@gmail.com>
2017-09-04 10:53:29 +0800

    SecondMinimumNodeInABinaryTree

---
+++ SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,29 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class SecondMinimumNodeInABinaryTree {
+	public int findSecondMinimumValue(TreeNode root) {
+		int result = search(root.val, root);
+		return (result == Integer.MAX_VALUE) ? -1 : result;
+	}
+
+	int search(int minValue, TreeNode node) {
+		if (node == null) {
+			return Integer.MAX_VALUE;
+		}
+
+		if (node.val != minValue) {
+			return node.val;
+		}
+
+		return Math.min(search(minValue, node.left), search(minValue, node.right));
+	}
+}
