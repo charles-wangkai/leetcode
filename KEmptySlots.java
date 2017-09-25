b81e037fc2d34c06739c5bdfd10858294e6665c5
Kai Wang <charles.wangkai@gmail.com>
2017-09-25 13:22:49 +0800

    KEmptySlots

---
+++ KEmptySlots.java
@@ -0,0 +1,22 @@
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class KEmptySlots {
+	public int kEmptySlots(int[] flowers, int k) {
+		NavigableSet<Integer> slots = new TreeSet<Integer>();
+		for (int i = 0; i < flowers.length; i++) {
+			Integer lowerSlot = slots.lower(flowers[i]);
+			if (lowerSlot != null && flowers[i] - lowerSlot - 1 == k) {
+				return i + 1;
+			}
+
+			Integer higherSlot = slots.higher(flowers[i]);
+			if (higherSlot != null && higherSlot - flowers[i] - 1 == k) {
+				return i + 1;
+			}
+
+			slots.add(flowers[i]);
+		}
+		return -1;
+	}
+}
