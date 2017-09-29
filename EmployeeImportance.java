77aa3804e1d2ed110e31a764d586e4684a6dda96
Kai Wang <charles.wangkai@gmail.com>
2017-09-29 14:16:09 +0800

    EmployeeImportance

---
+++ EmployeeImportance.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+// Employee info
+class Employee {
+	// It's the unique id of each node;
+	// unique id of this employee
+	public int id;
+	// the importance value of this employee
+	public int importance;
+	// the id of direct subordinates
+	public List<Integer> subordinates;
+};
+
+public class EmployeeImportance {
+	public int getImportance(List<Employee> employees, int id) {
+		Map<Integer, Employee> id2employee = employees.stream()
+				.collect(Collectors.toMap(employee -> employee.id, Function.identity()));
+
+		return search(id2employee, id);
+	}
+
+	int search(Map<Integer, Employee> id2employee, int id) {
+		return id2employee.get(id).importance + id2employee.get(id).subordinates.stream()
+				.mapToInt(subordinate -> search(id2employee, subordinate)).sum();
+	}
+}
