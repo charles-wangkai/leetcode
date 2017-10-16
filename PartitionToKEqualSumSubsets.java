d14952f2c30797418d4d25806f27d021777c0b70
Kai Wang <charles.wangkai@gmail.com>
2017-10-16 14:04:47 +0800

    PartitionToKEqualSumSubsets

---
+++ PartitionToKEqualSumSubsets.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class PartitionToKEqualSumSubsets {
+	public boolean canPartitionKSubsets(int[] nums, int k) {
+		int sum = Arrays.stream(nums).sum();
+		if (sum % k != 0) {
+			return false;
+		}
+
+		int subsetSum = sum / k;
+		if (Arrays.stream(nums).anyMatch(num -> num > subsetSum)) {
+			return false;
+		}
+
+		Arrays.sort(nums);
+		return search(nums, subsetSum, new boolean[nums.length], nums.length - 1, k, subsetSum);
+	}
+
+	boolean search(int[] nums, int subsetSum, boolean[] used, int startIndex, int remainSubset, int remainSum) {
+		if (startIndex < 0) {
+			return false;
+		}
+		if (remainSubset == 0) {
+			return true;
+		}
+
+		for (int i = startIndex; i >= 0; i--) {
+			if (!used[i] && nums[i] <= remainSum) {
+				used[i] = true;
+
+				if (nums[i] == remainSum) {
+					if (search(nums, subsetSum, used, nums.length - 1, remainSubset - 1, subsetSum)) {
+						return true;
+					}
+				} else {
+					if (search(nums, subsetSum, used, i - 1, remainSubset, remainSum - nums[i])) {
+						return true;
+					}
+				}
+
+				used[i] = false;
+			}
+		}
+		return false;
+	}
+}
