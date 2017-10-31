e2511d40f5be1bc847d0ba066404df2d45d27cf4
Kai Wang <charles.wangkai@gmail.com>
2017-11-01 01:37:26 +0800

    SubarrayProductLessThanK

---
+++ SubarrayProductLessThanK.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SubarrayProductLessThanK {
+	public int numSubarrayProductLessThanK(int[] nums, int k) {
+		if (k <= 1) {
+			return 0;
+		}
+
+		nums = Arrays.copyOf(nums, nums.length + 1);
+		nums[nums.length - 1] = k;
+		long product = 1;
+		int endIndex = -1;
+		int result = 0;
+		for (int beginIndex = 0; beginIndex < nums.length; beginIndex++) {
+			while (product < k) {
+				endIndex++;
+				product *= nums[endIndex];
+			}
+
+			result += endIndex - beginIndex;
+			product /= nums[beginIndex];
+		}
+		return result;
+	}
+}
