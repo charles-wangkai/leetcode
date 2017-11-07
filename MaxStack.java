3526a15f2c3ae7f96818623eb87bfcd2f14017cd
Kai Wang <charles.wangkai@gmail.com>
2017-11-07 10:58:49 +0800

    MaxStack

---
+++ MaxStack.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxStack {
+	/** initialize your data structure here. */
+	private List<Integer> values;
+
+	public MaxStack() {
+		values = new ArrayList<Integer>();
+	}
+
+	public void push(int x) {
+		values.add(x);
+	}
+
+	public int pop() {
+		return values.remove(values.size() - 1);
+	}
+
+	public int top() {
+		return values.get(values.size() - 1);
+	}
+
+	public int peekMax() {
+		return values.stream().mapToInt(x -> x).max().getAsInt();
+	}
+
+	public int popMax() {
+		int index = 0;
+		for (int i = 0; i < values.size(); i++) {
+			if (values.get(i) >= values.get(index)) {
+				index = i;
+			}
+		}
+		return values.remove(index);
+	}
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such: MaxStack obj =
+ * new MaxStack(); obj.push(x); int param_2 = obj.pop(); int param_3 =
+ * obj.top(); int param_4 = obj.peekMax(); int param_5 = obj.popMax();
+ */
