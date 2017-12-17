a4950471ae545151b61ed386a90c2d5210e7629e
Kai Wang <charles.wangkai@gmail.com>
2017-12-18 00:47:23 +0800

    ShortestCompletingWord

---
+++ ShortestCompletingWord.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShortestCompletingWord {
+	public String shortestCompletingWord(String licensePlate, String[] words) {
+		Map<Character, Integer> targetLetter2count = computeLetter2count(licensePlate);
+
+		String result = null;
+		for (String word : words) {
+			Map<Character, Integer> letter2count = computeLetter2count(word);
+			if (targetLetter2count.keySet().stream()
+					.allMatch(targetLetter -> letter2count.containsKey(targetLetter)
+							&& letter2count.get(targetLetter) >= targetLetter2count.get(targetLetter))
+					&& (result == null || word.length() < result.length())) {
+				result = word;
+			}
+		}
+		return result;
+	}
+
+	Map<Character, Integer> computeLetter2count(String s) {
+		Map<Character, Integer> letter2count = new HashMap<Character, Integer>();
+		for (char ch : s.toLowerCase().toCharArray()) {
+			if (Character.isLetter(ch)) {
+				letter2count.put(ch, letter2count.getOrDefault(ch, 0) + 1);
+			}
+		}
+		return letter2count;
+	}
+}
