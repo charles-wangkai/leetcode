18d8a95101be088bc3af2d50c4459bbe244fc60e
Kai Wang <charles.wangkai@gmail.com>
2017-12-18 13:10:33 +0800

    MinCostClimbingStairs

---
+++ MinCostClimbingStairs.java
@@ -0,0 +1,13 @@
+public class MinCostClimbingStairs {
+	public int minCostClimbingStairs(int[] cost) {
+		int prev = 0;
+		int prevprev = 0;
+		for (int i = 2; i <= cost.length; i++) {
+			int curr = Math.min(prev + cost[i - 1], prevprev + cost[i - 2]);
+
+			prevprev = prev;
+			prev = curr;
+		}
+		return prev;
+	}
+}
