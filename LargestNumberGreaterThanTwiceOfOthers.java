1fb4c4ddfc0b3ae4300806491fcc1eb351bb1000
Kai Wang <charles.wangkai@gmail.com>
2017-12-24 17:33:37 +0800

    LargestNumberGreaterThanTwiceOfOthers

---
+++ LargestNumberGreaterThanTwiceOfOthers.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class LargestNumberGreaterThanTwiceOfOthers {
+	public int dominantIndex(int[] nums) {
+		int largest = Arrays.stream(nums).max().getAsInt();
+		if (Arrays.stream(nums).allMatch(num -> num == largest || num * 2 <= largest)) {
+			return IntStream.range(0, nums.length).filter(i -> nums[i] == largest).toArray()[0];
+		} else {
+			return -1;
+		}
+	}
+}
