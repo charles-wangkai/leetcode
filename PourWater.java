2f795c8882af23265091677f489020ee925258f2
Kai Wang <charles.wangkai@gmail.com>
2018-01-01 01:37:23 +0800

    PourWater

---
+++ PourWater.java
@@ -0,0 +1,26 @@
+public class PourWater {
+	public int[] pourWater(int[] heights, int V, int K) {
+		for (int i = 0; i < V; i++) {
+			int minIndex = findMinIndex(heights, K, -1);
+			if (minIndex != K) {
+				heights[minIndex]++;
+				continue;
+			}
+
+			minIndex = findMinIndex(heights, K, 1);
+			heights[minIndex]++;
+		}
+		return heights;
+	}
+
+	int findMinIndex(int[] heights, int startIndex, int offset) {
+		int minIndex = startIndex;
+		for (int index = startIndex + offset; index >= 0 && index < heights.length
+				&& heights[index] <= heights[index - offset]; index += offset) {
+			if (heights[index] < heights[minIndex]) {
+				minIndex = index;
+			}
+		}
+		return minIndex;
+	}
+}
