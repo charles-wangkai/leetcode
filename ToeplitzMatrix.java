33298f7070f0408db1204b8bded6a15c38b1f976
Kai Wang <charles.wangkai@gmail.com>
2018-01-22 10:51:31 +0800

    ToeplitzMatrix

---
+++ ToeplitzMatrix.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+
+public class ToeplitzMatrix {
+	public boolean isToeplitzMatrix(int[][] matrix) {
+		return IntStream.range(0, Math.min(matrix.length, matrix[0].length))
+				.allMatch(i -> matrix[i][i] == matrix[0][0]);
+	}
+}
