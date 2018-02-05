beeb0649daef707fc2e02cd4a3d8f5fde4511954
Kai Wang <charles.wangkai@gmail.com>
2018-02-05 10:20:25 +0800

    KthSymbolInGrammar

---
+++ KthSymbolInGrammar.java
@@ -0,0 +1,26 @@
+public class KthSymbolInGrammar {
+	public int kthGrammar(int N, int K) {
+		return search(K);
+	}
+
+	int search(int K) {
+		if (K == 1) {
+			return 0;
+		}
+
+		int prev = search((K - 1) / 2 + 1);
+		if (prev == 0) {
+			if (K % 2 != 0) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} else {
+			if (K % 2 != 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+}
