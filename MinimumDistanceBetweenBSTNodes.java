a7b220a7d80e15af012fc782528674504b8541e4
Kai Wang <charles.wangkai@gmail.com>
2018-02-11 22:24:55 +0800

    MinimumDistanceBetweenBSTNodes

---
+++ MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class MinimumDistanceBetweenBSTNodes {
+	public int minDiffInBST(TreeNode root) {
+		List<Integer> values = new ArrayList<Integer>();
+		search(root, values);
+		return IntStream.range(0, values.size() - 1).map(i -> values.get(i + 1) - values.get(i)).min().getAsInt();
+	}
+
+	void search(TreeNode node, List<Integer> values) {
+		if (node != null) {
+			search(node.left, values);
+			values.add(node.val);
+			search(node.right, values);
+		}
+	}
+}
