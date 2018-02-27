7e118ee43fa370b90c6453b28f244f02fce8b8e2
Kai Wang <charles.wangkai@gmail.com>
2018-02-27 14:18:25 +0800

    EscapeTheGhosts

---
+++ EscapeTheGhosts.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class EscapeTheGhosts {
+	public boolean escapeGhosts(int[][] ghosts, int[] target) {
+		return Arrays.stream(ghosts)
+				.allMatch(ghost -> computeDistance(ghost, target) > computeDistance(new int[] { 0, 0 }, target));
+	}
+
+	int computeDistance(int[] p1, int[] p2) {
+		return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+	}
+}
