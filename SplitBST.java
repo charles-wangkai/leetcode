a589661fba98c2452efe0fc41db1eb53912187c7
Kai Wang <charles.wangkai@gmail.com>
2018-03-19 22:54:08 +0800

    SplitBST

---
+++ SplitBST.java
@@ -0,0 +1,30 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class SplitBST {
+	public TreeNode[] splitBST(TreeNode root, int V) {
+		if (root == null) {
+			return new TreeNode[2];
+		}
+
+		if (root.val <= V) {
+			TreeNode[] rightSubResult = splitBST(root.right, V);
+			root.right = rightSubResult[0];
+
+			return new TreeNode[] { root, rightSubResult[1] };
+		} else {
+			TreeNode[] leftSubResult = splitBST(root.left, V);
+			root.left = leftSubResult[1];
+
+			return new TreeNode[] { leftSubResult[0], root };
+		}
+	}
+}
