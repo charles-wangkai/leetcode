16b36fd171562c2b75cc86ef8d028e357dfb3d4b
Kai Wang <charles.wangkai@gmail.com>
2018-04-22 13:52:13 +0800

    CardFlippingGame

---
+++ CardFlippingGame.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class CardFlippingGame {
+	public int flipgame(int[] fronts, int[] backs) {
+		Set<Integer> impossibles = new HashSet<Integer>();
+		SortedSet<Integer> numbers = new TreeSet<Integer>();
+		for (int i = 0; i < fronts.length; i++) {
+			numbers.add(fronts[i]);
+			numbers.add(backs[i]);
+
+			if (fronts[i] == backs[i]) {
+				impossibles.add(fronts[i]);
+			}
+		}
+
+		for (int number : numbers) {
+			if (!impossibles.contains(number)) {
+				return number;
+			}
+		}
+		return 0;
+	}
+}
