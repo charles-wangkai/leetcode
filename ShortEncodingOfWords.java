bf3db0fc10b091de17f6d46dcd28c1d712b4be48
Kai Wang <charles.wangkai@gmail.com>
2018-04-22 13:21:17 +0800

    ShortEncodingOfWords

---
+++ ShortEncodingOfWords.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShortEncodingOfWords {
+	public int minimumLengthEncoding(String[] words) {
+		Arrays.sort(words, (word1, word2) -> Integer.compare(word2.length(), word1.length()));
+
+		Set<String> suffixes = new HashSet<String>();
+		int result = 0;
+		for (String word : words) {
+			if (!suffixes.contains(word)) {
+				result += word.length() + 1;
+
+				for (int i = 0; i < word.length(); i++) {
+					suffixes.add(word.substring(i));
+				}
+			}
+		}
+		return result;
+	}
+}
