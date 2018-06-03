6d6041b15ee304f22658797fd8efd124a57bfddb
Kai Wang <charles.wangkai@gmail.com>
2018-06-03 17:14:34 +0800

    KeysAndRooms

---
+++ KeysAndRooms.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class KeysAndRooms {
+	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		int N = rooms.size();
+		boolean[] visited = new boolean[N];
+		visited[0] = true;
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.offer(0);
+		int remain = N - 1;
+
+		while (remain > 0 && !queue.isEmpty()) {
+			int room = queue.poll();
+
+			for (int nextRoom : rooms.get(room)) {
+				if (!visited[nextRoom]) {
+					visited[nextRoom] = true;
+					queue.offer(nextRoom);
+					remain--;
+				}
+			}
+		}
+
+		return remain == 0;
+	}
+}
