be0247e0cffdb9236d5fa8bc0736d8b587d7e5d3
Kai Wang <charles.wangkai@gmail.com>
2018-06-03 17:38:17 +0800

    HandOfStraights

---
+++ HandOfStraights.java
@@ -0,0 +1,31 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class HandOfStraights {
+	public boolean isNStraightHand(int[] hand, int W) {
+		if (hand.length % W != 0) {
+			return false;
+		}
+
+		SortedMap<Integer, Integer> numberToCount = new TreeMap<Integer, Integer>();
+		for (int number : hand) {
+			numberToCount.put(number, numberToCount.getOrDefault(number, 0) + 1);
+		}
+
+		while (!numberToCount.isEmpty()) {
+			int minNumber = numberToCount.firstKey();
+			for (int number = minNumber; number < minNumber + W; number++) {
+				if (!numberToCount.containsKey(number)) {
+					return false;
+				}
+
+				numberToCount.put(number, numberToCount.get(number) - 1);
+				if (numberToCount.get(number) == 0) {
+					numberToCount.remove(number);
+				}
+			}
+		}
+
+		return true;
+	}
+}
