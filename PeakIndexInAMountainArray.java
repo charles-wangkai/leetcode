65314e71817aeb363e8b0116f3acfbf753ad8f14
Kai Wang <charles.wangkai@gmail.com>
2018-06-19 11:54:41 +0800

    PeakIndexInAMountainArray

---
+++ PeakIndexInAMountainArray.java
@@ -0,0 +1,16 @@
+public class PeakIndexInAMountainArray {
+	public int peakIndexInMountainArray(int[] A) {
+		int lowerIndex = 0;
+		int upperIndex = A.length - 1;
+		while (true) {
+			int middleIndex = (lowerIndex + upperIndex) / 2;
+			if (middleIndex < A.length - 1 && A[middleIndex] < A[middleIndex + 1]) {
+				lowerIndex = middleIndex + 1;
+			} else if (middleIndex > 0 && A[middleIndex - 1] > A[middleIndex]) {
+				upperIndex = middleIndex - 1;
+			} else {
+				return middleIndex;
+			}
+		}
+	}
+}
