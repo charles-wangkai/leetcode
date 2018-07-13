9154d3feae731f95f341f01a551a77edb1cc1a34
Kai Wang <charles.wangkai@gmail.com>
2018-07-13 13:28:09 +0800

    Kth Largest Element in a Stream

---
+++ kth-largest-element-in-a-stream/KthLargest.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KthLargest {
+	private PriorityQueue<Integer> pq = new PriorityQueue<>();
+	private int k;
+
+	public KthLargest(int k, int[] nums) {
+		this.k = k;
+
+		Arrays.stream(nums).forEach(this::add);
+	}
+
+	public int add(int val) {
+		if (pq.size() < k || val > pq.peek()) {
+			pq.offer(val);
+		}
+
+		if (pq.size() == k + 1) {
+			pq.poll();
+		}
+
+		return pq.peek();
+	}
+}
+
+// Your KthLargest object will be instantiated and called as such:
+// KthLargest obj = new KthLargest(k, nums);
+// int param_1 = obj.add(val);
