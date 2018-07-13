46fc0a4b2e2d69f9410c28777ed8c6e348244896
Kai Wang <charles.wangkai@gmail.com>
2018-07-13 12:40:16 +0800

    Search in a Binary Search Tree

---
+++ search-in-a-binary-search-tree/Solution.java
@@ -0,0 +1,25 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public TreeNode searchBST(TreeNode root, int val) {
+		if (root == null) {
+			return null;
+		}
+		if (root.val == val) {
+			return root;
+		} else if (root.val < val) {
+			return searchBST(root.right, val);
+		} else {
+			return searchBST(root.left, val);
+		}
+	}
+}
