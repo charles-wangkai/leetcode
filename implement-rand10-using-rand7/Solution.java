2acf25b5ada87e278a43ce6199c3311a79b0e5df
Kai Wang <charles.wangkai@gmail.com>
2018-07-16 12:12:57 +0800

    Implement Rand10() Using Rand7()

---
+++ implement-rand10-using-rand7/Solution.java
@@ -0,0 +1,18 @@
+class SolBase {
+	// return a random integer in the range 1 to 7
+	int rand7() {
+		throw new UnsupportedOperationException();
+	}
+}
+
+public class Solution extends SolBase {
+	public int rand10() {
+		while (true) {
+			int r = (rand7() - 1) * 7 + (rand7() - 1);
+
+			if (r < 40) {
+				return r % 10 + 1;
+			}
+		}
+	}
+}
