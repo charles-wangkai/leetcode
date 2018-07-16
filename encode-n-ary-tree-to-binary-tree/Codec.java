66bf2ed133f7dc576d489b679ef876f3179abbd7
Kai Wang <charles.wangkai@gmail.com>
2018-07-16 17:58:48 +0800

    Encode N-ary Tree to Binary Tree

---
+++ encode-n-ary-tree-to-binary-tree/Codec.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.List;
+
+// Definition for a Node.
+class Node {
+	public int val;
+	public List<Node> children;
+
+	public Node() {
+	}
+
+	public Node(int _val, List<Node> _children) {
+		val = _val;
+		children = _children;
+	}
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Codec {
+	// Encodes an n-ary tree to a binary tree.
+	public TreeNode encode(Node root) {
+		if (root == null) {
+			return null;
+		}
+
+		TreeNode rootTreeNode = new TreeNode(root.val);
+		for (Node child : root.children) {
+			TreeNode childTreeNode = encode(child);
+
+			childTreeNode.right = rootTreeNode.left;
+			rootTreeNode.left = childTreeNode;
+		}
+		return rootTreeNode;
+	}
+
+	// Decodes your binary tree to an n-ary tree.
+	public Node decode(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		Node rootNode = new Node(root.val, new LinkedList<>());
+		for (TreeNode child = root.left; child != null; child = child.right) {
+			rootNode.children.add(0, decode(child));
+		}
+		return rootNode;
+	}
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(root));
