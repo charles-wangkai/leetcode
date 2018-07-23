d97772a1118ef8a2d257543b619d0e52462eb4d9
Kai Wang <charles.wangkai@gmail.com>
2018-07-23 12:26:34 +0800

    Walking Robot Simulation

---
+++ walking-robot-simulation/Solution.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solution {
+	static final int[] X_OFFSETS = { 0, 1, 0, -1 };
+	static final int[] Y_OFFSETS = { 1, 0, -1, 0 };
+
+	public int robotSim(int[] commands, int[][] obstacles) {
+		Set<Point> obstacleSet = Arrays.stream(obstacles).map(obstacle -> new Point(obstacle[0], obstacle[1]))
+				.collect(Collectors.toSet());
+
+		int result = 0;
+		int x = 0;
+		int y = 0;
+		int direction = 0;
+		for (int command : commands) {
+			if (command == -2) {
+				direction = (direction - 1 + X_OFFSETS.length) % X_OFFSETS.length;
+			} else if (command == -1) {
+				direction = (direction + 1) % X_OFFSETS.length;
+			} else {
+				for (int i = 0; i < command; i++) {
+					int nextX = x + X_OFFSETS[direction];
+					int nextY = y + Y_OFFSETS[direction];
+
+					if (obstacleSet.contains(new Point(nextX, nextY))) {
+						break;
+					}
+
+					x = nextX;
+					y = nextY;
+				}
+
+				result = Math.max(result, x * x + y * y);
+			}
+		}
+		return result;
+	}
+}
+
+class Point {
+	int x;
+	int y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Point other = (Point) obj;
+		return x == other.x && y == other.y;
+	}
+}
