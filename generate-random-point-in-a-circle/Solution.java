1fdf648ae022d2b98513e383373550b647ffeb20
Kai Wang <charles.wangkai@gmail.com>
2018-07-27 15:09:19 +0800

    Generate Random Point in a Circle

---
+++ generate-random-point-in-a-circle/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+	private double radius;
+	private double xCenter;
+	private double yCenter;
+
+	public Solution(double radius, double x_center, double y_center) {
+		this.radius = radius;
+		xCenter = x_center;
+		yCenter = y_center;
+	}
+
+	public double[] randPoint() {
+		while (true) {
+			double x = xCenter - radius + Math.random() * (radius * 2);
+			double y = yCenter - radius + Math.random() * (radius * 2);
+
+			if (computeDistance(x, y, xCenter, yCenter) <= radius) {
+				return new double[] { x, y };
+			}
+		}
+	}
+
+	private double computeDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	}
+}
+
+// Your Solution object will be instantiated and called as such:
+// Solution obj = new Solution(radius, x_center, y_center);
+// double[] param_1 = obj.randPoint();
