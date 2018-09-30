efdeb4b5c0edec6173f781a2f7d5f0bdc457d5b6
Kai Wang <charles.wangkai@gmail.com>
2018-09-30 14:45:39 +0800

    Word Subsets

---
+++ word-subsets/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+	public List<String> wordSubsets(String[] A, String[] B) {
+		Map<Character, Integer> letterToMinCount = new HashMap<>();
+		for (String wordB : B) {
+			Map<Character, Integer> letterToCountB = buildLetterToCount(wordB);
+			for (char letter : letterToCountB.keySet()) {
+				letterToMinCount.put(letter,
+						Math.max(letterToMinCount.getOrDefault(letter, 0), letterToCountB.get(letter)));
+			}
+		}
+
+		return Arrays.stream(A).filter(wordA -> isUniversal(letterToMinCount, wordA)).collect(Collectors.toList());
+	}
+
+	Map<Character, Integer> buildLetterToCount(String word) {
+		Map<Character, Integer> letterToCount = new HashMap<>();
+		for (char letter : word.toCharArray()) {
+			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+		}
+		return letterToCount;
+	}
+
+	boolean isUniversal(Map<Character, Integer> letterToMinCount, String word) {
+		Map<Character, Integer> letterToCount = buildLetterToCount(word);
+		return letterToMinCount.entrySet().stream()
+				.allMatch(entry -> letterToCount.getOrDefault(entry.getKey(), 0) >= entry.getValue());
+	}
+}
