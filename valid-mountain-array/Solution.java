b27d91c8dec2fda5bc84a194dd20cf8ecd98a1aa
Kai Wang <charles.wangkai@gmail.com>
2018-11-18 20:04:33 +0800

    Valid Mountain Array

---
+++ valid-mountain-array/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+	public boolean validMountainArray(int[] A) {
+		if (A.length < 3) {
+			return false;
+		}
+
+		boolean down = false;
+		for (int i = 0; i < A.length - 1; i++) {
+			if (A[i] == A[i + 1]) {
+				return false;
+			}
+
+			if (down) {
+				if (A[i] < A[i + 1]) {
+					return false;
+				}
+			} else {
+				if (A[i] > A[i + 1]) {
+					if (i == 0) {
+						return false;
+					}
+
+					down = true;
+				}
+			}
+		}
+
+		return down;
+	}
+}
