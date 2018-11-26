b4aa694d144eeed4b806a3f2e86fe0ed0f9663d0
Kai Wang <charles.wangkai@gmail.com>
2018-11-26 11:19:28 +0800

    Validate Stack Sequences

---
+++ validate-stack-sequences/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Solution {
+	public boolean validateStackSequences(int[] pushed, int[] popped) {
+		Stack<Integer> stack = new Stack<>();
+		int pushedIndex = 0;
+		for (int target : popped) {
+			while (!(!stack.empty() && stack.peek() == target)) {
+				if (pushedIndex == pushed.length) {
+					return false;
+				}
+
+				stack.push(pushed[pushedIndex]);
+				pushedIndex++;
+			}
+			stack.pop();
+		}
+		return true;
+	}
+}
