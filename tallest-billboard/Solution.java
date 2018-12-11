54dc1ad809518b5fe5d0449d54f04d2d5effa491
Kai Wang <charles.wangkai@gmail.com>
2018-12-11 13:42:30 +0800

    Tallest Billboard

---
+++ tallest-billboard/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public int tallestBillboard(int[] rods) {
+		Map<Integer, Integer> diffToMaxHeight = new HashMap<>();
+		diffToMaxHeight.put(0, 0);
+
+		for (int rod : rods) {
+			Map<Integer, Integer> nextDiffToMaxHeight = new HashMap<>(diffToMaxHeight);
+			for (int diff : diffToMaxHeight.keySet()) {
+				nextDiffToMaxHeight.put(diff + rod,
+						Math.max(nextDiffToMaxHeight.getOrDefault(diff + rod, 0), diffToMaxHeight.get(diff)));
+
+				nextDiffToMaxHeight.put(Math.abs(diff - rod),
+						Math.max(nextDiffToMaxHeight.getOrDefault(Math.abs(diff - rod), 0),
+								diffToMaxHeight.get(diff) + Math.min(diff, rod)));
+			}
+
+			diffToMaxHeight = nextDiffToMaxHeight;
+		}
+
+		return diffToMaxHeight.get(0);
+	}
+}
