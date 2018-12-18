b6b942ba73cb1067596fac332c89878bdd5fd3b2
Kai Wang <charles.wangkai@gmail.com>
2018-12-18 10:38:16 +0800

    Prison Cells After N Days

---
+++ prison-cells-after-n-days/Solution.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+	public int[] prisonAfterNDays(int[] cells, int N) {
+		Map<Integer, Integer> codeToIndex = new HashMap<>();
+		List<Integer> codes = new ArrayList<>();
+		for (int i = 0;; i++) {
+			int code = encode(cells);
+
+			if (codeToIndex.containsKey(code)) {
+				int beginIndex = codeToIndex.get(code);
+				int cycleLength = i - beginIndex;
+
+				return decode(codes.get(findIndex(beginIndex, cycleLength, N)));
+			}
+
+			codeToIndex.put(code, i);
+			codes.add(code);
+
+			cells = change(cells);
+		}
+	}
+
+	int[] change(int[] cells) {
+		int[] result = new int[cells.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = (i != 0 && i != result.length - 1 && cells[i - 1] == cells[i + 1]) ? 1 : 0;
+		}
+		return result;
+	}
+
+	int findIndex(int beginIndex, int cycleLength, int N) {
+		if (N < beginIndex) {
+			return N;
+		} else {
+			return beginIndex + (N - beginIndex) % cycleLength;
+		}
+	}
+
+	int encode(int[] cells) {
+		return Arrays.stream(cells).reduce(0, (result, element) -> result * 2 + element);
+	}
+
+	int[] decode(int code) {
+		int[] result = new int[8];
+		for (int i = result.length - 1; i >= 0; i--) {
+			result[i] = code % 2;
+			code /= 2;
+		}
+		return result;
+	}
+}
