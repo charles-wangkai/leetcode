221016050854143bb81b608b8d85f7b79da57644
Kai Wang <charles.wangkai@gmail.com>
2019-01-28 11:35:13 +0800

    Minimum Cost For Tickets

---
+++ minimum-cost-for-tickets/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public int mincostTickets(int[] days, int[] costs) {
+		int[] minCosts = new int[days.length + 1];
+
+		for (int i = 1; i < minCosts.length; i++) {
+			minCosts[i] = minCosts[findLastLength(days, i, 1)] + costs[0];
+			minCosts[i] = Math.min(minCosts[i], minCosts[findLastLength(days, i, 7)] + costs[1]);
+			minCosts[i] = Math.min(minCosts[i], minCosts[findLastLength(days, i, 30)] + costs[2]);
+		}
+
+		return minCosts[days.length];
+	}
+
+	int findLastLength(int[] days, int length, int pass) {
+		int lastLength = length - 1;
+		while (lastLength > 0 && days[length - 1] - days[lastLength - 1] + 1 <= pass) {
+			lastLength--;
+		}
+		return lastLength;
+	}
+}
