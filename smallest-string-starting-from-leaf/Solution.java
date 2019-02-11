98350661b27a03f110582e28e3ee64be50a2475a
Kai Wang <charles.wangkai@gmail.com>
2019-02-11 12:08:01 +0800

    Smallest String Starting From Leaf

---
+++ smallest-string-starting-from-leaf/Solution.java
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	String minPath;
+
+	public String smallestFromLeaf(TreeNode root) {
+		minPath = null;
+		search(root, "");
+		return minPath;
+	}
+
+	void search(TreeNode node, String path) {
+		path = (char) (node.val + 'a') + path;
+
+		if (node.left == null && node.right == null) {
+			if (minPath == null || path.compareTo(minPath) < 0) {
+				minPath = path;
+			}
+		}
+
+		if (node.left != null) {
+			search(node.left, path);
+		}
+		if (node.right != null) {
+			search(node.right, path);
+		}
+	}
+}
