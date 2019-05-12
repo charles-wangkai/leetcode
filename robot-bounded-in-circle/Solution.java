56ab170a6d049b6b1a0421080610bcdcc394da88
Kai Wang <charles.wangkai@gmail.com>
2019-05-12 21:04:05 +0800

    Robot Bounded In Circle

---
+++ robot-bounded-in-circle/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+	static final int[] X_OFFSETS = { 0, 1, 0, -1 };
+	static final int[] Y_OFFSETS = { 1, 0, -1, 0 };
+
+	public boolean isRobotBounded(String instructions) {
+		int x = 0;
+		int y = 0;
+		int direction = 0;
+		for (char instruction : instructions.toCharArray()) {
+			if (instruction == 'G') {
+				x += X_OFFSETS[direction];
+				y += Y_OFFSETS[direction];
+			} else if (instruction == 'L') {
+				direction = (direction - 1 + X_OFFSETS.length) % X_OFFSETS.length;
+			} else if (instruction == 'R') {
+				direction = (direction + 1) % X_OFFSETS.length;
+			}
+		}
+		return direction != 0 || (x == 0 && y == 0);
+	}
+}
