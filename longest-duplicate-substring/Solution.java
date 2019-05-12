e7eae719f6ffa6e57f5466fddfa5435402ed0d55
Kai Wang <charles.wangkai@gmail.com>
2019-05-12 22:56:36 +0800

    Longest Duplicate Substring

---
+++ longest-duplicate-substring/Solution.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+	static final int MOD_DIVISOR = 1_000_000_007;
+
+	public String longestDupSubstring(String S) {
+		int lower = 1;
+		int upper = S.length();
+		String result = "";
+		while (lower <= upper) {
+			int middle = (lower + upper) / 2;
+			String found = find(S, middle);
+			if (found != null) {
+				result = found;
+
+				lower = middle + 1;
+			} else {
+				upper = middle - 1;
+			}
+		}
+		return result;
+	}
+
+	static String find(String S, int length) {
+		int hash = 0;
+		for (int i = 0; i < length; i++) {
+			hash = addMod(multiplyMod(hash, 26), S.charAt(i) - 'a');
+		}
+
+		Map<Integer, List<Integer>> hashToBeginIndices = new HashMap<>();
+		putValue(hashToBeginIndices, hash, 0);
+
+		int highestPlaceValue = 1;
+		for (int i = 0; i < length - 1; i++) {
+			highestPlaceValue = multiplyMod(highestPlaceValue, 26);
+		}
+
+		for (int beginIndex = 1; beginIndex + length <= S.length(); beginIndex++) {
+			hash = addMod(
+					multiplyMod(subtractMod(hash, multiplyMod(S.charAt(beginIndex - 1) - 'a', highestPlaceValue)), 26),
+					S.charAt(beginIndex + length - 1) - 'a');
+
+			if (hashToBeginIndices.containsKey(hash)) {
+				String found = S.substring(beginIndex, beginIndex + length);
+				if (hashToBeginIndices.get(hash).stream().anyMatch(bi -> S.substring(bi, bi + length).equals(found))) {
+					return found;
+				}
+			}
+
+			putValue(hashToBeginIndices, hash, beginIndex);
+		}
+
+		return null;
+	}
+
+	static void putValue(Map<Integer, List<Integer>> hashToBeginIndices, int hash, int beginIndex) {
+		if (!hashToBeginIndices.containsKey(hash)) {
+			hashToBeginIndices.put(hash, new ArrayList<>());
+		}
+
+		hashToBeginIndices.get(hash).add(beginIndex);
+	}
+
+	static int addMod(int x, int y) {
+		return (x + y) % MOD_DIVISOR;
+	}
+
+	static int subtractMod(int x, int y) {
+		return (x - y + MOD_DIVISOR) % MOD_DIVISOR;
+	}
+
+	static int multiplyMod(int x, int y) {
+		return (int) ((long) x * y % MOD_DIVISOR);
+	}
+}
