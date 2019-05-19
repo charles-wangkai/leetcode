b6e5b2637df963b66df6853434c9cd4f838f1569
Kai Wang <charles.wangkai@gmail.com>
2019-05-19 14:09:15 +0800

    Last Stone Weight

---
+++ last-stone-weight/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution {
+	public int lastStoneWeight(int[] stones) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+		for (int stone : stones) {
+			pq.offer(stone);
+		}
+
+		while (pq.size() >= 2) {
+			int max = pq.poll();
+			int secondMax = pq.poll();
+
+			if (max != secondMax) {
+				pq.offer(max - secondMax);
+			}
+		}
+
+		return (pq.size() == 1) ? pq.peek() : 0;
+	}
+}
