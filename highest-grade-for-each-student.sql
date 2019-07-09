8c9dbb241ef9baacabff306e3e55aa8c2247b608
Kai Wang <charles.wangkai@gmail.com>
2019-07-09 14:24:10 +0800

    Highest Grade For Each Student

---
+++ highest-grade-for-each-student.sql
@@ -0,0 +1,14 @@
+SELECT e1.student_id,
+       MIN(e1.course_id) AS course_id,
+       e1.grade
+FROM Enrollments e1,
+     (
+        SELECT student_id,
+            MAX(grade) AS max_grade
+        FROM Enrollments
+        GROUP BY student_id
+     ) e2
+WHERE e1.student_id = e2.student_id
+AND e1.grade = e2.max_grade
+GROUP BY e1.student_id, e1.grade
+ORDER BY e1.student_id
