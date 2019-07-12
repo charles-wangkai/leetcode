5b9b4b91d8b2da936dedbfc80f643357049e46e6
Kai Wang <charles.wangkai@gmail.com>
2019-07-12 18:58:59 +0800

    Print in Order

---
+++ print-in-order/Foo.java
@@ -0,0 +1,56 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Foo {
+	boolean firstDone;
+	boolean secondDone;
+
+	Lock lock = new ReentrantLock();
+	Condition canRunSecond = lock.newCondition();
+	Condition canRunThird = lock.newCondition();
+
+	public void first(Runnable printFirst) throws InterruptedException {
+		lock.lock();
+		try {
+			// printFirst.run() outputs "first". Do not change or remove this line.
+			printFirst.run();
+
+			firstDone = true;
+			canRunSecond.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void second(Runnable printSecond) throws InterruptedException {
+		lock.lock();
+		try {
+			while (!firstDone) {
+				canRunSecond.await();
+			}
+
+			// printSecond.run() outputs "second". Do not change or remove this line.
+			printSecond.run();
+
+			secondDone = true;
+			canRunThird.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public synchronized void third(Runnable printThird) throws InterruptedException {
+		lock.lock();
+		try {
+			while (!secondDone) {
+				canRunThird.await();
+			}
+
+			// printThird.run() outputs "third". Do not change or remove this line.
+			printThird.run();
+		} finally {
+			lock.unlock();
+		}
+	}
+}
