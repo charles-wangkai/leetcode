06a0873f5523a5f66567e705712568a63d2de568
Kai Wang <charles.wangkai@gmail.com>
2019-07-16 16:10:27 +0800

    User Purchase Platform

---
+++ user-purchase-platform.sql
@@ -0,0 +1,31 @@
+SELECT
+    t.spend_date,
+    p.platform,
+    SUM(COALESCE(s.amount, 0)) AS total_amount,
+    COUNT(DISTINCT s.user_id) AS total_users
+FROM (
+    SELECT DISTINCT spend_date AS spend_date
+    FROM Spending
+) t
+JOIN (
+    SELECT 'both' AS platform
+    UNION SELECT 'desktop' AS platform
+    UNION SELECT 'mobile' AS platform
+) p
+LEFT OUTER JOIN (
+    SELECT
+        spend_date,
+        user_id,
+        CASE WHEN COUNT(DISTINCT platform) = 2 then 'both'
+            WHEN MIN(platform) = 'desktop' then 'desktop'
+            ELSE 'mobile'
+        END AS category
+    FROM Spending
+    GROUP BY spend_date, user_id
+) u
+ON u.spend_date = t.spend_date
+AND u.category = p.platform
+LEFT OUTER JOIN Spending s
+ON s.user_id = u.user_id
+AND s.spend_date = t.spend_date
+GROUP BY t.spend_date, p.platform
