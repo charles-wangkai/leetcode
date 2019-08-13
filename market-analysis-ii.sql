7e4c6de5fe677393052e22b3fb8c6e9e16145c1e
Kai Wang <charles.wangkai@gmail.com>
2019-08-13 11:25:10 +0800

    Market Analysis II

---
+++ market-analysis-ii.sql
@@ -0,0 +1,21 @@
+SELECT
+    u.user_id AS seller_id,
+    IF(t.seller_id IS NULL, 'no', 'yes') AS 2nd_item_fav_brand
+FROM Users u
+LEFT OUTER JOIN (
+    SELECT
+        o.seller_id,
+        i.item_brand
+    FROM
+        Orders o,
+        Items i
+    WHERE o.item_id = i.item_id
+    AND (
+        SELECT COUNT(*)
+        FROM Orders o1
+        WHERE o1.seller_id = o.seller_id
+        AND o1.order_date < o.order_date
+    ) = 1
+) t
+ON u.user_id = t.seller_id
+AND u.favorite_brand = t.item_brand
