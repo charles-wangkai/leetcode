6ffd5be8eca446c0b914e3beab7559c7fc816be2
Kai Wang <charles.wangkai@gmail.com>
2019-08-19 11:50:23 +0800

    Find Words That Can Be Formed by Characters

---
+++ find-words-that-can-be-formed-by-characters/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public int countCharacters(String[] words, String chars) {
+		Map<Character, Integer> letterToCount = buildLetterToCount(chars);
+
+		return Arrays.stream(words).filter(word -> isCovered(buildLetterToCount(word), letterToCount))
+				.mapToInt(String::length).sum();
+	}
+
+	static Map<Character, Integer> buildLetterToCount(String s) {
+		Map<Character, Integer> letterToCount = new HashMap<>();
+		for (char letter : s.toCharArray()) {
+			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+		}
+
+		return letterToCount;
+	}
+
+	static boolean isCovered(Map<Character, Integer> letterToCount1, Map<Character, Integer> letterToCount2) {
+		return letterToCount1.keySet().stream()
+				.allMatch(letter -> letterToCount1.get(letter) <= letterToCount2.getOrDefault(letter, 0));
+	}
+}
