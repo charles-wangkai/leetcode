2be32db709bb5b1c198059ed8aecfaebf2712bf6
Kai Wang <charles.wangkai@gmail.com>
2019-09-12 10:23:06 +0800

    Design Bounded Blocking Queue

---
+++ design-bounded-blocking-queue/BoundedBlockingQueue.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BoundedBlockingQueue {
+	int capacity;
+	Queue<Integer> queue = new LinkedList<>();
+
+	Lock lock = new ReentrantLock();
+	Condition canRunEnqueue = lock.newCondition();
+	Condition canRunDequeue = lock.newCondition();
+
+	public BoundedBlockingQueue(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public void enqueue(int element) throws InterruptedException {
+		lock.lock();
+		try {
+			while (queue.size() == capacity) {
+				canRunEnqueue.await();
+			}
+
+			queue.offer(element);
+
+			canRunDequeue.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public int dequeue() throws InterruptedException {
+		lock.lock();
+		try {
+			while (queue.isEmpty()) {
+				canRunDequeue.await();
+			}
+
+			int result = queue.poll();
+
+			canRunEnqueue.signal();
+
+			return result;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public int size() throws InterruptedException {
+		return queue.size();
+	}
+}
