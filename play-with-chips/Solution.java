674de270be6315679dd205cd2b52baba6146fe6d
Kai Wang <charles.wangkai@gmail.com>
2019-10-06 20:36:45 +0800

    Play with Chips

---
+++ play-with-chips/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Solution {
+	public int minCostToMoveChips(int[] chips) {
+		return (int) Math.min(Arrays.stream(chips).filter(chip -> chip % 2 == 0).count(),
+				Arrays.stream(chips).filter(chip -> chip % 2 != 0).count());
+	}
+}
