5440f63d5efd980269554f9602e8c1cdee9fd187
Kai Wang <charles.wangkai@gmail.com>
2019-11-25 03:23:49 +0800

    Minimum Time Visiting All Points

---
+++ minimum-time-visiting-all-points/Solution.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+public class Solution {
+	public int minTimeToVisitAllPoints(int[][] points) {
+		return IntStream.range(0, points.length - 1).map(
+				i -> Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1])))
+				.sum();
+	}
+}
