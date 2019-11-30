447f08ed76231ba12fea2dc47dfc115dfc966610
Kai Wang <charles.wangkai@gmail.com>
2019-12-01 01:16:59 +0800

    Remove Interval

---
+++ remove-interval/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+	public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+		List<List<Integer>> result = new ArrayList<>();
+		for (int[] interval : intervals) {
+			if (interval[1] <= toBeRemoved[0] || interval[0] >= toBeRemoved[1]) {
+				result.add(Arrays.asList(interval[0], interval[1]));
+			} else {
+				if (toBeRemoved[0] > interval[0]) {
+					result.add(Arrays.asList(interval[0], toBeRemoved[0]));
+				}
+
+				if (toBeRemoved[1] < interval[1]) {
+					result.add(Arrays.asList(toBeRemoved[1], interval[1]));
+				}
+			}
+		}
+
+		return result;
+	}
+}
