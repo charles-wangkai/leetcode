63ee2bfc27f497eb3d5faa2019ba0ecccf3fc908
Kai Wang <charles.wangkai@gmail.com>
2019-12-07 02:46:24 +0800

    Students and Examinations

---
+++ students-and-examinations.sql
@@ -0,0 +1,12 @@
+SELECT
+    stud.student_id,
+    stud.student_name,
+    subj.subject_name,
+    COUNT(exam.student_id) AS attended_exams
+FROM Students stud
+JOIN Subjects subj
+LEFT OUTER JOIN Examinations exam
+ON exam.student_id = stud.student_id
+AND exam.subject_name = subj.subject_name
+GROUP BY stud.student_id, stud.student_name, subj.subject_name
+ORDER BY stud.student_id, subj.subject_name
