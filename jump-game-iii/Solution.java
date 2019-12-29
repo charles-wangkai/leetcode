f90674895eba18ef984eb1041008b43c187c46e8
Kai Wang <charles.wangkai@gmail.com>
2019-12-29 13:28:55 +0800

    Jump Game III

---
+++ jump-game-iii/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+	public boolean canReach(int[] arr, int start) {
+		return search(arr, new boolean[arr.length], start);
+	}
+
+	boolean search(int[] arr, boolean[] visited, int index) {
+		if (index < 0 || index >= arr.length || visited[index]) {
+			return false;
+		}
+
+		visited[index] = true;
+
+		if (arr[index] == 0) {
+			return true;
+		}
+
+		for (int offset : new int[] { -arr[index], arr[index] }) {
+			if (search(arr, visited, index + offset)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
