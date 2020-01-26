14e049ee6346389f1b5f93ba11eda369a6453f61
Kai Wang <charles.wangkai@gmail.com>
2020-01-26 20:48:59 +0800

    Sort the Matrix Diagonally

---
+++ sort-the-matrix-diagonally/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+	public int[][] diagonalSort(int[][] mat) {
+		int m = mat.length;
+		int n = mat[0].length;
+
+		boolean changed;
+		do {
+			changed = false;
+
+			for (int r = 0; r < m - 1; ++r) {
+				for (int c = 0; c < n - 1; ++c) {
+					if (mat[r][c] > mat[r + 1][c + 1]) {
+						int temp = mat[r][c];
+						mat[r][c] = mat[r + 1][c + 1];
+						mat[r + 1][c + 1] = temp;
+
+						changed = true;
+					}
+				}
+			}
+		} while (changed);
+
+		return mat;
+	}
+}
