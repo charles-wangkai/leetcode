9d063ba86c9a5e9bc5a6b7b46a97691475d1688f
Kai Wang <charles.wangkai@gmail.com>
2020-01-26 20:38:01 +0800

    Break a Palindrome

---
+++ break-a-palindrome/Solution.java
@@ -0,0 +1,19 @@
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class Solution {
+	public String breakPalindrome(String palindrome) {
+		if (palindrome.length() == 1) {
+			return "";
+		}
+
+		OptionalInt index = IntStream.range(0, palindrome.length())
+				.filter(i -> i != palindrome.length() - 1 - i && palindrome.charAt(i) != 'a').findFirst();
+		if (index.isPresent()) {
+			return String.format("%sa%s", palindrome.substring(0, index.getAsInt()),
+					palindrome.substring(index.getAsInt() + 1));
+		} else {
+			return String.format("%sb", palindrome.substring(0, palindrome.length() - 1));
+		}
+	}
+}
