293febbbc90ea8d5aa716c1a7be4efdba4b0cda7
Kai Wang <charles.wangkai@gmail.com>
2020-04-05 02:11:12 +0800

    Count Largest Group

---
+++ count-largest-group/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int countLargestGroup(int n) {
+        Map<Integer, Integer> digitSumToCount = new HashMap<>();
+        for (int i = 1; i <= n; ++i) {
+            int digitSum = String.valueOf(i).chars().map(ch -> ch - '0').sum();
+            digitSumToCount.put(digitSum, digitSumToCount.getOrDefault(digitSum, 0) + 1);
+        }
+
+        int maxCount = digitSumToCount.values().stream().mapToInt(x -> x).max().getAsInt();
+
+        return (int) digitSumToCount.keySet().stream().filter(digitSum -> digitSumToCount.get(digitSum) == maxCount)
+                .count();
+    }
+}
