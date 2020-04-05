c51b0139c4789dc9eef32624d9fd191c58557d50
Kai Wang <charles.wangkai@gmail.com>
2020-04-05 15:22:21 +0800

    Best Time to Buy and Sell Stock II

--- best-time-to-buy-and-sell-stock-ii/Solution.java
+++ best-time-to-buy-and-sell-stock-ii/Solution.java
@@ -1,9 +1,7 @@
+import java.util.stream.IntStream;
+
 public class Solution {
 	public int maxProfit(int[] prices) {
-		int maxEarn = 0;
-		for (int i = 0; i < prices.length - 1; i++) {
-			maxEarn += Math.max(0, prices[i + 1] - prices[i]);
-		}
-		return maxEarn;
+		return IntStream.range(0, prices.length - 1).map(i -> Math.max(0, prices[i + 1] - prices[i])).sum();
 	}
 }
