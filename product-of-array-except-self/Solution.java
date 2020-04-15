086c9e135ede2afa5143c71b9f994c63b92f53a9
Kai Wang <charles.wangkai@gmail.com>
2020-04-15 15:10:54 +0800

    Product of Array Except Self

--- product-of-array-except-self/Solution.java
+++ product-of-array-except-self/Solution.java
@@ -1,19 +1,21 @@
+import java.util.stream.IntStream;
+
 public class Solution {
 	public int[] productExceptSelf(int[] nums) {
-		int[] output = new int[nums.length];
+		int[] result = IntStream.range(0, nums.length).map(i -> 1).toArray();
 
 		int rightProduct = 1;
-		for (int i = output.length - 1; i >= 0; i--) {
-			output[i] = rightProduct;
+		for (int i = result.length - 1; i >= 0; i--) {
+			result[i] *= rightProduct;
 			rightProduct *= nums[i];
 		}
 
 		int leftProduct = 1;
-		for (int i = 0; i < output.length; i++) {
-			output[i] *= leftProduct;
+		for (int i = 0; i < result.length; i++) {
+			result[i] *= leftProduct;
 			leftProduct *= nums[i];
 		}
 
-		return output;
+		return result;
 	}
 }
