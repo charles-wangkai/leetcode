7695b6cc3cb57aaf16d72d36cdf6f36e09bd449b
Kai Wang <charles.wangkai@gmail.com>
2020-05-03 15:55:12 +0800

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/Solution.java
@@ -0,0 +1,23 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int result = 0;
+        SortedMap<Integer, Integer> valueToCount = new TreeMap<>();
+        int beginIndex = 0;
+        for (int endIndex = 0; endIndex < nums.length; ++endIndex) {
+            valueToCount.put(nums[endIndex], valueToCount.getOrDefault(nums[endIndex], 0) + 1);
+
+            while (valueToCount.lastKey() - valueToCount.firstKey() > limit) {
+                valueToCount.put(nums[beginIndex], valueToCount.get(nums[beginIndex]) - 1);
+                valueToCount.remove(nums[beginIndex], 0);
+                ++beginIndex;
+            }
+
+            result = Math.max(result, endIndex - beginIndex + 1);
+        }
+
+        return result;
+    }
+}
