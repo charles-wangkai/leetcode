96aeb1c83efdcb542b62abb16f9daec4e6a12b1d
Kai Wang <charles.wangkai@gmail.com>
2020-05-04 15:08:33 +0800

    Number Complement

--- number-complement/Solution.java
+++ number-complement/Solution.java
@@ -1,15 +1,5 @@
 public class Solution {
 	public int findComplement(int num) {
-		int complement = 0;
-		int base = 1;
-		while (num != 0) {
-			int digit = num & 1;
-
-			complement += (1 - digit) * base;
-
-			num >>= 1;
-			base <<= 1;
-		}
-		return complement;
+		return (1 << Integer.toBinaryString(num).length()) - 1 - num;
 	}
 }
