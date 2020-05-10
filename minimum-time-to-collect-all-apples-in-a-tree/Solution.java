bd1f717c3e7deda4e670132689e6b9fe60f68da4
Kai Wang <charles.wangkai@gmail.com>
2020-05-10 20:39:53 +0800

    Minimum Time to Collect All Apples in a Tree

---
+++ minimum-time-to-collect-all-apples-in-a-tree/Solution.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        @SuppressWarnings("unchecked")
+        List<Integer>[] adjLists = new List[n];
+        for (int i = 0; i < adjLists.length; ++i) {
+            adjLists[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            adjLists[edge[0]].add(edge[1]);
+            adjLists[edge[1]].add(edge[0]);
+        }
+
+        return search(adjLists, hasApple, new boolean[n], 0).time;
+    }
+
+    Element search(List<Integer>[] adjLists, List<Boolean> hasApple, boolean[] visited, int v) {
+        visited[v] = true;
+
+        int time = 0;
+        boolean foundApple = hasApple.get(v);
+        for (int adj : adjLists[v]) {
+            if (!visited[adj]) {
+                Element subResult = search(adjLists, hasApple, visited, adj);
+                if (subResult.foundApple) {
+                    time += 2 + subResult.time;
+                    foundApple = true;
+                }
+            }
+        }
+
+        return new Element(time, foundApple);
+    }
+}
+
+class Element {
+    int time;
+    boolean foundApple;
+
+    Element(int time, boolean foundApple) {
+        this.time = time;
+        this.foundApple = foundApple;
+    }
+}
