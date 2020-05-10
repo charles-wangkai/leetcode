64bd1e08389b48cbacc467de92ec4ce0e98cea46
Kai Wang <charles.wangkai@gmail.com>
2020-05-10 19:00:24 +0800

    Build an Array With Stack Operations

---
+++ build-an-array-with-stack-operations/Solution.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int targetIndex = 0;
+        for (int i = 1; i <= n && targetIndex != target.length; ++i) {
+            result.add("Push");
+
+            if (i == target[targetIndex]) {
+                ++targetIndex;
+            } else {
+                result.add("Pop");
+            }
+        }
+
+        return result;
+    }
+}
