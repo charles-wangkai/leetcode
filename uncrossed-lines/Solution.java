ae2f6f34ca04e6c6046bc3f76c8ddc267e39fd74
Kai Wang <charles.wangkai@gmail.com>
2020-05-25 19:10:35 +0800

    Uncrossed Lines

--- uncrossed-lines/Solution.java
+++ uncrossed-lines/Solution.java
@@ -1,15 +1,16 @@
 public class Solution {
 	public int maxUncrossedLines(int[] A, int[] B) {
-		int[][] maxLines = new int[A.length + 1][B.length + 1];
-		for (int i = 1; i <= A.length; i++) {
-			for (int j = 1; j <= B.length; j++) {
+		int[][] lineNums = new int[A.length + 1][B.length + 1];
+		for (int i = 1; i <= A.length; ++i) {
+			for (int j = 1; j <= B.length; ++j) {
 				if (A[i - 1] == B[j - 1]) {
-					maxLines[i][j] = maxLines[i - 1][j - 1] + 1;
+					lineNums[i][j] = lineNums[i - 1][j - 1] + 1;
 				} else {
-					maxLines[i][j] = Math.max(maxLines[i - 1][j], maxLines[i][j - 1]);
+					lineNums[i][j] = Math.max(lineNums[i - 1][j], lineNums[i][j - 1]);
 				}
 			}
 		}
-		return maxLines[A.length][B.length];
+
+		return lineNums[A.length][B.length];
 	}
 }
