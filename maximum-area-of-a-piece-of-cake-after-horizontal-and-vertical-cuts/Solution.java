d1689f65660d3b2e7ba27195bb17430f9dbc46dd
Kai Wang <charles.wangkai@gmail.com>
2020-05-31 13:55:58 +0800

    Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        return (int) (((long) computeMaxGap(h, horizontalCuts) * computeMaxGap(w, verticalCuts)) % 1_000_000_007);
+    }
+
+    int computeMaxGap(int limit, int[] cuts) {
+        int[] values = IntStream.concat(IntStream.of(0, limit), Arrays.stream(cuts)).sorted().toArray();
+
+        return IntStream.range(0, values.length - 1).map(i -> values[i + 1] - values[i]).max().getAsInt();
+    }
+}
