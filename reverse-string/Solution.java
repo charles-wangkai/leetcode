c494743ed49eb2fc41d77c3092e3d839dd28d6d7
Kai Wang <charles.wangkai@gmail.com>
2020-06-04 18:10:54 +0800

    Reverse String

--- reverse-string/Solution.java
+++ reverse-string/Solution.java
@@ -1,5 +1,9 @@
-public class Solution {
-	public String reverseString(String s) {
-		return new StringBuilder(s).reverse().toString();
+class Solution {
+	public void reverseString(char[] s) {
+		for (int i = 0, j = s.length - 1; i < j; ++i, --j) {
+			char temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+		}
 	}
 }
