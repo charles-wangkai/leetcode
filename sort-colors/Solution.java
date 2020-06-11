cd2e621f4d4369a396099dca480ec907074f8d58
Kai Wang <charles.wangkai@gmail.com>
2020-06-11 18:09:21 +0800

    Sort Colors

--- sort-colors/Solution.java
+++ sort-colors/Solution.java
@@ -2,14 +2,14 @@ public class Solution {
 	public void sortColors(int[] A) {
 		int index0 = -1;
 		int index2 = A.length;
-		for (int i = 0; i < index2; i++) {
+		for (int i = 0; i < index2; ++i) {
 			if (A[i] == 0) {
-				index0++;
+				++index0;
 				swap(A, i, index0);
 			} else if (A[i] == 2) {
-				index2--;
+				--index2;
 				swap(A, i, index2);
-				i--;
+				--i;
 			}
 		}
 	}
