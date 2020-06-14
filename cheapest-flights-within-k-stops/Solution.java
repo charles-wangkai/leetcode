7f8e382223141f0556ac0b120d3e4eb90f896f9c
Kai Wang <charles.wangkai@gmail.com>
2020-06-14 18:38:58 +0800

    Cheapest Flights Within K Stops

--- cheapest-flights-within-k-stops/Solution.java
+++ cheapest-flights-within-k-stops/Solution.java
@@ -4,32 +4,33 @@ import java.util.PriorityQueue;
 
 public class Solution {
 	public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-		Map<Integer, Map<Integer, Integer>> flightMap = new HashMap<Integer, Map<Integer, Integer>>();
+		@SuppressWarnings("unchecked")
+		Map<Integer, Integer>[] edgeLists = new Map[n];
+		for (int i = 0; i < edgeLists.length; ++i) {
+			edgeLists[i] = new HashMap<>();
+		}
+
 		for (int[] flight : flights) {
 			int from = flight[0];
 			int to = flight[1];
 			int price = flight[2];
 
-			if (!flightMap.containsKey(from)) {
-				flightMap.put(from, new HashMap<Integer, Integer>());
-			}
-			flightMap.get(from).put(to, price);
+			edgeLists[from].put(to, price);
 		}
 
-		PriorityQueue<City> pq = new PriorityQueue<City>((city1, city2) -> Integer.compare(city1.price, city2.price));
-		pq.add(new City(src, 0, 0));
+		PriorityQueue<City> pq = new PriorityQueue<>((c1, c2) -> Integer.compare(c1.cost, c2.cost));
+		pq.add(new City(src, 0, -1));
+
 		while (!pq.isEmpty()) {
 			City city = pq.poll();
 
 			if (city.index == dst) {
-				return city.price;
+				return city.cost;
 			}
 
-			if (city.move <= K && flightMap.containsKey(city.index)) {
-				for (int to : flightMap.get(city.index).keySet()) {
-					int price = flightMap.get(city.index).get(to);
-
-					pq.offer(new City(to, city.price + price, city.move + 1));
+			if (city.stopCount + 1 <= K) {
+				for (int to : edgeLists[city.index].keySet()) {
+					pq.offer(new City(to, city.cost + edgeLists[city.index].get(to), city.stopCount + 1));
 				}
 			}
 		}
@@ -40,12 +41,12 @@ public class Solution {
 
 class City {
 	int index;
-	int price;
-	int move;
+	int cost;
+	int stopCount;
 
-	City(int index, int price, int move) {
+	City(int index, int cost, int stopCount) {
 		this.index = index;
-		this.price = price;
-		this.move = move;
+		this.cost = cost;
+		this.stopCount = stopCount;
 	}
 }
