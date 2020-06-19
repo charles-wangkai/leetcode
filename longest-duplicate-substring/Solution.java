5bbd7a88cc5b507c2311c4374153452fc230e559
Kai Wang <charles.wangkai@gmail.com>
2020-06-19 15:56:38 +0800

    Longest Duplicate Substring

--- longest-duplicate-substring/Solution.java
+++ longest-duplicate-substring/Solution.java
@@ -4,29 +4,29 @@ import java.util.List;
 import java.util.Map;
 
 public class Solution {
-	static final int MOD_DIVISOR = 1_000_000_007;
+	static final int MODULUS = 1_000_000_007;
 
 	public String longestDupSubstring(String S) {
+		String result = "";
 		int lower = 1;
 		int upper = S.length();
-		String result = "";
 		while (lower <= upper) {
 			int middle = (lower + upper) / 2;
 			String found = find(S, middle);
 			if (found != null) {
 				result = found;
-
 				lower = middle + 1;
 			} else {
 				upper = middle - 1;
 			}
 		}
+
 		return result;
 	}
 
 	static String find(String S, int length) {
 		int hash = 0;
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < length; ++i) {
 			hash = addMod(multiplyMod(hash, 26), S.charAt(i) - 'a');
 		}
 
@@ -34,11 +34,11 @@ public class Solution {
 		putValue(hashToBeginIndices, hash, 0);
 
 		int highestPlaceValue = 1;
-		for (int i = 0; i < length - 1; i++) {
+		for (int i = 0; i < length - 1; ++i) {
 			highestPlaceValue = multiplyMod(highestPlaceValue, 26);
 		}
 
-		for (int beginIndex = 1; beginIndex + length <= S.length(); beginIndex++) {
+		for (int beginIndex = 1; beginIndex + length <= S.length(); ++beginIndex) {
 			hash = addMod(
 					multiplyMod(subtractMod(hash, multiplyMod(S.charAt(beginIndex - 1) - 'a', highestPlaceValue)), 26),
 					S.charAt(beginIndex + length - 1) - 'a');
@@ -65,14 +65,14 @@ public class Solution {
 	}
 
 	static int addMod(int x, int y) {
-		return (x + y) % MOD_DIVISOR;
+		return (x + y) % MODULUS;
 	}
 
 	static int subtractMod(int x, int y) {
-		return (x - y + MOD_DIVISOR) % MOD_DIVISOR;
+		return (x - y + MODULUS) % MODULUS;
 	}
 
 	static int multiplyMod(int x, int y) {
-		return (int) ((long) x * y % MOD_DIVISOR);
+		return (int) ((long) x * y % MODULUS);
 	}
 }
