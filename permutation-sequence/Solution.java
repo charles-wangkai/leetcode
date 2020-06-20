6b1403320e5c6a3976d57fe3b831903dbc1a47bc
Kai Wang <charles.wangkai@gmail.com>
2020-06-20 15:21:20 +0800

    Permutation Sequence

--- permutation-sequence/Solution.java
+++ permutation-sequence/Solution.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Solution {
 	public String getPermutation(int n, int k) {
-		int factorial = 1;
-		for (int i = 1; i <= n; i++) {
-			factorial *= i;
-		}
+		int factorial = IntStream.rangeClosed(1, n).reduce((x, y) -> x * y).getAsInt();
 
-		List<Integer> elements = new ArrayList<Integer>();
-		for (int i = 1; i <= n; i++) {
+		List<Integer> elements = new ArrayList<>();
+		for (int i = 1; i <= n; ++i) {
 			elements.add(i);
 		}
 
 		StringBuilder permutation = new StringBuilder();
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; ++i) {
 			factorial /= n - i;
 			int index = (k - 1) / factorial;
 			permutation.append(elements.remove(index));
 			k -= index * factorial;
 		}
+
 		return permutation.toString();
 	}
 }
