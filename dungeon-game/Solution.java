16c7ecfd1a1bf2ed180e75dfa91f31c640b294d2
Kai Wang <charles.wangkai@gmail.com>
2020-06-21 20:50:03 +0800

    Dungeon Game

--- dungeon-game/Solution.java
+++ dungeon-game/Solution.java
@@ -3,21 +3,19 @@ public class Solution {
 		int row = dungeon.length;
 		int col = dungeon[0].length;
 		int[][] minHPs = new int[row][col];
-		for (int i = row - 1; i >= 0; i--) {
-			for (int j = col - 1; j >= 0; j--) {
+		for (int i = row - 1; i >= 0; --i) {
+			for (int j = col - 1; j >= 0; --j) {
 				if (i == row - 1 && j == col - 1) {
-					minHPs[i][j] = (dungeon[i][j] < 0) ? (-dungeon[i][j] + 1)
-							: 1;
-					continue;
+					minHPs[i][j] = Math.max(1, -dungeon[i][j] + 1);
+				} else {
+					int right = (j == col - 1) ? Integer.MAX_VALUE : minHPs[i][j + 1];
+					int down = (i == row - 1) ? Integer.MAX_VALUE : minHPs[i + 1][j];
+					int minNext = Math.min(right, down);
+					minHPs[i][j] = Math.max(1, minNext - dungeon[i][j]);
 				}
-				int right = (j == col - 1) ? Integer.MAX_VALUE
-						: minHPs[i][j + 1];
-				int down = (i == row - 1) ? Integer.MAX_VALUE
-						: minHPs[i + 1][j];
-				int minNext = Math.min(right, down);
-				minHPs[i][j] = Math.max(minNext - dungeon[i][j], 1);
 			}
 		}
+
 		return minHPs[0][0];
 	}
 }
