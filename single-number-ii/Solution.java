c15d4a5eb726a4c5833250300bb9223a5d7148ea
Kai Wang <charles.wangkai@gmail.com>
2020-06-22 16:59:34 +0800

    Single Number II

--- single-number-ii/Solution.java
+++ single-number-ii/Solution.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
+
 public class Solution {
-	public int singleNumber(int[] A) {
+	public int singleNumber(int[] nums) {
 		int[] bits = new int[32];
-		for (int number : A) {
-			for (int i = bits.length - 1; i >= 0; i--) {
-				if ((number & (1 << (bits.length - 1 - i))) != 0) {
+		for (int num : nums) {
+			for (int i = 0; i < bits.length; ++i) {
+				if ((num & (1L << (bits.length - 1 - i))) != 0) {
 					bits[i] = (bits[i] + 1) % 3;
 				}
 			}
 		}
 
-		int result = 0;
-		for (int bit : bits) {
-			result = result * 2 + bit;
-		}
-		return result;
+		return Arrays.stream(bits).reduce(0, (res, elem) -> res * 2 + elem);
 	}
 }
