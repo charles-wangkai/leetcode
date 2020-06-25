e5f0e3bfc06b3bdc24b69d7537349bed9704048b
Kai Wang <charles.wangkai@gmail.com>
2020-06-25 22:19:41 +0800

    Find the Duplicate Number

--- find-the-duplicate-number/Solution.java
+++ find-the-duplicate-number/Solution.java
@@ -6,16 +6,15 @@ public class Solution {
 		int upper = nums.length - 1;
 		while (lower < upper) {
 			int middle = (lower + upper) / 2;
-			if (count(nums, lower, middle) > middle - lower + 1) {
+
+			int lower_ = lower;
+			if (Arrays.stream(nums).filter(num -> num >= lower_ && num <= middle).count() > middle - lower + 1) {
 				upper = middle;
 			} else {
 				lower = middle + 1;
 			}
 		}
-		return lower;
-	}
 
-	int count(int[] nums, int min, int max) {
-		return (int) Arrays.stream(nums).filter(num -> num >= min && num <= max).count();
+		return lower;
 	}
 }
