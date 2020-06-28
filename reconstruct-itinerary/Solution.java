df57577acd5712ea14d4c8554f8ea74a6307e557
Kai Wang <charles.wangkai@gmail.com>
2020-06-28 17:51:39 +0800

    Reconstruct Itinerary

--- reconstruct-itinerary/Solution.java
+++ reconstruct-itinerary/Solution.java
@@ -4,68 +4,62 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class Solution {
-	List<String> itinerary;
+	public List<String> findItinerary(List<List<String>> tickets) {
+		Map<String, SortedMap<String, Integer>> fromToTos = new HashMap<>();
+		for (List<String> ticket : tickets) {
+			String from = ticket.get(0);
+			String to = ticket.get(1);
 
-	public List<String> findItinerary(String[][] tickets) {
-		Map<String, SortedMap<String, Integer>> from2tos = new HashMap<String, SortedMap<String, Integer>>();
-		for (String[] ticket : tickets) {
-			String from = ticket[0];
-			String to = ticket[1];
-
-			addTicket(from2tos, from, to);
+			addTicket(fromToTos, from, to);
 		}
 
-		itinerary = null;
-		List<String> currentItinerary = new ArrayList<String>();
-		currentItinerary.add("JFK");
-		search(from2tos, currentItinerary);
-		return itinerary;
+		List<String> itinerary = new ArrayList<>();
+		itinerary.add("JFK");
+
+		return search(fromToTos, itinerary);
 	}
 
-	void search(Map<String, SortedMap<String, Integer>> from2tos, List<String> currentItinerary) {
-		if (from2tos.isEmpty()) {
-			itinerary = currentItinerary;
-			return;
+	List<String> search(Map<String, SortedMap<String, Integer>> fromToTos, List<String> itinerary) {
+		if (fromToTos.isEmpty()) {
+			return itinerary;
 		}
 
-		String from = currentItinerary.get(currentItinerary.size() - 1);
-		if (from2tos.containsKey(from)) {
-			TreeSet<String> tos = new TreeSet<String>(from2tos.get(from).keySet());
+		String from = itinerary.get(itinerary.size() - 1);
+		if (fromToTos.containsKey(from)) {
+			List<String> tos = new ArrayList<>(fromToTos.get(from).keySet());
 			for (String to : tos) {
-				removeTicket(from2tos, from, to);
-				currentItinerary.add(to);
+				removeTicket(fromToTos, from, to);
+				itinerary.add(to);
 
-				search(from2tos, currentItinerary);
-				if (itinerary != null) {
-					break;
+				List<String> subResult = search(fromToTos, itinerary);
+				if (subResult != null) {
+					return subResult;
 				}
 
-				currentItinerary.remove(currentItinerary.size() - 1);
-				addTicket(from2tos, from, to);
+				itinerary.remove(itinerary.size() - 1);
+				addTicket(fromToTos, from, to);
 			}
 		}
+
+		return null;
 	}
 
-	void addTicket(Map<String, SortedMap<String, Integer>> from2tos, String from, String to) {
-		if (!from2tos.containsKey(from)) {
-			from2tos.put(from, new TreeMap<String, Integer>());
+	void addTicket(Map<String, SortedMap<String, Integer>> fromToTos, String from, String to) {
+		if (!fromToTos.containsKey(from)) {
+			fromToTos.put(from, new TreeMap<>());
 		}
-		if (!from2tos.get(from).containsKey(to)) {
-			from2tos.get(from).put(to, 0);
-		}
-		from2tos.get(from).put(to, from2tos.get(from).get(to) + 1);
+
+		fromToTos.get(from).put(to, fromToTos.get(from).getOrDefault(to, 0) + 1);
 	}
 
-	void removeTicket(Map<String, SortedMap<String, Integer>> from2tos, String from, String to) {
-		from2tos.get(from).put(to, from2tos.get(from).get(to) - 1);
-		if (from2tos.get(from).get(to) == 0) {
-			from2tos.get(from).remove(to);
-		}
-		if (from2tos.get(from).isEmpty()) {
-			from2tos.remove(from);
+	void removeTicket(Map<String, SortedMap<String, Integer>> fromToTos, String from, String to) {
+		fromToTos.get(from).put(to, fromToTos.get(from).get(to) - 1);
+		fromToTos.get(from).remove(to, 0);
+
+		if (fromToTos.get(from).isEmpty()) {
+			fromToTos.remove(from);
 		}
 	}
 }
