c8e5fa1240a754e915a79deaa43d6c2769d4ab62
Kai Wang <charles.wangkai@gmail.com>
2020-06-29 18:42:48 +0800

    Unique Paths

--- unique-paths/Solution.java
+++ unique-paths/Solution.java
@@ -1,13 +1,13 @@
-public class Solution {
+class Solution {
 	public int uniquePaths(int m, int n) {
 		int[][] pathNums = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				pathNums[i][j] = (i == 0 && j == 0) ? 1 : ((i == 0 ? 0
-						: pathNums[i - 1][j]) + (j == 0 ? 0
-						: pathNums[i][j - 1]));
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				pathNums[i][j] = (i == 0 && j == 0) ? 1
+						: (((i == 0) ? 0 : pathNums[i - 1][j]) + ((j == 0) ? 0 : pathNums[i][j - 1]));
 			}
 		}
+
 		return pathNums[m - 1][n - 1];
 	}
 }
