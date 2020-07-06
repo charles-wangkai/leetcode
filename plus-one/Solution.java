125d0305c4087de2d7d3dc7cbbc70b2e6465ed41
Kai Wang <charles.wangkai@gmail.com>
2020-07-06 19:33:39 +0800

    Plus One

--- plus-one/Solution.java
+++ plus-one/Solution.java
@@ -1,27 +1,20 @@
-public class Solution {
-	public int[] plusOne(int[] digits) {
-		int[] result;
-		if (isAllNine(digits)) {
-			result = new int[digits.length + 1];
-			result[0] = 1;
-		} else {
-			result = new int[digits.length];
-			int carry = 1;
-			for (int i = result.length - 1; i >= 0; i--) {
-				int sum = digits[i] + carry;
-				result[i] = sum % 10;
-				carry = sum / 10;
-			}
-		}
-		return result;
-	}
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-	boolean isAllNine(int[] digits) {
-		for (int digit : digits) {
-			if (digit != 9) {
-				return false;
-			}
+class Solution {
+	public int[] plusOne(int[] digits) {
+		if (Arrays.stream(digits).allMatch(digit -> digit == 9)) {
+			return IntStream.range(0, digits.length + 1).map(i -> (i == 0) ? 1 : 0).toArray();
 		}
-		return true;
+
+		int[] result = new int[digits.length];
+		int carry = 1;
+		for (int i = result.length - 1; i >= 0; --i) {
+			int sum = digits[i] + carry;
+			result[i] = sum % 10;
+			carry = sum / 10;
+		}
+
+		return result;
 	}
 }
