40267a84e44640da06081c299f924c8794a6fc93
Kai Wang <charles.wangkai@gmail.com>
2020-07-07 15:31:19 +0800

    Island Perimeter

--- island-perimeter/Solution.java
+++ island-perimeter/Solution.java
@@ -1,4 +1,4 @@
-public class Solution {
+class Solution {
 	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
 	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
 
@@ -10,21 +10,22 @@ public class Solution {
 		int col = grid[0].length;
 
 		int perimeter = 0;
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
+		for (int r = 0; r < row; ++r) {
+			for (int c = 0; c < col; ++c) {
 				if (grid[r][c] == 1) {
-					for (int i = 0; i < R_OFFSETS.length; i++) {
+					for (int i = 0; i < R_OFFSETS.length; ++i) {
 						int neighborR = r + R_OFFSETS[i];
 						int neighborC = c + C_OFFSETS[i];
 
 						if (!(neighborR >= 0 && neighborR < row && neighborC >= 0 && neighborC < col)
 								|| grid[neighborR][neighborC] == 0) {
-							perimeter++;
+							++perimeter;
 						}
 					}
 				}
 			}
 		}
+
 		return perimeter;
 	}
 }
