8f8da88389d20ba92a14c625bf15246ee78ad01a
Kai Wang <charles.wangkai@gmail.com>
2020-07-17 16:08:29 +0800

    Top K Frequent Elements

--- top-k-frequent-elements/Solution.java
+++ top-k-frequent-elements/Solution.java
@@ -1,40 +1,24 @@
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.stream.Collectors;
 
-public class Solution {
-	public List<Integer> topKFrequent(int[] nums, int k) {
-		Map<Integer, Integer> number2count = new HashMap<Integer, Integer>();
-		for (int number : nums) {
-			if (!number2count.containsKey(number)) {
-				number2count.put(number, 0);
-			}
-			number2count.put(number, number2count.get(number) + 1);
+class Solution {
+	public int[] topKFrequent(int[] nums, int k) {
+		Map<Integer, Integer> valueToCount = new HashMap<>();
+		for (int value : nums) {
+			valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
 		}
 
-		PriorityQueue<NumberAndCount> pq = new PriorityQueue<NumberAndCount>((nc1, nc2) -> nc1.count - nc2.count);
-		for (int number : number2count.keySet()) {
-			int count = number2count.get(number);
+		PriorityQueue<Integer> pq = new PriorityQueue<>(
+				(v1, v2) -> Integer.compare(valueToCount.get(v1), valueToCount.get(v2)));
+		for (int value : valueToCount.keySet()) {
+			pq.offer(value);
 
-			pq.offer(new NumberAndCount(number, count));
-
-			if (pq.size() > k) {
+			if (pq.size() == k + 1) {
 				pq.poll();
 			}
 		}
 
-		return pq.stream().map(nc -> nc.number).collect(Collectors.toList());
-	}
-}
-
-class NumberAndCount {
-	int number;
-	int count;
-
-	NumberAndCount(int number, int count) {
-		this.number = number;
-		this.count = count;
+		return pq.stream().mapToInt(x -> x).toArray();
 	}
 }
