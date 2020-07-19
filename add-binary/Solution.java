a1581c72128f815231d249956186c96514eacb5e
Kai Wang <charles.wangkai@gmail.com>
2020-07-19 15:42:54 +0800

    Add Binary

--- add-binary/Solution.java
+++ add-binary/Solution.java
@@ -1,22 +1,17 @@
-public class Solution {
+class Solution {
 	public String addBinary(String a, String b) {
 		StringBuilder result = new StringBuilder();
 		int carry = 0;
-		for (int indexA = a.length() - 1, indexB = b.length() - 1; indexA >= 0
-				|| indexB >= 0; indexA--, indexB--) {
-			int sum = (indexA >= 0 ? convertToDigit(a.charAt(indexA)) : 0)
-					+ (indexB >= 0 ? convertToDigit(b.charAt(indexB)) : 0)
+		for (int indexA = a.length() - 1, indexB = b.length() - 1; indexA >= 0 || indexB >= 0; --indexA, --indexB) {
+			int sum = (indexA >= 0 ? (a.charAt(indexA) - '0') : 0) + (indexB >= 0 ? (b.charAt(indexB) - '0') : 0)
 					+ carry;
 			result.append(sum % 2);
 			carry = sum / 2;
 		}
-		if (carry > 0) {
+		if (carry != 0) {
 			result.append(carry);
 		}
-		return result.reverse().toString();
-	}
 
-	int convertToDigit(char ch) {
-		return ch - '0';
+		return result.reverse().toString();
 	}
 }
