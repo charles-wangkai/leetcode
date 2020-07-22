1821028ed6d9b8ebd43b12eba8aa94fef55f5062
Kai Wang <charles.wangkai@gmail.com>
2020-07-22 16:26:32 +0800

    Binary Tree Zigzag Level Order Traversal

--- binary-tree-zigzag-level-order-traversal/Solution.java
+++ binary-tree-zigzag-level-order-traversal/Solution.java
@@ -1,24 +1,35 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
 	}
 }
 
-public class Solution {
+class Solution {
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-		List<List<Integer>> levels = new ArrayList<List<Integer>>();
+		List<List<Integer>> levels = new ArrayList<>();
 		search(levels, root, 0);
 		for (int i = 1; i < levels.size(); i += 2) {
-			reverse(levels.get(i));
+			Collections.reverse(levels.get(i));
 		}
+
 		return levels;
 	}
 
@@ -26,19 +37,13 @@ public class Solution {
 		if (node == null) {
 			return;
 		}
+
 		if (depth == levels.size()) {
-			levels.add(new ArrayList<Integer>());
+			levels.add(new ArrayList<>());
 		}
 		levels.get(depth).add(node.val);
+
 		search(levels, node.left, depth + 1);
 		search(levels, node.right, depth + 1);
-	}
-
-	void reverse(List<Integer> level) {
-		for (int i = 0, j = level.size() - 1; i < j; i++, j--) {
-			int temp = level.get(i);
-			level.set(i, level.get(j));
-			level.set(j, temp);
-		}
 	}
 }
