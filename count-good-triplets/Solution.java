69227a5e2332f8945c081ca796866dcee87f4714
Kai Wang <charles.wangkai@gmail.com>
2020-08-02 20:37:44 +0800

    Count Good Triplets

---
+++ count-good-triplets/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int result = 0;
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = i + 1; j < arr.length; ++j) {
+                for (int k = j + 1; k < arr.length; ++k) {
+                    if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b
+                            && Math.abs(arr[k] - arr[i]) <= c) {
+                        ++result;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
