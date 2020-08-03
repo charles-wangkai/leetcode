59f2f6d0fed948ee24b224493dd6c74a3c3d567f
Kai Wang <charles.wangkai@gmail.com>
2020-08-03 19:59:07 +0800

    Valid Palindrome

--- valid-palindrome/Solution.java
+++ valid-palindrome/Solution.java
@@ -1,30 +1,10 @@
-public class Solution {
+import java.util.stream.Collectors;
+
+class Solution {
 	public boolean isPalindrome(String s) {
-		int i = 0;
-		int j = s.length() - 1;
-		while (true) {
-			while (i < s.length() && !isCheckableChar(s.charAt(i))) {
-				i++;
-			}
-			while (j >= 0 && !isCheckableChar(s.charAt(j))) {
-				j--;
-			}
-			if (i >= j) {
-				return true;
-			}
-			if (!isSameIgnoringCase(s.charAt(i), s.charAt(j))) {
-				return false;
-			}
-			i++;
-			j--;
-		}
-	}
+		String str = s.chars().filter(Character::isLetterOrDigit).mapToObj(ch -> String.valueOf((char) ch))
+				.collect(Collectors.joining());
 
-	boolean isCheckableChar(char ch) {
-		return Character.isLetterOrDigit(ch);
-	}
-
-	boolean isSameIgnoringCase(char ch1, char ch2) {
-		return Character.toLowerCase(ch1) == Character.toLowerCase(ch2);
+		return new StringBuilder(str).reverse().toString().equalsIgnoreCase(str);
 	}
 }
