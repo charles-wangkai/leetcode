53f5aae5f8bc9981930c6933d846366593c82a7a
Kai Wang <charles.wangkai@gmail.com>
2020-08-05 21:59:48 +0800

    Add and Search Word - Data structure design

--- add-and-search-word-data-structure-design/WordDictionary.java
+++ add-and-search-word-data-structure-design/WordDictionary.java
@@ -1,51 +1,45 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 class TrieNode {
 	// Initialize your data structure here.
-	private Map<Character, TrieNode> children;
+	private Map<Character, TrieNode> letterToChild;
 
 	public TrieNode() {
-		children = new HashMap<Character, TrieNode>();
+		letterToChild = new HashMap<>();
 	}
 
 	public boolean hasChild(Character ch) {
-		return children.containsKey(ch);
+		return letterToChild.containsKey(ch);
 	}
 
 	public TrieNode getChild(Character ch) {
-		return children.get(ch);
+		return letterToChild.get(ch);
 	}
 
 	public TrieNode putChild(Character ch) {
-		if (!hasChild(ch)) {
-			children.put(ch, new TrieNode());
-		}
+		letterToChild.putIfAbsent(ch, new TrieNode());
+
 		return getChild(ch);
 	}
 
-	public List<TrieNode> getChildrenNodes() {
-		List<TrieNode> childrenNodes = new ArrayList<TrieNode>();
-		for (Entry<Character, TrieNode> entry : children.entrySet()) {
-			if (entry.getKey() != null) {
-				childrenNodes.add(entry.getValue());
-			}
-		}
-		return childrenNodes;
+	public List<TrieNode> getChildren() {
+		return letterToChild.entrySet().stream().filter(entry -> entry.getKey() != null).map(Entry::getValue)
+				.collect(Collectors.toList());
 	}
 }
 
-public class WordDictionary {
+class WordDictionary {
 	TrieNode root = new TrieNode();
 
 	// Adds a word into the data structure.
 	public void addWord(String word) {
 		TrieNode node = root;
-		for (int i = 0; i < word.length(); i++) {
-			node = node.putChild(word.charAt(i));
+		for (char letter : word.toCharArray()) {
+			node = node.putChild(letter);
 		}
 		node.putChild(null);
 	}
@@ -63,17 +57,9 @@ public class WordDictionary {
 
 		char ch = word.charAt(index);
 		if (ch == '.') {
-			for (TrieNode childNode : node.getChildrenNodes()) {
-				if (search(word, index + 1, childNode)) {
-					return true;
-				}
-			}
-			return false;
+			return node.getChildren().stream().anyMatch(child -> search(word, index + 1, child));
 		} else {
-			if (!node.hasChild(ch)) {
-				return false;
-			}
-			return search(word, index + 1, node.getChild(ch));
+			return node.hasChild(ch) && search(word, index + 1, node.getChild(ch));
 		}
 	}
 }
