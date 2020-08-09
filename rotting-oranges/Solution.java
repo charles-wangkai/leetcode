f7456129ecaca8f5779a3f2bbc8c3c9f425a580d
Kai Wang <charles.wangkai@gmail.com>
2020-08-09 16:40:07 +0800

    Rotting Oranges

--- rotting-oranges/Solution.java
+++ rotting-oranges/Solution.java
@@ -1,51 +1,68 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Solution {
+class Solution {
 	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
 	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
 
 	public int orangesRotting(int[][] grid) {
-		int freshNum = countFresh(grid);
-		int minute = 0;
-		while (freshNum != 0) {
-			grid = elapse(grid);
-			int nextFreshNum = countFresh(grid);
-
-			if (nextFreshNum == freshNum) {
-				return -1;
-			}
-
-			freshNum = nextFreshNum;
-			minute++;
-		}
-		return minute;
-	}
-
-	int countFresh(int[][] grid) {
-		return Arrays.stream(grid).mapToInt(line -> (int) Arrays.stream(line).filter(x -> x == 1).count()).sum();
-	}
-
-	int[][] elapse(int[][] grid) {
 		int row = grid.length;
 		int col = grid[0].length;
 
-		int[][] nextGrid = new int[row][col];
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				nextGrid[r][c] = grid[r][c];
+		int[][] distances = new int[row][col];
+		for (int r = 0; r < row; ++r) {
+			Arrays.fill(distances[r], Integer.MAX_VALUE);
+		}
 
-				if (grid[r][c] == 1) {
-					for (int i = 0; i < R_OFFSETS.length; i++) {
-						int adjR = r + R_OFFSETS[i];
-						int adjC = c + C_OFFSETS[i];
-
-						if (adjR >= 0 && adjR < row && adjC >= 0 && adjC < col && grid[adjR][adjC] == 2) {
-							nextGrid[r][c] = 2;
-						}
-					}
+		Queue<Element> queue = new LinkedList<>();
+		for (int r = 0; r < row; ++r) {
+			for (int c = 0; c < col; ++c) {
+				if (grid[r][c] == 2) {
+					distances[r][c] = 0;
+					queue.offer(new Element(r, c));
 				}
 			}
 		}
-		return nextGrid;
+
+		while (!queue.isEmpty()) {
+			Element head = queue.poll();
+
+			for (int i = 0; i < R_OFFSETS.length; ++i) {
+				int adjR = head.r + R_OFFSETS[i];
+				int adjC = head.c + C_OFFSETS[i];
+
+				if (adjR >= 0 && adjR < row && adjC >= 0 && adjC < col && grid[adjR][adjC] == 1
+						&& distances[adjR][adjC] == Integer.MAX_VALUE) {
+					distances[adjR][adjC] = distances[head.r][head.c] + 1;
+					queue.offer(new Element(adjR, adjC));
+				}
+			}
+		}
+
+		int result = 0;
+		for (int r = 0; r < row; ++r) {
+			for (int c = 0; c < col; ++c) {
+				if (grid[r][c] == 1) {
+					if (distances[r][c] == Integer.MAX_VALUE) {
+						return -1;
+					}
+
+					result = Math.max(result, distances[r][c]);
+				}
+			}
+		}
+
+		return result;
+	}
+}
+
+class Element {
+	int r;
+	int c;
+
+	Element(int r, int c) {
+		this.r = r;
+		this.c = c;
 	}
 }
