619d36289aae12e41862c6b3d0eb00732b226001
Kai Wang <charles.wangkai@gmail.com>
2020-08-11 00:12:19 +0800

    Excel Sheet Column Number

--- excel-sheet-column-number/Solution.java
+++ excel-sheet-column-number/Solution.java
@@ -1,9 +1,5 @@
-public class Solution {
+class Solution {
 	public int titleToNumber(String s) {
-		int number = 0;
-		for (int i = 0; i < s.length(); i++) {
-			number = number * 26 + (s.charAt(i) - 'A' + 1);
-		}
-		return number;
+		return s.chars().reduce(0, (result, ch) -> result * 26 + (ch - 'A' + 1));
 	}
 }
