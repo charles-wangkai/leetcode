4b2b634d7cc8bccbc956b28c3272aa137f0c3999
Kai Wang <charles.wangkai@gmail.com>
2020-08-12 00:56:20 +0800

    H-Index

--- h-index/Solution.java
+++ h-index/Solution.java
@@ -1,13 +1,11 @@
 import java.util.Arrays;
 
-public class Solution {
+class Solution {
 	public int hIndex(int[] citations) {
 		Arrays.sort(citations);
 
-		for (int h = citations.length;; h--) {
-			if ((h == 0 || citations[citations.length - h] >= h)
-					&& (h == citations.length || citations[citations.length - h
-							- 1] <= h)) {
+		for (int h = citations.length;; --h) {
+			if ((h == 0 || citations[citations.length - h] >= h)) {
 				return h;
 			}
 		}
