383a44039386bc2450d9f4e876574d661ae93cd5
Kai Wang <charles.wangkai@gmail.com>
2020-08-12 22:13:51 +0800

    Pascal's Triangle II

--- pascals-triangle-ii/Solution.java
+++ pascals-triangle-ii/Solution.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Solution {
+class Solution {
 	public List<Integer> getRow(int rowIndex) {
-		List<Integer> row = new ArrayList<Integer>();
-		row.add(1);
-		for (int i = 1; i <= rowIndex; i++) {
-			List<Integer> nextRow = new ArrayList<Integer>();
+		List<Integer> row = Collections.singletonList(1);
+
+		for (int i = 1; i <= rowIndex; ++i) {
+			List<Integer> nextRow = new ArrayList<>();
 			nextRow.add(1);
-			for (int j = 0; j < row.size() - 1; j++) {
+			for (int j = 0; j < row.size() - 1; ++j) {
 				nextRow.add(row.get(j) + row.get(j + 1));
 			}
 			nextRow.add(1);
+
 			row = nextRow;
 		}
+
 		return row;
 	}
 }
