aadb0a67d95750d2ad458e5b18f42a121da21271
Kai Wang <charles.wangkai@gmail.com>
2020-08-15 15:42:22 +0800

    Find Permutation

--- find-permutation/Solution.java
+++ find-permutation/Solution.java
@@ -1,21 +1,21 @@
-public class Solution {
+class Solution {
 	public int[] findPermutation(String s) {
 		int[] result = new int[s.length() + 1];
 		int index = 0;
 
 		int startNumber = 1;
 		int countD = 0;
-		for (int i = 0; i <= s.length(); i++) {
-			if (i == s.length() || s.charAt(i) == 'I') {
-				for (int j = startNumber + countD; j >= startNumber; j--) {
+		for (int i = 0; i <= s.length(); ++i) {
+			if (i != s.length() && s.charAt(i) == 'D') {
+				++countD;
+			} else {
+				for (int j = startNumber + countD; j >= startNumber; --j) {
 					result[index] = j;
-					index++;
+					++index;
 				}
 
 				startNumber += countD + 1;
 				countD = 0;
-			} else {
-				countD++;
 			}
 		}
 
