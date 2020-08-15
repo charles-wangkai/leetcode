f2d7a71a221efa01d3f217fcc0bd6ac3de682c0a
Kai Wang <charles.wangkai@gmail.com>
2020-08-15 15:55:58 +0800

    Non-overlapping Intervals

--- non-overlapping-intervals/Solution.java
+++ non-overlapping-intervals/Solution.java
@@ -1,36 +1,19 @@
 import java.util.Arrays;
-import java.util.Stack;
-
-// Definition for an interval.
-class Interval {
-	int start;
-	int end;
-
-	Interval() {
-		start = 0;
-		end = 0;
-	}
-
-	Interval(int s, int e) {
-		start = s;
-		end = e;
-	}
-}
 
 public class Solution {
-	public int eraseOverlapIntervals(Interval[] intervals) {
-		Arrays.sort(intervals, (i1, i2) -> i1.start - i2.start);
+	public int eraseOverlapIntervals(int[][] intervals) {
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[1], i2[1]));
 
-		Stack<Interval> stack = new Stack<Interval>();
-		for (Interval interval : intervals) {
-			while (!stack.empty() && stack.peek().end > interval.end) {
-				stack.pop();
-			}
-
-			if (stack.empty() || stack.peek().end <= interval.start) {
-				stack.push(interval);
+		int result = 0;
+		int endTime = Integer.MIN_VALUE;
+		for (int[] interval : intervals) {
+			if (interval[0] >= endTime) {
+				endTime = interval[1];
+			} else {
+				++result;
 			}
 		}
-		return intervals.length - stack.size();
+
+		return result;
 	}
 }
