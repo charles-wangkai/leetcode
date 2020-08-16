189075a1fd309c62fc4a49a093f8bbf34e134388
Kai Wang <charles.wangkai@gmail.com>
2020-08-16 18:21:35 +0800

    Three Consecutive Odds

---
+++ three-consecutive-odds/Solution.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        return IntStream.range(0, arr.length - 2)
+                .anyMatch(i -> arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0);
+    }
+}
