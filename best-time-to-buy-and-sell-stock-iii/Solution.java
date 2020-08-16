042afc8bd8fa3e4f7a907fb834cc3ac1b0d666d2
Kai Wang <charles.wangkai@gmail.com>
2020-08-16 18:02:58 +0800

    Best Time to Buy and Sell Stock III

--- best-time-to-buy-and-sell-stock-iii/Solution.java
+++ best-time-to-buy-and-sell-stock-iii/Solution.java
@@ -1,21 +1,24 @@
-public class Solution {
+class Solution {
 	public int maxProfit(int[] prices) {
 		int[] maxAfterPrices = new int[prices.length];
 		int maxAfterPrice = Integer.MIN_VALUE;
-		for (int i = maxAfterPrices.length - 1; i >= 0; i--) {
+		for (int i = maxAfterPrices.length - 1; i >= 0; --i) {
 			maxAfterPrice = Math.max(maxAfterPrice, prices[i]);
 			maxAfterPrices[i] = maxAfterPrice;
 		}
 
-		int maxEarnTotal = 0;
+		int result = 0;
 		int maxEarn1 = 0;
 		int minPrice = Integer.MAX_VALUE;
-		for (int i = 0; i < prices.length; i++) {
+		for (int i = 0; i < prices.length; ++i) {
 			minPrice = Math.min(minPrice, prices[i]);
 			maxEarn1 = Math.max(maxEarn1, prices[i] - minPrice);
-			int maxEarn2 = Math.max(0, maxAfterPrices[i] - prices[i]);
-			maxEarnTotal = Math.max(maxEarnTotal, maxEarn1 + maxEarn2);
+
+			int maxEarn2 = maxAfterPrices[i] - prices[i];
+
+			result = Math.max(result, maxEarn1 + maxEarn2);
 		}
-		return maxEarnTotal;
+
+		return result;
 	}
 }
