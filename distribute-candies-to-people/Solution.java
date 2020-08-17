b0d8a7a06fba5620603f9afc2684b879af9687f6
Kai Wang <charles.wangkai@gmail.com>
2020-08-17 23:34:05 +0800

    Distribute Candies to People

--- distribute-candies-to-people/Solution.java
+++ distribute-candies-to-people/Solution.java
@@ -1,4 +1,4 @@
-public class Solution {
+class Solution {
 	public int[] distributeCandies(int candies, int num_people) {
 		int[] result = new int[num_people];
 		int index = 0;
@@ -8,7 +8,7 @@ public class Solution {
 			result[index] += targetNum;
 			candies -= targetNum;
 
-			targetNum++;
+			++targetNum;
 			index = (index + 1) % result.length;
 		}
 		result[index] += candies;
