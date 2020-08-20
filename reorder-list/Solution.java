b13087516ada47f706ed130f2bfd4edf5e8e035f
Kai Wang <charles.wangkai@gmail.com>
2020-08-20 22:34:02 +0800

    Reorder List

--- reorder-list/Solution.java
+++ reorder-list/Solution.java
@@ -3,29 +3,37 @@ class ListNode {
 	int val;
 	ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
 	}
 }
 
-public class Solution {
+class Solution {
 	public void reorderList(ListNode head) {
 		if (head == null) {
 			return;
 		}
 
 		int length = findLength(head);
+
 		ListNode node = head;
-		for (int i = 0; i < (length - 1) / 2; i++) {
+		for (int i = 0; i < (length - 1) / 2; ++i) {
 			node = node.next;
 		}
 		ListNode backHead = node.next;
 		node.next = null;
-
 		backHead = reverse(backHead);
 
-		for (ListNode p = head; backHead != null;) {
+		ListNode p = head;
+		while (backHead != null) {
 			ListNode q = backHead;
 			backHead = backHead.next;
 			q.next = p.next;
@@ -37,19 +45,21 @@ public class Solution {
 	int findLength(ListNode head) {
 		int length = 0;
 		for (ListNode node = head; node != null; node = node.next) {
-			length++;
+			++length;
 		}
+
 		return length;
 	}
 
 	ListNode reverse(ListNode head) {
-		ListNode tempHead = new ListNode(0);
+		ListNode tempHead = new ListNode();
 		while (head != null) {
 			ListNode node = head;
 			head = head.next;
 			node.next = tempHead.next;
 			tempHead.next = node;
 		}
+
 		return tempHead.next;
 	}
 }
