784adaf6b5eaa773f704727c43f5fda4ebb6ac83
Kai Wang <charles.wangkai@gmail.com>
2020-08-25 20:15:30 +0800

    Minimum Cost For Tickets

--- minimum-cost-for-tickets/Solution.java
+++ minimum-cost-for-tickets/Solution.java
@@ -1,11 +1,12 @@
-public class Solution {
+class Solution {
 	public int mincostTickets(int[] days, int[] costs) {
 		int[] minCosts = new int[days.length + 1];
 
-		for (int i = 1; i < minCosts.length; i++) {
-			minCosts[i] = minCosts[findLastLength(days, i, 1)] + costs[0];
-			minCosts[i] = Math.min(minCosts[i], minCosts[findLastLength(days, i, 7)] + costs[1]);
-			minCosts[i] = Math.min(minCosts[i], minCosts[findLastLength(days, i, 30)] + costs[2]);
+		for (int i = 1; i < minCosts.length; ++i) {
+			minCosts[i] = Math.min(
+					Math.min(minCosts[findLastLength(days, i, 1)] + costs[0],
+							minCosts[findLastLength(days, i, 7)] + costs[1]),
+					minCosts[findLastLength(days, i, 30)] + costs[2]);
 		}
 
 		return minCosts[days.length];
@@ -13,9 +14,10 @@ public class Solution {
 
 	int findLastLength(int[] days, int length, int pass) {
 		int lastLength = length - 1;
-		while (lastLength > 0 && days[length - 1] - days[lastLength - 1] + 1 <= pass) {
-			lastLength--;
+		while (lastLength != 0 && days[length - 1] - days[lastLength - 1] + 1 <= pass) {
+			--lastLength;
 		}
+
 		return lastLength;
 	}
 }
