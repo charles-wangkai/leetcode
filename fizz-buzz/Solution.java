317eb64b85b4c4fff053c3f44d718ca6087fc9d9
Kai Wang <charles.wangkai@gmail.com>
2020-08-26 18:47:34 +0800

    Fizz Buzz

--- fizz-buzz/Solution.java
+++ fizz-buzz/Solution.java
@@ -2,12 +2,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
+class Solution {
 	public List<String> fizzBuzz(int n) {
-		return IntStream.rangeClosed(1, n).mapToObj(this::output).collect(Collectors.toList());
-	}
-
-	String output(int x) {
-		return x % 15 == 0 ? "FizzBuzz" : (x % 3 == 0 ? "Fizz" : (x % 5 == 0 ? "Buzz" : String.valueOf(x)));
+		return IntStream.rangeClosed(1, n).mapToObj(x -> {
+			if (x % 15 == 0) {
+				return "FizzBuzz";
+			} else if (x % 3 == 0) {
+				return "Fizz";
+			} else if (x % 5 == 0) {
+				return "Buzz";
+			} else {
+				return String.valueOf(x);
+			}
+		}).collect(Collectors.toList());
 	}
 }
