89717e17a9f70b6da600e25f806bc7b1485fd9c8
Kai Wang <charles.wangkai@gmail.com>
2020-08-28 00:17:46 +0800

    Find Right Interval

--- find-right-interval/Solution.java
+++ find-right-interval/Solution.java
@@ -1,56 +1,36 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
-// Definition for an interval.
-class Interval {
-	int start;
-	int end;
-
-	Interval() {
-		start = 0;
-		end = 0;
-	}
-
-	Interval(int s, int e) {
-		start = s;
-		end = e;
-	}
-}
-
-public class Solution {
-	public int[] findRightInterval(Interval[] intervals) {
-		IndexAndInterval[] iis = new IndexAndInterval[intervals.length];
-		for (int i = 0; i < iis.length; i++) {
-			iis[i] = new IndexAndInterval(i, intervals[i]);
-		}
-
-		Arrays.sort(iis);
+class Solution {
+	public int[] findRightInterval(int[][] intervals) {
+		Element[] elements = IntStream.range(0, intervals.length).mapToObj(i -> new Element(i, intervals[i])).sorted()
+				.toArray(Element[]::new);
 
 		int[] result = new int[intervals.length];
-		for (int i = 0; i < result.length; i++) {
-			int index = Arrays.binarySearch(iis, new IndexAndInterval(-1, new Interval(iis[i].interval.end, -1)));
+		for (int i = 0; i < result.length; ++i) {
+			int index = Arrays.binarySearch(elements, new Element(-1, new int[] { intervals[i][1], -1 }));
 			if (index < 0) {
 				index = -1 - index;
 			}
-			if (index == iis.length) {
-				index = -1;
-			}
-			result[iis[i].index] = (index >= 0) ? iis[index].index : index;
+
+			result[i] = (index == elements.length) ? -1 : elements[index].index;
 		}
+
 		return result;
 	}
 }
 
-class IndexAndInterval implements Comparable<IndexAndInterval> {
+class Element implements Comparable<Element> {
 	int index;
-	Interval interval;
+	int[] interval;
 
-	IndexAndInterval(int index, Interval interval) {
+	Element(int index, int[] interval) {
 		this.index = index;
 		this.interval = interval;
 	}
 
 	@Override
-	public int compareTo(IndexAndInterval other) {
-		return interval.start - other.interval.start;
+	public int compareTo(Element other) {
+		return Integer.compare(interval[0], other.interval[0]);
 	}
 }
