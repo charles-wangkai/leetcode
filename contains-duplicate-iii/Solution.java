67f533a7332df9f326f69efe1bae5beb4954780e
Kai Wang <charles.wangkai@gmail.com>
2020-09-02 20:39:37 +0800

    Contains Duplicate III

--- contains-duplicate-iii/Solution.java
+++ contains-duplicate-iii/Solution.java
@@ -1,24 +1,27 @@
+import java.util.NavigableSet;
 import java.util.TreeSet;
 
-public class Solution {
+class Solution {
 	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 		if (t < 0) {
 			return false;
 		}
 
-		TreeSet<Long> history = new TreeSet<Long>();
-		int begin = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (i - begin > k) {
-				history.remove((long) nums[begin]);
-				begin++;
+		NavigableSet<Long> history = new TreeSet<>();
+		int beginIndex = 0;
+		for (int i = 0; i < nums.length; ++i) {
+			if (i - beginIndex > k) {
+				history.remove((long) nums[beginIndex]);
+				++beginIndex;
 			}
-			if (!history.subSet((long) nums[i] - t, true, (long) nums[i] + t,
-					true).isEmpty()) {
+
+			if (!history.subSet((long) nums[i] - t, true, (long) nums[i] + t, true).isEmpty()) {
 				return true;
 			}
+
 			history.add((long) nums[i]);
 		}
+
 		return false;
 	}
 }
