6da0a358b319e8a961b451576bb40d66cf32b164
Kai Wang <charles.wangkai@gmail.com>
2020-09-03 20:46:51 +0800

    Repeated Substring Pattern

--- repeated-substring-pattern/Solution.java
+++ repeated-substring-pattern/Solution.java
@@ -1,23 +1,25 @@
-public class Solution {
-	public boolean repeatedSubstringPattern(String str) {
-		for (int length = 1; length * 2 <= str.length(); length++) {
-			if (isRepeat(str, str.substring(0, length))) {
+class Solution {
+	public boolean repeatedSubstringPattern(String s) {
+		for (int patternLength = 1; patternLength * 2 <= s.length(); ++patternLength) {
+			if (isRepeat(s, patternLength)) {
 				return true;
 			}
 		}
+
 		return false;
 	}
 
-	boolean isRepeat(String str, String sub) {
-		if (str.length() % sub.length() != 0) {
+	boolean isRepeat(String str, int patternLength) {
+		if (str.length() % patternLength != 0) {
 			return false;
 		}
 
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) != sub.charAt(i % sub.length())) {
+		for (int i = 0; i < str.length(); ++i) {
+			if (str.charAt(i) != str.charAt(i % patternLength)) {
 				return false;
 			}
 		}
+
 		return true;
 	}
 }
