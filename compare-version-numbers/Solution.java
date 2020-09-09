d1c2fbb92a92cfddc8a29048f11c2935c92043eb
Kai Wang <charles.wangkai@gmail.com>
2020-09-10 00:19:04 +0800

    Compare Version Numbers

--- compare-version-numbers/Solution.java
+++ compare-version-numbers/Solution.java
@@ -1,30 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public class Solution {
+class Solution {
 	public int compareVersion(String version1, String version2) {
-		List<Integer> numbers1 = convertToNumbers(version1);
-		List<Integer> numbers2 = convertToNumbers(version2);
-		for (int i = 0; i < numbers1.size() || i < numbers2.size(); i++) {
+		int[] numbers1 = convertToNumbers(version1);
+		int[] numbers2 = convertToNumbers(version2);
+		for (int i = 0; i < numbers1.length || i < numbers2.length; ++i) {
 			int number1 = getNumber(numbers1, i);
 			int number2 = getNumber(numbers2, i);
-			int diff = number1 - number2;
-			if (diff != 0) {
-				return (int) Math.signum(diff);
+			if (number1 != number2) {
+				return Integer.compare(number1, number2);
 			}
 		}
+
 		return 0;
 	}
 
-	List<Integer> convertToNumbers(String version) {
-		List<Integer> numbers = new ArrayList<Integer>();
-		for (String part : version.split("\\.")) {
-			numbers.add(Integer.parseInt(part));
-		}
-		return numbers;
+	int[] convertToNumbers(String version) {
+		return Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
 	}
 
-	int getNumber(List<Integer> numbers, int index) {
-		return (index < numbers.size()) ? numbers.get(index) : 0;
+	int getNumber(int[] numbers, int index) {
+		return (index < numbers.length) ? numbers[index] : 0;
 	}
 }
