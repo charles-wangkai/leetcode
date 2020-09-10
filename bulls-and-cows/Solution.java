d0a7e4cb34bc509cb8ff33f64127feb1d767b88a
Kai Wang <charles.wangkai@gmail.com>
2020-09-10 19:34:45 +0800

    Bulls and Cows

--- bulls-and-cows/Solution.java
+++ bulls-and-cows/Solution.java
@@ -1,21 +1,22 @@
 import java.util.stream.IntStream;
 
-public class Solution {
+class Solution {
 	public String getHint(String secret, String guess) {
 		int[] secretCounts = new int[10];
 		int[] guessCounts = new int[10];
 		int bull = 0;
-		for (int i = 0; i < secret.length(); i++) {
+		for (int i = 0; i < secret.length(); ++i) {
 			int secretDigit = secret.charAt(i) - '0';
 			int guessDigit = guess.charAt(i) - '0';
 			if (secretDigit == guessDigit) {
-				bull++;
+				++bull;
 			} else {
-				secretCounts[secretDigit]++;
-				guessCounts[guessDigit]++;
+				++secretCounts[secretDigit];
+				++guessCounts[guessDigit];
 			}
 		}
 		int cow = IntStream.range(0, secretCounts.length).map(i -> Math.min(secretCounts[i], guessCounts[i])).sum();
+
 		return String.format("%dA%dB", bull, cow);
 	}
 }
