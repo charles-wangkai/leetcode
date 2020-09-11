95ce3f55e37e9c8d1267ffbc2c1c75f6434c7723
Kai Wang <charles.wangkai@gmail.com>
2020-09-11 21:59:11 +0800

    Customer Who Visited but Did Not Make Any Transactions

---
+++ customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+SELECT
+    v.customer_id,
+    COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT OUTER JOIN Transactions t
+ON t.visit_id = v.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
