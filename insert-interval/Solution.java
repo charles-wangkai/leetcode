a696e2ae38425c04d46f67cd1f72299e894f826e
Kai Wang <charles.wangkai@gmail.com>
2020-09-14 01:12:26 +0800

    Insert Interval

--- insert-interval/Solution.java
+++ insert-interval/Solution.java
@@ -1,48 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Definition for an interval.
-class Interval {
-	int start;
-	int end;
-
-	Interval() {
-		start = 0;
-		end = 0;
-	}
-
-	Interval(int s, int e) {
-		start = s;
-		end = e;
-	}
-}
-
-public class Solution {
-	public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-		List<Interval> inserted = new ArrayList<Interval>();
+class Solution {
+	public int[][] insert(int[][] intervals, int[] newInterval) {
+		List<int[]> inserted = new ArrayList<>();
 		boolean used = false;
-		for (int i = 0; i < intervals.size() || !used; i++) {
-			Interval selected;
-			if (i < intervals.size()
-					&& (used || intervals.get(i).start < newInterval.start)) {
-				selected = intervals.get(i);
+		for (int i = 0; i < intervals.length || !used; ++i) {
+			int[] selected;
+			if (i < intervals.length && (used || intervals[i][0] < newInterval[0])) {
+				selected = intervals[i];
 			} else {
 				selected = newInterval;
 				used = true;
-				i--;
+				--i;
 			}
 
-			if (inserted.isEmpty()
-					|| inserted.get(inserted.size() - 1).end < selected.start) {
+			if (inserted.isEmpty() || inserted.get(inserted.size() - 1)[1] < selected[0]) {
 				inserted.add(selected);
 			} else {
-				Interval last = inserted.get(inserted.size() - 1);
-				if (last.end < selected.end) {
-					inserted.remove(inserted.size() - 1);
-					inserted.add(new Interval(last.start, selected.end));
-				}
+				int[] last = inserted.get(inserted.size() - 1);
+				last[1] = Math.max(last[1], selected[1]);
 			}
 		}
-		return inserted;
+
+		return inserted.toArray(new int[0][]);
 	}
 }
