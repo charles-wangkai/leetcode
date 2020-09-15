1b65dbc2d14eb8481eac02da1ff9b491c6e96867
Kai Wang <charles.wangkai@gmail.com>
2020-09-15 22:25:25 +0800

    Inorder Successor in BST II

--- inorder-successor-in-bst-ii/Solution.java
+++ inorder-successor-in-bst-ii/Solution.java
@@ -6,24 +6,26 @@ class Node {
 	public Node parent;
 }
 
-public class Solution {
-	public Node inorderSuccessor(Node x) {
-		if (x == null) {
+class Solution {
+	public Node inorderSuccessor(Node node) {
+		if (node == null) {
 			return null;
 		}
 
-		if (x.right != null) {
-			Node result = x.right;
+		if (node.right != null) {
+			Node result = node.right;
 			while (result.left != null) {
 				result = result.left;
 			}
+
 			return result;
 		}
 
-		Node child = x;
+		Node child = node;
 		while (child.parent != null && child.parent.right == child) {
 			child = child.parent;
 		}
+
 		return child.parent;
 	}
 }
