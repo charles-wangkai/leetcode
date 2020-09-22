e8215a0b7abb01fef7fa066625b8df00ab915b22
Kai Wang <charles.wangkai@gmail.com>
2020-09-23 01:35:11 +0800

    Majority Element II

--- majority-element-ii/Solution.java
+++ majority-element-ii/Solution.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
+class Solution {
 	public List<Integer> majorityElement(int[] nums) {
 		int candidate1 = 0;
 		int count1 = 0;
@@ -12,29 +13,29 @@ public class Solution {
 				if (count2 <= 0 || num != candidate2) {
 					candidate1 = num;
 					count1 += 2;
-					count2--;
+					--count2;
 				} else {
-					count1--;
+					--count1;
 					count2 += 2;
 				}
 			} else if (count2 <= 0) {
 				if (num == candidate1) {
 					count1 += 2;
-					count2--;
+					--count2;
 				} else {
 					candidate2 = num;
-					count1--;
+					--count1;
 					count2 += 2;
 				}
 			} else if (num == candidate1) {
 				count1 += 2;
-				count2--;
+				--count2;
 			} else if (num == candidate2) {
-				count1--;
+				--count1;
 				count2 += 2;
 			} else {
-				count1--;
-				count2--;
+				--count1;
+				--count2;
 			}
 		}
 
@@ -45,16 +46,11 @@ public class Solution {
 		if (count2 > 0 && isMajority(nums, candidate2)) {
 			majorities.add(candidate2);
 		}
+
 		return majorities;
 	}
 
 	boolean isMajority(int[] nums, int candidate) {
-		int count = 0;
-		for (int num : nums) {
-			if (num == candidate) {
-				count++;
-			}
-		}
-		return count > nums.length / 3;
+		return Arrays.stream(nums).filter(num -> num == candidate).count() > nums.length / 3;
 	}
 }
