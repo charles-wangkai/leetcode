b77c38e9c23b9013694ba79469a967eca642c47f
Kai Wang <charles.wangkai@gmail.com>
2020-09-26 01:13:30 +0800

    Largest Number

--- largest-number/Solution.java
+++ largest-number/Solution.java
@@ -1,42 +1,14 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.stream.Collectors;
 
-public class Solution {
+class Solution {
 	public String largestNumber(int[] num) {
-		if (allZeros(num)) {
+		if (Arrays.stream(num).allMatch(x -> x == 0)) {
 			return "0";
 		}
 
-		Integer[] numbers = new Integer[num.length];
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = num[i];
-		}
-		Arrays.sort(numbers, new LargerComparator());
-
-		StringBuilder sb = new StringBuilder();
-		for (Integer number : numbers) {
-			sb.append(number);
-		}
-		return sb.toString();
-	}
-
-	boolean allZeros(int[] num) {
-		for (int oneNum : num) {
-			if (oneNum != 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-}
-
-class LargerComparator implements Comparator<Integer> {
-	@Override
-	public int compare(Integer num1, Integer num2) {
-		return (int) Math.signum(concat(num2, num1) - concat(num1, num2));
-	}
-
-	long concat(int num1, int num2) {
-		return Long.parseLong(num1 + "" + num2);
+		return Arrays.stream(num).boxed()
+				.sorted((x, y) -> -String.format("%d%d", x, y).compareTo(String.format("%d%d", y, x)))
+				.map(String::valueOf).collect(Collectors.joining());
 	}
 }
