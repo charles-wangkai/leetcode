baef55232814c7b318be567dd64bcdb84067f9e4
Kai Wang <charles.wangkai@gmail.com>
2020-09-26 18:19:44 +0800

    Teemo Attacking

--- teemo-attacking/Solution.java
+++ teemo-attacking/Solution.java
@@ -1,13 +1,14 @@
-public class Solution {
+class Solution {
 	public int findPoisonedDuration(int[] timeSeries, int duration) {
 		int result = 0;
-		for (int i = 0; i < timeSeries.length; i++) {
-			if (i + 1 < timeSeries.length && timeSeries[i + 1] - timeSeries[i] < duration) {
+		for (int i = 0; i < timeSeries.length; ++i) {
+			if (i + 1 != timeSeries.length && timeSeries[i + 1] - timeSeries[i] < duration) {
 				result += timeSeries[i + 1] - timeSeries[i];
 			} else {
 				result += duration;
 			}
 		}
+
 		return result;
 	}
 }
