03d9fdfaa2f384d3259d2e7a565a8609512b0789
Kai Wang <charles.wangkai@gmail.com>
2020-09-30 22:43:50 +0800

    First Missing Positive

--- first-missing-positive/Solution.java
+++ first-missing-positive/Solution.java
@@ -1,17 +1,17 @@
-public class Solution {
+class Solution {
 	public int firstMissingPositive(int[] A) {
-		for (int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; ++i) {
 			if (A[i] <= 0) {
 				A[i] = Integer.MAX_VALUE;
 			}
 		}
-		for (int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; ++i) {
 			int number = Math.abs(A[i]);
 			if (number <= A.length) {
 				A[number - 1] = -Math.abs(A[number - 1]);
 			}
 		}
-		for (int i = 0;; i++) {
+		for (int i = 0;; ++i) {
 			if (i == A.length || A[i] > 0) {
 				return i + 1;
 			}
