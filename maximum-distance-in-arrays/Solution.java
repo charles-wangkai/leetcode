2bba4c8d552c32cb05b533c9c27611f86f7c6fdd
Kai Wang <charles.wangkai@gmail.com>
2020-10-02 01:37:09 +0800

    Maximum Distance in Arrays

--- maximum-distance-in-arrays/Solution.java
+++ maximum-distance-in-arrays/Solution.java
@@ -1,16 +1,15 @@
-import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public int maxDistance(List<List<Integer>> arrays) {
-		List<Integer> mins = arrays.stream().mapToInt(array -> array.get(0)).sorted().collect(ArrayList<Integer>::new,
-				List::add, List::addAll);
+class Solution {
+  public int maxDistance(List<List<Integer>> arrays) {
+    int[] sortedMins = arrays.stream().mapToInt(array -> array.get(0)).sorted().toArray();
 
-		int result = 0;
-		for (List<Integer> array : arrays) {
-			result = Math.max(result,
-					array.get(array.size() - 1) - ((array.get(0) == mins.get(0)) ? mins.get(1) : mins.get(0)));
-		}
-		return result;
-	}
+    return arrays.stream()
+        .mapToInt(
+            array ->
+                array.get(array.size() - 1)
+                    - ((array.get(0) == sortedMins[0]) ? sortedMins[1] : sortedMins[0]))
+        .max()
+        .getAsInt();
+  }
 }
