244ff5f512f5458f265459a3f859a391ddc00822
Kai Wang <charles.wangkai@gmail.com>
2020-10-02 01:39:47 +0800

    Number of Recent Calls

--- number-of-recent-calls/RecentCounter.java
+++ number-of-recent-calls/RecentCounter.java
@@ -1,18 +1,18 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class RecentCounter {
-	Queue<Integer> times = new LinkedList<>();
+class RecentCounter {
+  Queue<Integer> times = new LinkedList<>();
 
-	public int ping(int t) {
-		times.offer(t);
+  public int ping(int t) {
+    times.offer(t);
 
-		while (times.peek() < t - 3000) {
-			times.poll();
-		}
+    while (t - times.peek() > 3000) {
+      times.poll();
+    }
 
-		return times.size();
-	}
+    return times.size();
+  }
 }
 
 // Your RecentCounter object will be instantiated and called as such:
