bf9b62ec38ad71418a16141a74c865647c65bb94
Kai Wang <charles.wangkai@gmail.com>
2020-10-05 00:35:52 +0800

    Remove Covered Intervals

--- remove-covered-intervals/Solution.java
+++ remove-covered-intervals/Solution.java
@@ -1,18 +1,21 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int removeCoveredIntervals(int[][] intervals) {
-		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+class Solution {
+  public int removeCoveredIntervals(int[][] intervals) {
+    Arrays.sort(
+        intervals,
+        (i1, i2) ->
+            (i1[0] != i2[0]) ? Integer.compare(i1[0], i2[0]) : -Integer.compare(i1[1], i2[1]));
 
-		int result = 0;
-		int right = -1;
-		for (int[] interval : intervals) {
-			if (interval[1] > right) {
-				right = interval[1];
-				result++;
-			}
-		}
+    int result = 0;
+    int right = -1;
+    for (int[] interval : intervals) {
+      if (interval[1] > right) {
+        right = interval[1];
+        ++result;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
