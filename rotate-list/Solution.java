3026b1c125e9453f027c39a95b05966117eac73e
Kai Wang <charles.wangkai@gmail.com>
2020-10-07 19:59:08 +0800

    Rotate List

--- rotate-list/Solution.java
+++ rotate-list/Solution.java
@@ -1,41 +1,50 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode rotateRight(ListNode head, int n) {
-		int length = findLen(head);
-		if (length == 0 || (n %= length) == 0) {
-			return head;
-		}
+class Solution {
+  public ListNode rotateRight(ListNode head, int k) {
+    int length = computeLength(head);
+    if (length == 0 || k % length == 0) {
+      return head;
+    }
+    k %= length;
 
-		ListNode leftTail = head;
-		for (int i = 0; i < length - n - 1; i++) {
-			leftTail = leftTail.next;
-		}
-		ListNode rightHead = leftTail.next;
-		leftTail.next = null;
-		ListNode rightTail = rightHead;
-		while (rightTail.next != null) {
-			rightTail = rightTail.next;
-		}
-		rightTail.next = head;
-		return rightHead;
-	}
+    ListNode leftTail = head;
+    for (int i = 0; i < length - k - 1; ++i) {
+      leftTail = leftTail.next;
+    }
+    ListNode rightHead = leftTail.next;
+    leftTail.next = null;
+    ListNode rightTail = rightHead;
+    while (rightTail.next != null) {
+      rightTail = rightTail.next;
+    }
+    rightTail.next = head;
 
-	int findLen(ListNode head) {
-		int length = 0;
-		while (head != null) {
-			length++;
-			head = head.next;
-		}
-		return length;
-	}
+    return rightHead;
+  }
+
+  int computeLength(ListNode head) {
+    int length = 0;
+    while (head != null) {
+      ++length;
+      head = head.next;
+    }
+
+    return length;
+  }
 }
