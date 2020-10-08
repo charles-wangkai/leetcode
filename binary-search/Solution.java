7bd48801d4f82f5a5bf49bb09088052866b02f18
Kai Wang <charles.wangkai@gmail.com>
2020-10-08 20:24:33 +0800

    Binary Search

--- binary-search/Solution.java
+++ binary-search/Solution.java
@@ -1,18 +1,19 @@
-public class Solution {
-	public int search(int[] nums, int target) {
-		int lowerIndex = 0;
-		int upperIndex = nums.length - 1;
-		while (lowerIndex <= upperIndex) {
-			int middleIndex = (lowerIndex + upperIndex) / 2;
+class Solution {
+  public int search(int[] nums, int target) {
+    int lowerIndex = 0;
+    int upperIndex = nums.length - 1;
+    while (lowerIndex <= upperIndex) {
+      int middleIndex = (lowerIndex + upperIndex) / 2;
 
-			if (nums[middleIndex] == target) {
-				return middleIndex;
-			} else if (nums[middleIndex] < target) {
-				lowerIndex = middleIndex + 1;
-			} else {
-				upperIndex = middleIndex - 1;
-			}
-		}
-		return -1;
-	}
+      if (nums[middleIndex] < target) {
+        lowerIndex = middleIndex + 1;
+      } else if (nums[middleIndex] > target) {
+        upperIndex = middleIndex - 1;
+      } else {
+        return middleIndex;
+      }
+    }
+
+    return -1;
+  }
 }
