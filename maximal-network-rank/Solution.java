56c85d9a1e1f35cab10fbbf1591b7e3b2f73cd71
Kai Wang <charles.wangkai@gmail.com>
2020-10-11 12:54:03 +0800

    Maximal Network Rank

---
+++ maximal-network-rank/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int maximalNetworkRank(int n, int[][] roads) {
+    int[] counts = new int[n];
+    int[][] connections = new int[n][n];
+    for (int[] road : roads) {
+      ++counts[road[0]];
+      ++counts[road[1]];
+
+      ++connections[road[0]][road[1]];
+      ++connections[road[1]][road[0]];
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i != j) {
+          result = Math.max(result, counts[i] + counts[j] - connections[i][j]);
+        }
+      }
+    }
+
+    return result;
+  }
+}
