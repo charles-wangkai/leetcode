c6d1457895e6b9ff7d7dfe93ad6cea362c6615eb
Kai Wang <charles.wangkai@gmail.com>
2020-10-12 19:31:23 +0800

    Buddy Strings

--- buddy-strings/Solution.java
+++ buddy-strings/Solution.java
@@ -1,36 +1,36 @@
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public boolean buddyStrings(String A, String B) {
-		if (A.length() != B.length()) {
-			return false;
-		}
+class Solution {
+  public boolean buddyStrings(String A, String B) {
+    if (A.length() != B.length()) {
+      return false;
+    }
 
-		Map<Character, Integer> letterToCountA = buildLetterToCount(A);
-		Map<Character, Integer> letterToCountB = buildLetterToCount(B);
+    Map<Character, Integer> letterToCountA = buildLetterToCount(A);
+    Map<Character, Integer> letterToCountB = buildLetterToCount(B);
 
-		if (!letterToCountA.equals(letterToCountB)) {
-			return false;
-		}
+    if (!letterToCountA.equals(letterToCountB)) {
+      return false;
+    }
 
-		if (A.equals(B)) {
-			return letterToCountA.values().stream().anyMatch(count -> count >= 2);
-		}
+    if (A.equals(B)) {
+      return letterToCountA.values().stream().anyMatch(count -> count >= 2);
+    }
 
-		List<Integer> diffIndices = IntStream.range(0, A.length()).filter(i -> A.charAt(i) != B.charAt(i)).boxed()
-				.collect(Collectors.toList());
-		return diffIndices.size() == 2 && A.charAt(diffIndices.get(0)) == B.charAt(diffIndices.get(1));
-	}
+    int[] diffIndices =
+        IntStream.range(0, A.length()).filter(i -> A.charAt(i) != B.charAt(i)).toArray();
 
-	Map<Character, Integer> buildLetterToCount(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
-		return letterToCount;
-	}
+    return diffIndices.length == 2 && A.charAt(diffIndices[0]) == B.charAt(diffIndices[1]);
+  }
+
+  Map<Character, Integer> buildLetterToCount(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
+
+    return letterToCount;
+  }
 }
