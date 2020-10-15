0ca9ac416098c67d448bea7e88db853e6a89aa0c
Kai Wang <charles.wangkai@gmail.com>
2020-10-15 20:37:34 +0800

    Meeting Rooms II

--- meeting-rooms-ii/Solution.java
+++ meeting-rooms-ii/Solution.java
@@ -1,36 +1,20 @@
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-// Definition for an interval.
-class Interval {
-	int start;
-	int end;
+class Solution {
+  public int minMeetingRooms(int[][] intervals) {
+    Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
 
-	Interval() {
-		start = 0;
-		end = 0;
-	}
+    int maxMeetingNum = 0;
+    PriorityQueue<Integer> ends = new PriorityQueue<>();
+    for (int[] interval : intervals) {
+      while (!ends.isEmpty() && ends.peek() <= interval[0]) {
+        ends.poll();
+      }
+      ends.offer(interval[1]);
+      maxMeetingNum = Math.max(maxMeetingNum, ends.size());
+    }
 
-	Interval(int s, int e) {
-		start = s;
-		end = e;
-	}
-}
-
-public class Solution {
-	public int minMeetingRooms(Interval[] intervals) {
-		Arrays.sort(intervals, (interval1, interval2) -> interval1.start
-				- interval2.start);
-
-		int maxMeetingNum = 0;
-		PriorityQueue<Integer> ends = new PriorityQueue<Integer>();
-		for (Interval interval : intervals) {
-			while (!ends.isEmpty() && ends.peek() <= interval.start) {
-				ends.poll();
-			}
-			ends.offer(interval.end);
-			maxMeetingNum = Math.max(maxMeetingNum, ends.size());
-		}
-		return maxMeetingNum;
-	}
+    return maxMeetingNum;
+  }
 }
