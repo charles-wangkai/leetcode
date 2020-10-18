05346d7bfd26f9205aebd54346ca5f821f450c29
Kai Wang <charles.wangkai@gmail.com>
2020-10-18 15:48:32 +0800

    Best Time to Buy and Sell Stock IV

--- best-time-to-buy-and-sell-stock-iv/Solution.java
+++ best-time-to-buy-and-sell-stock-iv/Solution.java
@@ -1,36 +1,33 @@
-public class Solution {
-	public int maxProfit(int k, int[] prices) {
-		if (k >= prices.length - 1) {
-			return maxProfitWithUnlimitedTransactions(prices);
-		}
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-		int[] inProfits = new int[k + 1];
-		int[] offProfits = new int[k + 1];
-		for (int i = 0; i < prices.length - 1; i++) {
-			int priceDelta = prices[i + 1] - prices[i];
-			int[] nextInProfits = new int[inProfits.length];
-			int[] nextOffProfits = new int[offProfits.length];
-			for (int j = 1; j < nextInProfits.length; j++) {
-				nextInProfits[j] = Math.max(inProfits[j], offProfits[j - 1])
-						+ priceDelta;
-				nextOffProfits[j] = Math.max(inProfits[j], offProfits[j]);
-			}
-			inProfits = nextInProfits;
-			offProfits = nextOffProfits;
-		}
+class Solution {
+  public int maxProfit(int k, int[] prices) {
+    if (k >= prices.length - 1) {
+      return maxProfitWithUnlimitedTransactions(prices);
+    }
 
-		int maxEarn = 0;
-		for (int i = 0; i < inProfits.length; i++) {
-			maxEarn = Math.max(maxEarn, Math.max(inProfits[i], offProfits[i]));
-		}
-		return maxEarn;
-	}
+    int[] inProfits = new int[k + 1];
+    int[] offProfits = new int[k + 1];
+    for (int i = 0; i < prices.length - 1; ++i) {
+      int priceDelta = prices[i + 1] - prices[i];
+      int[] nextInProfits = new int[inProfits.length];
+      int[] nextOffProfits = new int[offProfits.length];
+      for (int j = 1; j < nextInProfits.length; ++j) {
+        nextInProfits[j] = Math.max(inProfits[j], offProfits[j - 1]) + priceDelta;
+        nextOffProfits[j] = Math.max(inProfits[j], offProfits[j]);
+      }
 
-	int maxProfitWithUnlimitedTransactions(int[] prices) {
-		int maxEarn = 0;
-		for (int i = 0; i < prices.length - 1; i++) {
-			maxEarn += Math.max(0, prices[i + 1] - prices[i]);
-		}
-		return maxEarn;
-	}
+      inProfits = nextInProfits;
+      offProfits = nextOffProfits;
+    }
+
+    return IntStream.concat(Arrays.stream(inProfits), Arrays.stream(offProfits)).max().orElse(0);
+  }
+
+  int maxProfitWithUnlimitedTransactions(int[] prices) {
+    return IntStream.range(0, prices.length - 1)
+        .map(i -> Math.max(0, prices[i + 1] - prices[i]))
+        .sum();
+  }
 }
