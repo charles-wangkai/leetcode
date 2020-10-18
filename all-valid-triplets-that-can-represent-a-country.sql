e32b2a8ed3914eba3eafc69da623e69b2cf26586
Kai Wang <charles.wangkai@gmail.com>
2020-10-18 16:46:38 +0800

    All Valid Triplets That Can Represent a Country

---
+++ all-valid-triplets-that-can-represent-a-country.sql
@@ -0,0 +1,13 @@
+SELECT
+  a.student_name AS member_A,
+  b.student_name AS member_B,
+  c.student_name AS member_C
+FROM SchoolA a
+JOIN SchoolB b
+JOIN SchoolC c
+WHERE a.student_id <> b.student_id
+AND b.student_id <> c.student_id
+AND c.student_id <> a.student_id
+AND a.student_name <> b.student_name
+AND b.student_name <> c.student_name
+AND c.student_name <> a.student_name
