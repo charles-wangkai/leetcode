9c5227aac26e6fef8ecce1cdb3f89620b7b34880
Kai Wang <charles.wangkai@gmail.com>
2020-10-21 21:33:29 +0800

    Asteroid Collision

--- asteroid-collision/Solution.java
+++ asteroid-collision/Solution.java
@@ -1,35 +1,39 @@
 import java.util.Stack;
 
-public class Solution {
-	public int[] asteroidCollision(int[] asteroids) {
-		Stack<Integer> stack = new Stack<Integer>();
+class Solution {
+  public int[] asteroidCollision(int[] asteroids) {
+    Stack<Integer> stack = new Stack<>();
 
-		for (int asteroid : asteroids) {
-			if (asteroid > 0) {
-				stack.push(asteroid);
-			} else {
-				while (true) {
-					if (stack.empty()) {
-						stack.push(asteroid);
-						break;
-					}
+    for (int asteroid : asteroids) {
+      if (asteroid > 0) {
+        stack.push(asteroid);
+      } else {
+        while (true) {
+          if (stack.empty()) {
+            stack.push(asteroid);
 
-					int top = stack.peek();
-					if (top < 0) {
-						stack.push(asteroid);
-						break;
-					} else if (top + asteroid == 0) {
-						stack.pop();
-						break;
-					} else if (top + asteroid < 0) {
-						stack.pop();
-					} else {
-						break;
-					}
-				}
-			}
-		}
+            break;
+          }
 
-		return stack.stream().mapToInt(x -> x).toArray();
-	}
+          int top = stack.peek();
+          if (top < 0) {
+            stack.push(asteroid);
+
+            break;
+          } else if (top + asteroid == 0) {
+            stack.pop();
+
+            break;
+          } else if (top + asteroid < 0) {
+            stack.pop();
+
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    return stack.stream().mapToInt(x -> x).toArray();
+  }
 }
