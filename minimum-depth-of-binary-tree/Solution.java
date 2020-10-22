9975eff8ef1fde30f86bad83620a99cac6909385
Kai Wang <charles.wangkai@gmail.com>
2020-10-22 21:56:45 +0800

    Minimum Depth of Binary Tree

--- minimum-depth-of-binary-tree/Solution.java
+++ minimum-depth-of-binary-tree/Solution.java
@@ -1,24 +1,33 @@
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public int minDepth(TreeNode root) {
-		if (root == null) {
-			return 0;
-		}
-		if (root.left == null && root.right == null) {
-			return 1;
-		}
-		return 1 + Math.min(root.left == null ? Integer.MAX_VALUE
-				: minDepth(root.left), root.right == null ? Integer.MAX_VALUE
-				: minDepth(root.right));
-	}
+class Solution {
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    if (root.left == null && root.right == null) {
+      return 1;
+    }
+    return 1
+        + Math.min(
+            root.left == null ? Integer.MAX_VALUE : minDepth(root.left),
+            root.right == null ? Integer.MAX_VALUE : minDepth(root.right));
+  }
 }
