c8c766380b0fd7246ffa7cc31e443f905d371816
Kai Wang <charles.wangkai@gmail.com>
2020-10-22 21:44:08 +0800

    Search in a Sorted Array of Unknown Size

--- search-in-a-sorted-array-of-unknown-size/Solution.java
+++ search-in-a-sorted-array-of-unknown-size/Solution.java
@@ -1,25 +1,26 @@
-class ArrayReader {
-	int get(int k) {
-		throw new UnsupportedOperationException();
-	}
+// This is ArrayReader's API interface.
+// You should not implement it, or speculate about its implementation
+interface ArrayReader {
+  public int get(int index);
 }
 
-public class Solution {
-	public int search(ArrayReader reader, int target) {
-		int lowerIndex = 0;
-		int upperIndex = Integer.MAX_VALUE;
-		while (lowerIndex <= upperIndex) {
-			int middleIndex = (lowerIndex + upperIndex) / 2;
+class Solution {
+  public int search(ArrayReader reader, int target) {
+    int lowerIndex = 0;
+    int upperIndex = Integer.MAX_VALUE;
+    while (lowerIndex <= upperIndex) {
+      int middleIndex = (lowerIndex + upperIndex) / 2;
 
-			int number = reader.get(middleIndex);
-			if (number == target) {
-				return middleIndex;
-			} else if (number < target) {
-				lowerIndex = middleIndex + 1;
-			} else {
-				upperIndex = middleIndex - 1;
-			}
-		}
-		return -1;
-	}
+      int number = reader.get(middleIndex);
+      if (number == target) {
+        return middleIndex;
+      } else if (number < target) {
+        lowerIndex = middleIndex + 1;
+      } else {
+        upperIndex = middleIndex - 1;
+      }
+    }
+
+    return -1;
+  }
 }
