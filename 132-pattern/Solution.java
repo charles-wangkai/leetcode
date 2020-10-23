8f3917e6475d5d6e6bd145c8026a7fe1e47d167d
Kai Wang <charles.wangkai@gmail.com>
2020-10-23 21:50:57 +0800

    132 Pattern

--- 132-pattern/Solution.java
+++ 132-pattern/Solution.java
@@ -1,32 +1,32 @@
 import java.util.Stack;
 
-public class Solution {
-	public boolean find132pattern(int[] nums) {
-		Stack<Range> stack = new Stack<Range>();
-		for (int num : nums) {
-			Range current = new Range(num, num);
-			while (!stack.empty() && current.max > stack.peek().min) {
-				current.min = Math.min(current.min, stack.peek().min);
-				current.max = Math.max(current.max, stack.peek().max);
+class Solution {
+  public boolean find132pattern(int[] nums) {
+    Stack<Range> stack = new Stack<Range>();
+    for (int num : nums) {
+      Range current = new Range(num, num);
+      while (!stack.empty() && current.max > stack.peek().min) {
+        current.min = Math.min(current.min, stack.peek().min);
+        current.max = Math.max(current.max, stack.peek().max);
 
-				stack.pop();
-			}
-			stack.push(current);
+        stack.pop();
+      }
+      stack.push(current);
 
-			if (num > current.min && num < current.max) {
-				return true;
-			}
-		}
-		return false;
-	}
+      if (num > current.min && num < current.max) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 class Range {
-	int min;
-	int max;
+  int min;
+  int max;
 
-	Range(int min, int max) {
-		this.min = min;
-		this.max = max;
-	}
+  Range(int min, int max) {
+    this.min = min;
+    this.max = max;
+  }
 }
