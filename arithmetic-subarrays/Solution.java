c56cb1674c6aa8a5d7b4b95532a1d7c2b8d40678
Kai Wang <charles.wangkai@gmail.com>
2020-10-25 16:05:32 +0800

    Arithmetic Subarrays

---
+++ arithmetic-subarrays/Solution.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+  public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+    return IntStream.range(0, l.length)
+        .mapToObj(
+            i -> {
+              int[] sorted = IntStream.rangeClosed(l[i], r[i]).map(j -> nums[j]).sorted().toArray();
+
+              return IntStream.range(0, sorted.length - 1)
+                  .allMatch(j -> sorted[j + 1] - sorted[j] == sorted[1] - sorted[0]);
+            })
+        .collect(Collectors.toList());
+  }
+}
