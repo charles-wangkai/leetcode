c9f93be657ed657fc4ff0341782fa2e342f34d0b
Kai Wang <charles.wangkai@gmail.com>
2020-10-26 16:42:34 +0800

    Champagne Tower

--- champagne-tower/Solution.java
+++ champagne-tower/Solution.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
+class Solution {
+  public double champagneTower(int poured, int query_row, int query_glass) {
+    double[] row = {poured};
+    while (row.length < query_row + 1) {
+      double[] nextRow = new double[row.length + 1];
 
-public class Solution {
-	public double champagneTower(int poured, int query_row, int query_glass) {
-		double[] row = { poured };
-		while (row.length < query_row + 1) {
-			double[] excesses = Arrays.stream(row).map(p -> Math.max(0, p - 1)).toArray();
+      for (int i = 0; i < row.length; ++i) {
+        nextRow[i] += Math.max(0, row[i] - 1) / 2;
+        nextRow[i + 1] += Math.max(0, row[i] - 1) / 2;
+      }
 
-			row = new double[excesses.length + 1];
-			for (int i = 0; i < excesses.length; i++) {
-				row[i] += excesses[i] / 2;
-				row[i + 1] += excesses[i] / 2;
-			}
-		}
-		return Math.min(1, row[query_glass]);
-	}
+      row = nextRow;
+    }
+
+    return Math.min(1, row[query_glass]);
+  }
 }
