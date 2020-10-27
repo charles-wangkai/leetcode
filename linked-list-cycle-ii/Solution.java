e0d2f265f72a241101b7078b2c7409c984502f33
Kai Wang <charles.wangkai@gmail.com>
2020-10-27 23:36:01 +0800

    Linked List Cycle II

--- linked-list-cycle-ii/Solution.java
+++ linked-list-cycle-ii/Solution.java
@@ -1,31 +1,33 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
 }
 
-public class Solution {
-	public ListNode detectCycle(ListNode head) {
-		ListNode fast = head;
-		ListNode slow = head;
-		while (fast != null && fast.next != null) {
-			fast = fast.next.next;
-			slow = slow.next;
-			if (fast == slow) {
-				ListNode p1 = head;
-				ListNode p2 = slow;
-				while (p1 != p2) {
-					p1 = p1.next;
-					p2 = p2.next;
-				}
-				return p1;
-			}
-		}
-		return null;
-	}
+class Solution {
+  public ListNode detectCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        ListNode p1 = head;
+        ListNode p2 = slow;
+        while (p1 != p2) {
+          p1 = p1.next;
+          p2 = p2.next;
+        }
+
+        return p1;
+      }
+    }
+
+    return null;
+  }
 }
