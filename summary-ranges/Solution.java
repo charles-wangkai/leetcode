4baf4e04e3398afcbdd1db3bbb51e1eb64d12754
Kai Wang <charles.wangkai@gmail.com>
2020-10-28 21:45:36 +0800

    Summary Ranges

--- summary-ranges/Solution.java
+++ summary-ranges/Solution.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public List<String> summaryRanges(int[] nums) {
-		List<String> summary = new ArrayList<String>();
-		int begin = 0;
-		Integer end = null;
-		for (int i = 0; i <= nums.length; i++) {
-			if (i == nums.length || end == null || nums[i] != end + 1) {
-				if (end != null) {
-					if (begin == end) {
-						summary.add(begin + "");
-					} else {
-						summary.add(begin + "->" + end);
-					}
-				}
+class Solution {
+  public List<String> summaryRanges(int[] nums) {
+    List<String> summary = new ArrayList<>();
+    int begin = 0;
+    Integer end = null;
+    for (int i = 0; i <= nums.length; ++i) {
+      if (i == nums.length || end == null || nums[i] != end + 1) {
+        if (end != null) {
+          if (begin == end) {
+            summary.add(String.valueOf(begin));
+          } else {
+            summary.add(String.format("%d->%d", begin, end));
+          }
+        }
 
-				if (i != nums.length) {
-					begin = nums[i];
-					end = nums[i];
-				}
-			} else {
-				end = nums[i];
-			}
-		}
-		return summary;
-	}
+        if (i != nums.length) {
+          begin = nums[i];
+          end = nums[i];
+        }
+      } else {
+        end = nums[i];
+      }
+    }
+
+    return summary;
+  }
 }
