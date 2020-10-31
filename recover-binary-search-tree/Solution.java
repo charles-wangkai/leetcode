a33dec02f92df0d6740efed70db8fa0e454f3527
Kai Wang <charles.wangkai@gmail.com>
2020-10-31 22:09:41 +0800

    Recover Binary Search Tree

--- recover-binary-search-tree/Solution.java
+++ recover-binary-search-tree/Solution.java
@@ -1,39 +1,50 @@
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	TreeNode first;
-	TreeNode second;
-	TreeNode prev;
+class Solution {
+  TreeNode first;
+  TreeNode second;
+  TreeNode prev;
 
-	public void recoverTree(TreeNode root) {
-		inorder(root);
+  public void recoverTree(TreeNode root) {
+    inorder(root);
 
-		int temp = first.val;
-		first.val = second.val;
-		second.val = temp;
-	}
+    int temp = first.val;
+    first.val = second.val;
+    second.val = temp;
+  }
 
-	void inorder(TreeNode node) {
-		if (node == null) {
-			return;
-		}
-		inorder(node.left);
-		if (first == null && prev != null && node.val < prev.val) {
-			first = prev;
-		}
-		if (first != null && node.val <= first.val) {
-			second = node;
-		}
-		prev = node;
-		inorder(node.right);
-	}
+  void inorder(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+
+    inorder(node.left);
+
+    if (first == null && prev != null && node.val < prev.val) {
+      first = prev;
+    }
+    if (first != null && node.val < first.val) {
+      second = node;
+    }
+    prev = node;
+
+    inorder(node.right);
+  }
 }
