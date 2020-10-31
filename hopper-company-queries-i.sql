3b6203f28febc148cd44b50a8bda3d82e67f75be
Kai Wang <charles.wangkai@gmail.com>
2020-10-31 23:19:02 +0800

    Hopper Company Queries I

---
+++ hopper-company-queries-i.sql
@@ -0,0 +1,78 @@
+SELECT
+  t1.month,
+  t1.active_drivers,
+  t2.accepted_rides
+FROM (
+  SELECT
+    m.month,
+    COUNT(d.join_date) AS active_drivers
+  FROM (
+    SELECT 1 AS month
+    UNION
+    SELECT 2
+    UNION
+    SELECT 3
+    UNION
+    SELECT 4
+    UNION
+    SELECT 5
+    UNION
+    SELECT 6
+    UNION
+    SELECT 7
+    UNION
+    SELECT 8
+    UNION
+    SELECT 9
+    UNION
+    SELECT 10
+    UNION
+    SELECT 11
+    UNION
+    SELECT 12
+  ) m
+  LEFT OUTER JOIN Drivers d
+  ON d.join_date < DATE_ADD(CONCAT('2020-', m.month, '-01'), INTERVAL 1 MONTH)
+  GROUP BY m.month
+) t1
+JOIN (
+  SELECT
+    m.month,
+    COUNT(ar.requested_at) AS accepted_rides
+  FROM (
+    SELECT 1 AS month
+    UNION
+    SELECT 2
+    UNION
+    SELECT 3
+    UNION
+    SELECT 4
+    UNION
+    SELECT 5
+    UNION
+    SELECT 6
+    UNION
+    SELECT 7
+    UNION
+    SELECT 8
+    UNION
+    SELECT 9
+    UNION
+    SELECT 10
+    UNION
+    SELECT 11
+    UNION
+    SELECT 12
+  ) m
+  LEFT OUTER JOIN (
+    SELECT r.requested_at
+    FROM Rides r
+    JOIN AcceptedRides a
+    ON a.ride_id = r.ride_id
+    WHERE YEAR(r.requested_at) = 2020
+  ) ar
+  ON MONTH(ar.requested_at) = m.month
+  GROUP BY m.month
+) t2
+ON t2.month = t1.month
+ORDER BY t1.month
