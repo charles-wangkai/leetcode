153f2827e32d3300051745a7a35331e9501eda9a
Kai Wang <charles.wangkai@gmail.com>
2020-11-03 21:05:12 +0800

    Consecutive Characters

--- consecutive-characters/Solution.java
+++ consecutive-characters/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int maxPower(String s) {
-        int result = 1;
-        int count = 1;
-        for (int i = 1; i < s.length(); ++i) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                ++count;
-                result = Math.max(result, count);
-            } else {
-                count = 1;
-            }
-        }
-
-        return result;
+  public int maxPower(String s) {
+    int result = 1;
+    int count = 1;
+    for (int i = 1; i < s.length(); ++i) {
+      if (s.charAt(i) == s.charAt(i - 1)) {
+        ++count;
+        result = Math.max(result, count);
+      } else {
+        count = 1;
+      }
     }
+
+    return result;
+  }
 }
