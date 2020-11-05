f98b54041829f7bb9ddec8f54ad815ea85529aa2
Kai Wang <charles.wangkai@gmail.com>
2020-11-05 20:26:22 +0800

    Minimum Cost to Move Chips to The Same Position

--- play-with-chips/Solution.java
+++ play-with-chips/Solution.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int minCostToMoveChips(int[] chips) {
-		return (int) Math.min(Arrays.stream(chips).filter(chip -> chip % 2 == 0).count(),
-				Arrays.stream(chips).filter(chip -> chip % 2 != 0).count());
-	}
+class Solution {
+  public int minCostToMoveChips(int[] chips) {
+    return (int)
+        Math.min(
+            Arrays.stream(chips).filter(chip -> chip % 2 == 0).count(),
+            Arrays.stream(chips).filter(chip -> chip % 2 != 0).count());
+  }
 }
