9a858d51498cf58b9196468afa6315754b18cb85
Kai Wang <charles.wangkai@gmail.com>
2020-11-06 21:34:05 +0800

    Find the Smallest Divisor Given a Threshold

--- find-the-smallest-divisor-given-a-threshold/Solution.java
+++ find-the-smallest-divisor-given-a-threshold/Solution.java
@@ -1,26 +1,27 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int smallestDivisor(int[] nums, int threshold) {
-		int result = -1;
-		int lower = 1;
-		int upper = Arrays.stream(nums).max().getAsInt();
+class Solution {
+  public int smallestDivisor(int[] nums, int threshold) {
+    int result = -1;
+    int lower = 1;
+    int upper = Arrays.stream(nums).max().getAsInt();
 
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
 
-			if (check(nums, threshold, middle)) {
-				result = middle;
-				upper = middle - 1;
-			} else {
-				lower = middle + 1;
-			}
-		}
+      if (check(nums, threshold, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	boolean check(int[] nums, int threshold, int divisor) {
-		return Arrays.stream(nums).map(x -> x / divisor + (x % divisor == 0 ? 0 : 1)).sum() <= threshold;
-	}
+  boolean check(int[] nums, int threshold, int divisor) {
+    return Arrays.stream(nums).map(x -> (x + divisor - 1) / divisor).asLongStream().sum()
+        <= threshold;
+  }
 }
