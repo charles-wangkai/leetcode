49589a7912abd8ec013c5ea60fd0e593ce9b6904
Kai Wang <charles.wangkai@gmail.com>
2020-11-08 16:21:41 +0800

    Two Sum Less Than K

--- two-sum-less-than-k/Solution.java
+++ two-sum-less-than-k/Solution.java
@@ -1,8 +1,11 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int twoSumLessThanK(int[] A, int K) {
-		return IntStream.range(0, A.length).flatMap(i -> IntStream.range(i + 1, A.length).map(j -> A[i] + A[j]))
-				.filter(x -> x < K).max().orElse(-1);
-	}
+class Solution {
+  public int twoSumLessThanK(int[] A, int K) {
+    return IntStream.range(0, A.length)
+        .flatMap(i -> IntStream.range(i + 1, A.length).map(j -> A[i] + A[j]))
+        .filter(x -> x < K)
+        .max()
+        .orElse(-1);
+  }
 }
