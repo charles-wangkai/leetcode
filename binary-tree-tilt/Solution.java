d3503b9b5060cbe24b03e4bdd40440d6d61d29e6
Kai Wang <charles.wangkai@gmail.com>
2020-11-08 16:27:43 +0800

    Binary Tree Tilt

--- binary-tree-tilt/Solution.java
+++ binary-tree-tilt/Solution.java
@@ -1,33 +1,42 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	int tiltSum;
+class Solution {
+  int tiltSum;
 
-	public int findTilt(TreeNode root) {
-		tiltSum = 0;
-		search(root);
-		return tiltSum;
-	}
+  public int findTilt(TreeNode root) {
+    tiltSum = 0;
+    search(root);
 
-	int search(TreeNode node) {
-		if (node == null) {
-			return 0;
-		}
+    return tiltSum;
+  }
 
-		int leftSum = search(node.left);
-		int rightSum = search(node.right);
+  int search(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
 
-		tiltSum += Math.abs(leftSum - rightSum);
+    int leftSum = search(node.left);
+    int rightSum = search(node.right);
 
-		return node.val + leftSum + rightSum;
-	}
+    tiltSum += Math.abs(leftSum - rightSum);
+
+    return node.val + leftSum + rightSum;
+  }
 }
