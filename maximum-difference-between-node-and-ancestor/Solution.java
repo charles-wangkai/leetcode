8105c524e56c79b01ba956db12b3ad116eed6203
Kai Wang <charles.wangkai@gmail.com>
2020-11-09 22:18:27 +0800

    Maximum Difference Between Node and Ancestor

--- maximum-difference-between-node-and-ancestor/Solution.java
+++ maximum-difference-between-node-and-ancestor/Solution.java
@@ -1,29 +1,39 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public int maxAncestorDiff(TreeNode root) {
-		return search(root, root.val, root.val);
-	}
+class Solution {
+  public int maxAncestorDiff(TreeNode root) {
+    return search(root, root.val, root.val);
+  }
 
-	int search(TreeNode node, int minAncestor, int maxAncestor) {
-		if (node == null) {
-			return -1;
-		}
+  int search(TreeNode node, int minAncestor, int maxAncestor) {
+    if (node == null) {
+      return -1;
+    }
 
-		int nextMinAncestor = Math.min(minAncestor, node.val);
-		int nextMaxAncestor = Math.max(maxAncestor, node.val);
+    int nextMinAncestor = Math.min(minAncestor, node.val);
+    int nextMaxAncestor = Math.max(maxAncestor, node.val);
 
-		return Math.max(Math.max(Math.abs(minAncestor - node.val), Math.abs(maxAncestor - node.val)),
-				Math.max(search(node.left, nextMinAncestor, nextMaxAncestor),
-						search(node.right, nextMinAncestor, nextMaxAncestor)));
-	}
+    return Math.max(
+        Math.max(Math.abs(minAncestor - node.val), Math.abs(maxAncestor - node.val)),
+        Math.max(
+            search(node.left, nextMinAncestor, nextMaxAncestor),
+            search(node.right, nextMinAncestor, nextMaxAncestor)));
+  }
 }
