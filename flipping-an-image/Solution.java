ba5425e89eb5bbe61f1d428b6451d78bff313d37
Kai Wang <charles.wangkai@gmail.com>
2020-11-10 21:44:51 +0800

    Flipping an Image

--- flipping-an-image/Solution.java
+++ flipping-an-image/Solution.java
@@ -1,12 +1,13 @@
-public class Solution {
-	public int[][] flipAndInvertImage(int[][] A) {
-		int size = A.length;
-		int[][] result = new int[size][size];
-		for (int r = 0; r < size; r++) {
-			for (int c = 0; c < size; c++) {
-				result[r][c] = 1 - A[r][size - 1 - c];
-			}
-		}
-		return result;
-	}
+class Solution {
+  public int[][] flipAndInvertImage(int[][] A) {
+    int size = A.length;
+    int[][] result = new int[size][size];
+    for (int r = 0; r < size; ++r) {
+      for (int c = 0; c < size; ++c) {
+        result[r][c] = 1 - A[r][size - 1 - c];
+      }
+    }
+
+    return result;
+  }
 }
