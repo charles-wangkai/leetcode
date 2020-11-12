ba7e0240ffeee7f7d8d465de455fd4ab77ae4707
Kai Wang <charles.wangkai@gmail.com>
2020-11-12 21:55:39 +0800

    Permutations II

--- permutations-ii/Solution.java
+++ permutations-ii/Solution.java
@@ -2,38 +2,41 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public List<List<Integer>> permuteUnique(int[] num) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		search(result, num, 0);
-		return result;
-	}
+class Solution {
+  public List<List<Integer>> permuteUnique(int[] num) {
+    List<List<Integer>> permutations = new ArrayList<>();
+    search(permutations, num, 0);
 
-	void search(List<List<Integer>> result, int[] num, int index) {
-		if (index == num.length) {
-			List<Integer> permutation = new ArrayList<Integer>();
-			for (int oneNum : num) {
-				permutation.add(oneNum);
-			}
-			result.add(permutation);
-			return;
-		}
-		Set<Integer> used = new HashSet<Integer>();
-		for (int i = index; i < num.length; i++) {
-			if (used.contains(num[i])) {
-				continue;
-			}
-			used.add(num[i]);
-			swap(num, index, i);
-			search(result, num, index + 1);
-			swap(num, index, i);
-		}
-	}
+    return permutations;
+  }
 
-	void swap(int[] a, int index1, int index2) {
-		int temp = a[index1];
-		a[index1] = a[index2];
-		a[index2] = temp;
-	}
+  void search(List<List<Integer>> permutations, int[] num, int index) {
+    if (index == num.length) {
+      permutations.add(
+          IntStream.range(0, num.length).map(i -> num[i]).boxed().collect(Collectors.toList()));
+
+      return;
+    }
+
+    Set<Integer> used = new HashSet<>();
+    for (int i = index; i < num.length; ++i) {
+      if (used.contains(num[i])) {
+        continue;
+      }
+      used.add(num[i]);
+
+      swap(num, index, i);
+      search(permutations, num, index + 1);
+      swap(num, index, i);
+    }
+  }
+
+  void swap(int[] a, int index1, int index2) {
+    int temp = a[index1];
+    a[index1] = a[index2];
+    a[index2] = temp;
+  }
 }
