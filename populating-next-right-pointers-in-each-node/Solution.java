4a8db36ded99c885509d52b47d0775568016105e
Kai Wang <charles.wangkai@gmail.com>
2020-11-13 23:19:17 +0800

    Populating Next Right Pointers in Each Node

--- populating-next-right-pointers-in-each-node/Solution.java
+++ populating-next-right-pointers-in-each-node/Solution.java
@@ -1,27 +1,37 @@
-// Definition for binary tree with next pointer.
-class TreeLinkNode {
-	int val;
-	TreeLinkNode left, right, next;
+// Definition for a Node.
+class Node {
+  public int val;
+  public Node left;
+  public Node right;
+  public Node next;
 
-	TreeLinkNode(int x) {
-		val = x;
-	}
+  public Node() {}
+
+  public Node(int _val) {
+    val = _val;
+  }
+
+  public Node(int _val, Node _left, Node _right, Node _next) {
+    val = _val;
+    left = _left;
+    right = _right;
+    next = _next;
+  }
 }
 
-public class Solution {
-	public void connect(TreeLinkNode root) {
-		if (root == null) {
-			return;
-		}
-		for (TreeLinkNode head = root; head.left != null; head = head.left) {
-			TreeLinkNode prev = null;
-			for (TreeLinkNode node = head; node != null; node = node.next) {
-				node.left.next = node.right;
-				if (prev != null) {
-					prev.next = node.left;
-				}
-				prev = node.right;
-			}
-		}
-	}
+class Solution {
+  public Node connect(Node root) {
+    for (Node head = root; head != null && head.left != null; head = head.left) {
+      Node prev = null;
+      for (Node node = head; node != null; node = node.next) {
+        node.left.next = node.right;
+        if (prev != null) {
+          prev.next = node.left;
+        }
+        prev = node.right;
+      }
+    }
+
+    return root;
+  }
 }
