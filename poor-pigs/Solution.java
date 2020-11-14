cb9e0ed76dabc570ed76521f02cc3c60cc96404b
Kai Wang <charles.wangkai@gmail.com>
2020-11-14 16:18:09 +0800

    Poor Pigs

--- poor-pigs/Solution.java
+++ poor-pigs/Solution.java
@@ -1,12 +1,13 @@
-public class Solution {
-	public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-		int base = minutesToTest / minutesToDie + 1;
-		int pigNum = 0;
-		int power = 1;
-		while (power < buckets) {
-			power *= base;
-			pigNum++;
-		}
-		return pigNum;
-	}
+class Solution {
+  public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    int base = minutesToTest / minutesToDie + 1;
+    int pigNum = 0;
+    int power = 1;
+    while (power < buckets) {
+      power *= base;
+      ++pigNum;
+    }
+
+    return pigNum;
+  }
 }
