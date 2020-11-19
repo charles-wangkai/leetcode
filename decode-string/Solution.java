9b7d9f94c4f1929c2a5fbf18dbc07aca3d999863
Kai Wang <charles.wangkai@gmail.com>
2020-11-19 22:21:19 +0800

    Decode String

--- decode-string/Solution.java
+++ decode-string/Solution.java
@@ -1,41 +1,41 @@
-public class Solution {
-	public String decodeString(String s) {
-		int level = 0;
-		int startIndex = -1;
-		int k = 0;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (ch == '[') {
-				if (level == 0) {
-					startIndex = i;
-				}
+class Solution {
+  public String decodeString(String s) {
+    int level = 0;
+    int startIndex = -1;
+    int k = 0;
 
-				level++;
-			} else if (ch == ']') {
-				level--;
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < s.length(); ++i) {
+      char ch = s.charAt(i);
+      if (ch == '[') {
+        if (level == 0) {
+          startIndex = i;
+        }
 
-				if (level == 0) {
-					sb.append(repeat(decodeString(s.substring(startIndex + 1, i)), k));
-					k = 0;
-				}
-			} else if (Character.isDigit(ch)) {
-				if (level == 0) {
-					k = k * 10 + (ch - '0');
-				}
-			} else if (level == 0) {
-				sb.append(ch);
-			}
-		}
-		return sb.toString();
-	}
+        ++level;
+      } else if (ch == ']') {
+        --level;
 
-	String repeat(String str, int k) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < k; i++) {
-			sb.append(str);
-		}
-		return sb.toString();
-	}
+        if (level == 0) {
+          result.append(repeat(decodeString(s.substring(startIndex + 1, i)), k));
+          k = 0;
+        }
+      } else if (level == 0) {
+        if (Character.isDigit(ch)) {
+          k = k * 10 + (ch - '0');
+        } else {
+          result.append(ch);
+        }
+      }
+    }
+
+    return result.toString();
+  }
+
+  String repeat(String str, int k) {
+    return IntStream.range(0, k).mapToObj(i -> str).collect(Collectors.joining());
+  }
 }
