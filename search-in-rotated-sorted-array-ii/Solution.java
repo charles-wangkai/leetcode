50870181fcedf5823ddd907944b87eae7533127c
Kai Wang <charles.wangkai@gmail.com>
2020-11-20 20:12:11 +0800

    Search in Rotated Sorted Array II

--- search-in-rotated-sorted-array-ii/Solution.java
+++ search-in-rotated-sorted-array-ii/Solution.java
@@ -1,20 +1,21 @@
-public class Solution {
-	public boolean search(int[] A, int target) {
-		return search(A, target, 0, A.length - 1);
-	}
+class Solution {
+  public boolean search(int[] A, int target) {
+    return search(A, target, 0, A.length - 1);
+  }
 
-	boolean search(int[] A, int target, int lower, int upper) {
-		if (lower > upper) {
-			return false;
-		}
-		if (A[lower] < A[upper] && !(target >= A[lower] && target <= A[upper])) {
-			return false;
-		}
-		int middle = (lower + upper) / 2;
-		if (A[middle] == target) {
-			return true;
-		}
-		return search(A, target, middle + 1, upper)
-				|| search(A, target, lower, middle - 1);
-	}
+  boolean search(int[] A, int target, int lower, int upper) {
+    if (lower > upper) {
+      return false;
+    }
+    if (A[lower] < A[upper] && !(target >= A[lower] && target <= A[upper])) {
+      return false;
+    }
+
+    int middle = (lower + upper) / 2;
+    if (A[middle] == target) {
+      return true;
+    }
+
+    return search(A, target, lower, middle - 1) || search(A, target, middle + 1, upper);
+  }
 }
