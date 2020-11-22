0a8ed94b0d296404640d09c331dfbc24c516332a
Kai Wang <charles.wangkai@gmail.com>
2020-11-22 19:17:54 +0800

    Unique Morse Code Words

--- unique-morse-code-words/Solution.java
+++ unique-morse-code-words/Solution.java
@@ -1,15 +1,21 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Solution {
-	static final String[] MORSES = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
-			".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
-			"--.." };
+class Solution {
+  static final String[] MORSES = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+    "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+  };
 
-	public int uniqueMorseRepresentations(String[] words) {
-		return Arrays.stream(words)
-				.map(word -> String.join("",
-						word.chars().mapToObj(letter -> MORSES[letter - 'a']).collect(Collectors.toList())))
-				.collect(Collectors.toSet()).size();
-	}
+  public int uniqueMorseRepresentations(String[] words) {
+    return (int)
+        Arrays.stream(words)
+            .map(
+                word ->
+                    word.chars()
+                        .mapToObj(letter -> MORSES[letter - 'a'])
+                        .collect(Collectors.joining()))
+            .distinct()
+            .count();
+  }
 }
