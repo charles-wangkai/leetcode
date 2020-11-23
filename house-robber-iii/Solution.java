f5676623c17159ba8405c6bfd638547b4548b549
Kai Wang <charles.wangkai@gmail.com>
2020-11-23 20:10:10 +0800

    House Robber III

--- house-robber-iii/Solution.java
+++ house-robber-iii/Solution.java
@@ -1,38 +1,51 @@
-import java.util.HashMap;
-import java.util.Map;
-
-//Definition for a binary tree node.
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	Map<TreeNode, Integer> node2chosenMaxMoney = new HashMap<TreeNode, Integer>();
-	Map<TreeNode, Integer> node2notChosenMaxMoney = new HashMap<TreeNode, Integer>();
+class Solution {
+  public int rob(TreeNode root) {
+    Element subResult = search(root);
 
-	public int rob(TreeNode root) {
-		if (!node2chosenMaxMoney.containsKey(root)) {
-			search(root);
-		}
+    return Math.max(subResult.chosenSum, subResult.skipSum);
+  }
 
-		return Math.max(node2chosenMaxMoney.get(root), node2notChosenMaxMoney.get(root));
-	}
+  Element search(TreeNode node) {
+    if (node == null) {
+      return new Element(0, 0);
+    }
 
-	void search(TreeNode node) {
-		if (node == null) {
-			node2chosenMaxMoney.put(node, 0);
-			node2notChosenMaxMoney.put(node, 0);
-		} else {
-			node2chosenMaxMoney.put(node,
-					node.val + (node.left == null ? 0 : rob(node.left.left) + rob(node.left.right))
-							+ (node.right == null ? 0 : rob(node.right.left) + rob(node.right.right)));
-			node2notChosenMaxMoney.put(node, rob(node.left) + rob(node.right));
-		}
-	}
+    Element leftSubResult = search(node.left);
+    Element rightSubResult = search(node.right);
+    int chosenSum = node.val + leftSubResult.skipSum + rightSubResult.skipSum;
+    int skipSum =
+        Math.max(leftSubResult.chosenSum, leftSubResult.skipSum)
+            + Math.max(rightSubResult.chosenSum, rightSubResult.skipSum);
+
+    return new Element(chosenSum, skipSum);
+  }
+}
+
+class Element {
+  int chosenSum;
+  int skipSum;
+
+  Element(int chosenSum, int skipSum) {
+    this.chosenSum = chosenSum;
+    this.skipSum = skipSum;
+  }
 }
