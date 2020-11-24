3cab4acc5548b18091c69cd6fb066ef7537447e0
Kai Wang <charles.wangkai@gmail.com>
2020-11-24 19:45:59 +0800

    Basic Calculator II

--- basic-calculator-ii/Solution.java
+++ basic-calculator-ii/Solution.java
@@ -1,63 +1,66 @@
-public class Solution {
-	public int calculate(String s) {
-		long result = 0;
-		int sign = 1;
-		long term = 1;
+class Solution {
+  public int calculate(String s) {
+    long result = 0;
+    int sign = 1;
+    long term = -1;
 
-		Expression expr = new Expression(s);
-		String token;
-		while ((token = expr.readNext()) != null) {
-			if (token.equals("+") || token.equals("-")) {
-				result += sign * term;
-				term = 1;
-				sign = token.equals("+") ? 1 : -1;
-			} else if (token.equals("*") || token.equals("/")) {
-				long nextNumber = Long.parseLong(expr.readNext());
-				if (token.equals("*")) {
-					term *= nextNumber;
-				} else {
-					term /= nextNumber;
-				}
-			} else {
-				term = Long.parseLong(token);
-			}
-		}
-		result += sign * term;
+    Expression expr = new Expression(s);
+    while (true) {
+      String token = expr.readNext();
+      if (token == null) {
+        break;
+      }
 
-		return (int) result;
-	}
+      if (token.equals("+") || token.equals("-")) {
+        result += sign * term;
+        sign = token.equals("+") ? 1 : -1;
+      } else if (token.equals("*") || token.equals("/")) {
+        long nextNumber = Long.parseLong(expr.readNext());
+        if (token.equals("*")) {
+          term *= nextNumber;
+        } else {
+          term /= nextNumber;
+        }
+      } else {
+        term = Long.parseLong(token);
+      }
+    }
+    result += sign * term;
+
+    return (int) result;
+  }
 }
 
 class Expression {
-	private String str;
-	private int index = 0;
+  private String str;
+  private int index = 0;
 
-	public Expression(String str) {
-		this.str = str;
-	}
+  public Expression(String str) {
+    this.str = str;
+  }
 
-	public String readNext() {
-		while (index < str.length() && str.charAt(index) == ' ') {
-			index++;
-		}
+  public String readNext() {
+    while (index < str.length() && str.charAt(index) == ' ') {
+      ++index;
+    }
 
-		if (index == str.length()) {
-			return null;
-		}
+    if (index == str.length()) {
+      return null;
+    }
 
-		char ch = str.charAt(index);
-		if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-			index++;
-			return ch + "";
-		}
+    char ch = str.charAt(index);
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      ++index;
+      return String.valueOf(ch);
+    }
 
-		int nextIndex = index + 1;
-		while (nextIndex < str.length()
-				&& Character.isDigit(str.charAt(nextIndex))) {
-			nextIndex++;
-		}
-		String number = str.substring(index, nextIndex);
-		index = nextIndex;
-		return number;
-	}
+    int nextIndex = index + 1;
+    while (nextIndex < str.length() && Character.isDigit(str.charAt(nextIndex))) {
+      ++nextIndex;
+    }
+    String number = str.substring(index, nextIndex);
+    index = nextIndex;
+
+    return number;
+  }
 }
