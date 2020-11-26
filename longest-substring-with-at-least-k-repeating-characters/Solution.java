e8cd0f0d0257d7919d2eb6d7fedc7a585ef717d7
Kai Wang <charles.wangkai@gmail.com>
2020-11-26 20:58:53 +0800

    Longest Substring with At Least K Repeating Characters

--- longest-substring-with-at-least-k-repeating-characters/Solution.java
+++ longest-substring-with-at-least-k-repeating-characters/Solution.java
@@ -1,29 +1,27 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int longestSubstring(String s, int k) {
-		if (s.isEmpty()) {
-			return 0;
-		}
+class Solution {
+  public int longestSubstring(String s, int k) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		Map<Character, Integer> letter2count = new HashMap<Character, Integer>();
-		for (char letter : s.toCharArray()) {
-			if (!letter2count.containsKey(letter)) {
-				letter2count.put(letter, 0);
-			}
-			letter2count.put(letter, letter2count.get(letter) + 1);
-		}
+    int result = 0;
+    int beginIndex = 0;
+    for (int i = 0; i <= s.length(); ++i) {
+      if (i == s.length() || letterToCount.get(s.charAt(i)) < k) {
+        result =
+            Math.max(
+                result,
+                (i == s.length() && beginIndex == 0)
+                    ? s.length()
+                    : longestSubstring(s.substring(beginIndex, i), k));
+        beginIndex = i + 1;
+      }
+    }
 
-		int result = 0;
-		int beginIndex = 0;
-		for (int i = 0; i <= s.length(); i++) {
-			if (i == s.length() || letter2count.get(s.charAt(i)) < k) {
-				result = Math.max(result, (i == s.length() && beginIndex == 0) ? s.length()
-						: longestSubstring(s.substring(beginIndex, i), k));
-				beginIndex = i + 1;
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 }
