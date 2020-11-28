6ef14d40d05693e411d4d2cb877246640e53e2e0
Kai Wang <charles.wangkai@gmail.com>
2020-11-29 00:15:52 +0800

    Sliding Window Maximum

--- sliding-window-maximum/Solution.java
+++ sliding-window-maximum/Solution.java
@@ -1,27 +1,24 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class Solution {
-	public int[] maxSlidingWindow(int[] nums, int k) {
-		if (nums.length == 0) {
-			return new int[0];
-		}
+class Solution {
+  public int[] maxSlidingWindow(int[] nums, int k) {
+    int[] result = new int[nums.length - k + 1];
+    Deque<Integer> indices = new ArrayDeque<>();
+    for (int i = 0; i < nums.length; ++i) {
+      while (!indices.isEmpty() && indices.peekFirst() <= i - k) {
+        indices.pollFirst();
+      }
+      while (!indices.isEmpty() && nums[indices.peekLast()] <= nums[i]) {
+        indices.pollLast();
+      }
+      indices.offerLast(i);
 
-		int[] maxs = new int[nums.length - k + 1];
-		Deque<Integer> indices = new ArrayDeque<Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			while (!indices.isEmpty() && indices.peekFirst() <= i - k) {
-				indices.pollFirst();
-			}
-			while (!indices.isEmpty() && nums[indices.peekLast()] <= nums[i]) {
-				indices.pollLast();
-			}
-			indices.offerLast(i);
+      if (i - k + 1 >= 0) {
+        result[i - k + 1] = nums[indices.peekFirst()];
+      }
+    }
 
-			if (i - k + 1 >= 0) {
-				maxs[i - k + 1] = nums[indices.peekFirst()];
-			}
-		}
-		return maxs;
-	}
+    return result;
+  }
 }
