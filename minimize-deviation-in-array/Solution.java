89927bdb0befc8838f6cd1803cfed86dee2587d9
Kai Wang <charles.wangkai@gmail.com>
2020-11-29 17:23:19 +0800

    Minimize Deviation in Array

---
+++ minimize-deviation-in-array/Solution.java
@@ -0,0 +1,45 @@
+import java.util.PriorityQueue;
+
+class Solution {
+  public int minimumDeviation(int[] nums) {
+    PriorityQueue<Element> pq =
+        new PriorityQueue<>((e1, e2) -> Integer.compare(e1.current, e2.current));
+    int max = -1;
+    for (int num : nums) {
+      int limit = (num % 2 == 0) ? num : (num * 2);
+      int current = num;
+      while (current % 2 == 0) {
+        current /= 2;
+      }
+
+      max = Math.max(max, current);
+      pq.offer(new Element(current, limit));
+    }
+
+    int result = Integer.MAX_VALUE;
+    while (true) {
+      Element head = pq.poll();
+      result = Math.min(result, max - head.current);
+
+      int next = head.current * 2;
+      if (next > head.limit) {
+        break;
+      }
+
+      max = Math.max(max, next);
+      pq.offer(new Element(next, head.limit));
+    }
+
+    return result;
+  }
+}
+
+class Element {
+  int current;
+  int limit;
+
+  Element(int current, int limit) {
+    this.current = current;
+    this.limit = limit;
+  }
+}
