4df00f71a9382eb9b4de679f9427ef394b25f983
Kai Wang <charles.wangkai@gmail.com>
2020-12-03 22:26:44 +0800

    Increasing Order Search Tree

--- increasing-order-search-tree/Solution.java
+++ increasing-order-search-tree/Solution.java
@@ -3,36 +3,45 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 public class Solution {
-	public TreeNode increasingBST(TreeNode root) {
-		List<TreeNode> nodes = new ArrayList<>();
-		inorderSearch(root, nodes);
+  public TreeNode increasingBST(TreeNode root) {
+    List<TreeNode> nodes = new ArrayList<>();
+    inorderSearch(nodes, root);
 
-		for (int i = 0; i < nodes.size(); i++) {
-			TreeNode node = nodes.get(i);
+    for (int i = 0; i < nodes.size(); ++i) {
+      TreeNode node = nodes.get(i);
 
-			node.left = null;
-			node.right = (i + 1 == nodes.size()) ? null : nodes.get(i + 1);
-		}
-		return nodes.get(0);
-	}
+      node.left = null;
+      node.right = (i + 1 == nodes.size()) ? null : nodes.get(i + 1);
+    }
 
-	void inorderSearch(TreeNode node, List<TreeNode> nodes) {
-		if (node.left != null) {
-			inorderSearch(node.left, nodes);
-		}
-		nodes.add(node);
-		if (node.right != null) {
-			inorderSearch(node.right, nodes);
-		}
-	}
+    return nodes.get(0);
+  }
+
+  void inorderSearch(List<TreeNode> nodes, TreeNode node) {
+    if (node == null) {
+      return;
+    }
+
+    inorderSearch(nodes, node.left);
+    nodes.add(node);
+    inorderSearch(nodes, node.right);
+  }
 }
