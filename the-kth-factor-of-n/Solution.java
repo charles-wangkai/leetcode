58e0d22ff54d5d801ecaeb122a64f4242fe21370
Kai Wang <charles.wangkai@gmail.com>
2020-12-04 21:27:15 +0800

    The kth Factor of n

--- the-kth-factor-of-n/Solution.java
+++ the-kth-factor-of-n/Solution.java
@@ -3,20 +3,20 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public int kthFactor(int n, int k) {
-        List<Integer> factors = new ArrayList<>();
-        for (int i = 1; i * i <= n; ++i) {
-            if (n % i == 0) {
-                factors.add(i);
+  public int kthFactor(int n, int k) {
+    List<Integer> factors = new ArrayList<>();
+    for (int i = 1; i * i <= n; ++i) {
+      if (n % i == 0) {
+        factors.add(i);
 
-                if (n / i != i) {
-                    factors.add(n / i);
-                }
-            }
+        if (n / i != i) {
+          factors.add(n / i);
         }
-
-        Collections.sort(factors);
-
-        return (factors.size() < k) ? -1 : factors.get(k - 1);
+      }
     }
+
+    Collections.sort(factors);
+
+    return (factors.size() < k) ? -1 : factors.get(k - 1);
+  }
 }
