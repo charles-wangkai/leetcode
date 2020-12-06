ca6f36fa6bf51d85e303a666a9c7010ece77335b
Kai Wang <charles.wangkai@gmail.com>
2020-12-06 19:55:07 +0800

    Populating Next Right Pointers in Each Node II

--- populating-next-right-pointers-in-each-node-ii/Solution.java
+++ populating-next-right-pointers-in-each-node-ii/Solution.java
@@ -1,39 +1,52 @@
-// Definition for binary tree with next pointer.
-class TreeLinkNode {
-	int val;
-	TreeLinkNode left, right, next;
+// Definition for a Node.
+class Node {
+  public int val;
+  public Node left;
+  public Node right;
+  public Node next;
 
-	TreeLinkNode(int x) {
-		val = x;
-	}
+  public Node() {}
+
+  public Node(int _val) {
+    val = _val;
+  }
+
+  public Node(int _val, Node _left, Node _right, Node _next) {
+    val = _val;
+    left = _left;
+    right = _right;
+    next = _next;
+  }
 }
 
-public class Solution {
-	public void connect(TreeLinkNode root) {
-		TreeLinkNode nextHead;
-		for (TreeLinkNode head = root; head != null; head = nextHead) {
-			nextHead = null;
-			TreeLinkNode prev = null;
-			for (TreeLinkNode node = head; node != null; node = node.next) {
-				if (node.left != null) {
-					if (nextHead == null) {
-						nextHead = node.left;
-					}
-					if (prev != null) {
-						prev.next = node.left;
-					}
-					prev = node.left;
-				}
-				if (node.right != null) {
-					if (nextHead == null) {
-						nextHead = node.right;
-					}
-					if (prev != null) {
-						prev.next = node.right;
-					}
-					prev = node.right;
-				}
-			}
-		}
-	}
+class Solution {
+  public Node connect(Node root) {
+    Node nextHead;
+    for (Node head = root; head != null; head = nextHead) {
+      nextHead = null;
+      Node prev = null;
+      for (Node node = head; node != null; node = node.next) {
+        if (node.left != null) {
+          if (nextHead == null) {
+            nextHead = node.left;
+          }
+          if (prev != null) {
+            prev.next = node.left;
+          }
+          prev = node.left;
+        }
+        if (node.right != null) {
+          if (nextHead == null) {
+            nextHead = node.right;
+          }
+          if (prev != null) {
+            prev.next = node.right;
+          }
+          prev = node.right;
+        }
+      }
+    }
+
+    return root;
+  }
 }
