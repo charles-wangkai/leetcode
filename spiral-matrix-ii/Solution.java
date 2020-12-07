b019124211fe5e4ba0e5b7b0742b37e91cb86bf6
Kai Wang <charles.wangkai@gmail.com>
2020-12-07 16:35:48 +0800

    Spiral Matrix II

--- spiral-matrix-ii/Solution.java
+++ spiral-matrix-ii/Solution.java
@@ -1,26 +1,29 @@
-public class Solution {
-	static final int[] OFFSET_R = { 0, 1, 0, -1 };
-	static final int[] OFFSET_C = { 1, 0, -1, 0 };
+class Solution {
+  static final int[] R_OFFSETS = {0, 1, 0, -1};
+  static final int[] C_OFFSETS = {1, 0, -1, 0};
 
-	public int[][] generateMatrix(int n) {
-		int[][] matrix = new int[n][n];
-		int r = 0;
-		int c = -1;
-		int direction = 0;
-		for (int i = 1; i <= n * n; i++) {
-			while (true) {
-				int nextR = r + OFFSET_R[direction];
-				int nextC = c + OFFSET_C[direction];
-				if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < n
-						&& matrix[nextR][nextC] == 0) {
-					r = nextR;
-					c = nextC;
-					break;
-				}
-				direction = (direction + 1) % OFFSET_R.length;
-			}
-			matrix[r][c] = i;
-		}
-		return matrix;
-	}
+  public int[][] generateMatrix(int n) {
+    int[][] matrix = new int[n][n];
+    int r = 0;
+    int c = -1;
+    int direction = 0;
+    for (int i = 1; i <= n * n; ++i) {
+      while (true) {
+        int nextR = r + R_OFFSETS[direction];
+        int nextC = c + C_OFFSETS[direction];
+        if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < n && matrix[nextR][nextC] == 0) {
+          r = nextR;
+          c = nextC;
+
+          break;
+        }
+
+        direction = (direction + 1) % R_OFFSETS.length;
+      }
+
+      matrix[r][c] = i;
+    }
+
+    return matrix;
+  }
 }
