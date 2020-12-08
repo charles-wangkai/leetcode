c254932b161fbab398dd9f4414b8916e812adcf7
Kai Wang <charles.wangkai@gmail.com>
2020-12-08 18:42:00 +0800

    Pairs of Songs With Total Durations Divisible by 60

--- pairs-of-songs-with-total-durations-divisible-by-60/Solution.java
+++ pairs-of-songs-with-total-durations-divisible-by-60/Solution.java
@@ -1,21 +1,22 @@
-public class Solution {
-	static final int MOD_DIVISOR = 60;
+class Solution {
+  static final int MODULUS = 60;
 
-	public int numPairsDivisibleBy60(int[] time) {
-		int[] remainders = new int[MOD_DIVISOR];
-		for (int x : time) {
-			remainders[x % MOD_DIVISOR]++;
-		}
+  public int numPairsDivisibleBy60(int[] time) {
+    int[] remainders = new int[MODULUS];
+    for (int x : time) {
+      ++remainders[x % MODULUS];
+    }
 
-		int result = 0;
-		for (int i = 0; i * 2 <= MOD_DIVISOR; i++) {
-			int other = (MOD_DIVISOR - i) % MOD_DIVISOR;
-			if (i == other) {
-				result += remainders[i] * (remainders[i] - 1L) / 2;
-			} else {
-				result += remainders[i] * remainders[other];
-			}
-		}
-		return result;
-	}
+    int result = 0;
+    for (int i = 0; i * 2 <= MODULUS; ++i) {
+      int other = (MODULUS - i) % MODULUS;
+      if (i == other) {
+        result += remainders[i] * (remainders[i] - 1L) / 2;
+      } else {
+        result += remainders[i] * remainders[other];
+      }
+    }
+
+    return result;
+  }
 }
