a1aa94356daba84ad83bb08167712a8807ab0aef
Kai Wang <charles.wangkai@gmail.com>
2020-12-18 20:54:10 +0800

    Increasing Triplet Subsequence

--- increasing-triplet-subsequence/Solution.java
+++ increasing-triplet-subsequence/Solution.java
@@ -1,27 +1,28 @@
-public class Solution {
-	public boolean increasingTriplet(int[] nums) {
-		Integer one = null;
-		Integer first = null;
-		Integer second = null;
+class Solution {
+  public boolean increasingTriplet(int[] nums) {
+    Integer min = null;
+    Integer first = null;
+    Integer second = null;
 
-		for (int num : nums) {
-			if (second != null && num > second) {
-				return true;
-			}
+    for (int num : nums) {
+      if (second != null && num > second) {
+        return true;
+      }
 
-			if (first != null && num > first && (second == null || num < second)) {
-				second = num;
-			}
+      if (first != null && num > first && (second == null || num < second)) {
+        second = num;
+      }
 
-			if (one != null && num > one && (first == null || second == null || (one <= first && num <= second))) {
-				first = one;
-				second = num;
-			}
+      if (min != null && num > min && (first == null || (min <= first && num <= second))) {
+        first = min;
+        second = num;
+      }
 
-			if (one == null || num < one) {
-				one = num;
-			}
-		}
-		return false;
-	}
+      if (min == null || num < min) {
+        min = num;
+      }
+    }
+
+    return false;
+  }
 }
