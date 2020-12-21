d54ae5f327c9df93f053bef6656fe5a2404334a2
Kai Wang <charles.wangkai@gmail.com>
2020-12-21 18:50:19 +0800

    Smallest Range II

--- smallest-range-ii/Solution.java
+++ smallest-range-ii/Solution.java
@@ -2,39 +2,36 @@ import java.util.Arrays;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-public class Solution {
-	public int smallestRangeII(int[] A, int K) {
-		Arrays.sort(A);
+class Solution {
+  public int smallestRangeII(int[] A, int K) {
+    Arrays.sort(A);
 
-		SortedMap<Integer, Integer> numberToCount = new TreeMap<>();
-		for (int number : A) {
-			addNumber(numberToCount, number);
-		}
-		int minRange = computeRange(numberToCount);
+    SortedMap<Integer, Integer> numberToCount = new TreeMap<>();
+    for (int number : A) {
+      addNumber(numberToCount, number);
+    }
 
-		for (int number : A) {
-			removeNumber(numberToCount, number);
-			addNumber(numberToCount, number + 2 * K);
+    int minRange = computeRange(numberToCount);
+    for (int number : A) {
+      removeNumber(numberToCount, number);
+      addNumber(numberToCount, number + 2 * K);
 
-			minRange = Math.min(minRange, computeRange(numberToCount));
-		}
+      minRange = Math.min(minRange, computeRange(numberToCount));
+    }
 
-		return minRange;
-	}
+    return minRange;
+  }
 
-	void addNumber(SortedMap<Integer, Integer> numberToCount, int number) {
-		numberToCount.put(number, numberToCount.getOrDefault(number, 0) + 1);
-	}
+  void addNumber(SortedMap<Integer, Integer> numberToCount, int number) {
+    numberToCount.put(number, numberToCount.getOrDefault(number, 0) + 1);
+  }
 
-	void removeNumber(SortedMap<Integer, Integer> numberToCount, int number) {
-		numberToCount.put(number, numberToCount.get(number) - 1);
+  void removeNumber(SortedMap<Integer, Integer> numberToCount, int number) {
+    numberToCount.put(number, numberToCount.get(number) - 1);
+    numberToCount.remove(number, 0);
+  }
 
-		if (numberToCount.get(number) == 0) {
-			numberToCount.remove(number);
-		}
-	}
-
-	int computeRange(SortedMap<Integer, Integer> numberToCount) {
-		return numberToCount.lastKey() - numberToCount.firstKey();
-	}
+  int computeRange(SortedMap<Integer, Integer> numberToCount) {
+    return numberToCount.lastKey() - numberToCount.firstKey();
+  }
 }
