0ab2a5d58c18c4354507f6bd5540dbf3e1e4a1ae
Kai Wang <charles.wangkai@gmail.com>
2020-12-23 22:50:45 +0800

    Next Greater Element III

--- next-greater-element-iii/Solution.java
+++ next-greater-element-iii/Solution.java
@@ -2,42 +2,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Solution {
-	public int nextGreaterElement(int n) {
-		String s = String.valueOf(n);
+class Solution {
+  public int nextGreaterElement(int n) {
+    String s = String.valueOf(n);
 
-		int index = s.length() - 2;
-		while (index >= 0 && s.charAt(index) >= s.charAt(index + 1)) {
-			index--;
-		}
+    int index = s.length() - 2;
+    while (index >= 0 && s.charAt(index) >= s.charAt(index + 1)) {
+      --index;
+    }
+    if (index == -1) {
+      return -1;
+    }
 
-		if (index < 0) {
-			return -1;
-		}
+    char current = s.charAt(index);
+    List<Character> digits = new ArrayList<>();
+    for (int i = index; i < s.length(); ++i) {
+      digits.add(s.charAt(i));
+    }
 
-		char current = s.charAt(index);
-		List<Character> digits = new ArrayList<Character>();
-		for (int i = index; i < s.length(); i++) {
-			digits.add(s.charAt(i));
-		}
+    Collections.sort(digits);
 
-		Collections.sort(digits);
+    String result = s.substring(0, index);
+    for (int i = 0; ; ++i) {
+      char digit = digits.get(i);
+      if (digit > current) {
+        result += digit;
+        digits.remove(i);
 
-		String result = s.substring(0, index);
-		for (int i = 0;; i++) {
-			char digit = digits.get(i);
-			if (digit > current) {
-				result += digit;
+        for (char d : digits) {
+          result += d;
+        }
 
-				digits.remove(i);
-				for (char d : digits) {
-					result += d;
-				}
+        break;
+      }
+    }
 
-				break;
-			}
-		}
-
-		return (Long.parseLong(result) > Integer.MAX_VALUE) ? -1 : Integer.parseInt(result);
-	}
+    return (Long.parseLong(result) > Integer.MAX_VALUE) ? -1 : Integer.parseInt(result);
+  }
 }
