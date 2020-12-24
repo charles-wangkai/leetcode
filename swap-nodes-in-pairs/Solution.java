04d520b625eb8353055d1f0cd14c62dcc9ae791c
Kai Wang <charles.wangkai@gmail.com>
2020-12-24 20:46:05 +0800

    Swap Nodes in Pairs

--- swap-nodes-in-pairs/Solution.java
+++ swap-nodes-in-pairs/Solution.java
@@ -1,30 +1,36 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode swapPairs(ListNode head) {
-		ListNode tempHead = new ListNode(0);
-		tempHead.next = head;
+class Solution {
+  public ListNode swapPairs(ListNode head) {
+    ListNode tempHead = new ListNode();
+    tempHead.next = head;
 
-		ListNode current = tempHead;
-		while (current.next != null && current.next.next != null) {
-			ListNode node1 = current.next;
-			ListNode node2 = node1.next;
-			ListNode after = node2.next;
-			current.next = node2;
-			node2.next = node1;
-			node1.next = after;
-			current = current.next.next;
-		}
+    ListNode current = tempHead;
+    while (current.next != null && current.next.next != null) {
+      ListNode node1 = current.next;
+      ListNode node2 = node1.next;
+      ListNode after = node2.next;
+      current.next = node2;
+      node2.next = node1;
+      node1.next = after;
+      current = current.next.next;
+    }
 
-		return tempHead.next;
-	}
+    return tempHead.next;
+  }
 }
