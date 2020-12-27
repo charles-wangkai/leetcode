4b7b375733f51f8845db50b8a622681c4730b343
Kai Wang <charles.wangkai@gmail.com>
2020-12-27 11:56:23 +0800

    Decode Ways

--- decode-ways/Solution.java
+++ decode-ways/Solution.java
@@ -1,25 +1,28 @@
-public class Solution {
-	public int numDecodings(String s) {
-		if (s.isEmpty()) {
-			return 0;
-		}
+class Solution {
+  public int numDecodings(String s) {
+    if (s.isEmpty()) {
+      return 0;
+    }
 
-		int prev = 0;
-		int current = 1;
-		for (int i = 0; i < s.length(); i++) {
-			int next = 0;
-			int oneDigit = Integer.parseInt(s.substring(i, i + 1));
-			if (oneDigit >= 1 && oneDigit <= 9) {
-				next += current;
-			}
-			int twoDigit = Integer.parseInt(s.substring(Math.max(i - 1, 0),
-					i + 1));
-			if (twoDigit >= 10 && twoDigit <= 26) {
-				next += prev;
-			}
-			prev = current;
-			current = next;
-		}
-		return current;
-	}
+    int prev = 0;
+    int curr = 1;
+    for (int i = 0; i < s.length(); ++i) {
+      int next = 0;
+      int oneDigit = Integer.parseInt(s.substring(i, i + 1));
+      if (oneDigit >= 1 && oneDigit <= 9) {
+        next += curr;
+      }
+      if (i != 0) {
+        int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+        if (twoDigit >= 10 && twoDigit <= 26) {
+          next += prev;
+        }
+      }
+
+      prev = curr;
+      curr = next;
+    }
+
+    return curr;
+  }
 }
