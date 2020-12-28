b9b755497cf5af4eed06708c206938f108b7473c
Kai Wang <charles.wangkai@gmail.com>
2020-12-28 22:31:21 +0800

    Reach a Number

--- reach-a-number/Solution.java
+++ reach-a-number/Solution.java
@@ -1,10 +1,11 @@
-public class Solution {
-	public int reachNumber(int target) {
-		target = Math.abs(target);
-		int step = (int) Math.floor(Math.sqrt(target * 2));
-		while (step * (step + 1) / 2 < target || step * (step + 1) / 2 % 2 != target % 2) {
-			step++;
-		}
-		return step;
-	}
+class Solution {
+  public int reachNumber(int target) {
+    target = Math.abs(target);
+    int step = (int) Math.floor(Math.sqrt(target * 2));
+    while (step * (step + 1) / 2 < target || step * (step + 1) / 2 % 2 != target % 2) {
+      ++step;
+    }
+
+    return step;
+  }
 }
