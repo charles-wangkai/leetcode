a9376c78c6ad67a01780a59c05a79c9d2444d6c6
Kai Wang <charles.wangkai@gmail.com>
2020-12-29 19:01:25 +0800

    Pseudo-Palindromic Paths in a Binary Tree

--- pseudo-palindromic-paths-in-a-binary-tree/Solution.java
+++ pseudo-palindromic-paths-in-a-binary-tree/Solution.java
@@ -3,53 +3,52 @@ import java.util.Set;
 
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode() {
-    }
+  TreeNode() {}
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-    public int pseudoPalindromicPaths(TreeNode root) {
-        return search(root, new HashSet<>());
+  public int pseudoPalindromicPaths(TreeNode root) {
+    return search(root, new HashSet<>());
+  }
+
+  int search(TreeNode node, Set<Integer> odds) {
+    if (node == null) {
+      return 0;
     }
 
-    int search(TreeNode node, Set<Integer> odds) {
-        if (node == null) {
-            return 0;
-        }
+    updateOdds(odds, node.val);
 
-        updateOdds(odds, node.val);
-
-        int result;
-        if (node.left == null && node.right == null) {
-            result = (odds.size() <= 1) ? 1 : 0;
-        } else {
-            result = search(node.left, odds) + search(node.right, odds);
-        }
-
-        updateOdds(odds, node.val);
-
-        return result;
+    int result;
+    if (node.left == null && node.right == null) {
+      result = (odds.size() <= 1) ? 1 : 0;
+    } else {
+      result = search(node.left, odds) + search(node.right, odds);
     }
 
-    void updateOdds(Set<Integer> odds, int value) {
-        if (odds.contains(value)) {
-            odds.remove(value);
-        } else {
-            odds.add(value);
-        }
+    updateOdds(odds, node.val);
+
+    return result;
+  }
+
+  void updateOdds(Set<Integer> odds, int value) {
+    if (odds.contains(value)) {
+      odds.remove(value);
+    } else {
+      odds.add(value);
     }
+  }
 }
