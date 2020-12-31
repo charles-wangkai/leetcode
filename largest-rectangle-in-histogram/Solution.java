d42e33467f7a7fa52f04ad7ce4130eb63808b3bc
Kai Wang <charles.wangkai@gmail.com>
2020-12-31 21:50:30 +0800

    Largest Rectangle in Histogram

--- largest-rectangle-in-histogram/Solution.java
+++ largest-rectangle-in-histogram/Solution.java
@@ -1,19 +1,20 @@
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Solution {
-	public int largestRectangleArea(int[] height) {
-		height = Arrays.copyOf(height, height.length + 1);
-		Stack<Integer> indices = new Stack<Integer>();
-		int maxArea = 0;
-		for (int i = 0; i < height.length; i++) {
-			while (!indices.empty() && height[i] <= height[indices.peek()]) {
-				int h = height[indices.pop()];
-				maxArea = Math.max(maxArea, h
-						* (i - (indices.empty() ? 0 : (indices.peek() + 1))));
-			}
-			indices.push(i);
-		}
-		return maxArea;
-	}
+class Solution {
+  public int largestRectangleArea(int[] heights) {
+    heights = Arrays.copyOf(heights, heights.length + 1);
+    Stack<Integer> indices = new Stack<>();
+    int result = 0;
+    for (int i = 0; i < heights.length; ++i) {
+      while (!indices.empty() && heights[i] <= heights[indices.peek()]) {
+        int h = heights[indices.pop()];
+        result = Math.max(result, h * (i - (indices.empty() ? 0 : (indices.peek() + 1))));
+      }
+
+      indices.push(i);
+    }
+
+    return result;
+  }
 }
