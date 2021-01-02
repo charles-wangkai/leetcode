93f37df573bbc82dbf1e4b0eec440491e0cd7797
Kai Wang <charles.wangkai@gmail.com>
2021-01-02 16:41:56 +0800

    Find a Corresponding Node of a Binary Tree in a Clone of That Tree

--- find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/Solution.java
+++ find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/Solution.java
@@ -1,54 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
-    }
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
 class Solution {
-    List<Boolean> leftOrRightPath;
-
-    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        leftOrRightPath = null;
-        search(target, original, new ArrayList<Boolean>());
-
-        return find(cloned, 0);
+  public final TreeNode getTargetCopy(
+      final TreeNode original, final TreeNode cloned, final TreeNode target) {
+    if (original == null) {
+      return null;
+    }
+    if (original == target) {
+      return cloned;
     }
 
-    void search(TreeNode target, TreeNode node, List<Boolean> path) {
-        if (node == null) {
-            return;
-        }
-
-        if (node == target) {
-            leftOrRightPath = new ArrayList<>(path);
-        }
-
-        path.add(true);
-        search(target, node.left, path);
-        path.remove(path.size() - 1);
-
-        path.add(false);
-        search(target, node.right, path);
-        path.remove(path.size() - 1);
+    TreeNode subLeftResult = getTargetCopy(original.left, cloned.left, target);
+    if (subLeftResult != null) {
+      return subLeftResult;
     }
 
-    TreeNode find(TreeNode node, int index) {
-        if (index == leftOrRightPath.size()) {
-            return node;
-        }
-
-        if (leftOrRightPath.get(index)) {
-            return find(node.left, index + 1);
-        } else {
-            return find(node.right, index + 1);
-        }
-    }
+    return getTargetCopy(original.right, cloned.right, target);
+  }
 }
