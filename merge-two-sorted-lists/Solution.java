270438df3bb752858e64dd8556c03ce1d48f06dc
Kai Wang <charles.wangkai@gmail.com>
2021-01-04 16:24:44 +0800

    Merge Two Sorted Lists

--- merge-two-sorted-lists/Solution.java
+++ merge-two-sorted-lists/Solution.java
@@ -1,35 +1,43 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		ListNode mergedHead = null;
-		ListNode mergedTail = null;
-		while (l1 != null || l2 != null) {
-			ListNode node;
-			if (l1 != null && (l2 == null || l1.val < l2.val)) {
-				node = l1;
-				l1 = l1.next;
-			} else {
-				node = l2;
-				l2 = l2.next;
-			}
-			node.next = null;
-			if (mergedHead == null) {
-				mergedHead = node;
-			} else {
-				mergedTail.next = node;
-			}
-			mergedTail = node;
-		}
-		return mergedHead;
-	}
+class Solution {
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode mergedHead = null;
+    ListNode mergedTail = null;
+    while (l1 != null || l2 != null) {
+      ListNode node;
+      if (l1 != null && (l2 == null || l1.val < l2.val)) {
+        node = l1;
+        l1 = l1.next;
+      } else {
+        node = l2;
+        l2 = l2.next;
+      }
+
+      node.next = null;
+      if (mergedHead == null) {
+        mergedHead = node;
+      } else {
+        mergedTail.next = node;
+      }
+      mergedTail = node;
+    }
+
+    return mergedHead;
+  }
 }
