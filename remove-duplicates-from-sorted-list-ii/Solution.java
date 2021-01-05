45d314db2786fd1da2ca4d4218f2fe3172d5768d
Kai Wang <charles.wangkai@gmail.com>
2021-01-05 17:25:20 +0800

    Remove Duplicates from Sorted List II

--- remove-duplicates-from-sorted-list-ii/Solution.java
+++ remove-duplicates-from-sorted-list-ii/Solution.java
@@ -1,30 +1,37 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode deleteDuplicates(ListNode head) {
-		ListNode tempHead = new ListNode(0);
-		tempHead.next = head;
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode tempHead = new ListNode();
+    tempHead.next = head;
 
-		ListNode p = tempHead;
-		while (p.next != null && p.next.next != null) {
-			if (p.next.val != p.next.next.val) {
-				p = p.next;
-			} else {
-				int removedVal = p.next.val;
-				while (p.next != null && p.next.val == removedVal) {
-					p.next = p.next.next;
-				}
-			}
-		}
-		return tempHead.next;
-	}
+    ListNode p = tempHead;
+    while (p.next != null && p.next.next != null) {
+      if (p.next.val != p.next.next.val) {
+        p = p.next;
+      } else {
+        int removedVal = p.next.val;
+        while (p.next != null && p.next.val == removedVal) {
+          p.next = p.next.next;
+        }
+      }
+    }
+
+    return tempHead.next;
+  }
 }
