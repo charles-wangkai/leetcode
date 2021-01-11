b122c0b60bb6f32017aea3eaedfd63513908f2f5
Kai Wang <charles.wangkai@gmail.com>
2021-01-11 22:26:08 +0800

    Merge Sorted Array

--- merge-sorted-array/Solution.java
+++ merge-sorted-array/Solution.java
@@ -1,15 +1,15 @@
-public class Solution {
-	public void merge(int A[], int m, int B[], int n) {
-		int indexA = m - 1;
-		int indexB = n - 1;
-		for (int i = m + n - 1; i >= 0; i--) {
-			if (indexA >= 0 && (indexB < 0 || A[indexA] >= B[indexB])) {
-				A[i] = A[indexA];
-				indexA--;
-			} else {
-				A[i] = B[indexB];
-				indexB--;
-			}
-		}
-	}
+class Solution {
+  public void merge(int nums1[], int m, int nums2[], int n) {
+    int indexA = m - 1;
+    int indexB = n - 1;
+    for (int i = m + n - 1; i >= 0; --i) {
+      if (indexA >= 0 && (indexB < 0 || nums1[indexA] >= nums2[indexB])) {
+        nums1[i] = nums1[indexA];
+        --indexA;
+      } else {
+        nums1[i] = nums2[indexB];
+        --indexB;
+      }
+    }
+  }
 }
