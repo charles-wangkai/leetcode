b322d2ed7903001b461f6cc6cfa36c4e1017b4e9
Kai Wang <charles.wangkai@gmail.com>
2021-01-17 23:02:06 +0800

    Largest Submatrix With Rearrangements

---
+++ largest-submatrix-with-rearrangements/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+class Solution {
+  public int largestSubmatrix(int[][] matrix) {
+    int n = matrix[0].length;
+
+    int result = 0;
+    int[] heights = new int[n];
+    for (int[] row : matrix) {
+      for (int i = 0; i < n; ++i) {
+        if (row[i] == 1) {
+          ++heights[i];
+        } else {
+          heights[i] = 0;
+        }
+      }
+
+      int[] sortedHeights =
+          Arrays.stream(heights)
+              .boxed()
+              .sorted(Comparator.reverseOrder())
+              .mapToInt(x -> x)
+              .toArray();
+      result =
+          Math.max(
+              result, IntStream.range(0, n).map(i -> (i + 1) * sortedHeights[i]).max().getAsInt());
+    }
+
+    return result;
+  }
+}
