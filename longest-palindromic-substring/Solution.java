6c6ed0d3cfa2542e1d99662304f89e8fd1d4bb9e
Kai Wang <charles.wangkai@gmail.com>
2021-01-19 21:16:32 +0800

    Longest Palindromic Substring

--- longest-palindromic-substring/Solution.java
+++ longest-palindromic-substring/Solution.java
@@ -1,31 +1,32 @@
 public class Solution {
-	String result;
+  public String longestPalindrome(String s) {
+    String result = "";
+    for (int i = 0; i < s.length(); ++i) {
+      String palindrome = findMaxLengthPalindrome(s, i, i);
+      if (palindrome.length() > result.length()) {
+        result = palindrome;
+      }
+    }
+    for (int i = 0; i < s.length() - 1; ++i) {
+      if (s.charAt(i) == s.charAt(i + 1)) {
+        String palindrome = findMaxLengthPalindrome(s, i, i + 1);
+        if (palindrome.length() > result.length()) {
+          result = palindrome;
+        }
+      }
+    }
 
-	public String longestPalindrome(String s) {
-		result = "";
-		for (int i = 0; i < s.length(); i++) {
-			compareAndUpdateResult(findMaxLengthPalindrome(s, i, i));
-		}
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i) == s.charAt(i + 1)) {
-				compareAndUpdateResult(findMaxLengthPalindrome(s, i, i + 1));
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 
-	String findMaxLengthPalindrome(String s, int leftIndex, int rightIndex) {
-		while (leftIndex > 0 && rightIndex < s.length() - 1
-				&& s.charAt(leftIndex - 1) == s.charAt(rightIndex + 1)) {
-			leftIndex--;
-			rightIndex++;
-		}
-		return s.substring(leftIndex, rightIndex + 1);
-	}
+  String findMaxLengthPalindrome(String s, int leftIndex, int rightIndex) {
+    while (leftIndex >= 1
+        && rightIndex + 1 < s.length()
+        && s.charAt(leftIndex - 1) == s.charAt(rightIndex + 1)) {
+      --leftIndex;
+      ++rightIndex;
+    }
 
-	void compareAndUpdateResult(String palindrome) {
-		if (palindrome.length() > result.length()) {
-			result = palindrome;
-		}
-	}
+    return s.substring(leftIndex, rightIndex + 1);
+  }
 }
