78494791fdd40fdd663470f9a1ed16b55faa2644
Kai Wang <charles.wangkai@gmail.com>
2021-01-20 16:38:33 +0800

    Valid Parentheses

--- valid-parentheses/Solution.java
+++ valid-parentheses/Solution.java
@@ -1,24 +1,24 @@
 import java.util.Stack;
 
-public class Solution {
-	public boolean isValid(String s) {
-		Stack<Character> leftBrackets = new Stack<Character>();
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (ch == '(' || ch == '[' || ch == '{') {
-				leftBrackets.push(ch);
-			} else {
-				if (leftBrackets.empty()) {
-					return false;
-				}
-				char leftBracket = leftBrackets.pop();
-				if ((ch == ')' && leftBracket != '(')
-						|| (ch == ']' && leftBracket != '[')
-						|| (ch == '}' && leftBracket != '{')) {
-					return false;
-				}
-			}
-		}
-		return leftBrackets.empty();
-	}
+class Solution {
+  public boolean isValid(String s) {
+    Stack<Character> leftBrackets = new Stack<>();
+    for (char ch : s.toCharArray()) {
+      if (ch == '(' || ch == '[' || ch == '{') {
+        leftBrackets.push(ch);
+      } else {
+        if (leftBrackets.empty()) {
+          return false;
+        }
+        char leftBracket = leftBrackets.pop();
+        if ((ch == ')' && leftBracket != '(')
+            || (ch == ']' && leftBracket != '[')
+            || (ch == '}' && leftBracket != '{')) {
+          return false;
+        }
+      }
+    }
+
+    return leftBrackets.empty();
+  }
 }
