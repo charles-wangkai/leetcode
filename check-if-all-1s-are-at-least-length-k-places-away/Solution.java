91c28c2449143a6755f0848c79df00674a3b818b
Kai Wang <charles.wangkai@gmail.com>
2021-01-25 17:23:48 +0800

    Check If All 1's Are at Least Length K Places Away

--- check-if-all-1s-are-at-least-length-k-places-away/Solution.java
+++ check-if-all-1s-are-at-least-length-k-places-away/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean kLengthApart(int[] nums, int k) {
-        int prevIndex = -1;
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] == 1) {
-                if (prevIndex != -1 && i - prevIndex - 1 < k) {
-                    return false;
-                }
-
-                prevIndex = i;
-            }
+  public boolean kLengthApart(int[] nums, int k) {
+    int prevIndex = -1;
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] == 1) {
+        if (prevIndex != -1 && i - prevIndex - 1 < k) {
+          return false;
         }
 
-        return true;
+        prevIndex = i;
+      }
     }
+
+    return true;
+  }
 }
