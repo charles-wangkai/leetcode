39454b4a5d34e2d51fca699107d1ee10d021d90d
Kai Wang <charles.wangkai@gmail.com>
2021-01-31 16:27:21 +0800

    Next Permutation

--- next-permutation/Solution.java
+++ next-permutation/Solution.java
@@ -1,33 +1,35 @@
-public class Solution {
-	public void nextPermutation(int[] num) {
-		int startIndex = num.length - 2;
-		while (startIndex >= 0 && num[startIndex] >= num[startIndex + 1]) {
-			startIndex--;
-		}
+class Solution {
+  public void nextPermutation(int[] nums) {
+    int startIndex = nums.length - 2;
+    while (startIndex >= 0 && nums[startIndex] >= nums[startIndex + 1]) {
+      --startIndex;
+    }
 
-		if (startIndex < 0) {
-			reverse(num, 0, num.length - 1);
-			return;
-		}
+    if (startIndex == -1) {
+      reverse(nums, 0, nums.length - 1);
 
-		for (int i = num.length - 1;; i--) {
-			if (num[i] > num[startIndex]) {
-				swap(num, i, startIndex);
-				reverse(num, startIndex + 1, num.length - 1);
-				return;
-			}
-		}
-	}
+      return;
+    }
 
-	void reverse(int[] a, int beginIndex, int endIndex) {
-		for (int i = beginIndex, j = endIndex; i < j; i++, j--) {
-			swap(a, i, j);
-		}
-	}
+    for (int i = nums.length - 1; ; --i) {
+      if (nums[i] > nums[startIndex]) {
+        swap(nums, i, startIndex);
+        reverse(nums, startIndex + 1, nums.length - 1);
 
-	void swap(int a[], int index1, int index2) {
-		int temp = a[index1];
-		a[index1] = a[index2];
-		a[index2] = temp;
-	}
+        return;
+      }
+    }
+  }
+
+  void reverse(int[] a, int beginIndex, int endIndex) {
+    for (int i = beginIndex, j = endIndex; i < j; ++i, --j) {
+      swap(a, i, j);
+    }
+  }
+
+  void swap(int a[], int index1, int index2) {
+    int temp = a[index1];
+    a[index1] = a[index2];
+    a[index2] = temp;
+  }
 }
