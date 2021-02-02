adf5a1dc3676126c3c1a0b92013dfc5f5901c3f9
Kai Wang <charles.wangkai@gmail.com>
2021-02-02 17:23:45 +0800

    Trim a Binary Search Tree

--- trim-a-binary-search-tree/Solution.java
+++ trim-a-binary-search-tree/Solution.java
@@ -1,28 +1,37 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode trimBST(TreeNode root, int L, int R) {
-		if (root == null) {
-			return null;
-		}
+class Solution {
+  public TreeNode trimBST(TreeNode root, int low, int high) {
+    if (root == null) {
+      return null;
+    }
 
-		if (root.val < L) {
-			return trimBST(root.right, L, R);
-		} else if (root.val > R) {
-			return trimBST(root.left, L, R);
-		} else {
-			root.left = trimBST(root.left, L, R);
-			root.right = trimBST(root.right, L, R);
-			return root;
-		}
-	}
+    if (root.val < low) {
+      return trimBST(root.right, low, high);
+    } else if (root.val > high) {
+      return trimBST(root.left, low, high);
+    } else {
+      root.left = trimBST(root.left, low, high);
+      root.right = trimBST(root.right, low, high);
+
+      return root;
+    }
+  }
 }
