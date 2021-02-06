f936ae78838cb99fe30dad033a6cb90d05ea2305
Kai Wang <charles.wangkai@gmail.com>
2021-02-06 21:57:09 +0800

    Binary Tree Right Side View

--- binary-tree-right-side-view/Solution.java
+++ binary-tree-right-side-view/Solution.java
@@ -1,36 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 public class Solution {
-	public List<Integer> rightSideView(TreeNode root) {
-		List<Integer> result = new ArrayList<Integer>();
-		search(result, root, 0);
-		return result;
-	}
+  public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    search(result, root, 0);
 
-	void search(List<Integer> result, TreeNode node, int depth) {
-		if (node == null) {
-			return;
-		}
+    return result;
+  }
 
-		if (depth == result.size()) {
-			result.add(node.val);
-		} else {
-			result.set(depth, node.val);
-		}
+  void search(List<Integer> result, TreeNode node, int depth) {
+    if (node == null) {
+      return;
+    }
 
-		search(result, node.left, depth + 1);
-		search(result, node.right, depth + 1);
-	}
+    if (depth == result.size()) {
+      result.add(node.val);
+    } else {
+      result.set(depth, node.val);
+    }
+
+    search(result, node.left, depth + 1);
+    search(result, node.right, depth + 1);
+  }
 }
