27465113c421ad38e8c9b18e13273d138e615251
Kai Wang <charles.wangkai@gmail.com>
2021-02-08 17:46:13 +0800

    Peeking Iterator

--- peeking-iterator/PeekingIterator.java
+++ peeking-iterator/PeekingIterator.java
@@ -3,42 +3,43 @@ import java.util.Iterator;
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
-	Iterator<Integer> iterator;
-	boolean empty;
-	Integer nextElement;
+  private Iterator<Integer> iterator;
+  private boolean hasNext;
+  private Integer next;
 
-	public PeekingIterator(Iterator<Integer> iterator) {
-		// initialize any member here.
+  public PeekingIterator(Iterator<Integer> iterator) {
+    // initialize any member here.
 
-		this.iterator = iterator;
-		move();
-	}
+    this.iterator = iterator;
+    move();
+  }
 
-	// Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-		return nextElement;
-	}
+  // Returns the next element in the iteration without advancing the iterator.
+  public Integer peek() {
+    return next;
+  }
 
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-		Integer result = nextElement;
-		move();
-		return result;
-	}
+  // hasNext() and next() should behave the same as in the Iterator interface.
+  // Override them if needed.
+  @Override
+  public Integer next() {
+    Integer result = next;
+    move();
 
-	@Override
-	public boolean hasNext() {
-		return !empty;
-	}
+    return result;
+  }
 
-	private void move() {
-		if (iterator.hasNext()) {
-			empty = false;
-			nextElement = iterator.next();
-		} else {
-			empty = true;
-		}
-	}
+  @Override
+  public boolean hasNext() {
+    return hasNext;
+  }
+
+  private void move() {
+    if (iterator.hasNext()) {
+      hasNext = true;
+      next = iterator.next();
+    } else {
+      hasNext = false;
+    }
+  }
 }
