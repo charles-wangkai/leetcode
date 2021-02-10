aa34b1da93defb870439e1711ba3a487896d10b8
Kai Wang <charles.wangkai@gmail.com>
2021-02-10 16:48:39 +0800

    Copy List with Random Pointer

--- copy-list-with-random-pointer/Solution.java
+++ copy-list-with-random-pointer/Solution.java
@@ -1,35 +1,42 @@
 import java.util.HashMap;
 import java.util.Map;
 
-// Definition for singly-linked list with a random pointer.
-class RandomListNode {
-	int label;
-	RandomListNode next, random;
+// Definition for a Node.
+class Node {
+  int val;
+  Node next;
+  Node random;
 
-	RandomListNode(int x) {
-		this.label = x;
-	}
+  public Node(int val) {
+    this.val = val;
+    this.next = null;
+    this.random = null;
+  }
 }
 
-public class Solution {
-	public RandomListNode copyRandomList(RandomListNode head) {
-		if (head == null) {
-			return null;
-		}
+class Solution {
+  public Node copyRandomList(Node head) {
+    if (head == null) {
+      return null;
+    }
 
-		Map<RandomListNode, RandomListNode> cloneMap = new HashMap<RandomListNode, RandomListNode>();
+    Map<Node, Node> cloneMap = new HashMap<>();
 
-		RandomListNode clonedHead = new RandomListNode(head.label);
-		cloneMap.put(head, clonedHead);
-		for (RandomListNode node = head, clonedNode = clonedHead; node.next != null; node = node.next, clonedNode = clonedNode.next) {
-			clonedNode.next = new RandomListNode(node.next.label);
-			cloneMap.put(node.next, clonedNode.next);
-		}
+    Node clonedHead = new Node(head.val);
+    cloneMap.put(head, clonedHead);
+    for (Node node = head, clonedNode = clonedHead;
+        node.next != null;
+        node = node.next, clonedNode = clonedNode.next) {
+      clonedNode.next = new Node(node.next.val);
+      cloneMap.put(node.next, clonedNode.next);
+    }
 
-		for (RandomListNode node = head, clonedNode = clonedHead; node != null; node = node.next, clonedNode = clonedNode.next) {
-			clonedNode.random = cloneMap.get(node.random);
-		}
+    for (Node node = head, clonedNode = clonedHead;
+        node != null;
+        node = node.next, clonedNode = clonedNode.next) {
+      clonedNode.random = cloneMap.get(node.random);
+    }
 
-		return clonedHead;
-	}
+    return clonedHead;
+  }
 }
