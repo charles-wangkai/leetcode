dbb4edc0934da7df1771d5a4fa126a5ce44e72da
Kai Wang <charles.wangkai@gmail.com>
2021-02-14 09:38:13 +0800

    Shortest Path in Binary Matrix

--- shortest-path-in-binary-matrix/Solution.java
+++ shortest-path-in-binary-matrix/Solution.java
@@ -1,51 +1,55 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, -1, 0, 1, 1, 1, 0, -1 };
-	static final int[] C_OFFSETS = { 0, 1, 1, 1, 0, -1, -1, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, -1, 0, 1, 1, 1, 0, -1};
+  static final int[] C_OFFSETS = {0, 1, 1, 1, 0, -1, -1, -1};
 
-	public int shortestPathBinaryMatrix(int[][] grid) {
-		if (grid[0][0] == 1) {
-			return -1;
-		}
+  public int shortestPathBinaryMatrix(int[][] grid) {
+    if (grid[0][0] == 1) {
+      return -1;
+    }
 
-		int N = grid.length;
+    int N = grid.length;
 
-		int[][] minLengths = new int[N][N];
-		Queue<Point> queue = new LinkedList<>();
-		queue.offer(new Point(0, 0));
-		minLengths[0][0] = 1;
+    int[][] minLengths = new int[N][N];
+    Queue<Point> queue = new LinkedList<>();
+    queue.offer(new Point(0, 0));
+    minLengths[0][0] = 1;
 
-		while (!queue.isEmpty()) {
-			Point head = queue.poll();
+    while (!queue.isEmpty()) {
+      Point head = queue.poll();
+      if (head.r == N - 1 && head.c == N - 1) {
+        return minLengths[head.r][head.c];
+      }
 
-			for (int i = 0; i < R_OFFSETS.length; i++) {
-				int nextR = head.r + R_OFFSETS[i];
-				int nextC = head.c + C_OFFSETS[i];
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int nextR = head.r + R_OFFSETS[i];
+        int nextC = head.c + C_OFFSETS[i];
 
-				if (nextR >= 0 && nextR < N && nextC >= 0 && nextC < N && grid[nextR][nextC] == 0
-						&& minLengths[nextR][nextC] == 0) {
-					minLengths[nextR][nextC] = minLengths[head.r][head.c] + 1;
-					if (nextR == N - 1 && nextC == N - 1) {
-						return minLengths[nextR][nextC];
-					}
+        if (nextR >= 0
+            && nextR < N
+            && nextC >= 0
+            && nextC < N
+            && grid[nextR][nextC] == 0
+            && minLengths[nextR][nextC] == 0) {
+          minLengths[nextR][nextC] = minLengths[head.r][head.c] + 1;
 
-					queue.offer(new Point(nextR, nextC));
-				}
-			}
-		}
+          queue.offer(new Point(nextR, nextC));
+        }
+      }
+    }
 
-		return -1;
-	}
+    return -1;
+  }
 }
 
 class Point {
-	int r;
-	int c;
+  int r;
+  int c;
 
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
 }
