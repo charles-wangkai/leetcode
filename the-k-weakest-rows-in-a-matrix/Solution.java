3970ac9a819fa3dab3ff6617eebb01110c96da54
Kai Wang <charles.wangkai@gmail.com>
2021-02-15 22:30:08 +0800

    The K Weakest Rows in a Matrix

--- the-k-weakest-rows-in-a-matrix/Solution.java
+++ the-k-weakest-rows-in-a-matrix/Solution.java
@@ -1,14 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int[] kWeakestRows(int[][] mat, int k) {
-		int[] oneCounts = Arrays.stream(mat).mapToInt(row -> (int) Arrays.stream(row).filter(x -> x == 1).count())
-				.toArray();
+class Solution {
+  public int[] kWeakestRows(int[][] mat, int k) {
+    int[] oneCounts = Arrays.stream(mat).mapToInt(row -> Arrays.stream(row).sum()).toArray();
 
-		return IntStream.range(0, mat.length).boxed()
-				.sorted((i1, i2) -> (oneCounts[i1] != oneCounts[i2]) ? Integer.compare(oneCounts[i1], oneCounts[i2])
-						: Integer.compare(i1, i2))
-				.limit(k).mapToInt(x -> x).toArray();
-	}
+    return IntStream.range(0, mat.length)
+        .boxed()
+        .sorted(Comparator.comparing((Integer i) -> oneCounts[i]).thenComparing(i -> i))
+        .limit(k)
+        .mapToInt(x -> x)
+        .toArray();
+  }
 }
