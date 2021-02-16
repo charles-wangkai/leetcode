b696971a803788c0573340c4bddb33276ee87221
Kai Wang <charles.wangkai@gmail.com>
2021-02-16 18:59:35 +0800

    Letter Case Permutation

--- letter-case-permutation/Solution.java
+++ letter-case-permutation/Solution.java
@@ -1,22 +1,27 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-public class Solution {
-	public List<String> letterCasePermutation(String S) {
-		Set<String> result = new HashSet<String>();
-		search(result, S, "");
-		return new ArrayList<String>(result);
-	}
+class Solution {
+  public List<String> letterCasePermutation(String S) {
+    List<String> created = new ArrayList<>();
+    search(created, S, "");
 
-	void search(Set<String> result, String S, String transformed) {
-		if (transformed.length() == S.length()) {
-			result.add(transformed);
-			return;
-		}
+    return created;
+  }
 
-		search(result, S, transformed + Character.toLowerCase(S.charAt(transformed.length())));
-		search(result, S, transformed + Character.toUpperCase(S.charAt(transformed.length())));
-	}
+  void search(List<String> created, String S, String transformed) {
+    if (transformed.length() == S.length()) {
+      created.add(transformed);
+
+      return;
+    }
+
+    char ch = S.charAt(transformed.length());
+    if (Character.isDigit(ch)) {
+      search(created, S, transformed + ch);
+    } else {
+      search(created, S, transformed + Character.toLowerCase(ch));
+      search(created, S, transformed + Character.toUpperCase(ch));
+    }
+  }
 }
