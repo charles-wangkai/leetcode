86828008a85f79e55154a6866dd833d3478163ed
Kai Wang <charles.wangkai@gmail.com>
2021-02-17 19:21:16 +0800

    Container With Most Water

--- container-with-most-water/Solution.java
+++ container-with-most-water/Solution.java
@@ -1,18 +1,19 @@
-public class Solution {
-	public int maxArea(int[] height) {
-		int result = 0;
-		int leftIndex = 0;
-		int rightIndex = height.length - 1;
-		while (leftIndex < rightIndex) {
-			result = Math.max(result,
-					Math.min(height[leftIndex], height[rightIndex])
-							* (rightIndex - leftIndex));
-			if (height[leftIndex] <= height[rightIndex]) {
-				leftIndex++;
-			} else {
-				rightIndex--;
-			}
-		}
-		return result;
-	}
+class Solution {
+  public int maxArea(int[] height) {
+    int result = 0;
+    int leftIndex = 0;
+    int rightIndex = height.length - 1;
+    while (leftIndex != rightIndex) {
+      result =
+          Math.max(
+              result, Math.min(height[leftIndex], height[rightIndex]) * (rightIndex - leftIndex));
+      if (height[leftIndex] <= height[rightIndex]) {
+        ++leftIndex;
+      } else {
+        --rightIndex;
+      }
+    }
+
+    return result;
+  }
 }
