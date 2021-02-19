0435bfb1c00d0aec07ef9ad60df979f826c215d2
Kai Wang <charles.wangkai@gmail.com>
2021-02-19 17:51:37 +0800

    Minimum Remove to Make Valid Parentheses

--- minimum-remove-to-make-valid-parentheses/Solution.java
+++ minimum-remove-to-make-valid-parentheses/Solution.java
@@ -1,26 +1,29 @@
 import java.util.Stack;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public String minRemoveToMakeValid(String s) {
-		boolean[] kept = new boolean[s.length()];
-		Stack<Integer> stack = new Stack<>();
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
+class Solution {
+  public String minRemoveToMakeValid(String s) {
+    boolean[] kept = new boolean[s.length()];
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < s.length(); ++i) {
+      char ch = s.charAt(i);
 
-			if (ch == '(') {
-				stack.push(i);
-			} else if (ch == ')') {
-				if (!stack.empty()) {
-					kept[stack.pop()] = true;
-					kept[i] = true;
-				}
-			} else {
-				kept[i] = true;
-			}
-		}
+      if (ch == '(') {
+        stack.push(i);
+      } else if (ch == ')') {
+        if (!stack.empty()) {
+          kept[stack.pop()] = true;
+          kept[i] = true;
+        }
+      } else {
+        kept[i] = true;
+      }
+    }
 
-		return IntStream.range(0, s.length()).filter(i -> kept[i]).mapToObj(s::charAt)
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
-	}
+    return IntStream.range(0, s.length())
+        .filter(i -> kept[i])
+        .mapToObj(i -> String.valueOf(s.charAt(i)))
+        .collect(Collectors.joining());
+  }
 }
