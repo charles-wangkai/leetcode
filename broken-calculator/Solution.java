f3f6a7c848a4c4158377af5271615b842f240512
Kai Wang <charles.wangkai@gmail.com>
2021-02-21 16:14:20 +0800

    Broken Calculator

--- broken-calculator/Solution.java
+++ broken-calculator/Solution.java
@@ -1,17 +1,17 @@
-public class Solution {
-	public int brokenCalc(int X, int Y) {
-		int operationNum = 0;
-		while (Y > X) {
-			if (Y % 2 != 0) {
-				Y++;
-				operationNum++;
-			}
+class Solution {
+  public int brokenCalc(int X, int Y) {
+    int operationNum = 0;
+    while (Y > X) {
+      if (Y % 2 != 0) {
+        ++Y;
+        ++operationNum;
+      }
 
-			Y /= 2;
-			operationNum++;
-		}
-		operationNum += X - Y;
+      Y /= 2;
+      ++operationNum;
+    }
+    operationNum += X - Y;
 
-		return operationNum;
-	}
+    return operationNum;
+  }
 }
