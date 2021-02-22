b20ec74aa02b74347e530fb9da68c4bea0d183e3
Kai Wang <charles.wangkai@gmail.com>
2021-02-22 18:47:09 +0800

    Longest Word in Dictionary through Deleting

--- longest-word-in-dictionary-through-deleting/Solution.java
+++ longest-word-in-dictionary-through-deleting/Solution.java
@@ -1,28 +1,32 @@
 import java.util.List;
 
-public class Solution {
-	public String findLongestWord(String s, List<String> d) {
-		String result = null;
-		for (String word : d) {
-			if (canForm(s, word) && (result == null || word.length() > result.length()
-					|| (word.length() == result.length() && word.compareTo(result) < 0))) {
-				result = word;
-			}
-		}
-		return (result == null) ? "" : result;
-	}
+class Solution {
+  public String findLongestWord(String s, List<String> d) {
+    String result = null;
+    for (String word : d) {
+      if (canForm(s, word)
+          && (result == null
+              || word.length() > result.length()
+              || (word.length() == result.length() && word.compareTo(result) < 0))) {
+        result = word;
+      }
+    }
 
-	boolean canForm(String s, String word) {
-		int fromIndex = 0;
-		for (char ch : word.toCharArray()) {
-			int index = s.indexOf(ch, fromIndex);
+    return (result == null) ? "" : result;
+  }
 
-			if (index < 0) {
-				return false;
-			}
+  boolean canForm(String s, String word) {
+    int fromIndex = 0;
+    for (char ch : word.toCharArray()) {
+      int index = s.indexOf(ch, fromIndex);
 
-			fromIndex = index + 1;
-		}
-		return true;
-	}
+      if (index == -1) {
+        return false;
+      }
+
+      fromIndex = index + 1;
+    }
+
+    return true;
+  }
 }
