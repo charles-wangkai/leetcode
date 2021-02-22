94fef22201016c3da171ad3f0c77efa0aedc6eee
Kai Wang <charles.wangkai@gmail.com>
2021-02-22 08:35:50 +0800

    Map of Highest Peak

---
+++ map-of-highest-peak/Solution.java
@@ -0,0 +1,52 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
+
+  public int[][] highestPeak(int[][] isWater) {
+    int m = isWater.length;
+    int n = isWater[0].length;
+
+    int[][] result = new int[m][n];
+    for (int r = 0; r < result.length; ++r) {
+      Arrays.fill(result[r], -1);
+    }
+
+    Queue<Point> queue = new ArrayDeque<>();
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (isWater[r][c] == 1) {
+          result[r][c] = 0;
+          queue.offer(new Point(r, c));
+        }
+      }
+    }
+
+    while (!queue.isEmpty()) {
+      Point head = queue.poll();
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r + R_OFFSETS[i];
+        int adjC = head.c + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && result[adjR][adjC] == -1) {
+          result[adjR][adjC] = result[head.r][head.c] + 1;
+          queue.offer(new Point(adjR, adjC));
+        }
+      }
+    }
+
+    return result;
+  }
+}
+
+class Point {
+  int r;
+  int c;
+
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
+}
