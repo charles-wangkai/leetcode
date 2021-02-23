ee860d10420957b83e3f841a6bc7a236111637c6
Kai Wang <charles.wangkai@gmail.com>
2021-02-23 18:09:12 +0800

    Search a 2D Matrix II

--- search-a-2d-matrix-ii/Solution.java
+++ search-a-2d-matrix-ii/Solution.java
@@ -1,25 +1,20 @@
-public class Solution {
-	public boolean searchMatrix(int[][] matrix, int target) {
-		int row = matrix.length;
-		if (row == 0) {
-			return false;
-		}
-		int col = matrix[0].length;
-		if (col == 0) {
-			return false;
-		}
+class Solution {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int row = matrix.length;
+    int col = matrix[0].length;
 
-		int i = row - 1;
-		int j = 0;
-		while (i >= 0 && j < col) {
-			if (matrix[i][j] == target) {
-				return true;
-			} else if (matrix[i][j] < target) {
-				j++;
-			} else {
-				i--;
-			}
-		}
-		return false;
-	}
+    int r = row - 1;
+    int c = 0;
+    while (r >= 0 && c < col) {
+      if (matrix[r][c] == target) {
+        return true;
+      } else if (matrix[r][c] < target) {
+        ++c;
+      } else {
+        --r;
+      }
+    }
+
+    return false;
+  }
 }
