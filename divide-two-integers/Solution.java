f226a6c1fbc5da3100c23b2766a3d277e7eca6e6
Kai Wang <charles.wangkai@gmail.com>
2021-02-27 17:49:09 +0800

    Divide Two Integers

--- divide-two-integers/Solution.java
+++ divide-two-integers/Solution.java
@@ -1,41 +1,44 @@
-public class Solution {
-	public int divide(int dividend, int divisor) {
-		long a = dividend;
-		long b = divisor;
-		boolean positive = true;
-		if (a < 0) {
-			a = -a;
-			positive = !positive;
-		}
-		if (b < 0) {
-			b = -b;
-			positive = !positive;
-		}
+class Solution {
+  public int divide(int dividend, int divisor) {
+    long a = dividend;
+    long b = divisor;
+    boolean positive = true;
+    if (a < 0) {
+      a = -a;
+      positive = !positive;
+    }
+    if (b < 0) {
+      b = -b;
+      positive = !positive;
+    }
 
-		if (a < b) {
-			return 0;
-		}
+    if (a < b) {
+      return 0;
+    }
 
-		long result = 0;
-		long power = b;
-		long factor = 1;
-		while (power << 1 <= a) {
-			power <<= 1;
-			factor <<= 1;
-		}
+    long result = 0;
+    long power = b;
+    long factor = 1;
+    while (power << 1 <= a) {
+      power <<= 1;
+      factor <<= 1;
+    }
 
-		while (power >= b) {
-			if (a >= power) {
-				a -= power;
-				result += factor;
-			}
-			power >>= 1;
-			factor >>= 1;
-		}
+    while (power >= b) {
+      if (a >= power) {
+        a -= power;
+        result += factor;
+      }
+      power >>= 1;
+      factor >>= 1;
+    }
 
-		if (!positive) {
-			result = -result;
-		}
-		return (int) result;
-	}
+    if (!positive) {
+      result = -result;
+    }
+
+    return (result >= Integer.MIN_VALUE && result <= Integer.MAX_VALUE)
+        ? (int) result
+        : Integer.MAX_VALUE;
+  }
 }
