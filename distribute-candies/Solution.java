38c8bb1ab012d966af166c311291bc83555b6306
Kai Wang <charles.wangkai@gmail.com>
2021-03-01 22:13:56 +0800

    Distribute Candies

--- distribute-candies/Solution.java
+++ distribute-candies/Solution.java
@@ -1,10 +1,7 @@
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
-public class Solution {
-	public int distributeCandies(int[] candies) {
-		return Math.min(candies.length / 2,
-				Arrays.stream(candies).collect(HashSet<Integer>::new, Set::add, Set::addAll).size());
-	}
+class Solution {
+  public int distributeCandies(int[] candies) {
+    return Math.min(candies.length / 2, (int) Arrays.stream(candies).distinct().count());
+  }
 }
