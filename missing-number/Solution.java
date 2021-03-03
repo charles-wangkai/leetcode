0fde8a61b93d763a4243e8ed654f558e83d72170
Kai Wang <charles.wangkai@gmail.com>
2021-03-03 17:33:55 +0800

    Missing Number

--- missing-number/Solution.java
+++ missing-number/Solution.java
@@ -1,32 +1,8 @@
-public class Solution {
-	public int missingNumber(int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
-			int number = getNumber(nums, i);
-			if (number < nums.length) {
-				mark(nums, number);
-			}
-		}
-		for (int i = 0; i < nums.length; i++) {
-			if (!isMarked(nums, i)) {
-				return i;
-			}
-		}
-		return nums.length;
-	}
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-	int getNumber(int[] a, int index) {
-		return isMarked(a, index) ? flip(a[index]) : a[index];
-	}
-
-	void mark(int[] a, int index) {
-		a[index] = flip(a[index]);
-	}
-
-	boolean isMarked(int[] a, int index) {
-		return a[index] < 0;
-	}
-
-	int flip(int number) {
-		return -1 - number;
-	}
+class Solution {
+  public int missingNumber(int[] nums) {
+    return IntStream.rangeClosed(0, nums.length).sum() - Arrays.stream(nums).sum();
+  }
 }
