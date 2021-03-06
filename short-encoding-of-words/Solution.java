dcfe99baeac9957cdecaf78cc957bfd09fa393c5
Kai Wang <charles.wangkai@gmail.com>
2021-03-06 21:53:03 +0800

    Short Encoding of Words

--- short-encoding-of-words/Solution.java
+++ short-encoding-of-words/Solution.java
@@ -1,22 +1,24 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Solution {
-	public int minimumLengthEncoding(String[] words) {
-		Arrays.sort(words, (word1, word2) -> Integer.compare(word2.length(), word1.length()));
+class Solution {
+  public int minimumLengthEncoding(String[] words) {
+    Arrays.sort(words, Comparator.comparing(String::length).reversed());
 
-		Set<String> suffixes = new HashSet<String>();
-		int result = 0;
-		for (String word : words) {
-			if (!suffixes.contains(word)) {
-				result += word.length() + 1;
+    Set<String> suffixes = new HashSet<>();
+    int result = 0;
+    for (String word : words) {
+      if (!suffixes.contains(word)) {
+        result += word.length() + 1;
 
-				for (int i = 0; i < word.length(); i++) {
-					suffixes.add(word.substring(i));
-				}
-			}
-		}
-		return result;
-	}
+        for (int i = 0; i < word.length(); ++i) {
+          suffixes.add(word.substring(i));
+        }
+      }
+    }
+
+    return result;
+  }
 }
