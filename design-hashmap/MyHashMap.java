e73ae5db97fc95b3d9388d20ce8f8d26d7d170a1
Kai Wang <charles.wangkai@gmail.com>
2021-03-07 18:16:55 +0800

    Design HashMap

--- design-hashmap/MyHashMap.java
+++ design-hashmap/MyHashMap.java
@@ -1,33 +1,25 @@
-public class MyHashMap {
-	private static final int SIZE = 1000001;
+class MyHashMap {
+  private static final int SIZE = 1000001;
 
-	private Integer[] values;
+  private Integer[] values = new Integer[SIZE];
 
-	/** Initialize your data structure here. */
-	public MyHashMap() {
-		values = new Integer[SIZE];
-	}
+  /** value will always be positive. */
+  public void put(int key, int value) {
+    values[key] = value;
+  }
 
-	/** value will always be positive. */
-	public void put(int key, int value) {
-		values[key] = value;
-	}
+  /**
+   * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
+   * for the key
+   */
+  public int get(int key) {
+    return (values[key] == null) ? -1 : values[key];
+  }
 
-	/**
-	 * Returns the value to which the specified key is mapped, or -1 if this map
-	 * contains no mapping for the key
-	 */
-	public int get(int key) {
-		return values[key] == null ? -1 : values[key];
-	}
-
-	/**
-	 * Removes the mapping of the specified value key if this map contains a mapping
-	 * for the key
-	 */
-	public void remove(int key) {
-		values[key] = null;
-	}
+  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+  public void remove(int key) {
+    values[key] = null;
+  }
 }
 
 // Your MyHashMap object will be instantiated and called as such:
