81af432fe30c6c406250a0e25e35b7c34709be74
Kai Wang <charles.wangkai@gmail.com>
2021-03-12 17:37:10 +0800

    Check If a String Contains All Binary Codes of Size K

--- check-if-a-string-contains-all-binary-codes-of-size-k/Solution.java
+++ check-if-a-string-contains-all-binary-codes-of-size-k/Solution.java
@@ -1,7 +1,11 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public boolean hasAllCodes(String s, int k) {
-        return IntStream.range(0, s.length() - k + 1).mapToObj(i -> s.substring(i, i + k)).distinct().count() == 1 << k;
-    }
+  public boolean hasAllCodes(String s, int k) {
+    return IntStream.rangeClosed(0, s.length() - k)
+            .mapToObj(i -> s.substring(i, i + k))
+            .distinct()
+            .count()
+        == 1 << k;
+  }
 }
