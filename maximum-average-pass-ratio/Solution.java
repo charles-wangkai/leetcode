44deb87a54e27f984b4b30dc5d2dfa71dc2b2d42
Kai Wang <charles.wangkai@gmail.com>
2021-03-14 15:52:47 +0800

    Maximum Average Pass Ratio

---
+++ maximum-average-pass-ratio/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+  public double maxAverageRatio(int[][] classes, int extraStudents) {
+    PriorityQueue<Element> pq =
+        new PriorityQueue<>(
+            Comparator.comparing(
+                    (Element e) -> (e.pass + 1.0) / (e.total + 1) - (double) e.pass / e.total)
+                .reversed());
+    for (int[] cls : classes) {
+      pq.offer(new Element(cls[0], cls[1]));
+    }
+
+    for (int i = 0; i < extraStudents; ++i) {
+      Element head = pq.poll();
+      pq.offer(new Element(head.pass + 1, head.total + 1));
+    }
+
+    return pq.stream().mapToDouble(e -> (double) e.pass / e.total).average().getAsDouble();
+  }
+}
+
+class Element {
+  int pass;
+  int total;
+
+  Element(int pass, int total) {
+    this.pass = pass;
+    this.total = total;
+  }
+}
