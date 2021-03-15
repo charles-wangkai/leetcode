ccada148b609c6d81ea08877915eb8552bd8868d
Kai Wang <charles.wangkai@gmail.com>
2021-03-15 16:35:27 +0800

    Encode and Decode TinyURL

--- encode-and-decode-tinyurl/Codec.java
+++ encode-and-decode-tinyurl/Codec.java
@@ -4,33 +4,33 @@ import java.util.List;
 import java.util.Map;
 
 public class Codec {
-	static final String DOMAIN = "http://my_short_url.com/";
+  static final String DOMAIN = "http://my_short_url.com/";
 
-	List<String> longUrls = new ArrayList<String>();
-	Map<String, Integer> longUrl2index = new HashMap<String, Integer>();
+  List<String> longUrls = new ArrayList<String>();
+  Map<String, Integer> longUrlToIndex = new HashMap<>();
 
-	// Encodes a URL to a shortened URL.
-	public String encode(String longUrl) {
-		if (!longUrl2index.containsKey(longUrl)) {
-			longUrls.add(longUrl);
-			longUrl2index.put(longUrl, longUrls.size() - 1);
-		}
+  // Encodes a URL to a shortened URL.
+  public String encode(String longUrl) {
+    if (!longUrlToIndex.containsKey(longUrl)) {
+      longUrlToIndex.put(longUrl, longUrls.size());
+      longUrls.add(longUrl);
+    }
 
-		return toShortUrl(longUrl2index.get(longUrl));
-	}
+    return toShortUrl(longUrlToIndex.get(longUrl));
+  }
 
-	// Decodes a shortened URL to its original URL.
-	public String decode(String shortUrl) {
-		return longUrls.get(toIndex(shortUrl));
-	}
+  // Decodes a shortened URL to its original URL.
+  public String decode(String shortUrl) {
+    return longUrls.get(toIndex(shortUrl));
+  }
 
-	String toShortUrl(int index) {
-		return DOMAIN + index;
-	}
+  String toShortUrl(int index) {
+    return DOMAIN + index;
+  }
 
-	int toIndex(String shortUrl) {
-		return Integer.parseInt(shortUrl.substring(shortUrl.lastIndexOf('/') + 1));
-	}
+  int toIndex(String shortUrl) {
+    return Integer.parseInt(shortUrl.substring(DOMAIN.length()));
+  }
 }
 
 // Your Codec object will be instantiated and called as such:
