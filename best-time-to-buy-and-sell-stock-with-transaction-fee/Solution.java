77df6502e600132693df0e506ffa6d504dbc4af2
Kai Wang <charles.wangkai@gmail.com>
2021-03-16 16:57:42 +0800

    Best Time to Buy and Sell Stock with Transaction Fee

--- best-time-to-buy-and-sell-stock-with-transaction-fee/Solution.java
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/Solution.java
@@ -1,13 +1,14 @@
-public class Solution {
-	public int maxProfit(int[] prices, int fee) {
-		int profitWithoutStock = 0;
-		int profitWithStock = Integer.MIN_VALUE;
-		for (int price : prices) {
-			int oldProfitWithoutStock = profitWithoutStock;
+class Solution {
+  public int maxProfit(int[] prices, int fee) {
+    int profitWithoutStock = 0;
+    int profitWithStock = Integer.MIN_VALUE;
+    for (int price : prices) {
+      int oldProfitWithoutStock = profitWithoutStock;
 
-			profitWithoutStock = Math.max(profitWithoutStock, profitWithStock + price);
-			profitWithStock = Math.max(profitWithStock, oldProfitWithoutStock - price - fee);
-		}
-		return profitWithoutStock;
-	}
+      profitWithoutStock = Math.max(profitWithoutStock, profitWithStock + price);
+      profitWithStock = Math.max(profitWithStock, oldProfitWithoutStock - price - fee);
+    }
+
+    return profitWithoutStock;
+  }
 }
