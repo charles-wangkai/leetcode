559e032787842cc7770be6fae8399d2cd6287f71
Kai Wang <charles.wangkai@gmail.com>
2021-03-17 17:12:32 +0800

    Generate Random Point in a Circle

--- generate-random-point-in-a-circle/Solution.java
+++ generate-random-point-in-a-circle/Solution.java
@@ -1,28 +1,28 @@
-public class Solution {
-	private double radius;
-	private double xCenter;
-	private double yCenter;
+class Solution {
+  private double radius;
+  private double xCenter;
+  private double yCenter;
 
-	public Solution(double radius, double x_center, double y_center) {
-		this.radius = radius;
-		xCenter = x_center;
-		yCenter = y_center;
-	}
+  public Solution(double radius, double x_center, double y_center) {
+    this.radius = radius;
+    xCenter = x_center;
+    yCenter = y_center;
+  }
 
-	public double[] randPoint() {
-		while (true) {
-			double x = xCenter - radius + Math.random() * (radius * 2);
-			double y = yCenter - radius + Math.random() * (radius * 2);
+  public double[] randPoint() {
+    while (true) {
+      double x = xCenter + Math.random() * (radius * 2) - radius;
+      double y = yCenter + Math.random() * (radius * 2) - radius;
 
-			if (computeDistance(x, y, xCenter, yCenter) <= radius) {
-				return new double[] { x, y };
-			}
-		}
-	}
+      if (computeDistance(x, y, xCenter, yCenter) <= radius) {
+        return new double[] {x, y};
+      }
+    }
+  }
 
-	private double computeDistance(double x1, double y1, double x2, double y2) {
-		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-	}
+  private double computeDistance(double x1, double y1, double x2, double y2) {
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  }
 }
 
 // Your Solution object will be instantiated and called as such:
