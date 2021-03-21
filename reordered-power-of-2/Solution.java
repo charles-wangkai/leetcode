bc1f2b8dfa0e023574924c1ec3cf718085120b70
Kai Wang <charles.wangkai@gmail.com>
2021-03-21 18:05:21 +0800

    Reordered Power of 2

--- reordered-power-of-2/Solution.java
+++ reordered-power-of-2/Solution.java
@@ -1,35 +1,20 @@
-public class Solution {
-	public boolean reorderedPowerOf2(int N) {
-		return search(String.valueOf(N).toCharArray(), 0);
-	}
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-	boolean search(char[] digits, int index) {
-		if (index == digits.length) {
-			return isPowerOf2(Integer.parseInt(new String(digits)));
-		}
+class Solution {
+  public boolean reorderedPowerOf2(int N) {
+    return IntStream.range(0, 31).anyMatch(i -> check(N, 1 << i));
+  }
 
-		for (int i = index; i < digits.length; i++) {
-			if (index == 0 && digits[i] == '0') {
-				continue;
-			}
+  boolean check(int N, int power) {
+    return buildKey(N).equals(buildKey(power));
+  }
 
-			swap(digits, index, i);
-			if (search(digits, index + 1)) {
-				return true;
-			}
-			swap(digits, index, i);
-		}
-
-		return false;
-	}
-
-	boolean isPowerOf2(int x) {
-		return (x & (x - 1)) == 0;
-	}
-
-	void swap(char[] digits, int index1, int index2) {
-		char temp = digits[index1];
-		digits[index1] = digits[index2];
-		digits[index2] = temp;
-	}
+  String buildKey(int x) {
+    return String.valueOf(x)
+        .chars()
+        .sorted()
+        .mapToObj(ch -> String.valueOf((char) ch))
+        .collect(Collectors.joining());
+  }
 }
