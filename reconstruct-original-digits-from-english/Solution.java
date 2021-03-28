a6fbc56a46270d5ead958d73060632d4a9459904
Kai Wang <charles.wangkai@gmail.com>
2021-03-28 16:19:13 +0800

    Reconstruct Original Digits from English

--- reconstruct-original-digits-from-english/Solution.java
+++ reconstruct-original-digits-from-english/Solution.java
@@ -1,49 +1,50 @@
-public class Solution {
-	final String[] REPRESENTATIONS = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-	final int[] SEQUENCE = { 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
-	final char[] KEY_LETTERS = { 'z', 'o', 'w', 'r', 'u', 'f', 'x', 's', 'g', 'i' };
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-	public String originalDigits(String s) {
-		int[] letterCounts = buildLetterCounts(s);
+class Solution {
+  final String[] REPRESENTATIONS = {
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+  };
+  final int[] SEQUENCE = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+  final char[] KEY_LETTERS = {'z', 'o', 'w', 'r', 'u', 'f', 'x', 's', 'g', 'i'};
 
-		int[] digitCounts = new int[10];
+  public String originalDigits(String s) {
+    int[] letterCounts = buildLetterCounts(s);
 
-		for (int digit : SEQUENCE) {
-			digitCounts[digit] = getLetterCount(letterCounts, KEY_LETTERS[digit]);
+    int[] digitCounts = new int[10];
 
-			remove(letterCounts, REPRESENTATIONS[digit], digitCounts[digit]);
-		}
+    for (int digit : SEQUENCE) {
+      digitCounts[digit] = getLetterCount(letterCounts, KEY_LETTERS[digit]);
 
-		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < digitCounts.length; i++) {
-			for (int j = 0; j < digitCounts[i]; j++) {
-				result.append(i);
-			}
-		}
-		return result.toString();
-	}
+      remove(letterCounts, REPRESENTATIONS[digit], digitCounts[digit]);
+    }
 
-	void remove(int[] letterCounts, String representation, int count) {
-		for (char letter : representation.toCharArray()) {
-			setLetterCount(letterCounts, letter, getLetterCount(letterCounts, letter) - count);
-		}
-	}
+    return IntStream.range(0, digitCounts.length)
+        .mapToObj(i -> String.valueOf(i).repeat(digitCounts[i]))
+        .collect(Collectors.joining());
+  }
 
-	int[] buildLetterCounts(String s) {
-		int[] letterCounts = new int[26];
+  void remove(int[] letterCounts, String representation, int count) {
+    for (char letter : representation.toCharArray()) {
+      setLetterCount(letterCounts, letter, getLetterCount(letterCounts, letter) - count);
+    }
+  }
 
-		for (char letter : s.toCharArray()) {
-			setLetterCount(letterCounts, letter, getLetterCount(letterCounts, letter) + 1);
-		}
+  int[] buildLetterCounts(String s) {
+    int[] letterCounts = new int[26];
 
-		return letterCounts;
-	}
+    for (char letter : s.toCharArray()) {
+      setLetterCount(letterCounts, letter, getLetterCount(letterCounts, letter) + 1);
+    }
 
-	int getLetterCount(int[] letterCounts, char letter) {
-		return letterCounts[letter - 'a'];
-	}
+    return letterCounts;
+  }
 
-	void setLetterCount(int[] letterCounts, char letter, int letterCount) {
-		letterCounts[letter - 'a'] = letterCount;
-	}
+  int getLetterCount(int[] letterCounts, char letter) {
+    return letterCounts[letter - 'a'];
+  }
+
+  void setLetterCount(int[] letterCounts, char letter, int letterCount) {
+    letterCounts[letter - 'a'] = letterCount;
+  }
 }
