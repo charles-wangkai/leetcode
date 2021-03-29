da5e7705b7b4e2f76906bd60ff76d0463a3d2ba1
Kai Wang <charles.wangkai@gmail.com>
2021-03-29 21:07:16 +0800

    Flip Binary Tree To Match Preorder Traversal

--- flip-binary-tree-to-match-preorder-traversal/Solution.java
+++ flip-binary-tree-to-match-preorder-traversal/Solution.java
@@ -1,75 +1,84 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
-		List<Integer> result = search(root, Arrays.stream(voyage).boxed().collect(Collectors.toList()));
-		return (result == null) ? Collections.singletonList(-1) : result;
-	}
+class Solution {
+  public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+    List<Integer> result = search(root, Arrays.stream(voyage).boxed().collect(Collectors.toList()));
 
-	List<Integer> search(TreeNode node, List<Integer> voyage) {
-		if (node == null) {
-			return voyage.isEmpty() ? Collections.emptyList() : null;
-		}
+    return (result == null) ? List.of(-1) : result;
+  }
 
-		if (voyage.isEmpty() || voyage.get(0) != node.val) {
-			return null;
-		}
+  List<Integer> search(TreeNode node, List<Integer> voyage) {
+    if (node == null) {
+      return voyage.isEmpty() ? List.of() : null;
+    }
 
-		if (node.left == null) {
-			return search(node.right, voyage.subList(1, voyage.size()));
-		}
-		if (node.right == null) {
-			return search(node.left, voyage.subList(1, voyage.size()));
-		}
+    if (voyage.isEmpty() || voyage.get(0) != node.val) {
+      return null;
+    }
 
-		int leftBeginIndex = voyage.indexOf(node.left.val);
-		if (leftBeginIndex < 0) {
-			return null;
-		}
+    if (node.left == null) {
+      return search(node.right, voyage.subList(1, voyage.size()));
+    }
+    if (node.right == null) {
+      return search(node.left, voyage.subList(1, voyage.size()));
+    }
 
-		int rightBeginIndex = voyage.indexOf(node.right.val);
-		if (rightBeginIndex < 0) {
-			return null;
-		}
+    int leftBeginIndex = voyage.indexOf(node.left.val);
+    if (leftBeginIndex == -1) {
+      return null;
+    }
 
-		int leftEndIndex;
-		int rightEndIndex;
-		if (leftBeginIndex < rightBeginIndex) {
-			leftEndIndex = rightBeginIndex;
-			rightEndIndex = voyage.size();
-		} else {
-			rightEndIndex = leftBeginIndex;
-			leftEndIndex = voyage.size();
-		}
+    int rightBeginIndex = voyage.indexOf(node.right.val);
+    if (rightBeginIndex == -1) {
+      return null;
+    }
 
-		List<Integer> leftResult = search(node.left, voyage.subList(leftBeginIndex, leftEndIndex));
-		List<Integer> rightResult = search(node.right, voyage.subList(rightBeginIndex, rightEndIndex));
+    int leftEndIndex;
+    int rightEndIndex;
+    if (leftBeginIndex < rightBeginIndex) {
+      leftEndIndex = rightBeginIndex;
+      rightEndIndex = voyage.size();
+    } else {
+      rightEndIndex = leftBeginIndex;
+      leftEndIndex = voyage.size();
+    }
 
-		if (leftResult == null || rightResult == null) {
-			return null;
-		}
+    List<Integer> leftResult = search(node.left, voyage.subList(leftBeginIndex, leftEndIndex));
+    List<Integer> rightResult = search(node.right, voyage.subList(rightBeginIndex, rightEndIndex));
 
-		List<Integer> result = new ArrayList<>();
-		if (leftBeginIndex > rightBeginIndex) {
-			result.add(node.val);
-		}
-		result.addAll(leftResult);
-		result.addAll(rightResult);
-		return result;
-	}
+    if (leftResult == null || rightResult == null) {
+      return null;
+    }
+
+    List<Integer> result = new ArrayList<>();
+    if (leftBeginIndex > rightBeginIndex) {
+      result.add(node.val);
+    }
+    result.addAll(leftResult);
+    result.addAll(rightResult);
+
+    return result;
+  }
 }
