fdeb3219bddf3c0b494fa95e20125a0b2359183e
Kai Wang <charles.wangkai@gmail.com>
2021-03-30 22:40:42 +0800

    Russian Doll Envelopes

--- russian-doll-envelopes/Solution.java
+++ russian-doll-envelopes/Solution.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
-	public int maxEnvelopes(int[][] envelopes) {
-		Arrays.sort(envelopes, (e1, e2) -> e1[0] - e2[0]);
+  public int maxEnvelopes(int[][] envelopes) {
+    Arrays.sort(envelopes, Comparator.comparing(e -> e[0]));
 
-		int[] maxLengths = new int[envelopes.length];
-		int result = 0;
-		for (int i = 0; i < maxLengths.length; i++) {
-			maxLengths[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
-					maxLengths[i] = Math.max(maxLengths[i], maxLengths[j] + 1);
-				}
-			}
-			result = Math.max(result, maxLengths[i]);
-		}
-		return result;
-	}
+    int[] maxLengths = new int[envelopes.length];
+    for (int i = 0; i < maxLengths.length; ++i) {
+      maxLengths[i] = 1;
+      for (int j = 0; j < i; ++j) {
+        if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
+          maxLengths[i] = Math.max(maxLengths[i], maxLengths[j] + 1);
+        }
+      }
+    }
+
+    return Arrays.stream(maxLengths).max().getAsInt();
+  }
 }
