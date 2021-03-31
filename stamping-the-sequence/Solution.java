5d3d3eb8438dfd3c59e70186a6eeb17d4681c174
Kai Wang <charles.wangkai@gmail.com>
2021-03-31 18:00:48 +0800

    Stamping The Sequence

--- stamping-the-sequence/Solution.java
+++ stamping-the-sequence/Solution.java
@@ -1,61 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public int[] movesToStamp(String stamp, String target) {
-		List<Integer> indices = new ArrayList<>();
-		int remainNum = target.length();
-		StringBuilder sb = new StringBuilder(target);
-		while (remainNum != 0) {
-			boolean found = false;
-			for (int i = 0; i <= sb.length() - stamp.length(); i++) {
-				int matchNum = match(sb, stamp, i);
-				if (matchNum > 0) {
-					indices.add(i);
-					remainNum -= matchNum;
-					found = true;
-					break;
-				}
-			}
+class Solution {
+  public int[] movesToStamp(String stamp, String target) {
+    List<Integer> indices = new ArrayList<>();
+    int remainNum = target.length();
+    StringBuilder sb = new StringBuilder(target);
+    while (remainNum != 0) {
+      boolean found = false;
+      for (int i = 0; i <= sb.length() - stamp.length(); ++i) {
+        int matchNum = match(sb, stamp, i);
+        if (matchNum > 0) {
+          indices.add(i);
+          remainNum -= matchNum;
+          found = true;
 
-			if (!found) {
-				return new int[0];
-			}
-		}
+          break;
+        }
+      }
 
-		reverse(indices);
-		return indices.stream().mapToInt(x -> x).toArray();
-	}
+      if (!found) {
+        return new int[0];
+      }
+    }
 
-	int match(StringBuilder sb, String stamp, int beginIndex) {
-		int matchNum = 0;
-		for (int i = 0; i < stamp.length(); i++) {
-			char stampCh = stamp.charAt(i);
-			char targetCh = sb.charAt(beginIndex + i);
+    return IntStream.range(0, indices.size())
+        .map(i -> indices.get(indices.size() - 1 - i))
+        .toArray();
+  }
 
-			if (targetCh != '*') {
-				if (targetCh != stampCh) {
-					return -1;
-				} else {
-					matchNum++;
-				}
-			}
-		}
+  int match(StringBuilder sb, String stamp, int beginIndex) {
+    int matchNum = 0;
+    for (int i = 0; i < stamp.length(); ++i) {
+      char stampCh = stamp.charAt(i);
+      char targetCh = sb.charAt(beginIndex + i);
 
-		if (matchNum > 0) {
-			for (int i = 0; i < stamp.length(); i++) {
-				sb.setCharAt(beginIndex + i, '*');
-			}
-		}
+      if (targetCh != '*') {
+        if (targetCh != stampCh) {
+          return -1;
+        }
 
-		return matchNum;
-	}
+        ++matchNum;
+      }
+    }
 
-	void reverse(List<Integer> list) {
-		for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
-			int temp = list.get(i);
-			list.set(i, list.get(j));
-			list.set(j, temp);
-		}
-	}
+    for (int i = 0; i < stamp.length(); ++i) {
+      sb.setCharAt(beginIndex + i, '*');
+    }
+
+    return matchNum;
+  }
 }
