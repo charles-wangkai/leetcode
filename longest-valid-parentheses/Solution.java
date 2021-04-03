c796ec3ad92112b40af3d039bf53f0530799d4fa
Kai Wang <charles.wangkai@gmail.com>
2021-04-03 21:44:46 +0800

    Longest Valid Parentheses

--- longest-valid-parentheses/Solution.java
+++ longest-valid-parentheses/Solution.java
@@ -1,22 +1,23 @@
 import java.util.Stack;
 
-public class Solution {
-    public int longestValidParentheses(String s) {
-        int maxLength = 0;
-        Stack<Integer> leftIndices = new Stack<Integer>();
-        leftIndices.add(-1);
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                leftIndices.add(i);
-            } else {
-                leftIndices.pop();
-                if (leftIndices.empty()) {
-                    leftIndices.push(i);
-                } else {
-                    maxLength = Math.max(maxLength, i - leftIndices.peek());
-                }
-            }
+class Solution {
+  public int longestValidParentheses(String s) {
+    int maxLength = 0;
+    Stack<Integer> leftIndices = new Stack<>();
+    leftIndices.add(-1);
+    for (int i = 0; i < s.length(); ++i) {
+      if (s.charAt(i) == '(') {
+        leftIndices.push(i);
+      } else {
+        leftIndices.pop();
+        if (leftIndices.empty()) {
+          leftIndices.push(i);
+        } else {
+          maxLength = Math.max(maxLength, i - leftIndices.peek());
         }
-        return maxLength;
+      }
     }
+
+    return maxLength;
+  }
 }
