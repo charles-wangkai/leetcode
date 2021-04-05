7ce4bea93b8fbb24dea08a2369afe28bdc7cacdc
Kai Wang <charles.wangkai@gmail.com>
2021-04-05 16:41:08 +0800

    Global and Local Inversions

--- global-and-local-inversions/Solution.java
+++ global-and-local-inversions/Solution.java
@@ -1,7 +1,7 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public boolean isIdealPermutation(int[] A) {
-		return IntStream.range(0, A.length).allMatch(i -> Math.abs(A[i] - i) <= 1);
-	}
+class Solution {
+  public boolean isIdealPermutation(int[] A) {
+    return IntStream.range(0, A.length).allMatch(i -> Math.abs(A[i] - i) <= 1);
+  }
 }
