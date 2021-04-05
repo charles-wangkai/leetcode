91a1c28d7c6b47f1567313a02a94b8a343018743
Kai Wang <charles.wangkai@gmail.com>
2021-04-05 09:24:35 +0800

    Truncate Sentence

---
+++ truncate-sentence/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+  public String truncateSentence(String s, int k) {
+    return Arrays.stream(s.split(" ")).limit(k).collect(Collectors.joining(" "));
+  }
+}
