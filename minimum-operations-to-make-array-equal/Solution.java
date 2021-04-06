5cff47b677a3c4551e0a38a8f012323b1571779b
Kai Wang <charles.wangkai@gmail.com>
2021-04-06 16:35:53 +0800

    Minimum Operations to Make Array Equal

--- minimum-operations-to-make-array-equal/Solution.java
+++ minimum-operations-to-make-array-equal/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int minOperations(int n) {
-        if (n % 2 == 0) {
-            return n * n / 4;
-        } else {
-            return n / 2 * (n / 2 + 1);
-        }
+  public int minOperations(int n) {
+    if (n % 2 == 0) {
+      return n * n / 4;
+    } else {
+      return n / 2 * (n / 2 + 1);
     }
+  }
 }
