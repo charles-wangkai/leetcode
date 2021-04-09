b4b76e0352d9d4ee600f7bd530316b0a9c75641d
Kai Wang <charles.wangkai@gmail.com>
2021-04-09 17:28:50 +0800

    Verifying an Alien Dictionary

--- verifying-an-alien-dictionary/Solution.java
+++ verifying-an-alien-dictionary/Solution.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public boolean isAlienSorted(String[] words, String order) {
-		Map<Character, Character> translation = buildTranslation(order);
+class Solution {
+  public boolean isAlienSorted(String[] words, String order) {
+    Map<Character, Character> translation = buildTranslation(order);
+    String[] translated =
+        Arrays.stream(words).map(word -> translate(translation, word)).toArray(String[]::new);
 
-		String[] translated = Arrays.stream(words).map(word -> translate(translation, word)).toArray(String[]::new);
-		return IntStream.range(0, translated.length - 1).allMatch(i -> translated[i].compareTo(translated[i + 1]) <= 0);
-	}
+    return IntStream.range(0, translated.length - 1)
+        .allMatch(i -> translated[i].compareTo(translated[i + 1]) <= 0);
+  }
 
-	Map<Character, Character> buildTranslation(String order) {
-		Map<Character, Character> translation = new HashMap<>();
-		for (int i = 0; i < order.length(); i++) {
-			translation.put(order.charAt(i), (char) ('a' + i));
-		}
-		return translation;
-	}
+  Map<Character, Character> buildTranslation(String order) {
+    return IntStream.range(0, order.length())
+        .boxed()
+        .collect(Collectors.toMap(order::charAt, i -> (char) ('a' + i)));
+  }
 
-	String translate(Map<Character, Character> translation, String word) {
-		return word.chars().boxed().reduce(new StringBuilder(),
-				(result, element) -> result.append(translation.get((char) element.intValue())), StringBuilder::append)
-				.toString();
-	}
+  String translate(Map<Character, Character> translation, String word) {
+    return word.chars()
+        .mapToObj(ch -> String.valueOf(translation.get((char) ch)))
+        .collect(Collectors.joining());
+  }
 }
