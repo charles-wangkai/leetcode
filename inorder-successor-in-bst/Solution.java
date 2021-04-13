a01c7979e2b365e0b6aebabb39f0bcfcfed765bd
Kai Wang <charles.wangkai@gmail.com>
2021-04-13 09:52:17 +0800

    Inorder Successor in BST

--- inorder-successor-in-bst/Solution.java
+++ inorder-successor-in-bst/Solution.java
@@ -2,45 +2,50 @@ import java.util.Stack;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
-public class Solution {
-	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-		TreeNode successor;
-		if (p.right == null) {
-			Stack<TreeNode> path = new Stack<TreeNode>();
-			searchPath(root, p, path);
-			TreeNode node = path.pop();
-			while (!path.empty() && path.peek().left != node) {
-				node = path.pop();
-			}
-			successor = path.empty() ? null : path.peek();
-		} else {
-			successor = p.right;
-			while (successor.left != null) {
-				successor = successor.left;
-			}
-		}
-		return successor;
-	}
+class Solution {
+  public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    TreeNode successor;
+    if (p.right == null) {
+      Stack<TreeNode> path = new Stack<>();
+      searchPath(path, p, root);
 
-	void searchPath(TreeNode node, TreeNode p, Stack<TreeNode> path) {
-		path.push(node);
-		if (path.peek() != p && node.left != null) {
-			searchPath(node.left, p, path);
-		}
-		if (path.peek() != p && node.right != null) {
-			searchPath(node.right, p, path);
-		}
-		if (path.peek() != p) {
-			path.pop();
-		}
-	}
+      TreeNode node = path.pop();
+      while (!path.empty() && path.peek().left != node) {
+        node = path.pop();
+      }
+
+      successor = path.empty() ? null : path.peek();
+    } else {
+      successor = p.right;
+      while (successor.left != null) {
+        successor = successor.left;
+      }
+    }
+
+    return successor;
+  }
+
+  void searchPath(Stack<TreeNode> path, TreeNode p, TreeNode node) {
+    path.push(node);
+
+    if (path.peek() != p && node.left != null) {
+      searchPath(path, p, node.left);
+    }
+    if (path.peek() != p && node.right != null) {
+      searchPath(path, p, node.right);
+    }
+
+    if (path.peek() != p) {
+      path.pop();
+    }
+  }
 }
