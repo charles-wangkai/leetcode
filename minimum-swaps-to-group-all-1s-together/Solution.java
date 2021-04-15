41633d666ec35d73e3fbfd7cf759228a1c79e55f
Kai Wang <charles.wangkai@gmail.com>
2021-04-15 16:52:07 +0800

    Minimum Swaps to Group All 1's Together

--- minimum-swaps-to-group-all-1s-together/Solution.java
+++ minimum-swaps-to-group-all-1s-together/Solution.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int minSwaps(int[] data) {
-		int count1 = (int) Arrays.stream(data).filter(x -> x == 1).count();
-		if (count1 == 0) {
-			return 0;
-		}
+class Solution {
+  public int minSwaps(int[] data) {
+    int oneCount = (int) Arrays.stream(data).filter(x -> x == 1).count();
+    if (oneCount == 0) {
+      return 0;
+    }
 
-		int sum = IntStream.range(0, count1).map(i -> data[i]).sum();
-		int maxSum = sum;
-		for (int i = count1; i < data.length; i++) {
-			sum += data[i] - data[i - count1];
-			maxSum = Math.max(maxSum, sum);
-		}
+    int sum = IntStream.range(0, oneCount).map(i -> data[i]).sum();
+    int maxSum = sum;
+    for (int i = oneCount; i < data.length; ++i) {
+      sum += data[i] - data[i - oneCount];
+      maxSum = Math.max(maxSum, sum);
+    }
 
-		return count1 - maxSum;
-	}
+    return oneCount - maxSum;
+  }
 }
