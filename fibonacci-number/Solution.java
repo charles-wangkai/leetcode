28a933c3fce4f07e24c7f29023c07d56cfd69904
Kai Wang <charles.wangkai@gmail.com>
2021-04-15 16:56:26 +0800

    Fibonacci Number

--- fibonacci-number/Solution.java
+++ fibonacci-number/Solution.java
@@ -1,17 +1,18 @@
-public class Solution {
-	public int fib(int N) {
-		if (N == 0) {
-			return 0;
-		}
+class Solution {
+  public int fib(int N) {
+    if (N == 0) {
+      return 0;
+    }
 
-		int prev = 0;
-		int cur = 1;
-		for (int i = 0; i < N - 1; i++) {
-			int next = prev + cur;
+    int prev = 0;
+    int cur = 1;
+    for (int i = 0; i < N - 1; ++i) {
+      int next = prev + cur;
 
-			prev = cur;
-			cur = next;
-		}
-		return cur;
-	}
+      prev = cur;
+      cur = next;
+    }
+
+    return cur;
+  }
 }
