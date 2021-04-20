ef67d9f885e34d668193c11f0d084892bec34846
Kai Wang <charles.wangkai@gmail.com>
2021-04-20 15:14:38 +0800

    N-ary Tree Preorder Traversal

--- n-ary-tree-preorder-traversal/Solution.java
+++ n-ary-tree-preorder-traversal/Solution.java
@@ -1,38 +1,41 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
 // Definition for a Node.
 class Node {
-	public int val;
-	public List<Node> children;
+  public int val;
+  public List<Node> children;
 
-	public Node() {
-	}
+  public Node() {}
 
-	public Node(int _val, List<Node> _children) {
-		val = _val;
-		children = _children;
-	}
+  public Node(int _val) {
+    val = _val;
+  }
+
+  public Node(int _val, List<Node> _children) {
+    val = _val;
+    children = _children;
+  }
 }
 
-public class Solution {
-	public List<Integer> preorder(Node root) {
-		if (root == null) {
-			return Collections.emptyList();
-		}
+class Solution {
+  public List<Integer> preorder(Node root) {
+    if (root == null) {
+      return List.of();
+    }
 
-		List<Integer> result = new ArrayList<>();
-		Stack<Node> stack = new Stack<>();
-		stack.push(root);
-		while (!stack.empty()) {
-			Node top = stack.pop();
-			result.add(top.val);
-			for (int i = top.children.size() - 1; i >= 0; i--) {
-				stack.push(top.children.get(i));
-			}
-		}
-		return result;
-	}
+    List<Integer> result = new ArrayList<>();
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    while (!stack.empty()) {
+      Node top = stack.pop();
+      result.add(top.val);
+      for (int i = top.children.size() - 1; i >= 0; --i) {
+        stack.push(top.children.get(i));
+      }
+    }
+
+    return result;
+  }
 }
