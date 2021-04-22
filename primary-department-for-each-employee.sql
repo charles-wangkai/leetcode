c9b0d2f0d55d67b9f155074fa396d63b4b848a20
Kai Wang <charles.wangkai@gmail.com>
2021-04-22 20:51:54 +0800

    Primary Department for Each Employee

---
+++ primary-department-for-each-employee.sql
@@ -0,0 +1,20 @@
+SELECT
+  employee_id,
+  department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT
+  e.employee_id,
+  e.department_id
+FROM (
+  SELECT
+    employee_id
+  FROM Employee
+  GROUP BY employee_id
+  HAVING COUNT(*) = 1
+) t
+JOIN Employee e
+ON e.employee_id = t.employee_id
