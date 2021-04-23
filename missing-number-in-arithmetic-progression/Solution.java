cd487ff6ff24bc62db2c990c77d73f8510922279
Kai Wang <charles.wangkai@gmail.com>
2021-04-23 15:19:43 +0800

    Missing Number In Arithmetic Progression

--- missing-number-in-arithmetic-progression/Solution.java
+++ missing-number-in-arithmetic-progression/Solution.java
@@ -1,23 +1,21 @@
-public class Solution {
-	public int missingNumber(int[] arr) {
-		int firstDelta = arr[1] - arr[0];
-		int lastDelta = arr[arr.length - 1] - arr[arr.length - 2];
+class Solution {
+  public int missingNumber(int[] arr) {
+    int firstDelta = arr[1] - arr[0];
+    int lastDelta = arr[arr.length - 1] - arr[arr.length - 2];
 
-		if (firstDelta == 0) {
-			return 0;
-		}
+    int diff;
+    if (firstDelta < 0) {
+      diff = Math.max(firstDelta, lastDelta);
+    } else if (firstDelta > 0) {
+      diff = Math.min(firstDelta, lastDelta);
+    } else {
+      return arr[0];
+    }
 
-		int diff;
-		if (firstDelta < 0) {
-			diff = Math.max(firstDelta, lastDelta);
-		} else {
-			diff = Math.min(firstDelta, lastDelta);
-		}
-
-		for (int i = 0;; i++) {
-			if (arr[i + 1] != arr[i] + diff) {
-				return arr[i] + diff;
-			}
-		}
-	}
+    for (int i = 0; ; ++i) {
+      if (arr[i + 1] != arr[i] + diff) {
+        return arr[i] + diff;
+      }
+    }
+  }
 }
