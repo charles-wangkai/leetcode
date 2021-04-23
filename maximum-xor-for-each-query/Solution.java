4ac9e1a512904ed50b6fee3005d9fa7f1ec336f7
Kai Wang <charles.wangkai@gmail.com>
2021-04-23 15:44:56 +0800

    Maximum XOR for Each Query

---
+++ maximum-xor-for-each-query/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] getMaximumXor(int[] nums, int maximumBit) {
+    int target = (1 << maximumBit) - 1;
+
+    int[] result = new int[nums.length];
+    int prefixXor = Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
+    for (int i = 0; i < result.length; ++i) {
+      result[i] = prefixXor ^ target;
+      prefixXor ^= nums[nums.length - 1 - i];
+    }
+
+    return result;
+  }
+}
