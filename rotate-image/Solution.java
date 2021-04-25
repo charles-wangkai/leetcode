c9f70125b682f2215dd9e63bd9c828c46c5fc429
Kai Wang <charles.wangkai@gmail.com>
2021-04-25 23:41:31 +0800

    Rotate Image

--- rotate-image/Solution.java
+++ rotate-image/Solution.java
@@ -1,14 +1,14 @@
-public class Solution {
-	public void rotate(int[][] matrix) {
-		int size = matrix.length;
-		for (int i = 0; i * 2 < size; i++) {
-			for (int j = i; i + j < size - 1; j++) {
-				int temp = matrix[i][j];
-				matrix[i][j] = matrix[size - 1 - j][i];
-				matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j];
-				matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i];
-				matrix[j][size - 1 - i] = temp;
-			}
-		}
-	}
+class Solution {
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i * 2 < n; ++i) {
+      for (int j = i; i + j < n - 1; ++j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[n - 1 - j][i];
+        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+        matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+        matrix[j][n - 1 - i] = temp;
+      }
+    }
+  }
 }
