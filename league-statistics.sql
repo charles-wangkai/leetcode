42db98bd264db7729f0b3863f088a2d8372dc617
Kai Wang <charles.wangkai@gmail.com>
2021-04-26 09:48:08 +0800

    League Statistics

---
+++ league-statistics.sql
@@ -0,0 +1,26 @@
+SELECT
+  t.team_name,
+  COUNT(*) AS matches_played,
+  SUM(IF(m.goal_for > m.goal_against, 3, IF(m.goal_for = m.goal_against, 1, 0))) AS points,
+  SUM(m.goal_for) AS goal_for,
+  SUM(m.goal_against) AS goal_against,
+  SUM(m.goal_for) - SUM(m.goal_against) AS goal_diff
+FROM (
+  SELECT
+    home_team_id AS team_id,
+    home_team_goals AS goal_for,
+    away_team_goals AS goal_against
+  FROM Matches
+
+  UNION ALL
+
+  SELECT
+    away_team_id,
+    away_team_goals,
+    home_team_goals
+  FROM Matches
+) m
+JOIN Teams t
+ON t.team_id = m.team_id
+GROUP BY t.team_id, t.team_name
+ORDER BY 3 DESC, 6 DESC, 1
