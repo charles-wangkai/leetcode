15e9a7d6cc2715c10bc365edb92398bd44fc83ba
Kai Wang <charles.wangkai@gmail.com>
2021-04-28 15:20:10 +0800

    Unique Paths II

--- unique-paths-ii/Solution.java
+++ unique-paths-ii/Solution.java
@@ -1,17 +1,19 @@
-public class Solution {
-	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		int row = obstacleGrid.length;
-		int col = obstacleGrid[0].length;
-		int[][] pathNums = new int[row][col];
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (obstacleGrid[i][j] == 0) {
-					pathNums[i][j] = (i == 0 && j == 0) ? 1 : ((i == 0 ? 0
-							: pathNums[i - 1][j]) + (j == 0 ? 0
-							: pathNums[i][j - 1]));
-				}
-			}
-		}
-		return pathNums[row - 1][col - 1];
-	}
+class Solution {
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int row = obstacleGrid.length;
+    int col = obstacleGrid[0].length;
+    int[][] pathNums = new int[row][col];
+    for (int r = 0; r < row; ++r) {
+      for (int c = 0; c < col; ++c) {
+        if (obstacleGrid[r][c] == 0) {
+          pathNums[r][c] =
+              (r == 0 && c == 0)
+                  ? 1
+                  : ((r == 0 ? 0 : pathNums[r - 1][c]) + (c == 0 ? 0 : pathNums[r][c - 1]));
+        }
+      }
+    }
+
+    return pathNums[row - 1][col - 1];
+  }
 }
