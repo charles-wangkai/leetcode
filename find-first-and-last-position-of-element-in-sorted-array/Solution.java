9d98d160c26f602d17f3767a110d12d3c82d1d2a
Kai Wang <charles.wangkai@gmail.com>
2021-04-29 17:42:25 +0800

    Find First and Last Position of Element in Sorted Array

--- find-first-and-last-position-of-element-in-sorted-array/Solution.java
+++ find-first-and-last-position-of-element-in-sorted-array/Solution.java
@@ -1,32 +1,30 @@
-public class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int leftIndex = search(nums, target, true);
-        if (leftIndex < 0) {
-            return new int[] { -1, -1 };
+class Solution {
+  public int[] searchRange(int[] nums, int target) {
+    return new int[] {search(nums, target, true), search(nums, target, false)};
+  }
+
+  int search(int[] nums, int target, boolean leftOrRight) {
+    int result = -1;
+    int lower = 0;
+    int upper = nums.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+
+      if (nums[middle] > target) {
+        upper = middle - 1;
+      } else if (nums[middle] < target) {
+        lower = middle + 1;
+      } else {
+        result = middle;
+
+        if (leftOrRight) {
+          upper = middle - 1;
+        } else {
+          lower = middle + 1;
         }
-        int rightIndex = search(nums, target, false);
-        return new int[] { leftIndex, rightIndex };
+      }
     }
 
-    int search(int[] nums, int target, boolean leftOrRight) {
-        int lower = 0;
-        int upper = nums.length - 1;
-        int index = -1;
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
-            if (nums[middle] > target) {
-                upper = middle - 1;
-            } else if (nums[middle] < target) {
-                lower = middle + 1;
-            } else {
-                index = middle;
-                if (leftOrRight) {
-                    upper = middle - 1;
-                } else {
-                    lower = middle + 1;
-                }
-            }
-        }
-        return index;
-    }
+    return result;
+  }
 }
