90f6cbd2c28a32bc4d53fc70c27f2903a42fb672
Kai Wang <charles.wangkai@gmail.com>
2021-04-30 15:09:11 +0800

    Powerful Integers

--- powerful-integers/Solution.java
+++ powerful-integers/Solution.java
@@ -2,27 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public List<Integer> powerfulIntegers(int x, int y, int bound) {
-		List<Integer> xPowers = buildPowers(x, bound);
-		List<Integer> yPowers = buildPowers(y, bound);
+class Solution {
+  public List<Integer> powerfulIntegers(int x, int y, int bound) {
+    List<Integer> xPowers = buildPowers(x, bound);
+    List<Integer> yPowers = buildPowers(y, bound);
 
-		return xPowers.stream().flatMap(xPower -> yPowers.stream().map(yPower -> xPower + yPower))
-				.filter(n -> n <= bound).distinct().collect(Collectors.toList());
-	}
+    return xPowers.stream()
+        .flatMap(xPower -> yPowers.stream().map(yPower -> xPower + yPower))
+        .filter(n -> n <= bound)
+        .distinct()
+        .collect(Collectors.toList());
+  }
 
-	List<Integer> buildPowers(int base, int bound) {
-		List<Integer> powers = new ArrayList<>();
-		int power = 1;
-		while (power < bound) {
-			powers.add(power);
+  List<Integer> buildPowers(int base, int bound) {
+    List<Integer> powers = new ArrayList<>();
+    int power = 1;
+    while (power < bound) {
+      powers.add(power);
 
-			if (base == 1) {
-				break;
-			}
+      if (base == 1) {
+        break;
+      }
 
-			power *= base;
-		}
-		return powers;
-	}
+      power *= base;
+    }
+
+    return powers;
+  }
 }
