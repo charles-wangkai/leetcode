e70e7e5589722e813a37ea16280775e60ac9a64f
Kai Wang <charles.wangkai@gmail.com>
2021-05-01 17:29:08 +0800

    Prefix and Suffix Search

--- prefix-and-suffix-search/WordFilter.java
+++ prefix-and-suffix-search/WordFilter.java
@@ -1,30 +1,30 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class WordFilter {
-	Map<String, Integer> key2index = new HashMap<String, Integer>();
+class WordFilter {
+  Map<String, Integer> keyToIndex = new HashMap<>();
 
-	public WordFilter(String[] words) {
-		for (int i = 0; i < words.length; i++) {
-			String word = words[i];
-			for (int prefixLength = 0; prefixLength <= word.length(); prefixLength++) {
-				for (int suffixLength = 0; suffixLength <= word.length(); suffixLength++) {
-					key2index.put(
-							generateKey(word.substring(0, prefixLength), word.substring(word.length() - suffixLength)),
-							i);
-				}
-			}
-		}
-	}
+  public WordFilter(String[] words) {
+    for (int i = 0; i < words.length; ++i) {
+      for (int prefixLength = 0; prefixLength <= words[i].length(); ++prefixLength) {
+        for (int suffixLength = 0; suffixLength <= words[i].length(); ++suffixLength) {
+          keyToIndex.put(
+              generateKey(
+                  words[i].substring(0, prefixLength),
+                  words[i].substring(words[i].length() - suffixLength)),
+              i);
+        }
+      }
+    }
+  }
 
-	public int f(String prefix, String suffix) {
-		String key = generateKey(prefix, suffix);
-		return key2index.containsKey(key) ? key2index.get(key) : -1;
-	}
+  public int f(String prefix, String suffix) {
+    return keyToIndex.getOrDefault(generateKey(prefix, suffix), -1);
+  }
 
-	String generateKey(String prefix, String suffix) {
-		return prefix + "|" + suffix;
-	}
+  String generateKey(String prefix, String suffix) {
+    return String.format("%s|%s", prefix, suffix);
+  }
 }
 
 // Your WordFilter object will be instantiated and called as such:
