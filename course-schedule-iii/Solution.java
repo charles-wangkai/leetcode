f77ddcbb2013c6165ba5629557afb4aa687b7f44
Kai Wang <charles.wangkai@gmail.com>
2021-05-02 17:18:59 +0800

    Course Schedule III

--- course-schedule-iii/Solution.java
+++ course-schedule-iii/Solution.java
@@ -1,20 +1,22 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Solution {
-	public int scheduleCourse(int[][] courses) {
-		Arrays.sort(courses, (course1, course2) -> course1[1] - course2[1]);
+class Solution {
+  public int scheduleCourse(int[][] courses) {
+    Arrays.sort(courses, Comparator.comparing(course -> course[1]));
 
-		PriorityQueue<Integer> times = new PriorityQueue<Integer>((time1, time2) -> time2 - time1);
-		int start = 0;
-		for (int[] course : courses) {
-			start += course[0];
-			times.offer(course[0]);
+    PriorityQueue<Integer> times = new PriorityQueue<>(Comparator.reverseOrder());
+    int start = 0;
+    for (int[] course : courses) {
+      start += course[0];
+      times.offer(course[0]);
 
-			while (start > course[1]) {
-				start -= times.poll();
-			}
-		}
-		return times.size();
-	}
+      while (start > course[1]) {
+        start -= times.poll();
+      }
+    }
+
+    return times.size();
+  }
 }
