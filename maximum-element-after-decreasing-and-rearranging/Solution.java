7eb0047359ccc891f39a90e7a09e7ddfaaa84dc7
Kai Wang <charles.wangkai@gmail.com>
2021-05-02 08:09:56 +0800

    Maximum Element After Decreasing and Rearranging

---
+++ maximum-element-after-decreasing-and-rearranging/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+  public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+    int[] sorted = Arrays.stream(arr).boxed().sorted().mapToInt(x -> x).toArray();
+
+    int result = 0;
+    for (int i = 0; i < sorted.length; ++i) {
+      result = Math.min(result + 1, sorted[i]);
+    }
+
+    return result;
+  }
+}
