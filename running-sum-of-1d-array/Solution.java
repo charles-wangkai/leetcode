c6330575d7f9216da1587f2af3986413ca764e1f
Kai Wang <charles.wangkai@gmail.com>
2021-05-03 19:09:28 +0800

    Running Sum of 1d Array

--- running-sum-of-1d-array/Solution.java
+++ running-sum-of-1d-array/Solution.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int[] runningSum(int[] nums) {
-        int sum = 0;
-        int[] result = new int[nums.length];
-        for (int i = 0; i < result.length; ++i) {
-            sum += nums[i];
-            result[i] = sum;
-        }
-
-        return result;
+  public int[] runningSum(int[] nums) {
+    int[] result = new int[nums.length];
+    for (int i = 0; i < result.length; ++i) {
+      result[i] = ((i == 0) ? 0 : result[i - 1]) + nums[i];
     }
+
+    return result;
+  }
 }
