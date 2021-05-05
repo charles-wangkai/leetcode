f837b4f947650c2abba3e77c13d66884b7998b3a
Kai Wang <charles.wangkai@gmail.com>
2021-05-05 16:03:17 +0800

    Jump Game II

--- jump-game-ii/Solution.java
+++ jump-game-ii/Solution.java
@@ -1,15 +1,16 @@
-public class Solution {
-	public int jump(int[] A) {
-		int step = 0;
-		int maxIndex = 0;
-		int nextMaxIndex = -1;
-		for (int i = 0; maxIndex < A.length - 1; i++) {
-			if (i == maxIndex + 1) {
-				maxIndex = nextMaxIndex;
-				step++;
-			}
-			nextMaxIndex = Math.max(nextMaxIndex, i + A[i]);
-		}
-		return step;
-	}
+class Solution {
+  public int jump(int[] nums) {
+    int step = 0;
+    int maxIndex = 0;
+    int nextMaxIndex = -1;
+    for (int i = 0; maxIndex < nums.length - 1; ++i) {
+      if (i == maxIndex + 1) {
+        maxIndex = nextMaxIndex;
+        ++step;
+      }
+      nextMaxIndex = Math.max(nextMaxIndex, i + nums[i]);
+    }
+
+    return step;
+  }
 }
