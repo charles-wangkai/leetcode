1464a5c271edf7f580669fe5da40c913c70057f6
Kai Wang <charles.wangkai@gmail.com>
2021-05-06 15:30:40 +0800

    Convert Sorted List to Binary Search Tree

--- convert-sorted-list-to-binary-search-tree/Solution.java
+++ convert-sorted-list-to-binary-search-tree/Solution.java
@@ -1,50 +1,67 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode sortedListToBST(ListNode head) {
-		return sortedListToBST(head, getLength(head));
-	}
+class Solution {
+  public TreeNode sortedListToBST(ListNode head) {
+    return sortedListToBST(head, computeLength(head));
+  }
 
-	TreeNode sortedListToBST(ListNode head, int length) {
-		if (length == 0) {
-			return null;
-		}
-		int leftLength = length / 2;
-		ListNode middle = head;
-		for (int i = 0; i < leftLength; i++) {
-			middle = middle.next;
-		}
-		TreeNode root = new TreeNode(middle.val);
-		root.left = sortedListToBST(head, leftLength);
-		root.right = sortedListToBST(middle.next, length - leftLength - 1);
-		return root;
-	}
+  TreeNode sortedListToBST(ListNode head, int length) {
+    if (length == 0) {
+      return null;
+    }
 
-	int getLength(ListNode head) {
-		int length = 0;
-		for (ListNode p = head; p != null; p = p.next) {
-			length++;
-		}
-		return length;
-	}
+    int leftLength = length / 2;
+    ListNode middle = head;
+    for (int i = 0; i < leftLength; ++i) {
+      middle = middle.next;
+    }
+
+    return new TreeNode(
+        middle.val,
+        sortedListToBST(head, leftLength),
+        sortedListToBST(middle.next, length - leftLength - 1));
+  }
+
+  int computeLength(ListNode head) {
+    int length = 0;
+    for (ListNode p = head; p != null; p = p.next) {
+      ++length;
+    }
+
+    return length;
+  }
 }
