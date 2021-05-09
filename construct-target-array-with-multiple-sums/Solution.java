235512c8e9de104215864ec62eeaf3861fc492ef
Kai Wang <charles.wangkai@gmail.com>
2021-05-09 19:00:25 +0800

    Construct Target Array With Multiple Sums

--- construct-target-array-with-multiple-sums/Solution.java
+++ construct-target-array-with-multiple-sums/Solution.java
@@ -1,35 +1,40 @@
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Solution {
-    public boolean isPossible(int[] target) {
-        int oneCount = 0;
-        long pqSum = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int x : target) {
-            if (x == 1) {
-                ++oneCount;
-            } else {
-                pq.offer(x);
-                pqSum += x;
-            }
-        }
-
-        while (!pq.isEmpty()) {
-            int max = pq.poll();
-            pqSum -= max;
-
-            long prev = max - pqSum - oneCount;
-            if (prev < 1) {
-                return false;
-            } else if (prev == 1) {
-                ++oneCount;
-            } else {
-                pq.offer((int) prev);
-                pqSum += prev;
-            }
-        }
-
-        return true;
+class Solution {
+  public boolean isPossible(int[] target) {
+    int oneCount = 0;
+    long pqSum = 0;
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+    for (int x : target) {
+      if (x == 1) {
+        ++oneCount;
+      } else {
+        pq.offer(x);
+        pqSum += x;
+      }
     }
+
+    while (!pq.isEmpty()) {
+      int max = pq.poll();
+      pqSum -= max;
+
+      if (max <= pqSum + oneCount || pqSum + oneCount == 0) {
+        return false;
+      }
+
+      long prev = max % (pqSum + oneCount);
+      if (prev == 0 && pqSum + oneCount != 1) {
+        return false;
+      }
+      if (prev == 1 || pqSum + oneCount == 1) {
+        ++oneCount;
+      } else {
+        pq.offer((int) prev);
+        pqSum += prev;
+      }
+    }
+
+    return true;
+  }
 }
