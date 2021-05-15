0d4a31936726ec1174ec2eceed0d2c69c3af94c8
Kai Wang <charles.wangkai@gmail.com>
2021-05-15 17:21:02 +0800

    Valid Number

--- valid-number/Solution.java
+++ valid-number/Solution.java
@@ -1,52 +1,40 @@
-public class Solution {
-	public boolean isNumber(String s) {
-		s = s.trim();
+class Solution {
+  public boolean isNumber(String s) {
+    int eIndex = s.toLowerCase().indexOf('e');
+    if (eIndex == -1) {
+      return isDecimalOrInteger(s);
+    } else {
+      return isDecimalOrInteger(s.substring(0, eIndex)) && isInteger(s.substring(eIndex + 1));
+    }
+  }
 
-		int indexE = s.indexOf('e');
-		if (indexE >= 0) {
-			return isDecimal(s.substring(0, indexE))
-					&& isInteger(s.substring(indexE + 1));
-		} else {
-			return isDecimal(s);
-		}
-	}
+  boolean isDecimalOrInteger(String s) {
+    return isDecimal(s) || isInteger(s);
+  }
 
-	boolean isDecimal(String s) {
-		s = removeSign(s);
+  boolean isDecimal(String s) {
+    s = removeSign(s);
+    int pointIndex = s.indexOf('.');
 
-		int indexPoint = s.indexOf('.');
-		if (indexPoint >= 0) {
-			if (s.length() == 1) {
-				return false;
-			}
-			return (indexPoint == 0 || isInteger(s.substring(0, indexPoint)))
-					&& (indexPoint == s.length() - 1 || isIntegerWithoutSign(s
-							.substring(indexPoint + 1)));
-		} else {
-			return isInteger(s);
-		}
-	}
+    return pointIndex != -1
+        && s.length() != 1
+        && (pointIndex == 0 || isIntegerWithoutSign(s.substring(0, pointIndex)))
+        && (pointIndex == s.length() - 1 || isIntegerWithoutSign(s.substring(pointIndex + 1)));
+  }
 
-	boolean isInteger(String s) {
-		return isIntegerWithoutSign(removeSign(s));
-	}
+  boolean isInteger(String s) {
+    return isIntegerWithoutSign(removeSign(s));
+  }
 
-	String removeSign(String s) {
-		if (s.startsWith("+") || s.startsWith("-")) {
-			s = s.substring(1);
-		}
-		return s;
-	}
+  String removeSign(String s) {
+    if (s.startsWith("+") || s.startsWith("-")) {
+      s = s.substring(1);
+    }
 
-	boolean isIntegerWithoutSign(String s) {
-		if (s.isEmpty()) {
-			return false;
-		}
-		for (int i = 0; i < s.length(); i++) {
-			if (!Character.isDigit(s.charAt(i))) {
-				return false;
-			}
-		}
-		return true;
-	}
+    return s;
+  }
+
+  boolean isIntegerWithoutSign(String s) {
+    return !s.isEmpty() && s.chars().allMatch(Character::isDigit);
+  }
 }
