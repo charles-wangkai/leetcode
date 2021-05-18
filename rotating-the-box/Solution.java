4ed7d743632c2939c9d930c593e405ca73ea7900
Kai Wang <charles.wangkai@gmail.com>
2021-05-18 20:24:22 +0800

    Rotating the Box

---
+++ rotating-the-box/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+  public char[][] rotateTheBox(char[][] box) {
+    int m = box.length;
+    int n = box[0].length;
+
+    char[][] rotated = new char[n][m];
+    for (int r = 0; r < rotated.length; ++r) {
+      Arrays.fill(rotated[r], '.');
+    }
+    for (int r = 0; r < m; ++r) {
+      int lastC = n - 1;
+      for (int c = n - 1; c >= 0; --c) {
+        if (box[r][c] == '#') {
+          while (box[r][lastC] == '*') {
+            --lastC;
+          }
+          rotated[lastC][m - 1 - r] = '#';
+          --lastC;
+        } else if (box[r][c] == '*') {
+          rotated[c][m - 1 - r] = '*';
+          lastC = c;
+        }
+      }
+    }
+
+    return rotated;
+  }
+}
