79bb9a26d9b84d9ad18fec9b3f28504a4654ff91
Kai Wang <charles.wangkai@gmail.com>
2021-05-18 19:17:34 +0800

    Find Duplicate File in System

--- find-duplicate-file-in-system/Solution.java
+++ find-duplicate-file-in-system/Solution.java
@@ -4,24 +4,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public List<List<String>> findDuplicate(String[] paths) {
-		Map<String, List<String>> content2paths = new HashMap<String, List<String>>();
-		for (String path : paths) {
-			String[] fields = path.split(" ");
-			String directory = fields[0];
-			for (int i = 1; i < fields.length; i++) {
-				int startIndex = fields[i].indexOf('(');
-				String fileName = fields[i].substring(0, startIndex);
-				String content = fields[i].substring(startIndex + 1, fields[i].length() - 1);
+class Solution {
+  public List<List<String>> findDuplicate(String[] paths) {
+    Map<String, List<String>> contentToPaths = new HashMap<>();
+    for (String path : paths) {
+      String[] fields = path.split(" ");
+      String directory = fields[0];
+      for (int i = 1; i < fields.length; ++i) {
+        int startIndex = fields[i].indexOf('(');
+        String fileName = fields[i].substring(0, startIndex);
+        String content = fields[i].substring(startIndex + 1, fields[i].length() - 1);
 
-				if (!content2paths.containsKey(content)) {
-					content2paths.put(content, new ArrayList<String>());
-				}
-				content2paths.get(content).add(directory + "/" + fileName);
-			}
-		}
+        if (!contentToPaths.containsKey(content)) {
+          contentToPaths.put(content, new ArrayList<>());
+        }
+        contentToPaths.get(content).add(String.format("%s/%s", directory, fileName));
+      }
+    }
 
-		return content2paths.values().stream().filter(ps -> ps.size() >= 2).collect(Collectors.toList());
-	}
+    return contentToPaths.values().stream()
+        .filter(ps -> ps.size() >= 2)
+        .collect(Collectors.toList());
+  }
 }
