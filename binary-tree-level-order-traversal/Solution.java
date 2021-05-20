85712a4c9cba21974e21f8cac2271281e090fe4b
Kai Wang <charles.wangkai@gmail.com>
2021-05-20 18:34:09 +0800

    Binary Tree Level Order Traversal

--- binary-tree-level-order-traversal/Solution.java
+++ binary-tree-level-order-traversal/Solution.java
@@ -1,33 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<List<Integer>> levelOrder(TreeNode root) {
-		List<List<Integer>> levels = new ArrayList<List<Integer>>();
-		search(levels, root, 0);
-		return levels;
-	}
+class Solution {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> levels = new ArrayList<>();
+    search(levels, root, 0);
 
-	void search(List<List<Integer>> levels, TreeNode node, int depth) {
-		if (node == null) {
-			return;
-		}
-		if (depth == levels.size()) {
-			levels.add(new ArrayList<Integer>());
-		}
-		levels.get(depth).add(node.val);
-		search(levels, node.left, depth + 1);
-		search(levels, node.right, depth + 1);
-	}
+    return levels;
+  }
+
+  void search(List<List<Integer>> levels, TreeNode node, int depth) {
+    if (node == null) {
+      return;
+    }
+
+    if (depth == levels.size()) {
+      levels.add(new ArrayList<>());
+    }
+    levels.get(depth).add(node.val);
+
+    search(levels, node.left, depth + 1);
+    search(levels, node.right, depth + 1);
+  }
 }
