da11e4b3fa16995089a15f6c01725877ffe3a26f
Kai Wang <charles.wangkai@gmail.com>
2021-05-22 09:48:54 +0800

    Find and Replace Pattern

--- find-and-replace-pattern/Solution.java
+++ find-and-replace-pattern/Solution.java
@@ -4,23 +4,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public List<String> findAndReplacePattern(String[] words, String pattern) {
-		String patternSign = computeSign(pattern);
+class Solution {
+  public List<String> findAndReplacePattern(String[] words, String pattern) {
+    String patternKey = computeKey(pattern);
 
-		return Arrays.stream(words).filter(word -> computeSign(word).equals(patternSign)).collect(Collectors.toList());
-	}
+    return Arrays.stream(words)
+        .filter(word -> computeKey(word).equals(patternKey))
+        .collect(Collectors.toList());
+  }
 
-	String computeSign(String s) {
-		StringBuilder sign = new StringBuilder();
-		Map<Character, Character> letterToReplacement = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			if (!letterToReplacement.containsKey(letter)) {
-				letterToReplacement.put(letter, (char) ('a' + letterToReplacement.size()));
-			}
+  String computeKey(String s) {
+    StringBuilder key = new StringBuilder();
+    Map<Character, Character> letterToReplacement = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      if (!letterToReplacement.containsKey(letter)) {
+        letterToReplacement.put(letter, (char) ('a' + letterToReplacement.size()));
+      }
 
-			sign.append(letterToReplacement.get(letter));
-		}
-		return sign.toString();
-	}
+      key.append(letterToReplacement.get(letter));
+    }
+
+    return key.toString();
+  }
 }
