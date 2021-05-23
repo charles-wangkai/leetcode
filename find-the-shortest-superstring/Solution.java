1541e8a177ae4c34dff8434e8765d782a61ffadb
Kai Wang <charles.wangkai@gmail.com>
2021-05-23 20:55:22 +0800

    Find the Shortest Superstring

--- find-the-shortest-superstring/Solution.java
+++ find-the-shortest-superstring/Solution.java
@@ -1,79 +1,81 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public String shortestSuperstring(String[] A) {
-		int n = A.length;
+class Solution {
+  public String shortestSuperstring(String[] words) {
+    int n = words.length;
 
-		int[][] graph = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				graph[i][j] = computeOverlap(A[i], A[j]);
-				graph[j][i] = computeOverlap(A[j], A[i]);
-			}
-		}
+    int[][] graph = new int[n][n];
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        graph[i][j] = computeOverlap(words[i], words[j]);
+        graph[j][i] = computeOverlap(words[j], words[i]);
+      }
+    }
 
-		int[][] overlaps = new int[1 << n][n];
-		int[][] paths = new int[1 << n][n];
-		int last = -1;
-		int maxOverlap = -1;
-		for (int i = 1; i < (1 << n); i++) {
-			Arrays.fill(overlaps[i], -1);
+    int[][] overlaps = new int[1 << n][n];
+    int[][] paths = new int[1 << n][n];
+    for (int i = 1; i < (1 << n); ++i) {
+      Arrays.fill(overlaps[i], -1);
 
-			for (int j = 0; j < n; j++) {
-				if ((i & (1 << j)) != 0) {
-					int prev = i - (1 << j);
+      for (int j = 0; j < n; ++j) {
+        if ((i & (1 << j)) != 0) {
+          int prev = i - (1 << j);
 
-					if (prev == 0) {
-						overlaps[i][j] = 0;
-					} else {
-						for (int k = 0; k < n; k++) {
-							if (overlaps[prev][k] != -1 && overlaps[prev][k] + graph[k][j] > overlaps[i][j]) {
-								overlaps[i][j] = overlaps[prev][k] + graph[k][j];
-								paths[i][j] = k;
-							}
-						}
-					}
-				}
+          if (prev == 0) {
+            overlaps[i][j] = 0;
+          } else {
+            for (int k = 0; k < n; ++k) {
+              if (overlaps[prev][k] != -1 && overlaps[prev][k] + graph[k][j] > overlaps[i][j]) {
+                overlaps[i][j] = overlaps[prev][k] + graph[k][j];
+                paths[i][j] = k;
+              }
+            }
+          }
+        }
+      }
+    }
 
-				if (i == (1 << n) - 1 && overlaps[i][j] > maxOverlap) {
-					maxOverlap = overlaps[i][j];
-					last = j;
-				}
-			}
-		}
+    int last =
+        IntStream.range(0, n)
+            .boxed()
+            .max(Comparator.comparing(j -> overlaps[(1 << n) - 1][j]))
+            .get();
 
-		int cur = (1 << n) - 1;
-		Stack<Integer> stack = new Stack<>();
-		while (cur != 0) {
-			stack.push(last);
+    int cur = (1 << n) - 1;
+    Stack<Integer> stack = new Stack<>();
+    while (cur != 0) {
+      stack.push(last);
 
-			int nextLast = paths[cur][last];
-			cur -= (1 << last);
-			last = nextLast;
-		}
+      int nextLast = paths[cur][last];
+      cur -= 1 << last;
+      last = nextLast;
+    }
 
-		StringBuilder result = new StringBuilder();
-		int prev = -1;
-		while (!stack.empty()) {
-			int current = stack.pop();
+    StringBuilder result = new StringBuilder();
+    int prev = -1;
+    while (!stack.empty()) {
+      int current = stack.pop();
 
-			if (prev == -1) {
-				result.append(A[current]);
-			} else {
-				result.append(A[current].substring(graph[prev][current]));
-			}
+      if (prev == -1) {
+        result.append(words[current]);
+      } else {
+        result.append(words[current].substring(graph[prev][current]));
+      }
 
-			prev = current;
-		}
-		return result.toString();
-	}
+      prev = current;
+    }
 
-	static int computeOverlap(String a, String b) {
-		for (int beginIndex = 1;; beginIndex++) {
-			if (b.startsWith(a.substring(beginIndex))) {
-				return a.length() - beginIndex;
-			}
-		}
-	}
+    return result.toString();
+  }
+
+  static int computeOverlap(String a, String b) {
+    for (int beginIndex = 1; ; ++beginIndex) {
+      if (b.startsWith(a.substring(beginIndex))) {
+        return a.length() - beginIndex;
+      }
+    }
+  }
 }
