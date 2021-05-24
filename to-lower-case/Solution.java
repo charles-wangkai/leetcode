211bc1a083a51e1053f01a2f1134f06dad2ff238
Kai Wang <charles.wangkai@gmail.com>
2021-05-24 16:30:57 +0800

    To Lower Case

--- to-lower-case/Solution.java
+++ to-lower-case/Solution.java
@@ -1,14 +1,5 @@
-public class Solution {
-	public String toLowerCase(String str) {
-		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if (ch >= 'A' && ch <= 'Z') {
-				result.append((char) (ch - 'A' + 'a'));
-			} else {
-				result.append(ch);
-			}
-		}
-		return result.toString();
-	}
+class Solution {
+  public String toLowerCase(String s) {
+    return s.toLowerCase();
+  }
 }
