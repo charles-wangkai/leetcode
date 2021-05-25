13e608fcae8b2fddcaae2f2f088768e04713cd23
Kai Wang <charles.wangkai@gmail.com>
2021-05-25 19:29:56 +0800

    Evaluate Reverse Polish Notation

--- evaluate-reverse-polish-notation/Solution.java
+++ evaluate-reverse-polish-notation/Solution.java
@@ -1,28 +1,28 @@
 import java.util.Stack;
 
-public class Solution {
-	public int evalRPN(String[] tokens) {
-		Stack<Integer> stack = new Stack<Integer>();
-		for (String token : tokens) {
-			if (token.equals("+") || token.equals("-") || token.equals("*")
-					|| token.equals("/")) {
-				int operand2 = stack.pop();
-				int operand1 = stack.pop();
-				int result;
-				if (token.equals("+")) {
-					result = operand1 + operand2;
-				} else if (token.equals("-")) {
-					result = operand1 - operand2;
-				} else if (token.equals("*")) {
-					result = operand1 * operand2;
-				} else {
-					result = operand1 / operand2;
-				}
-				stack.push(result);
-			} else {
-				stack.push(Integer.parseInt(token));
-			}
-		}
-		return stack.peek();
-	}
+class Solution {
+  public int evalRPN(String[] tokens) {
+    Stack<Integer> stack = new Stack<>();
+    for (String token : tokens) {
+      if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+        int operand2 = stack.pop();
+        int operand1 = stack.pop();
+        int result;
+        if (token.equals("+")) {
+          result = operand1 + operand2;
+        } else if (token.equals("-")) {
+          result = operand1 - operand2;
+        } else if (token.equals("*")) {
+          result = operand1 * operand2;
+        } else {
+          result = operand1 / operand2;
+        }
+        stack.push(result);
+      } else {
+        stack.push(Integer.parseInt(token));
+      }
+    }
+
+    return stack.peek();
+  }
 }
