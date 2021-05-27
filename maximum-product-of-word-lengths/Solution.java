d4b9287ef8ad12a41666d5c83e0aade5ea00c5b9
Kai Wang <charles.wangkai@gmail.com>
2021-05-27 19:48:03 +0800

    Maximum Product of Word Lengths

--- maximum-product-of-word-lengths/Solution.java
+++ maximum-product-of-word-lengths/Solution.java
@@ -2,23 +2,25 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public int maxProduct(String[] words) {
-		Map<Integer, Integer> key2length = Arrays.stream(words)
-				.collect(Collectors.toMap(this::generateKey, String::length, Math::max));
+class Solution {
+  public int maxProduct(String[] words) {
+    Map<Integer, Integer> keyToLength =
+        Arrays.stream(words)
+            .collect(Collectors.toMap(this::generateKey, String::length, Math::max));
 
-		int result = 0;
-		for (int key1 : key2length.keySet()) {
-			for (int key2 : key2length.keySet()) {
-				if ((key1 & key2) == 0) {
-					result = Math.max(result, key2length.get(key1) * key2length.get(key2));
-				}
-			}
-		}
-		return result;
-	}
+    int result = 0;
+    for (int key1 : keyToLength.keySet()) {
+      for (int key2 : keyToLength.keySet()) {
+        if ((key1 & key2) == 0) {
+          result = Math.max(result, keyToLength.get(key1) * keyToLength.get(key2));
+        }
+      }
+    }
 
-	int generateKey(String word) {
-		return word.chars().map(ch -> 1 << (ch - 'a')).reduce(0, (x, y) -> x | y);
-	}
+    return result;
+  }
+
+  int generateKey(String word) {
+    return word.chars().map(ch -> 1 << (ch - 'a')).reduce(0, (x, y) -> x | y);
+  }
 }
