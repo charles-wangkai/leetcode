4f4a1c00d8ea0b131d9b7cdfa9fecaa313e6d81f
Kai Wang <charles.wangkai@gmail.com>
2021-06-04 16:23:57 +0800

    Open the Lock

--- open-the-lock/Solution.java
+++ open-the-lock/Solution.java
@@ -1,49 +1,48 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Solution {
-	static final String INITIAL_WHEEL = "0000";
+class Solution {
+  static final String INITIAL_WHEEL = "0000";
+  static final int[] OFFSETS = {-1, 1};
 
-	public int openLock(String[] deadends, String target) {
-		if (target.contains(INITIAL_WHEEL)) {
-			return 0;
-		}
+  public int openLock(String[] deadends, String target) {
+    Set<String> deads = Arrays.stream(deadends).collect(Collectors.toSet());
+    if (deads.contains(INITIAL_WHEEL)) {
+      return -1;
+    }
 
-		Set<String> deads = new HashSet<String>(Arrays.asList(deadends));
-		if (deads.contains(INITIAL_WHEEL)) {
-			return -1;
-		}
+    Map<String, Integer> wheelToMove = new HashMap<>();
+    wheelToMove.put(INITIAL_WHEEL, 0);
+    Queue<String> queue = new ArrayDeque<>();
+    queue.offer(INITIAL_WHEEL);
+    while (!queue.isEmpty()) {
+      String wheel = queue.poll();
+      if (wheel.equals(target)) {
+        return wheelToMove.get(wheel);
+      }
 
-		Map<String, Integer> wheel2move = new HashMap<String, Integer>();
-		wheel2move.put(INITIAL_WHEEL, 0);
-		Queue<String> queue = new LinkedList<String>();
-		queue.offer(INITIAL_WHEEL);
-		while (!queue.isEmpty()) {
-			String wheel = queue.poll();
+      for (int i = 0; i < target.length(); ++i) {
+        for (int offset : OFFSETS) {
+          String nextWheel =
+              String.format(
+                  "%s%d%s",
+                  wheel.substring(0, i),
+                  (wheel.charAt(i) - '0' + offset + 10) % 10,
+                  wheel.substring(i + 1));
 
-			for (int i = 0; i < 4; i++) {
-				for (int offset : new int[] { -1, 1 }) {
-					String nextWheel = wheel.substring(0, i) + (wheel.charAt(i) - '0' + offset + 10) % 10
-							+ wheel.substring(i + 1);
+          if (!deads.contains(nextWheel) && !wheelToMove.containsKey(nextWheel)) {
+            wheelToMove.put(nextWheel, wheelToMove.get(wheel) + 1);
+            queue.offer(nextWheel);
+          }
+        }
+      }
+    }
 
-					if (deads.contains(nextWheel) || wheel2move.containsKey(nextWheel)) {
-						continue;
-					}
-
-					int nextMove = wheel2move.get(wheel) + 1;
-					if (nextWheel.equals(target)) {
-						return nextMove;
-					}
-					wheel2move.put(nextWheel, nextMove);
-					queue.offer(nextWheel);
-				}
-			}
-		}
-		return -1;
-	}
+    return -1;
+  }
 }
