6be1705dcc07a57e36d7db9cb6fd71027a2cc9f1
Kai Wang <charles.wangkai@gmail.com>
2021-06-05 22:04:23 +0800

    Maximum Performance of a Team

--- maximum-performance-of-a-team/Solution.java
+++ maximum-performance-of-a-team/Solution.java
@@ -1,28 +1,32 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.stream.IntStream;
 
 class Solution {
-    static final int MODULUS = 1_000_000_007;
+  static final int MODULUS = 1_000_000_007;
 
-    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[] sortedIndices = IntStream.range(0, n).boxed().sorted((i1, i2) -> {
-            return -Integer.compare(efficiency[i1], efficiency[i2]);
-        }).mapToInt(x -> x).toArray();
+  public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+    int[] sortedIndices =
+        IntStream.range(0, n)
+            .boxed()
+            .sorted(Comparator.comparing((Integer i) -> efficiency[i]).reversed())
+            .mapToInt(x -> x)
+            .toArray();
 
-        long result = Long.MIN_VALUE;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        long speedSum = 0;
-        for (int sortedIndex : sortedIndices) {
-            pq.offer(speed[sortedIndex]);
-            speedSum += speed[sortedIndex];
+    long result = Long.MIN_VALUE;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    long speedSum = 0;
+    for (int sortedIndex : sortedIndices) {
+      pq.offer(speed[sortedIndex]);
+      speedSum += speed[sortedIndex];
 
-            if (pq.size() == k + 1) {
-                speedSum -= pq.poll();
-            }
+      if (pq.size() == k + 1) {
+        speedSum -= pq.poll();
+      }
 
-            result = Math.max(result, speedSum * efficiency[sortedIndex]);
-        }
-
-        return (int) (result % MODULUS);
+      result = Math.max(result, speedSum * efficiency[sortedIndex]);
     }
+
+    return (int) (result % MODULUS);
+  }
 }
