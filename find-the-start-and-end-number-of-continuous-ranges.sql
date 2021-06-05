6c9634f95cd6e39842cee4d5c2b6ecde3ecf45ef
saravananmadhappa <saravananm2002@gmail.com>
2021-06-04 17:01:46 -0700

    Update find-the-start-and-end-number-of-continuous-ranges.sql

--- find-the-start-and-end-number-of-continuous-ranges.sql
+++ find-the-start-and-end-number-of-continuous-ranges.sql
@@ -23,3 +23,28 @@ AND NOT EXISTS (
     )
 )
 ORDER BY start_id
+------------------
+/*Another approach*/
+WITH log_num AS
+(
+  SELECT
+    s.log_id AS start_id,
+    e.log_id AS end_id
+  FROM Logs s
+  ,Logs e
+  WHERE s.log_id <= e.log_id
+  AND e.log_id - s.log_id + 1 = (
+    SELECT COUNT(1)
+    FROM cloud_s2_ext.logs
+    WHERE log_id BETWEEN s.log_id AND e.log_id
+  )
+)
+
+SELECT MIN(start_id) start_num,end_num
+FROM
+(
+SELECT start_id,end_id,MAX(end_id) over (partition by start_id) end_num FROM log_num 
+)
+GROUP BY 2
+order by 1,2
+;
