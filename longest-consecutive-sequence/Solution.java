89c4296f2c100b3eb5acea640c9d1d62b20aaef0
Kai Wang <charles.wangkai@gmail.com>
2021-06-06 16:09:22 +0800

    Longest Consecutive Sequence

--- longest-consecutive-sequence/Solution.java
+++ longest-consecutive-sequence/Solution.java
@@ -1,26 +1,27 @@
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Solution {
-	public int longestConsecutive(int[] num) {
-		Set<Integer> numbers = new HashSet<Integer>();
-		for (int number : num) {
-			numbers.add(number);
-		}
-		int maxLength = 0;
-		while (!numbers.isEmpty()) {
-			int middle = numbers.iterator().next();
-			int length = 0;
-			for (int i = middle; numbers.contains(i); i++) {
-				numbers.remove(i);
-				length++;
-			}
-			for (int i = middle - 1; numbers.contains(i); i--) {
-				numbers.remove(i);
-				length++;
-			}
-			maxLength = Math.max(maxLength, length);
-		}
-		return maxLength;
-	}
+class Solution {
+  public int longestConsecutive(int[] nums) {
+    Set<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+    int maxLength = 0;
+    while (!numbers.isEmpty()) {
+      int middle = numbers.iterator().next();
+      int length = 0;
+      for (int i = middle; numbers.contains(i); ++i) {
+        numbers.remove(i);
+        ++length;
+      }
+      for (int i = middle - 1; numbers.contains(i); --i) {
+        numbers.remove(i);
+        ++length;
+      }
+
+      maxLength = Math.max(maxLength, length);
+    }
+
+    return maxLength;
+  }
 }
