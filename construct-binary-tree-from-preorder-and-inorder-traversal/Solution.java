64d2083964af91f5cab19b1a4d69fe3dbdec13d4
Kai Wang <charles.wangkai@gmail.com>
2021-06-08 17:18:53 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

--- construct-binary-tree-from-preorder-and-inorder-traversal/Solution.java
+++ construct-binary-tree-from-preorder-and-inorder-traversal/Solution.java
@@ -1,43 +1,49 @@
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
-		return buildTree(preorder, 0, preorder.length - 1, inorder, 0,
-				inorder.length - 1);
-	}
+class Solution {
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+  }
 
-	TreeNode buildTree(int[] preorder, int beginPre, int endPre, int[] inorder,
-			int beginIn, int endIn) {
-		if (beginPre > endPre) {
-			return null;
-		}
-		int rootValue = preorder[beginPre];
-		int rootIn = findValue(inorder, beginIn, endIn, rootValue);
-		int leftLen = rootIn - beginIn;
-		int rightLen = endIn - rootIn;
-		TreeNode root = new TreeNode(rootValue);
-		root.left = buildTree(preorder, beginPre + 1, beginPre + leftLen,
-				inorder, beginIn, rootIn - 1);
-		root.right = buildTree(preorder, endPre - rightLen + 1, endPre,
-				inorder, rootIn + 1, endIn);
-		return root;
-	}
+  TreeNode buildTree(
+      int[] preorder, int beginPre, int endPre, int[] inorder, int beginIn, int endIn) {
+    if (beginPre > endPre) {
+      return null;
+    }
 
-	int findValue(int[] a, int begin, int end, int target) {
-		for (int i = begin; i <= end; i++) {
-			if (a[i] == target) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    int rootValue = preorder[beginPre];
+    int rootIn = findValue(inorder, beginIn, rootValue);
+    int leftLen = rootIn - beginIn;
+    int rightLen = endIn - rootIn;
+
+    return new TreeNode(
+        rootValue,
+        buildTree(preorder, beginPre + 1, beginPre + leftLen, inorder, beginIn, rootIn - 1),
+        buildTree(preorder, endPre - rightLen + 1, endPre, inorder, rootIn + 1, endIn));
+  }
+
+  int findValue(int[] a, int begin, int target) {
+    for (int i = begin; ; ++i) {
+      if (a[i] == target) {
+        return i;
+      }
+    }
+  }
 }
