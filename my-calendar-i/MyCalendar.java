53ff0f8e30a0e66f7ef23d460557d71734585661
Kai Wang <charles.wangkai@gmail.com>
2021-06-10 15:53:58 +0800

    My Calendar I

--- my-calendar-i/MyCalendar.java
+++ my-calendar-i/MyCalendar.java
@@ -1,36 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyCalendar {
-	List<Event> events = new ArrayList<Event>();
+class MyCalendar {
+  List<Event> events = new ArrayList<>();
 
-	public MyCalendar() {
-	}
+  public boolean book(int start, int end) {
+    Event event = new Event(start, end);
 
-	public boolean book(int start, int end) {
-		Event event = new Event(start, end);
+    if (events.stream().anyMatch(e -> isIntersect(e, event))) {
+      return false;
+    }
 
-		if (events.stream().anyMatch(e -> e.isIntersect(event))) {
-			return false;
-		}
+    events.add(event);
 
-		events.add(event);
-		return true;
-	}
+    return true;
+  }
+
+  boolean isIntersect(Event e1, Event e2) {
+    return !(e1.end <= e2.start || e1.start >= e2.end);
+  }
 }
 
 class Event {
-	int start;
-	int end;
+  int start;
+  int end;
 
-	Event(int start, int end) {
-		this.start = start;
-		this.end = end;
-	}
-
-	boolean isIntersect(Event other) {
-		return !(end <= other.start || start >= other.end);
-	}
+  Event(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
 }
 
 // Your MyCalendar object will be instantiated and called as such:
