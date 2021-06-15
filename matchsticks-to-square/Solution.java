33eeb829db0c7cd5db0e1c7b8cc595deaa97498e
Kai Wang <charles.wangkai@gmail.com>
2021-06-16 06:22:13 +0800

    Matchsticks to Square

--- matchsticks-to-square/Solution.java
+++ matchsticks-to-square/Solution.java
@@ -1,48 +1,47 @@
 import java.util.Arrays;
 
-public class Solution {
-	public boolean makesquare(int[] nums) {
-		Arrays.sort(nums);
+class Solution {
+  public boolean makesquare(int[] matchsticks) {
+    Arrays.sort(matchsticks);
 
-		long perimeter = Arrays.stream(nums).asLongStream().sum();
+    long perimeter = Arrays.stream(matchsticks).asLongStream().sum();
+    if (perimeter % 4 != 0) {
+      return false;
+    }
+    long size = perimeter / 4;
 
-		if (perimeter % 4 != 0) {
-			return false;
-		}
+    return search(matchsticks, size, new boolean[matchsticks.length], 0, 0, matchsticks.length - 1);
+  }
 
-		long size = perimeter / 4;
-		return search(nums, size, new boolean[nums.length], 0, 0, nums.length - 1);
-	}
+  boolean search(int[] matchsticks, long size, boolean[] used, int side, int length, int index) {
+    if (side == 3) {
+      return true;
+    }
+    if (index == -1) {
+      return false;
+    }
 
-	boolean search(int[] nums, long size, boolean[] used, int side, int length, int index) {
-		if (side == 3) {
-			return true;
-		}
-		if (index < 0) {
-			return false;
-		}
+    if (search(matchsticks, size, used, side, length, index - 1)) {
+      return true;
+    }
 
-		if (search(nums, size, used, side, length, index - 1)) {
-			return true;
-		}
+    if (!used[index] && length + matchsticks[index] <= size) {
+      int nextSide = side;
+      int nextLength = length + matchsticks[index];
+      int nextIndex = index - 1;
+      if (nextLength == size) {
+        ++nextSide;
+        nextLength = 0;
+        nextIndex = matchsticks.length - 1;
+      }
 
-		if (!used[index] && length + nums[index] <= size) {
-			int nextSide = side;
-			int nextLength = length + nums[index];
-			int nextIndex = index;
-			if (nextLength == size) {
-				nextSide++;
-				nextLength = 0;
-				nextIndex = nums.length - 1;
-			}
+      used[index] = true;
+      if (search(matchsticks, size, used, nextSide, nextLength, nextIndex)) {
+        return true;
+      }
+      used[index] = false;
+    }
 
-			used[index] = true;
-			if (search(nums, size, used, nextSide, nextLength, nextIndex)) {
-				return true;
-			}
-			used[index] = false;
-		}
-
-		return false;
-	}
+    return false;
+  }
 }
