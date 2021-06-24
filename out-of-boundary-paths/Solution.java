de1a2d1ae31f0814052ec75f4f99d803a4b90762
Kai Wang <charles.wangkai@gmail.com>
2021-06-24 17:17:11 +0800

    Out of Boundary Paths

--- out-of-boundary-paths/Solution.java
+++ out-of-boundary-paths/Solution.java
@@ -1,43 +1,43 @@
-public class Solution {
-	static final int MODULUS = 1000000007;
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int MODULUS = 1_000_000_007;
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int findPaths(int m, int n, int N, int i, int j) {
-		int result = 0;
+  public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+    int[][] pathNums = new int[m][n];
+    for (int c = 0; c < n; ++c) {
+      pathNums[0][c] = addMod(pathNums[0][c], 1);
+      pathNums[m - 1][c] = addMod(pathNums[m - 1][c], 1);
+    }
+    for (int r = 0; r < m; ++r) {
+      pathNums[r][0] = addMod(pathNums[r][0], 1);
+      pathNums[r][n - 1] = addMod(pathNums[r][n - 1], 1);
+    }
 
-		int[][] pathNums = new int[m][n];
-		for (int c = 0; c < n; c++) {
-			pathNums[0][c] = addMod(pathNums[0][c], 1);
-			pathNums[m - 1][c] = addMod(pathNums[m - 1][c], 1);
-		}
-		for (int r = 0; r < m; r++) {
-			pathNums[r][0] = addMod(pathNums[r][0], 1);
-			pathNums[r][n - 1] = addMod(pathNums[r][n - 1], 1);
-		}
+    int result = 0;
+    for (int i = 0; i < maxMove; ++i) {
+      result = addMod(result, pathNums[startRow][startColumn]);
 
-		for (int k = 0; k < N; k++) {
-			result = addMod(result, pathNums[i][j]);
+      int[][] nextPathNums = new int[m][n];
+      for (int r = 0; r < m; ++r) {
+        for (int c = 0; c < n; ++c) {
+          for (int j = 0; j < R_OFFSETS.length; ++j) {
+            int adjR = r + R_OFFSETS[j];
+            int adjC = c + C_OFFSETS[j];
+            if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n) {
+              nextPathNums[r][c] = addMod(nextPathNums[r][c], pathNums[adjR][adjC]);
+            }
+          }
+        }
+      }
 
-			int[][] nextPathNums = new int[m][n];
-			for (int r = 0; r < m; r++) {
-				for (int c = 0; c < n; c++) {
-					for (int p = 0; p < R_OFFSETS.length; p++) {
-						int adjR = r + R_OFFSETS[p];
-						int adjC = c + C_OFFSETS[p];
-						if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n) {
-							nextPathNums[r][c] = addMod(nextPathNums[r][c], pathNums[adjR][adjC]);
-						}
-					}
-				}
-			}
-			pathNums = nextPathNums;
-		}
+      pathNums = nextPathNums;
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	int addMod(int x, int y) {
-		return (x + y) % MODULUS;
-	}
+  int addMod(int x, int y) {
+    return (x + y) % MODULUS;
+  }
 }
