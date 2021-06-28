1fb3fd4a149d3ed6e6f0f0e8acfecc1e3aa70264
Kai Wang <charles.wangkai@gmail.com>
2021-06-28 11:16:18 +0800

    Remove All Occurrences of a Substring

---
+++ remove-all-occurrences-of-a-substring/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public String removeOccurrences(String s, String part) {
+    while (true) {
+      int index = s.indexOf(part);
+      if (index == -1) {
+        break;
+      }
+
+      s = String.format("%s%s", s.substring(0, index), s.substring(index + part.length()));
+    }
+
+    return s;
+  }
+}
