86b2316b39c1dbfe75d863d5ed14715493efeb77
Kai Wang <charles.wangkai@gmail.com>
2021-06-29 17:56:35 +0800

    Max Consecutive Ones III

--- max-consecutive-ones-iii/Solution.java
+++ max-consecutive-ones-iii/Solution.java
@@ -2,22 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int longestOnes(int[] A, int K) {
-		List<Integer> zeroIndices = new ArrayList<>();
-		zeroIndices.add(-1);
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] == 0) {
-				zeroIndices.add(i);
-			}
-		}
-		zeroIndices.add(A.length);
+class Solution {
+  public int longestOnes(int[] nums, int k) {
+    List<Integer> zeroIndices = new ArrayList<>();
+    zeroIndices.add(-1);
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] == 0) {
+        zeroIndices.add(i);
+      }
+    }
+    zeroIndices.add(nums.length);
 
-		if (zeroIndices.size() - 2 < K) {
-			return A.length;
-		}
+    if (zeroIndices.size() - 2 <= k) {
+      return nums.length;
+    }
 
-		return IntStream.range(0, zeroIndices.size() - K - 1)
-				.map(i -> zeroIndices.get(i + K + 1) - zeroIndices.get(i) - 1).max().getAsInt();
-	}
+    return IntStream.range(0, zeroIndices.size() - k - 1)
+        .map(i -> zeroIndices.get(i + k + 1) - zeroIndices.get(i) - 1)
+        .max()
+        .getAsInt();
+  }
 }
