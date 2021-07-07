6fa0d38267387828151fea23d6637d4b708cea7b
Kai Wang <charles.wangkai@gmail.com>
2021-07-07 12:45:22 +0800

    Reduce Array Size to The Half

--- reduce-array-size-to-the-half/Solution.java
+++ reduce-array-size-to-the-half/Solution.java
@@ -1,26 +1,24 @@
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public class Solution {
-	public int minSetSize(int[] arr) {
-		Map<Integer, Integer> valueToCount = new HashMap<>();
-		for (int value : arr) {
-			valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
-		}
+class Solution {
+  public int minSetSize(int[] arr) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int value : arr) {
+      valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
+    }
 
-		List<Integer> counts = valueToCount.values().stream().sorted(Collections.reverseOrder())
-				.collect(Collectors.toList());
+    int[] counts =
+        valueToCount.values().stream().sorted(Comparator.reverseOrder()).mapToInt(x -> x).toArray();
 
-		int sum = 0;
-		for (int i = 0;; ++i) {
-			sum += counts.get(i);
+    int sum = 0;
+    for (int i = 0; ; ++i) {
+      sum += counts[i];
 
-			if (sum * 2 >= arr.length) {
-				return i + 1;
-			}
-		}
-	}
+      if (sum * 2 >= arr.length) {
+        return i + 1;
+      }
+    }
+  }
 }
