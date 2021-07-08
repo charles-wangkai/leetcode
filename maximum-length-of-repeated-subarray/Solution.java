65ca13e8c2ae862fc95c48963695e3f648dd8cd8
Kai Wang <charles.wangkai@gmail.com>
2021-07-08 18:15:43 +0800

    Maximum Length of Repeated Subarray

--- maximum-length-of-repeated-subarray/Solution.java
+++ maximum-length-of-repeated-subarray/Solution.java
@@ -1,15 +1,19 @@
-public class Solution {
-	public int findLength(int[] A, int[] B) {
-		int result = 0;
-		int[][] maxLengths = new int[A.length + 1][B.length + 1];
-		for (int i = 0; i <= A.length; i++) {
-			for (int j = 0; j <= B.length; j++) {
-				if (i > 0 && j > 0 && A[i - 1] == B[j - 1]) {
-					maxLengths[i][j] = maxLengths[i - 1][j - 1] + 1;
-					result = Math.max(result, maxLengths[i][j]);
-				}
-			}
-		}
-		return result;
-	}
+class Solution {
+  public int findLength(int[] nums1, int[] nums2) {
+    int result = 0;
+    for (int beginIndex1 = 0; beginIndex1 < nums1.length; ++beginIndex1) {
+      for (int beginIndex2 = 0; beginIndex2 < nums2.length; ++beginIndex2) {
+        int length = 0;
+        for (int i = beginIndex1, j = beginIndex2;
+            i < nums1.length && j < nums2.length && nums1[i] == nums2[j];
+            ++i, ++j) {
+          ++length;
+        }
+
+        result = Math.max(result, length);
+      }
+    }
+
+    return result;
+  }
 }
