3bf35181ad453eab5823bcef22500858f3486af1
Kai Wang <charles.wangkai@gmail.com>
2021-07-12 14:13:23 +0800

    Find Median from Data Stream

--- find-median-from-data-stream/MedianFinder.java
+++ find-median-from-data-stream/MedianFinder.java
@@ -1,38 +1,36 @@
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class MedianFinder {
-	PriorityQueue<Integer> lowerHalf = new PriorityQueue<Integer>(Collections.reverseOrder());
-	PriorityQueue<Integer> upperHalf = new PriorityQueue<Integer>();
+class MedianFinder {
+  PriorityQueue<Integer> lowerHalf = new PriorityQueue<>(Comparator.reverseOrder());
+  PriorityQueue<Integer> upperHalf = new PriorityQueue<>();
 
-	// Adds a number into the data structure.
-	public void addNum(int num) {
-		if (lowerHalf.isEmpty() || num <= lowerHalf.peek()) {
-			lowerHalf.offer(num);
-		} else {
-			upperHalf.offer(num);
-		}
+  public void addNum(int num) {
+    if (lowerHalf.isEmpty() || num <= lowerHalf.peek()) {
+      lowerHalf.offer(num);
+    } else {
+      upperHalf.offer(num);
+    }
 
-		if (lowerHalf.size() >= upperHalf.size() + 2) {
-			upperHalf.offer(lowerHalf.poll());
-		} else if (upperHalf.size() >= lowerHalf.size() + 2) {
-			lowerHalf.offer(upperHalf.poll());
-		}
-	}
+    if (lowerHalf.size() >= upperHalf.size() + 2) {
+      upperHalf.offer(lowerHalf.poll());
+    } else if (upperHalf.size() >= lowerHalf.size() + 2) {
+      lowerHalf.offer(upperHalf.poll());
+    }
+  }
 
-	// Returns the median of current data stream
-	public double findMedian() {
-		if (lowerHalf.size() == upperHalf.size()) {
-			return ((double) lowerHalf.peek() + upperHalf.peek()) / 2;
-		} else if (lowerHalf.size() > upperHalf.size()) {
-			return lowerHalf.peek();
-		} else {
-			return upperHalf.peek();
-		}
-	}
+  public double findMedian() {
+    if (lowerHalf.size() > upperHalf.size()) {
+      return lowerHalf.peek();
+    } else if (lowerHalf.size() < upperHalf.size()) {
+      return upperHalf.peek();
+    }
+
+    return ((double) lowerHalf.peek() + upperHalf.peek()) / 2;
+  }
 }
 
 // Your MedianFinder object will be instantiated and called as such:
-// MedianFinder mf = new MedianFinder();
-// mf.addNum(1);
-// mf.findMedian();
+// MedianFinder obj = new MedianFinder();
+// obj.addNum(num);
+// double param_2 = obj.findMedian();
