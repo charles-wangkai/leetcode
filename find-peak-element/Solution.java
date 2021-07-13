ccedcf40ae35ced592cb3f94e016e1d419565ef8
Kai Wang <charles.wangkai@gmail.com>
2021-07-13 22:46:40 +0800

    Find Peak Element

--- find-peak-element/Solution.java
+++ find-peak-element/Solution.java
@@ -1,25 +1,27 @@
-public class Solution {
-	public int findPeakElement(int[] num) {
-		return findPeakElement(num, 0, num.length - 1);
-	}
+class Solution {
+  public int findPeakElement(int[] nums) {
+    return findPeakElement(nums, 0, nums.length - 1);
+  }
 
-	int findPeakElement(int[] num, int begin, int end) {
-		if (begin == end) {
-			return begin;
-		}
-		int middle = (begin + end) / 2;
-		if (isPeak(num, middle)) {
-			return middle;
-		}
-		if (num[middle + 1] > num[middle]) {
-			return findPeakElement(num, middle + 1, end);
-		} else {
-			return findPeakElement(num, begin, middle - 1);
-		}
-	}
+  int findPeakElement(int[] nums, int begin, int end) {
+    if (begin == end) {
+      return begin;
+    }
 
-	boolean isPeak(int[] num, int index) {
-		return (index == 0 || num[index] > num[index - 1])
-				&& (index == num.length - 1 || num[index] > num[index + 1]);
-	}
+    int middle = (begin + end) / 2;
+    if (isPeak(nums, middle)) {
+      return middle;
+    }
+
+    if (nums[middle + 1] > nums[middle]) {
+      return findPeakElement(nums, middle + 1, end);
+    } else {
+      return findPeakElement(nums, begin, middle - 1);
+    }
+  }
+
+  boolean isPeak(int[] nums, int index) {
+    return (index == 0 || nums[index] > nums[index - 1])
+        && (index == nums.length - 1 || nums[index] > nums[index + 1]);
+  }
 }
