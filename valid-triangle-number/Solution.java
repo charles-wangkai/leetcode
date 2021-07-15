59741a28eb8cabbe7d6ba4ce16bc23ed928ea5c4
Kai Wang <charles.wangkai@gmail.com>
2021-07-15 20:13:09 +0800

    Valid Triangle Number

--- valid-triangle-number/Solution.java
+++ valid-triangle-number/Solution.java
@@ -1,32 +1,34 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int triangleNumber(int[] nums) {
-		Arrays.sort(nums);
+class Solution {
+  public int triangleNumber(int[] nums) {
+    Arrays.sort(nums);
 
-		int result = 0;
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				result += Math.max(0, searchLimit(nums, nums[i] + nums[j]) - j);
-			}
-		}
-		return result;
-	}
+    int result = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      for (int j = i + 1; j < nums.length; ++j) {
+        result += computeLongestSideNum(nums, nums[i] + nums[j], j);
+      }
+    }
 
-	int searchLimit(int[] nums, int limit) {
-		int lower = 0;
-		int upper = nums.length - 1;
-		int result = -1;
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
-			if (nums[middle] < limit) {
-				result = middle;
+    return result;
+  }
 
-				lower = middle + 1;
-			} else {
-				upper = middle - 1;
-			}
-		}
-		return result;
-	}
+  int computeLongestSideNum(int[] nums, int limit, int fromIndex) {
+    int result = 0;
+    int lower = fromIndex + 1;
+    int upper = nums.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (nums[middle] < limit) {
+        result = middle - fromIndex;
+
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
+    }
+
+    return result;
+  }
 }
