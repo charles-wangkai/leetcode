355add6867d922864a34acaafe73b15578470488
Kai Wang <charles.wangkai@gmail.com>
2021-07-16 17:27:08 +0800

    4Sum

--- 4sum/Solution.java
+++ 4sum/Solution.java
@@ -1,50 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public List<List<Integer>> fourSum(int[] num, int target) {
-		Map<Integer, List<Integer[]>> sum2indices = new HashMap<Integer, List<Integer[]>>();
-		for (int i = 0; i < num.length; i++) {
-			for (int j = i + 1; j < num.length; j++) {
-				int sum = num[i] + num[j];
-				if (!sum2indices.containsKey(sum)) {
-					sum2indices.put(sum, new ArrayList<Integer[]>());
-				}
-				sum2indices.get(sum).add(new Integer[] { i, j });
-			}
-		}
+class Solution {
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    Set<List<Integer>> result = new HashSet<>();
+    Set<Integer> lastValues = new HashSet<>();
+    for (int k = nums.length - 2; k >= 0; --k) {
+      lastValues.add(nums[k + 1]);
+      for (int j = 0; j < k; ++j) {
+        for (int i = 0; i < j; ++i) {
+          long lastValue = (long) target - nums[i] - nums[j] - nums[k];
+          if (lastValue >= Integer.MIN_VALUE
+              && lastValue <= Integer.MAX_VALUE
+              && lastValues.contains((int) lastValue)) {
+            result.add(
+                IntStream.of(nums[i], nums[j], nums[k], (int) lastValue)
+                    .sorted()
+                    .boxed()
+                    .collect(Collectors.toList()));
+          }
+        }
+      }
+    }
 
-		Set<List<Integer>> solutionSet = new HashSet<List<Integer>>();
-		for (Entry<Integer, List<Integer[]>> entry : sum2indices.entrySet()) {
-			int sum = entry.getKey();
-			List<Integer[]> indices = entry.getValue();
-			List<Integer[]> otherIndices = sum2indices.get(target - sum);
-			if (otherIndices == null) {
-				continue;
-			}
-			for (Integer[] ind1 : indices) {
-				for (Integer[] ind2 : otherIndices) {
-					Set<Integer> totalIndices = new HashSet<Integer>();
-					totalIndices.addAll(Arrays.asList(ind1));
-					totalIndices.addAll(Arrays.asList(ind2));
-					if (totalIndices.size() == 4) {
-						List<Integer> solution = new ArrayList<Integer>();
-						for (int index : totalIndices) {
-							solution.add(num[index]);
-						}
-						Collections.sort(solution);
-						solutionSet.add(solution);
-					}
-				}
-			}
-		}
-		return new ArrayList<List<Integer>>(solutionSet);
-	}
+    return List.copyOf(result);
+  }
 }
