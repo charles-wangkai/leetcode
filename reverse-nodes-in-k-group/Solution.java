e554bef6a21f3402ab39cc3d07c34f225add1c7f
Kai Wang <charles.wangkai@gmail.com>
2021-07-18 20:13:58 +0800

    Reverse Nodes in k-Group

--- reverse-nodes-in-k-group/Solution.java
+++ reverse-nodes-in-k-group/Solution.java
@@ -1,49 +1,56 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode reverseKGroup(ListNode head, int k) {
-		ListNode tempHead = new ListNode(0);
-		tempHead.next = head;
+class Solution {
+  public ListNode reverseKGroup(ListNode head, int k) {
+    ListNode tempHead = new ListNode(0, head);
 
-		ListNode current = tempHead;
-		while (true) {
-			ListNode rHead = null;
-			ListNode rTail = null;
+    ListNode current = tempHead;
+    while (true) {
+      ListNode p = current;
+      for (int i = 0; i < k; ++i) {
+        if (p.next == null) {
+          return tempHead.next;
+        }
 
-			ListNode p = current;
-			for (int i = 0; i < k; i++) {
-				if (p.next == null) {
-					return tempHead.next;
-				}
-				p = p.next;
-			}
+        p = p.next;
+      }
 
-			for (int i = 0; i < k; i++) {
-				ListNode node = current.next;
-				current.next = node.next;
-				node.next = rHead;
-				rHead = node;
-				if (rTail == null) {
-					rTail = node;
-				}
-			}
+      ListNode rHead = null;
+      ListNode rTail = null;
+      for (int i = 0; i < k; ++i) {
+        ListNode node = current.next;
+        current.next = node.next;
 
-			ListNode after = current.next;
-			current.next = rHead;
-			rTail.next = after;
+        node.next = rHead;
+        rHead = node;
 
-			for (int i = 0; i < k; i++) {
-				current = current.next;
-			}
-		}
-	}
+        if (rTail == null) {
+          rTail = node;
+        }
+      }
+
+      ListNode after = current.next;
+      current.next = rHead;
+      rTail.next = after;
+
+      for (int i = 0; i < k; ++i) {
+        current = current.next;
+      }
+    }
+  }
 }
