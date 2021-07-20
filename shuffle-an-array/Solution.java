05ee470ff3d9c96a75fd5f0a83533c23999a0f6b
Kai Wang <charles.wangkai@gmail.com>
2021-07-20 17:59:12 +0800

    Shuffle an Array

--- shuffle-an-array/Solution.java
+++ shuffle-an-array/Solution.java
@@ -1,53 +1,45 @@
+import java.util.Arrays;
 import java.util.Random;
 
-public class Solution {
-	private Random random = new Random();
+class Solution {
+  Random random = new Random();
+  int[] nums;
 
-	int[] nums;
+  public Solution(int[] nums) {
+    this.nums = nums;
+  }
 
-	public Solution(int[] nums) {
-		this.nums = nums;
-	}
+  /** Resets the array to its original configuration and return it. */
+  public int[] reset() {
+    return nums;
+  }
 
-	/** Resets the array to its original configuration and return it. */
-	public int[] reset() {
-		return nums;
-	}
+  /** Returns a random shuffling of the array. */
+  public int[] shuffle() {
+    int[] shuffled = Arrays.copyOf(nums, nums.length);
+    permute(shuffled, 0);
 
-	/** Returns a random shuffling of the array. */
-	public int[] shuffle() {
-		int[] shuffled = copy(nums);
-		permute(shuffled, 0);
-		return shuffled;
-	}
+    return shuffled;
+  }
 
-	private void permute(int[] shuffled, int index) {
-		if (index == shuffled.length) {
-			return;
-		}
+  void permute(int[] shuffled, int index) {
+    if (index == shuffled.length) {
+      return;
+    }
 
-		int chosenIndex = randomChoose(index, shuffled.length - 1);
-		swap(shuffled, index, chosenIndex);
-		permute(shuffled, index + 1);
-	}
+    swap(shuffled, index, randomChoose(index, shuffled.length - 1));
+    permute(shuffled, index + 1);
+  }
 
-	private void swap(int[] a, int index1, int index2) {
-		int temp = a[index1];
-		a[index1] = a[index2];
-		a[index2] = temp;
-	}
+  void swap(int[] a, int index1, int index2) {
+    int temp = a[index1];
+    a[index1] = a[index2];
+    a[index2] = temp;
+  }
 
-	private int randomChoose(int begin, int end) {
-		return random.nextInt(end - begin + 1) + begin;
-	}
-
-	private int[] copy(int[] a) {
-		int[] result = new int[a.length];
-		for (int i = 0; i < result.length; i++) {
-			result[i] = a[i];
-		}
-		return result;
-	}
+  int randomChoose(int begin, int end) {
+    return random.nextInt(end - begin + 1) + begin;
+  }
 }
 
 // Your Solution object will be instantiated and called as such:
