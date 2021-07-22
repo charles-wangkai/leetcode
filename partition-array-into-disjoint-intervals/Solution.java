727a36b425a9a4c42482c30241db0025033c74b6
Kai Wang <charles.wangkai@gmail.com>
2021-07-22 20:08:55 +0800

    Partition Array into Disjoint Intervals

--- partition-array-into-disjoint-intervals/Solution.java
+++ partition-array-into-disjoint-intervals/Solution.java
@@ -1,23 +1,23 @@
-public class Solution {
-	public int partitionDisjoint(int[] A) {
-		int leftMax = Integer.MIN_VALUE;
-		int[] leftMaxs = new int[A.length];
-		for (int i = 0; i < leftMaxs.length; i++) {
-			leftMax = Math.max(leftMax, A[i]);
-			leftMaxs[i] = leftMax;
-		}
+class Solution {
+  public int partitionDisjoint(int[] nums) {
+    int leftMax = Integer.MIN_VALUE;
+    int[] leftMaxs = new int[nums.length];
+    for (int i = 0; i < leftMaxs.length; ++i) {
+      leftMax = Math.max(leftMax, nums[i]);
+      leftMaxs[i] = leftMax;
+    }
 
-		int rightMin = Integer.MAX_VALUE;
-		int[] rightMins = new int[A.length];
-		for (int i = rightMins.length - 1; i >= 0; i--) {
-			rightMin = Math.min(rightMin, A[i]);
-			rightMins[i] = rightMin;
-		}
+    int rightMin = Integer.MAX_VALUE;
+    int[] rightMins = new int[nums.length];
+    for (int i = rightMins.length - 1; i >= 0; --i) {
+      rightMin = Math.min(rightMin, nums[i]);
+      rightMins[i] = rightMin;
+    }
 
-		for (int i = 0;; i++) {
-			if (leftMaxs[i] <= rightMins[i + 1]) {
-				return i + 1;
-			}
-		}
-	}
+    for (int i = 0; ; ++i) {
+      if (leftMaxs[i] <= rightMins[i + 1]) {
+        return i + 1;
+      }
+    }
+  }
 }
