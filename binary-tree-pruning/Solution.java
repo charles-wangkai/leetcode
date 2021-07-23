f31a8f898c26aec7215cbae3e72cc37b7967284a
Kai Wang <charles.wangkai@gmail.com>
2021-07-23 17:19:26 +0800

    Binary Tree Pruning

--- binary-tree-pruning/Solution.java
+++ binary-tree-pruning/Solution.java
@@ -1,27 +1,31 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode pruneTree(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
+class Solution {
+  public TreeNode pruneTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
 
-		root.left = pruneTree(root.left);
-		root.right = pruneTree(root.right);
+    root.left = pruneTree(root.left);
+    root.right = pruneTree(root.right);
 
-		if (root.left == null && root.right == null && root.val == 0) {
-			return null;
-		} else {
-			return root;
-		}
-	}
+    return (root.left == null && root.right == null && root.val == 0) ? null : root;
+  }
 }
