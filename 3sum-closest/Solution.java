2da4cac25561f0e976b1b610071bb08f33d57c49
Kai Wang <charles.wangkai@gmail.com>
2021-07-27 19:09:04 +0800

    3Sum Closest

--- 3sum-closest/Solution.java
+++ 3sum-closest/Solution.java
@@ -1,24 +1,25 @@
 import java.util.Arrays;
 
 public class Solution {
-	public int threeSumClosest(int[] num, int target) {
-		Arrays.sort(num);
-		Integer closest = null;
-		for (int i = 0; i < num.length; i++) {
-			int remain = target - num[i];
-			for (int j = i + 1, k = num.length - 1; j < k;) {
-				int sum = num[i] + num[j] + num[k];
-				if (closest == null
-						|| Math.abs(sum - target) < Math.abs(closest - target)) {
-					closest = sum;
-				}
-				if (num[j] + num[k] <= remain) {
-					j++;
-				} else {
-					k--;
-				}
-			}
-		}
-		return closest;
-	}
+  public int threeSumClosest(int[] nums, int target) {
+    Arrays.sort(nums);
+
+    Integer result = null;
+    for (int i = 0; i < nums.length; ++i) {
+      for (int j = i + 1, k = nums.length - 1; j < k; ) {
+        int sum = nums[i] + nums[j] + nums[k];
+        if (result == null || Math.abs(sum - target) < Math.abs(result - target)) {
+          result = sum;
+        }
+
+        if (sum <= target) {
+          ++j;
+        } else {
+          --k;
+        }
+      }
+    }
+
+    return result;
+  }
 }
