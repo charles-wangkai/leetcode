693010192c188d071535a088046e6ea091faf2fc
Kai Wang <charles.wangkai@gmail.com>
2021-07-29 17:32:21 +0800

    01 Matrix

--- 01-matrix/Solution.java
+++ 01-matrix/Solution.java
@@ -1,61 +1,54 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
-		int row = matrix.size();
-		int col = matrix.get(0).size();
+  public int[][] updateMatrix(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
 
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		for (int r = 0; r < row; r++) {
-			List<Integer> line = new ArrayList<Integer>();
-			for (int c = 0; c < col; c++) {
-				line.add(-1);
-			}
-			result.add(line);
-		}
+    int[][] result = new int[m][n];
+    for (int r = 0; r < result.length; ++r) {
+      Arrays.fill(result[r], -1);
+    }
 
-		Queue<Point> queue = new LinkedList<Point>();
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				if (matrix.get(r).get(c) == 0) {
-					queue.offer(new Point(r, c));
+    Queue<Point> queue = new ArrayDeque<>();
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (mat[r][c] == 0) {
+          result[r][c] = 0;
+          queue.offer(new Point(r, c));
+        }
+      }
+    }
 
-					result.get(r).set(c, 0);
-				}
-			}
-		}
+    while (!queue.isEmpty()) {
+      Point head = queue.poll();
 
-		while (!queue.isEmpty()) {
-			Point head = queue.poll();
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r + R_OFFSETS[i];
+        int adjC = head.c + C_OFFSETS[i];
 
-			for (int i = 0; i < R_OFFSETS.length; i++) {
-				int nextR = head.r + R_OFFSETS[i];
-				int nextC = head.c + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && result[adjR][adjC] == -1) {
+          result[adjR][adjC] = result[head.r][head.c] + 1;
+          queue.offer(new Point(adjR, adjC));
+        }
+      }
+    }
 
-				if (nextR >= 0 && nextR < row && nextC >= 0 && nextC < col && result.get(nextR).get(nextC) < 0) {
-					queue.offer(new Point(nextR, nextC));
-
-					result.get(nextR).set(nextC, result.get(head.r).get(head.c) + 1);
-				}
-			}
-		}
-
-		return result;
-	}
+    return result;
+  }
 }
 
 class Point {
-	int r;
-	int c;
+  int r;
+  int c;
 
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
 }
