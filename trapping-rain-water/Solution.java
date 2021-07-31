9d38303d6764ee9c1c2c41ef2a31b4bbe6c5c30c
Kai Wang <charles.wangkai@gmail.com>
2021-07-31 17:38:00 +0800

    Trapping Rain Water

--- trapping-rain-water/Solution.java
+++ trapping-rain-water/Solution.java
@@ -1,20 +1,19 @@
-public class Solution {
-	public int trap(int[] A) {
-		int[] maxLeft = new int[A.length];
-		for (int i = 0; i < maxLeft.length; i++) {
-			maxLeft[i] = Math.max(A[i], (i == 0) ? 0 : maxLeft[i - 1]);
-		}
+import java.util.stream.IntStream;
 
-		int[] maxRight = new int[A.length];
-		for (int i = maxRight.length - 1; i >= 0; i--) {
-			maxRight[i] = Math.max(A[i], (i == maxRight.length - 1) ? 0
-					: maxRight[i + 1]);
-		}
+class Solution {
+  public int trap(int[] height) {
+    int n = height.length;
 
-		int water = 0;
-		for (int i = 0; i < A.length; i++) {
-			water += Math.min(maxLeft[i], maxRight[i]) - A[i];
-		}
-		return water;
-	}
+    int[] leftMaxs = new int[n];
+    for (int i = 0; i < leftMaxs.length; ++i) {
+      leftMaxs[i] = Math.max((i == 0) ? 0 : leftMaxs[i - 1], height[i]);
+    }
+
+    int[] rightMaxs = new int[n];
+    for (int i = rightMaxs.length - 1; i >= 0; --i) {
+      rightMaxs[i] = Math.max((i == rightMaxs.length - 1) ? 0 : rightMaxs[i + 1], height[i]);
+    }
+
+    return IntStream.range(0, n).map(i -> Math.min(leftMaxs[i], rightMaxs[i]) - height[i]).sum();
+  }
 }
