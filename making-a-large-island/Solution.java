efb2c32661c442cfbc4a08cab92bd9b5f68b71ae
Kai Wang <charles.wangkai@gmail.com>
2021-08-01 19:57:02 +0800

    Making A Large Island

--- making-a-large-island/Solution.java
+++ making-a-large-island/Solution.java
@@ -1,102 +1,105 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int largestIsland(int[][] grid) {
-		int size = grid.length;
-		boolean[][] visited = new boolean[size][size];
-		int[][] marks = new int[size][size];
-		int[][] areas = new int[size][size];
+  public int largestIsland(int[][] grid) {
+    int n = grid.length;
 
-		int mark = 1;
-		for (int r = 0; r < size; r++) {
-			for (int c = 0; c < size; c++) {
-				if (!visited[r][c] && grid[r][c] == 1) {
-					fill(grid, visited, marks, areas, mark, r, c);
+    boolean[][] visited = new boolean[n][n];
+    int[][] marks = new int[n][n];
+    int[][] areas = new int[n][n];
 
-					mark++;
-				}
-			}
-		}
+    int mark = 1;
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (!visited[r][c] && grid[r][c] == 1) {
+          fill(grid, visited, marks, areas, mark, r, c);
 
-		int result = 0;
-		for (int r = 0; r < size; r++) {
-			for (int c = 0; c < size; c++) {
-				if (grid[r][c] == 1) {
-					result = Math.max(result, areas[r][c]);
-				} else {
-					int totalArea = 1;
-					Set<Integer> usedMarks = new HashSet<>();
-					for (int i = 0; i < R_OFFSETS.length; i++) {
-						int adjR = r + R_OFFSETS[i];
-						int adjC = c + C_OFFSETS[i];
+          ++mark;
+        }
+      }
+    }
 
-						if (isValid(size, adjR, adjC) && !usedMarks.contains(marks[adjR][adjC])) {
-							usedMarks.add(marks[adjR][adjC]);
-							totalArea += areas[adjR][adjC];
-						}
-					}
+    int result = 0;
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (grid[r][c] == 1) {
+          result = Math.max(result, areas[r][c]);
+        } else {
+          int totalArea = 1;
+          Set<Integer> usedMarks = new HashSet<>();
+          for (int i = 0; i < R_OFFSETS.length; ++i) {
+            int adjR = r + R_OFFSETS[i];
+            int adjC = c + C_OFFSETS[i];
 
-					result = Math.max(result, totalArea);
-				}
-			}
-		}
-		return result;
-	}
+            if (adjR >= 0
+                && adjR < n
+                && adjC >= 0
+                && adjC < n
+                && !usedMarks.contains(marks[adjR][adjC])) {
+              usedMarks.add(marks[adjR][adjC]);
+              totalArea += areas[adjR][adjC];
+            }
+          }
 
-	void fill(int[][] grid, boolean[][] visited, int[][] marks, int[][] areas, int mark, int beginR, int beginC) {
-		List<Point> island = new ArrayList<>();
+          result = Math.max(result, totalArea);
+        }
+      }
+    }
 
-		search(grid, visited, island, beginR, beginC);
+    return result;
+  }
 
-		for (Point point : island) {
-			marks[point.r][point.c] = mark;
-			areas[point.r][point.c] = island.size();
-		}
-	}
+  void fill(
+      int[][] grid,
+      boolean[][] visited,
+      int[][] marks,
+      int[][] areas,
+      int mark,
+      int beginR,
+      int beginC) {
+    List<Point> island = new ArrayList<>();
+    search(grid, visited, island, beginR, beginC);
 
-	void search(int[][] grid, boolean[][] visited, List<Point> island, int r, int c) {
-		visited[r][c] = true;
-		island.add(new Point(r, c));
+    for (Point point : island) {
+      marks[point.r][point.c] = mark;
+      areas[point.r][point.c] = island.size();
+    }
+  }
 
-		for (int i = 0; i < R_OFFSETS.length; i++) {
-			int adjR = r + R_OFFSETS[i];
-			int adjC = c + C_OFFSETS[i];
+  void search(int[][] grid, boolean[][] visited, List<Point> island, int r, int c) {
+    int n = grid.length;
 
-			if (isValid(grid.length, adjR, adjC) && grid[adjR][adjC] == 1 && !visited[adjR][adjC]) {
-				search(grid, visited, island, adjR, adjC);
-			}
-		}
-	}
+    visited[r][c] = true;
+    island.add(new Point(r, c));
 
-	boolean isValid(int size, int r, int c) {
-		return r >= 0 && r < size && c >= 0 && c < size;
-	}
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int adjR = r + R_OFFSETS[i];
+      int adjC = c + C_OFFSETS[i];
+
+      if (adjR >= 0
+          && adjR < n
+          && adjC >= 0
+          && adjC < n
+          && grid[adjR][adjC] == 1
+          && !visited[adjR][adjC]) {
+        search(grid, visited, island, adjR, adjC);
+      }
+    }
+  }
 }
 
 class Point {
-	int r;
-	int c;
+  int r;
+  int c;
 
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(r, c);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		Point other = (Point) obj;
-		return r == other.r && c == other.c;
-	}
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
 }
