67a28d5a31796e904e62d71832861dd4445b5844
Kai Wang <charles.wangkai@gmail.com>
2021-08-05 09:17:56 +0800

    Path Sum II

--- path-sum-ii/Solution.java
+++ path-sum-ii/Solution.java
@@ -1,42 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<List<Integer>> pathSum(TreeNode root, int sum) {
-		List<List<Integer>> paths = new ArrayList<List<Integer>>();
-		search(paths, new ArrayList<Integer>(), root, sum);
-		return paths;
-	}
+class Solution {
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return List.of();
+    }
 
-	void search(List<List<Integer>> paths, List<Integer> path, TreeNode node,
-			int sum) {
-		if (node == null) {
-			return;
-		}
-		path.add(node.val);
-		if (node.left == null && node.right == null) {
-			if (sum == node.val) {
-				paths.add(new ArrayList<Integer>(path));
-			}
-		} else {
-			if (node.left != null) {
-				search(paths, path, node.left, sum - node.val);
-			}
-			if (node.right != null) {
-				search(paths, path, node.right, sum - node.val);
-			}
-		}
-		path.remove(path.size() - 1);
-	}
+    List<List<Integer>> paths = new ArrayList<>();
+    search(paths, new ArrayList<>(), root, targetSum);
+
+    return paths;
+  }
+
+  void search(List<List<Integer>> paths, List<Integer> path, TreeNode node, int restSum) {
+    path.add(node.val);
+
+    if (node.left == null && node.right == null && node.val == restSum) {
+      paths.add(List.copyOf(path));
+    } else {
+      if (node.left != null) {
+        search(paths, path, node.left, restSum - node.val);
+      }
+      if (node.right != null) {
+        search(paths, path, node.right, restSum - node.val);
+      }
+    }
+
+    path.remove(path.size() - 1);
+  }
 }
