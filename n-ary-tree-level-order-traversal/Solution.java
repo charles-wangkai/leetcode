392e9d47b448dfdbfd9217d14885b92bfe76036b
Kai Wang <charles.wangkai@gmail.com>
2021-08-06 23:56:52 +0800

    N-ary Tree Level Order Traversal

--- n-ary-tree-level-order-traversal/Solution.java
+++ n-ary-tree-level-order-traversal/Solution.java
@@ -1,40 +1,43 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 // Definition for a Node.
 class Node {
-	public int val;
-	public List<Node> children;
+  public int val;
+  public List<Node> children;
 
-	public Node() {
-	}
+  public Node() {}
 
-	public Node(int _val, List<Node> _children) {
-		val = _val;
-		children = _children;
-	}
+  public Node(int _val) {
+    val = _val;
+  }
+
+  public Node(int _val, List<Node> _children) {
+    val = _val;
+    children = _children;
+  }
 }
 
-public class Solution {
-	public List<List<Integer>> levelOrder(Node root) {
-		if (root == null) {
-			return Collections.emptyList();
-		}
+class Solution {
+  public List<List<Integer>> levelOrder(Node root) {
+    if (root == null) {
+      return List.of();
+    }
 
-		List<List<Integer>> result = new ArrayList<>();
-		search(result, root, 0);
-		return result;
-	}
+    List<List<Integer>> levels = new ArrayList<>();
+    search(levels, root, 0);
 
-	void search(List<List<Integer>> result, Node node, int depth) {
-		if (depth == result.size()) {
-			result.add(new ArrayList<>());
-		}
-		result.get(depth).add(node.val);
+    return levels;
+  }
 
-		for (Node child : node.children) {
-			search(result, child, depth + 1);
-		}
-	}
+  void search(List<List<Integer>> levels, Node node, int depth) {
+    if (depth == levels.size()) {
+      levels.add(new ArrayList<>());
+    }
+    levels.get(depth).add(node.val);
+
+    for (Node child : node.children) {
+      search(levels, child, depth + 1);
+    }
+  }
 }
