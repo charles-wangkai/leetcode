85a06e49bc425c8f83e273a8b61fab7fa68a4db6
Kai Wang <charles.wangkai@gmail.com>
2021-08-10 06:14:17 +0800

    Find the Longest Valid Obstacle Course at Each Position

---
+++ find-the-longest-valid-obstacle-course-at-each-position/Solution.java
@@ -0,0 +1,32 @@
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+class Solution {
+  public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+    int[] result = new int[obstacles.length];
+    NavigableMap<Integer, Integer> obstacleToLength = new TreeMap<>();
+    for (int i = 0; i < result.length; ++i) {
+      Integer floorObstacle = obstacleToLength.floorKey(obstacles[i]);
+      int length;
+      if (floorObstacle == null) {
+        length = 1;
+      } else {
+        length = obstacleToLength.get(floorObstacle) + 1;
+      }
+
+      result[i] = length;
+      obstacleToLength.put(obstacles[i], length);
+
+      while (true) {
+        Integer higherObstacle = obstacleToLength.higherKey(obstacles[i]);
+        if (higherObstacle == null || obstacleToLength.get(higherObstacle) >= length) {
+          break;
+        }
+
+        obstacleToLength.remove(higherObstacle);
+      }
+    }
+
+    return result;
+  }
+}
