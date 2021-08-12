4d2a19baa3e7714b73059fd01b94e8b44f61d807
Kai Wang <charles.wangkai@gmail.com>
2021-08-12 19:22:35 +0800

    Group Anagrams

--- group-anagrams/Solution.java
+++ group-anagrams/Solution.java
@@ -4,22 +4,25 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public List<List<String>> groupAnagrams(String[] strs) {
-		Map<String, List<String>> keyToWords = new HashMap<>();
-		for (String str : strs) {
-			String key = generateKey(str);
-			if (!keyToWords.containsKey(key)) {
-				keyToWords.put(key, new ArrayList<>());
-			}
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> keyToWords = new HashMap<>();
+    for (String str : strs) {
+      String key = generateKey(str);
+      if (!keyToWords.containsKey(key)) {
+        keyToWords.put(key, new ArrayList<>());
+      }
 
-			keyToWords.get(key).add(str);
-		}
+      keyToWords.get(key).add(str);
+    }
 
-		return new ArrayList<>(keyToWords.values());
-	}
+    return List.copyOf(keyToWords.values());
+  }
 
-	String generateKey(String word) {
-		return word.chars().sorted().mapToObj(ch -> String.valueOf((char) ch)).collect(Collectors.joining());
-	}
+  String generateKey(String word) {
+    return word.chars()
+        .sorted()
+        .mapToObj(ch -> String.valueOf((char) ch))
+        .collect(Collectors.joining());
+  }
 }
