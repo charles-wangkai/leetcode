d25af8bbc33e6807f5f6cc7cd5d41dccafcf3e9a
Kai Wang <charles.wangkai@gmail.com>
2021-08-17 21:50:17 +0800

    Count Good Nodes in Binary Tree

--- count-good-nodes-in-binary-tree/Solution.java
+++ count-good-nodes-in-binary-tree/Solution.java
@@ -1,34 +1,34 @@
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode() {
-    }
+  TreeNode() {}
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-    public int goodNodes(TreeNode root) {
-        return search(root, Integer.MIN_VALUE);
+  public int goodNodes(TreeNode root) {
+    return search(root, Integer.MIN_VALUE);
+  }
+
+  int search(TreeNode node, int maxValue) {
+    if (node == null) {
+      return 0;
     }
 
-    int search(TreeNode node, int maxValue) {
-        if (node == null) {
-            return 0;
-        }
-
-        return ((node.val >= maxValue) ? 1 : 0) + search(node.left, Math.max(maxValue, node.val))
-                + search(node.right, Math.max(maxValue, node.val));
-    }
+    return ((node.val >= maxValue) ? 1 : 0)
+        + search(node.left, Math.max(maxValue, node.val))
+        + search(node.right, Math.max(maxValue, node.val));
+  }
 }
