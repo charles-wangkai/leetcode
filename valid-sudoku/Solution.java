5a77091911a0d565c5ce627e30d40c28ab956061
Kai Wang <charles.wangkai@gmail.com>
2021-08-20 16:33:46 +0800

    Valid Sudoku

--- valid-sudoku/Solution.java
+++ valid-sudoku/Solution.java
@@ -1,45 +1,29 @@
-public class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            if (!checkValid(board, i, 0, 9)) {
-                return false;
-            }
+import java.util.stream.IntStream;
+
+class Solution {
+  public boolean isValidSudoku(char[][] board) {
+    return IntStream.range(0, 9).allMatch(r -> check(board, r, 0, 1, 9))
+        && IntStream.range(0, 9).allMatch(c -> check(board, 0, c, 9, 1))
+        && IntStream.range(0, 3)
+            .allMatch(i -> IntStream.range(0, 3).allMatch(j -> check(board, i * 3, j * 3, 3, 3)));
+  }
+
+  boolean check(char[][] board, int r, int c, int row, int col) {
+    boolean[] used = new boolean[10];
+    for (int dr = 0; dr < row; ++dr) {
+      for (int dc = 0; dc < col; ++dc) {
+        char cell = board[r + dr][c + dc];
+        if (cell != '.') {
+          int digit = cell - '0';
+          if (used[digit]) {
+            return false;
+          }
+
+          used[digit] = true;
         }
-        for (int j = 0; j < 9; j++) {
-            if (!checkValid(board, 0, j, 1)) {
-                return false;
-            }
-        }
-        for (int i = 0; i < 9; i += 3) {
-            for (int j = 0; j < 9; j += 3) {
-                if (!checkValid(board, i, j, 3)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+      }
     }
 
-    boolean checkValid(char[][] board, int r, int c, int columnLimit) {
-        boolean[] used = new boolean[10];
-        int offsetR = 0;
-        int offsetC = 0;
-        for (int i = 0; i < 9; i++) {
-            char cell = board[r + offsetR][c + offsetC];
-            if (cell != '.') {
-                int digit = cell - '0';
-                if (used[digit]) {
-                    return false;
-                }
-                used[digit] = true;
-            }
-
-            offsetC++;
-            if (offsetC == columnLimit) {
-                offsetR++;
-                offsetC = 0;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
