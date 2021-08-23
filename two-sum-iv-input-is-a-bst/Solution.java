74979ee71f353c3098967ed0f8316657b42b4252
Kai Wang <charles.wangkai@gmail.com>
2021-08-23 15:11:49 +0800

    Two Sum IV - Input is a BST

--- two-sum-iv-input-is-a-bst/Solution.java
+++ two-sum-iv-input-is-a-bst/Solution.java
@@ -2,85 +2,88 @@ import java.util.Stack;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public boolean findTarget(TreeNode root, int k) {
-		if (root == null) {
-			return false;
-		}
+class Solution {
+  public boolean findTarget(TreeNode root, int k) {
+    Stack<Element> lowers = new Stack<>();
+    lowers.push(new Element(root, true));
+    TreeNode lower = leftScan(lowers);
 
-		Stack<Element> lowers = new Stack<Element>();
-		lowers.push(new Element(root, true));
-		TreeNode lower = leftScan(lowers);
+    Stack<Element> uppers = new Stack<>();
+    uppers.push(new Element(root, true));
+    TreeNode upper = rightScan(uppers);
 
-		Stack<Element> uppers = new Stack<Element>();
-		uppers.push(new Element(root, true));
-		TreeNode upper = rightScan(uppers);
+    while (lower != upper) {
+      int sum = lower.val + upper.val;
+      if (sum > k) {
+        upper = rightScan(uppers);
+      } else if (sum < k) {
+        lower = leftScan(lowers);
+      } else {
+        return true;
+      }
+    }
 
-		while (lower != upper) {
-			int sum = lower.val + upper.val;
-			if (sum > k) {
-				upper = rightScan(uppers);
-			} else if (sum < k) {
-				lower = leftScan(lowers);
-			} else {
-				return true;
-			}
-		}
-		return false;
-	}
+    return false;
+  }
 
-	TreeNode leftScan(Stack<Element> lowers) {
-		while (true) {
-			Element head = lowers.pop();
+  TreeNode leftScan(Stack<Element> lowers) {
+    while (true) {
+      Element head = lowers.pop();
+      if (!head.toExpand) {
+        return head.node;
+      }
 
-			if (!head.toExpand) {
-				return head.node;
-			}
+      if (head.node.right != null) {
+        lowers.push(new Element(head.node.right, true));
+      }
+      lowers.push(new Element(head.node, false));
+      if (head.node.left != null) {
+        lowers.push(new Element(head.node.left, true));
+      }
+    }
+  }
 
-			if (head.node.right != null) {
-				lowers.push(new Element(head.node.right, true));
-			}
-			lowers.push(new Element(head.node, false));
-			if (head.node.left != null) {
-				lowers.push(new Element(head.node.left, true));
-			}
-		}
-	}
+  TreeNode rightScan(Stack<Element> uppers) {
+    while (true) {
+      Element head = uppers.pop();
+      if (!head.toExpand) {
+        return head.node;
+      }
 
-	TreeNode rightScan(Stack<Element> uppers) {
-		while (true) {
-			Element head = uppers.pop();
-
-			if (!head.toExpand) {
-				return head.node;
-			}
-
-			if (head.node.left != null) {
-				uppers.push(new Element(head.node.left, true));
-			}
-			uppers.push(new Element(head.node, false));
-			if (head.node.right != null) {
-				uppers.push(new Element(head.node.right, true));
-			}
-		}
-	}
+      if (head.node.left != null) {
+        uppers.push(new Element(head.node.left, true));
+      }
+      uppers.push(new Element(head.node, false));
+      if (head.node.right != null) {
+        uppers.push(new Element(head.node.right, true));
+      }
+    }
+  }
 }
 
 class Element {
-	TreeNode node;
-	boolean toExpand;
+  TreeNode node;
+  boolean toExpand;
 
-	Element(TreeNode node, boolean toExpand) {
-		this.node = node;
-		this.toExpand = toExpand;
-	}
+  Element(TreeNode node, boolean toExpand) {
+    this.node = node;
+    this.toExpand = toExpand;
+  }
 }
