95b5ce14134b8341bf299ea46465d72758c7fc80
Kai Wang <charles.wangkai@gmail.com>
2021-08-23 15:01:57 +0800

    Rectangle Area II

--- rectangle-area-ii/Solution.java
+++ rectangle-area-ii/Solution.java
@@ -1,49 +1,59 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-	static final int MODULUS = 1000000007;
+class Solution {
+  static final int MODULUS = 1_000_000_007;
 
-	public int rectangleArea(int[][] rectangles) {
-		int[] xValues = Arrays.stream(rectangles).flatMapToInt(rectangle -> IntStream.of(rectangle[0], rectangle[2]))
-				.distinct().sorted().toArray();
-		int[] yValues = Arrays.stream(rectangles).flatMapToInt(rectangle -> IntStream.of(rectangle[1], rectangle[3]))
-				.distinct().sorted().toArray();
+  public int rectangleArea(int[][] rectangles) {
+    int[] xValues =
+        Arrays.stream(rectangles)
+            .flatMapToInt(rectangle -> IntStream.of(rectangle[0], rectangle[2]))
+            .distinct()
+            .sorted()
+            .toArray();
+    int[] yValues =
+        Arrays.stream(rectangles)
+            .flatMapToInt(rectangle -> IntStream.of(rectangle[1], rectangle[3]))
+            .distinct()
+            .sorted()
+            .toArray();
 
-		boolean[][] fills = new boolean[xValues.length - 1][yValues.length - 1];
-		for (int[] rectangle : rectangles) {
-			int xMinIndex = findIndex(xValues, rectangle[0]);
-			int xMaxIndex = findIndex(xValues, rectangle[2]);
-			int yMinIndex = findIndex(yValues, rectangle[1]);
-			int yMaxIndex = findIndex(yValues, rectangle[3]);
+    boolean[][] fills = new boolean[xValues.length - 1][yValues.length - 1];
+    for (int[] rectangle : rectangles) {
+      int xMinIndex = findIndex(xValues, rectangle[0]);
+      int xMaxIndex = findIndex(xValues, rectangle[2]);
+      int yMinIndex = findIndex(yValues, rectangle[1]);
+      int yMaxIndex = findIndex(yValues, rectangle[3]);
 
-			for (int xIndex = xMinIndex; xIndex < xMaxIndex; xIndex++) {
-				for (int yIndex = yMinIndex; yIndex < yMaxIndex; yIndex++) {
-					fills[xIndex][yIndex] = true;
-				}
-			}
-		}
+      for (int xIndex = xMinIndex; xIndex < xMaxIndex; ++xIndex) {
+        for (int yIndex = yMinIndex; yIndex < yMaxIndex; ++yIndex) {
+          fills[xIndex][yIndex] = true;
+        }
+      }
+    }
 
-		int area = 0;
-		for (int i = 0; i < fills.length; i++) {
-			for (int j = 0; j < fills[0].length; j++) {
-				if (fills[i][j]) {
-					area = addMod(area, multiplyMod(xValues[i + 1] - xValues[i], yValues[j + 1] - yValues[j]));
-				}
-			}
-		}
-		return area;
-	}
+    int area = 0;
+    for (int i = 0; i < fills.length; ++i) {
+      for (int j = 0; j < fills[0].length; ++j) {
+        if (fills[i][j]) {
+          area =
+              addMod(area, multiplyMod(xValues[i + 1] - xValues[i], yValues[j + 1] - yValues[j]));
+        }
+      }
+    }
 
-	int findIndex(int[] values, int target) {
-		return Arrays.binarySearch(values, target);
-	}
+    return area;
+  }
 
-	int addMod(int x, int y) {
-		return (x + y) % MODULUS;
-	}
+  int findIndex(int[] values, int target) {
+    return Arrays.binarySearch(values, target);
+  }
 
-	int multiplyMod(int x, int y) {
-		return (int) ((long) x * y % MODULUS);
-	}
+  int addMod(int x, int y) {
+    return (x + y) % MODULUS;
+  }
+
+  int multiplyMod(int x, int y) {
+    return (int) ((long) x * y % MODULUS);
+  }
 }
