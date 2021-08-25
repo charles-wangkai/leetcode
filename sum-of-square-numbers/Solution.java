feb8bba9b3d4a24aea9717a6a17247ef83c30e8b
Kai Wang <charles.wangkai@gmail.com>
2021-08-25 22:45:10 +0800

    Sum of Square Numbers

--- sum-of-square-numbers/Solution.java
+++ sum-of-square-numbers/Solution.java
@@ -1,15 +1,17 @@
-public class Solution {
-	public boolean judgeSquareSum(int c) {
-		for (int i = 0; 2L * i * i <= c; i++) {
-			if (isSquare(c - i * i)) {
-				return true;
-			}
-		}
-		return false;
-	}
+class Solution {
+  public boolean judgeSquareSum(int c) {
+    for (int i = 0; 2L * i * i <= c; ++i) {
+      if (isSquare(c - i * i)) {
+        return true;
+      }
+    }
 
-	boolean isSquare(int n) {
-		int root = (int) Math.round(Math.sqrt(n));
-		return root * root == n;
-	}
+    return false;
+  }
+
+  boolean isSquare(int n) {
+    int root = (int) Math.round(Math.sqrt(n));
+
+    return root * root == n;
+  }
 }
