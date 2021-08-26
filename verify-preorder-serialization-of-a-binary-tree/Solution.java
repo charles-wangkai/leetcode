f2c25a4f3907915260c7e6a706951a78ac89590b
Kai Wang <charles.wangkai@gmail.com>
2021-08-26 16:47:57 +0800

    Verify Preorder Serialization of a Binary Tree

--- verify-preorder-serialization-of-a-binary-tree/Solution.java
+++ verify-preorder-serialization-of-a-binary-tree/Solution.java
@@ -1,37 +1,36 @@
 import java.util.Stack;
 
-public class Solution {
-	static final String NULL = "#";
+class Solution {
+  static final String NULL = "#";
 
-	public boolean isValidSerialization(String preorder) {
-		if (preorder.isEmpty()) {
-			return true;
-		}
+  public boolean isValidSerialization(String preorder) {
+    Stack<String> stack = new Stack<>();
+    for (String value : preorder.split(",")) {
+      stack.push(value);
 
-		Stack<String> stack = new Stack<String>();
-		for (String value : preorder.split(",")) {
-			stack.push(value);
+      while (canReduce(stack)) {
+        reduce(stack);
+      }
+    }
 
-			while (canReduce(stack)) {
-				reduce(stack);
-			}
-		}
-		return stack.size() == 1 && stack.peek().equals(NULL);
-	}
+    return stack.size() == 1 && stack.peek().equals(NULL);
+  }
 
-	boolean canReduce(Stack<String> stack) {
-		int size = stack.size();
-		if (size < 3) {
-			return false;
-		}
-		return !stack.get(size - 3).equals(NULL) && stack.get(size - 2).equals(NULL)
-				&& stack.get(size - 1).equals(NULL);
-	}
+  boolean canReduce(Stack<String> stack) {
+    int size = stack.size();
+    if (size < 3) {
+      return false;
+    }
 
-	void reduce(Stack<String> stack) {
-		stack.pop();
-		stack.pop();
-		stack.pop();
-		stack.push(NULL);
-	}
+    return !stack.get(size - 3).equals(NULL)
+        && stack.get(size - 2).equals(NULL)
+        && stack.get(size - 1).equals(NULL);
+  }
+
+  void reduce(Stack<String> stack) {
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.push(NULL);
+  }
 }
