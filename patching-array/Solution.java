8bdffb9ff51bf20f2ae5842a5891aea74706b98b
Kai Wang <charles.wangkai@gmail.com>
2021-08-29 23:25:01 +0800

    Patching Array

--- patching-array/Solution.java
+++ patching-array/Solution.java
@@ -1,17 +1,18 @@
-public class Solution {
-	public int minPatches(int[] nums, int n) {
-		long miss = 1;
-		int patchNum = 0;
-		int index = 0;
-		while (miss <= n) {
-			if (index < nums.length && nums[index] <= miss) {
-				miss += nums[index];
-				index++;
-			} else {
-				miss += miss;
-				patchNum++;
-			}
-		}
-		return patchNum;
-	}
+class Solution {
+  public int minPatches(int[] nums, int n) {
+    long miss = 1;
+    int result = 0;
+    int index = 0;
+    while (miss <= n) {
+      if (index != nums.length && nums[index] <= miss) {
+        miss += nums[index];
+        ++index;
+      } else {
+        miss += miss;
+        ++result;
+      }
+    }
+
+    return result;
+  }
 }
