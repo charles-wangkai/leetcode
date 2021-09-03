dab9453061a2dd7f6095d653875b30733b04cf3f
Kai Wang <charles.wangkai@gmail.com>
2021-09-03 13:33:20 +0800

    Unique Binary Search Trees II

--- unique-binary-search-trees-ii/Solution.java
+++ unique-binary-search-trees-ii/Solution.java
@@ -1,46 +1,48 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-		left = null;
-		right = null;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<TreeNode> generateTrees(int n) {
-		int[] values = new int[n];
-		for (int i = 0; i < values.length; i++) {
-			values[i] = i + 1;
-		}
-		return generateTrees(values, 0, n - 1);
-	}
+class Solution {
+  public List<TreeNode> generateTrees(int n) {
+    return buildTrees(IntStream.rangeClosed(1, n).toArray(), 0, n - 1);
+  }
 
-	List<TreeNode> generateTrees(int[] values, int begin, int end) {
-		List<TreeNode> roots = new ArrayList<TreeNode>();
-		if (begin > end) {
-			roots.add(null);
-		} else {
-			for (int i = begin; i <= end; i++) {
-				List<TreeNode> lefts = generateTrees(values, begin, i - 1);
-				List<TreeNode> rights = generateTrees(values, i + 1, end);
-				for (TreeNode left : lefts) {
-					for (TreeNode right : rights) {
-						TreeNode root = new TreeNode(values[i]);
-						root.left = left;
-						root.right = right;
-						roots.add(root);
-					}
-				}
-			}
-		}
-		return roots;
-	}
+  List<TreeNode> buildTrees(int[] values, int begin, int end) {
+    if (begin > end) {
+      return Collections.singletonList(null);
+    }
+
+    List<TreeNode> roots = new ArrayList<>();
+    for (int i = begin; i <= end; ++i) {
+      List<TreeNode> lefts = buildTrees(values, begin, i - 1);
+      List<TreeNode> rights = buildTrees(values, i + 1, end);
+      for (TreeNode left : lefts) {
+        for (TreeNode right : rights) {
+          roots.add(new TreeNode(values[i], left, right));
+        }
+      }
+    }
+
+    return roots;
+  }
 }
