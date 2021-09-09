c6a01355119ec02f08671f12e07c8a1abe4798cc
Kai Wang <charles.wangkai@gmail.com>
2021-09-09 18:49:49 +0800

    Largest Plus Sign

--- largest-plus-sign/Solution.java
+++ largest-plus-sign/Solution.java
@@ -1,45 +1,46 @@
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+import java.util.Arrays;
 
-	public int orderOfLargestPlusSign(int N, int[][] mines) {
-		char[][] grid = new char[N][N];
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				grid[r][c] = '1';
-			}
-		}
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-		for (int[] mine : mines) {
-			grid[mine[0]][mine[1]] = '0';
-		}
+  public int orderOfLargestPlusSign(int n, int[][] mines) {
+    char[][] grid = new char[n][n];
+    for (int r = 0; r < n; ++r) {
+      Arrays.fill(grid[r], '1');
+    }
+    for (int[] mine : mines) {
+      grid[mine[0]][mine[1]] = '0';
+    }
 
-		int maxOrder = 0;
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				maxOrder = Math.max(maxOrder, getOrder(grid, r, c));
-			}
-		}
-		return maxOrder;
-	}
+    int maxOrder = 0;
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < n; ++c) {
+        maxOrder = Math.max(maxOrder, computeOrder(grid, r, c));
+      }
+    }
 
-	int getOrder(char[][] grid, int r, int c) {
-		for (int order = 1;; order++) {
-			if (!isAllOne(grid, r, c, order)) {
-				return order - 1;
-			}
-		}
-	}
+    return maxOrder;
+  }
 
-	boolean isAllOne(char[][] grid, int r, int c, int order) {
-		int N = grid.length;
-		for (int i = 0; i < R_OFFSETS.length; i++) {
-			int nextR = r + R_OFFSETS[i] * (order - 1);
-			int nextC = c + C_OFFSETS[i] * (order - 1);
-			if (!(nextR >= 0 && nextR < N && nextC >= 0 && nextC < N && grid[nextR][nextC] == '1')) {
-				return false;
-			}
-		}
-		return true;
-	}
+  int computeOrder(char[][] grid, int r, int c) {
+    for (int order = 1; ; ++order) {
+      if (!isAllOne(grid, r, c, order)) {
+        return order - 1;
+      }
+    }
+  }
+
+  boolean isAllOne(char[][] grid, int r, int c, int order) {
+    int n = grid.length;
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int nextR = r + R_OFFSETS[i] * (order - 1);
+      int nextC = c + C_OFFSETS[i] * (order - 1);
+      if (!(nextR >= 0 && nextR < n && nextC >= 0 && nextC < n && grid[nextR][nextC] == '1')) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
