d030ec04ab9a5bdc8f55ffcb5e89ddf9dd859127
Kai Wang <charles.wangkai@gmail.com>
2021-09-11 22:16:16 +0800

    Basic Calculator

--- basic-calculator/Solution.java
+++ basic-calculator/Solution.java
@@ -1,63 +1,64 @@
 import java.util.Stack;
 
-public class Solution {
-	public int calculate(String s) {
-		Stack<Integer> factors = new Stack<Integer>();
-		factors.push(1);
-		int sign = 1;
-		int result = 0;
+class Solution {
+  public int calculate(String s) {
+    Stack<Integer> factors = new Stack<>();
+    factors.push(1);
+    int sign = 1;
+    int result = 0;
 
-		Expression expr = new Expression(s);
-		String token;
-		while ((token = expr.readNext()) != null) {
-			if (token.equals("(")) {
-				factors.push(factors.peek() * sign);
-				sign = 1;
-			} else if (token.equals(")")) {
-				factors.pop();
-			} else if (token.equals("+")) {
-				sign = 1;
-			} else if (token.equals("-")) {
-				sign = -1;
-			} else {
-				result += factors.peek() * sign * Integer.parseInt(token);
-			}
-		}
+    Expression expr = new Expression(s);
+    String token;
+    while ((token = expr.readNext()) != null) {
+      if (token.equals("(")) {
+        factors.push(factors.peek() * sign);
+        sign = 1;
+      } else if (token.equals(")")) {
+        factors.pop();
+      } else if (token.equals("+")) {
+        sign = 1;
+      } else if (token.equals("-")) {
+        sign = -1;
+      } else {
+        result += factors.peek() * sign * Integer.parseInt(token);
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
 
 class Expression {
-	private String str;
-	private int index = 0;
+  private String str;
+  private int index = 0;
 
-	public Expression(String str) {
-		this.str = str;
-	}
+  public Expression(String str) {
+    this.str = str;
+  }
 
-	public String readNext() {
-		while (index < str.length() && str.charAt(index) == ' ') {
-			index++;
-		}
+  public String readNext() {
+    while (index != str.length() && str.charAt(index) == ' ') {
+      ++index;
+    }
 
-		if (index == str.length()) {
-			return null;
-		}
+    if (index == str.length()) {
+      return null;
+    }
 
-		char ch = str.charAt(index);
-		if (ch == '(' || ch == ')' || ch == '+' || ch == '-') {
-			index++;
-			return ch + "";
-		}
+    char ch = str.charAt(index);
+    if (ch == '(' || ch == ')' || ch == '+' || ch == '-') {
+      ++index;
 
-		int nextIndex = index + 1;
-		while (nextIndex < str.length()
-				&& Character.isDigit(str.charAt(nextIndex))) {
-			nextIndex++;
-		}
-		String number = str.substring(index, nextIndex);
-		index = nextIndex;
-		return number;
-	}
+      return String.valueOf(ch);
+    }
+
+    int nextIndex = index;
+    while (nextIndex != str.length() && Character.isDigit(str.charAt(nextIndex))) {
+      ++nextIndex;
+    }
+    String number = str.substring(index, nextIndex);
+    index = nextIndex;
+
+    return number;
+  }
 }
