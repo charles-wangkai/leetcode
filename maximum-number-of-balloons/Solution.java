0c4755df32f3bb1ce390425ed666145f713b2d32
Kai Wang <charles.wangkai@gmail.com>
2021-09-13 22:25:29 +0800

    Maximum Number of Balloons

--- maximum-number-of-balloons/Solution.java
+++ maximum-number-of-balloons/Solution.java
@@ -1,22 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int maxNumberOfBalloons(String text) {
-		Map<Character, Integer> targetLetterToCount = buildLetterToCount("balloon");
-		Map<Character, Integer> letterToCount = buildLetterToCount(text);
+class Solution {
+  public int maxNumberOfBalloons(String text) {
+    Map<Character, Integer> targetLetterToCount = buildLetterToCount("balloon");
+    Map<Character, Integer> letterToCount = buildLetterToCount(text);
 
-		return targetLetterToCount.keySet().stream()
-				.mapToInt(letter -> letterToCount.getOrDefault(letter, 0) / targetLetterToCount.get(letter)).min()
-				.getAsInt();
-	}
+    return targetLetterToCount.keySet().stream()
+        .mapToInt(letter -> letterToCount.getOrDefault(letter, 0) / targetLetterToCount.get(letter))
+        .min()
+        .getAsInt();
+  }
 
-	Map<Character, Integer> buildLetterToCount(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+  Map<Character, Integer> buildLetterToCount(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		return letterToCount;
-	}
+    return letterToCount;
+  }
 }
