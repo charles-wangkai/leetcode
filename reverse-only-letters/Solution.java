12a8857e028e58e2b215ee9353770daa76703dd6
Kai Wang <charles.wangkai@gmail.com>
2021-09-14 21:22:30 +0800

    Reverse Only Letters

--- reverse-only-letters/Solution.java
+++ reverse-only-letters/Solution.java
@@ -1,14 +1,18 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public String reverseOnlyLetters(String S) {
-		StringBuilder reversed = new StringBuilder(S);
-		int[] letterIndices = IntStream.range(0, S.length()).filter(i -> Character.isLetter(S.charAt(i))).toArray();
-		for (int i = 0, j = letterIndices.length - 1; i < j; i++, j--) {
-			char temp = reversed.charAt(letterIndices[i]);
-			reversed.setCharAt(letterIndices[i], reversed.charAt(letterIndices[j]));
-			reversed.setCharAt(letterIndices[j], temp);
-		}
-		return reversed.toString();
-	}
+class Solution {
+  public String reverseOnlyLetters(String s) {
+    char[] letters = s.toCharArray();
+
+    int[] letterIndices =
+        IntStream.range(0, letters.length).filter(i -> Character.isLetter(letters[i])).toArray();
+
+    for (int i = 0, j = letterIndices.length - 1; i < j; ++i, --j) {
+      char temp = letters[letterIndices[i]];
+      letters[letterIndices[i]] = letters[letterIndices[j]];
+      letters[letterIndices[j]] = temp;
+    }
+
+    return new String(letters);
+  }
 }
