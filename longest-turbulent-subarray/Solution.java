8bddc7cf96182d86b9c8f41c46a5fc5986b14df1
Kai Wang <charles.wangkai@gmail.com>
2021-09-15 23:23:31 +0800

    Longest Turbulent Subarray

--- longest-turbulent-subarray/Solution.java
+++ longest-turbulent-subarray/Solution.java
@@ -1,26 +1,26 @@
-public class Solution {
-	public int maxTurbulenceSize(int[] A) {
-		int maxSize = 1;
-		int size = 1;
-		Boolean prevUpOrDown = null;
-		for (int i = 1; i < A.length; i++) {
-			if (prevUpOrDown != null && ((prevUpOrDown && A[i - 1] > A[i]) || (!prevUpOrDown && A[i - 1] < A[i]))) {
-				prevUpOrDown = !prevUpOrDown;
-				size++;
-			} else {
-				if (A[i - 1] > A[i]) {
-					prevUpOrDown = false;
-					size = 2;
-				} else if (A[i - 1] < A[i]) {
-					prevUpOrDown = true;
-					size = 2;
-				} else {
-					prevUpOrDown = null;
-				}
-			}
+class Solution {
+  public int maxTurbulenceSize(int[] arr) {
+    int maxSize = 1;
+    int size = 1;
+    Boolean prevUpOrDown = null;
+    for (int i = 1; i < arr.length; ++i) {
+      if (prevUpOrDown != null
+          && ((prevUpOrDown && arr[i - 1] > arr[i]) || (!prevUpOrDown && arr[i - 1] < arr[i]))) {
+        prevUpOrDown = !prevUpOrDown;
+        ++size;
+      } else if (arr[i - 1] > arr[i]) {
+        prevUpOrDown = false;
+        size = 2;
+      } else if (arr[i - 1] < arr[i]) {
+        prevUpOrDown = true;
+        size = 2;
+      } else {
+        prevUpOrDown = null;
+      }
 
-			maxSize = Math.max(maxSize, size);
-		}
-		return maxSize;
-	}
+      maxSize = Math.max(maxSize, size);
+    }
+
+    return maxSize;
+  }
 }
