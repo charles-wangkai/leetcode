a1f244cffc6ad008803b96417470e2fdd7d9d3ed
Kai Wang <charles.wangkai@gmail.com>
2021-09-18 23:14:43 +0800

    Expression Add Operators

--- expression-add-operators/Solution.java
+++ expression-add-operators/Solution.java
@@ -1,72 +1,74 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Solution {
-	public List<String> addOperators(String num, int target) {
-		return buildNumbersList(num).stream().map(numbers -> search(numbers, 0, target, true))
-				.collect(ArrayList<String>::new, List::addAll, List::addAll);
-	}
+class Solution {
+  public List<String> addOperators(String num, int target) {
+    return buildNumbersList(num).stream()
+        .flatMap(numbers -> search(numbers, 0, target, true).stream())
+        .collect(Collectors.toList());
+  }
 
-	List<List<Long>> buildNumbersList(String num) {
-		List<List<Long>> numbersList = new ArrayList<List<Long>>();
-		split(numbersList, num, 0, new ArrayList<Long>());
-		return numbersList;
-	}
+  List<List<Long>> buildNumbersList(String num) {
+    List<List<Long>> numbersList = new ArrayList<>();
+    split(numbersList, num, 0, new ArrayList<Long>());
 
-	void split(List<List<Long>> numbersList, String num, int startIndex, List<Long> numbers) {
-		if (startIndex == num.length()) {
-			numbersList.add(new ArrayList<Long>(numbers));
-			return;
-		}
+    return numbersList;
+  }
 
-		long number = 0;
-		for (int i = startIndex; i < num.length(); i++) {
-			number = number * 10 + (num.charAt(i) - '0');
+  void split(List<List<Long>> numbersList, String num, int startIndex, List<Long> numbers) {
+    if (startIndex == num.length()) {
+      numbersList.add(List.copyOf(numbers));
 
-			numbers.add(number);
-			split(numbersList, num, i + 1, numbers);
-			numbers.remove(numbers.size() - 1);
+      return;
+    }
 
-			if (number == 0) {
-				break;
-			}
-		}
-	}
+    long number = 0;
+    for (int i = startIndex; i < num.length(); ++i) {
+      number = number * 10 + (num.charAt(i) - '0');
 
-	List<String> search(List<Long> numbers, int startIndex, long target, boolean first) {
-		List<String> result = new ArrayList<String>();
+      numbers.add(number);
+      split(numbersList, num, i + 1, numbers);
+      numbers.remove(numbers.size() - 1);
 
-		StringBuilder term = new StringBuilder();
-		long product = 1;
-		for (int i = startIndex; i < numbers.size(); i++) {
-			long number = numbers.get(i);
-			term.append(term.length() == 0 ? "" : "*").append(number);
-			product *= number;
+      if (number == 0) {
+        break;
+      }
+    }
+  }
 
-			if (i == numbers.size() - 1) {
-				if (product == target) {
-					result.add((first ? "" : "+") + term.toString());
-				}
-				if (!first && product == -target) {
-					result.add("-" + term.toString());
-				}
-			} else {
-				result.addAll(
-						combine((first ? "" : "+") + term.toString(), search(numbers, i + 1, target - product, false)));
-				if (!first) {
-					result.addAll(combine("-" + term.toString(), search(numbers, i + 1, target + product, false)));
-				}
-			}
-		}
+  List<String> search(List<Long> numbers, int startIndex, long target, boolean first) {
+    List<String> result = new ArrayList<>();
 
-		return result;
-	}
+    StringBuilder term = new StringBuilder();
+    long product = 1;
+    for (int i = startIndex; i < numbers.size(); ++i) {
+      term.append((term.length() == 0) ? "" : "*").append(numbers.get(i));
+      product *= numbers.get(i);
 
-	List<String> combine(String leftPart, List<String> rightParts) {
-		List<String> combined = new ArrayList<String>();
-		for (String rightPart : rightParts) {
-			combined.add(leftPart + rightPart);
-		}
-		return combined;
-	}
+      if (i == numbers.size() - 1) {
+        if (product == target) {
+          result.add((first ? "" : "+") + term.toString());
+        }
+        if (!first && product == -target) {
+          result.add("-" + term.toString());
+        }
+      } else {
+        result.addAll(
+            combine(
+                (first ? "" : "+") + term.toString(),
+                search(numbers, i + 1, target - product, false)));
+        if (!first) {
+          result.addAll(
+              combine("-" + term.toString(), search(numbers, i + 1, target + product, false)));
+        }
+      }
+    }
+
+    return result;
+  }
+
+  List<String> combine(String leftPart, List<String> rightParts) {
+    return rightParts.stream().map(rightPart -> leftPart + rightPart).collect(Collectors.toList());
+  }
 }
