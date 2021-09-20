f70e62ad0fb5a6ab5b3905677c864cb055853737
Kai Wang <charles.wangkai@gmail.com>
2021-09-20 23:09:04 +0800

    Find Winner on a Tic Tac Toe Game

--- find-winner-on-a-tic-tac-toe-game/Solution.java
+++ find-winner-on-a-tic-tac-toe-game/Solution.java
@@ -1,33 +1,36 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	static final int SIZE = 3;
+class Solution {
+  static final int SIZE = 3;
 
-	public String tictactoe(int[][] moves) {
-		char[][] grid = new char[SIZE][SIZE];
-		for (int i = 0; i < moves.length; i++) {
-			grid[moves[i][0]][moves[i][1]] = (i % 2 == 0) ? 'X' : 'O';
-		}
+  public String tictactoe(int[][] moves) {
+    char[][] grid = new char[SIZE][SIZE];
+    for (int i = 0; i < moves.length; ++i) {
+      grid[moves[i][0]][moves[i][1]] = (i % 2 == 0) ? 'X' : 'O';
+    }
 
-		if (isWin(grid, 'X')) {
-			return "A";
-		} else if (isWin(grid, 'O')) {
-			return "B";
-		} else if (isFinished(grid)) {
-			return "Draw";
-		} else {
-			return "Pending";
-		}
-	}
+    if (isWin(grid, 'X')) {
+      return "A";
+    } else if (isWin(grid, 'O')) {
+      return "B";
+    } else if (isFinished(grid)) {
+      return "Draw";
+    }
 
-	boolean isWin(char[][] grid, char target) {
-		return IntStream.range(0, SIZE).anyMatch(r -> IntStream.range(0, SIZE).allMatch(c -> grid[r][c] == target))
-				|| IntStream.range(0, SIZE).anyMatch(c -> IntStream.range(0, SIZE).allMatch(r -> grid[r][c] == target))
-				|| IntStream.range(0, SIZE).allMatch(r -> grid[r][r] == target)
-				|| IntStream.range(0, SIZE).allMatch(r -> grid[r][SIZE - 1 - r] == target);
-	}
+    return "Pending";
+  }
 
-	boolean isFinished(char[][] grid) {
-		return IntStream.range(0, SIZE).allMatch(r -> IntStream.range(0, SIZE).allMatch(c -> grid[r][c] != 0));
-	}
+  boolean isWin(char[][] grid, char target) {
+    return IntStream.range(0, SIZE)
+            .anyMatch(r -> IntStream.range(0, SIZE).allMatch(c -> grid[r][c] == target))
+        || IntStream.range(0, SIZE)
+            .anyMatch(c -> IntStream.range(0, SIZE).allMatch(r -> grid[r][c] == target))
+        || IntStream.range(0, SIZE).allMatch(r -> grid[r][r] == target)
+        || IntStream.range(0, SIZE).allMatch(r -> grid[r][SIZE - 1 - r] == target);
+  }
+
+  boolean isFinished(char[][] grid) {
+    return IntStream.range(0, SIZE)
+        .allMatch(r -> IntStream.range(0, SIZE).allMatch(c -> grid[r][c] != 0));
+  }
 }
