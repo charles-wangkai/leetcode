b4746bea09aa25b47b6c95fd0b0a80c7e9500898
Kai Wang <charles.wangkai@gmail.com>
2021-09-22 22:00:51 +0800

    Maximum Length of a Concatenated String with Unique Characters

--- maximum-length-of-a-concatenated-string-with-unique-characters/Solution.java
+++ maximum-length-of-a-concatenated-string-with-unique-characters/Solution.java
@@ -1,30 +1,30 @@
 import java.util.List;
 
-public class Solution {
-	public int maxLength(List<String> arr) {
-		return search(arr, new boolean[26], 0, 0);
-	}
+class Solution {
+  public int maxLength(List<String> arr) {
+    return search(arr, new boolean[26], 0, 0);
+  }
 
-	int search(List<String> arr, boolean[] used, int index, int length) {
-		if (index == arr.size()) {
-			return length;
-		}
+  int search(List<String> arr, boolean[] used, int index, int length) {
+    if (index == arr.size()) {
+      return length;
+    }
 
-		int result = search(arr, used, index + 1, length);
+    int result = search(arr, used, index + 1, length);
 
-		String s = arr.get(index);
-		if (s.chars().distinct().count() == s.length() && s.chars().allMatch(ch -> !used[ch - 'a'])) {
-			for (char ch : s.toCharArray()) {
-				used[ch - 'a'] = true;
-			}
+    String s = arr.get(index);
+    if (s.chars().distinct().count() == s.length() && s.chars().allMatch(ch -> !used[ch - 'a'])) {
+      for (char ch : s.toCharArray()) {
+        used[ch - 'a'] = true;
+      }
 
-			result = Math.max(result, search(arr, used, index + 1, length + s.length()));
+      result = Math.max(result, search(arr, used, index + 1, length + s.length()));
 
-			for (char ch : s.toCharArray()) {
-				used[ch - 'a'] = false;
-			}
-		}
+      for (char ch : s.toCharArray()) {
+        used[ch - 'a'] = false;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
