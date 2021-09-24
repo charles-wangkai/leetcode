d67fb113bee338d8976f86ef791522600d952ea1
Kai Wang <charles.wangkai@gmail.com>
2021-09-25 00:29:23 +0800

    N-th Tribonacci Number

--- n-th-tribonacci-number/Solution.java
+++ n-th-tribonacci-number/Solution.java
@@ -1,20 +1,20 @@
-public class Solution {
-	public int tribonacci(int n) {
-		if (n <= 1) {
-			return n;
-		}
+class Solution {
+  public int tribonacci(int n) {
+    if (n <= 1) {
+      return n;
+    }
 
-		int prevPrev = 0;
-		int prev = 1;
-		int curr = 1;
-		for (int i = 0; i < n - 2; i++) {
-			int next = prevPrev + prev + curr;
+    int prevPrev = 0;
+    int prev = 1;
+    int curr = 1;
+    for (int i = 0; i < n - 2; ++i) {
+      int next = prevPrev + prev + curr;
 
-			prevPrev = prev;
-			prev = curr;
-			curr = next;
-		}
+      prevPrev = prev;
+      prev = curr;
+      curr = next;
+    }
 
-		return curr;
-	}
+    return curr;
+  }
 }
