c0bde10d291cf202379a2247219934697c1caddc
Kai Wang <charles.wangkai@gmail.com>
2021-09-28 22:58:22 +0800

    Sort Array By Parity II

--- sort-array-by-parity-ii/Solution.java
+++ sort-array-by-parity-ii/Solution.java
@@ -1,17 +1,18 @@
-public class Solution {
-	public int[] sortArrayByParityII(int[] A) {
-		int[] result = new int[A.length];
-		int evenIndex = 0;
-		int oddIndex = 1;
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] % 2 == 0) {
-				result[evenIndex] = A[i];
-				evenIndex += 2;
-			} else {
-				result[oddIndex] = A[i];
-				oddIndex += 2;
-			}
-		}
-		return result;
-	}
+class Solution {
+  public int[] sortArrayByParityII(int[] nums) {
+    int[] result = new int[nums.length];
+    int evenIndex = 0;
+    int oddIndex = 1;
+    for (int num : nums) {
+      if (num % 2 == 0) {
+        result[evenIndex] = num;
+        evenIndex += 2;
+      } else {
+        result[oddIndex] = num;
+        oddIndex += 2;
+      }
+    }
+
+    return result;
+  }
 }
