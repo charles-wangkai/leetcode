80a594206b165950187e8ba183632eed2c249728
Kai Wang <charles.wangkai@gmail.com>
2021-09-29 16:35:27 +0800

    Split Linked List in Parts

--- split-linked-list-in-parts/Solution.java
+++ split-linked-list-in-parts/Solution.java
@@ -1,46 +1,56 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode[] splitListToParts(ListNode root, int k) {
-		int length = getLength(root);
-		int averageLength = length / k;
-		int extraPartNum = length % k;
+class Solution {
+  public ListNode[] splitListToParts(ListNode head, int k) {
+    int length = getLength(head);
+    int averageLength = length / k;
+    int extraPartNum = length % k;
 
-		ListNode[] parts = new ListNode[k];
-		for (int i = 0; i < parts.length; i++) {
-			ListNode tempHead = new ListNode(0);
-			root = retrieve(root, tempHead, averageLength + (i < extraPartNum ? 1 : 0));
-			parts[i] = tempHead.next;
-		}
-		return parts;
-	}
+    ListNode[] parts = new ListNode[k];
+    for (int i = 0; i < parts.length; ++i) {
+      ListNode tempHead = new ListNode();
+      head = retrieve(head, tempHead, averageLength + ((i < extraPartNum) ? 1 : 0));
+      parts[i] = tempHead.next;
+    }
 
-	ListNode retrieve(ListNode root, ListNode tempHead, int partLength) {
-		ListNode prev = tempHead;
-		for (int i = 0; i < partLength; i++) {
-			ListNode node = root;
-			root = root.next;
-			node.next = null;
-			prev.next = node;
-			prev = node;
-		}
-		return root;
-	}
+    return parts;
+  }
 
-	int getLength(ListNode root) {
-		int length = 0;
-		while (root != null) {
-			root = root.next;
-			length++;
-		}
-		return length;
-	}
+  ListNode retrieve(ListNode head, ListNode tempHead, int partLength) {
+    ListNode prev = tempHead;
+    for (int i = 0; i < partLength; ++i) {
+      ListNode node = head;
+      head = head.next;
+      node.next = null;
+      prev.next = node;
+      prev = node;
+    }
+
+    return head;
+  }
+
+  int getLength(ListNode head) {
+    int length = 0;
+    while (head != null) {
+      ++length;
+      head = head.next;
+    }
+
+    return length;
+  }
 }
