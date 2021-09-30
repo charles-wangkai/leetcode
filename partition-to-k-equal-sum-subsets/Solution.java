aa0acbae12ffefc2a30c4ed28768d4cc5e83e2eb
Kai Wang <charles.wangkai@gmail.com>
2021-09-30 15:11:15 +0800

    Partition to K Equal Sum Subsets

--- partition-to-k-equal-sum-subsets/Solution.java
+++ partition-to-k-equal-sum-subsets/Solution.java
@@ -1,46 +1,44 @@
 import java.util.Arrays;
 
-public class Solution {
-	public boolean canPartitionKSubsets(int[] nums, int k) {
-		int sum = Arrays.stream(nums).sum();
-		if (sum % k != 0) {
-			return false;
-		}
+class Solution {
+  public boolean canPartitionKSubsets(int[] nums, int k) {
+    int sum = Arrays.stream(nums).sum();
+    if (sum % k != 0) {
+      return false;
+    }
 
-		int subsetSum = sum / k;
-		if (Arrays.stream(nums).anyMatch(num -> num > subsetSum)) {
-			return false;
-		}
+    int subsetSum = sum / k;
+    if (Arrays.stream(nums).anyMatch(num -> num > subsetSum)) {
+      return false;
+    }
 
-		Arrays.sort(nums);
-		return search(nums, subsetSum, new boolean[nums.length], nums.length - 1, k, subsetSum);
-	}
+    Arrays.sort(nums);
 
-	boolean search(int[] nums, int subsetSum, boolean[] used, int startIndex, int remainSubset, int remainSum) {
-		if (startIndex < 0) {
-			return false;
-		}
-		if (remainSubset == 0) {
-			return true;
-		}
+    return search(nums, subsetSum, new boolean[nums.length], nums.length - 1, k, subsetSum);
+  }
 
-		for (int i = startIndex; i >= 0; i--) {
-			if (!used[i] && nums[i] <= remainSum) {
-				used[i] = true;
+  boolean search(
+      int[] nums, int subsetSum, boolean[] used, int startIndex, int remainSubset, int remainSum) {
+    if (remainSubset == 0) {
+      return true;
+    }
 
-				if (nums[i] == remainSum) {
-					if (search(nums, subsetSum, used, nums.length - 1, remainSubset - 1, subsetSum)) {
-						return true;
-					}
-				} else {
-					if (search(nums, subsetSum, used, i - 1, remainSubset, remainSum - nums[i])) {
-						return true;
-					}
-				}
+    for (int i = startIndex; i >= 0; --i) {
+      if (!used[i] && nums[i] <= remainSum) {
+        used[i] = true;
 
-				used[i] = false;
-			}
-		}
-		return false;
-	}
+        if (nums[i] == remainSum) {
+          if (search(nums, subsetSum, used, nums.length - 1, remainSubset - 1, subsetSum)) {
+            return true;
+          }
+        } else if (search(nums, subsetSum, used, i - 1, remainSubset, remainSum - nums[i])) {
+          return true;
+        }
+
+        used[i] = false;
+      }
+    }
+
+    return false;
+  }
 }
