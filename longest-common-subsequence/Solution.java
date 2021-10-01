06562e28739021dc744e3c4e137f5af7a3a1ea95
Kai Wang <charles.wangkai@gmail.com>
2021-10-01 20:47:21 +0800

    Longest Common Subsequence

--- longest-common-subsequence/Solution.java
+++ longest-common-subsequence/Solution.java
@@ -1,21 +1,18 @@
-public class Solution {
-	public int longestCommonSubsequence(String text1, String text2) {
-		int length1 = text1.length();
-		int length2 = text2.length();
+class Solution {
+  public int longestCommonSubsequence(String text1, String text2) {
+    int[][] maxLengths = new int[text1.length() + 1][text2.length() + 1];
+    for (int i = 0; i <= text1.length(); ++i) {
+      for (int j = 0; j <= text2.length(); ++j) {
+        if (i != 0 && j != 0) {
+          if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+            maxLengths[i][j] = maxLengths[i - 1][j - 1] + 1;
+          } else {
+            maxLengths[i][j] = Math.max(maxLengths[i - 1][j], maxLengths[i][j - 1]);
+          }
+        }
+      }
+    }
 
-		int[][] maxLengths = new int[length1 + 1][length2 + 1];
-		for (int i = 0; i <= length1; i++) {
-			for (int j = 0; j <= length2; j++) {
-				if (i != 0 && j != 0) {
-					if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-						maxLengths[i][j] = maxLengths[i - 1][j - 1] + 1;
-					} else {
-						maxLengths[i][j] = Math.max(maxLengths[i - 1][j], maxLengths[i][j - 1]);
-					}
-				}
-			}
-		}
-
-		return maxLengths[length1][length2];
-	}
+    return maxLengths[text1.length()][text2.length()];
+  }
 }
