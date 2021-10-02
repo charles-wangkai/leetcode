56a6fb42bea6d81823fb637deee47d32f6642153
Kai Wang <charles.wangkai@gmail.com>
2021-10-02 23:05:15 +0800

    Dungeon Game

--- dungeon-game/Solution.java
+++ dungeon-game/Solution.java
@@ -1,21 +1,22 @@
-public class Solution {
-	public int calculateMinimumHP(int[][] dungeon) {
-		int row = dungeon.length;
-		int col = dungeon[0].length;
-		int[][] minHPs = new int[row][col];
-		for (int i = row - 1; i >= 0; --i) {
-			for (int j = col - 1; j >= 0; --j) {
-				if (i == row - 1 && j == col - 1) {
-					minHPs[i][j] = Math.max(1, -dungeon[i][j] + 1);
-				} else {
-					int right = (j == col - 1) ? Integer.MAX_VALUE : minHPs[i][j + 1];
-					int down = (i == row - 1) ? Integer.MAX_VALUE : minHPs[i + 1][j];
-					int minNext = Math.min(right, down);
-					minHPs[i][j] = Math.max(1, minNext - dungeon[i][j]);
-				}
-			}
-		}
+class Solution {
+  public int calculateMinimumHP(int[][] dungeon) {
+    int m = dungeon.length;
+    int n = dungeon[0].length;
 
-		return minHPs[0][0];
-	}
+    int[][] minHPs = new int[m][n];
+    for (int i = m - 1; i >= 0; --i) {
+      for (int j = n - 1; j >= 0; --j) {
+        if (i == m - 1 && j == n - 1) {
+          minHPs[i][j] = Math.max(1, -dungeon[i][j] + 1);
+        } else {
+          int right = (j == n - 1) ? Integer.MAX_VALUE : minHPs[i][j + 1];
+          int down = (i == m - 1) ? Integer.MAX_VALUE : minHPs[i + 1][j];
+          int minNext = Math.min(right, down);
+          minHPs[i][j] = Math.max(1, minNext - dungeon[i][j]);
+        }
+      }
+    }
+
+    return minHPs[0][0];
+  }
 }
