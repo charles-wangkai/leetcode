8788f22e6bacf07f9a8376f0efa0b8eb877ef7aa
Kai Wang <charles.wangkai@gmail.com>
2021-10-05 10:03:02 +0800

    Climbing Stairs

--- climbing-stairs/Solution.java
+++ climbing-stairs/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-	public int climbStairs(int n) {
-		if (n <= 2) {
-			return n;
-		}
+  public int climbStairs(int n) {
+    if (n <= 2) {
+      return n;
+    }
 
-		int prev = 1;
-		int curr = 2;
-		for (int i = 3; i <= n; ++i) {
-			int next = prev + curr;
+    int prev = 1;
+    int curr = 2;
+    for (int i = 3; i <= n; ++i) {
+      int next = prev + curr;
 
-			prev = curr;
-			curr = next;
-		}
+      prev = curr;
+      curr = next;
+    }
 
-		return curr;
-	}
+    return curr;
+  }
 }
