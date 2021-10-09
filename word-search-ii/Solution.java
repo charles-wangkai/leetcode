3fc5d05f3a9ca31e25a90714de61afd06966609f
Kai Wang <charles.wangkai@gmail.com>
2021-10-09 23:18:14 +0800

    Word Search II

--- word-search-ii/Solution.java
+++ word-search-ii/Solution.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -7,84 +5,84 @@ import java.util.Map;
 import java.util.Set;
 
 class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public List<String> findWords(char[][] board, String[] words) {
-		int row = board.length;
-		if (row == 0) {
-			return Collections.emptyList();
-		}
-		int col = board[0].length;
-		if (col == 0) {
-			return Collections.emptyList();
-		}
+  public List<String> findWords(char[][] board, String[] words) {
+    int m = board.length;
+    int n = board[0].length;
 
-		Trie trie = new Trie();
-		for (String word : words) {
-			insert(trie, word);
-		}
+    Trie trie = new Trie();
+    for (String word : words) {
+      insert(trie, word);
+    }
 
-		Set<String> foundWords = new HashSet<>();
-		boolean[][] visited = new boolean[row][col];
-		for (int r = 0; r < row; ++r) {
-			for (int c = 0; c < col; ++c) {
-				search(board, foundWords, trie, visited, r, c, new StringBuilder());
-			}
-		}
+    Set<String> foundWords = new HashSet<>();
+    boolean[][] visited = new boolean[m][n];
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        search(board, foundWords, trie, visited, r, c, new StringBuilder());
+      }
+    }
 
-		return new ArrayList<>(foundWords);
-	}
+    return List.copyOf(foundWords);
+  }
 
-	void search(char[][] board, Set<String> foundWords, Trie node, boolean[][] visited, int r, int c,
-			StringBuilder current) {
-		int row = board.length;
-		int col = board[0].length;
+  void search(
+      char[][] board,
+      Set<String> foundWords,
+      Trie node,
+      boolean[][] visited,
+      int r,
+      int c,
+      StringBuilder current) {
+    int m = board.length;
+    int n = board[0].length;
 
-		if (!(r >= 0 && r < row && c >= 0 && c < col) || visited[r][c] || !node.hasChild(board[r][c])) {
-			return;
-		}
+    if (!(r >= 0 && r < m && c >= 0 && c < n) || visited[r][c] || !node.hasChild(board[r][c])) {
+      return;
+    }
 
-		Trie child = node.getChild(board[r][c]);
-		current.append(board[r][c]);
+    Trie child = node.getChild(board[r][c]);
+    current.append(board[r][c]);
 
-		if (child.hasChild(null)) {
-			foundWords.add(current.toString());
-		}
+    if (child.hasChild(null)) {
+      foundWords.add(current.toString());
+    }
 
-		visited[r][c] = true;
-		for (int i = 0; i < R_OFFSETS.length; ++i) {
-			search(board, foundWords, child, visited, r + R_OFFSETS[i], c + C_OFFSETS[i], current);
-		}
-		visited[r][c] = false;
+    visited[r][c] = true;
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      search(board, foundWords, child, visited, r + R_OFFSETS[i], c + C_OFFSETS[i], current);
+    }
+    visited[r][c] = false;
 
-		current.deleteCharAt(current.length() - 1);
-	}
+    current.deleteCharAt(current.length() - 1);
+  }
 
-	void insert(Trie node, String word) {
-		for (int i = 0; i < word.length(); ++i) {
-			node.addChild(word.charAt(i));
-			node = node.getChild(word.charAt(i));
-		}
+  void insert(Trie node, String word) {
+    for (int i = 0; i < word.length(); ++i) {
+      node.addChild(word.charAt(i));
+      node = node.getChild(word.charAt(i));
+    }
 
-		node.addChild(null);
-	}
+    node.addChild(null);
+  }
 }
 
 class Trie {
-	private Map<Character, Trie> children = new HashMap<>();
+  private Map<Character, Trie> letterToChild = new HashMap<>();
 
-	public boolean hasChild(Character ch) {
-		return children.containsKey(ch);
-	}
+  public boolean hasChild(Character letter) {
+    return letterToChild.containsKey(letter);
+  }
 
-	public Trie getChild(Character ch) {
-		return children.get(ch);
-	}
+  public Trie getChild(Character letter) {
+    return letterToChild.get(letter);
+  }
 
-	public void addChild(Character ch) {
-		if (!hasChild(ch)) {
-			children.put(ch, new Trie());
-		}
-	}
+  public void addChild(Character letter) {
+    if (!hasChild(letter)) {
+      letterToChild.put(letter, new Trie());
+    }
+  }
 }
