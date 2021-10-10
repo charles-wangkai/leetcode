132112dee1512e96041a703e1f5a58042f7e447b
Kai Wang <charles.wangkai@gmail.com>
2021-10-10 15:08:40 +0800

    Bitwise AND of Numbers Range

--- bitwise-and-of-numbers-range/Solution.java
+++ bitwise-and-of-numbers-range/Solution.java
@@ -1,17 +1,17 @@
-public class Solution {
-	public int rangeBitwiseAnd(int m, int n) {
-		int base = 1;
-		int result = 0;
-		while (n != 0) {
-			if (m == n && (m & 1) != 0) {
-				result += base;
-			}
+class Solution {
+  public int rangeBitwiseAnd(int left, int right) {
+    int base = 1;
+    int result = 0;
+    while (right != 0) {
+      if (left == right && (left & 1) != 0) {
+        result += base;
+      }
 
-			m >>= 1;
-			n >>= 1;
-			base <<= 1;
-		}
+      left >>= 1;
+      right >>= 1;
+      base <<= 1;
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
