f4ecf97b0a360e7143b7ac094938602e8201fb9c
Kai Wang <charles.wangkai@gmail.com>
2021-10-10 23:14:48 +0800

    Two Out of Three

---
+++ two-out-of-three/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+  public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int[] nums : new int[][] {nums1, nums2, nums3}) {
+      for (int num : Arrays.stream(nums).distinct().toArray()) {
+        valueToCount.put(num, valueToCount.getOrDefault(num, 0) + 1);
+      }
+    }
+
+    return valueToCount.keySet().stream()
+        .filter(value -> valueToCount.get(value) >= 2)
+        .collect(Collectors.toList());
+  }
+}
