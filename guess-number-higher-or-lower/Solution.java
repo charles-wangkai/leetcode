57cda1f309b638ee0d6de463fa78c983c9c99aab
Kai Wang <charles.wangkai@gmail.com>
2021-10-12 22:19:32 +0800

    Guess Number Higher or Lower

--- guess-number-higher-or-lower/Solution.java
+++ guess-number-higher-or-lower/Solution.java
@@ -1,28 +1,31 @@
-/* The guess API is defined in the parent class GuessGame.
-   @param num, your guess
-   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
-      int guess(int num); */
+// Forward declaration of guess API.
+// @param  num   your guess
+// @return 	     -1 if num is lower than the guess number
+//			      1 if num is higher than the guess number
+//               otherwise return 0
+// int guess(int num);
 
 class GuessGame {
-	int guess(int num) {
-		return -1;
-	}
+  int guess(int num) {
+    throw new UnsupportedOperationException();
+  }
 }
 
 public class Solution extends GuessGame {
-	public int guessNumber(int n) {
-		int lower = 1;
-		int upper = n;
-		while (true) {
-			int middle = (int) (((long) lower + upper) / 2);
-			int result = guess(middle);
-			if (result == 0) {
-				return middle;
-			} else if (result < 0) {
-				upper = middle - 1;
-			} else {
-				lower = middle + 1;
-			}
-		}
-	}
+  public int guessNumber(int n) {
+    int lower = 1;
+    int upper = n;
+    while (true) {
+      int middle = lower + (upper - lower) / 2;
+
+      int resp = guess(middle);
+      if (resp < 0) {
+        upper = middle - 1;
+      } else if (resp > 0) {
+        lower = middle + 1;
+      } else {
+        return middle;
+      }
+    }
+  }
 }
