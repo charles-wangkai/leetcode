cd52f1f0477419cd4a35e6b4e4974c4cdd3eb9c3
Kai Wang <charles.wangkai@gmail.com>
2021-10-13 21:59:47 +0800

    Construct Binary Search Tree from Preorder Traversal

--- construct-binary-search-tree-from-preorder-traversal/Solution.java
+++ construct-binary-search-tree-from-preorder-traversal/Solution.java
@@ -2,31 +2,37 @@ import java.util.Arrays;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode bstFromPreorder(int[] preorder) {
-		if (preorder.length == 0) {
-			return null;
-		}
+class Solution {
+  public TreeNode bstFromPreorder(int[] preorder) {
+    if (preorder.length == 0) {
+      return null;
+    }
 
-		TreeNode node = new TreeNode(preorder[0]);
+    int endIndex = 1;
+    while (endIndex != preorder.length && preorder[endIndex] < preorder[0]) {
+      ++endIndex;
+    }
 
-		int endIndex = 1;
-		while (endIndex < preorder.length && preorder[endIndex] < preorder[0]) {
-			++endIndex;
-		}
-
-		node.left = bstFromPreorder(Arrays.copyOfRange(preorder, 1, endIndex));
-		node.right = bstFromPreorder(Arrays.copyOfRange(preorder, endIndex, preorder.length));
-
-		return node;
-	}
+    return new TreeNode(
+        preorder[0],
+        bstFromPreorder(Arrays.copyOfRange(preorder, 1, endIndex)),
+        bstFromPreorder(Arrays.copyOfRange(preorder, endIndex, preorder.length)));
+  }
 }
