3aae27a9cab4a1d3e1f08cfbcccf63c5dbe5c4b5
Kai Wang <charles.wangkai@gmail.com>
2021-10-14 23:37:29 +0800

    Perfect Squares

--- perfect-squares/Solution.java
+++ perfect-squares/Solution.java
@@ -1,55 +1,55 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	Map<Integer, Integer> cache = new HashMap<>();
+class Solution {
+  Map<Integer, Integer> cache = new HashMap<>();
 
-	public int numSquares(int n) {
-		return search(n, computeResultLimit(n));
-	}
+  public int numSquares(int n) {
+    return search(n, computeSquareNumLimit(n));
+  }
 
-	int computeResultLimit(int n) {
-		int resultLimit = 0;
-		while (n != 0) {
-			int root = (int) Math.round(Math.sqrt(n));
-			if (root * root > n) {
-				--root;
-			}
-			n -= root * root;
-			++resultLimit;
-		}
+  int computeSquareNumLimit(int n) {
+    int squareNumLimit = 0;
+    while (n != 0) {
+      int root = (int) Math.round(Math.sqrt(n));
+      if (root * root > n) {
+        --root;
+      }
+      n -= root * root;
+      ++squareNumLimit;
+    }
 
-		return resultLimit;
-	}
+    return squareNumLimit;
+  }
 
-	Integer search(int n, int resultLimit) {
-		if (n == 0) {
-			return 0;
-		}
-		if (resultLimit == 0) {
-			return null;
-		}
+  Integer search(int n, int squareNumLimit) {
+    if (n == 0) {
+      return 0;
+    }
+    if (squareNumLimit == 0) {
+      return null;
+    }
 
-		if (cache.containsKey(n)) {
-			int result = cache.get(n);
+    if (cache.containsKey(n)) {
+      int result = cache.get(n);
 
-			return (resultLimit >= result) ? result : null;
-		}
+      return (result <= squareNumLimit) ? result : null;
+    }
 
-		Integer result = null;
-		for (int i = 1; i * i <= n; ++i) {
-			Integer subResult = search(n - i * i, resultLimit - 1);
+    Integer result = null;
+    for (int i = 1; i * i <= n; ++i) {
+      Integer subResult = search(n - i * i, squareNumLimit - 1);
 
-			if (subResult != null && (result == null || subResult + 1 < result)) {
-				result = subResult + 1;
-				resultLimit = subResult;
-			}
-		}
+      if (subResult != null && (result == null || subResult + 1 < result)) {
+        result = subResult + 1;
+        squareNumLimit = subResult;
+      }
+    }
 
-		if (result != null) {
-			cache.put(n, result);
-		}
+    if (result != null) {
+      cache.put(n, result);
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
