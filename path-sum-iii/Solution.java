812339defc445b4dba4a08c6c41e8e06f3c80d63
Kai Wang <charles.wangkai@gmail.com>
2021-10-17 22:55:05 +0800

    Path Sum III

--- path-sum-iii/Solution.java
+++ path-sum-iii/Solution.java
@@ -3,44 +3,45 @@ import java.util.Map;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public int pathSum(TreeNode root, int sum) {
-		Map<Integer, Integer> sumToCount = new HashMap<>();
-		sumToCount.put(0, 1);
+  public int pathSum(TreeNode root, int targetSum) {
+    Map<Integer, Integer> sumToCount = new HashMap<>();
+    sumToCount.put(0, 1);
 
-		return search(sum, sumToCount, 0, root);
-	}
+    return search(targetSum, sumToCount, 0, root);
+  }
 
-	int search(int sum, Map<Integer, Integer> sumToCount, int prev, TreeNode node) {
-		if (node == null) {
-			return 0;
-		}
+  int search(int targetSum, Map<Integer, Integer> sumToCount, int prev, TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
 
-		int current = prev + node.val;
-		int result = sumToCount.getOrDefault(current - sum, 0);
+    int current = prev + node.val;
+    int result = sumToCount.getOrDefault(current - targetSum, 0);
 
-		sumToCount.put(current, sumToCount.getOrDefault(current, 0) + 1);
-		result += search(sum, sumToCount, current, node.left) + search(sum, sumToCount, current, node.right);
-		sumToCount.put(current, sumToCount.get(current) - 1);
+    sumToCount.put(current, sumToCount.getOrDefault(current, 0) + 1);
+    result +=
+        search(targetSum, sumToCount, current, node.left)
+            + search(targetSum, sumToCount, current, node.right);
+    sumToCount.put(current, sumToCount.get(current) - 1);
 
-		return result;
-	}
+    return result;
+  }
 }
