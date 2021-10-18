d8cecb11ee68429db3f7ff6d080c022593a726d9
Kai Wang <charles.wangkai@gmail.com>
2021-10-19 00:13:06 +0800

    Cousins in Binary Tree

--- cousins-in-binary-tree/Solution.java
+++ cousins-in-binary-tree/Solution.java
@@ -1,50 +1,54 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public boolean isCousins(TreeNode root, int x, int y) {
-		Location locationX = search(root, x, 0);
-		Location locationY = search(root, y, 0);
+class Solution {
+  public boolean isCousins(TreeNode root, int x, int y) {
+    Location locationX = search(root, null, x, 0);
+    Location locationY = search(root, null, y, 0);
 
-		return locationX.depth == locationY.depth && locationX.parent != locationY.parent;
-	}
+    return locationX.depth == locationY.depth && locationX.parent != locationY.parent;
+  }
 
-	Location search(TreeNode node, int target, int depth) {
-		if (node == null) {
-			return null;
-		}
+  Location search(TreeNode node, TreeNode parent, int target, int depth) {
+    if (node == null) {
+      return null;
+    }
 
-		if (node.val == target) {
-			return new Location(depth, null);
-		}
+    if (node.val == target) {
+      return new Location(depth, parent);
+    }
 
-		if ((node.left != null && node.left.val == target) || (node.right != null && node.right.val == target)) {
-			return new Location(depth + 1, node);
-		}
+    Location leftSubResult = search(node.left, node, target, depth + 1);
+    if (leftSubResult != null) {
+      return leftSubResult;
+    }
 
-		Location leftSubResult = search(node.left, target, depth + 1);
-		if (leftSubResult != null) {
-			return leftSubResult;
-		}
-
-		return search(node.right, target, depth + 1);
-	}
+    return search(node.right, node, target, depth + 1);
+  }
 }
 
 class Location {
-	int depth;
-	TreeNode parent;
+  int depth;
+  TreeNode parent;
 
-	Location(int depth, TreeNode parent) {
-		this.depth = depth;
-		this.parent = parent;
-	}
+  Location(int depth, TreeNode parent) {
+    this.depth = depth;
+    this.parent = parent;
+  }
 }
