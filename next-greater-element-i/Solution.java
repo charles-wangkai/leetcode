e5bfa9e64462bffb542eb2f3c825e704ee8bbc38
Kai Wang <charles.wangkai@gmail.com>
2021-10-19 19:15:14 +0800

    Next Greater Element I

--- next-greater-element-i/Solution.java
+++ next-greater-element-i/Solution.java
@@ -1,23 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public int[] nextGreaterElement(int[] findNums, int[] nums) {
-		Map<Integer, Integer> number2greater = new HashMap<Integer, Integer>();
-		Stack<Integer> stack = new Stack<Integer>();
-		for (int num : nums) {
-			while (!stack.empty() && num > stack.peek()) {
-				number2greater.put(stack.pop(), num);
-			}
+class Solution {
+  public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    Map<Integer, Integer> numToGreater = new HashMap<>();
+    Stack<Integer> stack = new Stack<>();
+    for (int num : nums2) {
+      while (!stack.empty() && num > stack.peek()) {
+        numToGreater.put(stack.pop(), num);
+      }
 
-			stack.push(num);
-		}
+      stack.push(num);
+    }
 
-		int[] result = new int[findNums.length];
-		for (int i = 0; i < result.length; i++) {
-			result[i] = number2greater.containsKey(findNums[i]) ? number2greater.get(findNums[i]) : -1;
-		}
-		return result;
-	}
+    return IntStream.range(0, nums1.length)
+        .map(i -> numToGreater.getOrDefault(nums1[i], -1))
+        .toArray();
+  }
 }
