95e43ef59e80cb2f5d97ad0a0c6655e363827db4
Kai Wang <charles.wangkai@gmail.com>
2021-10-20 09:38:12 +0800

    Reverse Words in a String

--- reverse-words-in-a-string/Solution.java
+++ reverse-words-in-a-string/Solution.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution {
-	public String reverseWords(String s) {
-		List<String> words = Arrays.stream(s.split(" ")).filter(part -> !part.isEmpty()).collect(Collectors.toList());
-		Collections.reverse(words);
+  String reverseWords(String s) {
+    List<String> words =
+        Arrays.stream(s.split(" ")).filter(part -> !part.isEmpty()).collect(Collectors.toList());
+    Collections.reverse(words);
 
-		return String.join(" ", words);
-	}
+    return String.join(" ", words);
+  }
 }
