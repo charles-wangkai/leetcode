ddee220102ceb1d406b01536aad984ac54c18ee7
Kai Wang <charles.wangkai@gmail.com>
2021-10-22 08:50:11 +0800

    Sort Characters By Frequency

--- sort-characters-by-frequency/Solution.java
+++ sort-characters-by-frequency/Solution.java
@@ -1,26 +1,18 @@
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public String frequencySort(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+class Solution {
+  public String frequencySort(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		List<Character> sortedLetters = letterToCount.keySet().stream()
-				.sorted((l1, l2) -> -Integer.compare(letterToCount.get(l1), letterToCount.get(l2)))
-				.collect(Collectors.toList());
-
-		StringBuilder result = new StringBuilder();
-		for (char letter : sortedLetters) {
-			for (int i = 0; i < letterToCount.get(letter); ++i) {
-				result.append(letter);
-			}
-		}
-
-		return result.toString();
-	}
+    return letterToCount.keySet().stream()
+        .sorted(Comparator.comparing(letterToCount::get).reversed())
+        .map(letter -> String.valueOf(letter).repeat(letterToCount.get(letter)))
+        .collect(Collectors.joining());
+  }
 }
