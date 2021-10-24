cd3ea6d50259e287a165e9f5d20e6ef149c91e87
Kai Wang <charles.wangkai@gmail.com>
2021-10-24 23:36:21 +0800

    Count Complete Tree Nodes

--- count-complete-tree-nodes/Solution.java
+++ count-complete-tree-nodes/Solution.java
@@ -1,71 +1,69 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public int countNodes(TreeNode root) {
-		if (root == null) {
-			return 0;
-		}
+class Solution {
+  public int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
 
-		int height = findHeight(root);
+    int height = findHeight(root);
 
-		int result = -1;
-		int lower = 1 << (height - 1);
-		int upper = (1 << height) - 1;
-		while (lower <= upper) {
-			int middle = lower + (upper - lower) / 2;
-			if (check(root, height, middle)) {
-				result = middle;
-				lower = middle + 1;
-			} else {
-				upper = middle - 1;
-			}
-		}
+    int result = -1;
+    int lower = 1 << (height - 1);
+    int upper = (1 << height) - 1;
+    while (lower <= upper) {
+      int middle = lower + (upper - lower) / 2;
+      if (check(root, height, middle)) {
+        result = middle;
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	int findHeight(TreeNode root) {
-		int result = 0;
-		while (root != null) {
-			++result;
-			root = root.left;
-		}
+  int findHeight(TreeNode root) {
+    int result = 0;
+    for (TreeNode node = root; node != null; node = node.left) {
+      ++result;
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	boolean check(TreeNode root, int height, int value) {
-		TreeNode node = root;
-		for (int i = 0; i < height - 1; ++i) {
-			if ((value & (1 << (height - 2 - i))) == 0) {
-				node = node.left;
-			} else {
-				node = node.right;
-			}
+  boolean check(TreeNode root, int height, int value) {
+    TreeNode node = root;
+    for (int i = 0; i < height - 1; ++i) {
+      if ((value & (1 << (height - 2 - i))) == 0) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
 
-			if (node == null) {
-				return false;
-			}
-		}
+      if (node == null) {
+        return false;
+      }
+    }
 
-		return true;
-	}
+    return true;
+  }
 }
