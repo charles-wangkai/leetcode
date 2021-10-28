d53121f2ff84e8266d23c35f5a889918c643e6bd
Kai Wang <charles.wangkai@gmail.com>
2021-10-28 22:20:27 +0800

    3Sum

--- 3sum/Solution.java
+++ 3sum/Solution.java
@@ -1,36 +1,40 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Solution {
-	public List<List<Integer>> threeSum(int[] nums) {
-		Map<Integer, Integer> valueToCount = new HashMap<>();
-		for (int value : nums) {
-			valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
-		}
+  public List<List<Integer>> threeSum(int[] nums) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int value : nums) {
+      updateMap(valueToCount, value, 1);
+    }
 
-		List<List<Integer>> result = new ArrayList<>();
-		for (int a : valueToCount.keySet()) {
-			valueToCount.put(a, valueToCount.get(a) - 1);
+    List<List<Integer>> result = new ArrayList<>();
+    for (int a : List.copyOf(valueToCount.keySet())) {
+      updateMap(valueToCount, a, -1);
 
-			for (int b : valueToCount.keySet()) {
-				if (b >= a && valueToCount.get(b) != 0) {
-					valueToCount.put(b, valueToCount.get(b) - 1);
+      for (int b : List.copyOf(valueToCount.keySet())) {
+        if (b >= a) {
+          updateMap(valueToCount, b, -1);
 
-					int c = -a - b;
-					if (c >= b && valueToCount.getOrDefault(c, 0) != 0) {
-						result.add(Arrays.asList(a, b, c));
-					}
+          int c = -a - b;
+          if (c >= b && valueToCount.containsKey(c)) {
+            result.add(List.of(a, b, c));
+          }
 
-					valueToCount.put(b, valueToCount.get(b) + 1);
-				}
-			}
+          updateMap(valueToCount, b, 1);
+        }
+      }
 
-			valueToCount.put(a, valueToCount.get(a) + 1);
-		}
+      updateMap(valueToCount, a, 1);
+    }
 
-		return result;
-	}
+    return result;
+  }
+
+  void updateMap(Map<Integer, Integer> valueToCount, int value, int delta) {
+    valueToCount.put(value, valueToCount.getOrDefault(value, 0) + delta);
+    valueToCount.remove(value, 0);
+  }
 }
