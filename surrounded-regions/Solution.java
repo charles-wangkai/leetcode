4f157dfe24d6f05bfda8936d6eff9af5ab5fdf5c
Kai Wang <charles.wangkai@gmail.com>
2021-11-01 09:46:07 +0800

    Surrounded Regions

--- surrounded-regions/Solution.java
+++ surrounded-regions/Solution.java
@@ -1,76 +1,64 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public void solve(char[][] board) {
-		int row = board.length;
-		if (row == 0) {
-			return;
-		}
-		int col = board[0].length;
-		if (col == 0) {
-			return;
-		}
+  public void solve(char[][] board) {
+    fill(board);
 
-		fill(board);
+    replace(board, 'O', 'X');
+    replace(board, '_', 'O');
+  }
 
-		replace(board, 'O', 'X');
-		replace(board, '_', 'O');
-	}
+  void fill(char[][] board) {
+    int row = board.length;
+    int col = board[0].length;
 
-	void fill(char[][] board) {
-		int row = board.length;
-		int col = board[0].length;
+    Queue<Point> queue = new ArrayDeque<>();
+    for (int r = 0; r < row; ++r) {
+      for (int c = 0; c < col; ++c) {
+        if ((r == 0 || r == row - 1 || c == 0 || c == col - 1) && board[r][c] == 'O') {
+          board[r][c] = '_';
+          queue.offer(new Point(r, c));
+        }
+      }
+    }
 
-		Queue<Point> queue = new LinkedList<>();
-		for (int r = 0; r < row; ++r) {
-			for (int c = 0; c < col; ++c) {
-				if ((r == 0 || r == row - 1 || c == 0 || c == col - 1) && board[r][c] == 'O') {
-					queue.offer(new Point(r, c));
-				}
-			}
-		}
+    while (!queue.isEmpty()) {
+      Point head = queue.poll();
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r + R_OFFSETS[i];
+        int adjC = head.c + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < row && adjC >= 0 && adjC < col && board[adjR][adjC] == 'O') {
+          board[adjR][adjC] = '_';
+          queue.offer(new Point(adjR, adjC));
+        }
+      }
+    }
+  }
 
-		while (!queue.isEmpty()) {
-			Point head = queue.poll();
-			if (!(head.r >= 0 && head.r < row && head.c >= 0 && head.c < col && board[head.r][head.c] == 'O')) {
-				continue;
-			}
+  void replace(char[][] board, char from, char to) {
+    int row = board.length;
+    int col = board[0].length;
 
-			board[head.r][head.c] = '_';
-
-			for (int i = 0; i < R_OFFSETS.length; ++i) {
-				int adjR = head.r + R_OFFSETS[i];
-				int adjC = head.c + C_OFFSETS[i];
-
-				queue.offer(new Point(adjR, adjC));
-			}
-		}
-	}
-
-	void replace(char[][] board, char from, char to) {
-		int row = board.length;
-		int col = board[0].length;
-
-		for (int r = 0; r < row; ++r) {
-			for (int c = 0; c < col; ++c) {
-				if (board[r][c] == from) {
-					board[r][c] = to;
-				}
-			}
-		}
-	}
+    for (int r = 0; r < row; ++r) {
+      for (int c = 0; c < col; ++c) {
+        if (board[r][c] == from) {
+          board[r][c] = to;
+        }
+      }
+    }
+  }
 }
 
 class Point {
-	int r;
-	int c;
+  int r;
+  int c;
 
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
 }
