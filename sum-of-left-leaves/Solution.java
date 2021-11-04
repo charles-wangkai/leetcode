4b1dbef35a464927bdebe1adb4c12a240ca9cefc
Kai Wang <charles.wangkai@gmail.com>
2021-11-04 13:13:19 +0800

    Sum of Left Leaves

--- sum-of-left-leaves/Solution.java
+++ sum-of-left-leaves/Solution.java
@@ -1,41 +1,41 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	enum Direction {
-		LEFT, RIGHT
-	};
+  enum Direction {
+    LEFT,
+    RIGHT
+  };
 
-	public int sumOfLeftLeaves(TreeNode root) {
-		return search(root, null);
-	}
+  public int sumOfLeftLeaves(TreeNode root) {
+    return search(root, null);
+  }
 
-	int search(TreeNode node, Direction direction) {
-		if (node == null) {
-			return 0;
-		}
+  int search(TreeNode node, Direction direction) {
+    if (node == null) {
+      return 0;
+    }
 
-		if (node.left == null && node.right == null && direction == Direction.LEFT) {
-			return node.val;
-		}
+    if (node.left == null && node.right == null && direction == Direction.LEFT) {
+      return node.val;
+    }
 
-		return search(node.left, Direction.LEFT) + search(node.right, Direction.RIGHT);
-	}
+    return search(node.left, Direction.LEFT) + search(node.right, Direction.RIGHT);
+  }
 }
