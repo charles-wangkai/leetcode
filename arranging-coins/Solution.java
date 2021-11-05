6691bc20a94be455fc004ff4c7414c0c08e41279
Kai Wang <charles.wangkai@gmail.com>
2021-11-05 23:05:44 +0800

    Arranging Coins

--- arranging-coins/Solution.java
+++ arranging-coins/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
-	public int arrangeCoins(int n) {
-		int row = (int) Math.floor(Math.sqrt(n * 2L));
+  public int arrangeCoins(int n) {
+    int row = (int) Math.floor(Math.sqrt(n * 2L));
 
-		return (row * (row + 1L) <= n * 2L) ? row : (row - 1);
-	}
+    return (row * (row + 1L) <= n * 2L) ? row : (row - 1);
+  }
 }
