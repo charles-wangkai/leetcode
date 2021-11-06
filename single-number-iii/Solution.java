ae84b5260030c0b941a5170e080e2e6357a57469
Kai Wang <charles.wangkai@gmail.com>
2021-11-06 10:56:01 +0800

    Single Number III

--- single-number-iii/Solution.java
+++ single-number-iii/Solution.java
@@ -1,19 +1,23 @@
 import java.util.Arrays;
 
 class Solution {
-	public int[] singleNumber(int[] nums) {
-		int xor = Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
+  public int[] singleNumber(int[] nums) {
+    int xor = Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
 
-		int diffPos = 0;
-		while ((xor & (1 << diffPos)) == 0) {
-			++diffPos;
-		}
+    int diffPos = 0;
+    while ((xor & (1 << diffPos)) == 0) {
+      ++diffPos;
+    }
 
-		return new int[] { singleNumberForOne(nums, diffPos, false), singleNumberForOne(nums, diffPos, true) };
-	}
+    return new int[] {
+      singleNumberForOne(nums, diffPos, false), singleNumberForOne(nums, diffPos, true)
+    };
+  }
 
-	int singleNumberForOne(int[] nums, int pos, boolean setBit) {
-		return Arrays.stream(nums).filter(num -> ((num & (1 << pos)) != 0) == setBit).reduce((x, y) -> x ^ y)
-				.getAsInt();
-	}
+  int singleNumberForOne(int[] nums, int pos, boolean setBit) {
+    return Arrays.stream(nums)
+        .filter(num -> ((num & (1 << pos)) != 0) == setBit)
+        .reduce((x, y) -> x ^ y)
+        .getAsInt();
+  }
 }
