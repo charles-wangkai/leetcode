f51b84ee0d2ad0ce714303af5986a7ae4b592a53
Kai Wang <charles.wangkai@gmail.com>
2021-11-07 12:39:18 +0800

    Minimized Maximum of Products Distributed to Any Store

---
+++ minimized-maximum-of-products-distributed-to-any-store/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+  public int minimizedMaximum(int n, int[] quantities) {
+    int result = -1;
+    int lower = 1;
+    int upper = Arrays.stream(quantities).max().getAsInt();
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(n, quantities, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
+
+    return result;
+  }
+
+  boolean check(int n, int[] quantities, int maxProductNum) {
+    return Arrays.stream(quantities)
+            .mapToLong(quantity -> (quantity + maxProductNum - 1) / maxProductNum)
+            .sum()
+        <= n;
+  }
+}
