5588042b0d86992e4563b5f64fdf3c02e5cf0e5f
Kai Wang <charles.wangkai@gmail.com>
2021-11-08 10:22:37 +0800

    Unique Binary Search Trees

--- unique-binary-search-trees/Solution.java
+++ unique-binary-search-trees/Solution.java
@@ -1,14 +1,14 @@
-public class Solution {
-	public int numTrees(int n) {
-		int[] treeNums = new int[n + 1];
-		treeNums[0] = 1;
+class Solution {
+  public int numTrees(int n) {
+    int[] treeNums = new int[n + 1];
+    treeNums[0] = 1;
 
-		for (int i = 1; i < treeNums.length; ++i) {
-			for (int j = 0; j < i; ++j) {
-				treeNums[i] += treeNums[j] * treeNums[i - 1 - j];
-			}
-		}
+    for (int i = 1; i < treeNums.length; ++i) {
+      for (int j = 0; j < i; ++j) {
+        treeNums[i] += treeNums[j] * treeNums[i - 1 - j];
+      }
+    }
 
-		return treeNums[n];
-	}
+    return treeNums[n];
+  }
 }
