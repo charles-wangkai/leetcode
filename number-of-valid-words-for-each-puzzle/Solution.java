fa165168b2e1877a2850f95ee5d1cef0f74a9410
Kai Wang <charles.wangkai@gmail.com>
2021-11-09 11:38:02 +0800

    Number of Valid Words for Each Puzzle

--- number-of-valid-words-for-each-puzzle/Solution.java
+++ number-of-valid-words-for-each-puzzle/Solution.java
@@ -1,44 +1,41 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
-		Map<String, Integer> patternToCount = new HashMap<>();
-		for (String word : words) {
-			String pattern = buildPattern(word);
+class Solution {
+  public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+    Map<String, Integer> patternToCount = new HashMap<>();
+    for (String word : words) {
+      String pattern = buildPattern(word);
 
-			patternToCount.put(pattern, patternToCount.getOrDefault(pattern, 0) + 1);
-		}
+      patternToCount.put(pattern, patternToCount.getOrDefault(pattern, 0) + 1);
+    }
 
-		List<Integer> result = new ArrayList<>();
-		for (String puzzle : puzzles) {
-			int matchedCount = 0;
-			for (int code = 0; code < 1 << (puzzle.length() - 1); code++) {
-				StringBuilder sb = new StringBuilder();
-				sb.append(puzzle.charAt(0));
+    return Arrays.stream(puzzles)
+        .map(
+            puzzle ->
+                IntStream.range(0, 1 << (puzzle.length() - 1))
+                    .map(
+                        code ->
+                            patternToCount.getOrDefault(
+                                buildPattern(
+                                    IntStream.range(0, puzzle.length())
+                                        .filter(i -> i == 0 || (code & (1 << (i - 1))) != 0)
+                                        .mapToObj(i -> String.valueOf(puzzle.charAt(i)))
+                                        .collect(Collectors.joining())),
+                                0))
+                    .sum())
+        .collect(Collectors.toList());
+  }
 
-				int p = code;
-				for (int i = 1; i < puzzle.length(); i++) {
-					if (p % 2 != 0) {
-						sb.append(puzzle.charAt(i));
-					}
-
-					p /= 2;
-				}
-
-				matchedCount += patternToCount.getOrDefault(buildPattern(sb.toString()), 0);
-			}
-
-			result.add(matchedCount);
-		}
-
-		return result;
-	}
-
-	String buildPattern(String s) {
-		return s.chars().distinct().sorted().mapToObj(ch -> String.valueOf((char) ch)).collect(Collectors.joining());
-	}
+  String buildPattern(String s) {
+    return s.chars()
+        .distinct()
+        .sorted()
+        .mapToObj(ch -> String.valueOf((char) ch))
+        .collect(Collectors.joining());
+  }
 }
