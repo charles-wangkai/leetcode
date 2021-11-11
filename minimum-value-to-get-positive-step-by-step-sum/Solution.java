3f5b8b0dec5fbdde38b50fbd5c2e300af2bb5583
Kai Wang <charles.wangkai@gmail.com>
2021-11-11 23:50:34 +0800

    Minimum Value to Get Positive Step by Step Sum

--- minimum-value-to-get-positive-step-by-step-sum/Solution.java
+++ minimum-value-to-get-positive-step-by-step-sum/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int minStartValue(int[] nums) {
-        int minSum = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-            minSum = Math.min(minSum, sum);
-        }
-
-        return Math.max(1, 1 - minSum);
+  public int minStartValue(int[] nums) {
+    int minSum = Integer.MAX_VALUE;
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+      minSum = Math.min(minSum, sum);
     }
+
+    return Math.max(1, 1 - minSum);
+  }
 }
