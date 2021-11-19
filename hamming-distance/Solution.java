35b946139f129f00428b961c6f8cbf0188dc0690
Kai Wang <charles.wangkai@gmail.com>
2021-11-19 11:34:26 +0800

    Hamming Distance

--- hamming-distance/Solution.java
+++ hamming-distance/Solution.java
@@ -1,15 +1,15 @@
-public class Solution {
-	public int hammingDistance(int x, int y) {
-		int result = 0;
-		while (x != y) {
-			if ((x & 1) != (y & 1)) {
-				++result;
-			}
+class Solution {
+  public int hammingDistance(int x, int y) {
+    int result = 0;
+    while (x != y) {
+      if ((x & 1) != (y & 1)) {
+        ++result;
+      }
 
-			x >>= 1;
-			y >>= 1;
-		}
+      x >>= 1;
+      y >>= 1;
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
