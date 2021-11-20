28c2950ca4462af7d12fb7e0ee00708c587cc8c1
Kai Wang <charles.wangkai@gmail.com>
2021-11-20 20:50:26 +0800

    Single Element in a Sorted Array

--- single-element-in-a-sorted-array/Solution.java
+++ single-element-in-a-sorted-array/Solution.java
@@ -1,26 +1,19 @@
-public class Solution {
-	public int singleNonDuplicate(int[] nums) {
-		int lower = 0;
-		int upper = nums.length - 1;
-		while (lower < upper) {
-			int middle = (lower + upper) / 2;
-			if (nums[middle] == nums[middle - 1]) {
-				if ((middle - lower + 1) % 2 == 0) {
-					lower = middle + 1;
-				} else {
-					upper = middle;
-				}
-			} else if (nums[middle] == nums[middle + 1]) {
-				if ((upper - middle + 1) % 2 == 0) {
-					upper = middle - 1;
-				} else {
-					lower = middle;
-				}
-			} else {
-				return nums[middle];
-			}
-		}
+class Solution {
+  public int singleNonDuplicate(int[] nums) {
+    int result = -1;
+    int lower = 0;
+    int upper = nums.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if ((middle % 2 == 0 && middle + 1 != nums.length && nums[middle] == nums[middle + 1])
+          || (middle % 2 != 0 && middle != 0 && nums[middle] == nums[middle - 1])) {
+        lower = middle + 1;
+      } else {
+        result = nums[middle];
+        upper = middle - 1;
+      }
+    }
 
-		return nums[lower];
-	}
+    return result;
+  }
 }
