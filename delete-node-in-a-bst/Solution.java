a0422f160bbdfa8931e9c567158e91fe340625a4
Kai Wang <charles.wangkai@gmail.com>
2021-11-22 10:00:06 +0800

    Delete Node in a BST

--- delete-node-in-a-bst/Solution.java
+++ delete-node-in-a-bst/Solution.java
@@ -1,55 +1,56 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public TreeNode deleteNode(TreeNode root, int key) {
-		if (root == null) {
-			return null;
-		}
+  public TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
 
-		if (root.val == key) {
-			if (root.left != null) {
-				TreeNode mostRightInLeft = root.left;
-				while (mostRightInLeft.right != null) {
-					mostRightInLeft = mostRightInLeft.right;
-				}
+    if (root.val == key) {
+      if (root.left != null) {
+        TreeNode mostRightInLeft = root.left;
+        while (mostRightInLeft.right != null) {
+          mostRightInLeft = mostRightInLeft.right;
+        }
 
-				return new TreeNode(mostRightInLeft.val, deleteNode(root.left, mostRightInLeft.val), root.right);
-			} else if (root.right != null) {
-				TreeNode mostLeftInRight = root.right;
-				while (mostLeftInRight.left != null) {
-					mostLeftInRight = mostLeftInRight.left;
-				}
+        return new TreeNode(
+            mostRightInLeft.val, deleteNode(root.left, mostRightInLeft.val), root.right);
+      } else if (root.right != null) {
+        TreeNode mostLeftInRight = root.right;
+        while (mostLeftInRight.left != null) {
+          mostLeftInRight = mostLeftInRight.left;
+        }
 
-				return new TreeNode(mostLeftInRight.val, root.left, deleteNode(root.right, mostLeftInRight.val));
-			} else {
-				return null;
-			}
-		}
+        return new TreeNode(
+            mostLeftInRight.val, root.left, deleteNode(root.right, mostLeftInRight.val));
+      } else {
+        return null;
+      }
+    }
 
-		if (key < root.val) {
-			root.left = deleteNode(root.left, key);
-		} else {
-			root.right = deleteNode(root.right, key);
-		}
+    if (key < root.val) {
+      root.left = deleteNode(root.left, key);
+    } else {
+      root.right = deleteNode(root.right, key);
+    }
 
-		return root;
-	}
+    return root;
+  }
 }
