57e5be9fc7695db0efbcc91d8986f3ef660a35df
Kai Wang <charles.wangkai@gmail.com>
2021-11-23 15:01:15 +0800

    Largest Component Size by Common Factor

--- largest-component-size-by-common-factor/Solution.java
+++ largest-component-size-by-common-factor/Solution.java
@@ -3,67 +3,67 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-	public int largestComponentSize(int[] A) {
-		int maxValue = Arrays.stream(A).max().getAsInt();
+  public int largestComponentSize(int[] nums) {
+    int maxValue = Arrays.stream(nums).max().getAsInt();
 
-		int[] parents = new int[maxValue + 1];
-		Arrays.fill(parents, -1);
-		for (int a : A) {
-			parents[a] = a;
-		}
+    int[] parents = new int[maxValue + 1];
+    Arrays.fill(parents, -1);
+    for (int num : nums) {
+      parents[num] = num;
+    }
 
-		boolean[] primes = new boolean[parents.length];
-		Arrays.fill(primes, true);
-		for (int i = 2; i < parents.length; ++i) {
-			if (primes[i]) {
-				int last = -1;
+    boolean[] primes = new boolean[parents.length];
+    Arrays.fill(primes, true);
+    for (int i = 2; i < parents.length; ++i) {
+      if (primes[i]) {
+        int last = -1;
 
-				for (int j = i; j < primes.length; j += i) {
-					primes[j] = false;
+        for (int j = i; j < primes.length; j += i) {
+          primes[j] = false;
 
-					if (parents[j] != -1) {
-						if (last != -1) {
-							union(parents, j, last);
-						}
+          if (parents[j] != -1) {
+            if (last != -1) {
+              union(parents, j, last);
+            }
 
-						last = j;
-					}
-				}
-			}
-		}
+            last = j;
+          }
+        }
+      }
+    }
 
-		Map<Integer, Integer> rootToSize = new HashMap<>();
-		for (int a : A) {
-			int root = findRoot(parents, a);
-			rootToSize.put(root, rootToSize.getOrDefault(root, 0) + 1);
-		}
+    Map<Integer, Integer> rootToSize = new HashMap<>();
+    for (int a : nums) {
+      int root = findRoot(parents, a);
+      rootToSize.put(root, rootToSize.getOrDefault(root, 0) + 1);
+    }
 
-		return rootToSize.values().stream().mapToInt(x -> x).max().getAsInt();
-	}
+    return rootToSize.values().stream().mapToInt(x -> x).max().getAsInt();
+  }
 
-	static void union(int[] parents, int value1, int value2) {
-		int root1 = findRoot(parents, value1);
-		int root2 = findRoot(parents, value2);
+  static void union(int[] parents, int value1, int value2) {
+    int root1 = findRoot(parents, value1);
+    int root2 = findRoot(parents, value2);
 
-		if (root1 != root2) {
-			parents[root2] = root1;
-		}
-	}
+    if (root1 != root2) {
+      parents[root2] = root1;
+    }
+  }
 
-	static int findRoot(int[] parents, int value) {
-		int root = value;
-		while (parents[root] != root) {
-			root = parents[root];
-		}
+  static int findRoot(int[] parents, int value) {
+    int root = value;
+    while (parents[root] != root) {
+      root = parents[root];
+    }
 
-		int p = value;
-		while (p != root) {
-			int next = parents[p];
-			parents[p] = root;
+    int p = value;
+    while (p != root) {
+      int next = parents[p];
+      parents[p] = root;
 
-			p = next;
-		}
+      p = next;
+    }
 
-		return root;
-	}
+    return root;
+  }
 }
