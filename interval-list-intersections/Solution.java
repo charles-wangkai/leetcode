2a0a01fd61f8be2168747b7207a8299e12b601b0
Kai Wang <charles.wangkai@gmail.com>
2021-11-24 09:46:34 +0800

    Interval List Intersections

--- interval-list-intersections/Solution.java
+++ interval-list-intersections/Solution.java
@@ -1,34 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public int[][] intervalIntersection(int[][] A, int[][] B) {
-		List<int[]> result = new ArrayList<>();
-		int indexA = 0;
-		int indexB = 0;
-		while (indexA < A.length && indexB < B.length) {
-			int[] intervalA = A[indexA];
-			int[] intervalB = B[indexB];
+class Solution {
+  public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+    List<int[]> result = new ArrayList<>();
+    int index1 = 0;
+    int index2 = 0;
+    while (index1 != firstList.length && index2 != secondList.length) {
+      int[] interval1 = firstList[index1];
+      int[] interval2 = secondList[index2];
 
-			if (intervalA[1] <= intervalB[1]) {
-				if (intervalB[0] <= intervalA[1]) {
-					result.add(new int[] { Math.max(intervalA[0], intervalB[0]), intervalA[1] });
+      if (interval1[1] <= interval2[1]) {
+        if (interval2[0] <= interval1[1]) {
+          result.add(new int[] {Math.max(interval1[0], interval2[0]), interval1[1]});
+        }
 
-					B[indexB][0] = intervalA[1];
-				}
+        ++index1;
+      } else {
+        if (interval1[0] <= interval2[1]) {
+          result.add(new int[] {Math.max(interval1[0], interval2[0]), interval2[1]});
+        }
 
-				++indexA;
-			} else {
-				if (intervalA[0] <= intervalB[1]) {
-					result.add(new int[] { Math.max(intervalA[0], intervalB[0]), intervalB[1] });
+        ++index2;
+      }
+    }
 
-					A[indexA][0] = intervalB[1];
-				}
-
-				++indexB;
-			}
-		}
-
-		return result.toArray(new int[0][]);
-	}
+    return result.toArray(new int[0][]);
+  }
 }
