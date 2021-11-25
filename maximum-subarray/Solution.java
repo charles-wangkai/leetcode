9b75caa43bab4887979440d8ef1e54af35fef302
Kai Wang <charles.wangkai@gmail.com>
2021-11-25 10:26:56 +0800

    Maximum Subarray

--- maximum-subarray/Solution.java
+++ maximum-subarray/Solution.java
@@ -1,12 +1,13 @@
-public class Solution {
-	public int maxSubArray(int[] A) {
-		int result = Integer.MIN_VALUE;
-		int sum = 0;
-		for (int elem : A) {
-			sum += elem;
-			result = Math.max(result, sum);
-			sum = Math.max(sum, 0);
-		}
-		return result;
-	}
+class Solution {
+  public int maxSubArray(int[] nums) {
+    int result = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+      result = Math.max(result, sum);
+      sum = Math.max(sum, 0);
+    }
+
+    return result;
+  }
 }
