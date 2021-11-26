2486b9d93bd3d27a1b3475a0c9b552e1818fab4d
Kai Wang <charles.wangkai@gmail.com>
2021-11-26 12:56:34 +0800

    Search Insert Position

--- search-insert-position/Solution.java
+++ search-insert-position/Solution.java
@@ -1,19 +1,19 @@
-public class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int result = 0;
-        int lower = 0;
-        int upper = nums.length - 1;
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+    int result = nums.length;
+    int lower = 0;
+    int upper = nums.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
 
-            if (nums[middle] < target) {
-                result = middle + 1;
-                lower = middle + 1;
-            } else {
-                upper = middle - 1;
-            }
-        }
-
-        return result;
+      if (nums[middle] >= target) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
     }
+
+    return result;
+  }
 }
