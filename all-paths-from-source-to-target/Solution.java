f31ecca5f60472a6642de361efd85899b1ea458e
Kai Wang <charles.wangkai@gmail.com>
2021-11-28 08:13:34 +0800

    All Paths From Source to Target

--- all-paths-from-source-to-target/Solution.java
+++ all-paths-from-source-to-target/Solution.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-	public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-		List<List<Integer>> paths = new ArrayList<>();
-		search(paths, graph, 0, new ArrayList<>());
+  public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    List<List<Integer>> paths = new ArrayList<>();
+    search(paths, graph, 0, new ArrayList<>());
 
-		return paths;
-	}
+    return paths;
+  }
 
-	void search(List<List<Integer>> paths, int[][] graph, int node, List<Integer> path) {
-		path.add(node);
+  void search(List<List<Integer>> paths, int[][] graph, int node, List<Integer> path) {
+    path.add(node);
 
-		if (node == graph.length - 1) {
-			paths.add(new ArrayList<>(path));
-		}
+    if (node == graph.length - 1) {
+      paths.add(List.copyOf(path));
+    }
 
-		for (int adj : graph[node]) {
-			search(paths, graph, adj, path);
-		}
+    for (int adj : graph[node]) {
+      search(paths, graph, adj, path);
+    }
 
-		path.remove(path.size() - 1);
-	}
+    path.remove(path.size() - 1);
+  }
 }
