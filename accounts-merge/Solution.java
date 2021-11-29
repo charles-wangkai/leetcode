2007873942fe70f5f3fc5214cce3b5bba20ba1b3
Kai Wang <charles.wangkai@gmail.com>
2021-11-29 11:36:52 +0800

    Accounts Merge

--- accounts-merge/Solution.java
+++ accounts-merge/Solution.java
@@ -1,76 +1,66 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Solution {
-	public List<List<String>> accountsMerge(List<List<String>> accounts) {
-		List<String> noEmailNames = new ArrayList<String>();
-		Map<String, String> email2name = new HashMap<String, String>();
-		for (List<String> account : accounts) {
-			Iterator<String> iter = account.iterator();
-			String name = iter.next();
+class Solution {
+  public List<List<String>> accountsMerge(List<List<String>> accounts) {
+    Map<String, String> emailToName = new HashMap<>();
+    for (List<String> account : accounts) {
+      for (int i = 1; i < account.size(); ++i) {
+        emailToName.put(account.get(i), account.get(0));
+      }
+    }
 
-			if (!iter.hasNext()) {
-				noEmailNames.add(name);
-			}
+    Map<String, String> emailToParent = new HashMap<>();
+    for (List<String> account : accounts) {
+      for (int i = 1; i + 1 < account.size(); ++i) {
+        union(emailToParent, account.get(i), account.get(i + 1));
+      }
+    }
 
-			while (iter.hasNext()) {
-				String email = iter.next();
-				email2name.put(email, name);
-			}
-		}
-		Set<String> emails = email2name.keySet();
+    Map<String, List<String>> rootToEmails = new HashMap<>();
+    for (String email : emailToName.keySet()) {
+      String root = findRoot(emailToParent, email);
+      if (!rootToEmails.containsKey(root)) {
+        rootToEmails.put(root, new ArrayList<>());
+      }
+      rootToEmails.get(root).add(email);
+    }
 
-		Map<String, String> email2parent = new HashMap<String, String>();
-		for (String email : emails) {
-			email2parent.put(email, null);
-		}
-		for (List<String> account : accounts) {
-			Iterator<String> iter = account.iterator();
-			iter.next();
-			if (iter.hasNext()) {
-				String firstEmail = iter.next();
-				while (iter.hasNext()) {
-					String email = iter.next();
-					union(email2parent, firstEmail, email);
-				}
-			}
-		}
+    return rootToEmails.keySet().stream()
+        .map(
+            root ->
+                Stream.concat(
+                        Stream.of(emailToName.get(root)), rootToEmails.get(root).stream().sorted())
+                    .collect(Collectors.toList()))
+        .collect(Collectors.toList());
+  }
 
-		List<List<String>> mergedAccounts = new ArrayList<List<String>>();
-		for (String noEmailName : noEmailNames) {
-			mergedAccounts.add(Collections.singletonList(noEmailName));
-		}
-		for (String root : emails) {
-			if (email2parent.get(root) == null) {
-				List<String> account = new ArrayList<String>();
-				account.add(email2name.get(root));
-				account.addAll(emails.stream().filter(email -> find(email2parent, email).equals(root)).sorted()
-						.collect(Collectors.toList()));
-				mergedAccounts.add(account);
-			}
-		}
-		return mergedAccounts;
-	}
+  String findRoot(Map<String, String> emailToParent, String email) {
+    String root = email;
+    while (emailToParent.containsKey(root)) {
+      root = emailToParent.get(root);
+    }
 
-	String find(Map<String, String> email2parent, String email) {
-		String root = email;
-		while (email2parent.get(root) != null) {
-			root = email2parent.get(root);
-		}
-		return root;
-	}
+    String p = email;
+    while (!p.equals(root)) {
+      String next = emailToParent.get(p);
+      emailToParent.put(p, root);
 
-	void union(Map<String, String> email2parent, String email1, String email2) {
-		String root1 = find(email2parent, email1);
-		String root2 = find(email2parent, email2);
-		if (!root1.equals(root2)) {
-			email2parent.put(root2, root1);
-		}
-	}
+      p = next;
+    }
+
+    return root;
+  }
+
+  void union(Map<String, String> emailToParent, String email1, String email2) {
+    String root1 = findRoot(emailToParent, email1);
+    String root2 = findRoot(emailToParent, email2);
+    if (!root1.equals(root2)) {
+      emailToParent.put(root2, root1);
+    }
+  }
 }
