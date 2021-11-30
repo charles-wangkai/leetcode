d7a37f520a2a9186d73ed8aebe406287e352c20d
Kai Wang <charles.wangkai@gmail.com>
2021-11-30 09:40:50 +0800

    Maximal Rectangle

--- maximal-rectangle/Solution.java
+++ maximal-rectangle/Solution.java
@@ -1,45 +1,46 @@
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Solution {
-	public int maximalRectangle(char[][] matrix) {
-		int row = matrix.length;
-		if (row == 0) {
-			return 0;
-		}
-		int col = matrix[0].length;
+class Solution {
+  public int maximalRectangle(char[][] matrix) {
+    int row = matrix.length;
+    if (row == 0) {
+      return 0;
+    }
+    int col = matrix[0].length;
 
-		int maxArea = 0;
-		int[] heights = new int[col];
-		for (int i = 0; i < row; i++) {
-			updateHeights(heights, matrix[i]);
-			maxArea = Math.max(maxArea, largestRectangleInHistogram(heights));
-		}
-		return maxArea;
-	}
+    int maxArea = 0;
+    int[] heights = new int[col];
+    for (int r = 0; r < row; ++r) {
+      updateHeights(heights, matrix[r]);
+      maxArea = Math.max(maxArea, largestRectangleInHistogram(heights));
+    }
 
-	void updateHeights(int[] heights, char[] line) {
-		for (int i = 0; i < heights.length; i++) {
-			if (line[i] == '1') {
-				heights[i]++;
-			} else {
-				heights[i] = 0;
-			}
-		}
-	}
+    return maxArea;
+  }
 
-	int largestRectangleInHistogram(int[] heights) {
-		heights = Arrays.copyOf(heights, heights.length + 1);
-		Stack<Integer> indices = new Stack<Integer>();
-		int maxArea = 0;
-		for (int i = 0; i < heights.length; i++) {
-			while (!indices.empty() && heights[i] <= heights[indices.peek()]) {
-				int h = heights[indices.pop()];
-				maxArea = Math.max(maxArea, h
-						* (i - (indices.empty() ? 0 : (indices.peek() + 1))));
-			}
-			indices.push(i);
-		}
-		return maxArea;
-	}
+  void updateHeights(int[] heights, char[] line) {
+    for (int c = 0; c < heights.length; ++c) {
+      if (line[c] == '1') {
+        ++heights[c];
+      } else {
+        heights[c] = 0;
+      }
+    }
+  }
+
+  int largestRectangleInHistogram(int[] heights) {
+    heights = Arrays.copyOf(heights, heights.length + 1);
+    Stack<Integer> indices = new Stack<>();
+    int result = 0;
+    for (int i = 0; i < heights.length; ++i) {
+      while (!indices.empty() && heights[i] <= heights[indices.peek()]) {
+        int h = heights[indices.pop()];
+        result = Math.max(result, h * (i - (indices.empty() ? 0 : (indices.peek() + 1))));
+      }
+      indices.push(i);
+    }
+
+    return result;
+  }
 }
