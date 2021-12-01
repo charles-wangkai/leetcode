45e3b7acdfd952630ae099d116d0923db018faa9
Kai Wang <charles.wangkai@gmail.com>
2021-12-01 08:33:24 +0800

    House Robber

--- house-robber/Solution.java
+++ house-robber/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-	public int rob(int[] num) {
-		int prev3 = 0;
-		int prev2 = 0;
-		int prev1 = 0;
-		for (int x : num) {
-			int current = Math.max(prev3, prev2) + x;
+  public int rob(int[] nums) {
+    int prev3 = 0;
+    int prev2 = 0;
+    int prev1 = 0;
+    for (int x : nums) {
+      int current = Math.max(prev3, prev2) + x;
 
-			prev3 = prev2;
-			prev2 = prev1;
-			prev1 = current;
-		}
+      prev3 = prev2;
+      prev2 = prev1;
+      prev1 = current;
+    }
 
-		return Math.max(prev2, prev1);
-	}
+    return Math.max(prev2, prev1);
+  }
 }
