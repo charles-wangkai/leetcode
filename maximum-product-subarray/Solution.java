52d885628dab73e041aad33ccf35550cd55f9cae
Kai Wang <charles.wangkai@gmail.com>
2021-12-03 10:07:51 +0800

    Maximum Product Subarray

--- maximum-product-subarray/Solution.java
+++ maximum-product-subarray/Solution.java
@@ -1,49 +1,49 @@
 class Solution {
-	public int maxProduct(int[] nums) {
-		int result = Integer.MIN_VALUE;
-		long product = 0;
-		long frontNegative = 1;
-		long backNegative = 0;
-		int size = 0;
+  public int maxProduct(int[] nums) {
+    int result = Integer.MIN_VALUE;
+    long product = 0;
+    long frontNegative = 1;
+    long backNegative = 0;
+    int size = 0;
 
-		for (int i = 0; i <= nums.length; ++i) {
-			if (i < nums.length) {
-				result = Math.max(result, nums[i]);
-			}
+    for (int i = 0; i <= nums.length; ++i) {
+      if (i != nums.length) {
+        result = Math.max(result, nums[i]);
+      }
 
-			if (i == nums.length || nums[i] == 0) {
-				if (product > 0) {
-					result = Math.max(result, (int) product);
-				} else if (product < 0 && size >= 2) {
-					result = Math.max(result, (int) (product / Math.max(frontNegative, backNegative)));
-				}
+      if (i == nums.length || nums[i] == 0) {
+        if (product > 0) {
+          result = Math.max(result, (int) product);
+        } else if (product < 0 && size >= 2) {
+          result = Math.max(result, (int) (product / Math.max(frontNegative, backNegative)));
+        }
 
-				product = 0;
-				frontNegative = 1;
-				backNegative = 0;
-				size = 0;
-			} else {
-				if (product == 0) {
-					product = 1;
-				}
-				product *= nums[i];
-				if (nums[i] > 0) {
-					if (frontNegative > 0) {
-						frontNegative *= nums[i];
-					}
-					if (backNegative < 0) {
-						backNegative *= nums[i];
-					}
-				} else {
-					if (frontNegative > 0) {
-						frontNegative *= nums[i];
-					}
-					backNegative = nums[i];
-				}
-				++size;
-			}
-		}
+        product = 0;
+        frontNegative = 1;
+        backNegative = 0;
+        size = 0;
+      } else {
+        if (product == 0) {
+          product = 1;
+        }
+        product *= nums[i];
 
-		return result;
-	}
+        if (frontNegative > 0) {
+          frontNegative *= nums[i];
+        }
+
+        if (nums[i] > 0) {
+          if (backNegative < 0) {
+            backNegative *= nums[i];
+          }
+        } else {
+          backNegative = nums[i];
+        }
+
+        ++size;
+      }
+    }
+
+    return result;
+  }
 }
