d9611edc104af0ddff505cb963b2bbf4af2eb2fc
Kai Wang <charles.wangkai@gmail.com>
2021-12-04 11:57:31 +0800

    Stream of Characters

--- stream-of-characters/StreamChecker.java
+++ stream-of-characters/StreamChecker.java
@@ -4,56 +4,56 @@ import java.util.List;
 import java.util.Map;
 
 class StreamChecker {
-	private TrieNode trie = new TrieNode();
-	private List<Character> queries = new ArrayList<>();
+  private TrieNode trie = new TrieNode();
+  private List<Character> queries = new ArrayList<>();
 
-	public StreamChecker(String[] words) {
-		for (String word : words) {
-			trie.add(word);
-		}
-	}
+  public StreamChecker(String[] words) {
+    for (String word : words) {
+      trie.add(word);
+    }
+  }
 
-	public boolean query(char letter) {
-		queries.add(letter);
+  public boolean query(char letter) {
+    queries.add(letter);
 
-		TrieNode node = trie;
-		for (int i = queries.size() - 1; i >= 0; --i) {
-			char query = queries.get(i);
+    TrieNode node = trie;
+    for (int i = queries.size() - 1; i >= 0; --i) {
+      char query = queries.get(i);
 
-			if (!node.letterToChild.containsKey(query)) {
-				return false;
-			}
+      if (!node.letterToChild.containsKey(query)) {
+        return false;
+      }
 
-			node = node.letterToChild.get(query);
-			if (node.letterToChild.containsKey(null)) {
-				return true;
-			}
-		}
+      node = node.letterToChild.get(query);
+      if (node.letterToChild.containsKey(null)) {
+        return true;
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 }
 
 class TrieNode {
-	Map<Character, TrieNode> letterToChild = new HashMap<>();
+  Map<Character, TrieNode> letterToChild = new HashMap<>();
 
-	public void add(String word) {
-		add(word, word.length() - 1);
-	}
+  public void add(String word) {
+    add(word, word.length() - 1);
+  }
 
-	private void add(String word, int index) {
-		if (index == -1) {
-			letterToChild.put(null, null);
+  private void add(String word, int index) {
+    if (index == -1) {
+      letterToChild.put(null, null);
 
-			return;
-		}
+      return;
+    }
 
-		char firstLetter = word.charAt(index);
-		if (!letterToChild.containsKey(firstLetter)) {
-			letterToChild.put(firstLetter, new TrieNode());
-		}
-		letterToChild.get(firstLetter).add(word, index - 1);
-	}
+    char letter = word.charAt(index);
+    if (!letterToChild.containsKey(letter)) {
+      letterToChild.put(letter, new TrieNode());
+    }
+    letterToChild.get(letter).add(word, index - 1);
+  }
 }
 
 // Your StreamChecker object will be instantiated and called as such:
