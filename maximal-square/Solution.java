886b33034c912272e1b3819de5ad67d006edd9a3
Kai Wang <charles.wangkai@gmail.com>
2021-12-17 10:02:14 +0800

    Maximal Square

--- maximal-square/Solution.java
+++ maximal-square/Solution.java
@@ -1,54 +1,53 @@
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Solution {
-	public int maximalSquare(char[][] matrix) {
-		int row = matrix.length;
-		if (row == 0) {
-			return 0;
-		}
-		int col = matrix[0].length;
+class Solution {
+  public int maximalSquare(char[][] matrix) {
+    int row = matrix.length;
+    int col = matrix[0].length;
 
-		int maxArea = 0;
-		int[] heights = new int[col];
-		for (int i = 0; i < row; ++i) {
-			updateHeights(heights, matrix[i]);
-			maxArea = Math.max(maxArea, computeLargestSquareInHistogram(heights));
-		}
+    int maxArea = 0;
+    int[] heights = new int[col];
+    for (int i = 0; i < row; ++i) {
+      updateHeights(heights, matrix[i]);
+      maxArea = Math.max(maxArea, computeLargestSquareInHistogram(heights));
+    }
 
-		return maxArea;
-	}
+    return maxArea;
+  }
 
-	void updateHeights(int[] heights, char[] line) {
-		for (int i = 0; i < heights.length; ++i) {
-			if (line[i] == '1') {
-				++heights[i];
-			} else {
-				heights[i] = 0;
-			}
-		}
-	}
+  void updateHeights(int[] heights, char[] line) {
+    for (int i = 0; i < heights.length; ++i) {
+      if (line[i] == '1') {
+        ++heights[i];
+      } else {
+        heights[i] = 0;
+      }
+    }
+  }
 
-	int computeLargestSquareInHistogram(int[] heights) {
-		heights = Arrays.copyOf(heights, heights.length + 1);
+  int computeLargestSquareInHistogram(int[] heights) {
+    heights = Arrays.copyOf(heights, heights.length + 1);
 
-		Stack<Integer> indices = new Stack<Integer>();
-		int maxArea = 0;
-		for (int i = 0; i < heights.length; ++i) {
-			while (!indices.empty() && heights[i] <= heights[indices.peek()]) {
-				int h = heights[indices.pop()];
-				maxArea = Math.max(maxArea, computeMaxSquareArea(h, i - (indices.empty() ? 0 : (indices.peek() + 1))));
-			}
+    Stack<Integer> indices = new Stack<>();
+    int maxArea = 0;
+    for (int i = 0; i < heights.length; ++i) {
+      while (!indices.empty() && heights[i] <= heights[indices.peek()]) {
+        int h = heights[indices.pop()];
+        maxArea =
+            Math.max(
+                maxArea, computeMaxSquareArea(h, i - (indices.empty() ? 0 : (indices.peek() + 1))));
+      }
 
-			indices.push(i);
-		}
+      indices.push(i);
+    }
 
-		return maxArea;
-	}
+    return maxArea;
+  }
 
-	int computeMaxSquareArea(int height, int width) {
-		int size = Math.min(height, width);
+  int computeMaxSquareArea(int height, int width) {
+    int size = Math.min(height, width);
 
-		return size * size;
-	}
+    return size * size;
+  }
 }
