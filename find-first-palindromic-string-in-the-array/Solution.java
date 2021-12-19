ae74bdda6cc3024ca02ca87e71722f876306e9e8
Kai Wang <charles.wangkai@gmail.com>
2021-12-19 16:35:22 +0800

    Find First Palindromic String in the Array

---
+++ find-first-palindromic-string-in-the-array/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+  public String firstPalindrome(String[] words) {
+    return Arrays.stream(words)
+        .filter(word -> word.equals(new StringBuilder(word).reverse().toString()))
+        .findFirst()
+        .orElse("");
+  }
+}
