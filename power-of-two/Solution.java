ed16aa2cbed93199d27f80c0a65adba719732ae0
Kai Wang <charles.wangkai@gmail.com>
2021-12-21 15:07:57 +0800

    Power of Two

--- power-of-two/Solution.java
+++ power-of-two/Solution.java
@@ -1,5 +1,5 @@
-public class Solution {
-	public boolean isPowerOfTwo(int n) {
-		return n > 0 && (n & (n - 1)) == 0;
-	}
+class Solution {
+  public boolean isPowerOfTwo(int n) {
+    return n >= 1 && (n & (n - 1)) == 0;
+  }
 }
