aef9075722cb43926c6b43ee6aee4ec8aca29594
Kai Wang <charles.wangkai@gmail.com>
2021-12-22 21:51:14 +0800

    Reorder List

--- reorder-list/Solution.java
+++ reorder-list/Solution.java
@@ -1,65 +1,60 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode() {
-	}
+  ListNode() {}
 
-	ListNode(int val) {
-		this.val = val;
-	}
+  ListNode(int val) {
+    this.val = val;
+  }
 
-	ListNode(int val, ListNode next) {
-		this.val = val;
-		this.next = next;
-	}
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
 class Solution {
-	public void reorderList(ListNode head) {
-		if (head == null) {
-			return;
-		}
+  public void reorderList(ListNode head) {
+    int length = computeLength(head);
 
-		int length = findLength(head);
+    ListNode node = head;
+    for (int i = 0; i < (length - 1) / 2; ++i) {
+      node = node.next;
+    }
+    ListNode backHead = node.next;
+    node.next = null;
+    backHead = reverse(backHead);
 
-		ListNode node = head;
-		for (int i = 0; i < (length - 1) / 2; ++i) {
-			node = node.next;
-		}
-		ListNode backHead = node.next;
-		node.next = null;
-		backHead = reverse(backHead);
+    ListNode p = head;
+    while (backHead != null) {
+      ListNode q = backHead;
+      backHead = backHead.next;
+      q.next = p.next;
+      p.next = q;
+      p = q.next;
+    }
+  }
 
-		ListNode p = head;
-		while (backHead != null) {
-			ListNode q = backHead;
-			backHead = backHead.next;
-			q.next = p.next;
-			p.next = q;
-			p = q.next;
-		}
-	}
+  int computeLength(ListNode head) {
+    int length = 0;
+    for (ListNode node = head; node != null; node = node.next) {
+      ++length;
+    }
 
-	int findLength(ListNode head) {
-		int length = 0;
-		for (ListNode node = head; node != null; node = node.next) {
-			++length;
-		}
+    return length;
+  }
 
-		return length;
-	}
+  ListNode reverse(ListNode head) {
+    ListNode tempHead = new ListNode();
+    while (head != null) {
+      ListNode node = head;
+      head = head.next;
+      node.next = tempHead.next;
+      tempHead.next = node;
+    }
 
-	ListNode reverse(ListNode head) {
-		ListNode tempHead = new ListNode();
-		while (head != null) {
-			ListNode node = head;
-			head = head.next;
-			node.next = tempHead.next;
-			tempHead.next = node;
-		}
-
-		return tempHead.next;
-	}
+    return tempHead.next;
+  }
 }
