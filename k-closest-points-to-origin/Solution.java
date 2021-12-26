590d8eea76d13fbf2ce04952f1df22768607eb64
Kai Wang <charles.wangkai@gmail.com>
2021-12-26 09:48:17 +0800

    K Closest Points to Origin

--- k-closest-points-to-origin/Solution.java
+++ k-closest-points-to-origin/Solution.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class Solution {
-	public int[][] kClosest(int[][] points, int K) {
-		return Arrays.stream(points)
-				.sorted((p1, p2) -> Integer.compare(computeDistanceSquare(p1), computeDistanceSquare(p2))).limit(K)
-				.toArray(int[][]::new);
-	}
+class Solution {
+  public int[][] kClosest(int[][] points, int k) {
+    return Arrays.stream(points)
+        .sorted(Comparator.comparing(this::computeDistanceSquare))
+        .limit(k)
+        .toArray(int[][]::new);
+  }
 
-	int computeDistanceSquare(int[] point) {
-		return point[0] * point[0] + point[1] * point[1];
-	}
+  int computeDistanceSquare(int[] point) {
+    return point[0] * point[0] + point[1] * point[1];
+  }
 }
