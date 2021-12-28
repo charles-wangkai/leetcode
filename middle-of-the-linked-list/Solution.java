85e65908ed95fb345fa7ae7411b8da790c545917
Kai Wang <charles.wangkai@gmail.com>
2021-12-28 09:05:59 +0800

    Middle of the Linked List

--- middle-of-the-linked-list/Solution.java
+++ middle-of-the-linked-list/Solution.java
@@ -1,25 +1,32 @@
 // Definition for singly-linked list.
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
-public class Solution {
-	public ListNode middleNode(ListNode head) {
-		ListNode fast = head;
-		ListNode slow = head;
+class Solution {
+  public ListNode middleNode(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
 
-		while (true) {
-			if (fast == null || fast.next == null) {
-				return slow;
-			}
+    while (true) {
+      if (fast == null || fast.next == null) {
+        return slow;
+      }
 
-			fast = fast.next.next;
-			slow = slow.next;
-		}
-	}
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+  }
 }
