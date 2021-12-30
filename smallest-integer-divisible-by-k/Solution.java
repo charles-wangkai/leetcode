cbe45401a6206fc057c5006676b866583388b7c5
Kai Wang <charles.wangkai@gmail.com>
2021-12-30 09:00:09 +0800

    Smallest Integer Divisible by K

--- smallest-integer-divisible-by-k/Solution.java
+++ smallest-integer-divisible-by-k/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-  public int smallestRepunitDivByK(int K) {
-    boolean[] visited = new boolean[K];
+  public int smallestRepunitDivByK(int k) {
+    boolean[] visited = new boolean[k];
     int remainder = 0;
     for (int length = 1; ; ++length) {
-      remainder = (remainder * 10 + 1) % K;
+      remainder = (remainder * 10 + 1) % k;
       if (remainder == 0) {
         return length;
       }
