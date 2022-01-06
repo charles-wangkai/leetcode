e696f82e14e2c7361d724d63f61fa3ad66f15190
Kai Wang <charles.wangkai@gmail.com>
2022-01-06 08:24:02 +0800

    Car Pooling

--- car-pooling/Solution.java
+++ car-pooling/Solution.java
@@ -1,20 +1,22 @@
 class Solution {
-	public boolean carPooling(int[][] trips, int capacity) {
-		int[] deltas = new int[1001];
-		for (int[] trip : trips) {
-			deltas[trip[1]] += trip[0];
-			deltas[trip[2]] -= trip[0];
-		}
+  static final int LIMIT = 1000;
 
-		int passengerNum = 0;
-		for (int i = 0; i < deltas.length; ++i) {
-			passengerNum += deltas[i];
+  public boolean carPooling(int[][] trips, int capacity) {
+    int[] deltas = new int[LIMIT + 1];
+    for (int[] trip : trips) {
+      deltas[trip[1]] += trip[0];
+      deltas[trip[2]] -= trip[0];
+    }
 
-			if (passengerNum > capacity) {
-				return false;
-			}
-		}
+    int passengerNum = 0;
+    for (int i = 0; i < deltas.length; ++i) {
+      passengerNum += deltas[i];
 
-		return true;
-	}
+      if (passengerNum > capacity) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
