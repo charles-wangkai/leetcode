1e3119ca499700a353dc8924f91e889952d250f1
Kai Wang <charles.wangkai@gmail.com>
2022-01-07 13:03:00 +0800

    Linked List Random Node

--- linked-list-random-node/Solution.java
+++ linked-list-random-node/Solution.java
@@ -21,29 +21,21 @@ class Solution {
   private Random random = new Random();
   private ListNode head;
 
-  /**
-   * @param head The linked list's head. Note that the head is guanranteed to be not null, so it
-   *     contains at least one node.
-   */
   public Solution(ListNode head) {
     this.head = head;
   }
 
-  /** Returns a random node's value. */
   public int getRandom() {
-    int chosen = 0;
+    int result = 0;
     int size = 0;
-    ListNode node = head;
-    while (node != null) {
+    for (ListNode node = head; node != null; node = node.next) {
       ++size;
       if (random.nextInt(size) == 0) {
-        chosen = node.val;
+        result = node.val;
       }
-
-      node = node.next;
     }
 
-    return chosen;
+    return result;
   }
 }
 
