8ed8d7a5b1efc523f28436bbae9ccde03a87bc4c
Kai Wang <charles.wangkai@gmail.com>
2022-01-09 09:07:16 +0800

    Robot Bounded In Circle

--- robot-bounded-in-circle/Solution.java
+++ robot-bounded-in-circle/Solution.java
@@ -1,22 +1,22 @@
-public class Solution {
-	static final int[] X_OFFSETS = { 0, 1, 0, -1 };
-	static final int[] Y_OFFSETS = { 1, 0, -1, 0 };
+class Solution {
+  static final int[] X_OFFSETS = {0, 1, 0, -1};
+  static final int[] Y_OFFSETS = {1, 0, -1, 0};
 
-	public boolean isRobotBounded(String instructions) {
-		int x = 0;
-		int y = 0;
-		int direction = 0;
-		for (char instruction : instructions.toCharArray()) {
-			if (instruction == 'G') {
-				x += X_OFFSETS[direction];
-				y += Y_OFFSETS[direction];
-			} else if (instruction == 'L') {
-				direction = (direction - 1 + X_OFFSETS.length) % X_OFFSETS.length;
-			} else if (instruction == 'R') {
-				direction = (direction + 1) % X_OFFSETS.length;
-			}
-		}
+  public boolean isRobotBounded(String instructions) {
+    int x = 0;
+    int y = 0;
+    int direction = 0;
+    for (char instruction : instructions.toCharArray()) {
+      if (instruction == 'L') {
+        direction = (direction - 1 + X_OFFSETS.length) % X_OFFSETS.length;
+      } else if (instruction == 'R') {
+        direction = (direction + 1) % X_OFFSETS.length;
+      } else {
+        x += X_OFFSETS[direction];
+        y += Y_OFFSETS[direction];
+      }
+    }
 
-		return direction != 0 || (x == 0 && y == 0);
-	}
+    return direction != 0 || (x == 0 && y == 0);
+  }
 }
