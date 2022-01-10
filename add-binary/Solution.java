edddece45beb7396a415a482b9d7abf53c8c678e
Kai Wang <charles.wangkai@gmail.com>
2022-01-10 12:59:46 +0800

    Add Binary

--- add-binary/Solution.java
+++ add-binary/Solution.java
@@ -1,17 +1,21 @@
 class Solution {
-	public String addBinary(String a, String b) {
-		StringBuilder result = new StringBuilder();
-		int carry = 0;
-		for (int indexA = a.length() - 1, indexB = b.length() - 1; indexA >= 0 || indexB >= 0; --indexA, --indexB) {
-			int sum = (indexA >= 0 ? (a.charAt(indexA) - '0') : 0) + (indexB >= 0 ? (b.charAt(indexB) - '0') : 0)
-					+ carry;
-			result.append(sum % 2);
-			carry = sum / 2;
-		}
-		if (carry != 0) {
-			result.append(carry);
-		}
+  public String addBinary(String a, String b) {
+    StringBuilder result = new StringBuilder();
+    int carry = 0;
+    for (int indexA = a.length() - 1, indexB = b.length() - 1;
+        indexA >= 0 || indexB >= 0;
+        --indexA, --indexB) {
+      int sum =
+          (indexA >= 0 ? (a.charAt(indexA) - '0') : 0)
+              + (indexB >= 0 ? (b.charAt(indexB) - '0') : 0)
+              + carry;
+      result.append(sum % 2);
+      carry = sum / 2;
+    }
+    if (carry != 0) {
+      result.append(carry);
+    }
 
-		return result.reverse().toString();
-	}
+    return result.reverse().toString();
+  }
 }
