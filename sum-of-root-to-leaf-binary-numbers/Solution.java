4b09714ead95bc62cff56587878fd934c2926167
Kai Wang <charles.wangkai@gmail.com>
2022-01-11 09:04:19 +0800

    Sum of Root To Leaf Binary Numbers

--- sum-of-root-to-leaf-binary-numbers/Solution.java
+++ sum-of-root-to-leaf-binary-numbers/Solution.java
@@ -1,39 +1,38 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public int sumRootToLeaf(TreeNode root) {
-		return search(root, 0);
-	}
+  public int sumRootToLeaf(TreeNode root) {
+    return search(root, 0);
+  }
 
-	int search(TreeNode node, int parentNumber) {
-		if (node == null) {
-			return 0;
-		}
+  int search(TreeNode node, int parentNumber) {
+    if (node == null) {
+      return 0;
+    }
 
-		int currentNumber = parentNumber * 2 + node.val;
+    int currentNumber = parentNumber * 2 + node.val;
 
-		if (node.left == null && node.right == null) {
-			return currentNumber;
-		}
+    if (node.left == null && node.right == null) {
+      return currentNumber;
+    }
 
-		return search(node.left, currentNumber) + search(node.right, currentNumber);
-	}
+    return search(node.left, currentNumber) + search(node.right, currentNumber);
+  }
 }
