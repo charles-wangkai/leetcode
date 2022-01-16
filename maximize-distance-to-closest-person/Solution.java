6eca5e0443398640d93e4d97c1588c55f2fe3d58
Kai Wang <charles.wangkai@gmail.com>
2022-01-16 08:43:49 +0800

    Maximize Distance to Closest Person

--- maximize-distance-to-closest-person/Solution.java
+++ maximize-distance-to-closest-person/Solution.java
@@ -5,33 +5,32 @@ class Solution {
     int leftPersonIndex = -1;
     int[] leftDistances = new int[seats.length];
     for (int i = 0; i < leftDistances.length; ++i) {
-      if (leftPersonIndex < 0) {
+      if (seats[i] == 1) {
+        leftPersonIndex = i;
+      }
+
+      if (leftPersonIndex == -1) {
         leftDistances[i] = Integer.MAX_VALUE;
       } else {
         leftDistances[i] = i - leftPersonIndex;
-      }
-
-      if (seats[i] == 1) {
-        leftPersonIndex = i;
       }
     }
 
     int rightPersonIndex = -1;
     int[] rightDistances = new int[seats.length];
     for (int i = rightDistances.length - 1; i >= 0; --i) {
-      if (rightPersonIndex < 0) {
+      if (seats[i] == 1) {
+        rightPersonIndex = i;
+      }
+
+      if (rightPersonIndex == -1) {
         rightDistances[i] = Integer.MAX_VALUE;
       } else {
         rightDistances[i] = rightPersonIndex - i;
       }
-
-      if (seats[i] == 1) {
-        rightPersonIndex = i;
-      }
     }
 
     return IntStream.range(0, seats.length)
-        .filter(i -> seats[i] == 0)
         .map(i -> Math.min(leftDistances[i], rightDistances[i]))
         .max()
         .getAsInt();
