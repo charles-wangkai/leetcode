7ed4aeb62ecc6d9ac3eb179083bc4c942fa5820d
Kai Wang <charles.wangkai@gmail.com>
2022-01-17 11:19:56 +0800

    Word Pattern

--- word-pattern/Solution.java
+++ word-pattern/Solution.java
@@ -2,25 +2,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-	public boolean wordPattern(String pattern, String str) {
-		String[] words = str.split(" ");
-		if (words.length != pattern.length()) {
-			return false;
-		}
+  public boolean wordPattern(String pattern, String s) {
+    String[] words = s.split(" ");
+    if (words.length != pattern.length()) {
+      return false;
+    }
 
-		Map<Character, String> letterToWord = new HashMap<>();
-		for (int i = 0; i < pattern.length(); ++i) {
-			char letter = pattern.charAt(i);
-			String word = words[i];
-			if (letterToWord.containsKey(letter)) {
-				if (!letterToWord.get(letter).equals(word)) {
-					return false;
-				}
-			} else {
-				letterToWord.put(letter, word);
-			}
-		}
+    Map<Character, String> letterToWord = new HashMap<>();
+    for (int i = 0; i < pattern.length(); ++i) {
+      char letter = pattern.charAt(i);
+      if (letterToWord.containsKey(letter)) {
+        if (!letterToWord.get(letter).equals(words[i])) {
+          return false;
+        }
+      } else {
+        letterToWord.put(letter, words[i]);
+      }
+    }
 
-		return letterToWord.size() == letterToWord.values().stream().distinct().count();
-	}
+    return letterToWord.size() == letterToWord.values().stream().distinct().count();
+  }
 }
