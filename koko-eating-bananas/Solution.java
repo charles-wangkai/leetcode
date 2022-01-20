69607900f3d1fedcb4b70b3aaf287a7c49b82a53
Kai Wang <charles.wangkai@gmail.com>
2022-01-20 11:37:17 +0800

    Koko Eating Bananas

--- koko-eating-bananas/Solution.java
+++ koko-eating-bananas/Solution.java
@@ -1,28 +1,25 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int minEatingSpeed(int[] piles, int H) {
-		int result = -1;
-		int lower = 1;
-		int upper = Arrays.stream(piles).max().getAsInt();
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
-			if (canEatAll(piles, H, middle)) {
-				result = middle;
+class Solution {
+  public int minEatingSpeed(int[] piles, int h) {
+    int result = -1;
+    int lower = 1;
+    int upper = Arrays.stream(piles).max().getAsInt();
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (canEatAll(piles, h, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
 
-				upper = middle - 1;
-			} else {
-				lower = middle + 1;
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 
-	boolean canEatAll(int[] piles, int H, int eatSpeed) {
-		return Arrays.stream(piles).map(pile -> divideToCeil(pile, eatSpeed)).sum() <= H;
-	}
-
-	int divideToCeil(int x, int y) {
-		return x / y + (x % y == 0 ? 0 : 1);
-	}
+  boolean canEatAll(int[] piles, int h, int eatSpeed) {
+    return Arrays.stream(piles).map(pile -> (pile + eatSpeed - 1) / eatSpeed).asLongStream().sum()
+        <= h;
+  }
 }
