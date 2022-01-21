3ffc86224792b5b74e31b6d7a18c5859b26640f1
Kai Wang <charles.wangkai@gmail.com>
2022-01-21 10:20:20 +0800

    Gas Station

--- gas-station/Solution.java
+++ gas-station/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-	public int canCompleteCircuit(int[] gas, int[] cost) {
-		int total = 0;
-		int current = 0;
-		int startingIndex = 0;
-		for (int i = 0; i < gas.length; ++i) {
-			total += gas[i] - cost[i];
-			current += gas[i] - cost[i];
-			if (current < 0) {
-				startingIndex = i + 1;
-				current = 0;
-			}
-		}
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int total = 0;
+    int current = 0;
+    int startingIndex = 0;
+    for (int i = 0; i < gas.length; ++i) {
+      total += gas[i] - cost[i];
+      current += gas[i] - cost[i];
+      if (current < 0) {
+        startingIndex = i + 1;
+        current = 0;
+      }
+    }
 
-		return total >= 0 ? startingIndex : -1;
-	}
+    return (total >= 0) ? startingIndex : -1;
+  }
 }
