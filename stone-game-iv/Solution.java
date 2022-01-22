82950f1029f68c3ea8a9821db246ff2d0415c52a
Kai Wang <charles.wangkai@gmail.com>
2022-01-22 09:34:23 +0800

    Stone Game IV

--- stone-game-iv/Solution.java
+++ stone-game-iv/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean winnerSquareGame(int n) {
-        boolean[] wins = new boolean[n + 1];
-        for (int i = 1; i < wins.length; ++i) {
-            for (int j = 1; j * j <= i; ++j) {
-                if (!wins[i - j * j]) {
-                    wins[i] = true;
+  public boolean winnerSquareGame(int n) {
+    boolean[] wins = new boolean[n + 1];
+    for (int i = 1; i < wins.length; ++i) {
+      for (int j = 1; j * j <= i; ++j) {
+        if (!wins[i - j * j]) {
+          wins[i] = true;
 
-                    break;
-                }
-            }
+          break;
         }
-
-        return wins[n];
+      }
     }
+
+    return wins[n];
+  }
 }
