e5caef4fb7b9882c86539619fe7d4ce69dac0255
Kai Wang <charles.wangkai@gmail.com>
2022-01-23 12:46:23 +0800

    Find All Lonely Numbers in the Array

---
+++ find-all-lonely-numbers-in-the-array/Solution.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Solution {
+  public List<Integer> findLonely(int[] nums) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int num : nums) {
+      valueToCount.put(num, valueToCount.getOrDefault(num, 0) + 1);
+    }
+
+    return valueToCount.keySet().stream()
+        .filter(
+            value ->
+                valueToCount.get(value) == 1
+                    && !valueToCount.containsKey(value - 1)
+                    && !valueToCount.containsKey(value + 1))
+        .collect(Collectors.toList());
+  }
+}
