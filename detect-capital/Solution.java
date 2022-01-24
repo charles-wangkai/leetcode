baf0a2cf2333f149cfcb9efa316e2123f0f9800e
Kai Wang <charles.wangkai@gmail.com>
2022-01-24 13:02:16 +0800

    Detect Capital

--- detect-capital/Solution.java
+++ detect-capital/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
-	public boolean detectCapitalUse(String word) {
-		return word.chars().allMatch(Character::isUpperCase) || word.chars().allMatch(Character::isLowerCase)
-				|| (Character.isUpperCase(word.charAt(0))
-						&& word.substring(1).chars().allMatch(Character::isLowerCase));
-	}
+  public boolean detectCapitalUse(String word) {
+    return word.chars().allMatch(Character::isUpperCase)
+        || word.chars().allMatch(Character::isLowerCase)
+        || (Character.isUpperCase(word.charAt(0))
+            && word.substring(1).chars().allMatch(Character::isLowerCase));
+  }
 }
