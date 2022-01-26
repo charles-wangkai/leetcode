e2e7fb4d85a4fe1289ea8212a208a82bb6b05c9f
Kai Wang <charles.wangkai@gmail.com>
2022-01-26 10:37:08 +0800

    All Elements in Two Binary Search Trees

--- all-elements-in-two-binary-search-trees/Solution.java
+++ all-elements-in-two-binary-search-trees/Solution.java
@@ -3,59 +3,59 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-		List<Integer> values1 = new ArrayList<>();
-		search(values1, root1);
+  public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+    List<Integer> values1 = new ArrayList<>();
+    search(values1, root1);
 
-		List<Integer> values2 = new ArrayList<>();
-		search(values2, root2);
+    List<Integer> values2 = new ArrayList<>();
+    search(values2, root2);
 
-		return merge(values1, values2);
-	}
+    return merge(values1, values2);
+  }
 
-	void search(List<Integer> values, TreeNode node) {
-		if (node == null) {
-			return;
-		}
+  void search(List<Integer> values, TreeNode node) {
+    if (node == null) {
+      return;
+    }
 
-		search(values, node.left);
-		values.add(node.val);
-		search(values, node.right);
-	}
+    search(values, node.left);
+    values.add(node.val);
+    search(values, node.right);
+  }
 
-	List<Integer> merge(List<Integer> values1, List<Integer> values2) {
-		List<Integer> result = new ArrayList<>();
-		int index1 = 0;
-		int index2 = 0;
-		while (index1 != values1.size() || index2 != values2.size()) {
-			if (index2 == values2.size() || (index1 != values1.size() && values1.get(index1) <= values2.get(index2))) {
-				result.add(values1.get(index1));
-				++index1;
-			} else {
-				result.add(values2.get(index2));
-				++index2;
-			}
-		}
+  List<Integer> merge(List<Integer> values1, List<Integer> values2) {
+    List<Integer> result = new ArrayList<>();
+    int index1 = 0;
+    int index2 = 0;
+    while (index1 != values1.size() || index2 != values2.size()) {
+      if (index2 == values2.size()
+          || (index1 != values1.size() && values1.get(index1) <= values2.get(index2))) {
+        result.add(values1.get(index1));
+        ++index1;
+      } else {
+        result.add(values2.get(index2));
+        ++index2;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
