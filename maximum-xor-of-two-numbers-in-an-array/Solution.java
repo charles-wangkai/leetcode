2fef4a41f05d44c0943f2355de015ea302f7d464
Kai Wang <charles.wangkai@gmail.com>
2022-01-27 15:05:02 +0800

    Maximum XOR of Two Numbers in an Array

--- maximum-xor-of-two-numbers-in-an-array/Solution.java
+++ maximum-xor-of-two-numbers-in-an-array/Solution.java
@@ -3,25 +3,22 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 class Solution {
-	public int findMaximumXOR(int[] nums) {
-		int max = 0;
-		int mask = 0;
-		for (int i = 30; i >= 0; --i) {
-			mask |= 1 << i;
+  public int findMaximumXOR(int[] nums) {
+    int max = 0;
+    int mask = 0;
+    for (int i = 30; i >= 0; --i) {
+      mask |= 1 << i;
 
-			int mask_ = mask;
-			Set<Integer> prefixes = Arrays.stream(nums).map(num -> num & mask_).boxed().collect(Collectors.toSet());
+      int mask_ = mask;
+      Set<Integer> prefixes =
+          Arrays.stream(nums).map(num -> num & mask_).boxed().collect(Collectors.toSet());
 
-			int xor = max | (1 << i);
-			for (int prefix : prefixes) {
-				if (prefixes.contains(prefix ^ xor)) {
-					max = xor;
+      int xor = max | (1 << i);
+      if (prefixes.stream().anyMatch(prefix -> prefixes.contains(prefix ^ xor))) {
+        max = xor;
+      }
+    }
 
-					break;
-				}
-			}
-		}
-
-		return max;
-	}
+    return max;
+  }
 }
