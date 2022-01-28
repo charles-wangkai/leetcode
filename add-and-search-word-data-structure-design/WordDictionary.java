6ce29dd9ddb585b85efe9c7fa8daaa4d561eefc4
Kai Wang <charles.wangkai@gmail.com>
2022-01-28 10:36:12 +0800

    Design Add and Search Words Data Structure

--- add-and-search-word-data-structure-design/WordDictionary.java
+++ add-and-search-word-data-structure-design/WordDictionary.java
@@ -4,67 +4,60 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-class TrieNode {
-	// Initialize your data structure here.
-	private Map<Character, TrieNode> letterToChild;
+class WordDictionary {
+  TrieNode root = new TrieNode();
 
-	public TrieNode() {
-		letterToChild = new HashMap<>();
-	}
+  public void addWord(String word) {
+    TrieNode node = root;
+    for (char letter : word.toCharArray()) {
+      node = node.putChild(letter);
+    }
+    node.putChild(null);
+  }
 
-	public boolean hasChild(Character ch) {
-		return letterToChild.containsKey(ch);
-	}
+  public boolean search(String word) {
+    return search(word, 0, root);
+  }
 
-	public TrieNode getChild(Character ch) {
-		return letterToChild.get(ch);
-	}
+  private boolean search(String word, int index, TrieNode node) {
+    if (index == word.length()) {
+      return node.hasChild(null);
+    }
 
-	public TrieNode putChild(Character ch) {
-		letterToChild.putIfAbsent(ch, new TrieNode());
+    char ch = word.charAt(index);
 
-		return getChild(ch);
-	}
-
-	public List<TrieNode> getChildren() {
-		return letterToChild.entrySet().stream().filter(entry -> entry.getKey() != null).map(Entry::getValue)
-				.collect(Collectors.toList());
-	}
+    return (ch == '.')
+        ? node.getChildren().stream().anyMatch(child -> search(word, index + 1, child))
+        : (node.hasChild(ch) && search(word, index + 1, node.getChild(ch)));
+  }
 }
 
-class WordDictionary {
-	TrieNode root = new TrieNode();
+class TrieNode {
+  private Map<Character, TrieNode> letterToChild = new HashMap<>();
 
-	// Adds a word into the data structure.
-	public void addWord(String word) {
-		TrieNode node = root;
-		for (char letter : word.toCharArray()) {
-			node = node.putChild(letter);
-		}
-		node.putChild(null);
-	}
+  public boolean hasChild(Character ch) {
+    return letterToChild.containsKey(ch);
+  }
 
-	// Returns if the word is in the data structure. A word could
-	// contain the dot character '.' to represent any one letter.
-	public boolean search(String word) {
-		return search(word, 0, root);
-	}
+  public TrieNode getChild(Character ch) {
+    return letterToChild.get(ch);
+  }
 
-	private boolean search(String word, int index, TrieNode node) {
-		if (index == word.length()) {
-			return node.hasChild(null);
-		}
+  public TrieNode putChild(Character ch) {
+    letterToChild.putIfAbsent(ch, new TrieNode());
 
-		char ch = word.charAt(index);
-		if (ch == '.') {
-			return node.getChildren().stream().anyMatch(child -> search(word, index + 1, child));
-		} else {
-			return node.hasChild(ch) && search(word, index + 1, node.getChild(ch));
-		}
-	}
+    return getChild(ch);
+  }
+
+  public List<TrieNode> getChildren() {
+    return letterToChild.entrySet().stream()
+        .filter(entry -> entry.getKey() != null)
+        .map(Entry::getValue)
+        .collect(Collectors.toList());
+  }
 }
 
 // Your WordDictionary object will be instantiated and called as such:
-// WordDictionary wordDictionary = new WordDictionary();
-// wordDictionary.addWord("word");
-// wordDictionary.search("pattern");
+// WordDictionary obj = new WordDictionary();
+// obj.addWord(word);
+// boolean param_2 = obj.search(word);
