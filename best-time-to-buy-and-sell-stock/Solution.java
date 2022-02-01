cc7e3f521f8fe0fc1768e7fedd2b2c25ab649aab
Kai Wang <charles.wangkai@gmail.com>
2022-02-01 09:03:59 +0800

    Best Time to Buy and Sell Stock

--- best-time-to-buy-and-sell-stock/Solution.java
+++ best-time-to-buy-and-sell-stock/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-	public int maxProfit(int[] prices) {
-		int maxEarn = 0;
-		int minPrice = Integer.MAX_VALUE;
-		for (int price : prices) {
-			minPrice = Math.min(minPrice, price);
-			maxEarn = Math.max(maxEarn, price - minPrice);
-		}
+  public int maxProfit(int[] prices) {
+    int result = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int price : prices) {
+      minPrice = Math.min(minPrice, price);
+      result = Math.max(result, price - minPrice);
+    }
 
-		return maxEarn;
-	}
+    return result;
+  }
 }
