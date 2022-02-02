9c73a446569d3193f3b6e161f56eaaec5a5e51bf
Kai Wang <charles.wangkai@gmail.com>
2022-02-02 09:58:57 +0800

    Find All Anagrams in a String

--- find-all-anagrams-in-a-string/Solution.java
+++ find-all-anagrams-in-a-string/Solution.java
@@ -2,33 +2,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
-	public List<Integer> findAnagrams(String s, String p) {
-		List<Integer> indices = new ArrayList<>();
+class Solution {
+  public List<Integer> findAnagrams(String s, String p) {
+    if (s.length() < p.length()) {
+      return List.of();
+    }
 
-		if (s.length() >= p.length()) {
-			int[] letterToCount = buildLetterToCount(p);
-			int[] currentKey = buildLetterToCount(s.substring(0, p.length() - 1));
-			for (int i = p.length() - 1; i < s.length(); ++i) {
-				++currentKey[s.charAt(i) - 'a'];
+    List<Integer> result = new ArrayList<>();
+    int[] target = buildCounts(p);
+    int[] counts = buildCounts(s.substring(0, p.length() - 1));
+    for (int i = p.length() - 1; i < s.length(); ++i) {
+      ++counts[s.charAt(i) - 'a'];
 
-				if (Arrays.equals(currentKey, letterToCount)) {
-					indices.add(i - p.length() + 1);
-				}
+      if (Arrays.equals(counts, target)) {
+        result.add(i - p.length() + 1);
+      }
 
-				--currentKey[s.charAt(i - p.length() + 1) - 'a'];
-			}
-		}
+      --counts[s.charAt(i - p.length() + 1) - 'a'];
+    }
 
-		return indices;
-	}
+    return result;
+  }
 
-	int[] buildLetterToCount(String str) {
-		int[] letterToCount = new int[26];
-		for (char ch : str.toCharArray()) {
-			letterToCount[ch - 'a']++;
-		}
+  int[] buildCounts(String str) {
+    int[] counts = new int[26];
+    for (char ch : str.toCharArray()) {
+      ++counts[ch - 'a'];
+    }
 
-		return letterToCount;
-	}
+    return counts;
+  }
 }
