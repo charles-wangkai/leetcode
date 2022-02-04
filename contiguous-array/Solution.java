699921cc0685cfa802a17db7ed413c8d770d8185
Kai Wang <charles.wangkai@gmail.com>
2022-02-04 16:12:18 +0800

    Contiguous Array

--- contiguous-array/Solution.java
+++ contiguous-array/Solution.java
@@ -1,28 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int findMaxLength(int[] nums) {
-		int maxLength = 0;
+class Solution {
+  public int findMaxLength(int[] nums) {
+    int maxLength = 0;
 
-		Map<Integer, Integer> deltaToFirstIndex = new HashMap<>();
-		deltaToFirstIndex.put(0, -1);
+    Map<Integer, Integer> deltaToFirstIndex = new HashMap<>();
+    deltaToFirstIndex.put(0, -1);
 
-		int delta = 0;
-		for (int i = 0; i < nums.length; ++i) {
-			if (nums[i] == 0) {
-				++delta;
-			} else {
-				--delta;
-			}
+    int delta = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] == 0) {
+        ++delta;
+      } else {
+        --delta;
+      }
 
-			if (deltaToFirstIndex.containsKey(delta)) {
-				maxLength = Math.max(maxLength, i - deltaToFirstIndex.get(delta));
-			} else {
-				deltaToFirstIndex.put(delta, i);
-			}
-		}
+      if (deltaToFirstIndex.containsKey(delta)) {
+        maxLength = Math.max(maxLength, i - deltaToFirstIndex.get(delta));
+      } else {
+        deltaToFirstIndex.put(delta, i);
+      }
+    }
 
-		return maxLength;
-	}
+    return maxLength;
+  }
 }
