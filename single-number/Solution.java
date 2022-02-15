57e50f85fae0cd4ad467822f37aeccbf5b9fb34f
Kai Wang <charles.wangkai@gmail.com>
2022-02-15 08:38:15 +0800

    Single Number

--- single-number/Solution.java
+++ single-number/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int singleNumber(int[] A) {
-		return Arrays.stream(A).reduce((x, y) -> x ^ y).getAsInt();
-	}
+class Solution {
+  public int singleNumber(int[] nums) {
+    return Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
+  }
 }
