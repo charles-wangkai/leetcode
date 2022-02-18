a7a687f99f36d324171175f5513c26ec6375c938
Kai Wang <charles.wangkai@gmail.com>
2022-02-18 08:47:35 +0800

    Remove K Digits

--- remove-k-digits/Solution.java
+++ remove-k-digits/Solution.java
@@ -1,40 +1,40 @@
-public class Solution {
-	public String removeKdigits(String num, int k) {
-		num += '0';
+class Solution {
+  public String removeKdigits(String num, int k) {
+    num += '0';
 
-		int[] digitCounts = new int[10];
-		for (int i = 0; i < k + 1; i++) {
-			++digitCounts[num.charAt(i) - '0'];
-		}
+    int[] digitCounts = new int[10];
+    for (int i = 0; i < k + 1; ++i) {
+      ++digitCounts[num.charAt(i) - '0'];
+    }
 
-		StringBuilder result = new StringBuilder();
-		int endIndex = k + 1;
-		for (int i = 0; i < num.length() - 1; ++i) {
-			int digit = num.charAt(i) - '0';
-			int minDigit = findMinDigit(digitCounts);
+    StringBuilder result = new StringBuilder();
+    int endIndex = k + 1;
+    for (int i = 0; i < num.length() - 1; ++i) {
+      int minDigit = findMinDigit(digitCounts);
 
-			if (digit == minDigit) {
-				if (digit != 0 || result.length() != 0) {
-					result.append(digit);
-				}
+      int digit = num.charAt(i) - '0';
+      if (digit == minDigit) {
+        if (digit != 0 || result.length() != 0) {
+          result.append(digit);
+        }
 
-				if (endIndex < num.length()) {
-					digitCounts[num.charAt(endIndex) - '0']++;
-					++endIndex;
-				}
-			}
+        if (endIndex != num.length()) {
+          ++digitCounts[num.charAt(endIndex) - '0'];
+          ++endIndex;
+        }
+      }
 
-			--digitCounts[digit];
-		}
+      --digitCounts[digit];
+    }
 
-		return (result.length() == 0) ? "0" : result.toString();
-	}
+    return (result.length() == 0) ? "0" : result.toString();
+  }
 
-	int findMinDigit(int[] digitCounts) {
-		for (int i = 0;; ++i) {
-			if (digitCounts[i] != 0) {
-				return i;
-			}
-		}
-	}
+  int findMinDigit(int[] digitCounts) {
+    for (int i = 0; ; ++i) {
+      if (digitCounts[i] != 0) {
+        return i;
+      }
+    }
+  }
 }
