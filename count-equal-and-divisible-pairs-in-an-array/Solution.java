76be9a69352ec271e948be98e576dbd75789f97b
Kai Wang <charles.wangkai@gmail.com>
2022-02-20 00:08:30 +0800

    Count Equal and Divisible Pairs in an Array

---
+++ count-equal-and-divisible-pairs-in-an-array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int countPairs(int[] nums, int k) {
+    int result = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      for (int j = i + 1; j < nums.length; ++j) {
+        if (nums[i] == nums[j] && i * j % k == 0) {
+          ++result;
+        }
+      }
+    }
+
+    return result;
+  }
+}
