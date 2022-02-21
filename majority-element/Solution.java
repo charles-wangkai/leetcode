30a50126237cfe569f40f227068f1148d1deb105
Kai Wang <charles.wangkai@gmail.com>
2022-02-21 11:23:18 +0800

    Majority Element

--- majority-element/Solution.java
+++ majority-element/Solution.java
@@ -1,19 +1,19 @@
-public class Solution {
-	public int majorityElement(int[] nums) {
-		int majority = -1;
-		int count = 0;
-		for (int num : nums) {
-			if (count == 0) {
-				majority = num;
-			}
+class Solution {
+  public int majorityElement(int[] nums) {
+    int majority = -1;
+    int count = 0;
+    for (int num : nums) {
+      if (count == 0) {
+        majority = num;
+      }
 
-			if (num == majority) {
-				++count;
-			} else {
-				--count;
-			}
-		}
+      if (num == majority) {
+        ++count;
+      } else {
+        --count;
+      }
+    }
 
-		return majority;
-	}
+    return majority;
+  }
 }
