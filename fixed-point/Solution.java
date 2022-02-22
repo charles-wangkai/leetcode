1cc5e5eb91aa41a5bc17ec6766bdfb7a7d326c97
Kai Wang <charles.wangkai@gmail.com>
2022-02-22 10:08:58 +0800

    Fixed Point

--- fixed-point/Solution.java
+++ fixed-point/Solution.java
@@ -1,21 +1,21 @@
-public class Solution {
-	public int fixedPoint(int[] A) {
-		int result = -1;
-		int lowerIndex = 0;
-		int upperIndex = A.length - 1;
-		while (lowerIndex <= upperIndex) {
-			int middleIndex = (lowerIndex + upperIndex) / 2;
+class Solution {
+  public int fixedPoint(int[] arr) {
+    int result = -1;
+    int lowerIndex = 0;
+    int upperIndex = arr.length - 1;
+    while (lowerIndex <= upperIndex) {
+      int middleIndex = (lowerIndex + upperIndex) / 2;
 
-			if (A[middleIndex] > middleIndex) {
-				upperIndex = middleIndex - 1;
-			} else if (A[middleIndex] < middleIndex) {
-				lowerIndex = middleIndex + 1;
-			} else {
-				result = middleIndex;
+      if (arr[middleIndex] > middleIndex) {
+        upperIndex = middleIndex - 1;
+      } else if (arr[middleIndex] < middleIndex) {
+        lowerIndex = middleIndex + 1;
+      } else {
+        result = middleIndex;
+        upperIndex = middleIndex - 1;
+      }
+    }
 
-				upperIndex = middleIndex - 1;
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 }
