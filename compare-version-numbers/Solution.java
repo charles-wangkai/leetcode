367e358e29651c25e8664f5e18c133a35f7fe97c
Kai Wang <charles.wangkai@gmail.com>
2022-02-25 08:16:23 +0800

    Compare Version Numbers

--- compare-version-numbers/Solution.java
+++ compare-version-numbers/Solution.java
@@ -1,25 +1,23 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
-	public int compareVersion(String version1, String version2) {
-		int[] numbers1 = convertToNumbers(version1);
-		int[] numbers2 = convertToNumbers(version2);
-		for (int i = 0; i < numbers1.length || i < numbers2.length; ++i) {
-			int number1 = getNumber(numbers1, i);
-			int number2 = getNumber(numbers2, i);
-			if (number1 != number2) {
-				return Integer.compare(number1, number2);
-			}
-		}
+  public int compareVersion(String version1, String version2) {
+    int[] numbers1 = convertToNumbers(version1);
+    int[] numbers2 = convertToNumbers(version2);
 
-		return 0;
-	}
+    return IntStream.range(0, Math.max(numbers1.length, numbers2.length))
+        .map(i -> Integer.compare(getNumber(numbers1, i), getNumber(numbers2, i)))
+        .filter(x -> x != 0)
+        .findFirst()
+        .orElse(0);
+  }
 
-	int[] convertToNumbers(String version) {
-		return Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
-	}
+  int[] convertToNumbers(String version) {
+    return Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
+  }
 
-	int getNumber(int[] numbers, int index) {
-		return (index < numbers.length) ? numbers[index] : 0;
-	}
+  int getNumber(int[] numbers, int index) {
+    return (index < numbers.length) ? numbers[index] : 0;
+  }
 }
