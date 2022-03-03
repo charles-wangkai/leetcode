ff4180dfcb4a014495852768cf356a2c035ec0a6
Kai Wang <charles.wangkai@gmail.com>
2022-03-03 08:12:55 +0800

    Arithmetic Slices

--- arithmetic-slices/Solution.java
+++ arithmetic-slices/Solution.java
@@ -1,32 +1,18 @@
 class Solution {
-  public int numberOfArithmeticSlices(int[] A) {
-    int prevDiff = 0;
-    int count = 0;
+  public int numberOfArithmeticSlices(int[] nums) {
     int result = 0;
-    for (int i = 1; i < A.length; ++i) {
-      int diff = A[i] - A[i - 1];
+    int prevDiff = Integer.MIN_VALUE;
+    int count = 0;
+    for (int i = 1; i < nums.length; ++i) {
+      int diff = nums[i] - nums[i - 1];
       if (diff == prevDiff) {
         ++count;
       } else {
-        result += C(count, 2);
-
         prevDiff = diff;
         count = 1;
       }
-    }
-    result += C(count, 2);
 
-    return result;
-  }
-
-  int C(int n, int r) {
-    if (n < r) {
-      return 0;
-    }
-
-    int result = 1;
-    for (int i = 0; i < r; ++i) {
-      result = result * (n - i) / (i + 1);
+      result += Math.max(0, count - 1);
     }
 
     return result;
