af1f2f61f62c7f710caccc391817854a29e2a831
Kai Wang <charles.wangkai@gmail.com>
2022-03-06 21:21:23 +0800

    Cells in a Range on an Excel Sheet

---
+++ cells-in-a-range-on-an-excel-sheet/Solution.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+  public List<String> cellsInRange(String s) {
+    char minC = s.charAt(0);
+    char minR = s.charAt(1);
+    char maxC = s.charAt(3);
+    char maxR = s.charAt(4);
+
+    return IntStream.rangeClosed(minC, maxC)
+        .boxed()
+        .flatMap(c -> IntStream.rangeClosed(minR, maxR).mapToObj(r -> String.format("%c%c", c, r)))
+        .collect(Collectors.toList());
+  }
+}
