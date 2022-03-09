940899a4145634fd597cc5334a84ac9a96bd144f
Kai Wang <charles.wangkai@gmail.com>
2022-03-09 08:24:50 +0800

    Strobogrammatic Number II

--- strobogrammatic-number-ii/Solution.java
+++ strobogrammatic-number-ii/Solution.java
@@ -1,31 +1,33 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public List<String> findStrobogrammatic(int n) {
-		return searchStrobogrammatic(n, false);
-	}
+class Solution {
+  static final int[] LEFTS = {0, 1, 6, 8, 9};
+  static final int[] RIGHTS = {0, 1, 9, 8, 6};
 
-	public List<String> searchStrobogrammatic(int n, boolean allowLeadingZero) {
-		List<String> result = new ArrayList<String>();
-		if (n == 0) {
-			result.add("");
-		} else if (n == 1) {
-			result.add("0");
-			result.add("1");
-			result.add("8");
-		} else {
-			List<String> subResult = searchStrobogrammatic(n - 2, true);
-			for (String oneSubResult : subResult) {
-				if (allowLeadingZero) {
-					result.add("0" + oneSubResult + "0");
-				}
-				result.add("1" + oneSubResult + "1");
-				result.add("8" + oneSubResult + "8");
-				result.add("6" + oneSubResult + "9");
-				result.add("9" + oneSubResult + "6");
-			}
-		}
-		return result;
-	}
+  public List<String> findStrobogrammatic(int n) {
+    return search(n, false);
+  }
+
+  public List<String> search(int n, boolean allowLeadingZero) {
+    if (n == 0) {
+      return List.of("");
+    }
+    if (n == 1) {
+      return IntStream.range(0, LEFTS.length)
+          .filter(i -> LEFTS[i] == RIGHTS[i])
+          .map(i -> LEFTS[i])
+          .mapToObj(String::valueOf)
+          .collect(Collectors.toList());
+    }
+
+    return search(n - 2, true).stream()
+        .flatMap(
+            subResult ->
+                IntStream.range(0, LEFTS.length)
+                    .filter(i -> allowLeadingZero || LEFTS[i] != 0)
+                    .mapToObj(i -> String.format("%d%s%d", LEFTS[i], subResult, RIGHTS[i])))
+        .collect(Collectors.toList());
+  }
 }
