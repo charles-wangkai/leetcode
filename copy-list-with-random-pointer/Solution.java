41c64b86fe65508c98bf3e53be97b9c01319441f
Kai Wang <charles.wangkai@gmail.com>
2022-03-12 22:09:59 +0800

    Copy List with Random Pointer

--- copy-list-with-random-pointer/Solution.java
+++ copy-list-with-random-pointer/Solution.java
@@ -20,21 +20,21 @@ class Solution {
       return null;
     }
 
-    Map<Node, Node> cloneMap = new HashMap<>();
+    Map<Node, Node> originToCloned = new HashMap<>();
 
     Node clonedHead = new Node(head.val);
-    cloneMap.put(head, clonedHead);
+    originToCloned.put(head, clonedHead);
     for (Node node = head, clonedNode = clonedHead;
         node.next != null;
         node = node.next, clonedNode = clonedNode.next) {
       clonedNode.next = new Node(node.next.val);
-      cloneMap.put(node.next, clonedNode.next);
+      originToCloned.put(node.next, clonedNode.next);
     }
 
     for (Node node = head, clonedNode = clonedHead;
         node != null;
         node = node.next, clonedNode = clonedNode.next) {
-      clonedNode.random = cloneMap.get(node.random);
+      clonedNode.random = originToCloned.get(node.random);
     }
 
     return clonedHead;
