1a2f6ec301ad962917a583fcf0d7487d9ae761ff
Kai Wang <charles.wangkai@gmail.com>
2022-03-13 12:10:14 +0800

    Find All K-Distant Indices in an Array

---
+++ find-all-k-distant-indices-in-an-array/Solution.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+  public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+    return IntStream.range(0, nums.length)
+        .filter(
+            i ->
+                IntStream.rangeClosed(i - k, i + k)
+                    .anyMatch(j -> j >= 0 && j < nums.length && nums[j] == key))
+        .boxed()
+        .collect(Collectors.toList());
+  }
+}
