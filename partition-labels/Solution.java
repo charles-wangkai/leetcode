dfaa5b5c3d0be811861660fe03414e61b91fdd02
Kai Wang <charles.wangkai@gmail.com>
2022-03-21 09:43:10 +0800

    Partition Labels

--- partition-labels/Solution.java
+++ partition-labels/Solution.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-	public List<Integer> partitionLabels(String S) {
-		int[] lastIndices = new int[26];
-		for (int i = 0; i < S.length(); ++i) {
-			lastIndices[S.charAt(i) - 'a'] = i;
-		}
+  public List<Integer> partitionLabels(String s) {
+    int[] lastIndices = new int[26];
+    for (int i = 0; i < s.length(); ++i) {
+      lastIndices[s.charAt(i) - 'a'] = i;
+    }
 
-		List<Integer> sizes = new ArrayList<>();
-		int beginIndex = 0;
-		int endIndex = -1;
-		for (int i = 0; i < S.length(); ++i) {
-			endIndex = Math.max(endIndex, lastIndices[S.charAt(i) - 'a']);
+    List<Integer> sizes = new ArrayList<>();
+    int beginIndex = 0;
+    int endIndex = -1;
+    for (int i = 0; i < s.length(); ++i) {
+      endIndex = Math.max(endIndex, lastIndices[s.charAt(i) - 'a']);
 
-			if (endIndex == i) {
-				sizes.add(endIndex - beginIndex + 1);
-				beginIndex = endIndex + 1;
-			}
-		}
+      if (endIndex == i) {
+        sizes.add(endIndex - beginIndex + 1);
+        beginIndex = endIndex + 1;
+      }
+    }
 
-		return sizes;
-	}
+    return sizes;
+  }
 }
