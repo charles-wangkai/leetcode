ea41126422e3a90b04a77b7eff8fe3196dfbf8cf
Kai Wang <charles.wangkai@gmail.com>
2022-03-25 09:30:17 +0800

    Two City Scheduling

--- two-city-scheduling/Solution.java
+++ two-city-scheduling/Solution.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
 public class Solution {
-	public int twoCitySchedCost(int[][] costs) {
-		Arrays.sort(costs, (cost1, cost2) -> Integer.compare(cost1[0] - cost1[1], cost2[0] - cost2[1]));
+  public int twoCitySchedCost(int[][] costs) {
+    Arrays.sort(costs, Comparator.comparing(cost -> cost[0] - cost[1]));
 
-		return IntStream.range(0, costs.length).map(i -> (i < costs.length / 2) ? costs[i][0] : costs[i][1]).sum();
-	}
+    return IntStream.range(0, costs.length)
+        .map(i -> (i < costs.length / 2) ? costs[i][0] : costs[i][1])
+        .sum();
+  }
 }
