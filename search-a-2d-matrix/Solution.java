15ea8ec1c1dba77117ce56deccd4a74289c9e540
Kai Wang <charles.wangkai@gmail.com>
2022-03-30 08:56:24 +0800

    Search a 2D Matrix

--- search-a-2d-matrix/Solution.java
+++ search-a-2d-matrix/Solution.java
@@ -1,24 +1,15 @@
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 class Solution {
   public boolean searchMatrix(int[][] matrix, int target) {
-    int row = matrix.length;
-    if (row == 0) {
-      return false;
-    }
-    int col = matrix[0].length;
-    if (col == 0 || target < matrix[0][0]) {
-      return false;
-    }
-
-    int rowIndex = findIndex(IntStream.range(0, row).map(r -> matrix[r][0]).toArray(), target);
+    int rowIndex = findIndex(Arrays.stream(matrix).mapToInt(row -> row[0]).toArray(), target);
     int colIndex = findIndex(matrix[rowIndex], target);
 
     return matrix[rowIndex][colIndex] == target;
   }
 
   int findIndex(int[] a, int target) {
-    int result = -1;
+    int result = 0;
     int lower = 0;
     int upper = a.length - 1;
     while (lower <= upper) {
