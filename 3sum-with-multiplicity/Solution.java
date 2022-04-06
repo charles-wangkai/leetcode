1c79ec3a496dea3044bd67a6fbad2bb6f8a7c36c
Kai Wang <charles.wangkai@gmail.com>
2022-04-06 10:59:11 +0800

    3Sum With Multiplicity

--- 3sum-with-multiplicity/Solution.java
+++ 3sum-with-multiplicity/Solution.java
@@ -4,32 +4,18 @@ import java.util.Map;
 class Solution {
   static final int MODULUS = 1_000_000_007;
 
-  public int threeSumMulti(int[] A, int target) {
-    Map<Integer, Integer> valueToCount = new HashMap<>();
-    for (int value : A) {
-      valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
-    }
-
+  public int threeSumMulti(int[] arr, int target) {
     int result = 0;
-    for (int i = 0; i < A.length; ++i) {
-      update(valueToCount, A[i], -1);
-
-      for (int j = i + 1; j < A.length; ++j) {
-        update(valueToCount, A[j], -1);
-
-        result = addMod(result, valueToCount.getOrDefault(target - A[i] - A[j], 0));
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int i = 0; i < arr.length; ++i) {
+      for (int j = i + 1; j < arr.length; ++j) {
+        result = addMod(result, valueToCount.getOrDefault(target - arr[i] - arr[j], 0));
       }
 
-      for (int j = i + 1; j < A.length; ++j) {
-        update(valueToCount, A[j], 1);
-      }
+      valueToCount.put(arr[i], valueToCount.getOrDefault(arr[i], 0) + 1);
     }
 
     return result;
-  }
-
-  void update(Map<Integer, Integer> valueToCount, int value, int delta) {
-    valueToCount.put(value, valueToCount.getOrDefault(value, 0) + delta);
   }
 
   int addMod(int x, int y) {
