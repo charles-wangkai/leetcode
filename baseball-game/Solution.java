96a761d2ab116bfae072d1b1d7782a31c1deabe0
Kai Wang <charles.wangkai@gmail.com>
2022-04-10 13:07:03 +0800

    Baseball Game

--- baseball-game/Solution.java
+++ baseball-game/Solution.java
@@ -1,20 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public int calPoints(String[] ops) {
-		List<Integer> points = new ArrayList<Integer>();
-		for (String op : ops) {
-			if (op.equals("+")) {
-				points.add(points.get(points.size() - 2) + points.get(points.size() - 1));
-			} else if (op.equals("D")) {
-				points.add(points.get(points.size() - 1) * 2);
-			} else if (op.equals("C")) {
-				points.remove(points.size() - 1);
-			} else {
-				points.add(Integer.parseInt(op));
-			}
-		}
-		return points.stream().mapToInt(x -> x).sum();
-	}
+class Solution {
+  public int calPoints(String[] ops) {
+    List<Integer> points = new ArrayList<>();
+    for (String op : ops) {
+      if (op.equals("+")) {
+        points.add(points.get(points.size() - 2) + points.get(points.size() - 1));
+      } else if (op.equals("D")) {
+        points.add(points.get(points.size() - 1) * 2);
+      } else if (op.equals("C")) {
+        points.remove(points.size() - 1);
+      } else {
+        points.add(Integer.parseInt(op));
+      }
+    }
+
+    return points.stream().mapToInt(x -> x).sum();
+  }
 }
