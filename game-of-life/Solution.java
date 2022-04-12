c7a46a43119437df6e5c8e77fa8ce99eceb5e996
Kai Wang <charles.wangkai@gmail.com>
2022-04-12 09:02:30 +0800

    Game of Life

--- game-of-life/Solution.java
+++ game-of-life/Solution.java
@@ -1,13 +1,10 @@
 class Solution {
-  static final int[] R_OFFSETS = {-1, -1, 0, 1, 1, 1, 0, -1};
-  static final int[] C_OFFSETS = {0, 1, 1, 1, 0, -1, -1, -1};
-
   public void gameOfLife(int[][] board) {
-    int row = board.length;
-    int col = board[0].length;
+    int m = board.length;
+    int n = board[0].length;
 
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
         int neighbor = countNeighbor(board, r, c);
         if (getPrevious(board, r, c) == 1) {
           if (neighbor == 2 || neighbor == 3) {
@@ -25,27 +22,30 @@ class Solution {
       }
     }
 
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
         board[r][c] = getCurrent(board, r, c);
       }
     }
   }
 
   int countNeighbor(int[][] board, int r, int c) {
-    int row = board.length;
-    int col = board[0].length;
+    int m = board.length;
+    int n = board[0].length;
 
     int result = 0;
-    for (int i = 0; i < R_OFFSETS.length; ++i) {
-      int neighborR = r + R_OFFSETS[i];
-      int neighborC = c + C_OFFSETS[i];
-      if (neighborR >= 0
-          && neighborR < row
-          && neighborC >= 0
-          && neighborC < col
-          && getPrevious(board, neighborR, neighborC) == 1) {
-        ++result;
+    for (int dr = -1; dr <= 1; ++dr) {
+      int neighborR = r + dr;
+      for (int dc = -1; dc <= 1; ++dc) {
+        int neighborC = c + dc;
+        if ((dr != 0 || dc != 0)
+            && neighborR >= 0
+            && neighborR < m
+            && neighborC >= 0
+            && neighborC < n
+            && getPrevious(board, neighborR, neighborC) == 1) {
+          ++result;
+        }
       }
     }
 
