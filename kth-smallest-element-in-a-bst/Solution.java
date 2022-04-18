7259deab4d0fa4c2f848ba619f6eb897964f9c9c
Kai Wang <charles.wangkai@gmail.com>
2022-04-18 09:41:58 +0800

    Kth Smallest Element in a BST

--- kth-smallest-element-in-a-bst/Solution.java
+++ kth-smallest-element-in-a-bst/Solution.java
@@ -1,37 +1,45 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	int kthMin;
-	int sequence;
+class Solution {
+  int kthMin;
+  int sequence;
 
-	public int kthSmallest(TreeNode root, int k) {
-		sequence = 0;
-		search(root, k);
+  public int kthSmallest(TreeNode root, int k) {
+    sequence = 0;
+    search(root, k);
 
-		return kthMin;
-	}
+    return kthMin;
+  }
 
-	void search(TreeNode node, int k) {
-		if (node == null || sequence == k) {
-			return;
-		}
+  void search(TreeNode node, int k) {
+    if (node == null || sequence == k) {
+      return;
+    }
 
-		search(node.left, k);
+    search(node.left, k);
 
-		++sequence;
-		if (sequence == k) {
-			kthMin = node.val;
-		}
+    ++sequence;
+    if (sequence == k) {
+      kthMin = node.val;
+    }
 
-		search(node.right, k);
-	}
+    search(node.right, k);
+  }
 }
