5c54b8b133fe739babb5f7d11ea22d83b104a90a
Kai Wang <charles.wangkai@gmail.com>
2022-04-21 08:54:51 +0800

    Design HashSet

--- design-hashset/MyHashSet.java
+++ design-hashset/MyHashSet.java
@@ -1,20 +1,19 @@
 class MyHashSet {
-	private static final int SIZE = 1_000_001;
+  private static final int SIZE = 1_000_001;
 
-	private boolean[] used = new boolean[SIZE];
+  private boolean[] used = new boolean[SIZE];
 
-	public void add(int key) {
-		used[key] = true;
-	}
+  public void add(int key) {
+    used[key] = true;
+  }
 
-	public void remove(int key) {
-		used[key] = false;
-	}
+  public void remove(int key) {
+    used[key] = false;
+  }
 
-	/** Returns true if this set contains the specified element */
-	public boolean contains(int key) {
-		return used[key];
-	}
+  public boolean contains(int key) {
+    return used[key];
+  }
 }
 
 // Your MyHashSet object will be instantiated and called as such:
