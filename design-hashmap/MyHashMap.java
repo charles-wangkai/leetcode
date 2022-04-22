63f24ac72c9a78cbc09d580707690bb7ce1143ed
Kai Wang <charles.wangkai@gmail.com>
2022-04-22 08:36:30 +0800

    Design HashMap

--- design-hashmap/MyHashMap.java
+++ design-hashmap/MyHashMap.java
@@ -1,22 +1,16 @@
 class MyHashMap {
-  private static final int SIZE = 1000001;
+  private static final int SIZE = 1_000_001;
 
   private Integer[] values = new Integer[SIZE];
 
-  /** value will always be positive. */
   public void put(int key, int value) {
     values[key] = value;
   }
 
-  /**
-   * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping
-   * for the key
-   */
   public int get(int key) {
     return (values[key] == null) ? -1 : values[key];
   }
 
-  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
   public void remove(int key) {
     values[key] = null;
   }
