ad80dd2442c4a0152c3325ec2a0bd8d07f93ef23
Kai Wang <charles.wangkai@gmail.com>
2022-04-24 08:07:30 +0800

    Design Underground System

--- design-underground-system/UndergroundSystem.java
+++ design-underground-system/UndergroundSystem.java
@@ -3,51 +3,51 @@ import java.util.Map;
 import java.util.Objects;
 
 class UndergroundSystem {
-    private Map<Pair, Long> pairToTotalTime = new HashMap<>();
-    private Map<Pair, Integer> pairToCount = new HashMap<>();
-    private Map<Integer, String> idToStartStation = new HashMap<>();
-    private Map<Integer, Integer> idToStartTime = new HashMap<>();
+  private Map<Pair, Long> pairToTotalTime = new HashMap<>();
+  private Map<Pair, Integer> pairToCount = new HashMap<>();
+  private Map<Integer, String> idToStartStation = new HashMap<>();
+  private Map<Integer, Integer> idToStartTime = new HashMap<>();
 
-    public void checkIn(int id, String stationName, int t) {
-        idToStartStation.put(id, stationName);
-        idToStartTime.put(id, t);
-    }
+  public void checkIn(int id, String stationName, int t) {
+    idToStartStation.put(id, stationName);
+    idToStartTime.put(id, t);
+  }
 
-    public void checkOut(int id, String stationName, int t) {
-        Pair pair = new Pair(idToStartStation.remove(id), stationName);
-        int time = t - idToStartTime.remove(id);
+  public void checkOut(int id, String stationName, int t) {
+    Pair pair = new Pair(idToStartStation.remove(id), stationName);
+    int time = t - idToStartTime.remove(id);
 
-        pairToTotalTime.put(pair, pairToTotalTime.getOrDefault(pair, 0L) + time);
-        pairToCount.put(pair, pairToCount.getOrDefault(pair, 0) + 1);
-    }
+    pairToTotalTime.put(pair, pairToTotalTime.getOrDefault(pair, 0L) + time);
+    pairToCount.put(pair, pairToCount.getOrDefault(pair, 0) + 1);
+  }
 
-    public double getAverageTime(String startStation, String endStation) {
-        Pair pair = new Pair(startStation, endStation);
+  public double getAverageTime(String startStation, String endStation) {
+    Pair pair = new Pair(startStation, endStation);
 
-        return (double) pairToTotalTime.get(pair) / pairToCount.get(pair);
-    }
+    return (double) pairToTotalTime.get(pair) / pairToCount.get(pair);
+  }
 }
 
 class Pair {
-    String startStation;
-    String endStation;
+  String startStation;
+  String endStation;
 
-    Pair(String startStation, String endStation) {
-        this.startStation = startStation;
-        this.endStation = endStation;
-    }
+  Pair(String startStation, String endStation) {
+    this.startStation = startStation;
+    this.endStation = endStation;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(startStation, endStation);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(startStation, endStation);
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        Pair other = (Pair) obj;
+  @Override
+  public boolean equals(Object obj) {
+    Pair other = (Pair) obj;
 
-        return startStation.equals(other.startStation) && endStation.equals(other.endStation);
-    }
+    return startStation.equals(other.startStation) && endStation.equals(other.endStation);
+  }
 }
 
 // Your UndergroundSystem object will be instantiated and called as such:
