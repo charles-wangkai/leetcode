29a1d5588b4900cff209df6c92624d129770b544
Kai Wang <charles.wangkai@gmail.com>
2022-04-27 08:46:12 +0800

    Smallest String With Swaps

--- smallest-string-with-swaps/Solution.java
+++ smallest-string-with-swaps/Solution.java
@@ -5,58 +5,56 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-		int[] parents = new int[s.length()];
-		Arrays.fill(parents, -1);
+class Solution {
+  public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+    int[] parents = new int[s.length()];
+    Arrays.fill(parents, -1);
 
-		for (List<Integer> pair : pairs) {
-			int a = pair.get(0);
-			int b = pair.get(1);
+    for (List<Integer> pair : pairs) {
+      int a = pair.get(0);
+      int b = pair.get(1);
 
-			int rootA = findRoot(parents, a);
-			int rootB = findRoot(parents, b);
-			if (rootA != rootB) {
-				parents[rootB] = rootA;
-			}
-		}
+      int rootA = findRoot(parents, a);
+      int rootB = findRoot(parents, b);
+      if (rootA != rootB) {
+        parents[rootB] = rootA;
+      }
+    }
 
-		Map<Integer, List<Integer>> rootToIndices = new HashMap<>();
-		for (int i = 0; i < s.length(); i++) {
-			int root = findRoot(parents, i);
-			if (!rootToIndices.containsKey(root)) {
-				rootToIndices.put(root, new ArrayList<>());
-			}
+    Map<Integer, List<Integer>> rootToIndices = new HashMap<>();
+    for (int i = 0; i < s.length(); ++i) {
+      int root = findRoot(parents, i);
+      rootToIndices.putIfAbsent(root, new ArrayList<>());
+      rootToIndices.get(root).add(i);
+    }
 
-			rootToIndices.get(root).add(i);
-		}
+    char[] result = new char[s.length()];
+    for (List<Integer> indices : rootToIndices.values()) {
+      List<Character> letters =
+          indices.stream().map(s::charAt).sorted().collect(Collectors.toList());
 
-		char[] result = new char[s.length()];
-		for (List<Integer> indices : rootToIndices.values()) {
-			List<Character> letters = indices.stream().map(s::charAt).sorted().collect(Collectors.toList());
+      for (int i = 0; i < indices.size(); ++i) {
+        result[indices.get(i)] = letters.get(i);
+      }
+    }
 
-			for (int i = 0; i < indices.size(); i++) {
-				result[indices.get(i)] = letters.get(i);
-			}
-		}
+    return new String(result);
+  }
 
-		return new String(result);
-	}
+  int findRoot(int[] parents, int node) {
+    int root = node;
+    while (parents[root] != -1) {
+      root = parents[root];
+    }
 
-	int findRoot(int[] parents, int node) {
-		int root = node;
-		while (parents[root] != -1) {
-			root = parents[root];
-		}
+    int p = node;
+    while (p != root) {
+      int next = parents[p];
+      parents[p] = root;
 
-		int p = node;
-		while (p != root) {
-			int next = parents[p];
-			parents[p] = root;
+      p = next;
+    }
 
-			p = next;
-		}
-
-		return root;
-	}
+    return root;
+  }
 }
