c73e9066fa737d8ef9bd70e02c5d263bcc63bb7e
Kai Wang <charles.wangkai@gmail.com>
2022-04-30 08:40:05 +0800

    Evaluate Division

--- evaluate-division/Solution.java
+++ evaluate-division/Solution.java
@@ -1,59 +1,56 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Solution {
-	public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-		List<String> variables = new ArrayList<>();
-		Map<String, Integer> variableToIndex = new HashMap<>();
-		for (List<String> equation : equations) {
-			for (String variable : equation) {
-				if (!variableToIndex.containsKey(variable)) {
-					variables.add(variable);
-					variableToIndex.put(variable, variables.size() - 1);
-				}
-			}
-		}
+  public double[] calcEquation(
+      List<List<String>> equations, double[] values, List<List<String>> queries) {
+    Map<String, Integer> variableToIndex = new HashMap<>();
+    for (List<String> equation : equations) {
+      for (String variable : equation) {
+        variableToIndex.putIfAbsent(variable, variableToIndex.size());
+      }
+    }
 
-		int n = variables.size();
-		Double[][] matrix = new Double[n][n];
+    Double[][] matrix = new Double[variableToIndex.size()][variableToIndex.size()];
 
-		for (int i = 0; i < equations.size(); ++i) {
-			String numerator = equations.get(i).get(0);
-			String denominator = equations.get(i).get(1);
+    for (int i = 0; i < equations.size(); ++i) {
+      int numerIndex = variableToIndex.get(equations.get(i).get(0));
+      int denomIndex = variableToIndex.get(equations.get(i).get(1));
 
-			matrix[variableToIndex.get(numerator)][variableToIndex.get(denominator)] = values[i];
-			matrix[variableToIndex.get(denominator)][variableToIndex.get(numerator)] = 1 / values[i];
-		}
+      matrix[numerIndex][denomIndex] = values[i];
+      matrix[denomIndex][numerIndex] = 1 / values[i];
+    }
 
-		computeClosure(matrix);
+    computeClosure(matrix);
 
-		double[] results = new double[queries.size()];
-		for (int i = 0; i < results.length; ++i) {
-			String numerator = queries.get(i).get(0);
-			String denominator = queries.get(i).get(1);
+    return queries.stream()
+        .mapToDouble(
+            query -> {
+              String numer = query.get(0);
+              String denom = query.get(1);
 
-			Double result = (variableToIndex.containsKey(numerator) && variableToIndex.containsKey(denominator))
-					? matrix[variableToIndex.get(numerator)][variableToIndex.get(denominator)]
-					: null;
+              Double result =
+                  (variableToIndex.containsKey(numer) && variableToIndex.containsKey(denom))
+                      ? matrix[variableToIndex.get(numer)][variableToIndex.get(denom)]
+                      : null;
 
-			results[i] = (result == null) ? -1 : result;
-		}
-		return results;
-	}
+              return (result == null) ? -1 : result;
+            })
+        .toArray();
+  }
 
-	void computeClosure(Double[][] matrix) {
-		int n = matrix.length;
+  void computeClosure(Double[][] matrix) {
+    int n = matrix.length;
 
-		for (int k = 0; k < n; ++k) {
-			for (int i = 0; i < n; ++i) {
-				for (int j = 0; j < n; ++j) {
-					if (matrix[i][k] != null && matrix[k][j] != null) {
-						matrix[i][j] = matrix[i][k] * matrix[k][j];
-					}
-				}
-			}
-		}
-	}
+    for (int k = 0; k < n; ++k) {
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          if (matrix[i][k] != null && matrix[k][j] != null) {
+            matrix[i][j] = matrix[i][k] * matrix[k][j];
+          }
+        }
+      }
+    }
+  }
 }
