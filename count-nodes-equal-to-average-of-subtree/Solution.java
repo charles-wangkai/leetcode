79f2556bf748e108569aaa83fe9eb1a5d1e87c8c
Kai Wang <charles.wangkai@gmail.com>
2022-05-08 22:19:19 +0800

    Count Nodes Equal to Average of Subtree

---
+++ count-nodes-equal-to-average-of-subtree/Solution.java
@@ -0,0 +1,54 @@
+// Definition for a binary tree node.
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public int averageOfSubtree(TreeNode root) {
+    return search(root).sameValueAsAvgNum;
+  }
+
+  Outcome search(TreeNode node) {
+    if (node == null) {
+      return new Outcome(0, 0, 0);
+    }
+
+    Outcome leftOutcome = search(node.left);
+    Outcome rightOutcome = search(node.right);
+
+    int sum = node.val + leftOutcome.sum + rightOutcome.sum;
+    int count = 1 + leftOutcome.count + rightOutcome.count;
+    int sameValueAsAvgNum =
+        ((node.val == sum / count) ? 1 : 0)
+            + leftOutcome.sameValueAsAvgNum
+            + rightOutcome.sameValueAsAvgNum;
+
+    return new Outcome(sum, count, sameValueAsAvgNum);
+  }
+}
+
+class Outcome {
+  int sum;
+  int count;
+  int sameValueAsAvgNum;
+
+  Outcome(int sum, int count, int sameValueAsAvgNum) {
+    this.sum = sum;
+    this.count = count;
+    this.sameValueAsAvgNum = sameValueAsAvgNum;
+  }
+}
