b0cc6649d77896fae05160291bd6bdb7e3bd7774
Kai Wang <charles.wangkai@gmail.com>
2022-05-11 09:44:27 +0800

    Count Sorted Vowel Strings

--- count-sorted-vowel-strings/Solution.java
+++ count-sorted-vowel-strings/Solution.java
@@ -5,7 +5,7 @@ class Solution {
   public int countVowelStrings(int n) {
     int[] counts = IntStream.range(0, 5).map(i -> 1).toArray();
     for (int i = 0; i < n - 1; ++i) {
-      int[] nextCounts = new int[5];
+      int[] nextCounts = new int[counts.length];
       for (int j = 0; j < nextCounts.length; ++j) {
         for (int k = 0; k <= j; ++k) {
           nextCounts[j] += counts[k];
