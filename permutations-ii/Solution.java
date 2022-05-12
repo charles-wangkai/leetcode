28bb5ad19566d5a3d5b0645cc43d37e262b9a0da
Kai Wang <charles.wangkai@gmail.com>
2022-05-12 10:23:43 +0800

    Permutations II

--- permutations-ii/Solution.java
+++ permutations-ii/Solution.java
@@ -1,36 +1,34 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Solution {
-  public List<List<Integer>> permuteUnique(int[] num) {
+  public List<List<Integer>> permuteUnique(int[] nums) {
     List<List<Integer>> permutations = new ArrayList<>();
-    search(permutations, num, 0);
+    search(permutations, nums, 0);
 
     return permutations;
   }
 
-  void search(List<List<Integer>> permutations, int[] num, int index) {
-    if (index == num.length) {
-      permutations.add(
-          IntStream.range(0, num.length).map(i -> num[i]).boxed().collect(Collectors.toList()));
+  void search(List<List<Integer>> permutations, int[] nums, int index) {
+    if (index == nums.length) {
+      permutations.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
 
       return;
     }
 
-    Set<Integer> used = new HashSet<>();
-    for (int i = index; i < num.length; ++i) {
-      if (used.contains(num[i])) {
-        continue;
-      }
-      used.add(num[i]);
+    Set<Integer> seen = new HashSet<>();
+    for (int i = index; i < nums.length; ++i) {
+      if (!seen.contains(nums[i])) {
+        seen.add(nums[i]);
 
-      swap(num, index, i);
-      search(permutations, num, index + 1);
-      swap(num, index, i);
+        swap(nums, index, i);
+        search(permutations, nums, index + 1);
+        swap(nums, index, i);
+      }
     }
   }
 
