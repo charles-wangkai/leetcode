21abf6e939d664874f188c0f9e28f1bf3bcfefad
Kai Wang <charles.wangkai@gmail.com>
2022-05-17 16:27:37 +0800

    Find Resultant Array After Removing Anagrams

---
+++ find-resultant-array-after-removing-anagrams/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+  public List<String> removeAnagrams(String[] words) {
+    String[] keys = Arrays.stream(words).map(this::buildKey).toArray(String[]::new);
+
+    return IntStream.range(0, keys.length)
+        .filter(i -> i == 0 || !keys[i].equals(keys[i - 1]))
+        .mapToObj(i -> words[i])
+        .collect(Collectors.toList());
+  }
+
+  String buildKey(String word) {
+    return word.chars()
+        .sorted()
+        .mapToObj(c -> String.valueOf((char) c))
+        .collect(Collectors.joining());
+  }
+}
