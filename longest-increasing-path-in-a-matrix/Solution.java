9c3ec90f1a2683167050a8f2df5df38ed1678b8a
Kai Wang <charles.wangkai@gmail.com>
2022-05-19 09:34:33 +0800

    Longest Increasing Path in a Matrix

--- longest-increasing-path-in-a-matrix/Solution.java
+++ longest-increasing-path-in-a-matrix/Solution.java
@@ -3,13 +3,13 @@ class Solution {
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
   public int longestIncreasingPath(int[][] matrix) {
-    int row = matrix.length;
-    int col = matrix[0].length;
+    int m = matrix.length;
+    int n = matrix[0].length;
 
-    int[][] maxLengths = new int[row][col];
+    int[][] maxLengths = new int[m][n];
     int result = 0;
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
         result = Math.max(result, search(matrix, maxLengths, r, c));
       }
     }
@@ -18,19 +18,15 @@ class Solution {
   }
 
   int search(int[][] matrix, int[][] maxLengths, int r, int c) {
-    int row = matrix.length;
-    int col = matrix[0].length;
+    int m = matrix.length;
+    int n = matrix[0].length;
 
     if (maxLengths[r][c] == 0) {
       maxLengths[r][c] = 1;
       for (int i = 0; i < R_OFFSETS.length; ++i) {
         int adjR = r + R_OFFSETS[i];
         int adjC = c + C_OFFSETS[i];
-        if (adjR >= 0
-            && adjR < row
-            && adjC >= 0
-            && adjC < col
-            && matrix[adjR][adjC] > matrix[r][c]) {
+        if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && matrix[adjR][adjC] > matrix[r][c]) {
           maxLengths[r][c] = Math.max(maxLengths[r][c], 1 + search(matrix, maxLengths, adjR, adjC));
         }
       }
