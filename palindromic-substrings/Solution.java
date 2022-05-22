48cff64d0011041b5d31a706e792bdcee1a1bd84
Kai Wang <charles.wangkai@gmail.com>
2022-05-22 08:41:52 +0800

    Palindromic Substrings

--- palindromic-substrings/Solution.java
+++ palindromic-substrings/Solution.java
@@ -2,12 +2,12 @@ class Solution {
   public int countSubstrings(String s) {
     int result = 0;
     for (int i = 0; i < s.length(); ++i) {
-      for (int j = i, k = i; j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k); --j, ++k) {
-        ++result;
-      }
-
-      for (int j = i, k = i + 1; j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k); --j, ++k) {
-        ++result;
+      for (int offset = 0; offset <= 1; ++offset) {
+        for (int j = i, k = i + offset;
+            j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k);
+            --j, ++k) {
+          ++result;
+        }
       }
     }
 
