1ce1eeec24993e639f1c336998eaf50feb0a1f5b
Kai Wang <charles.wangkai@gmail.com>
2022-06-01 17:22:01 +0800

    Minimum Obstacle Removal to Reach Corner

---
+++ minimum-obstacle-removal-to-reach-corner/Solution.java
@@ -0,0 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
+
+  public int minimumObstacles(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+
+    int[][] distances = new int[m][n];
+    for (int r = 0; r < distances.length; ++r) {
+      Arrays.fill(distances[r], -1);
+    }
+
+    Deque<Point> deque = new ArrayDeque<>();
+    distances[0][0] = 0;
+    deque.offer(new Point(0, 0));
+    while (!deque.isEmpty()) {
+      Point head = deque.pollFirst();
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r + R_OFFSETS[i];
+        int adjC = head.c + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && distances[adjR][adjC] == -1) {
+          distances[adjR][adjC] = distances[head.r][head.c] + grid[adjR][adjC];
+          if (grid[adjR][adjC] == 0) {
+            deque.offerFirst(new Point(adjR, adjC));
+          } else {
+            deque.offerLast(new Point(adjR, adjC));
+          }
+        }
+      }
+    }
+
+    return distances[m - 1][n - 1];
+  }
+}
+
+class Point {
+  int r;
+  int c;
+
+  Point(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
+}
