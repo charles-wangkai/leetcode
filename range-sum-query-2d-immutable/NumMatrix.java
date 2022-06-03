0c5f55cc918e0e609c3f6c55fa3fec9b69d86b6d
Kai Wang <charles.wangkai@gmail.com>
2022-06-03 08:15:45 +0800

    Range Sum Query 2D - Immutable

--- range-sum-query-2d-immutable/NumMatrix.java
+++ range-sum-query-2d-immutable/NumMatrix.java
@@ -1,27 +1,26 @@
-public class NumMatrix {
-	int[][] sums;
+class NumMatrix {
+  int[][] sums;
 
-	public NumMatrix(int[][] matrix) {
-		int row = matrix.length;
-		if (row == 0) {
-			return;
-		}
-		int col = matrix[0].length;
+  public NumMatrix(int[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
 
-		sums = new int[row + 1][col + 1];
-		for (int i = 1; i <= row; i++) {
-			for (int j = 1; j <= col; j++) {
-				sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + matrix[i - 1][j - 1];
-			}
-		}
-	}
+    sums = new int[m + 1][n + 1];
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + matrix[i - 1][j - 1];
+      }
+    }
+  }
 
-	public int sumRegion(int row1, int col1, int row2, int col2) {
-		return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
-	}
+  public int sumRegion(int row1, int col1, int row2, int col2) {
+    return sums[row2 + 1][col2 + 1]
+        - sums[row1][col2 + 1]
+        - sums[row2 + 1][col1]
+        + sums[row1][col1];
+  }
 }
 
 // Your NumMatrix object will be instantiated and called as such:
-// NumMatrix numMatrix = new NumMatrix(matrix);
-// numMatrix.sumRegion(0, 1, 2, 3);
-// numMatrix.sumRegion(1, 2, 3, 4);
+// NumMatrix obj = new NumMatrix(matrix);
+// int param_1 = obj.sumRegion(row1,col1,row2,col2);
