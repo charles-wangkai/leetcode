d4a8695ded95042260f6cbac832b424153b611d0
Kai Wang <charles.wangkai@gmail.com>
2022-06-14 09:57:27 +0800

    Delete Operation for Two Strings

--- delete-operation-for-two-strings/Solution.java
+++ delete-operation-for-two-strings/Solution.java
@@ -1,17 +1,16 @@
 class Solution {
   public int minDistance(String word1, String word2) {
-    return word1.length() + word2.length() - computeMaxCommonSubsequence(word1, word2) * 2;
+    return word1.length() + word2.length() - 2 * computeMaxCommonSubsequence(word1, word2);
   }
 
   int computeMaxCommonSubsequence(String word1, String word2) {
     int[][] dp = new int[word1.length() + 1][word2.length() + 1];
     for (int i = 1; i <= word1.length(); ++i) {
       for (int j = 1; j <= word2.length(); ++j) {
-        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-          dp[i][j] = dp[i - 1][j - 1] + 1;
-        } else {
-          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-        }
+        dp[i][j] =
+            (word1.charAt(i - 1) == word2.charAt(j - 1))
+                ? (dp[i - 1][j - 1] + 1)
+                : Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
 
