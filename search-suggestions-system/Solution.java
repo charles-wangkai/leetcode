ea8e2264ffc92fd99275ca9cad805cbd43eac956
Kai Wang <charles.wangkai@gmail.com>
2022-06-19 08:30:48 +0800

    Search Suggestions System

--- search-suggestions-system/Solution.java
+++ search-suggestions-system/Solution.java
@@ -6,10 +6,12 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
+  static final int LIMIT = 3;
+
   public List<List<String>> suggestedProducts(String[] products, String searchWord) {
     Node trie = new Node();
     for (String product : products) {
-      addToTrie(trie, product);
+      addToTrie(trie, product, 0);
     }
 
     List<List<String>> suggestionLists =
@@ -21,21 +23,19 @@ class Solution {
     return suggestionLists;
   }
 
-  void addToTrie(Node trie, String word) {
-    addToTrie(trie, word, 0);
-  }
-
   void addToTrie(Node node, String word, int index) {
     if (index == word.length()) {
       node.ending = true;
-    } else {
-      char letter = word.charAt(index);
-      if (!node.letterToChild.containsKey(letter)) {
-        node.letterToChild.put(letter, new Node());
-      }
 
-      addToTrie(node.letterToChild.get(letter), word, index + 1);
+      return;
     }
+
+    char letter = word.charAt(index);
+    if (!node.letterToChild.containsKey(letter)) {
+      node.letterToChild.put(letter, new Node());
+    }
+
+    addToTrie(node.letterToChild.get(letter), word, index + 1);
   }
 
   void search(List<List<String>> suggestionLists, Node node, String searchWord, int index) {
@@ -50,33 +50,29 @@ class Solution {
     Node child = node.letterToChild.get(letter);
 
     traverse(
-        suggestionLists.get(index),
-        child,
-        new StringBuilder(searchWord.substring(0, index + 1)),
-        3);
+        suggestionLists.get(index), child, new StringBuilder(searchWord.substring(0, index + 1)));
 
     search(suggestionLists, child, searchWord, index + 1);
   }
 
-  int traverse(List<String> suggestionList, Node node, StringBuilder s, int rest) {
-    if (node.ending && rest != 0) {
+  void traverse(List<String> suggestionList, Node node, StringBuilder s) {
+    if (node.ending) {
       suggestionList.add(s.toString());
-      --rest;
-    }
-
-    if (rest != 0) {
-      for (char letter : node.letterToChild.keySet()) {
-        s.append(letter);
-        rest = traverse(suggestionList, node.letterToChild.get(letter), s, rest);
-        s.deleteCharAt(s.length() - 1);
-
-        if (rest == 0) {
-          break;
-        }
+      if (suggestionList.size() == LIMIT) {
+        return;
       }
     }
 
-    return rest;
+    for (char letter : node.letterToChild.keySet()) {
+      s.append(letter);
+
+      traverse(suggestionList, node.letterToChild.get(letter), s);
+      if (suggestionList.size() == LIMIT) {
+        return;
+      }
+
+      s.deleteCharAt(s.length() - 1);
+    }
   }
 }
 
