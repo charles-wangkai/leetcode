6259f5d45a87c401ce6a35ff4c7b803d22098c07
Kai Wang <charles.wangkai@gmail.com>
2022-06-23 10:58:02 +0800

    Course Schedule III

--- course-schedule-iii/Solution.java
+++ course-schedule-iii/Solution.java
@@ -6,17 +6,17 @@ class Solution {
   public int scheduleCourse(int[][] courses) {
     Arrays.sort(courses, Comparator.comparing(course -> course[1]));
 
-    PriorityQueue<Integer> times = new PriorityQueue<>(Comparator.reverseOrder());
+    PriorityQueue<Integer> durations = new PriorityQueue<>(Comparator.reverseOrder());
     int start = 0;
     for (int[] course : courses) {
       start += course[0];
-      times.offer(course[0]);
+      durations.offer(course[0]);
 
       while (start > course[1]) {
-        start -= times.poll();
+        start -= durations.poll();
       }
     }
 
-    return times.size();
+    return durations.size();
   }
 }
