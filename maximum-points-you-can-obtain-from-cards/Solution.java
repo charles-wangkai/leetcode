4084d13670466fbdedb3544709d698b3f3b384ad
Kai Wang <charles.wangkai@gmail.com>
2022-06-26 08:35:33 +0800

    Maximum Points You Can Obtain from Cards

--- maximum-points-you-can-obtain-from-cards/Solution.java
+++ maximum-points-you-can-obtain-from-cards/Solution.java
@@ -1,19 +1,19 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int maxScore(int[] cardPoints, int k) {
-        int leftIndex = k - 1;
-        int rightIndex = cardPoints.length;
-        int score = IntStream.range(0, k).map(i -> cardPoints[i]).sum();
-        int result = score;
-        while (leftIndex >= 0) {
-            score += cardPoints[rightIndex - 1] - cardPoints[leftIndex];
-            result = Math.max(result, score);
+  public int maxScore(int[] cardPoints, int k) {
+    int leftIndex = k - 1;
+    int rightIndex = cardPoints.length;
+    int score = IntStream.range(0, k).map(i -> cardPoints[i]).sum();
+    int result = score;
+    while (leftIndex >= 0) {
+      score += cardPoints[rightIndex - 1] - cardPoints[leftIndex];
+      result = Math.max(result, score);
 
-            --leftIndex;
-            --rightIndex;
-        }
-
-        return result;
+      --leftIndex;
+      --rightIndex;
     }
+
+    return result;
+  }
 }
