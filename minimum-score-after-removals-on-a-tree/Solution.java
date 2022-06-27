8a36d3b70e49db9eef248e10fefd7b1f0e7dbba3
Kai Wang <charles.wangkai@gmail.com>
2022-06-27 14:22:57 +0800

    Minimum Score After Removals on a Tree

---
+++ minimum-score-after-removals-on-a-tree/Solution.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Solution {
+  public int minimumScore(int[] nums, int[][] edges) {
+    int n = nums.length;
+
+    @SuppressWarnings("unchecked")
+    List<Integer>[] adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<>();
+    }
+    for (int[] edge : edges) {
+      adjLists[edge[0]].add(edge[1]);
+      adjLists[edge[1]].add(edge[0]);
+    }
+
+    int[] xors = new int[n];
+    boolean[][] parents = new boolean[n][n];
+    search(nums, adjLists, xors, parents, new boolean[n], new ArrayList<>(), 0);
+
+    int result = Integer.MAX_VALUE;
+    for (int i = 1; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (parents[i][j]) {
+          result = Math.min(result, computeScore(xors[0] ^ xors[j], xors[j] ^ xors[i], xors[i]));
+        } else if (parents[j][i]) {
+          result = Math.min(result, computeScore(xors[0] ^ xors[i], xors[i] ^ xors[j], xors[j]));
+        } else {
+          result = Math.min(result, computeScore(xors[0] ^ xors[i] ^ xors[j], xors[i], xors[j]));
+        }
+      }
+    }
+
+    return result;
+  }
+
+  int computeScore(int xor1, int xor2, int xor3) {
+    int[] sorted = IntStream.of(xor1, xor2, xor3).sorted().toArray();
+
+    return sorted[sorted.length - 1] - sorted[0];
+  }
+
+  int search(
+      int[] nums,
+      List<Integer>[] adjLists,
+      int[] xors,
+      boolean[][] parents,
+      boolean[] visited,
+      List<Integer> path,
+      int node) {
+    visited[node] = true;
+
+    for (int p : path) {
+      parents[node][p] = true;
+    }
+
+    path.add(node);
+
+    xors[node] = nums[node];
+    for (int adj : adjLists[node]) {
+      if (!visited[adj]) {
+        xors[node] ^= search(nums, adjLists, xors, parents, visited, path, adj);
+      }
+    }
+
+    path.remove(path.size() - 1);
+
+    return xors[node];
+  }
+}
