d8ce976b9aef93773e45bbd0202cb6028a82198f
Kai Wang <charles.wangkai@gmail.com>
2022-06-27 09:37:30 +0800

    Partitioning Into Minimum Number Of Deci-Binary Numbers

--- partitioning-into-minimum-number-of-deci-binary-numbers/Solution.java
+++ partitioning-into-minimum-number-of-deci-binary-numbers/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
   public int minPartitions(String n) {
-    return n.chars().map(ch -> ch - '0').max().getAsInt();
+    return n.chars().map(c -> c - '0').max().getAsInt();
   }
 }
