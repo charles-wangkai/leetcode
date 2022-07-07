9b976432779ecb1ae294e5821e82105b8d1d1467
Kai Wang <charles.wangkai@gmail.com>
2022-07-07 11:19:26 +0800

    Interleaving String

--- interleaving-string/Solution.java
+++ interleaving-string/Solution.java
@@ -1,27 +1,22 @@
 class Solution {
   public boolean isInterleave(String s1, String s2, String s3) {
-    int s1Length = s1.length();
-    int s2Length = s2.length();
-    int s3Length = s3.length();
-    if (s1Length + s2Length != s3Length) {
+    if (s3.length() != s1.length() + s2.length()) {
       return false;
     }
 
-    boolean[][] interleaves = new boolean[s1Length + 1][s2Length + 1];
-    for (int i = 0; i <= s1Length; ++i) {
-      for (int j = 0; j <= s2Length; ++j) {
-        int totalLength = i + j;
-        if (totalLength == 0) {
-          interleaves[i][j] = true;
-        } else {
-          char ch = s3.charAt(totalLength - 1);
-          interleaves[i][j] =
-              (i != 0 && ch == s1.charAt(i - 1) && interleaves[i - 1][j])
-                  || (j != 0 && ch == s2.charAt(j - 1) && interleaves[i][j - 1]);
-        }
+    boolean[][] interleaves = new boolean[s1.length() + 1][s2.length() + 1];
+    for (int i = 0; i <= s1.length(); ++i) {
+      for (int j = 0; j <= s2.length(); ++j) {
+        interleaves[i][j] =
+            (i == 0 && j == 0)
+                ? true
+                : (i != 0 && s3.charAt(i + j - 1) == s1.charAt(i - 1) && interleaves[i - 1][j])
+                    || (j != 0
+                        && s3.charAt(i + j - 1) == s2.charAt(j - 1)
+                        && interleaves[i][j - 1]);
       }
     }
 
-    return interleaves[s1Length][s2Length];
+    return interleaves[s1.length()][s2.length()];
   }
 }
