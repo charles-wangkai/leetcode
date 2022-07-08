362389250a2b431481a91b5fa282508c49a355ee
Kai Wang <charles.wangkai@gmail.com>
2022-07-09 01:30:13 +0800

    Paint House III

--- paint-house-iii/Solution.java
+++ paint-house-iii/Solution.java
@@ -1,42 +1,38 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 class Solution {
-    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        int[][] minCosts = new int[n + 1][target + 1];
-        for (int i = 0; i <= n; ++i) {
-            Arrays.fill(minCosts[i], Integer.MAX_VALUE);
-        }
-        minCosts[0][0] = 0;
-
-        for (int h = 0; h < m; ++h) {
-            int[][] nextMinCosts = new int[n + 1][target + 1];
-            for (int i = 0; i <= n; ++i) {
-                Arrays.fill(nextMinCosts[i], Integer.MAX_VALUE);
-            }
-
-            for (int i = 1; i <= n; ++i) {
-                if (houses[h] != 0 && i != houses[h]) {
-                    continue;
-                }
-
-                for (int j = 1; j <= target; ++j) {
-                    for (int prevI = 0; prevI <= n; ++prevI) {
-                        int prevJ = (prevI == i) ? j : (j - 1);
-
-                        if (minCosts[prevI][prevJ] != Integer.MAX_VALUE) {
-                            nextMinCosts[i][j] = Math.min(nextMinCosts[i][j],
-                                    minCosts[prevI][prevJ] + ((houses[h] == 0) ? cost[h][i - 1] : 0));
-                        }
-                    }
-                }
-            }
-
-            minCosts = nextMinCosts;
-        }
-
-        int[][] minCosts_ = minCosts;
-        return IntStream.range(0, minCosts.length).filter(i -> minCosts_[i][target] != Integer.MAX_VALUE)
-                .map(i -> minCosts_[i][target]).min().orElse(-1);
+  public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+    int[][] dp = new int[target + 1][n + 1];
+    for (int i = 0; i < dp.length; ++i) {
+      Arrays.fill(dp[i], Integer.MAX_VALUE);
     }
+    dp[0][0] = 0;
+
+    for (int h = 0; h < houses.length; ++h) {
+      int[][] nextDp = new int[target + 1][n + 1];
+      for (int i = 0; i < nextDp.length; ++i) {
+        Arrays.fill(nextDp[i], Integer.MAX_VALUE);
+      }
+
+      for (int c = 1; c <= n; ++c) {
+        if (houses[h] == 0 || c == houses[h]) {
+          for (int i = 1; i <= target; ++i) {
+            for (int prevC = 0; prevC <= n; ++prevC) {
+              int prevI = i - ((prevC == c) ? 0 : 1);
+
+              if (dp[prevI][prevC] != Integer.MAX_VALUE) {
+                nextDp[i][c] =
+                    Math.min(
+                        nextDp[i][c], dp[prevI][prevC] + ((houses[h] == 0) ? cost[h][c - 1] : 0));
+              }
+            }
+          }
+        }
+      }
+
+      dp = nextDp;
+    }
+
+    return Arrays.stream(dp[target]).filter(x -> x != Integer.MAX_VALUE).min().orElse(-1);
+  }
 }
