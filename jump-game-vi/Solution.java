523a2554fb9bde2dd352a204256d05ee4fbec326
Kai Wang <charles.wangkai@gmail.com>
2022-07-09 19:21:12 +0800

    Jump Game VI

--- jump-game-vi/Solution.java
+++ jump-game-vi/Solution.java
@@ -7,14 +7,18 @@ class Solution {
     SortedMap<Integer, Integer> valueToCount = new TreeMap<>();
     for (int i = 0; i < maxScores.length; ++i) {
       maxScores[i] = (valueToCount.isEmpty() ? 0 : valueToCount.lastKey()) + nums[i];
-      valueToCount.put(maxScores[i], valueToCount.getOrDefault(maxScores[i], 0) + 1);
+      updateMap(valueToCount, maxScores[i], 1);
 
       if (i >= k) {
-        valueToCount.put(maxScores[i - k], valueToCount.get(maxScores[i - k]) - 1);
-        valueToCount.remove(maxScores[i - k], 0);
+        updateMap(valueToCount, maxScores[i - k], -1);
       }
     }
 
     return maxScores[maxScores.length - 1];
+  }
+
+  void updateMap(SortedMap<Integer, Integer> valueToCount, int value, int delta) {
+    valueToCount.put(value, valueToCount.getOrDefault(value, 0) + delta);
+    valueToCount.remove(value, 0);
   }
 }
