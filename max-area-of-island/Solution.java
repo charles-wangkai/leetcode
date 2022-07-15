fa09654d2858ded03a7ad457f98a6a8eb0cce73d
Kai Wang <charles.wangkai@gmail.com>
2022-07-15 12:51:11 +0800

    Max Area of Island

--- max-area-of-island/Solution.java
+++ max-area-of-island/Solution.java
@@ -3,13 +3,13 @@ class Solution {
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
   public int maxAreaOfIsland(int[][] grid) {
-    int row = grid.length;
-    int col = grid[0].length;
+    int m = grid.length;
+    int n = grid[0].length;
 
     int maxArea = 0;
-    boolean[][] visited = new boolean[row][col];
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
+    boolean[][] visited = new boolean[m][n];
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
         maxArea = Math.max(maxArea, search(grid, visited, r, c));
       }
     }
@@ -18,10 +18,10 @@ class Solution {
   }
 
   int search(int[][] grid, boolean[][] visited, int r, int c) {
-    int row = grid.length;
-    int col = grid[0].length;
+    int m = grid.length;
+    int n = grid[0].length;
 
-    if (!(r >= 0 && r < row && c >= 0 && c < col) || visited[r][c] || grid[r][c] == 0) {
+    if (!(r >= 0 && r < m && c >= 0 && c < n) || visited[r][c] || grid[r][c] == 0) {
       return 0;
     }
 
