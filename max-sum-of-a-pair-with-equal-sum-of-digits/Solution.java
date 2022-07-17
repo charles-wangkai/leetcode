f2b825a73fbb76f01caeacfd74849e0ed7a8ec03
Kai Wang <charles.wangkai@gmail.com>
2022-07-17 14:50:07 +0800

    Max Sum of a Pair With Equal Sum of Digits

---
+++ max-sum-of-a-pair-with-equal-sum-of-digits/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+  public int maximumSum(int[] nums) {
+    Map<Integer, List<Integer>> digitSumToValues = new HashMap<>();
+    for (int value : nums) {
+      int digitSum = String.valueOf(value).chars().map(c -> c - '0').sum();
+      digitSumToValues.putIfAbsent(digitSum, new ArrayList<>());
+      digitSumToValues.get(digitSum).add(value);
+    }
+
+    return digitSumToValues.values().stream()
+        .filter(values -> values.size() >= 2)
+        .mapToInt(
+            values ->
+                values.stream().sorted(Comparator.reverseOrder()).limit(2).mapToInt(x -> x).sum())
+        .max()
+        .orElse(-1);
+  }
+}
