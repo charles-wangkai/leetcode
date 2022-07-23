beb3a5039997c8853d7a3f9783bc0e0594bc7c83
Kai Wang <charles.wangkai@gmail.com>
2022-07-23 08:42:27 +0800

    Count of Smaller Numbers After Self

--- count-of-smaller-numbers-after-self/Solution.java
+++ count-of-smaller-numbers-after-self/Solution.java
@@ -1,64 +1,47 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
   public List<Integer> countSmaller(int[] nums) {
-    List<Integer> smallerNums =
-        IntStream.range(0, nums.length).map(i -> 0).boxed().collect(Collectors.toList());
+    int[] smallerNums = new int[nums.length];
 
-    Element[] elements =
-        IntStream.range(0, nums.length)
-            .mapToObj(i -> new Element(i, nums[i]))
-            .toArray(Element[]::new);
+    sort(nums, smallerNums, IntStream.range(0, nums.length).toArray(), 0, nums.length - 1);
 
-    sort(smallerNums, elements, 0, elements.length - 1);
-
-    return smallerNums;
+    return Arrays.stream(smallerNums).boxed().collect(Collectors.toList());
   }
 
-  void sort(List<Integer> smallerNums, Element[] elements, int beginIndex, int endIndex) {
+  void sort(int[] nums, int[] smallerNums, int[] indices, int beginIndex, int endIndex) {
     if (beginIndex >= endIndex) {
       return;
     }
 
     int middleIndex = (beginIndex + endIndex) / 2;
 
-    sort(smallerNums, elements, beginIndex, middleIndex);
-    sort(smallerNums, elements, middleIndex + 1, endIndex);
+    sort(nums, smallerNums, indices, beginIndex, middleIndex);
+    sort(nums, smallerNums, indices, middleIndex + 1, endIndex);
 
-    List<Element> merged = new ArrayList<>();
+    List<Integer> merged = new ArrayList<>();
     int leftIndex = beginIndex;
     int rightIndex = middleIndex + 1;
     while (leftIndex != middleIndex + 1 || rightIndex != endIndex + 1) {
       if (rightIndex == endIndex + 1
           || (leftIndex != middleIndex + 1
-              && elements[leftIndex].value > elements[rightIndex].value)) {
-        smallerNums.set(
-            elements[leftIndex].index,
-            smallerNums.get(elements[leftIndex].index) + (endIndex + 1 - rightIndex));
+              && nums[indices[leftIndex]] > nums[indices[rightIndex]])) {
+        smallerNums[indices[leftIndex]] += endIndex + 1 - rightIndex;
 
-        merged.add(elements[leftIndex]);
+        merged.add(indices[leftIndex]);
         ++leftIndex;
       } else {
-        merged.add(elements[rightIndex]);
+        merged.add(indices[rightIndex]);
         ++rightIndex;
       }
     }
 
     for (int i = beginIndex; i <= endIndex; ++i) {
-      elements[i] = merged.get(i - beginIndex);
+      indices[i] = merged.get(i - beginIndex);
     }
-  }
-}
-
-class Element {
-  int index;
-  int value;
-
-  Element(int index, int value) {
-    this.index = index;
-    this.value = value;
   }
 }
