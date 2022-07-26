39e0a630c9d134e4d66325af3c443e2c1b11975c
Kai Wang <charles.wangkai@gmail.com>
2022-07-26 15:00:21 +0800

    Number of Zero-Filled Subarrays

---
+++ number-of-zero-filled-subarrays/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+  public long zeroFilledSubarray(int[] nums) {
+    long result = 0;
+
+    int count = 0;
+    for (int i = 0; i <= nums.length; ++i) {
+      if (i != nums.length && nums[i] == 0) {
+        ++count;
+      } else {
+        result += count * (count + 1L) / 2;
+        count = 0;
+      }
+    }
+
+    return result;
+  }
+}
