f8e4dd9a293b8aef94cdf0b92f75a9828984a12d
Kai Wang <charles.wangkai@gmail.com>
2022-07-29 09:16:06 +0800

    Find and Replace Pattern

--- find-and-replace-pattern/Solution.java
+++ find-and-replace-pattern/Solution.java
@@ -2,25 +2,19 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
   public List<String> findAndReplacePattern(String[] words, String pattern) {
     String patternKey = computeKey(pattern);
 
-    return Arrays.stream(words)
-        .filter(word -> computeKey(word).equals(patternKey))
-        .collect(Collectors.toList());
+    return Arrays.stream(words).filter(word -> computeKey(word).equals(patternKey)).toList();
   }
 
   String computeKey(String s) {
     StringBuilder key = new StringBuilder();
     Map<Character, Character> letterToReplacement = new HashMap<>();
     for (char letter : s.toCharArray()) {
-      if (!letterToReplacement.containsKey(letter)) {
-        letterToReplacement.put(letter, (char) ('a' + letterToReplacement.size()));
-      }
-
+      letterToReplacement.putIfAbsent(letter, (char) ('a' + letterToReplacement.size()));
       key.append(letterToReplacement.get(letter));
     }
 
