bdeac23129b512d3efda9bbc8013adb6ddd095d7
Kai Wang <charles.wangkai@gmail.com>
2022-08-02 09:55:16 +0800

    Kth Smallest Element in a Sorted Matrix

--- kth-smallest-element-in-a-sorted-matrix/Solution.java
+++ kth-smallest-element-in-a-sorted-matrix/Solution.java
@@ -5,30 +5,22 @@ class Solution {
   public int kthSmallest(int[][] matrix, int k) {
     int n = matrix.length;
 
-    PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparing(p -> matrix[p.r][p.c]));
+    PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparing(p -> matrix[p.r()][p.c()]));
     pq.offer(new Point(0, 0));
 
     for (int i = 0; i < k - 1; ++i) {
       Point head = pq.poll();
 
-      if (head.c + 1 != n) {
-        pq.offer(new Point(head.r, head.c + 1));
+      if (head.c() + 1 != n) {
+        pq.offer(new Point(head.r(), head.c() + 1));
       }
-      if (head.c == 0 && head.r + 1 != n) {
-        pq.offer(new Point(head.r + 1, head.c));
+      if (head.c() == 0 && head.r() + 1 != n) {
+        pq.offer(new Point(head.r() + 1, head.c()));
       }
     }
 
-    return matrix[pq.peek().r][pq.peek().c];
+    return matrix[pq.peek().r()][pq.peek().c()];
   }
 }
 
-class Point {
-  int r;
-  int c;
-
-  Point(int r, int c) {
-    this.r = r;
-    this.c = c;
-  }
-}
+record Point(int r, int c) {}
