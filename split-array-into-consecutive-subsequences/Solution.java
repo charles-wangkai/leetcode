1d06a1d8c0e7042b4418aa07e99a3f20e0e5f5bd
Kai Wang <charles.wangkai@gmail.com>
2022-08-19 08:49:13 +0800

    Split Array into Consecutive Subsequences

--- split-array-into-consecutive-subsequences/Solution.java
+++ split-array-into-consecutive-subsequences/Solution.java
@@ -1,32 +1,32 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public boolean isPossible(int[] nums) {
-		Map<Integer, Integer> num2available = new HashMap<Integer, Integer>();
-		for (int num : nums) {
-			num2available.put(num, num2available.getOrDefault(num, 0) + 1);
-		}
+class Solution {
+  public boolean isPossible(int[] nums) {
+    Map<Integer, Integer> valueToNeeded = new HashMap<>();
+    for (int value : nums) {
+      valueToNeeded.put(value, valueToNeeded.getOrDefault(value, 0) + 1);
+    }
 
-		Map<Integer, Integer> num2appendable = new HashMap<Integer, Integer>();
-		for (int num : nums) {
-			if (num2available.get(num) == 0) {
-				continue;
-			}
+    Map<Integer, Integer> valueToAppendable = new HashMap<>();
+    for (int value : nums) {
+      if (valueToNeeded.get(value) != 0) {
+        if (valueToAppendable.getOrDefault(value, 0) != 0) {
+          valueToAppendable.put(value, valueToAppendable.get(value) - 1);
+          valueToAppendable.put(value + 1, valueToAppendable.getOrDefault(value + 1, 0) + 1);
+        } else if (valueToNeeded.getOrDefault(value + 1, 0) != 0
+            && valueToNeeded.getOrDefault(value + 2, 0) != 0) {
+          valueToNeeded.put(value + 1, valueToNeeded.get(value + 1) - 1);
+          valueToNeeded.put(value + 2, valueToNeeded.get(value + 2) - 1);
+          valueToAppendable.put(value + 3, valueToAppendable.getOrDefault(value + 3, 0) + 1);
+        } else {
+          return false;
+        }
 
-			if (num2appendable.getOrDefault(num, 0) > 0) {
-				num2appendable.put(num, num2appendable.get(num) - 1);
-				num2appendable.put(num + 1, num2appendable.getOrDefault(num + 1, 0) + 1);
-			} else if (num2available.getOrDefault(num + 1, 0) > 0 && num2available.getOrDefault(num + 2, 0) > 0) {
-				num2available.put(num + 1, num2available.get(num + 1) - 1);
-				num2available.put(num + 2, num2available.get(num + 2) - 1);
-				num2appendable.put(num + 3, num2appendable.getOrDefault(num + 3, 0) + 1);
-			} else {
-				return false;
-			}
+        valueToNeeded.put(value, valueToNeeded.get(value) - 1);
+      }
+    }
 
-			num2available.put(num, num2available.get(num) - 1);
-		}
-		return true;
-	}
+    return true;
+  }
 }
