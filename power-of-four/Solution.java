bd733366dbb1ce649614890b64a1038c8a96f909
Kai Wang <charles.wangkai@gmail.com>
2022-08-22 09:49:38 +0800

    Power of Four

--- power-of-four/Solution.java
+++ power-of-four/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-	public boolean isPowerOfFour(int num) {
-		return num > 0 && 1073741824 % num == 0 && (num & 0x55555555) != 0;
-	}
+  public boolean isPowerOfFour(int num) {
+    return num > 0 && (1 << 30) % num == 0 && (num & 0x55555555) != 0;
+  }
 }
