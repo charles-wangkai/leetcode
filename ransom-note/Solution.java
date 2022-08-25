f200791487dd46ab566369e63a8f9f5318e2584e
Kai Wang <charles.wangkai@gmail.com>
2022-08-25 09:20:04 +0800

    Ransom Note

--- ransom-note/Solution.java
+++ ransom-note/Solution.java
@@ -1,21 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public boolean canConstruct(String ransomNote, String magazine) {
-		Map<Character, Integer> ransomLetterToCount = buildLetterToCount(ransomNote);
-		Map<Character, Integer> magazineLetterToCount = buildLetterToCount(magazine);
+class Solution {
+  public boolean canConstruct(String ransomNote, String magazine) {
+    Map<Character, Integer> ransomLetterToCount = buildLetterToCount(ransomNote);
+    Map<Character, Integer> magazineLetterToCount = buildLetterToCount(magazine);
 
-		return ransomLetterToCount.keySet().stream()
-				.allMatch(letter -> magazineLetterToCount.getOrDefault(letter, 0) >= ransomLetterToCount.get(letter));
-	}
+    return ransomLetterToCount.keySet().stream()
+        .allMatch(
+            letter ->
+                magazineLetterToCount.getOrDefault(letter, 0) >= ransomLetterToCount.get(letter));
+  }
 
-	Map<Character, Integer> buildLetterToCount(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+  Map<Character, Integer> buildLetterToCount(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		return letterToCount;
-	}
+    return letterToCount;
+  }
 }
