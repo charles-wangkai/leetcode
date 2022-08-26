df81a42fa7b4290b645adcf88fe41ca4bc6e3f8d
Kai Wang <charles.wangkai@gmail.com>
2022-08-26 13:12:58 +0800

    Reordered Power of 2

--- reordered-power-of-2/Solution.java
+++ reordered-power-of-2/Solution.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 
 class Solution {
   public boolean reorderedPowerOf2(int N) {
-    return IntStream.range(0, 31).anyMatch(i -> check(N, 1 << i));
+    return IntStream.range(0, 30).anyMatch(i -> check(N, 1 << i));
   }
 
   boolean check(int N, int power) {
@@ -14,7 +14,7 @@ class Solution {
     return String.valueOf(x)
         .chars()
         .sorted()
-        .mapToObj(ch -> String.valueOf((char) ch))
+        .mapToObj(c -> String.valueOf((char) c))
         .collect(Collectors.joining());
   }
 }
