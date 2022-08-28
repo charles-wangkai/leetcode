029c26cea62e476bdcf91feb1f06ff9e27289749
Kai Wang <charles.wangkai@gmail.com>
2022-08-28 19:05:27 +0800

    Minimum Amount of Time to Collect Garbage

---
+++ minimum-amount-of-time-to-collect-garbage/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public int garbageCollection(String[] garbage, int[] travel) {
+    Map<Character, Integer> typeToTravelTime = new HashMap<>();
+    int travelTime = 0;
+    for (int i = 1; i < garbage.length; ++i) {
+      travelTime += travel[i - 1];
+      for (char type : garbage[i].toCharArray()) {
+        typeToTravelTime.put(type, travelTime);
+      }
+    }
+
+    return Arrays.stream(garbage).mapToInt(String::length).sum()
+        + typeToTravelTime.values().stream().mapToInt(x -> x).sum();
+  }
+}
