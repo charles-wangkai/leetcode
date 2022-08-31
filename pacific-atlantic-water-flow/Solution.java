86f2f408035245f2296c5dbf1b2dfa7862a8e926
Kai Wang <charles.wangkai@gmail.com>
2022-08-31 09:30:41 +0800

    Pacific Atlantic Water Flow

--- pacific-atlantic-water-flow/Solution.java
+++ pacific-atlantic-water-flow/Solution.java
@@ -1,44 +1,37 @@
 import java.util.ArrayDeque;
 import java.util.List;
 import java.util.Queue;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
   static int[] R_OFFSETS = {-1, 0, 1, 0};
   static int[] C_OFFSETS = {0, 1, 0, -1};
 
-  public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-    int row = matrix.length;
-    if (row == 0) {
-      return List.of();
-    }
-    int col = matrix[0].length;
-    if (col == 0) {
-      return List.of();
-    }
+  public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    int m = heights.length;
+    int n = heights[0].length;
 
-    boolean[][] pacific = bfs(matrix, 0, 0);
-    boolean[][] atlantic = bfs(matrix, row - 1, col - 1);
+    boolean[][] pacific = bfs(heights, 0, 0);
+    boolean[][] atlantic = bfs(heights, m - 1, n - 1);
 
-    return IntStream.range(0, row)
+    return IntStream.range(0, m)
         .boxed()
         .flatMap(
             r ->
-                IntStream.range(0, col)
+                IntStream.range(0, n)
                     .filter(c -> pacific[r][c] && atlantic[r][c])
                     .mapToObj(c -> List.of(r, c)))
-        .collect(Collectors.toList());
+        .toList();
   }
 
-  boolean[][] bfs(int[][] matrix, int edgeR, int edgeC) {
-    int row = matrix.length;
-    int col = matrix[0].length;
+  boolean[][] bfs(int[][] heights, int edgeR, int edgeC) {
+    int m = heights.length;
+    int n = heights[0].length;
 
-    boolean[][] reaches = new boolean[row][col];
+    boolean[][] reaches = new boolean[m][n];
     Queue<Point> queue = new ArrayDeque<>();
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
         if (r == edgeR || c == edgeC) {
           reaches[r][c] = true;
           queue.offer(new Point(r, c));
@@ -50,15 +43,15 @@ class Solution {
       Point head = queue.poll();
 
       for (int i = 0; i < R_OFFSETS.length; ++i) {
-        int adjR = head.r + R_OFFSETS[i];
-        int adjC = head.c + C_OFFSETS[i];
+        int adjR = head.r() + R_OFFSETS[i];
+        int adjC = head.c() + C_OFFSETS[i];
 
         if (adjR >= 0
-            && adjR < row
+            && adjR < m
             && adjC >= 0
-            && adjC < col
+            && adjC < n
             && !reaches[adjR][adjC]
-            && matrix[adjR][adjC] >= matrix[head.r][head.c]) {
+            && heights[adjR][adjC] >= heights[head.r()][head.c()]) {
           reaches[adjR][adjC] = true;
           queue.offer(new Point(adjR, adjC));
         }
@@ -69,12 +62,4 @@ class Solution {
   }
 }
 
-class Point {
-  int r;
-  int c;
-
-  Point(int r, int c) {
-    this.r = r;
-    this.c = c;
-  }
-}
+record Point(int r, int c) {}
