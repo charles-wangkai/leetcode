23ef6a6ee5b3d2dd75e554b5d36e88e3ab61b02b
Kai Wang <charles.wangkai@gmail.com>
2022-09-08 09:37:21 +0800

    Binary Tree Inorder Traversal

--- binary-tree-inorder-traversal/Solution.java
+++ binary-tree-inorder-traversal/Solution.java
@@ -2,37 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<Integer> inorderTraversal(TreeNode root) {
-		Stack<Object> stack = new Stack<Object>();
-		stack.push(root);
-		List<Integer> values = new ArrayList<Integer>();
-		while (!stack.empty()) {
-			Object top = stack.pop();
-			if (top == null) {
-				continue;
-			}
-			if (top instanceof TreeNode) {
-				TreeNode node = (TreeNode) top;
-				stack.push(node.right);
-				stack.push(node.val);
-				stack.push(node.left);
-			} else {
-				int value = (Integer) top;
-				values.add(value);
-			}
-		}
-		return values;
-	}
+class Solution {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    Stack<Object> stack = new Stack<>();
+    stack.push(root);
+    while (!stack.empty()) {
+      Object top = stack.pop();
+      if (top != null) {
+        if (top instanceof TreeNode) {
+          TreeNode node = (TreeNode) top;
+          stack.push(node.right);
+          stack.push(node.val);
+          stack.push(node.left);
+        } else {
+          result.add((Integer) top);
+        }
+      }
+    }
+
+    return result;
+  }
 }
