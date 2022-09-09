3307266e30ab82608d30777a80944683d64c90ab
Kai Wang <charles.wangkai@gmail.com>
2022-09-09 09:27:48 +0800

    The Number of Weak Characters in the Game

--- the-number-of-weak-characters-in-the-game/Solution.java
+++ the-number-of-weak-characters-in-the-game/Solution.java
@@ -3,27 +3,16 @@ import java.util.Comparator;
 
 class Solution {
   public int numberOfWeakCharacters(int[][] properties) {
-    Arrays.sort(properties, Comparator.comparing(p -> p[0]));
+    Arrays.sort(
+        properties, Comparator.comparing((int[] p) -> p[0]).reversed().thenComparing(p -> p[1]));
 
     int result = 0;
-    int higherMax = -1;
-    int currentKey = Integer.MAX_VALUE;
-    int currentMax = -1;
-    for (int i = properties.length - 1; i >= 0; --i) {
-      if (properties[i][0] == currentKey) {
-        if (properties[i][1] < higherMax) {
-          ++result;
-        }
-
-        currentMax = Math.max(currentMax, properties[i][1]);
+    int maxDefense = -1;
+    for (int[] property : properties) {
+      if (property[1] < maxDefense) {
+        ++result;
       } else {
-        if (properties[i][1] < higherMax || properties[i][1] < currentMax) {
-          ++result;
-        }
-
-        higherMax = Math.max(higherMax, currentMax);
-        currentKey = properties[i][0];
-        currentMax = properties[i][1];
+        maxDefense = property[1];
       }
     }
 
