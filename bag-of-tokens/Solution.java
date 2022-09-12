fd932a7538b3eff770b395dc1299477b0c2f085f
Kai Wang <charles.wangkai@gmail.com>
2022-09-12 18:18:27 +0800

    Bag of Tokens

--- bag-of-tokens/Solution.java
+++ bag-of-tokens/Solution.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 
 class Solution {
-  public int bagOfTokensScore(int[] tokens, int P) {
+  public int bagOfTokensScore(int[] tokens, int power) {
     Arrays.sort(tokens);
 
     int endIndex = -1;
     int sum = 0;
-    while (endIndex + 1 != tokens.length && sum + tokens[endIndex + 1] <= P) {
+    while (endIndex + 1 != tokens.length && sum + tokens[endIndex + 1] <= power) {
       sum += tokens[endIndex + 1];
       ++endIndex;
     }
     int maxPoint = endIndex + 1;
 
-    int power = P;
-    for (int i = 0, j = tokens.length - 1; i < j && power >= tokens[i]; ++i, --j) {
-      power += tokens[j] - tokens[i];
+    int p = power;
+    for (int i = 0, j = tokens.length - 1; i < j && p >= tokens[i]; ++i, --j) {
+      p += tokens[j] - tokens[i];
 
       sum -= tokens[i];
       if (endIndex == j) {
         sum -= tokens[endIndex];
         --endIndex;
       }
-      while (endIndex + 1 != j && sum + tokens[endIndex + 1] <= power) {
+      while (endIndex + 1 != j && sum + tokens[endIndex + 1] <= p) {
         sum += tokens[endIndex + 1];
         ++endIndex;
       }
