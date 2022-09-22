cedd0aca19726ca170aaa265dbc6f299803aea3e
Kai Wang <charles.wangkai@gmail.com>
2022-09-22 10:25:11 +0800

    Reverse Words in a String III

--- reverse-words-in-a-string-iii/Solution.java
+++ reverse-words-in-a-string-iii/Solution.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Solution {
-	public String reverseWords(String s) {
-		return String.join(" ", Arrays.stream(s.split(" ")).map(word -> new StringBuilder(word).reverse().toString())
-				.collect(Collectors.toList()));
-	}
+class Solution {
+  public String reverseWords(String s) {
+    return Arrays.stream(s.split(" "))
+        .map(word -> new StringBuilder(word).reverse().toString())
+        .collect(Collectors.joining(" "));
+  }
 }
