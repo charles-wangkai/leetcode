a7b803928876c78ff071d8f3c844991cf0315724
Kai Wang <charles.wangkai@gmail.com>
2022-09-23 09:19:59 +0800

    Concatenation of Consecutive Binary Numbers

--- concatenation-of-consecutive-binary-numbers/Solution.java
+++ concatenation-of-consecutive-binary-numbers/Solution.java
@@ -1,22 +1,19 @@
+import java.util.stream.IntStream;
+
 class Solution {
   static final int MODULUS = 1_000_000_007;
 
   public int concatenatedBinary(int n) {
-    int result = 0;
-    for (int i = 1; i <= n; ++i) {
-      for (char ch : Integer.toBinaryString(i).toCharArray()) {
-        result = addMod(multiplyMod(result, 2), ch - '0');
-      }
-    }
-
-    return result;
+    return IntStream.rangeClosed(1, n)
+        .reduce((result, i) -> addMod(multiplyMod(result, (Integer.highestOneBit(i) * 2)), i))
+        .getAsInt();
   }
 
   int addMod(int x, int y) {
-    return (x + y) % MODULUS;
+    return Math.floorMod(x + y, MODULUS);
   }
 
   int multiplyMod(int x, int y) {
-    return (int) ((long) x * y % MODULUS);
+    return Math.floorMod((long) x * y, MODULUS);
   }
 }
