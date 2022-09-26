0f2b283a78ed32b85925405f9b819befdb4ba3bd
Kai Wang <charles.wangkai@gmail.com>
2022-09-27 01:59:53 +0800

    Satisfiability of Equality Equations

--- satisfiability-of-equality-equations/Solution.java
+++ satisfiability-of-equality-equations/Solution.java
@@ -1,57 +1,52 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public boolean equationsPossible(String[] equations) {
-		Map<Character, Character> variableToParent = new HashMap<>();
-		for (char variable = 'a'; variable <= 'z'; variable++) {
-			variableToParent.put(variable, null);
-		}
+class Solution {
+  public boolean equationsPossible(String[] equations) {
+    Map<Character, Character> variableToParent = new HashMap<>();
+    for (String equation : equations) {
+      if (equation.charAt(1) == '=') {
+        char leftVariable = equation.charAt(0);
+        char rightVariable = equation.charAt(3);
 
-		for (String equation : equations) {
-			if (equation.charAt(1) == '=') {
-				char leftVariable = equation.charAt(0);
-				char rightVariable = equation.charAt(3);
+        char leftRoot = findRoot(variableToParent, leftVariable);
+        char rightRoot = findRoot(variableToParent, rightVariable);
 
-				char leftRoot = findRoot(variableToParent, leftVariable);
-				char rightRoot = findRoot(variableToParent, rightVariable);
+        if (leftRoot != rightRoot) {
+          variableToParent.put(rightRoot, leftRoot);
+        }
+      }
+    }
 
-				if (leftRoot != rightRoot) {
-					variableToParent.put(rightRoot, leftRoot);
-				}
-			}
-		}
+    return Arrays.stream(equations)
+        .filter(equation -> equation.charAt(1) == '!')
+        .allMatch(
+            equation -> {
+              char leftVariable = equation.charAt(0);
+              char rightVariable = equation.charAt(3);
 
-		for (String equation : equations) {
-			if (equation.charAt(1) == '!') {
-				char leftVariable = equation.charAt(0);
-				char rightVariable = equation.charAt(3);
+              char leftRoot = findRoot(variableToParent, leftVariable);
+              char rightRoot = findRoot(variableToParent, rightVariable);
 
-				char leftRoot = findRoot(variableToParent, leftVariable);
-				char rightRoot = findRoot(variableToParent, rightVariable);
+              return leftRoot != rightRoot;
+            });
+  }
 
-				if (leftRoot == rightRoot) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+  char findRoot(Map<Character, Character> variableToParent, char variable) {
+    char root = variable;
+    while (variableToParent.containsKey(root)) {
+      root = variableToParent.get(root);
+    }
 
-	char findRoot(Map<Character, Character> variableToParent, char variable) {
-		char root = variable;
-		while (variableToParent.get(root) != null) {
-			root = variableToParent.get(root);
-		}
+    char p = variable;
+    while (p != root) {
+      char next = variableToParent.get(p);
+      variableToParent.put(p, root);
 
-		char p = variable;
-		while (p != root) {
-			char next = variableToParent.get(p);
-			variableToParent.put(p, root);
+      p = next;
+    }
 
-			p = next;
-		}
-
-		return root;
-	}
+    return root;
+  }
 }
