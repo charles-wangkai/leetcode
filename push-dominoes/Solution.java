30f4ebbd278a8e3b3527c6033f9fe1773d966374
Kai Wang <charles.wangkai@gmail.com>
2022-09-27 08:39:44 +0800

    Push Dominoes

--- push-dominoes/Solution.java
+++ push-dominoes/Solution.java
@@ -5,14 +5,13 @@ class Solution {
   public String pushDominoes(String dominoes) {
     Side[] leftSides = new Side[dominoes.length()];
     char state = 0;
-    int distance = 1;
+    int distance = 0;
     for (int i = 0; i < leftSides.length; ++i) {
-      char ch = dominoes.charAt(i);
-      if (ch != '.') {
-        state = ch;
+      char c = dominoes.charAt(i);
+      if (c != '.') {
+        state = c;
         distance = 0;
       }
-
       leftSides[i] = new Side(state, distance);
 
       ++distance;
@@ -20,30 +19,30 @@ class Solution {
 
     Side[] rightSides = new Side[dominoes.length()];
     state = 0;
-    distance = 1;
+    distance = 0;
     for (int i = rightSides.length - 1; i >= 0; --i) {
-      char ch = dominoes.charAt(i);
-      if (ch != '.') {
-        state = ch;
+      char c = dominoes.charAt(i);
+      if (c != '.') {
+        state = c;
         distance = 0;
       }
-
       rightSides[i] = new Side(state, distance);
 
       ++distance;
     }
 
     return IntStream.range(0, dominoes.length())
-        .mapToObj(i -> String.valueOf(computeFinalState(leftSides[i], rightSides[i])))
+        .mapToObj(i -> computeFinalState(leftSides[i], rightSides[i]))
+        .map(String::valueOf)
         .collect(Collectors.joining());
   }
 
   char computeFinalState(Side leftSide, Side rightSide) {
-    if (leftSide.state == 'R') {
-      if (rightSide.state == 'L') {
-        if (leftSide.distance < rightSide.distance) {
+    if (leftSide.state() == 'R') {
+      if (rightSide.state() == 'L') {
+        if (leftSide.distance() < rightSide.distance()) {
           return 'R';
-        } else if (leftSide.distance > rightSide.distance) {
+        } else if (leftSide.distance() > rightSide.distance()) {
           return 'L';
         } else {
           return '.';
@@ -53,16 +52,8 @@ class Solution {
       return 'R';
     }
 
-    return (rightSide.state == 'L') ? 'L' : '.';
+    return (rightSide.state() == 'L') ? 'L' : '.';
   }
 }
 
-class Side {
-  char state;
-  int distance;
-
-  Side(char state, int distance) {
-    this.state = state;
-    this.distance = distance;
-  }
-}
+record Side(char state, int distance) {}
