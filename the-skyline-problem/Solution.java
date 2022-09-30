5b0d4ea694aae0401f1b233016fcee0a30af8c5c
Kai Wang <charles.wangkai@gmail.com>
2022-09-30 09:14:16 +0800

    The Skyline Problem

--- the-skyline-problem/Solution.java
+++ the-skyline-problem/Solution.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Stream;
@@ -14,42 +16,32 @@ class Solution {
                     Stream.of(
                         new Corner(building[0], building[2], true),
                         new Corner(building[1], building[2], false)))
-            .sorted((c1, c2) -> Integer.compare(c1.x, c2.x))
+            .sorted(Comparator.comparing(Corner::x))
             .toArray(Corner[]::new);
 
     SortedMap<Integer, Integer> heightToCount = new TreeMap<>();
-    heightToCount.put(0, 1);
+    updateMap(heightToCount, 0, 1);
     List<List<Integer>> skyline = new ArrayList<>();
     for (Corner corner : corners) {
-      if (corner.leftOrRight) {
-        heightToCount.put(corner.y, heightToCount.getOrDefault(corner.y, 0) + 1);
-      } else {
-        heightToCount.put(corner.y, heightToCount.get(corner.y) - 1);
-        heightToCount.remove(corner.y, 0);
-      }
+      updateMap(heightToCount, corner.y(), corner.leftOrRight() ? 1 : -1);
 
-      if (!skyline.isEmpty() && corner.x == skyline.get(skyline.size() - 1).get(0)) {
+      if (!skyline.isEmpty() && corner.x() == skyline.get(skyline.size() - 1).get(0)) {
         skyline.remove(skyline.size() - 1);
       }
 
       int nextHeight = heightToCount.lastKey();
       if (skyline.isEmpty() || nextHeight != skyline.get(skyline.size() - 1).get(1)) {
-        skyline.add(Arrays.asList(corner.x, nextHeight));
+        skyline.add(List.of(corner.x(), nextHeight));
       }
     }
 
     return skyline;
   }
-}
 
-class Corner {
-  int x;
-  int y;
-  boolean leftOrRight;
-
-  Corner(int x, int y, boolean leftOrRight) {
-    this.x = x;
-    this.y = y;
-    this.leftOrRight = leftOrRight;
+  void updateMap(Map<Integer, Integer> map, int key, int delta) {
+    map.put(key, map.getOrDefault(key, 0) + delta);
+    map.remove(key, 0);
   }
 }
+
+record Corner(int x, int y, boolean leftOrRight) {}
