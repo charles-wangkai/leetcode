c100b47afed14818769a61345021ec27280c7018
Kai Wang <charles.wangkai@gmail.com>
2022-10-02 10:20:53 +0800

    Number of Dice Rolls With Target Sum

--- number-of-dice-rolls-with-target-sum/Solution.java
+++ number-of-dice-rolls-with-target-sum/Solution.java
@@ -1,25 +1,25 @@
-public class Solution {
-	static final int MODULUS = 1_000_000_007;
+class Solution {
+  static final int MODULUS = 1_000_000_007;
 
-	public int numRollsToTarget(int d, int f, int target) {
-		int[] ways = new int[target + 1];
-		ways[0] = 1;
+  public int numRollsToTarget(int n, int k, int target) {
+    int[] dp = new int[target + 1];
+    dp[0] = 1;
 
-		for (int i = 0; i < d; i++) {
-			int[] nextWays = new int[ways.length];
-			for (int j = 0; j < nextWays.length; j++) {
-				for (int k = 1; k <= f && k <= j; k++) {
-					nextWays[j] = addMod(nextWays[j], ways[j - k]);
-				}
-			}
+    for (int i = 0; i < n; ++i) {
+      int[] nextDp = new int[dp.length];
+      for (int j = 0; j < nextDp.length; ++j) {
+        for (int d = 1; d <= k && d <= j; ++d) {
+          nextDp[j] = addMod(nextDp[j], dp[j - d]);
+        }
+      }
 
-			ways = nextWays;
-		}
+      dp = nextDp;
+    }
 
-		return ways[target];
-	}
+    return dp[target];
+  }
 
-	static int addMod(int x, int y) {
-		return (x + y) % MODULUS;
-	}
+  static int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 }
