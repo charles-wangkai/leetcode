240e62458e5ff81b80db8f39ec54cab4bd9a6805
Kai Wang <charles.wangkai@gmail.com>
2022-10-04 09:23:30 +0800

    Path Sum

--- path-sum/Solution.java
+++ path-sum/Solution.java
@@ -1,23 +1,32 @@
-// Definition for binary tree
+// Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public boolean hasPathSum(TreeNode root, int sum) {
-		if (root == null) {
-			return false;
-		}
-		if (root.left == null && root.right == null) {
-			return sum == root.val;
-		}
-		return (root.left != null && hasPathSum(root.left, sum - root.val))
-				|| (root.right != null && hasPathSum(root.right, sum - root.val));
-	}
+class Solution {
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+    if (root.left == null && root.right == null) {
+      return targetSum == root.val;
+    }
+
+    return (root.left != null && hasPathSum(root.left, targetSum - root.val))
+        || (root.right != null && hasPathSum(root.right, targetSum - root.val));
+  }
 }
