c0d0f4413d5db1507f7246e2d5ed0dee8e5141dc
Kai Wang <charles.wangkai@gmail.com>
2022-10-16 18:07:19 +0800

    Range Product Queries of Powers

---
+++ range-product-queries-of-powers/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+  static final int MODULUS = 1_000_000_007;
+
+  public int[] productQueries(int n, int[][] queries) {
+    int[] powers =
+        IntStream.rangeClosed(0, 30).map(i -> 1 << i).filter(x -> (n & x) != 0).toArray();
+
+    return Arrays.stream(queries)
+        .mapToInt(
+            query ->
+                IntStream.rangeClosed(query[0], query[1])
+                    .reduce(1, (x, y) -> multiplyMod(x, powers[y])))
+        .toArray();
+  }
+
+  static int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, MODULUS);
+  }
+}
