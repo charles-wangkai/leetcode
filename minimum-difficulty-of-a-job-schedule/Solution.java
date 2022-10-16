cf0773bc79aeb23a95a4fb383adf710e771460f4
Kai Wang <charles.wangkai@gmail.com>
2022-10-16 09:04:32 +0800

    Minimum Difficulty of a Job Schedule

--- minimum-difficulty-of-a-job-schedule/Solution.java
+++ minimum-difficulty-of-a-job-schedule/Solution.java
@@ -1,31 +1,30 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int minDifficulty(int[] jobDifficulty, int d) {
-		if (jobDifficulty.length < d) {
-			return -1;
-		}
+class Solution {
+  public int minDifficulty(int[] jobDifficulty, int d) {
+    if (jobDifficulty.length < d) {
+      return -1;
+    }
 
-		int[][] minDifficultySums = new int[jobDifficulty.length + 1][d + 1];
-		for (int i = 0; i < minDifficultySums.length; ++i) {
-			Arrays.fill(minDifficultySums[i], Integer.MAX_VALUE);
-		}
-		minDifficultySums[0][0] = 0;
+    int[][] dp = new int[jobDifficulty.length + 1][d + 1];
+    for (int i = 0; i < dp.length; ++i) {
+      Arrays.fill(dp[i], Integer.MAX_VALUE);
+    }
+    dp[0][0] = 0;
 
-		for (int length = 1; length < minDifficultySums.length; ++length) {
-			int maxDifficulty = -1;
-			for (int i = length - 1; i >= 0; --i) {
-				maxDifficulty = Math.max(maxDifficulty, jobDifficulty[i]);
+    for (int length = 1; length < dp.length; ++length) {
+      int maxDifficulty = -1;
+      for (int i = length - 1; i >= 0; --i) {
+        maxDifficulty = Math.max(maxDifficulty, jobDifficulty[i]);
 
-				for (int day = 1; day <= d; ++day) {
-					if (minDifficultySums[i][day - 1] != Integer.MAX_VALUE) {
-						minDifficultySums[length][day] = Math.min(minDifficultySums[length][day],
-								minDifficultySums[i][day - 1] + maxDifficulty);
-					}
-				}
-			}
-		}
+        for (int day = 1; day <= d; ++day) {
+          if (dp[i][day - 1] != Integer.MAX_VALUE) {
+            dp[length][day] = Math.min(dp[length][day], dp[i][day - 1] + maxDifficulty);
+          }
+        }
+      }
+    }
 
-		return minDifficultySums[jobDifficulty.length][d];
-	}
+    return dp[jobDifficulty.length][d];
+  }
 }
