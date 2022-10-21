24a5c77a5dadc779ea55681c030133f18bf7973d
Kai Wang <charles.wangkai@gmail.com>
2022-10-21 10:12:26 +0800

    Contains Duplicate II

--- contains-duplicate-ii/Solution.java
+++ contains-duplicate-ii/Solution.java
@@ -1,20 +1,22 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Solution {
-	public boolean containsNearbyDuplicate(int[] nums, int k) {
-		Set<Integer> history = new HashSet<Integer>();
-		int begin = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (i - begin > k) {
-				history.remove(nums[begin]);
-				begin++;
-			}
-			if (history.contains(nums[i])) {
-				return true;
-			}
-			history.add(nums[i]);
-		}
-		return false;
-	}
+class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Set<Integer> seen = new HashSet<>();
+    int beginIndex = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (i - beginIndex == k + 1) {
+        seen.remove(nums[beginIndex]);
+        ++beginIndex;
+      }
+      if (seen.contains(nums[i])) {
+        return true;
+      }
+
+      seen.add(nums[i]);
+    }
+
+    return false;
+  }
 }
