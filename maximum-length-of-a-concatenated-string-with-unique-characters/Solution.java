090ec05c3551645529923edcc9e556caf5b4db7e
Kai Wang <charles.wangkai@gmail.com>
2022-10-24 09:11:02 +0800

    Maximum Length of a Concatenated String with Unique Characters

--- maximum-length-of-a-concatenated-string-with-unique-characters/Solution.java
+++ maximum-length-of-a-concatenated-string-with-unique-characters/Solution.java
@@ -13,15 +13,15 @@ class Solution {
     int result = search(arr, used, index + 1, length);
 
     String s = arr.get(index);
-    if (s.chars().distinct().count() == s.length() && s.chars().allMatch(ch -> !used[ch - 'a'])) {
-      for (char ch : s.toCharArray()) {
-        used[ch - 'a'] = true;
+    if (s.chars().distinct().count() == s.length() && s.chars().allMatch(c -> !used[c - 'a'])) {
+      for (char c : s.toCharArray()) {
+        used[c - 'a'] = true;
       }
 
       result = Math.max(result, search(arr, used, index + 1, length + s.length()));
 
-      for (char ch : s.toCharArray()) {
-        used[ch - 'a'] = false;
+      for (char c : s.toCharArray()) {
+        used[c - 'a'] = false;
       }
     }
 
