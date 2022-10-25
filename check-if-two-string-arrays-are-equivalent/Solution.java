aa4c05e837f0535fd9ea92c2c92c76157b6626ee
Kai Wang <charles.wangkai@gmail.com>
2022-10-25 08:06:42 +0800

    Check If Two String Arrays are Equivalent

--- check-if-two-string-arrays-are-equivalent/Solution.java
+++ check-if-two-string-arrays-are-equivalent/Solution.java
@@ -1,10 +1,5 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 class Solution {
   public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-    return Arrays.stream(word1)
-        .collect(Collectors.joining())
-        .equals(Arrays.stream(word2).collect(Collectors.joining()));
+    return String.join("", word1).equals(String.join("", word2));
   }
 }
