7518746c912409e303e423ed8a139b551acf4409
Kai Wang <charles.wangkai@gmail.com>
2022-10-26 09:29:09 +0800

    Continuous Subarray Sum

--- continuous-subarray-sum/Solution.java
+++ continuous-subarray-sum/Solution.java
@@ -1,44 +1,26 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Solution {
-	public boolean checkSubarraySum(int[] nums, int k) {
-		if (k == 0) {
-			return hasTwoContinuousZeros(nums);
-		}
+class Solution {
+  public boolean checkSubarraySum(int[] nums, int k) {
+    Set<Integer> seen = new HashSet<>();
 
-		Set<Integer> remainders = new HashSet<Integer>();
+    int curr = 0;
+    int prev = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      curr = addMod(curr, nums[i], k);
+      if (seen.contains(curr)) {
+        return true;
+      }
 
-		int[] runningSums = new int[nums.length];
-		int runningSum = 0;
-		for (int i = 0; i < runningSums.length; i++) {
-			runningSum = addMod(runningSum, nums[i], k);
-			runningSums[i] = runningSum;
+      seen.add(prev);
+      prev = curr;
+    }
 
-			if (i == 1) {
-				remainders.add(0);
-			} else if (i >= 2) {
-				remainders.add(runningSums[i - 2]);
-			}
+    return false;
+  }
 
-			if (remainders.contains(runningSums[i])) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	int addMod(int x, int y, int modulus) {
-		return (x + y) % modulus;
-	}
-
-	boolean hasTwoContinuousZeros(int[] nums) {
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i] == 0 && nums[i + 1] == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
+  int addMod(int x, int y, int m) {
+    return Math.floorMod(x + y, m);
+  }
 }
