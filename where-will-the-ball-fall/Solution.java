c46e20288f0e647a342a01ada67b868fbaa1c9ca
Kai Wang <charles.wangkai@gmail.com>
2022-11-01 08:34:52 +0800

    Where Will the Ball Fall

--- where-will-the-ball-fall/Solution.java
+++ where-will-the-ball-fall/Solution.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
   public int[] findBall(int[][] grid) {
     int m = grid.length;
     int n = grid[0].length;
 
     int[] result = new int[n];
+    Arrays.fill(result, -1);
     for (int i = 0; i < result.length; ++i) {
       int c = i;
       for (int r = 0; r <= m; ++r) {
@@ -11,16 +14,12 @@ class Solution {
           result[i] = c;
         } else if (grid[r][c] == 1) {
           if (c == n - 1 || grid[r][c + 1] == -1) {
-            result[i] = -1;
-
             break;
           }
 
           ++c;
         } else {
           if (c == 0 || grid[r][c - 1] == 1) {
-            result[i] = -1;
-
             break;
           }
 
