ac207e16a0c0526b25af92e6c2534a6dafe4105d
Kai Wang <charles.wangkai@gmail.com>
2022-11-05 19:26:24 +0800

    My Calendar III

--- my-calendar-iii/MyCalendarThree.java
+++ my-calendar-iii/MyCalendarThree.java
@@ -1,51 +1,23 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 class MyCalendarThree {
-  private SortedSet<Event> events = new TreeSet<>(Comparator.comparing(Event::start));
-  private int maxCount = 1;
+  SortedMap<Integer, Integer> timeToDelta = new TreeMap<>();
 
-  public int book(int start, int end) {
-    int start_ = start;
-    List<Event> impacts =
-        events.stream().filter(e -> !(e.start() >= end || e.end() <= start_)).toList();
-    events.removeAll(impacts);
+  public int book(int startTime, int endTime) {
+    timeToDelta.put(startTime, timeToDelta.getOrDefault(startTime, 0) + 1);
+    timeToDelta.put(endTime, timeToDelta.getOrDefault(endTime, 0) - 1);
 
-    if (!impacts.isEmpty()) {
-      Event firstImpact = impacts.get(0);
-      if (firstImpact.start() < start) {
-        events.add(new Event(firstImpact.start(), start, firstImpact.count()));
-      }
-
-      Event lastImpact = impacts.get(impacts.size() - 1);
-      if (lastImpact.end() > end) {
-        events.add(new Event(end, lastImpact.end(), lastImpact.count()));
-      }
+    int result = 0;
+    int depth = 0;
+    for (int delta : timeToDelta.values()) {
+      depth += delta;
+      result = Math.max(result, depth);
     }
 
-    for (Event impact : impacts) {
-      if (start < impact.start()) {
-        events.add(new Event(start, impact.start(), 1));
-      }
-
-      int currStart = Math.max(start, impact.start());
-      int nextStart = Math.min(end, impact.end());
-      events.add(new Event(currStart, nextStart, impact.count() + 1));
-      maxCount = Math.max(maxCount, impact.count() + 1);
-
-      start = nextStart;
-    }
-    if (start < end) {
-      events.add(new Event(start, end, 1));
-    }
-
-    return maxCount;
+    return result;
   }
 }
-
-record Event(int start, int end, int count) {}
 
 // Your MyCalendarThree object will be instantiated and called as such:
 // MyCalendarThree obj = new MyCalendarThree();
