95796130858287073c3c363d307ee264a4e4dd38
Kai Wang <charles.wangkai@gmail.com>
2022-11-07 08:39:33 +0800

    Maximum 69 Number

--- maximum-69-number/Solution.java
+++ maximum-69-number/Solution.java
@@ -1,11 +1,10 @@
-public class Solution {
-	public int maximum69Number(int num) {
-		String s = String.valueOf(num);
-		int index = s.indexOf('6');
-		if (index >= 0) {
-			return Integer.parseInt(String.format("%s9%s", s.substring(0, index), s.substring(index + 1)));
-		} else {
-			return num;
-		}
-	}
+class Solution {
+  public int maximum69Number(int num) {
+    String s = String.valueOf(num);
+    int index = s.indexOf('6');
+
+    return (index >= 0)
+        ? Integer.parseInt(String.format("%s9%s", s.substring(0, index), s.substring(index + 1)))
+        : num;
+  }
 }
