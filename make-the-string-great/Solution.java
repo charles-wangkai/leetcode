eab74abc4d423df904df4ab4e136493118131f08
Kai Wang <charles.wangkai@gmail.com>
2022-11-08 14:02:01 +0800

    Make The String Great

--- make-the-string-great/Solution.java
+++ make-the-string-great/Solution.java
@@ -2,17 +2,18 @@ import java.util.Stack;
 import java.util.stream.Collectors;
 
 class Solution {
-    public String makeGood(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (!stack.empty() && stack.peek() != ch
-                    && Character.toLowerCase(stack.peek()) == Character.toLowerCase(ch)) {
-                stack.pop();
-            } else {
-                stack.push(ch);
-            }
-        }
-
-        return stack.stream().map(String::valueOf).collect(Collectors.joining());
+  public String makeGood(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (!stack.empty()
+          && stack.peek() != c
+          && Character.toLowerCase(stack.peek()) == Character.toLowerCase(c)) {
+        stack.pop();
+      } else {
+        stack.push(c);
+      }
     }
+
+    return stack.stream().map(String::valueOf).collect(Collectors.joining());
+  }
 }
