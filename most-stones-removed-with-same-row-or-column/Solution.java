53c00d37d02acdeef61c9594af3593fec9adcf72
Kai Wang <charles.wangkai@gmail.com>
2022-11-14 21:03:08 +0800

    Most Stones Removed with Same Row or Column

--- most-stones-removed-with-same-row-or-column/Solution.java
+++ most-stones-removed-with-same-row-or-column/Solution.java
@@ -1,52 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public int removeStones(int[][] stones) {
-		Stone[] stoneArray = new Stone[stones.length];
-		for (int i = 0; i < stoneArray.length; i++) {
-			stoneArray[i] = new Stone(stones[i][0], stones[i][1]);
-		}
+class Solution {
+  public int removeStones(int[][] stones) {
+    int n = stones.length;
 
-		for (int i = 0; i < stones.length; i++) {
-			for (int j = i + 1; j < stones.length; j++) {
-				if (stoneArray[i].x == stoneArray[j].x || stoneArray[i].y == stoneArray[j].y) {
-					stoneArray[i].adjacents.add(j);
-					stoneArray[j].adjacents.add(i);
-				}
-			}
-		}
+    @SuppressWarnings("unchecked")
+    List<Integer>[] adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+          adjLists[i].add(j);
+          adjLists[j].add(i);
+        }
+      }
+    }
 
-		int result = 0;
-		boolean[] visited = new boolean[stoneArray.length];
-		for (int i = 0; i < stoneArray.length; i++) {
-			if (visited[i]) {
-				result++;
-			} else {
-				dfs(stoneArray, visited, i);
-			}
-		}
-		return result;
-	}
+    int result = 0;
+    boolean[] visited = new boolean[n];
+    for (int i = 0; i < n; ++i) {
+      if (visited[i]) {
+        ++result;
+      } else {
+        search(stones, adjLists, visited, i);
+      }
+    }
 
-	void dfs(Stone[] stoneArray, boolean[] visited, int index) {
-		visited[index] = true;
+    return result;
+  }
 
-		for (int adjacent : stoneArray[index].adjacents) {
-			if (!visited[adjacent]) {
-				dfs(stoneArray, visited, adjacent);
-			}
-		}
-	}
-}
+  void search(int[][] stones, List<Integer>[] adjLists, boolean[] visited, int index) {
+    visited[index] = true;
 
-class Stone {
-	int x;
-	int y;
-	List<Integer> adjacents = new ArrayList<>();
-
-	Stone(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    for (int adj : adjLists[index]) {
+      if (!visited[adj]) {
+        search(stones, adjLists, visited, adj);
+      }
+    }
+  }
 }
