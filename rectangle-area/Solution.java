27507fe56fb2768bbed1ad88e641adb7ac73d340
Kai Wang <charles.wangkai@gmail.com>
2022-11-17 08:30:36 +0800

    Rectangle Area

--- rectangle-area/Solution.java
+++ rectangle-area/Solution.java
@@ -1,11 +1,11 @@
-public class Solution {
-	public int computeArea(int A, int B, int C, int D, int E, int F, int G,
-			int H) {
-		return (int) (getArea(A, B, C, D) + getArea(E, F, G, H) - getArea(
-				Math.max(A, E), Math.max(B, F), Math.min(C, G), Math.min(D, H)));
-	}
+class Solution {
+  public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+    return getArea(ax1, ay1, ax2, ay2)
+        + getArea(bx1, by1, bx2, by2)
+        - getArea(Math.max(ax1, bx1), Math.max(ay1, by1), Math.min(ax2, bx2), Math.min(ay2, by2));
+  }
 
-	long getArea(int x0, int y0, int x1, int y1) {
-		return Math.max(x1 - x0, 0L) * Math.max(y1 - y0, 0L);
-	}
+  int getArea(int x1, int y1, int x2, int y2) {
+    return Math.max(x2 - x1, 0) * Math.max(y2 - y1, 0);
+  }
 }
