17929e5a6c0fbc211888762f7034ae82670631dd
Kai Wang <charles.wangkai@gmail.com>
2022-11-22 10:03:38 +0800

    Perfect Squares

--- perfect-squares/Solution.java
+++ perfect-squares/Solution.java
@@ -1,55 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
-  Map<Integer, Integer> cache = new HashMap<>();
-
   public int numSquares(int n) {
-    return search(n, computeSquareNumLimit(n));
-  }
-
-  int computeSquareNumLimit(int n) {
-    int squareNumLimit = 0;
-    while (n != 0) {
-      int root = (int) Math.round(Math.sqrt(n));
-      if (root * root > n) {
-        --root;
-      }
-      n -= root * root;
-      ++squareNumLimit;
-    }
-
-    return squareNumLimit;
-  }
-
-  Integer search(int n, int squareNumLimit) {
-    if (n == 0) {
-      return 0;
-    }
-    if (squareNumLimit == 0) {
-      return null;
-    }
-
-    if (cache.containsKey(n)) {
-      int result = cache.get(n);
-
-      return (result <= squareNumLimit) ? result : null;
-    }
-
-    Integer result = null;
-    for (int i = 1; i * i <= n; ++i) {
-      Integer subResult = search(n - i * i, squareNumLimit - 1);
-
-      if (subResult != null && (result == null || subResult + 1 < result)) {
-        result = subResult + 1;
-        squareNumLimit = subResult;
+    int[] dp = new int[n + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
+    for (int i = 1; i < dp.length; ++i) {
+      for (int j = 1; j * j <= i; ++j) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
       }
     }
 
-    if (result != null) {
-      cache.put(n, result);
-    }
-
-    return result;
+    return dp[dp.length - 1];
   }
 }
