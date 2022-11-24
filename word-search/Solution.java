e336f8febea103c7a20fae0cf487c73d5ca524a5
Kai Wang <charles.wangkai@gmail.com>
2022-11-24 09:00:59 +0800

    Word Search

--- word-search/Solution.java
+++ word-search/Solution.java
@@ -3,13 +3,13 @@ class Solution {
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
   public boolean exist(char[][] board, String word) {
-    int row = board.length;
-    int col = board[0].length;
+    int m = board.length;
+    int n = board[0].length;
 
-    boolean[][] used = new boolean[row][col];
-    for (int r = 0; r < row; ++r) {
-      for (int c = 0; c < col; ++c) {
-        if (check(board, word, used, r, c, 0)) {
+    boolean[][] visited = new boolean[m][n];
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (check(board, word, visited, r, c, 0)) {
           return true;
         }
       }
@@ -18,30 +18,30 @@ class Solution {
     return false;
   }
 
-  boolean check(char[][] board, String word, boolean[][] used, int r, int c, int index) {
-    int row = board.length;
-    int col = board[0].length;
+  boolean check(char[][] board, String word, boolean[][] visited, int r, int c, int index) {
+    int m = board.length;
+    int n = board[0].length;
 
     if (index == word.length()) {
       return true;
     }
 
-    if (!(r >= 0 && r < row && c >= 0 && c < col)
-        || used[r][c]
+    if (!(r >= 0 && r < m && c >= 0 && c < n)
+        || visited[r][c]
         || board[r][c] != word.charAt(index)) {
       return false;
     }
 
     boolean result = false;
-    used[r][c] = true;
+    visited[r][c] = true;
     for (int i = 0; i < R_OFFSETS.length; ++i) {
-      if (check(board, word, used, r + R_OFFSETS[i], c + C_OFFSETS[i], index + 1)) {
+      if (check(board, word, visited, r + R_OFFSETS[i], c + C_OFFSETS[i], index + 1)) {
         result = true;
 
         break;
       }
     }
-    used[r][c] = false;
+    visited[r][c] = false;
 
     return result;
   }
