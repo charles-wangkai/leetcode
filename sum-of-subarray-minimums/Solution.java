1df64c55bf3ffe66b5e4485b9bf79981a33fd9db
Kai Wang <charles.wangkai@gmail.com>
2022-11-25 10:23:17 +0800

    Sum of Subarray Minimums

--- sum-of-subarray-minimums/Solution.java
+++ sum-of-subarray-minimums/Solution.java
@@ -1,43 +1,44 @@
 import java.util.Stack;
+import java.util.stream.IntStream;
 
-public class Solution {
-	static final int MOD_DIVISOR = 1_000_000_007;
+class Solution {
+  static final int MODULUS = 1_000_000_007;
 
-	public int sumSubarrayMins(int[] A) {
-		int[] leftIndices = new int[A.length];
-		Stack<Integer> leftAscendingIndices = new Stack<>();
-		for (int i = 0; i < leftIndices.length; i++) {
-			while (!leftAscendingIndices.empty() && A[i] <= A[leftAscendingIndices.peek()]) {
-				leftAscendingIndices.pop();
-			}
+  public int sumSubarrayMins(int[] arr) {
+    int[] leftIndices = new int[arr.length];
+    Stack<Integer> leftAscendingIndices = new Stack<>();
+    for (int i = 0; i < leftIndices.length; ++i) {
+      while (!leftAscendingIndices.empty() && arr[i] <= arr[leftAscendingIndices.peek()]) {
+        leftAscendingIndices.pop();
+      }
 
-			leftIndices[i] = (leftAscendingIndices.empty() ? -1 : leftAscendingIndices.peek()) + 1;
-			leftAscendingIndices.push(i);
-		}
+      leftIndices[i] = (leftAscendingIndices.empty() ? -1 : leftAscendingIndices.peek()) + 1;
+      leftAscendingIndices.push(i);
+    }
 
-		int[] rightIndices = new int[A.length];
-		Stack<Integer> rightAscendingIndices = new Stack<>();
-		for (int i = rightIndices.length - 1; i >= 0; i--) {
-			while (!rightAscendingIndices.empty() && A[i] < A[rightAscendingIndices.peek()]) {
-				rightAscendingIndices.pop();
-			}
+    int[] rightIndices = new int[arr.length];
+    Stack<Integer> rightAscendingIndices = new Stack<>();
+    for (int i = rightIndices.length - 1; i >= 0; --i) {
+      while (!rightAscendingIndices.empty() && arr[i] < arr[rightAscendingIndices.peek()]) {
+        rightAscendingIndices.pop();
+      }
 
-			rightIndices[i] = (rightAscendingIndices.empty() ? rightIndices.length : rightAscendingIndices.peek()) - 1;
-			rightAscendingIndices.push(i);
-		}
+      rightIndices[i] =
+          (rightAscendingIndices.empty() ? rightIndices.length : rightAscendingIndices.peek()) - 1;
+      rightAscendingIndices.push(i);
+    }
 
-		int result = 0;
-		for (int i = 0; i < A.length; i++) {
-			result = addMod(result, multiplyMod(A[i], multiplyMod(i - leftIndices[i] + 1, rightIndices[i] - i + 1)));
-		}
-		return result;
-	}
+    return IntStream.range(0, arr.length)
+        .map(i -> multiplyMod(arr[i], multiplyMod(i - leftIndices[i] + 1, rightIndices[i] - i + 1)))
+        .reduce(this::addMod)
+        .getAsInt();
+  }
 
-	int addMod(int x, int y) {
-		return (x + y) % MOD_DIVISOR;
-	}
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 
-	int multiplyMod(int x, int y) {
-		return (int) ((long) x * y % MOD_DIVISOR);
-	}
+  int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, MODULUS);
+  }
 }
