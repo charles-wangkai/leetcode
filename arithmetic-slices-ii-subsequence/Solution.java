16bf119f942d9e4e8654cd225f9ab06c141d6deb
Kai Wang <charles.wangkai@gmail.com>
2022-11-27 09:53:04 +0800

    Arithmetic Slices II - Subsequence

--- arithmetic-slices-ii-subsequence/Solution.java
+++ arithmetic-slices-ii-subsequence/Solution.java
@@ -13,14 +13,14 @@ class Solution {
     for (int i = 0; i < nums.length; ++i) {
       for (int j = 0; j < i; ++j) {
         long d = (long) nums[i] - nums[j];
-        if (d < Integer.MIN_VALUE || d > Integer.MAX_VALUE) {
-          continue;
-        }
-        int delta = (int) d;
+        if (d >= Integer.MIN_VALUE && d <= Integer.MAX_VALUE) {
+          int delta = (int) d;
 
-        int prevCount = deltaToCountMaps[j].getOrDefault(delta, 0);
-        deltaToCountMaps[i].put(delta, deltaToCountMaps[i].getOrDefault(delta, 0) + prevCount + 1);
-        result += prevCount;
+          int prevCount = deltaToCountMaps[j].getOrDefault(delta, 0);
+          deltaToCountMaps[i].put(
+              delta, deltaToCountMaps[i].getOrDefault(delta, 0) + prevCount + 1);
+          result += prevCount;
+        }
       }
     }
 
