37ed5a8be89d43cfea57d60c20a9dfb36bd35fa3
Kai Wang <charles.wangkai@gmail.com>
2022-11-28 09:28:55 +0800

    Find Players With Zero or One Losses

--- find-players-with-zero-or-one-losses/Solution.java
+++ find-players-with-zero-or-one-losses/Solution.java
@@ -1,16 +1,12 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
   public List<List<Integer>> findWinners(int[][] matches) {
     Map<Integer, Integer> playerToLoseCount = new HashMap<>();
     for (int[] match : matches) {
-      if (!playerToLoseCount.containsKey(match[0])) {
-        playerToLoseCount.put(match[0], 0);
-      }
-
+      playerToLoseCount.putIfAbsent(match[0], 0);
       playerToLoseCount.put(match[1], playerToLoseCount.getOrDefault(match[1], 0) + 1);
     }
 
@@ -21,6 +17,6 @@ class Solution {
     return playerToLoseCount.keySet().stream()
         .filter(player -> playerToLoseCount.get(player) == loseCount)
         .sorted()
-        .collect(Collectors.toList());
+        .toList();
   }
 }
