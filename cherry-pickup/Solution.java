b61368648eeaf7865b9314dca9bcd05fd8bae268
Kai Wang <charles.wangkai@gmail.com>
2022-12-02 11:39:29 +0800

    Cherry Pickup

--- cherry-pickup/Solution.java
+++ cherry-pickup/Solution.java
@@ -1,51 +1,49 @@
-public class Solution {
-	public int cherryPickup(int[][] grid) {
-		int N = grid.length;
+// https://leetcode.com/problems/cherry-pickup/solutions/127413/cherry-pickup/
+// Approach #3: Dynamic Programming (Bottom Up) [Accepted]
 
-		int[][] dp = init(N);
-		dp[0][0] = grid[0][0];
+class Solution {
+  public int cherryPickup(int[][] grid) {
+    int n = grid.length;
 
-		for (int k = 1; k <= 2 * (N - 1); k++) {
-			int[][] curr = init(N);
+    int[][] dp = init(n);
+    dp[0][0] = grid[0][0];
 
-			for (int i = Math.max(0, k - N + 1); i < N && i <= k; i++) {
-				for (int j = Math.max(0, k - N + 1); j < N && j <= k; j++) {
-					if (grid[i][k - i] < 0 || grid[j][k - j] < 0) {
-						continue;
-					}
+    for (int step = 1; step <= 2 * (n - 1); ++step) {
+      int[][] nextDp = init(n);
+      for (int r1 = 0; r1 < n; ++r1) {
+        int c1 = step - r1;
+        if (c1 >= 0 && c1 < n) {
+          for (int r2 = 0; r2 < n; ++r2) {
+            int c2 = step - r2;
+            if (c2 >= 0 && c2 < n) {
+              int maxPrev =
+                  Math.max(
+                      Math.max(dp[r1][r2], (r1 == 0) ? -1 : dp[r1 - 1][r2]),
+                      Math.max(
+                          (r2 == 0) ? -1 : dp[r1][r2 - 1],
+                          (r1 == 0 || r2 == 0) ? -1 : dp[r1 - 1][r2 - 1]));
+              if (maxPrev != -1 && grid[r1][c1] != -1 && grid[r2][c2] != -1) {
+                nextDp[r1][r2] = maxPrev + grid[r1][c1] + ((r1 == r2) ? 0 : grid[r2][c2]);
+              }
+            }
+          }
+        }
+      }
 
-					int cherries = dp[i][j];
-					if (i > 0) {
-						cherries = Math.max(cherries, dp[i - 1][j]);
-					}
-					if (j > 0) {
-						cherries = Math.max(cherries, dp[i][j - 1]);
-					}
-					if (i > 0 && j > 0) {
-						cherries = Math.max(cherries, dp[i - 1][j - 1]);
-					}
+      dp = nextDp;
+    }
 
-					if (cherries < 0) {
-						continue;
-					}
+    return Math.max(0, dp[n - 1][n - 1]);
+  }
 
-					curr[i][j] = cherries + grid[i][k - i] + (i == j ? 0 : grid[j][k - j]);
-				}
-			}
+  int[][] init(int n) {
+    int[][] result = new int[n][n];
+    for (int r1 = 0; r1 < n; ++r1) {
+      for (int r2 = 0; r2 < n; ++r2) {
+        result[r1][r2] = -1;
+      }
+    }
 
-			dp = curr;
-		}
-
-		return Math.max(0, dp[N - 1][N - 1]);
-	}
-
-	int[][] init(int size) {
-		int[][] result = new int[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				result[i][j] = -1;
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 }
