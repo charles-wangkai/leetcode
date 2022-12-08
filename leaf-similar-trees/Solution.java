8ae5d0df7ff94fc52c2a5b90e4d1d12f86b5310a
Kai Wang <charles.wangkai@gmail.com>
2022-12-08 08:57:53 +0800

    Leaf-Similar Trees

--- leaf-similar-trees/Solution.java
+++ leaf-similar-trees/Solution.java
@@ -3,37 +3,45 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-		List<Integer> leafValues1 = new ArrayList<>();
-		search(leafValues1, root1);
+class Solution {
+  public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    List<Integer> leafValues1 = new ArrayList<>();
+    search(leafValues1, root1);
 
-		List<Integer> leafValues2 = new ArrayList<>();
-		search(leafValues2, root2);
+    List<Integer> leafValues2 = new ArrayList<>();
+    search(leafValues2, root2);
 
-		return leafValues1.equals(leafValues2);
-	}
+    return leafValues1.equals(leafValues2);
+  }
 
-	void search(List<Integer> leafValues, TreeNode node) {
-		if (node == null) {
-			return;
-		}
+  void search(List<Integer> leafValues, TreeNode node) {
+    if (node.left != null) {
+      search(leafValues, node.left);
+    }
 
-		search(leafValues, node.left);
+    if (node.left == null && node.right == null) {
+      leafValues.add(node.val);
+    }
 
-		if (node.left == null && node.right == null) {
-			leafValues.add(node.val);
-		}
-
-		search(leafValues, node.right);
-	}
+    if (node.right != null) {
+      search(leafValues, node.right);
+    }
+  }
 }
