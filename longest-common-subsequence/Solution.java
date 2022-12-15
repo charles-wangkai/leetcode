b9d0de448222830bb009a428e5a07f6e4e45f1f5
Kai Wang <charles.wangkai@gmail.com>
2022-12-15 11:34:13 +0800

    Longest Common Subsequence

--- longest-common-subsequence/Solution.java
+++ longest-common-subsequence/Solution.java
@@ -1,15 +1,12 @@
 class Solution {
   public int longestCommonSubsequence(String text1, String text2) {
     int[][] maxLengths = new int[text1.length() + 1][text2.length() + 1];
-    for (int i = 0; i <= text1.length(); ++i) {
-      for (int j = 0; j <= text2.length(); ++j) {
-        if (i != 0 && j != 0) {
-          if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-            maxLengths[i][j] = maxLengths[i - 1][j - 1] + 1;
-          } else {
-            maxLengths[i][j] = Math.max(maxLengths[i - 1][j], maxLengths[i][j - 1]);
-          }
-        }
+    for (int i = 1; i <= text1.length(); ++i) {
+      for (int j = 1; j <= text2.length(); ++j) {
+        maxLengths[i][j] =
+            (text1.charAt(i - 1) == text2.charAt(j - 1))
+                ? (maxLengths[i - 1][j - 1] + 1)
+                : Math.max(maxLengths[i - 1][j], maxLengths[i][j - 1]);
       }
     }
 
