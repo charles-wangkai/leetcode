77d85285f798b2f54e4f01faff415abd3b88576b
Kai Wang <charles.wangkai@gmail.com>
2022-12-17 10:22:45 +0800

    Evaluate Reverse Polish Notation

--- evaluate-reverse-polish-notation/Solution.java
+++ evaluate-reverse-polish-notation/Solution.java
@@ -1,23 +1,24 @@
+import static java.util.Map.entry;
+
+import java.util.Map;
 import java.util.Stack;
+import java.util.function.BiFunction;
 
 class Solution {
+  static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATOR_TO_CALCULATOR =
+      Map.ofEntries(
+          entry("+", (x, y) -> x + y),
+          entry("-", (x, y) -> x - y),
+          entry("*", (x, y) -> x * y),
+          entry("/", (x, y) -> x / y));
+
   public int evalRPN(String[] tokens) {
     Stack<Integer> stack = new Stack<>();
     for (String token : tokens) {
-      if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+      if (OPERATOR_TO_CALCULATOR.containsKey(token)) {
         int operand2 = stack.pop();
         int operand1 = stack.pop();
-        int result;
-        if (token.equals("+")) {
-          result = operand1 + operand2;
-        } else if (token.equals("-")) {
-          result = operand1 - operand2;
-        } else if (token.equals("*")) {
-          result = operand1 * operand2;
-        } else {
-          result = operand1 / operand2;
-        }
-        stack.push(result);
+        stack.push(OPERATOR_TO_CALCULATOR.get(token).apply(operand1, operand2));
       } else {
         stack.push(Integer.parseInt(token));
       }
