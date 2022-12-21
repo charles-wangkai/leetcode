f46e66d89e5ea43b68dd4f4f2049296266860366
Kai Wang <charles.wangkai@gmail.com>
2022-12-21 09:22:51 +0800

    Possible Bipartition

--- possible-bipartition/Solution.java
+++ possible-bipartition/Solution.java
@@ -1,47 +1,44 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public boolean possibleBipartition(int N, int[][] dislikes) {
-		@SuppressWarnings("unchecked")
-		List<Integer>[] adjacentLists = new List[N];
-		for (int i = 0; i < adjacentLists.length; ++i) {
-			adjacentLists[i] = new ArrayList<>();
-		}
+class Solution {
+  public boolean possibleBipartition(int n, int[][] dislikes) {
+    @SuppressWarnings("unchecked")
+    List<Integer>[] adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<>();
+    }
+    for (int[] dislike : dislikes) {
+      int person1 = dislike[0] - 1;
+      int person2 = dislike[1] - 1;
 
-		for (int[] dislike : dislikes) {
-			int person1 = dislike[0] - 1;
-			int person2 = dislike[1] - 1;
+      adjLists[person1].add(person2);
+      adjLists[person2].add(person1);
+    }
 
-			adjacentLists[person1].add(person2);
-			adjacentLists[person2].add(person1);
-		}
+    int[] groups = new int[n];
+    for (int i = 0; i < groups.length; ++i) {
+      if (groups[i] == 0 && !search(adjLists, groups, i, 1)) {
+        return false;
+      }
+    }
 
-		int[] groups = new int[N];
-		for (int i = 0; i < groups.length; i++) {
-			if (groups[i] == 0) {
-				if (!search(adjacentLists, groups, i, 1)) {
-					return false;
-				}
-			}
-		}
+    return true;
+  }
 
-		return true;
-	}
+  boolean search(List<Integer>[] adjLists, int[] groups, int person, int expectedGroup) {
+    if (groups[person] != 0) {
+      return groups[person] == expectedGroup;
+    }
 
-	boolean search(List<Integer>[] adjacentLists, int[] groups, int person, int expectedGroup) {
-		if (groups[person] != 0) {
-			return groups[person] == expectedGroup;
-		}
+    groups[person] = expectedGroup;
 
-		groups[person] = expectedGroup;
+    for (int adj : adjLists[person]) {
+      if (!search(adjLists, groups, adj, 3 - expectedGroup)) {
+        return false;
+      }
+    }
 
-		for (int adjacent : adjacentLists[person]) {
-			if (!search(adjacentLists, groups, adjacent, 3 - expectedGroup)) {
-				return false;
-			}
-		}
-
-		return true;
-	}
+    return true;
+  }
 }
