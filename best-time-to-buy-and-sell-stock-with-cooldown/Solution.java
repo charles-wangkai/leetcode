33e608a7494763b4db16db163f8effdaa841012c
Kai Wang <charles.wangkai@gmail.com>
2022-12-23 09:09:01 +0800

    Best Time to Buy and Sell Stock with Cooldown

--- best-time-to-buy-and-sell-stock-with-cooldown/Solution.java
+++ best-time-to-buy-and-sell-stock-with-cooldown/Solution.java
@@ -1,17 +1,14 @@
 class Solution {
-	public int maxProfit(int[] prices) {
-		if (prices.length == 0) {
-			return 0;
-		}
+  public int maxProfit(int[] prices) {
+    int[] profits = new int[prices.length];
+    for (int i = 1; i < profits.length; ++i) {
+      profits[i] = profits[i - 1];
+      for (int j = 0; j < i; ++j) {
+        profits[i] =
+            Math.max(profits[i], (prices[i] - prices[j]) + ((j >= 2) ? profits[j - 2] : 0));
+      }
+    }
 
-		int[] profits = new int[prices.length];
-		for (int i = 1; i < profits.length; ++i) {
-			profits[i] = profits[i - 1];
-			for (int j = 0; j < i; ++j) {
-				profits[i] = Math.max(profits[i], (prices[i] - prices[j]) + (j >= 2 ? profits[j - 2] : 0));
-			}
-		}
-
-		return profits[profits.length - 1];
-	}
+    return profits[profits.length - 1];
+  }
 }
