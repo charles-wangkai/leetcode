0cfa37a4ec17f105e456a8d1068049815a674a9f
Kai Wang <charles.wangkai@gmail.com>
2022-12-26 08:07:56 +0800

    Jump Game

--- jump-game/Solution.java
+++ jump-game/Solution.java
@@ -1,15 +1,14 @@
 class Solution {
   public boolean canJump(int[] nums) {
     int maxIndex = 0;
-    for (int i = 0; ; ++i) {
-      if (maxIndex >= nums.length - 1) {
-        return true;
-      }
+    for (int i = 0; i < nums.length; ++i) {
       if (i == maxIndex + 1) {
         return false;
       }
 
       maxIndex = Math.max(maxIndex, i + nums[i]);
     }
+
+    return true;
   }
 }
