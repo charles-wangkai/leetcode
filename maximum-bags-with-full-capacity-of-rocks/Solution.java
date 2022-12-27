091ef51ccf2172d9ca2f2ac905b1cb2753b7e580
Kai Wang <charles.wangkai@gmail.com>
2022-12-27 09:00:24 +0800

    Maximum Bags With Full Capacity of Rocks

--- maximum-bags-with-full-capacity-of-rocks/Solution.java
+++ maximum-bags-with-full-capacity-of-rocks/Solution.java
@@ -3,12 +3,7 @@ import java.util.stream.IntStream;
 class Solution {
   public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
     int[] diffs =
-        IntStream.range(0, capacity.length)
-            .map(i -> capacity[i] - rocks[i])
-            .boxed()
-            .sorted()
-            .mapToInt(x -> x)
-            .toArray();
+        IntStream.range(0, capacity.length).map(i -> capacity[i] - rocks[i]).sorted().toArray();
 
     int result = 0;
     for (int i = 0; i < diffs.length; ++i) {
