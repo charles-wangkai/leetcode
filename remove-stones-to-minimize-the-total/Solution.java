b0b39c8784473a1874207f06b331318c8b26c751
Kai Wang <charles.wangkai@gmail.com>
2022-12-28 12:57:19 +0800

    Remove Stones to Minimize the Total

--- remove-stones-to-minimize-the-total/Solution.java
+++ remove-stones-to-minimize-the-total/Solution.java
@@ -1,15 +1,15 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
   public int minStoneSum(int[] piles, int k) {
     PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-    int result = 0;
     for (int pile : piles) {
       pq.offer(pile);
-      result += pile;
     }
 
+    int result = Arrays.stream(piles).sum();
     for (int i = 0; i < k; ++i) {
       int head = pq.poll();
       int rest = head - head / 2;
