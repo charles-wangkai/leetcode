f5f291877ac648c006bff4661c0c9491c6fa0545
Kai Wang <charles.wangkai@gmail.com>
2023-01-01 08:36:52 +0800

    Confusing Number

--- confusing-number/Solution.java
+++ confusing-number/Solution.java
@@ -1,34 +1,22 @@
-import java.util.HashMap;
+import static java.util.Map.entry;
+
 import java.util.Map;
 
-public class Solution {
-	static final Map<Integer, Integer> DIGIT_TO_ROTATED = new HashMap<>();
-	static {
-		DIGIT_TO_ROTATED.put(0, 0);
-		DIGIT_TO_ROTATED.put(1, 1);
-		DIGIT_TO_ROTATED.put(6, 9);
-		DIGIT_TO_ROTATED.put(8, 8);
-		DIGIT_TO_ROTATED.put(9, 6);
-	}
+class Solution {
+  static final Map<Integer, Integer> DIGIT_TO_ROTATED =
+      Map.ofEntries(entry(0, 0), entry(1, 1), entry(6, 9), entry(8, 8), entry(9, 6));
 
-	public boolean confusingNumber(int N) {
-		Integer rotated = rotate(N);
+  public boolean confusingNumber(int n) {
+    int rotated = 0;
+    for (int rest = n; rest != 0; rest /= 10) {
+      int digit = rest % 10;
+      if (!DIGIT_TO_ROTATED.containsKey(digit)) {
+        return false;
+      }
 
-		return rotated != null && rotated != N;
-	}
+      rotated = rotated * 10 + DIGIT_TO_ROTATED.get(digit);
+    }
 
-	Integer rotate(int N) {
-		int result = 0;
-		while (N != 0) {
-			int digit = N % 10;
-
-			if (!DIGIT_TO_ROTATED.containsKey(digit)) {
-				return null;
-			}
-
-			result = result * 10 + DIGIT_TO_ROTATED.get(digit);
-			N /= 10;
-		}
-		return result;
-	}
+    return rotated != n;
+  }
 }
