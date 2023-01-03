41e47b7f4b13cb917059b525a2701f146b6d8c51
Kai Wang <charles.wangkai@gmail.com>
2023-01-03 09:25:31 +0800

    Delete Columns to Make Sorted

--- delete-columns-to-make-sorted/Solution.java
+++ delete-columns-to-make-sorted/Solution.java
@@ -1,11 +1,13 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int minDeletionSize(String[] A) {
-		return (int) IntStream.range(0, A[0].length()).filter(c -> !isNonDecreasing(A, c)).count();
-	}
-
-	static boolean isNonDecreasing(String[] A, int c) {
-		return IntStream.range(0, A.length - 1).allMatch(i -> A[i].charAt(c) <= A[i + 1].charAt(c));
-	}
+class Solution {
+  public int minDeletionSize(String[] strs) {
+    return (int)
+        IntStream.range(0, strs[0].length())
+            .filter(
+                c ->
+                    IntStream.range(0, strs.length - 1)
+                        .anyMatch(i -> strs[i].charAt(c) > strs[i + 1].charAt(c)))
+            .count();
+  }
 }
