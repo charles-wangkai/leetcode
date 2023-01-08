adbcb1c1eddde35c9dea57516d7d9c1edc4ae410
Kai Wang <charles.wangkai@gmail.com>
2023-01-08 10:31:23 +0800

    Max Points on a Line

--- max-points-on-a-line/Solution.java
+++ max-points-on-a-line/Solution.java
@@ -1,93 +1,26 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.IntStream;
 
-// Definition for a point.
-class Point {
-	int x;
-	int y;
+class Solution {
+  public int maxPoints(int[][] points) {
+    int result = 1;
+    for (int i = 0; i < points.length; ++i) {
+      for (int j = i + 1; j < points.length; ++j) {
+        int i_ = i;
+        int j_ = j;
+        result =
+            Math.max(
+                result,
+                (int)
+                    IntStream.range(0, points.length)
+                        .filter(
+                            k ->
+                                (points[i_][1] - points[k][1]) * (points[j_][0] - points[k][0])
+                                    == (points[j_][1] - points[k][1])
+                                        * (points[i_][0] - points[k][0]))
+                        .count());
+      }
+    }
 
-	Point() {
-		x = 0;
-		y = 0;
-	}
-
-	Point(int a, int b) {
-		x = a;
-		y = b;
-	}
-}
-
-public class Solution {
-	public int maxPoints(Point[] points) {
-		int result = 0;
-		for (int i = 0; i < points.length; i++) {
-			Point origin = points[i];
-			int originNum = 1;
-			Map<Slope, Integer> slope2count = new HashMap<Slope, Integer>();
-			for (int j = 0; j < points.length; j++) {
-				if (j == i) {
-					continue;
-				}
-				if (points[j].x == origin.x && points[j].y == origin.y) {
-					originNum++;
-				} else {
-					Slope slope = new Slope(points[j].x - origin.x, points[j].y
-							- origin.y);
-					if (!slope2count.containsKey(slope)) {
-						slope2count.put(slope, 0);
-					}
-					slope2count.put(slope, slope2count.get(slope) + 1);
-				}
-			}
-
-			int maxCount = 0;
-			for (int count : slope2count.values()) {
-				maxCount = Math.max(maxCount, count);
-			}
-			result = Math.max(result, originNum + maxCount);
-		}
-		return result;
-	}
-}
-
-class Slope {
-	int dx;
-	int dy;
-
-	Slope(int dx, int dy) {
-		this.dx = dx;
-		this.dy = dy;
-		normalize();
-	}
-
-	private void normalize() {
-		int common = gcd(Math.abs(dx), Math.abs(dy));
-		dx /= common;
-		dy /= common;
-
-		if (dx < 0) {
-			dx = -dx;
-			dy = -dy;
-		}
-	}
-
-	private int gcd(int a, int b) {
-		while (b != 0) {
-			int c = a % b;
-			a = b;
-			b = c;
-		}
-		return a;
-	}
-
-	@Override
-	public int hashCode() {
-		return dx * dy;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		Slope other = (Slope) obj;
-		return dx == other.dx && dy == other.dy;
-	}
+    return result;
+  }
 }
