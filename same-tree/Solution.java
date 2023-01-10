8b5ac72790cf4b6f47a8e478dad453d13a0355bf
Kai Wang <charles.wangkai@gmail.com>
2023-01-10 09:15:50 +0800

    Same Tree

--- same-tree/Solution.java
+++ same-tree/Solution.java
@@ -1,32 +1,29 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if (p == q) {
-			return true;
-		}
-		if (p == null || q == null || p.val != q.val) {
-			return false;
-		}
-
-		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-	}
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    return (p == null && q == null)
+        || (p != null
+            && q != null
+            && p.val == q.val
+            && isSameTree(p.left, q.left)
+            && isSameTree(p.right, q.right));
+  }
 }
