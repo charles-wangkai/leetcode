b52ea8e711f9e665644ac5df659cc126510b0ddc
Kai Wang <charles.wangkai@gmail.com>
2023-01-16 09:02:35 +0800

    Insert Interval

--- insert-interval/Solution.java
+++ insert-interval/Solution.java
@@ -2,27 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-	public int[][] insert(int[][] intervals, int[] newInterval) {
-		List<int[]> inserted = new ArrayList<>();
-		boolean used = false;
-		for (int i = 0; i < intervals.length || !used; ++i) {
-			int[] selected;
-			if (i < intervals.length && (used || intervals[i][0] < newInterval[0])) {
-				selected = intervals[i];
-			} else {
-				selected = newInterval;
-				used = true;
-				--i;
-			}
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    List<int[]> result = new ArrayList<>();
+    int index = 0;
+    boolean used = false;
+    while (index != intervals.length || !used) {
+      int[] selected;
+      if (index != intervals.length && (used || intervals[index][0] <= newInterval[0])) {
+        selected = intervals[index];
+        ++index;
+      } else {
+        selected = newInterval;
+        used = true;
+      }
 
-			if (inserted.isEmpty() || inserted.get(inserted.size() - 1)[1] < selected[0]) {
-				inserted.add(selected);
-			} else {
-				int[] last = inserted.get(inserted.size() - 1);
-				last[1] = Math.max(last[1], selected[1]);
-			}
-		}
+      if (result.isEmpty() || result.get(result.size() - 1)[1] < selected[0]) {
+        result.add(selected);
+      } else {
+        int[] last = result.get(result.size() - 1);
+        last[1] = Math.max(last[1], selected[1]);
+      }
+    }
 
-		return inserted.toArray(new int[0][]);
-	}
+    return result.toArray(int[][]::new);
+  }
 }
