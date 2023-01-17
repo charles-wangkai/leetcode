ff0cc3c914e317050c005d5801549fb0eb7c82d0
Kai Wang <charles.wangkai@gmail.com>
2023-01-17 09:54:19 +0800

    Flip String to Monotone Increasing

--- flip-string-to-monotone-increasing/Solution.java
+++ flip-string-to-monotone-increasing/Solution.java
@@ -1,23 +1,17 @@
-import java.util.stream.IntStream;
-
 class Solution {
   public int minFlipsMonoIncr(String s) {
-    int[] leftOneNums = new int[s.length()];
-    for (int i = 0; i < leftOneNums.length; ++i) {
-      leftOneNums[i] = ((i == 0) ? 0 : leftOneNums[i - 1]) + ((s.charAt(i) == '1') ? 1 : 0);
+    int flipNum = (int) s.chars().filter(c -> c == '0').count();
+    int result = flipNum;
+    for (char c : s.toCharArray()) {
+      if (c == '0') {
+        --flipNum;
+      } else {
+        ++flipNum;
+      }
+
+      result = Math.min(result, flipNum);
     }
 
-    int[] rightZeroNums = new int[s.length()];
-    for (int i = rightZeroNums.length - 1; i >= 0; --i) {
-      rightZeroNums[i] =
-          ((i == rightZeroNums.length - 1) ? 0 : rightZeroNums[i + 1])
-              + ((s.charAt(i) == '0') ? 1 : 0);
-    }
-
-    return IntStream.concat(
-            IntStream.of(rightZeroNums[0], leftOneNums[leftOneNums.length - 1]),
-            IntStream.range(0, s.length() - 1).map(i -> leftOneNums[i] + rightZeroNums[i + 1]))
-        .min()
-        .getAsInt();
+    return result;
   }
 }
