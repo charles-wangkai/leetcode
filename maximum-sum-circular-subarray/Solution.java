4d33ce6018dc7b7ea7c2fb7b2002b4783fc1dbfa
Kai Wang <charles.wangkai@gmail.com>
2023-01-18 08:49:24 +0800

    Maximum Sum Circular Subarray

--- maximum-sum-circular-subarray/Solution.java
+++ maximum-sum-circular-subarray/Solution.java
@@ -1,28 +1,28 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int maxSubarraySumCircular(int[] A) {
-		int maxSum = computeMaxSum(A);
-		if (maxSum < 0) {
-			return maxSum;
-		}
+class Solution {
+  public int maxSubarraySumCircular(int[] nums) {
+    int maxSum = computeMaxSum(nums);
+    if (maxSum < 0) {
+      return maxSum;
+    }
 
-		return Math.max(maxSum, Arrays.stream(A).sum() - computeMinSum(A));
-	}
+    return Math.max(maxSum, Arrays.stream(nums).sum() - computeMinSum(nums));
+  }
 
-	int computeMaxSum(int[] a) {
-		int sum = 0;
-		int maxSum = Integer.MIN_VALUE;
-		for (int number : a) {
-			sum += number;
-			maxSum = Math.max(maxSum, sum);
-			sum = Math.max(0, sum);
-		}
+  int computeMaxSum(int[] a) {
+    int sum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    for (int ai : a) {
+      sum += ai;
+      maxSum = Math.max(maxSum, sum);
+      sum = Math.max(0, sum);
+    }
 
-		return maxSum;
-	}
+    return maxSum;
+  }
 
-	int computeMinSum(int[] a) {
-		return -computeMaxSum(Arrays.stream(a).map(x -> -x).toArray());
-	}
+  int computeMinSum(int[] a) {
+    return -computeMaxSum(Arrays.stream(a).map(x -> -x).toArray());
+  }
 }
