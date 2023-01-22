c6942fd91488a2430590041c8220b89b3b298e6c
Kai Wang <charles.wangkai@gmail.com>
2023-01-22 10:51:17 +0800

    Confusing Number II

--- confusing-number-ii/Solution.java
+++ confusing-number-ii/Solution.java
@@ -1,58 +1,57 @@
-import java.util.HashMap;
+import static java.util.Map.entry;
+
 import java.util.Map;
+import java.util.stream.IntStream;
 
-public class Solution {
-	static final Map<Integer, Integer> DIGIT_TO_ROTATED = new HashMap<>();
-	static {
-		DIGIT_TO_ROTATED.put(0, 0);
-		DIGIT_TO_ROTATED.put(1, 1);
-		DIGIT_TO_ROTATED.put(6, 9);
-		DIGIT_TO_ROTATED.put(8, 8);
-		DIGIT_TO_ROTATED.put(9, 6);
-	}
+class Solution {
+  static final Map<Integer, Integer> DIGIT_TO_ROTATED =
+      Map.ofEntries(entry(0, 0), entry(1, 1), entry(6, 9), entry(8, 8), entry(9, 6));
+  static final int[] DIGITS =
+      DIGIT_TO_ROTATED.keySet().stream().mapToInt(Integer::intValue).sorted().toArray();
+  static final int DIGIT_NUM_LIMIT = 9;
 
-	static int confusingNumberCount;
+  int[] indices = new int[DIGIT_NUM_LIMIT];
 
-	public int confusingNumberII(int N) {
-		confusingNumberCount = 0;
+  public int confusingNumberII(int n) {
+    int result = (n == 1_000_000_000) ? 1 : 0;
+    int limit = pow(DIGITS.length, DIGIT_NUM_LIMIT) - 1;
+    for (int code = 0; code <= limit; ++code) {
+      decode(code);
+      int value = 0;
+      for (int index : indices) {
+        value = value * 10 + DIGITS[index];
+      }
+      if (value > n) {
+        break;
+      }
 
-		if (N == 1_000_000_000) {
-			confusingNumberCount++;
-		}
+      if (value >= 1 && isConfusing(value)) {
+        ++result;
+      }
+    }
 
-		search(N, 0, 0);
+    return result;
+  }
 
-		return confusingNumberCount;
-	}
+  boolean isConfusing(int x) {
+    String s = String.valueOf(x);
+    for (int i = 0, j = s.length() - 1; i <= j; ++i, --j) {
+      if (DIGIT_TO_ROTATED.get(s.charAt(i) - '0') != s.charAt(j) - '0') {
+        return true;
+      }
+    }
 
-	void search(int N, int length, int current) {
-		if (length == 9) {
-			return;
-		}
+    return false;
+  }
 
-		for (int validDigit : DIGIT_TO_ROTATED.keySet()) {
-			if (validDigit == 0 && length == 0) {
-				continue;
-			}
+  int pow(int base, int exponent) {
+    return IntStream.range(0, exponent).reduce(1, (x, y) -> x * base);
+  }
 
-			int next = current * 10 + validDigit;
-			if (next <= N && isConfusingNumber(next)) {
-				confusingNumberCount++;
-			}
-
-			search(N, length + 1, next);
-		}
-	}
-
-	boolean isConfusingNumber(int x) {
-		int rotated = 0;
-		int p = x;
-		while (p != 0) {
-			rotated = rotated * 10 + DIGIT_TO_ROTATED.get(p % 10);
-
-			p /= 10;
-		}
-
-		return rotated != x;
-	}
+  void decode(int code) {
+    for (int i = indices.length - 1; i >= 0; --i) {
+      indices[i] = code % DIGITS.length;
+      code /= DIGITS.length;
+    }
+  }
 }
