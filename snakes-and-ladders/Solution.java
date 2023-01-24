f39ebc0cc2f2a296d01c9e8698fefcd91d0fad73
Kai Wang <charles.wangkai@gmail.com>
2023-01-24 10:13:28 +0800

    Snakes and Ladders

--- snakes-and-ladders/Solution.java
+++ snakes-and-ladders/Solution.java
@@ -1,56 +1,52 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
 
-public class Solution {
-	public int snakesAndLadders(int[][] board) {
-		int[] destinations = flatten(board);
+class Solution {
+  public int snakesAndLadders(int[][] board) {
+    int[] destinations = flatten(board);
 
-		int[] moveNums = new int[destinations.length];
-		Arrays.fill(moveNums, -1);
-		moveNums[0] = 0;
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(0);
-		while (!queue.isEmpty()) {
-			int current = queue.poll();
-			for (int i = current + 1; i <= current + 6 && i < destinations.length; i++) {
-				int next = (destinations[i] == -1) ? i : destinations[i];
+    int[] moveNums = new int[destinations.length];
+    Arrays.fill(moveNums, -1);
+    moveNums[0] = 0;
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(0);
+    while (!queue.isEmpty()) {
+      int current = queue.poll();
+      for (int i = current + 1; i <= current + 6 && i < destinations.length; ++i) {
+        int next = (destinations[i] == -1) ? i : destinations[i];
+        if (moveNums[next] == -1) {
+          moveNums[next] = moveNums[current] + 1;
+          if (next == destinations.length - 1) {
+            return moveNums[next];
+          }
 
-				if (moveNums[next] == -1) {
-					moveNums[next] = moveNums[current] + 1;
+          queue.offer(next);
+        }
+      }
+    }
 
-					if (next == destinations.length - 1) {
-						return moveNums[next];
-					}
+    return -1;
+  }
 
-					queue.offer(next);
-				}
-			}
-		}
-		return -1;
-	}
+  int[] flatten(int[][] board) {
+    int n = board.length;
 
-	int[] flatten(int[][] board) {
-		int N = board.length;
-		int r = N - 1;
-		int c = -1;
-		int offsetC = 1;
-		int[] destinations = new int[N * N];
-		for (int i = 0; i < destinations.length; i++) {
-			c += offsetC;
-			if (c == N || c == -1) {
-				r--;
-				offsetC *= -1;
+    int r = n - 1;
+    int c = -1;
+    int offsetC = 1;
+    int[] destinations = new int[n * n];
+    for (int i = 0; i < destinations.length; ++i) {
+      c += offsetC;
+      if (c == n || c == -1) {
+        c -= offsetC;
+        offsetC *= -1;
+        --r;
+      }
 
-				if (c == N) {
-					c = N - 1;
-				} else {
-					c = 0;
-				}
-			}
+      destinations[i] = (board[r][c] == -1) ? -1 : (board[r][c] - 1);
+    }
 
-			destinations[i] = (board[r][c] == -1) ? -1 : (board[r][c] - 1);
-		}
-		return destinations;
-	}
+    return destinations;
+  }
 }
