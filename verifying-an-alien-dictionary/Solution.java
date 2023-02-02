bd1a49f6878636793f83b50ef2b0190d392964ae
Kai Wang <charles.wangkai@gmail.com>
2023-02-02 08:53:54 +0800

    Verifying an Alien Dictionary

--- verifying-an-alien-dictionary/Solution.java
+++ verifying-an-alien-dictionary/Solution.java
@@ -5,23 +5,21 @@ import java.util.stream.IntStream;
 
 class Solution {
   public boolean isAlienSorted(String[] words, String order) {
-    Map<Character, Character> translation = buildTranslation(order);
+    Map<Character, Character> translation =
+        IntStream.range(0, order.length())
+            .boxed()
+            .collect(Collectors.toMap(order::charAt, i -> (char) ('a' + i)));
     String[] translated =
-        Arrays.stream(words).map(word -> translate(translation, word)).toArray(String[]::new);
+        Arrays.stream(words)
+            .map(
+                word ->
+                    word.chars()
+                        .mapToObj(c -> translation.get((char) c))
+                        .map(String::valueOf)
+                        .collect(Collectors.joining()))
+            .toArray(String[]::new);
 
     return IntStream.range(0, translated.length - 1)
         .allMatch(i -> translated[i].compareTo(translated[i + 1]) <= 0);
-  }
-
-  Map<Character, Character> buildTranslation(String order) {
-    return IntStream.range(0, order.length())
-        .boxed()
-        .collect(Collectors.toMap(order::charAt, i -> (char) ('a' + i)));
-  }
-
-  String translate(Map<Character, Character> translation, String word) {
-    return word.chars()
-        .mapToObj(ch -> String.valueOf(translation.get((char) ch)))
-        .collect(Collectors.joining());
   }
 }
