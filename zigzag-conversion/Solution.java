87840f7167116ae846488e6a850a5fe09fd32df7
Kai Wang <charles.wangkai@gmail.com>
2023-02-03 10:03:44 +0800

    Zigzag Conversion

--- zigzag-conversion/Solution.java
+++ zigzag-conversion/Solution.java
@@ -1,39 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+class Solution {
+  public String convert(String s, int numRows) {
+    if (numRows == 1) {
+      return s;
+    }
 
-public class Solution {
-	public String convert(String s, int nRows) {
-		if (nRows == 1) {
-			return s;
-		}
+    String[] rows = new String[numRows];
+    for (int i = 0; i < rows.length; ++i) {
+      rows[i] = "";
+    }
+    int rowIndex = 0;
+    int offset = 1;
+    for (char c : s.toCharArray()) {
+      rows[rowIndex] += c;
+      rowIndex += offset;
 
-		@SuppressWarnings("unchecked")
-		List<Character> rows[] = new ArrayList[nRows];
-		for (int i = 0; i < rows.length; i++) {
-			rows[i] = new ArrayList<Character>();
-		}
+      if (rowIndex == rows.length) {
+        rowIndex = rows.length - 2;
+        offset = -1;
+      } else if (rowIndex == -1) {
+        rowIndex = 1;
+        offset = 1;
+      }
+    }
 
-		int rowIndex = 0;
-		int offset = 1;
-		for (int i = 0; i < s.length(); i++) {
-			rows[rowIndex].add(s.charAt(i));
-			rowIndex += offset;
-
-			if (rowIndex == rows.length) {
-				rowIndex = rows.length - 2;
-				offset = -1;
-			} else if (rowIndex == -1) {
-				rowIndex = 1;
-				offset = 1;
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (List<Character> row : rows) {
-			for (char ch : row) {
-				sb.append(ch);
-			}
-		}
-		return sb.toString();
-	}
+    return String.join("", rows);
+  }
 }
