8f44ee861127bb7d6cb2612905cdd5de2a8c88db
Kai Wang <charles.wangkai@gmail.com>
2023-02-06 08:34:22 +0800

    Shuffle the Array

--- shuffle-the-array/Solution.java
+++ shuffle-the-array/Solution.java
@@ -1,7 +1,9 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int[] shuffle(int[] nums, int n) {
-        return IntStream.range(0, n * 2).map(i -> nums[i / 2 + ((i % 2 != 0) ? n : 0)]).toArray();
-    }
+  public int[] shuffle(int[] nums, int n) {
+    return IntStream.range(0, nums.length)
+        .map(i -> nums[i / 2 + ((i % 2 == 0) ? 0 : (nums.length / 2))])
+        .toArray();
+  }
 }
