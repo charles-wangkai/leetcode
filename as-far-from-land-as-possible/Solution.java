4fb1998b368cc1b81ce895eba82fab5b76ffb7d4
Kai Wang <charles.wangkai@gmail.com>
2023-02-10 09:42:05 +0800

    As Far from Land as Possible

--- as-far-from-land-as-possible/Solution.java
+++ as-far-from-land-as-possible/Solution.java
@@ -1,51 +1,46 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int maxDistance(int[][] grid) {
-		int N = grid.length;
+  public int maxDistance(int[][] grid) {
+    int n = grid.length;
 
-		int[][] distances = new int[N][N];
-		Queue<Point> queue = new LinkedList<>();
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				if (grid[r][c] == 1) {
-					queue.offer(new Point(r, c));
-				}
-			}
-		}
+    int[][] distances = new int[n][n];
+    Queue<Point> queue = new ArrayDeque<>();
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (grid[r][c] == 1) {
+          queue.offer(new Point(r, c));
+        }
+      }
+    }
 
-		int result = -1;
-		while (!queue.isEmpty()) {
-			Point head = queue.poll();
+    int result = -1;
+    while (!queue.isEmpty()) {
+      Point head = queue.poll();
 
-			for (int i = 0; i < R_OFFSETS.length; i++) {
-				int adjR = head.r + R_OFFSETS[i];
-				int adjC = head.c + C_OFFSETS[i];
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r() + R_OFFSETS[i];
+        int adjC = head.c() + C_OFFSETS[i];
+        if (adjR >= 0
+            && adjR < n
+            && adjC >= 0
+            && adjC < n
+            && grid[adjR][adjC] == 0
+            && distances[adjR][adjC] == 0) {
+          distances[adjR][adjC] = distances[head.r()][head.c()] + 1;
+          result = distances[adjR][adjC];
 
-				if (adjR >= 0 && adjR < N && adjC >= 0 && adjC < N && grid[adjR][adjC] == 0
-						&& distances[adjR][adjC] == 0) {
-					distances[adjR][adjC] = distances[head.r][head.c] + 1;
-					result = distances[adjR][adjC];
+          queue.offer(new Point(adjR, adjC));
+        }
+      }
+    }
 
-					queue.offer(new Point(adjR, adjC));
-				}
-			}
-		}
-
-		return result;
-	}
+    return result;
+  }
 }
 
-class Point {
-	int r;
-	int c;
-
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
-}
+record Point(int r, int c) {}
