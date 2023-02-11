cc484701e3db4c3ccc7b939073a2bfb05ee6958b
Kai Wang <charles.wangkai@gmail.com>
2023-02-11 10:10:08 +0800

    Shortest Path with Alternating Colors

--- shortest-path-with-alternating-colors/Solution.java
+++ shortest-path-with-alternating-colors/Solution.java
@@ -1,77 +1,64 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-		List<Integer>[] viaRedAdjLists = buildAdjLists(n, red_edges);
-		List<Integer>[] viaBlueAdjLists = buildAdjLists(n, blue_edges);
+class Solution {
+  public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+    List<Integer>[] redAdjLists = buildAdjLists(n, redEdges);
+    List<Integer>[] blueAdjLists = buildAdjLists(n, blueEdges);
 
-		int[] endRedMinDistances = initMinDistances(n);
-		int[] endBlueMinDistances = initMinDistances(n);
+    int[] redMinDistances = new int[n];
+    Arrays.fill(redMinDistances, Integer.MAX_VALUE);
+    redMinDistances[0] = 0;
+    int[] blueMinDistances = new int[n];
+    Arrays.fill(blueMinDistances, Integer.MAX_VALUE);
+    blueMinDistances[0] = 0;
 
-		PriorityQueue<Element> pq = new PriorityQueue<>((e1, e2) -> Integer.compare(e1.distance, e2.distance));
-		pq.offer(new Element(0, 0, true));
-		pq.offer(new Element(0, 0, false));
+    Queue<Element> queue = new ArrayDeque<>();
+    queue.offer(new Element(0, true));
+    queue.offer(new Element(0, false));
 
-		while (!pq.isEmpty()) {
-			Element head = pq.poll();
+    while (!queue.isEmpty()) {
+      Element head = queue.poll();
 
-			if (head.endRedOrBlue) {
-				if (head.distance < endRedMinDistances[head.node]) {
-					endRedMinDistances[head.node] = head.distance;
+      if (head.redOrBlue()) {
+        for (int blueAdj : blueAdjLists[head.node()]) {
+          if (blueMinDistances[blueAdj] == Integer.MAX_VALUE) {
+            blueMinDistances[blueAdj] = redMinDistances[head.node()] + 1;
+            queue.offer(new Element(blueAdj, false));
+          }
+        }
+      } else {
+        for (int redAdj : redAdjLists[head.node()]) {
+          if (redMinDistances[redAdj] == Integer.MAX_VALUE) {
+            redMinDistances[redAdj] = blueMinDistances[head.node()] + 1;
+            queue.offer(new Element(redAdj, true));
+          }
+        }
+      }
+    }
 
-					for (int viaBlueAdj : viaBlueAdjLists[head.node]) {
-						pq.offer(new Element(viaBlueAdj, head.distance + 1, false));
-					}
-				}
-			} else {
-				if (head.distance < endBlueMinDistances[head.node]) {
-					endBlueMinDistances[head.node] = head.distance;
+    return IntStream.range(0, n)
+        .map(i -> Math.min(redMinDistances[i], blueMinDistances[i]))
+        .map(x -> (x == Integer.MAX_VALUE) ? -1 : x)
+        .toArray();
+  }
 
-					for (int viaRedAdj : viaRedAdjLists[head.node]) {
-						pq.offer(new Element(viaRedAdj, head.distance + 1, true));
-					}
-				}
-			}
-		}
+  List<Integer>[] buildAdjLists(int n, int[][] edges) {
+    @SuppressWarnings("unchecked")
+    List<Integer>[] adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<>();
+    }
+    for (int[] edge : edges) {
+      adjLists[edge[0]].add(edge[1]);
+    }
 
-		return IntStream.range(0, n).map(i -> Math.min(endRedMinDistances[i], endBlueMinDistances[i]))
-				.map(x -> (x == Integer.MAX_VALUE) ? -1 : x).toArray();
-	}
-
-	int[] initMinDistances(int n) {
-		int[] minDistances = new int[n];
-		Arrays.fill(minDistances, Integer.MAX_VALUE);
-
-		return minDistances;
-	}
-
-	List<Integer>[] buildAdjLists(int n, int[][] edges) {
-		@SuppressWarnings("unchecked")
-		List<Integer>[] adjLists = new List[n];
-		for (int i = 0; i < adjLists.length; i++) {
-			adjLists[i] = new ArrayList<>();
-		}
-
-		for (int[] edge : edges) {
-			adjLists[edge[0]].add(edge[1]);
-		}
-
-		return adjLists;
-	}
+    return adjLists;
+  }
 }
 
-class Element {
-	int node;
-	int distance;
-	boolean endRedOrBlue;
-
-	Element(int node, int distance, boolean endRedOrBlue) {
-		this.node = node;
-		this.distance = distance;
-		this.endRedOrBlue = endRedOrBlue;
-	}
-}
+record Element(int node, boolean redOrBlue) {}
