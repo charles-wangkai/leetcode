27af8799c0d68658a0c5679cae256062574cc5a6
Kai Wang <charles.wangkai@gmail.com>
2023-02-13 09:19:54 +0800

    Count Odd Numbers in an Interval Range

--- count-odd-numbers-in-an-interval-range/Solution.java
+++ count-odd-numbers-in-an-interval-range/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countOdds(int low, int high) {
-        return ((high - (1 - high % 2)) - (low + (1 - low % 2))) / 2 + 1;
-    }
+  public int countOdds(int low, int high) {
+    return ((high - (1 - high % 2)) - (low + (1 - low % 2))) / 2 + 1;
+  }
 }
