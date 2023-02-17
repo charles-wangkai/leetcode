4e885dc800c778a41569fa08c01854d85174113b
Kai Wang <charles.wangkai@gmail.com>
2023-02-17 09:38:01 +0800

    Minimum Distance Between BST Nodes

--- minimum-distance-between-bst-nodes/Solution.java
+++ minimum-distance-between-bst-nodes/Solution.java
@@ -4,27 +4,39 @@ import java.util.stream.IntStream;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 public class Solution {
-	public int minDiffInBST(TreeNode root) {
-		List<Integer> values = new ArrayList<Integer>();
-		search(root, values);
-		return IntStream.range(0, values.size() - 1).map(i -> values.get(i + 1) - values.get(i)).min().getAsInt();
-	}
+  public int minDiffInBST(TreeNode root) {
+    List<Integer> values = new ArrayList<>();
+    search(root, values);
 
-	void search(TreeNode node, List<Integer> values) {
-		if (node != null) {
-			search(node.left, values);
-			values.add(node.val);
-			search(node.right, values);
-		}
-	}
+    return IntStream.range(0, values.size() - 1)
+        .map(i -> values.get(i + 1) - values.get(i))
+        .min()
+        .getAsInt();
+  }
+
+  void search(TreeNode node, List<Integer> values) {
+    if (node != null) {
+      search(node.left, values);
+      values.add(node.val);
+      search(node.right, values);
+    }
+  }
 }
