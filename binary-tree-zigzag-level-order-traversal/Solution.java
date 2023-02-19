b8db927b68493557d892e05df1fc42aefc5c4fdb
Kai Wang <charles.wangkai@gmail.com>
2023-02-19 10:23:30 +0800

    Binary Tree Zigzag Level Order Traversal

--- binary-tree-zigzag-level-order-traversal/Solution.java
+++ binary-tree-zigzag-level-order-traversal/Solution.java
@@ -4,46 +4,43 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode() {
-	}
+  TreeNode() {}
 
-	TreeNode(int val) {
-		this.val = val;
-	}
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-	TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-		List<List<Integer>> levels = new ArrayList<>();
-		search(levels, root, 0);
-		for (int i = 1; i < levels.size(); i += 2) {
-			Collections.reverse(levels.get(i));
-		}
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> levels = new ArrayList<>();
+    search(levels, root, 0);
+    for (int i = 1; i < levels.size(); i += 2) {
+      Collections.reverse(levels.get(i));
+    }
 
-		return levels;
-	}
+    return levels;
+  }
 
-	void search(List<List<Integer>> levels, TreeNode node, int depth) {
-		if (node == null) {
-			return;
-		}
+  void search(List<List<Integer>> levels, TreeNode node, int depth) {
+    if (node != null) {
+      if (depth == levels.size()) {
+        levels.add(new ArrayList<>());
+      }
+      levels.get(depth).add(node.val);
 
-		if (depth == levels.size()) {
-			levels.add(new ArrayList<>());
-		}
-		levels.get(depth).add(node.val);
-
-		search(levels, node.left, depth + 1);
-		search(levels, node.right, depth + 1);
-	}
+      search(levels, node.left, depth + 1);
+      search(levels, node.right, depth + 1);
+    }
+  }
 }
