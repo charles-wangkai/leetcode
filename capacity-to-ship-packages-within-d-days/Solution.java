e548325c2268b02e822a1cb83c05b19ab8794179
Kai Wang <charles.wangkai@gmail.com>
2023-02-22 08:53:02 +0800

    Capacity To Ship Packages Within D Days

--- capacity-to-ship-packages-within-d-days/Solution.java
+++ capacity-to-ship-packages-within-d-days/Solution.java
@@ -1,36 +1,35 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int shipWithinDays(int[] weights, int D) {
-		int lower = Arrays.stream(weights).max().getAsInt();
-		int upper = Arrays.stream(weights).sum();
+class Solution {
+  public int shipWithinDays(int[] weights, int days) {
+    int result = -1;
+    int lower = Arrays.stream(weights).max().getAsInt();
+    int upper = Arrays.stream(weights).sum();
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(weights, days, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
 
-		int result = -1;
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
+    return result;
+  }
 
-			if (computeDay(weights, middle) <= D) {
-				result = middle;
+  boolean check(int[] weights, int days, int capacity) {
+    int dayNum = 0;
+    int rest = 0;
+    for (int weight : weights) {
+      if (weight > rest) {
+        ++dayNum;
+        rest = capacity;
+      }
 
-				upper = middle - 1;
-			} else {
-				lower = middle + 1;
-			}
-		}
-		return result;
-	}
+      rest -= weight;
+    }
 
-	static int computeDay(int[] weights, int capacity) {
-		int day = 0;
-		int remain = 0;
-		for (int weight : weights) {
-			if (weight > remain) {
-				day++;
-				remain = capacity;
-			}
-
-			remain -= weight;
-		}
-		return day;
-	}
+    return dayNum <= days;
+  }
 }
