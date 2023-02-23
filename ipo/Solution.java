c455bf1408233622d9336ad9016575ecb1fdb573
Kai Wang <charles.wangkai@gmail.com>
2023-02-23 09:49:46 +0800

    IPO

--- ipo/Solution.java
+++ ipo/Solution.java
@@ -1,37 +1,30 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Solution {
-	public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
-		PriorityQueue<Project> remainProjects = new PriorityQueue<Project>(
-				(p1, p2) -> (p1.capital != p2.capital) ? (p1.capital - p2.capital) : (p2.profit - p1.profit));
-		for (int i = 0; i < Profits.length; i++) {
-			remainProjects.add(new Project(Profits[i], Capital[i]));
-		}
+class Solution {
+  public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+    PriorityQueue<Integer> restProjects =
+        new PriorityQueue<>(Comparator.comparing(i -> capital[i]));
+    for (int i = 0; i < profits.length; ++i) {
+      restProjects.add(i);
+    }
 
-		PriorityQueue<Project> readyProjects = new PriorityQueue<Project>((p1, p2) -> p2.profit - p1.profit);
+    PriorityQueue<Integer> readyProjects =
+        new PriorityQueue<>(Comparator.comparing((Integer i) -> profits[i]).reversed());
 
-		int maxCapital = W;
-		for (int i = 0; i < Math.min(k, Profits.length); i++) {
-			while (!remainProjects.isEmpty() && remainProjects.peek().capital <= maxCapital) {
-				readyProjects.add(remainProjects.poll());
-			}
+    int result = w;
+    for (int i = 0; i < Math.min(k, profits.length); ++i) {
+      while (!restProjects.isEmpty() && capital[restProjects.peek()] <= result) {
+        readyProjects.add(restProjects.poll());
+      }
 
-			if (readyProjects.isEmpty()) {
-				break;
-			}
+      if (readyProjects.isEmpty()) {
+        break;
+      }
 
-			maxCapital += readyProjects.poll().profit;
-		}
-		return maxCapital;
-	}
-}
+      result += profits[readyProjects.poll()];
+    }
 
-class Project {
-	int profit;
-	int capital;
-
-	Project(int profit, int capital) {
-		this.profit = profit;
-		this.capital = capital;
-	}
+    return result;
+  }
 }
