42c6b1fe28507262980f50f918bc0d6bc7a27cdb
Kai Wang <charles.wangkai@gmail.com>
2023-02-27 00:00:58 +0800

    Edit Distance

--- edit-distance/Solution.java
+++ edit-distance/Solution.java
@@ -1,23 +1,21 @@
-public class Solution {
-	public int minDistance(String word1, String word2) {
-		int[][] distances = new int[word1.length() + 1][word2.length() + 1];
-		for (int i = 0; i <= word1.length(); ++i) {
-			for (int j = 0; j <= word2.length(); ++j) {
-				if (i == 0) {
-					distances[i][j] = j;
-				} else if (j == 0) {
-					distances[i][j] = i;
-				} else {
-					if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-						distances[i][j] = distances[i - 1][j - 1];
-					} else {
-						distances[i][j] = Math.min(Math.min(distances[i - 1][j], distances[i][j - 1]),
-								distances[i - 1][j - 1]) + 1;
-					}
-				}
-			}
-		}
+class Solution {
+  public int minDistance(String word1, String word2) {
+    int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+    for (int i = 0; i <= word1.length(); ++i) {
+      for (int j = 0; j <= word2.length(); ++j) {
+        if (i == 0) {
+          dp[i][j] = j;
+        } else if (j == 0) {
+          dp[i][j] = i;
+        } else {
+          dp[i][j] =
+              (word1.charAt(i - 1) == word2.charAt(j - 1))
+                  ? dp[i - 1][j - 1]
+                  : (Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1);
+        }
+      }
+    }
 
-		return distances[word1.length()][word2.length()];
-	}
+    return dp[word1.length()][word2.length()];
+  }
 }
