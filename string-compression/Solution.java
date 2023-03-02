363a78a429ebaedb737ccab2ccfc75750a875b05
Kai Wang <charles.wangkai@gmail.com>
2023-03-02 08:57:56 +0800

    String Compression

--- string-compression/Solution.java
+++ string-compression/Solution.java
@@ -1,42 +1,42 @@
-public class Solution {
-	public int compress(char[] chars) {
-		char prev = 0;
-		int count = 0;
+class Solution {
+  public int compress(char[] chars) {
+    int length = 0;
+    char current = 0;
+    int count = 0;
+    for (int i = 0; i <= chars.length; ++i) {
+      if (i < chars.length && chars[i] == current) {
+        count++;
+      } else {
+        if (current != 0) {
+          chars[length] = current;
+          ++length;
+        }
+        if (count != 1) {
+          int beginIndex = length;
+          while (count != 0) {
+            chars[length] = (char) (count % 10 + '0');
+            ++length;
 
-		int length = 0;
-		for (int i = 0; i <= chars.length; i++) {
-			if (i < chars.length && chars[i] == prev) {
-				count++;
-			} else {
-				if (prev != 0) {
-					chars[length] = prev;
-					length++;
-				}
-				if (count != 1) {
-					int beginIndex = length;
-					while (count != 0) {
-						chars[length] = (char) (count % 10 + '0');
-						length++;
+            count /= 10;
+          }
+          reverse(chars, beginIndex, length - 1);
+        }
 
-						count /= 10;
-					}
-					reverse(chars, beginIndex, length - 1);
-				}
+        if (i != chars.length) {
+          current = chars[i];
+          count = 1;
+        }
+      }
+    }
 
-				if (i < chars.length) {
-					prev = chars[i];
-					count = 1;
-				}
-			}
-		}
-		return length;
-	}
+    return length;
+  }
 
-	void reverse(char[] chars, int beginIndex, int endIndex) {
-		for (int i = beginIndex, j = endIndex; i < j; i++, j--) {
-			char temp = chars[i];
-			chars[i] = chars[j];
-			chars[j] = temp;
-		}
-	}
+  void reverse(char[] chars, int beginIndex, int endIndex) {
+    for (int i = beginIndex, j = endIndex; i < j; ++i, --j) {
+      char temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+    }
+  }
 }
