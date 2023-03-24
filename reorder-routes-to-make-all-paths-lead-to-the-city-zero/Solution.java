706f5d0e9afb4ab9bf40758ad07e17c08c6e2937
Kai Wang <charles.wangkai@gmail.com>
2023-03-24 09:29:31 +0800

    Reorder Routes to Make All Paths Lead to the City Zero

--- reorder-routes-to-make-all-paths-lead-to-the-city-zero/Solution.java
+++ reorder-routes-to-make-all-paths-lead-to-the-city-zero/Solution.java
@@ -2,38 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int minReorder(int n, int[][] connections) {
-        @SuppressWarnings("unchecked")
-        List<Integer>[] edgeLists = new List[n];
-        for (int i = 0; i < n; ++i) {
-            edgeLists[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < connections.length; ++i) {
-            edgeLists[connections[i][0]].add(i);
-            edgeLists[connections[i][1]].add(i);
-        }
-
-        return search(connections, edgeLists, new boolean[n], 0);
+  public int minReorder(int n, int[][] connections) {
+    @SuppressWarnings("unchecked")
+    List<Integer>[] edgeLists = new List[n];
+    for (int i = 0; i < n; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < connections.length; ++i) {
+      edgeLists[connections[i][0]].add(i);
+      edgeLists[connections[i][1]].add(i);
     }
 
-    int search(int[][] connections, List<Integer>[] edgeLists, boolean[] visited, int node) {
-        visited[node] = true;
+    return search(connections, edgeLists, -1, 0);
+  }
 
-        int result = 0;
-        for (int edge : edgeLists[node]) {
-            int[] connection = connections[edge];
-
-            int other = (node == connection[0]) ? connection[1] : connection[0];
-            if (!visited[other]) {
-                if (node == connection[0]) {
-                    ++result;
-                }
-
-                result += search(connections, edgeLists, visited, other);
-            }
+  int search(int[][] connections, List<Integer>[] edgeLists, int parent, int node) {
+    int result = 0;
+    for (int edge : edgeLists[node]) {
+      int[] connection = connections[edge];
+      int other = (node == connection[0]) ? connection[1] : connection[0];
+      if (other != parent) {
+        if (node == connection[0]) {
+          ++result;
         }
 
-        return result;
+        result += search(connections, edgeLists, node, other);
+      }
     }
+
+    return result;
+  }
 }
