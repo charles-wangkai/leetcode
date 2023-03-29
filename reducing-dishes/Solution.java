28e86c435da7c0bd0e4982c092157fde29d5c1fe
Kai Wang <charles.wangkai@gmail.com>
2023-03-29 08:51:44 +0800

    Reducing Dishes

--- reducing-dishes/Solution.java
+++ reducing-dishes/Solution.java
@@ -1,19 +1,19 @@
 import java.util.Arrays;
 
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
-        Arrays.sort(satisfaction);
+  public int maxSatisfaction(int[] satisfaction) {
+    Arrays.sort(satisfaction);
 
-        int result = 0;
-        int suffixSum = 0;
-        int total = 0;
-        for (int i = satisfaction.length - 1; i >= 0; --i) {
-            total += suffixSum + satisfaction[i];
-            result = Math.max(result, total);
+    int result = 0;
+    int suffixSum = 0;
+    int total = 0;
+    for (int i = satisfaction.length - 1; i >= 0; --i) {
+      total += suffixSum + satisfaction[i];
+      result = Math.max(result, total);
 
-            suffixSum += satisfaction[i];
-        }
-
-        return result;
+      suffixSum += satisfaction[i];
     }
+
+    return result;
+  }
 }
