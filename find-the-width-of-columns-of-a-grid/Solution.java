a815eb63a69f069255e477ca78849bf5b365a9a1
Kai Wang <charles.wangkai@gmail.com>
2023-04-16 22:37:29 +0800

    Find the Width of Columns of a Grid

---
+++ find-the-width-of-columns-of-a-grid/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+  public int[] findColumnWidth(int[][] grid) {
+    return IntStream.range(0, grid[0].length)
+        .map(
+            c ->
+                Arrays.stream(grid)
+                    .mapToInt(line -> String.valueOf(line[c]).toString().length())
+                    .max()
+                    .getAsInt())
+        .toArray();
+  }
+}
