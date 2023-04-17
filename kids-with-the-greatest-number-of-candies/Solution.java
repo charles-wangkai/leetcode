ade826ed1edeca6a458bf4939831f638dd321756
Kai Wang <charles.wangkai@gmail.com>
2023-04-17 08:16:41 +0800

    Kids With the Greatest Number of Candies

--- kids-with-the-greatest-number-of-candies/Solution.java
+++ kids-with-the-greatest-number-of-candies/Solution.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int maxCandy = Arrays.stream(candies).max().getAsInt();
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    int maxCandy = Arrays.stream(candies).max().getAsInt();
 
-        return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= maxCandy).collect(Collectors.toList());
-    }
+    return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= maxCandy).toList();
+  }
 }
