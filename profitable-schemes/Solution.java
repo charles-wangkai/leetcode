9f51e9c1ef819e59d0c4333a89975f1440fa7778
Kai Wang <charles.wangkai@gmail.com>
2023-04-21 08:47:32 +0800

    Profitable Schemes

--- profitable-schemes/Solution.java
+++ profitable-schemes/Solution.java
@@ -1,62 +1,38 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-public class Solution {
-	static final int MODULUS = 1000000007;
+class Solution {
+  static final int MODULUS = 1_000_000_007;
 
-	public int profitableSchemes(int G, int P, int[] group, int[] profit) {
-		Map<State, Integer> stateToSchemeNum = new HashMap<>();
-		stateToSchemeNum.put(new State(0, 0), 1);
-		for (int i = 0; i < group.length; i++) {
-			Map<State, Integer> nextStateToSchemeNum = new HashMap<>();
+  public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+    Map<State, Integer> stateToSchemeNum = new HashMap<>();
+    stateToSchemeNum.put(new State(0, 0), 1);
+    for (int i = 0; i < group.length; ++i) {
+      Map<State, Integer> nextStateToSchemeNum = new HashMap<>(stateToSchemeNum);
 
-			for (State state : stateToSchemeNum.keySet()) {
-				nextStateToSchemeNum.put(state, stateToSchemeNum.get(state));
-			}
+      for (State state : stateToSchemeNum.keySet()) {
+        State nextState =
+            new State(state.group() + group[i], Math.min(minProfit, state.profit() + profit[i]));
+        if (nextState.group() <= n) {
+          nextStateToSchemeNum.put(
+              nextState,
+              addMod(nextStateToSchemeNum.getOrDefault(nextState, 0), stateToSchemeNum.get(state)));
+        }
+      }
 
-			for (State state : stateToSchemeNum.keySet()) {
-				State nextState = new State(state.group + group[i], Math.min(P, state.profit + profit[i]));
-				if (nextState.group <= G) {
-					nextStateToSchemeNum.put(nextState,
-							addMod(nextStateToSchemeNum.getOrDefault(nextState, 0), stateToSchemeNum.get(state)));
-				}
-			}
+      stateToSchemeNum = nextStateToSchemeNum;
+    }
 
-			stateToSchemeNum = nextStateToSchemeNum;
-		}
+    return stateToSchemeNum.keySet().stream()
+        .filter(state -> state.profit() >= minProfit)
+        .map(stateToSchemeNum::get)
+        .reduce(this::addMod)
+        .orElse(0);
+  }
 
-		int result = 0;
-		for (State state : stateToSchemeNum.keySet()) {
-			if (state.profit >= P) {
-				result = addMod(result, stateToSchemeNum.get(state));
-			}
-		}
-		return result;
-	}
-
-	static int addMod(int x, int y) {
-		return (x + y) % MODULUS;
-	}
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 }
 
-class State {
-	int group;
-	int profit;
-
-	State(int group, int profit) {
-		this.group = group;
-		this.profit = profit;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(group, profit);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		State other = (State) obj;
-		return group == other.group && profit == other.profit;
-	}
-}
+record State(int group, int profit) {}
