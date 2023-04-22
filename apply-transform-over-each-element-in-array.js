83d48dc9016b0f9de605f7b1702b846bb2e69614
Kai Wang <charles.wangkai@gmail.com>
2023-04-22 10:57:47 +0800

    Apply Transform Over Each Element in Array

---
+++ apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; ++i) {
+    result.push(fn(arr[i], i));
+  }
+
+  return result;
+};
