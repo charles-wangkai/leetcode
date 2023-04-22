b582b90b4191537c6292cf917adf5bfe87f160d5
Kai Wang <charles.wangkai@gmail.com>
2023-04-22 10:54:59 +0800

    Filter Elements from Array

---
+++ filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; ++i) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+};
