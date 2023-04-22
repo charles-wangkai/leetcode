91d747b2c7f0ad4220a0eaa63194250dc5735b22
Kai Wang <charles.wangkai@gmail.com>
2023-04-22 10:49:38 +0800

    Minimum Insertion Steps to Make a String Palindrome

--- minimum-insertion-steps-to-make-a-string-palindrome/Solution.java
+++ minimum-insertion-steps-to-make-a-string-palindrome/Solution.java
@@ -1,21 +1,21 @@
-public class Solution {
-	public int minInsertions(String s) {
-		int length = s.length();
+class Solution {
+  public int minInsertions(String s) {
+    int length = s.length();
 
-		int[][] stepNums = new int[length][length];
-		for (int size = 2; size <= length; ++size) {
-			for (int beginIndex = 0; beginIndex + size <= length; ++beginIndex) {
-				int endIndex = beginIndex + size - 1;
+    int[][] stepNums = new int[length][length];
+    for (int size = 2; size <= length; ++size) {
+      for (int beginIndex = 0; beginIndex + size <= length; ++beginIndex) {
+        int endIndex = beginIndex + size - 1;
 
-				if (s.charAt(beginIndex) == s.charAt(endIndex)) {
-					stepNums[beginIndex][endIndex] = (size == 2) ? 0 : stepNums[beginIndex + 1][endIndex - 1];
-				} else {
-					stepNums[beginIndex][endIndex] = Math.min(stepNums[beginIndex + 1][endIndex],
-							stepNums[beginIndex][endIndex - 1]) + 1;
-				}
-			}
-		}
+        if (s.charAt(beginIndex) == s.charAt(endIndex)) {
+          stepNums[beginIndex][endIndex] = stepNums[beginIndex + 1][endIndex - 1];
+        } else {
+          stepNums[beginIndex][endIndex] =
+              Math.min(stepNums[beginIndex + 1][endIndex], stepNums[beginIndex][endIndex - 1]) + 1;
+        }
+      }
+    }
 
-		return stepNums[0][length - 1];
-	}
+    return stepNums[0][length - 1];
+  }
 }
