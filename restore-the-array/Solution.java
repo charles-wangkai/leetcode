c66d395aeb16943a5980031c07fe66196af6faac
Kai Wang <charles.wangkai@gmail.com>
2023-04-23 09:03:52 +0800

    Restore The Array

--- restore-the-array/Solution.java
+++ restore-the-array/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
-    static final int MODULUS = 1_000_000_007;
+  static final int MODULUS = 1_000_000_007;
 
-    public int numberOfArrays(String s, int k) {
-        int kLength = String.valueOf(k).length();
+  public int numberOfArrays(String s, int k) {
+    int kLength = String.valueOf(k).length();
 
-        int[] wayNums = new int[s.length() + 1];
-        wayNums[0] = 1;
-        for (int i = 1; i < wayNums.length; ++i) {
-            for (int j = 1; j <= Math.min(i, kLength); ++j) {
-                String part = s.substring(i - j, i);
-                if (part.charAt(0) != '0' && Long.parseLong(part) <= k) {
-                    wayNums[i] = addMod(wayNums[i], wayNums[i - j]);
-                }
-            }
+    int[] dp = new int[s.length() + 1];
+    dp[0] = 1;
+    for (int i = 1; i < dp.length; ++i) {
+      for (int j = 1; j <= Math.min(i, kLength); ++j) {
+        String part = s.substring(i - j, i);
+        if (part.charAt(0) != '0' && Long.parseLong(part) <= k) {
+          dp[i] = addMod(dp[i], dp[i - j]);
         }
-
-        return wayNums[wayNums.length - 1];
+      }
     }
 
-    int addMod(int x, int y) {
-        return (x + y) % MODULUS;
-    }
+    return dp[dp.length - 1];
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 }
