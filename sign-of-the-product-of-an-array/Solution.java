a753902c53e9e292c0688e5eb3d1c4917fd8b4bb
Kai Wang <charles.wangkai@gmail.com>
2023-05-02 09:03:59 +0800

    Sign of the Product of an Array

--- sign-of-the-product-of-an-array/Solution.java
+++ sign-of-the-product-of-an-array/Solution.java
@@ -2,10 +2,6 @@ import java.util.Arrays;
 
 class Solution {
   public int arraySign(int[] nums) {
-    if (Arrays.stream(nums).anyMatch(num -> num == 0)) {
-      return 0;
-    }
-
-    return (Arrays.stream(nums).filter(num -> num < 0).count() % 2 == 0) ? 1 : -1;
+    return Arrays.stream(nums).map(Integer::signum).reduce((acc, x) -> acc * x).getAsInt();
   }
 }
