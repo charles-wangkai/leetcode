404a05a785f88c699289d017a3654bfd9b35a93a
Kai Wang <charles.wangkai@gmail.com>
2023-05-05 08:44:50 +0800

    Maximum Number of Vowels in a Substring of Given Length

--- maximum-number-of-vowels-in-a-substring-of-given-length/Solution.java
+++ maximum-number-of-vowels-in-a-substring-of-given-length/Solution.java
@@ -1,20 +1,20 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int maxVowels(String s, int k) {
-        int vowelCount = (int) IntStream.range(0, k - 1).filter(i -> isVowel(s.charAt(i))).count();
-        int result = vowelCount;
-        for (int i = k - 1; i < s.length(); ++i) {
-            vowelCount += isVowel(s.charAt(i)) ? 1 : 0;
-            result = Math.max(result, vowelCount);
+  public int maxVowels(String s, int k) {
+    int result = -1;
+    int vowelCount = (int) IntStream.range(0, k - 1).filter(i -> isVowel(s.charAt(i))).count();
+    for (int i = k - 1; i < s.length(); ++i) {
+      vowelCount += isVowel(s.charAt(i)) ? 1 : 0;
+      result = Math.max(result, vowelCount);
 
-            vowelCount -= isVowel(s.charAt(i - k + 1)) ? 1 : 0;
-        }
-
-        return result;
+      vowelCount -= isVowel(s.charAt(i - k + 1)) ? 1 : 0;
     }
 
-    boolean isVowel(char ch) {
-        return "aeiou".indexOf(ch) >= 0;
-    }
+    return result;
+  }
+
+  boolean isVowel(char c) {
+    return "aeiou".indexOf(c) != -1;
+  }
 }
