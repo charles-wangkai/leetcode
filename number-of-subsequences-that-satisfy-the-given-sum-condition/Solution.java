fe00b991dfc295961ccadec96d07d4be0d9a8aaf
Kai Wang <charles.wangkai@gmail.com>
2023-05-06 09:16:27 +0800

    Number of Subsequences That Satisfy the Given Sum Condition

--- number-of-subsequences-that-satisfy-the-given-sum-condition/Solution.java
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/Solution.java
@@ -1,62 +1,38 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
-    static final int MODULUS = 1_000_000_007;
+  static final int MODULUS = 1_000_000_007;
 
-    public int numSubseq(int[] nums, int target) {
-        int[] twoPowers = new int[nums.length + 1];
-        twoPowers[0] = 1;
-        for (int i = 1; i < nums.length; ++i) {
-            twoPowers[i] = multiplyMod(twoPowers[i - 1], 2);
-        }
-
-        Map<Integer, Integer> valueToCount = new HashMap<>();
-        for (int value : nums) {
-            valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
-        }
-
-        nums = Arrays.stream(nums).boxed().sorted().mapToInt(x -> x).toArray();
-
-        int result = 0;
-        for (int i = 0; i < nums.length && nums[i] * 2 <= target; ++i) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                result = addMod(result, multiplyMod(subtractMod(twoPowers[valueToCount.get(nums[i])], 1),
-                        twoPowers[computeExtraNum(nums, target - nums[i], i + valueToCount.get(nums[i]))]));
-            }
-        }
-
-        return result;
+  public int numSubseq(int[] nums, int target) {
+    int[] twoPowers = new int[nums.length + 1];
+    twoPowers[0] = 1;
+    for (int i = 1; i < nums.length; ++i) {
+      twoPowers[i] = multiplyMod(twoPowers[i - 1], 2);
     }
 
-    static int computeExtraNum(int[] nums, int limit, int beginIndex) {
-        int index = beginIndex - 1;
-        int lower = beginIndex;
-        int upper = nums.length - 1;
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
+    Arrays.sort(nums);
 
-            if (nums[middle] <= limit) {
-                index = middle;
-                lower = middle + 1;
-            } else {
-                upper = middle - 1;
-            }
-        }
+    int result = 0;
+    int endIndex = nums.length - 1;
+    for (int beginIndex = 0; ; ++beginIndex) {
+      while (endIndex >= beginIndex && nums[beginIndex] + nums[endIndex] > target) {
+        --endIndex;
+      }
+      if (endIndex < beginIndex) {
+        break;
+      }
 
-        return index - beginIndex + 1;
+      result = addMod(result, twoPowers[endIndex - beginIndex]);
     }
 
-    static int addMod(int x, int y) {
-        return (x + y) % MODULUS;
-    }
+    return result;
+  }
 
-    static int subtractMod(int x, int y) {
-        return (x - y + MODULUS) % MODULUS;
-    }
+  static int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 
-    static int multiplyMod(int x, int y) {
-        return (int) ((long) x * y % MODULUS);
-    }
+  static int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, MODULUS);
+  }
 }
