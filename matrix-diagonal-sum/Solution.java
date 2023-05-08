ddeea47bd9fbfd5a3f7a451ac9b4080bc2eecaab
Kai Wang <charles.wangkai@gmail.com>
2023-05-08 08:10:29 +0800

    Matrix Diagonal Sum

--- matrix-diagonal-sum/Solution.java
+++ matrix-diagonal-sum/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int diagonalSum(int[][] mat) {
-        int size = mat.length;
+  public int diagonalSum(int[][] mat) {
+    int size = mat.length;
 
-        int result = 0;
-        for (int r = 0; r < size; ++r) {
-            for (int c = 0; c < size; ++c) {
-                if (r == c || r + c == size - 1) {
-                    result += mat[r][c];
-                }
-            }
+    int result = 0;
+    for (int r = 0; r < size; ++r) {
+      for (int c = 0; c < size; ++c) {
+        if (r == c || r + c == size - 1) {
+          result += mat[r][c];
         }
-
-        return result;
+      }
     }
+
+    return result;
+  }
 }
