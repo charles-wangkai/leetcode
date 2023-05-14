9c076a4e89d0b98bec4d6ad2d1d6e2b520281c82
Kai Wang <charles.wangkai@gmail.com>
2023-05-14 11:27:59 +0800

    Number of Senior Citizens

---
+++ number-of-senior-citizens/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+  public int countSeniors(String[] details) {
+    return (int)
+        Arrays.stream(details)
+            .filter(detail -> Integer.parseInt(detail.substring(11, 13)) > 60)
+            .count();
+  }
+}
