1a06102423d260711d7d68701737f806a7b942a4
Kai Wang <charles.wangkai@gmail.com>
2023-05-18 09:44:48 +0800

    Minimum Number of Vertices to Reach All Nodes

--- minimum-number-of-vertices-to-reach-all-nodes/Solution.java
+++ minimum-number-of-vertices-to-reach-all-nodes/Solution.java
@@ -1,14 +1,13 @@
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
-    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        boolean[] incomes = new boolean[n];
-        for (List<Integer> edge : edges) {
-            incomes[edge.get(1)] = true;
-        }
-
-        return IntStream.range(0, incomes.length).filter(i -> !incomes[i]).boxed().collect(Collectors.toList());
+  public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+    boolean[] incomes = new boolean[n];
+    for (List<Integer> edge : edges) {
+      incomes[edge.get(1)] = true;
     }
+
+    return IntStream.range(0, incomes.length).filter(i -> !incomes[i]).boxed().toList();
+  }
 }
