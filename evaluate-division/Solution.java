70687fd57c0d33e4943817db36d54f32db3bc4e6
Kai Wang <charles.wangkai@gmail.com>
2023-05-20 08:41:27 +0800

    Evaluate Division

--- evaluate-division/Solution.java
+++ evaluate-division/Solution.java
@@ -13,7 +13,6 @@ class Solution {
     }
 
     Double[][] matrix = new Double[variableToIndex.size()][variableToIndex.size()];
-
     for (int i = 0; i < equations.size(); ++i) {
       int numerIndex = variableToIndex.get(equations.get(i).get(0));
       int denomIndex = variableToIndex.get(equations.get(i).get(1));
@@ -21,8 +20,15 @@ class Solution {
       matrix[numerIndex][denomIndex] = values[i];
       matrix[denomIndex][numerIndex] = 1 / values[i];
     }
-
-    computeClosure(matrix);
+    for (int k = 0; k < variableToIndex.size(); ++k) {
+      for (int i = 0; i < variableToIndex.size(); ++i) {
+        for (int j = 0; j < variableToIndex.size(); ++j) {
+          if (matrix[i][k] != null && matrix[k][j] != null) {
+            matrix[i][j] = matrix[i][k] * matrix[k][j];
+          }
+        }
+      }
+    }
 
     return queries.stream()
         .mapToDouble(
@@ -38,19 +44,5 @@ class Solution {
               return (result == null) ? -1 : result;
             })
         .toArray();
-  }
-
-  void computeClosure(Double[][] matrix) {
-    int n = matrix.length;
-
-    for (int k = 0; k < n; ++k) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-          if (matrix[i][k] != null && matrix[k][j] != null) {
-            matrix[i][j] = matrix[i][k] * matrix[k][j];
-          }
-        }
-      }
-    }
   }
 }
