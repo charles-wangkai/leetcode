0f5cd2d7b4ac0901f3122b0cf513bd7add78e366
Kai Wang <charles.wangkai@gmail.com>
2023-05-21 09:13:51 +0800

    Shortest Bridge

--- shortest-bridge/Solution.java
+++ shortest-bridge/Solution.java
@@ -1,81 +1,72 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int shortestBridge(int[][] A) {
-		int row = A.length;
-		int col = A[0].length;
+  public int shortestBridge(int[][] grid) {
+    int n = grid.length;
 
-		int[][] distances = new int[row][col];
-		for (int r = 0; r < row; r++) {
-			Arrays.fill(distances[r], -1);
-		}
+    int[][] distances = new int[n][n];
+    for (int r = 0; r < n; ++r) {
+      Arrays.fill(distances[r], -1);
+    }
 
-		Point pointOne = findPointOne(A);
-		Queue<Point> queue = new LinkedList<>();
-		dfs(A, distances, pointOne.r, pointOne.c, queue);
+    Point pointOne = findPointOne(grid);
+    Queue<Point> queue = new ArrayDeque<>();
+    dfs(grid, distances, queue, pointOne.r(), pointOne.c());
 
-		while (true) {
-			Point head = queue.poll();
+    while (true) {
+      Point head = queue.poll();
 
-			for (int i = 0; i < R_OFFSETS.length; i++) {
-				int nextR = head.r + R_OFFSETS[i];
-				int nextC = head.c + C_OFFSETS[i];
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r() + R_OFFSETS[i];
+        int adjC = head.c() + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && distances[adjR][adjC] == -1) {
+          distances[adjR][adjC] = distances[head.r()][head.c()] + 1;
+          if (grid[adjR][adjC] == 1) {
+            return distances[adjR][adjC] - 1;
+          }
 
-				if (nextR >= 0 && nextR < row && nextC >= 0 && nextC < col && distances[nextR][nextC] == -1) {
-					distances[nextR][nextC] = distances[head.r][head.c] + 1;
+          queue.offer(new Point(adjR, adjC));
+        }
+      }
+    }
+  }
 
-					if (A[nextR][nextC] == 1) {
-						return distances[nextR][nextC] - 1;
-					}
+  Point findPointOne(int[][] grid) {
+    int n = grid.length;
 
-					queue.offer(new Point(nextR, nextC));
-				}
-			}
-		}
-	}
+    for (int r = 0; ; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (grid[r][c] == 1) {
+          return new Point(r, c);
+        }
+      }
+    }
+  }
 
-	Point findPointOne(int[][] A) {
-		int col = A[0].length;
+  void dfs(int[][] grid, int[][] distances, Queue<Point> queue, int r, int c) {
+    int n = grid.length;
 
-		for (int r = 0;; r++) {
-			for (int c = 0; c < col; c++) {
-				if (A[r][c] == 1) {
-					return new Point(r, c);
-				}
-			}
-		}
-	}
+    distances[r][c] = 0;
+    queue.offer(new Point(r, c));
 
-	void dfs(int[][] A, int[][] distances, int r, int c, Queue<Point> queue) {
-		int row = A.length;
-		int col = A[0].length;
-
-		distances[r][c] = 0;
-		queue.offer(new Point(r, c));
-
-		for (int i = 0; i < R_OFFSETS.length; i++) {
-			int nextR = r + R_OFFSETS[i];
-			int nextC = c + C_OFFSETS[i];
-
-			if (nextR >= 0 && nextR < row && nextC >= 0 && nextC < col && A[nextR][nextC] == 1
-					&& distances[nextR][nextC] == -1) {
-				dfs(A, distances, nextR, nextC, queue);
-			}
-		}
-	}
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int adjR = r + R_OFFSETS[i];
+      int adjC = c + C_OFFSETS[i];
+      if (adjR >= 0
+          && adjR < n
+          && adjC >= 0
+          && adjC < n
+          && grid[adjR][adjC] == 1
+          && distances[adjR][adjC] == -1) {
+        dfs(grid, distances, queue, adjR, adjC);
+      }
+    }
+  }
 }
 
-class Point {
-	int r;
-	int c;
-
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
-}
+record Point(int r, int c) {}
