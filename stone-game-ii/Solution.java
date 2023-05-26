74450a361463a5619ecc5a28f74febc0a7f639c0
Kai Wang <charles.wangkai@gmail.com>
2023-05-26 08:51:30 +0800

    Stone Game II

--- stone-game-ii/Solution.java
+++ stone-game-ii/Solution.java
@@ -1,59 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+import java.util.stream.IntStream;
 
-public class Solution {
-	Map<State, Integer> cache;
+class Solution {
+  Map<State, Integer> cache;
 
-	public int stoneGameII(int[] piles) {
-		cache = new HashMap<>();
-		return computeMaxStoneNumForFirstPlayer(piles, 0, 1);
-	}
+  public int stoneGameII(int[] piles) {
+    cache = new HashMap<>();
 
-	int computeMaxStoneNumForFirstPlayer(int[] piles, int beginIndex, int M) {
-		State state = new State(beginIndex, M);
-		if (cache.containsKey(state)) {
-			return cache.get(state);
-		}
+    return computeMaxStoneNumForFirstPlayer(piles, 0, 1);
+  }
 
-		int result = 0;
-		int taken = 0;
-		int remain = 0;
-		for (int i = beginIndex; i < piles.length; i++) {
-			remain += piles[i];
-		}
-		for (int X = 1; X <= 2 * M && beginIndex + X <= piles.length; X++) {
-			taken += piles[beginIndex + X - 1];
-			remain -= piles[beginIndex + X - 1];
+  int computeMaxStoneNumForFirstPlayer(int[] piles, int beginIndex, int M) {
+    State state = new State(beginIndex, M);
+    if (!cache.containsKey(state)) {
+      int result = 0;
+      int total = IntStream.range(beginIndex, piles.length).map(i -> piles[i]).sum();
+      for (int X = 1; X <= 2 * M && beginIndex + X <= piles.length; ++X) {
+        result =
+            Math.max(
+                result,
+                total - computeMaxStoneNumForFirstPlayer(piles, beginIndex + X, Math.max(M, X)));
+      }
 
-			int subResult = computeMaxStoneNumForFirstPlayer(piles, beginIndex + X, Math.max(M, X));
+      cache.put(state, result);
+    }
 
-			result = Math.max(result, taken + (remain - subResult));
-		}
-
-		cache.put(state, result);
-		return result;
-	}
-
+    return cache.get(state);
+  }
 }
 
-class State {
-	int beginIndex;
-	int M;
-
-	State(int beginIndex, int M) {
-		this.beginIndex = beginIndex;
-		this.M = M;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(beginIndex, M);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		State other = (State) obj;
-		return beginIndex == other.beginIndex && M == other.M;
-	}
-}
+record State(int beginIndex, int M) {}
