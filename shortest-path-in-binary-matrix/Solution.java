90abba2d2e2213f0d1c79c08a15b2318ffe3364d
Kai Wang <charles.wangkai@gmail.com>
2023-06-01 09:23:12 +0800

    Shortest Path in Binary Matrix

--- shortest-path-in-binary-matrix/Solution.java
+++ shortest-path-in-binary-matrix/Solution.java
@@ -1,10 +1,8 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
 class Solution {
-  static final int[] R_OFFSETS = {-1, -1, 0, 1, 1, 1, 0, -1};
-  static final int[] C_OFFSETS = {0, 1, 1, 1, 0, -1, -1, -1};
-
   public int shortestPathBinaryMatrix(int[][] grid) {
     if (grid[0][0] == 1) {
       return -1;
@@ -13,43 +11,36 @@ class Solution {
     int n = grid.length;
 
     int[][] minLengths = new int[n][n];
+    for (int r = 0; r < minLengths.length; ++r) {
+      Arrays.fill(minLengths[r], -1);
+    }
+    minLengths[0][0] = 1;
+
     Queue<Point> queue = new ArrayDeque<>();
     queue.offer(new Point(0, 0));
-    minLengths[0][0] = 1;
 
     while (!queue.isEmpty()) {
       Point head = queue.poll();
-      if (head.r == n - 1 && head.c == n - 1) {
-        return minLengths[head.r][head.c];
-      }
 
-      for (int i = 0; i < R_OFFSETS.length; ++i) {
-        int adjR = head.r + R_OFFSETS[i];
-        int adjC = head.c + C_OFFSETS[i];
-
-        if (adjR >= 0
-            && adjR < n
-            && adjC >= 0
-            && adjC < n
-            && grid[adjR][adjC] == 0
-            && minLengths[adjR][adjC] == 0) {
-          minLengths[adjR][adjC] = minLengths[head.r][head.c] + 1;
-
-          queue.offer(new Point(adjR, adjC));
+      for (int dr = -1; dr <= 1; ++dr) {
+        for (int dc = -1; dc <= 1; ++dc) {
+          int adjR = head.r() + dr;
+          int adjC = head.c() + dc;
+          if (adjR >= 0
+              && adjR < n
+              && adjC >= 0
+              && adjC < n
+              && grid[adjR][adjC] == 0
+              && minLengths[adjR][adjC] == -1) {
+            minLengths[adjR][adjC] = minLengths[head.r()][head.c()] + 1;
+            queue.offer(new Point(adjR, adjC));
+          }
         }
       }
     }
 
-    return -1;
+    return minLengths[n - 1][n - 1];
   }
 }
 
-class Point {
-  int r;
-  int c;
-
-  Point(int r, int c) {
-    this.r = r;
-    this.c = c;
-  }
-}
+record Point(int r, int c) {}
