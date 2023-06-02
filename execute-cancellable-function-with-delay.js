e187096e3372923fd3681070c4c1db108e764d15
Kai Wang <charles.wangkai@gmail.com>
2023-06-02 09:57:38 +0800

    Execute Cancellable Function With Delay

---
+++ execute-cancellable-function-with-delay.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  const timeoutId = setTimeout(() => fn(...args), t);
+
+  return function () {
+    clearTimeout(timeoutId);
+  };
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *
+ *  const cancel = cancellable(fn, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
