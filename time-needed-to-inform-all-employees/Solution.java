69c05893e5ba5fe317356ef1171a3b9b296487db
Kai Wang <charles.wangkai@gmail.com>
2023-06-03 08:32:50 +0800

    Time Needed to Inform All Employees

--- time-needed-to-inform-all-employees/Solution.java
+++ time-needed-to-inform-all-employees/Solution.java
@@ -2,28 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        @SuppressWarnings("unchecked")
-        List<Integer>[] childrens = new List[n];
-        for (int i = 0; i < childrens.length; ++i) {
-            childrens[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < manager.length; ++i) {
-            if (manager[i] != -1) {
-                childrens[manager[i]].add(i);
-            }
-        }
-
-        return search(childrens, informTime, headID, 0);
+  public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+    @SuppressWarnings("unchecked")
+    List<Integer>[] childrens = new List[n];
+    for (int i = 0; i < childrens.length; ++i) {
+      childrens[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < manager.length; ++i) {
+      if (manager[i] != -1) {
+        childrens[manager[i]].add(i);
+      }
     }
 
-    int search(List<Integer>[] childrens, int[] informTime, int node, int time) {
-        int result = time;
-        for (int child : childrens[node]) {
-            result = Math.max(result, search(childrens, informTime, child, time + informTime[node]));
-        }
+    return search(childrens, informTime, headID);
+  }
 
-        return result;
-    }
+  int search(List<Integer>[] childrens, int[] informTime, int node) {
+    return informTime[node]
+        + childrens[node].stream()
+            .mapToInt(child -> search(childrens, informTime, child))
+            .max()
+            .orElse(0);
+  }
 }
