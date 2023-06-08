4364ffc7e401328749b2f584eb2a3bb6d638fd64
Kai Wang <charles.wangkai@gmail.com>
2023-06-08 09:46:31 +0800

    Interval Cancellation

---
+++ interval-cancellation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  fn(...args);
+  const intervalId = setInterval(() => fn(...args), t);
+
+  return () => clearInterval(intervalId);
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *
+ *  const cancel = cancellable(fn, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT)
+ */
