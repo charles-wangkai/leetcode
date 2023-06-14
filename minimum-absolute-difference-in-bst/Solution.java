8642b5e26f87efcfba2ea0c3c939529a209e4b9b
Kai Wang <charles.wangkai@gmail.com>
2023-06-14 09:38:28 +0800

    Minimum Absolute Difference in BST

--- minimum-absolute-difference-in-bst/Solution.java
+++ minimum-absolute-difference-in-bst/Solution.java
@@ -1,39 +1,46 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	Integer prevValue;
-	int minDiff;
+class Solution {
+  Integer prevValue;
+  int minDiff;
 
-	public int getMinimumDifference(TreeNode root) {
-		prevValue = null;
-		minDiff = Integer.MAX_VALUE;
+  public int getMinimumDifference(TreeNode root) {
+    prevValue = null;
+    minDiff = Integer.MAX_VALUE;
+    search(root);
 
-		search(root);
+    return minDiff;
+  }
 
-		return minDiff;
-	}
+  void search(TreeNode node) {
+    if (node == null) {
+      return;
+    }
 
-	void search(TreeNode node) {
-		if (node == null) {
-			return;
-		}
+    search(node.left);
 
-		search(node.left);
+    if (prevValue != null) {
+      minDiff = Math.min(minDiff, node.val - prevValue);
+    }
+    prevValue = node.val;
 
-		if (prevValue != null) {
-			minDiff = Math.min(minDiff, node.val - prevValue);
-		}
-		prevValue = node.val;
-
-		search(node.right);
-	}
+    search(node.right);
+  }
 }
