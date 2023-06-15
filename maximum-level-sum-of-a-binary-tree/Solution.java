a1b761219775fa69a5f76e73eaf58ac9bc1b2fa5
Kai Wang <charles.wangkai@gmail.com>
2023-06-15 08:15:22 +0800

    Maximum Level Sum of a Binary Tree

--- maximum-level-sum-of-a-binary-tree/Solution.java
+++ maximum-level-sum-of-a-binary-tree/Solution.java
@@ -4,36 +4,43 @@ import java.util.stream.IntStream;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public int maxLevelSum(TreeNode root) {
-		List<Integer> sums = new ArrayList<>();
-		search(sums, root, 0);
+class Solution {
+  public int maxLevelSum(TreeNode root) {
+    List<Integer> sums = new ArrayList<>();
+    search(sums, root, 0);
 
-		int maxSum = sums.stream().mapToInt(x -> x).max().getAsInt();
-		return IntStream.range(0, sums.size()).filter(i -> sums.get(i) == maxSum).findFirst().getAsInt() + 1;
-	}
+    int maxSum = sums.stream().mapToInt(Integer::intValue).max().getAsInt();
 
-	void search(List<Integer> sums, TreeNode node, int index) {
-		if (index == sums.size()) {
-			sums.add(0);
-		}
+    return IntStream.range(0, sums.size()).filter(i -> sums.get(i) == maxSum).findFirst().getAsInt()
+        + 1;
+  }
 
-		sums.set(index, sums.get(index) + node.val);
+  void search(List<Integer> sums, TreeNode node, int index) {
+    if (node != null) {
+      if (index == sums.size()) {
+        sums.add(0);
+      }
+      sums.set(index, sums.get(index) + node.val);
 
-		if (node.left != null) {
-			search(sums, node.left, index + 1);
-		}
-		if (node.right != null) {
-			search(sums, node.right, index + 1);
-		}
-	}
+      search(sums, node.left, index + 1);
+      search(sums, node.right, index + 1);
+    }
+  }
 }
