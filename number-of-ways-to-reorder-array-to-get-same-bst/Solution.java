5d3cd2ad7143c7edc20a0689b115666da70f168f
Kai Wang <charles.wangkai@gmail.com>
2023-06-16 10:25:08 +0800

    Number of Ways to Reorder Array to Get Same BST

--- number-of-ways-to-reorder-array-to-get-same-bst/Solution.java
+++ number-of-ways-to-reorder-array-to-get-same-bst/Solution.java
@@ -1,77 +1,78 @@
 import java.math.BigInteger;
 
 class Solution {
-    static final int MODULUS = 1_000_000_007;
+  static final int MODULUS = 1_000_000_007;
 
-    static int wayNum;
+  static int wayNum;
 
-    public int numOfWays(int[] nums) {
-        Node root = new Node(nums[0]);
-        for (int i = 1; i < nums.length; ++i) {
-            insert(root, nums[i]);
-        }
-
-        wayNum = 1;
-        search(root);
-
-        return subtractMod(wayNum, 1);
+  public int numOfWays(int[] nums) {
+    Node root = null;
+    for (int i = 0; i < nums.length; ++i) {
+      root = insert(root, nums[i]);
     }
 
-    void insert(Node node, int value) {
-        if (value < node.value) {
-            if (node.left == null) {
-                node.left = new Node(value);
-            } else {
-                insert(node.left, value);
-            }
-        } else {
-            if (node.right == null) {
-                node.right = new Node(value);
-            } else {
-                insert(node.right, value);
-            }
-        }
+    wayNum = 1;
+    search(root);
+
+    return addMod(wayNum, -1);
+  }
+
+  Node insert(Node node, int value) {
+    if (node == null) {
+      return new Node(value);
     }
 
-    int search(Node node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int size = 1;
-        int leftSize = search(node.left);
-        int rightSize = search(node.right);
-
-        wayNum = multiplyMod(wayNum, C(leftSize + rightSize, leftSize));
-
-        return leftSize + rightSize + 1;
+    if (value < node.value) {
+      node.left = insert(node.left, value);
+    } else {
+      node.right = insert(node.right, value);
     }
 
-    int subtractMod(int x, int y) {
-        return (x - y + MODULUS) % MODULUS;
+    return node;
+  }
+
+  int search(Node node) {
+    if (node == null) {
+      return 0;
     }
 
-    int multiplyMod(int x, int y) {
-        return (int) ((long) x * y % MODULUS);
+    int leftSize = search(node.left);
+    int rightSize = search(node.right);
+
+    wayNum = multiplyMod(wayNum, CMod(leftSize + rightSize, leftSize));
+
+    return leftSize + rightSize + 1;
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
+
+  int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, MODULUS);
+  }
+
+  int CMod(int n, int r) {
+    int result = 1;
+    for (int i = 0; i < r; ++i) {
+      result =
+          multiplyMod(
+              result,
+              multiplyMod(
+                  n - i,
+                  BigInteger.valueOf(i + 1).modInverse(BigInteger.valueOf(MODULUS)).intValue()));
     }
 
-    int C(int n, int r) {
-        int result = 1;
-        for (int i = 0; i < r; ++i) {
-            result = multiplyMod(result,
-                    multiplyMod(n - i, BigInteger.valueOf(i + 1).modInverse(BigInteger.valueOf(MODULUS)).intValue()));
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
 
 class Node {
-    int value;
-    Node left;
-    Node right;
+  int value;
+  Node left;
+  Node right;
 
-    public Node(int value) {
-        this.value = value;
-    }
+  Node(int value) {
+    this.value = value;
+  }
 }
