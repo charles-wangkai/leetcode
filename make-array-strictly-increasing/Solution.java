add1d00eb423f9cdb8c47b8e348f7a16c180826d
Kai Wang <charles.wangkai@gmail.com>
2023-06-17 10:47:16 +0800

    Make Array Strictly Increasing

--- make-array-strictly-increasing/Solution.java
+++ make-array-strictly-increasing/Solution.java
@@ -2,48 +2,52 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-		arr2 = Arrays.stream(arr2).distinct().sorted().toArray();
+class Solution {
+  public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+    Arrays.sort(arr2);
 
-		Map<Integer, Integer> operationNumToLastValue = new HashMap<>();
-		operationNumToLastValue.put(0, Integer.MIN_VALUE);
-		for (int value : arr1) {
-			Map<Integer, Integer> nextOperationNumToLastValue = new HashMap<>();
-			for (int operationNum : operationNumToLastValue.keySet()) {
-				int lastValue = operationNumToLastValue.get(operationNum);
+    Map<Integer, Integer> operationNumToLastValue = Map.of(0, Integer.MIN_VALUE);
+    for (int value : arr1) {
+      Map<Integer, Integer> nextOperationNumToLastValue = new HashMap<>();
+      for (int operationNum : operationNumToLastValue.keySet()) {
+        int lastValue = operationNumToLastValue.get(operationNum);
 
-				if (value > lastValue) {
-					update(nextOperationNumToLastValue, operationNum, value);
-				}
+        if (value > lastValue) {
+          updateMap(nextOperationNumToLastValue, operationNum, value);
+        }
 
-				Integer largerValue = findLargerValue(arr2, lastValue);
-				if (largerValue != null) {
-					update(nextOperationNumToLastValue, operationNum + 1, largerValue);
-				}
-			}
+        Integer largerValue = findLargerValue(arr2, lastValue);
+        if (largerValue != null) {
+          updateMap(nextOperationNumToLastValue, operationNum + 1, largerValue);
+        }
+      }
 
-			operationNumToLastValue = nextOperationNumToLastValue;
-		}
+      operationNumToLastValue = nextOperationNumToLastValue;
+    }
 
-		return operationNumToLastValue.keySet().stream().mapToInt(x -> x).min().orElse(-1);
-	}
+    return operationNumToLastValue.keySet().stream().mapToInt(Integer::intValue).min().orElse(-1);
+  }
 
-	void update(Map<Integer, Integer> operationNumToLastValue, int operationNum, int lastValue) {
-		operationNumToLastValue.put(operationNum,
-				Math.min(operationNumToLastValue.getOrDefault(operationNum, Integer.MAX_VALUE), lastValue));
-	}
+  void updateMap(Map<Integer, Integer> operationNumToLastValue, int operationNum, int lastValue) {
+    operationNumToLastValue.put(
+        operationNum,
+        Math.min(operationNumToLastValue.getOrDefault(operationNum, Integer.MAX_VALUE), lastValue));
+  }
 
-	Integer findLargerValue(int[] arr2, int value) {
-		int index = Arrays.binarySearch(arr2, value);
+  Integer findLargerValue(int[] arr2, int value) {
+    int index = -1;
+    int lower = 0;
+    int upper = arr2.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (arr2[middle] > value) {
+        index = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
 
-		int indexWithLargerValue;
-		if (index >= 0) {
-			indexWithLargerValue = index + 1;
-		} else {
-			indexWithLargerValue = -1 - index;
-		}
-
-		return (indexWithLargerValue == arr2.length) ? null : arr2[indexWithLargerValue];
-	}
+    return (index == -1) ? null : arr2[index];
+  }
 }
