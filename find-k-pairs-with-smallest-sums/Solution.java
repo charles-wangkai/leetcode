bec678581b263fcfba8c9484f24c8365c388fab8
Kai Wang <charles.wangkai@gmail.com>
2023-06-27 09:37:44 +0800

    Find K Pairs with Smallest Sums

--- find-k-pairs-with-smallest-sums/Solution.java
+++ find-k-pairs-with-smallest-sums/Solution.java
@@ -1,38 +1,30 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
-public class Solution {
-	public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-		PriorityQueue<Pair> pq = new PriorityQueue<Pair>(
-				(p1, p2) -> (nums1[p1.index1] + nums2[p1.index2]) - (nums1[p2.index1] + nums2[p2.index2]));
+class Solution {
+  public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    PriorityQueue<Pair> pq =
+        new PriorityQueue<Pair>(
+            Comparator.comparing(pair -> nums1[pair.index1()] + nums2[pair.index2()]));
+    pq.offer(new Pair(0, 0));
 
-		List<int[]> result = new ArrayList<int[]>();
-		if (nums1.length > 0 && nums2.length > 0) {
-			pq.offer(new Pair(0, 0));
-			while (!pq.isEmpty() && result.size() < k) {
-				Pair head = pq.poll();
+    List<List<Integer>> result = new ArrayList<>();
+    for (int i = 0; i < k && !pq.isEmpty(); ++i) {
+      Pair head = pq.poll();
+      result.add(List.of(nums1[head.index1()], nums2[head.index2()]));
 
-				result.add(new int[] { nums1[head.index1], nums2[head.index2] });
+      if (head.index2() != nums2.length - 1) {
+        pq.offer(new Pair(head.index1(), head.index2() + 1));
+      }
+      if (head.index2() == 0 && head.index1() != nums1.length - 1) {
+        pq.offer(new Pair(head.index1() + 1, 0));
+      }
+    }
 
-				if (head.index2 + 1 < nums2.length) {
-					pq.offer(new Pair(head.index1, head.index2 + 1));
-				}
-				if (head.index2 == 0 && head.index1 + 1 < nums1.length) {
-					pq.offer(new Pair(head.index1 + 1, 0));
-				}
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 }
 
-class Pair {
-	int index1;
-	int index2;
-
-	Pair(int index1, int index2) {
-		this.index1 = index1;
-		this.index2 = index2;
-	}
-}
+record Pair(int index1, int index2) {}
