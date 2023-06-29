2b2877932e500272fbcd54da64089dccaeed4514
Kai Wang <charles.wangkai@gmail.com>
2023-06-29 12:18:16 +0800

    Shortest Path to Get All Keys

--- shortest-path-to-get-all-keys/Solution.java
+++ shortest-path-to-get-all-keys/Solution.java
@@ -1,141 +1,64 @@
-import java.util.Collections;
+import java.util.ArrayDeque;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+class Solution {
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int shortestPathAllKeys(String[] grid) {
-		Point startPoint = findStartPoint(grid);
-		int totalKeyNum = countKeys(grid);
-		State startState = new State(startPoint, Collections.emptySet());
-		Set<State> history = new HashSet<>();
-		history.add(startState);
-		Queue<Element> queue = new LinkedList<>();
-		queue.offer(new Element(startState, 0));
-		while (!queue.isEmpty()) {
-			Element head = queue.poll();
+  public int shortestPathAllKeys(String[] grid) {
+    int m = grid.length;
+    int n = grid[0].length();
 
-			if (head.state.keys.size() == totalKeyNum) {
-				return head.moveNum;
-			}
+    int keyCount = 0;
+    Map<State, Integer> stateToMoveNum = new HashMap<>();
+    Queue<State> queue = new ArrayDeque<>();
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (grid[r].charAt(c) == '@') {
+          State state = new State(r, c, Set.of());
+          stateToMoveNum.put(state, 0);
+          queue.offer(state);
+        } else if (Character.isLowerCase(grid[r].charAt(c))) {
+          ++keyCount;
+        }
+      }
+    }
 
-			for (int i = 0; i < R_OFFSETS.length; i++) {
-				int nextR = head.state.point.r + R_OFFSETS[i];
-				int nextC = head.state.point.c + C_OFFSETS[i];
+    while (!queue.isEmpty()) {
+      State head = queue.poll();
+      if (head.keys().size() == keyCount) {
+        return stateToMoveNum.get(head);
+      }
 
-				State nextState = buildNextState(head.state, grid, nextR, nextC);
-				if (nextState != null && !history.contains(nextState)) {
-					history.add(nextState);
-					queue.offer(new Element(nextState, head.moveNum + 1));
-				}
-			}
-		}
+      for (int i = 0; i < R_OFFSETS.length; ++i) {
+        int adjR = head.r() + R_OFFSETS[i];
+        int adjC = head.c() + C_OFFSETS[i];
+        if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n) {
+          char cell = grid[adjR].charAt(adjC);
+          if (cell != '#'
+              && (!Character.isUpperCase(cell)
+                  || head.keys().contains(Character.toLowerCase(cell)))) {
+            Set<Character> nextKeys = new HashSet<>(head.keys());
+            if (Character.isLowerCase(cell)) {
+              nextKeys.add(cell);
+            }
 
-		return -1;
-	}
+            State nextState = new State(adjR, adjC, nextKeys);
+            if (!stateToMoveNum.containsKey(nextState)) {
+              stateToMoveNum.put(nextState, stateToMoveNum.get(head) + 1);
+              queue.offer(nextState);
+            }
+          }
+        }
+      }
+    }
 
-	State buildNextState(State state, String[] grid, int nextR, int nextC) {
-		int row = grid.length;
-		int col = grid[0].length();
-
-		if (!(nextR >= 0 && nextR < row && nextC >= 0 && nextC < col)) {
-			return null;
-		}
-
-		Set<Character> nextKeys = new HashSet<>(state.keys);
-		char cell = grid[nextR].charAt(nextC);
-		if (cell == '#') {
-			return null;
-		} else if (Character.isUpperCase(cell)) {
-			if (!state.keys.contains(Character.toLowerCase(cell))) {
-				return null;
-			}
-		} else if (Character.isLowerCase(cell)) {
-			nextKeys.add(cell);
-		}
-
-		return new State(new Point(nextR, nextC), nextKeys);
-	}
-
-	Point findStartPoint(String[] grid) {
-		for (int r = 0;; r++) {
-			for (int c = 0; c < grid[0].length(); c++) {
-				if (grid[r].charAt(c) == '@') {
-					return new Point(r, c);
-				}
-			}
-		}
-	}
-
-	int countKeys(String[] grid) {
-		int row = grid.length;
-		int col = grid[0].length();
-
-		int totalKeyNum = 0;
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				if (Character.isLowerCase(grid[r].charAt(c))) {
-					totalKeyNum++;
-				}
-			}
-		}
-		return totalKeyNum;
-	}
+    return -1;
+  }
 }
 
-class Point {
-	int r;
-	int c;
-
-	Point(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(r, c);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		Point other = (Point) obj;
-		return r == other.r && c == other.c;
-	}
-}
-
-class State {
-	Point point;
-	Set<Character> keys;
-
-	State(Point point, Set<Character> keys) {
-		this.point = point;
-		this.keys = keys;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(point, keys);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		State other = (State) obj;
-		return point.equals(other.point) && keys.equals(other.keys);
-	}
-}
-
-class Element {
-	State state;
-	int moveNum;
-
-	Element(State state, int moveNum) {
-		this.state = state;
-		this.moveNum = moveNum;
-	}
-}
+record State(int r, int c, Set<Character> keys) {}
