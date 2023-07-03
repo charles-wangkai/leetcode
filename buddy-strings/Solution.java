cd33db59d6f798fa1f1b50a33634d14cd39793cc
Kai Wang <charles.wangkai@gmail.com>
2023-07-03 09:14:31 +0800

    Buddy Strings

--- buddy-strings/Solution.java
+++ buddy-strings/Solution.java
@@ -3,31 +3,26 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 class Solution {
-  public boolean buddyStrings(String A, String B) {
-    if (A.length() != B.length()) {
+  public boolean buddyStrings(String s, String goal) {
+    Map<Character, Integer> letterToCountS = buildLetterToCount(s);
+    Map<Character, Integer> letterToCountGoal = buildLetterToCount(goal);
+    if (!letterToCountS.equals(letterToCountGoal)) {
       return false;
     }
 
-    Map<Character, Integer> letterToCountA = buildLetterToCount(A);
-    Map<Character, Integer> letterToCountB = buildLetterToCount(B);
-
-    if (!letterToCountA.equals(letterToCountB)) {
-      return false;
-    }
-
-    if (A.equals(B)) {
-      return letterToCountA.values().stream().anyMatch(count -> count >= 2);
+    if (s.equals(goal)) {
+      return letterToCountS.values().stream().anyMatch(count -> count >= 2);
     }
 
     int[] diffIndices =
-        IntStream.range(0, A.length()).filter(i -> A.charAt(i) != B.charAt(i)).toArray();
+        IntStream.range(0, s.length()).filter(i -> s.charAt(i) != goal.charAt(i)).toArray();
 
-    return diffIndices.length == 2 && A.charAt(diffIndices[0]) == B.charAt(diffIndices[1]);
+    return diffIndices.length == 2 && s.charAt(diffIndices[0]) == goal.charAt(diffIndices[1]);
   }
 
-  Map<Character, Integer> buildLetterToCount(String s) {
+  Map<Character, Integer> buildLetterToCount(String str) {
     Map<Character, Integer> letterToCount = new HashMap<>();
-    for (char letter : s.toCharArray()) {
+    for (char letter : str.toCharArray()) {
       letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
     }
 
