640445cc43837654e2737a001b1673c1eb45b849
Kai Wang <charles.wangkai@gmail.com>
2023-07-05 12:07:04 +0800

    Longest Subarray of 1's After Deleting One Element

--- longest-subarray-of-1s-after-deleting-one-element/Solution.java
+++ longest-subarray-of-1s-after-deleting-one-element/Solution.java
@@ -1,31 +1,34 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int longestSubarray(int[] nums) {
-        int[] leftOneNums = new int[nums.length];
-        int leftOneNum = 0;
-        for (int i = 0; i < leftOneNums.length; ++i) {
-            leftOneNums[i] = leftOneNum;
+  public int longestSubarray(int[] nums) {
+    int[] leftOneNums = new int[nums.length];
+    int leftOneNum = 0;
+    for (int i = 0; i < leftOneNums.length; ++i) {
+      leftOneNums[i] = leftOneNum;
 
-            if (nums[i] == 1) {
-                ++leftOneNum;
-            } else {
-                leftOneNum = 0;
-            }
-        }
-
-        int[] rightOneNums = new int[nums.length];
-        int rightOneNum = 0;
-        for (int i = rightOneNums.length - 1; i >= 0; --i) {
-            rightOneNums[i] = rightOneNum;
-
-            if (nums[i] == 1) {
-                ++rightOneNum;
-            } else {
-                rightOneNum = 0;
-            }
-        }
-
-        return IntStream.range(0, nums.length).map(i -> leftOneNums[i] + rightOneNums[i]).max().getAsInt();
+      if (nums[i] == 1) {
+        ++leftOneNum;
+      } else {
+        leftOneNum = 0;
+      }
     }
+
+    int[] rightOneNums = new int[nums.length];
+    int rightOneNum = 0;
+    for (int i = rightOneNums.length - 1; i >= 0; --i) {
+      rightOneNums[i] = rightOneNum;
+
+      if (nums[i] == 1) {
+        ++rightOneNum;
+      } else {
+        rightOneNum = 0;
+      }
+    }
+
+    return IntStream.range(0, nums.length)
+        .map(i -> leftOneNums[i] + rightOneNums[i])
+        .max()
+        .getAsInt();
+  }
 }
