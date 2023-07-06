a6afee4f7da13fe909df0ef660c14103df21c0b5
Kai Wang <charles.wangkai@gmail.com>
2023-07-06 08:22:06 +0800

    Minimum Size Subarray Sum

--- minimum-size-subarray-sum/Solution.java
+++ minimum-size-subarray-sum/Solution.java
@@ -1,18 +1,21 @@
-public class Solution {
-	public int minSubArrayLen(int s, int[] nums) {
-		int minLength = Integer.MAX_VALUE;
-		int beginIndex = 0;
-		int sum = 0;
-		for (int i = 0; i < nums.length; i++) {
-			sum += nums[i];
-			while (sum - nums[beginIndex] >= s) {
-				sum -= nums[beginIndex];
-				beginIndex++;
-			}
-			if (sum >= s) {
-				minLength = Math.min(minLength, i - beginIndex + 1);
-			}
-		}
-		return minLength == Integer.MAX_VALUE ? 0 : minLength;
-	}
+class Solution {
+  public int minSubArrayLen(int target, int[] nums) {
+    int minLength = Integer.MAX_VALUE;
+    int beginIndex = 0;
+    int sum = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      sum += nums[i];
+
+      while (sum - nums[beginIndex] >= target) {
+        sum -= nums[beginIndex];
+        ++beginIndex;
+      }
+
+      if (sum >= target) {
+        minLength = Math.min(minLength, i - beginIndex + 1);
+      }
+    }
+
+    return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
+  }
 }
