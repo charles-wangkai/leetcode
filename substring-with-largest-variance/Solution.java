d434796afea69900338b5c61564ce09296ccbdbd
Kai Wang <charles.wangkai@gmail.com>
2023-07-09 18:55:34 +0800

    Substring With Largest Variance

--- substring-with-largest-variance/Solution.java
+++ substring-with-largest-variance/Solution.java
@@ -1,14 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 class Solution {
   public int largestVariance(String s) {
     int result = 0;
     for (char letter1 = 'a'; letter1 <= 'z'; ++letter1) {
-      for (char letter2 = (char) (letter1 + 1); letter2 <= 'z'; ++letter2) {
-        result = Math.max(result, computeMaxVariance(s, letter1, letter2));
+      for (char letter2 = 'a'; letter2 <= 'z'; ++letter2) {
+        if (letter2 != letter1) {
+          result = Math.max(result, computeMaxVariance(s, letter1, letter2));
+        }
       }
     }
 
@@ -16,68 +13,33 @@ class Solution {
   }
 
   int computeMaxVariance(String s, char letter1, char letter2) {
-    List<Element> elements = new ArrayList<>();
-    for (char letter : s.toCharArray()) {
-      if (letter == letter1 || letter == letter2) {
-        if (!elements.isEmpty() && letter == elements.get(elements.size() - 1).letter) {
-          ++elements.get(elements.size() - 1).count;
+    int result = 0;
+    int sum = 0;
+    boolean firstNegative = false;
+    boolean hasNegative = false;
+    for (char c : s.toCharArray()) {
+      if (c == letter1) {
+        ++sum;
+      } else if (c == letter2) {
+        if (firstNegative) {
+          firstNegative = false;
         } else {
-          elements.add(new Element(letter, 1));
-        }
-      }
-    }
-
-    SortedMap<Integer, Integer> diffToLastIndex = new TreeMap<>();
-    int diff = 0;
-    for (int i = 0; i < elements.size(); ++i) {
-      diff += ((elements.get(i).letter == letter1) ? 1 : -1) * elements.get(i).count;
-      diffToLastIndex.put(diff, i);
-    }
-
-    int result =
-        (elements.size() >= 2)
-            ? elements.stream().mapToInt(element -> element.count - 1).max().getAsInt()
-            : 0;
-    diff = 0;
-    for (int i = 0; i < elements.size(); ++i) {
-      int maxDiff = Integer.MAX_VALUE;
-      while (!diffToLastIndex.isEmpty()) {
-        maxDiff = diffToLastIndex.lastKey();
-        if (diffToLastIndex.get(maxDiff) > i) {
-          break;
+          --sum;
         }
 
-        diffToLastIndex.remove(maxDiff);
-      }
-      if (diffToLastIndex.isEmpty()) {
-        break;
+        hasNegative = true;
       }
 
-      int minDiff;
-      while (true) {
-        minDiff = diffToLastIndex.firstKey();
-        if (diffToLastIndex.get(minDiff) > i) {
-          break;
-        }
-
-        diffToLastIndex.remove(minDiff);
+      if (hasNegative) {
+        result = Math.max(result, sum);
       }
 
-      result = Math.max(result, Math.max(Math.abs(diff - maxDiff), Math.abs(diff - minDiff)));
-
-      diff += ((elements.get(i).letter == letter1) ? 1 : -1) * elements.get(i).count;
+      sum = Math.max(-1, sum);
+      if (sum == -1) {
+        firstNegative = true;
+      }
     }
 
     return result;
-  }
-}
-
-class Element {
-  char letter;
-  int count;
-
-  Element(char letter, int count) {
-    this.letter = letter;
-    this.count = count;
   }
 }
