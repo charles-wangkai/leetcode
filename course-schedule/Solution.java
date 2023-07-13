adcd737ae5b876574ce5860243bd2fd7076430b0
Kai Wang <charles.wangkai@gmail.com>
2023-07-13 08:29:28 +0800

    Course Schedule

--- course-schedule/Solution.java
+++ course-schedule/Solution.java
@@ -1,47 +1,41 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public boolean canFinish(int numCourses, int[][] prerequisites) {
-		@SuppressWarnings("unchecked")
-		Set<Integer>[] fromSets = new Set[numCourses];
-		for (int i = 0; i < fromSets.length; ++i) {
-			fromSets[i] = new HashSet<>();
-		}
+class Solution {
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int[] fromRests = new int[numCourses];
 
-		@SuppressWarnings("unchecked")
-		Set<Integer>[] toSets = new Set[numCourses];
-		for (int i = 0; i < toSets.length; ++i) {
-			toSets[i] = new HashSet<>();
-		}
+    @SuppressWarnings("unchecked")
+    List<Integer>[] toLists = new List[numCourses];
+    for (int i = 0; i < toLists.length; ++i) {
+      toLists[i] = new ArrayList<>();
+    }
 
-		for (int[] prerequisite : prerequisites) {
-			int to = prerequisite[0];
-			int from = prerequisite[1];
+    for (int[] prerequisite : prerequisites) {
+      toLists[prerequisite[1]].add(prerequisite[0]);
+      ++fromRests[prerequisite[0]];
+    }
 
-			fromSets[to].add(from);
-			toSets[from].add(to);
-		}
+    boolean[] takens = new boolean[numCourses];
+    for (int i = 0; i < numCourses; ++i) {
+      if (!takens[i] && fromRests[i] == 0) {
+        take(fromRests, toLists, takens, i);
+      }
+    }
 
-		boolean[] takens = new boolean[numCourses];
-		for (int i = 0; i < numCourses; ++i) {
-			if (!takens[i] && fromSets[i].isEmpty()) {
-				take(fromSets, toSets, takens, i);
-			}
-		}
+    return IntStream.range(0, takens.length).allMatch(i -> takens[i]);
+  }
 
-		return Arrays.stream(fromSets).allMatch(Set::isEmpty);
-	}
+  void take(int[] fromRests, List<Integer>[] toLists, boolean[] takens, int node) {
+    takens[node] = true;
 
-	void take(Set<Integer>[] fromSets, Set<Integer>[] toSets, boolean[] takens, int index) {
-		takens[index] = true;
-		for (int to : toSets[index]) {
-			fromSets[to].remove(index);
+    for (int to : toLists[node]) {
+      --fromRests[to];
 
-			if (!takens[to] && fromSets[to].isEmpty()) {
-				take(fromSets, toSets, takens, to);
-			}
-		}
-	}
+      if (!takens[to] && fromRests[to] == 0) {
+        take(fromRests, toLists, takens, to);
+      }
+    }
+  }
 }
