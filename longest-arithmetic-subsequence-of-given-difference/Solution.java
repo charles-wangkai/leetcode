174dbcfd70a0e4eb7717bfdaa4b0fa1f87ce5b79
Kai Wang <charles.wangkai@gmail.com>
2023-07-14 08:18:14 +0800

    Longest Arithmetic Subsequence of Given Difference

--- longest-arithmetic-subsequence-of-given-difference/Solution.java
+++ longest-arithmetic-subsequence-of-given-difference/Solution.java
@@ -1,13 +1,13 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int longestSubsequence(int[] arr, int difference) {
-		Map<Integer, Integer> lastNumberToLength = new HashMap<>();
-		for (int element : arr) {
-			lastNumberToLength.put(element, lastNumberToLength.getOrDefault(element - difference, 0) + 1);
-		}
+class Solution {
+  public int longestSubsequence(int[] arr, int difference) {
+    Map<Integer, Integer> lastNumberToLength = new HashMap<>();
+    for (int value : arr) {
+      lastNumberToLength.put(value, lastNumberToLength.getOrDefault(value - difference, 0) + 1);
+    }
 
-		return lastNumberToLength.values().stream().mapToInt(x -> x).max().getAsInt();
-	}
+    return lastNumberToLength.values().stream().mapToInt(Integer::intValue).max().getAsInt();
+  }
 }
