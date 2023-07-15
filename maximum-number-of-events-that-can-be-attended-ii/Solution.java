70151957332f7804fa6374f4ea9ebf07d5e6684f
Kai Wang <charles.wangkai@gmail.com>
2023-07-15 09:56:19 +0800

    Maximum Number of Events That Can Be Attended II

--- maximum-number-of-events-that-can-be-attended-ii/Solution.java
+++ maximum-number-of-events-that-can-be-attended-ii/Solution.java
@@ -7,33 +7,31 @@ class Solution {
   public int maxValue(int[][] events, int k) {
     int[] days =
         Arrays.stream(events)
-            .flatMapToInt(e -> IntStream.of(e[0], e[1]))
+            .flatMapToInt(event -> IntStream.of(event[0], event[1]))
             .distinct()
             .sorted()
             .toArray();
     Map<Integer, Integer> dayToSequence =
         IntStream.range(0, days.length).boxed().collect(Collectors.toMap(i -> days[i], i -> i + 1));
 
-    int[] maxValueSums = new int[days.length + 1];
+    int[] dp = new int[days.length + 1];
     for (int i = 0; i < k; ++i) {
-      int[] nextSums = Arrays.copyOf(maxValueSums, maxValueSums.length);
+      int[] nextSums = dp.clone();
       for (int[] event : events) {
         nextSums[dayToSequence.get(event[1])] =
             Math.max(
                 nextSums[dayToSequence.get(event[1])],
-                maxValueSums[dayToSequence.get(event[0]) - 1] + event[2]);
+                dp[dayToSequence.get(event[0]) - 1] + event[2]);
       }
 
-      int nextValueSum = 0;
-      int[] nextValueSums = new int[maxValueSums.length];
-      for (int j = 0; j < nextValueSums.length; ++j) {
-        nextValueSum = Math.max(nextValueSum, nextSums[j]);
-        nextValueSums[j] = nextValueSum;
+      int[] nextDp = new int[dp.length];
+      for (int j = 0; j < nextDp.length; ++j) {
+        nextDp[j] = Math.max((j == 0) ? 0 : nextDp[j - 1], nextSums[j]);
       }
 
-      maxValueSums = nextValueSums;
+      dp = nextDp;
     }
 
-    return maxValueSums[maxValueSums.length - 1];
+    return dp[dp.length - 1];
   }
 }
