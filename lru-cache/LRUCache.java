40caf98cba4fed2487224bd518a81aa1cdd72f71
Kai Wang <charles.wangkai@gmail.com>
2023-07-18 09:47:22 +0800

    LRU Cache

--- lru-cache/LRUCache.java
+++ lru-cache/LRUCache.java
@@ -1,98 +1,98 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class LRUCache {
-	private int capacity;
-	private Map<Integer, Integer> cache = new HashMap<>();
-	private LinkList<Integer> keys = new LinkList<>();
-	private Map<Integer, Node<Integer>> keyToNode = new HashMap<>();
+class LRUCache {
+  private int capacity;
+  private Map<Integer, Integer> cache = new HashMap<>();
+  private LinkList<Integer> keys = new LinkList<>();
+  private Map<Integer, Node<Integer>> keyToNode = new HashMap<>();
 
-	public LRUCache(int capacity) {
-		this.capacity = capacity;
-	}
+  public LRUCache(int capacity) {
+    this.capacity = capacity;
+  }
 
-	public int get(int key) {
-		if (!cache.containsKey(key)) {
-			return -1;
-		}
+  public int get(int key) {
+    if (!cache.containsKey(key)) {
+      return -1;
+    }
 
-		keys.moveToHead(keyToNode.get(key));
+    keys.moveToHead(keyToNode.get(key));
 
-		return cache.get(key);
-	}
+    return cache.get(key);
+  }
 
-	public void put(int key, int value) {
-		if (cache.containsKey(key)) {
-			keys.moveToHead(keyToNode.get(key));
-		} else {
-			if (cache.size() == capacity) {
-				Node<Integer> tail = keys.tail;
-				keys.remove(tail);
-				keyToNode.remove(tail.value);
-				cache.remove(tail.value);
-			}
+  public void put(int key, int value) {
+    if (cache.containsKey(key)) {
+      keys.moveToHead(keyToNode.get(key));
+    } else {
+      if (cache.size() == capacity) {
+        Node<Integer> tail = keys.tail;
+        keys.remove(tail);
+        keyToNode.remove(tail.value);
+        cache.remove(tail.value);
+      }
 
-			Node<Integer> node = new Node<>(key);
-			keys.insertToHead(node);
-			keyToNode.put(key, node);
-		}
+      Node<Integer> node = new Node<>(key);
+      keys.insertToHead(node);
+      keyToNode.put(key, node);
+    }
 
-		cache.put(key, value);
-	}
+    cache.put(key, value);
+  }
 }
 
 class Node<T> {
-	T value;
-	Node<T> prev;
-	Node<T> next;
+  T value;
+  Node<T> prev;
+  Node<T> next;
 
-	Node(T value) {
-		this.value = value;
-	}
+  Node(T value) {
+    this.value = value;
+  }
 }
 
 class LinkList<T> {
-	Node<T> head;
-	Node<T> tail;
+  Node<T> head;
+  Node<T> tail;
 
-	void remove(Node<T> node) {
-		if (node != head && node != tail) {
-			node.prev.next = node.next;
-			node.next.prev = node.prev;
-		} else {
-			if (node == head) {
-				head = node.next;
-				if (head != null) {
-					head.prev = null;
-				}
-			}
+  void remove(Node<T> node) {
+    if (node != head && node != tail) {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    } else {
+      if (node == head) {
+        head = node.next;
+        if (head != null) {
+          head.prev = null;
+        }
+      }
 
-			if (node == tail) {
-				tail = node.prev;
-				if (tail != null) {
-					tail.next = null;
-				}
-			}
-		}
-	}
+      if (node == tail) {
+        tail = node.prev;
+        if (tail != null) {
+          tail.next = null;
+        }
+      }
+    }
+  }
 
-	void insertToHead(Node<T> node) {
-		node.next = head;
-		node.prev = null;
-		
-		if (head == null) {
-			tail = node;
-		} else {
-			head.prev = node;
-		}
+  void insertToHead(Node<T> node) {
+    node.next = head;
+    node.prev = null;
 
-		head = node;
-	}
+    if (head == null) {
+      tail = node;
+    } else {
+      head.prev = node;
+    }
 
-	void moveToHead(Node<T> node) {
-		remove(node);
-		insertToHead(node);
-	}
+    head = node;
+  }
+
+  void moveToHead(Node<T> node) {
+    remove(node);
+    insertToHead(node);
+  }
 }
 
 // Your LRUCache object will be instantiated and called as such:
