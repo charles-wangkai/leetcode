117905f17d7dc88b7d10fee642b65bbc7c5867ed
Kai Wang <charles.wangkai@gmail.com>
2023-07-23 17:15:34 +0800

    All Possible Full Binary Trees

--- all-possible-full-binary-trees/Solution.java
+++ all-possible-full-binary-trees/Solution.java
@@ -1,44 +1,48 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public List<TreeNode> allPossibleFBT(int N) {
-		if (N % 2 == 0) {
-			return Collections.emptyList();
-		}
+class Solution {
+  public List<TreeNode> allPossibleFBT(int n) {
+    if (n % 2 == 0) {
+      return List.of();
+    }
 
-		if (N == 1) {
-			return Collections.singletonList(new TreeNode(0));
-		}
+    if (n == 1) {
+      return List.of(new TreeNode());
+    }
 
-		List<TreeNode> result = new ArrayList<>();
-		for (int left = 1; left < N - 1; left += 2) {
-			int right = N - 1 - left;
+    List<TreeNode> result = new ArrayList<>();
+    for (int left = 1; left < n - 1; left += 2) {
+      int right = n - 1 - left;
 
-			List<TreeNode> leftSubTrees = allPossibleFBT(left);
-			List<TreeNode> rightSubTrees = allPossibleFBT(right);
-			for (TreeNode leftSubTree : leftSubTrees) {
-				for (TreeNode rightSubTree : rightSubTrees) {
-					TreeNode root = new TreeNode(0);
-					root.left = leftSubTree;
-					root.right = rightSubTree;
+      List<TreeNode> leftSubTrees = allPossibleFBT(left);
+      List<TreeNode> rightSubTrees = allPossibleFBT(right);
+      for (TreeNode leftSubTree : leftSubTrees) {
+        for (TreeNode rightSubTree : rightSubTrees) {
+          result.add(new TreeNode(0, leftSubTree, rightSubTree));
+        }
+      }
+    }
 
-					result.add(root);
-				}
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 }
