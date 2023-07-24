cf008b39cd0733c7eb18f5ea92e61bad4aef3902
Kai Wang <charles.wangkai@gmail.com>
2023-07-24 09:39:31 +0800

    Pow(x, n)

--- powx-n/Solution.java
+++ powx-n/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
-	public double myPow(double x, int n) {
-		long exponent = n;
-		if (n < 0) {
-			x = 1 / x;
-			exponent *= -1;
-		}
+  public double myPow(double x, int n) {
+    long exponent = n;
+    if (exponent < 0) {
+      x = 1 / x;
+      exponent *= -1;
+    }
 
-		double result = 1;
-		while (exponent != 0) {
-			if ((exponent & 1) != 0) {
-				result *= x;
-			}
+    double result = 1;
+    while (exponent != 0) {
+      if ((exponent & 1) == 1) {
+        result *= x;
+      }
 
-			x *= x;
-			exponent >>= 1;
-		}
+      x *= x;
+      exponent >>= 1;
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
