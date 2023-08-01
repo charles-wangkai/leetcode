519c35ce1aa611dfc05ecfad86117897bb3197cf
Kai Wang <charles.wangkai@gmail.com>
2023-08-01 08:52:17 +0800

    Combinations

--- combinations/Solution.java
+++ combinations/Solution.java
@@ -1,24 +1,17 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class Solution {
+class Solution {
   public List<List<Integer>> combine(int n, int k) {
-    List<List<Integer>> result = new ArrayList<>();
-    search(result, n, k, new ArrayList<>(), 1);
-    return result;
-  }
-
-  void search(List<List<Integer>> result, int n, int k, List<Integer> combination, int number) {
-    if (combination.size() == k) {
-      result.add(new ArrayList<>(combination));
-      return;
-    }
-    if (combination.size() + (n - number + 1) < k) {
-      return;
-    }
-    search(result, n, k, combination, number + 1);
-    combination.add(number);
-    search(result, n, k, combination, number + 1);
-    combination.remove(combination.size() - 1);
+    return IntStream.range(0, 1 << n)
+        .filter(mask -> Integer.bitCount(mask) == k)
+        .mapToObj(
+            mask ->
+                IntStream.range(0, n)
+                    .filter(i -> ((mask >> i) & 1) == 1)
+                    .map(i -> i + 1)
+                    .boxed()
+                    .toList())
+        .toList();
   }
 }
