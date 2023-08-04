fb00fa6bce51e649e39f77c1a1841a2db3199fb9
Kai Wang <charles.wangkai@gmail.com>
2023-08-04 09:50:58 +0800

    Word Break

--- word-break/Solution.java
+++ word-break/Solution.java
@@ -1,20 +1,20 @@
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 class Solution {
-	public boolean wordBreak(String s, List<String> wordDict) {
-		Set<String> words = new HashSet<>(wordDict);
+  public boolean wordBreak(String s, List<String> wordDict) {
+    Set<String> words = Set.copyOf(wordDict);
 
-		boolean[] separables = new boolean[s.length() + 1];
-		separables[0] = true;
-		for (int i = 0; i < s.length(); ++i) {
-			int i_ = i;
-			separables[i + 1] = IntStream.rangeClosed(0, i)
-					.anyMatch(j -> separables[j] && words.contains(s.substring(j, i_ + 1)));
-		}
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[0] = true;
+    for (int i = 0; i < s.length(); ++i) {
+      int i_ = i;
+      dp[i + 1] =
+          IntStream.rangeClosed(0, i)
+              .anyMatch(j -> dp[j] && words.contains(s.substring(j, i_ + 1)));
+    }
 
-		return separables[s.length()];
-	}
+    return dp[s.length()];
+  }
 }
