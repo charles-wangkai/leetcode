d85044b87f2c46d3afde0f0e18540af5f1c8eed9
Kai Wang <charles.wangkai@gmail.com>
2023-08-08 10:34:27 +0800

    Search in Rotated Sorted Array

--- search-in-rotated-sorted-array/Solution.java
+++ search-in-rotated-sorted-array/Solution.java
@@ -1,26 +1,28 @@
-public class Solution {
-    public int search(int[] A, int target) {
-        int lower = 0;
-        int upper = A.length - 1;
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
-            if (A[middle] == target) {
-                return middle;
-            } else if (A[lower] <= A[middle]) {
-                if (target >= A[lower] && target < A[middle]) {
-                    upper = middle - 1;
-                } else {
-                    lower = middle + 1;
-                }
-            } else {
-                if (target > A[middle] && target <= A[upper]) {
-                    lower = middle + 1;
-                } else {
-                    upper = middle - 1;
-                }
-            }
-        }
+class Solution {
+  public int search(int[] nums, int target) {
+    int lower = 0;
+    int upper = nums.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (nums[middle] == target) {
+        return middle;
+      }
 
-        return -1;
+      if (nums[lower] <= nums[middle]) {
+        if (target >= nums[lower] && target < nums[middle]) {
+          upper = middle - 1;
+        } else {
+          lower = middle + 1;
+        }
+      } else {
+        if (target > nums[middle] && target <= nums[upper]) {
+          lower = middle + 1;
+        } else {
+          upper = middle - 1;
+        }
+      }
     }
+
+    return -1;
+  }
 }
