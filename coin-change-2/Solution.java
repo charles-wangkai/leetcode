04f104b0a66187f546dfe5aae9d58f80b39be4f8
Kai Wang <charles.wangkai@gmail.com>
2023-08-11 08:32:56 +0800

    Coin Change II

--- coin-change-2/Solution.java
+++ coin-change-2/Solution.java
@@ -1,20 +1,13 @@
-public class Solution {
-	public int change(int amount, int[] coins) {
-		if (coins.length == 0) {
-			return amount == 0 ? 1 : 0;
-		}
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+    for (int coin : coins) {
+      for (int i = coin; i < dp.length; ++i) {
+        dp[i] += dp[i - coin];
+      }
+    }
 
-		int[][] ways = new int[amount + 1][coins.length];
-		for (int i = 0; i <= amount; i++) {
-			for (int j = 0; j < coins.length; j++) {
-				if (i == 0 && j == 0) {
-					ways[i][j] = 1;
-				} else {
-					ways[i][j] = (j == 0 ? 0 : ways[i][j - 1]) + (i >= coins[j] ? ways[i - coins[j]][j] : 0);
-				}
-			}
-		}
-
-		return ways[amount][coins.length - 1];
-	}
+    return dp[amount];
+  }
 }
