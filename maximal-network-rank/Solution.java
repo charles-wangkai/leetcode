19cbeffcd44fbcb35b37f5802b531ab1a1d715ce
Kai Wang <charles.wangkai@gmail.com>
2023-08-18 09:23:46 +0800

    Maximal Network Rank

--- maximal-network-rank/Solution.java
+++ maximal-network-rank/Solution.java
@@ -12,10 +12,8 @@ class Solution {
 
     int result = 0;
     for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        if (i != j) {
-          result = Math.max(result, counts[i] + counts[j] - connections[i][j]);
-        }
+      for (int j = i + 1; j < n; ++j) {
+        result = Math.max(result, counts[i] + counts[j] - connections[i][j]);
       }
     }
 
