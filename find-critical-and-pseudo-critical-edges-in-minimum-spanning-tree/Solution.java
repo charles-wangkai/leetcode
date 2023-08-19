319089cabdf86e016d6106e9fda7e9c0771f1813
Kai Wang <charles.wangkai@gmail.com>
2023-08-19 09:44:26 +0800

    Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree

--- find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/Solution.java
+++ find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/Solution.java
@@ -1,84 +1,80 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
-    public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
-        int mstWeight = computeMSTWeight(n, edges, -1, -1);
+  public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
+    int mstWeight = computeMSTWeight(n, edges, -1, -1);
 
-        List<Integer> criticals = new ArrayList<>();
-        List<Integer> pseudos = new ArrayList<>();
-        for (int i = 0; i < edges.length; ++i) {
-            if (isCritical(n, edges, mstWeight, i)) {
-                criticals.add(i);
-            } else if (isPseudo(n, edges, mstWeight, i)) {
-                pseudos.add(i);
-            }
-        }
-
-        return Arrays.asList(criticals, pseudos);
+    List<Integer> criticals = new ArrayList<>();
+    List<Integer> pseudos = new ArrayList<>();
+    for (int i = 0; i < edges.length; ++i) {
+      if (isCritical(n, edges, mstWeight, i)) {
+        criticals.add(i);
+      } else if (isPseudo(n, edges, mstWeight, i)) {
+        pseudos.add(i);
+      }
     }
 
-    Integer computeMSTWeight(int n, int[][] edges, int removedEdgeIndex, int requiredEdgeIndex) {
-        int[] sortedIndices = IntStream.range(0, edges.length).filter(i -> i != removedEdgeIndex).boxed()
-                .sorted((i1, i2) -> Integer.compare(edges[i1][2], edges[i2][2])).mapToInt(x -> x).toArray();
+    return List.of(criticals, pseudos);
+  }
 
-        int[] parents = new int[n];
-        Arrays.fill(parents, -1);
+  Integer computeMSTWeight(int n, int[][] edges, int removedEdgeIndex, int requiredEdgeIndex) {
+    int[] sortedIndices =
+        IntStream.range(0, edges.length)
+            .filter(i -> i != removedEdgeIndex)
+            .boxed()
+            .sorted(Comparator.comparing(i -> edges[i][2]))
+            .mapToInt(Integer::intValue)
+            .toArray();
 
-        int result = 0;
+    int[] parents = new int[n];
+    Arrays.fill(parents, -1);
 
-        if (requiredEdgeIndex != -1) {
-            result += union(parents, edges[requiredEdgeIndex]);
-        }
-
-        for (int sortedIndex : sortedIndices) {
-            result += union(parents, edges[sortedIndex]);
-        }
-
-        return result;
+    int result = 0;
+    if (requiredEdgeIndex != -1) {
+      result += union(parents, edges[requiredEdgeIndex]);
+    }
+    for (int sortedIndex : sortedIndices) {
+      result += union(parents, edges[sortedIndex]);
     }
 
-    int union(int[] parents, int[] edge) {
-        int root1 = findRoot(parents, edge[0]);
-        int root2 = findRoot(parents, edge[1]);
+    return (IntStream.range(0, n).map(i -> findRoot(parents, i)).distinct().count() == 1)
+        ? result
+        : null;
+  }
 
-        if (root1 == root2) {
-            return 0;
-        }
-
-        parents[root2] = root1;
-
-        return edge[2];
+  int union(int[] parents, int[] edge) {
+    int root1 = findRoot(parents, edge[0]);
+    int root2 = findRoot(parents, edge[1]);
+    if (root1 == root2) {
+      return 0;
     }
 
-    int findRoot(int[] parents, int node) {
-        int root = node;
-        while (parents[root] != -1) {
-            root = parents[root];
-        }
+    parents[root2] = root1;
 
-        int p = node;
-        while (p != root) {
-            int next = parents[p];
-            parents[p] = root;
+    return edge[2];
+  }
 
-            p = next;
-        }
-
-        return root;
+  int findRoot(int[] parents, int node) {
+    if (parents[node] == -1) {
+      return node;
     }
 
-    boolean isCritical(int n, int[][] edges, int mstWeight, int edgeIndex) {
-        Integer weight = computeMSTWeight(n, edges, edgeIndex, -1);
+    parents[node] = findRoot(parents, parents[node]);
 
-        return weight == null || weight != mstWeight;
-    }
+    return parents[node];
+  }
 
-    boolean isPseudo(int n, int[][] edges, int mstWeight, int edgeIndex) {
-        Integer weight = computeMSTWeight(n, edges, edgeIndex, edgeIndex);
+  boolean isCritical(int n, int[][] edges, int mstWeight, int edgeIndex) {
+    Integer weight = computeMSTWeight(n, edges, edgeIndex, -1);
 
-        return weight != null && weight == mstWeight;
-    }
+    return weight == null || weight != mstWeight;
+  }
+
+  boolean isPseudo(int n, int[][] edges, int mstWeight, int edgeIndex) {
+    return computeMSTWeight(n, edges, -1, edgeIndex) == mstWeight;
+  }
 }
