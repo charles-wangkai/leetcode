65bcde39082d87af5282de0f2d25e0c39dbc91c3
Kai Wang <charles.wangkai@gmail.com>
2023-08-20 09:41:23 +0800

    Sort Items by Groups Respecting Dependencies

--- sort-items-by-groups-respecting-dependencies/Solution.java
+++ sort-items-by-groups-respecting-dependencies/Solution.java
@@ -1,113 +1,106 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-public class Solution {
-	public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
-		int groupNum = m;
-		for (int i = 0; i < n; i++) {
-			if (group[i] == -1) {
-				group[i] = groupNum;
-				groupNum++;
-			}
-		}
+class Solution {
+  public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+    int groupNum = m;
+    for (int i = 0; i < group.length; ++i) {
+      if (group[i] == -1) {
+        group[i] = groupNum;
+        ++groupNum;
+      }
+    }
 
-		Graph groupGraph = new Graph();
-		for (int g : group) {
-			groupGraph.addNode(g);
-		}
+    Graph groupGraph = new Graph();
+    for (int g : group) {
+      groupGraph.addNode(g);
+    }
 
-		Map<Integer, Graph> groupToItemGraph = new HashMap<>();
-		for (int g : groupGraph.nodeToAdjacents.keySet()) {
-			groupToItemGraph.put(g, new Graph());
-		}
-		for (int i = 0; i < n; i++) {
-			groupToItemGraph.get(group[i]).addNode(i);
-		}
+    Map<Integer, Graph> groupToItemGraph = new HashMap<>();
+    for (int g : groupGraph.nodeToAdjs.keySet()) {
+      groupToItemGraph.put(g, new Graph());
+    }
+    for (int i = 0; i < group.length; ++i) {
+      groupToItemGraph.get(group[i]).addNode(i);
+    }
 
-		for (int i = 0; i < n; i++) {
-			for (int beforeItem : beforeItems.get(i)) {
-				if (group[i] == group[beforeItem]) {
-					groupToItemGraph.get(group[i]).addEdge(beforeItem, i);
-				} else {
-					groupGraph.addEdge(group[beforeItem], group[i]);
-				}
-			}
-		}
+    for (int i = 0; i < beforeItems.size(); ++i) {
+      for (int beforeItem : beforeItems.get(i)) {
+        if (group[i] == group[beforeItem]) {
+          groupToItemGraph.get(group[i]).addEdge(beforeItem, i);
+        } else {
+          groupGraph.addEdge(group[beforeItem], group[i]);
+        }
+      }
+    }
 
-		List<Integer> sortedGroups = groupGraph.topologicalSort();
-		if (sortedGroups == null) {
-			return new int[0];
-		}
+    List<Integer> sortedGroups = groupGraph.topologicalSort();
+    if (sortedGroups == null) {
+      return new int[0];
+    }
 
-		List<Integer> result = new ArrayList<>();
-		for (int g : sortedGroups) {
-			List<Integer> sortedItems = groupToItemGraph.get(g).topologicalSort();
-			if (sortedItems == null) {
-				return new int[0];
-			}
+    List<Integer> result = new ArrayList<>();
+    for (int g : sortedGroups) {
+      List<Integer> sortedItems = groupToItemGraph.get(g).topologicalSort();
+      if (sortedItems == null) {
+        return new int[0];
+      }
 
-			result.addAll(sortedItems);
-		}
+      result.addAll(sortedItems);
+    }
 
-		return result.stream().mapToInt(x -> x).toArray();
-	}
+    return result.stream().mapToInt(Integer::intValue).toArray();
+  }
 }
 
 class Graph {
-	Map<Integer, List<Integer>> nodeToAdjacents = new HashMap<>();
+  Map<Integer, List<Integer>> nodeToAdjs = new HashMap<>();
 
-	void addNode(int node) {
-		if (!nodeToAdjacents.containsKey(node)) {
-			nodeToAdjacents.put(node, new ArrayList<>());
-		}
-	}
+  void addNode(int node) {
+    nodeToAdjs.putIfAbsent(node, new ArrayList<>());
+  }
 
-	void addEdge(int from, int to) {
-		nodeToAdjacents.get(from).add(to);
-	}
+  void addEdge(int from, int to) {
+    nodeToAdjs.get(from).add(to);
+  }
 
-	List<Integer> topologicalSort() {
-		Map<Integer, Integer> nodeToInDegree = new HashMap<>();
-		for (int node : nodeToAdjacents.keySet()) {
-			nodeToInDegree.put(node, 0);
-		}
+  List<Integer> topologicalSort() {
+    Map<Integer, Integer> nodeToInDegree = new HashMap<>();
+    for (int node : nodeToAdjs.keySet()) {
+      nodeToInDegree.put(node, 0);
+    }
 
-		for (List<Integer> adjacents : nodeToAdjacents.values()) {
-			for (int adjacent : adjacents) {
-				nodeToInDegree.put(adjacent, nodeToInDegree.get(adjacent) + 1);
-			}
-		}
+    for (List<Integer> adjs : nodeToAdjs.values()) {
+      for (int adj : adjs) {
+        nodeToInDegree.put(adj, nodeToInDegree.get(adj) + 1);
+      }
+    }
 
-		List<Integer> result = new ArrayList<>();
-		Queue<Integer> queue = new LinkedList<>();
-		for (int node : nodeToInDegree.keySet()) {
-			if (nodeToInDegree.get(node) == 0) {
-				queue.offer(node);
-			}
-		}
+    List<Integer> result = new ArrayList<>();
+    Queue<Integer> queue = new ArrayDeque<>();
+    for (int node : nodeToInDegree.keySet()) {
+      if (nodeToInDegree.get(node) == 0) {
+        queue.offer(node);
+      }
+    }
 
-		while (!queue.isEmpty()) {
-			int head = queue.poll();
+    while (!queue.isEmpty()) {
+      int head = queue.poll();
 
-			result.add(head);
+      result.add(head);
 
-			for (int adjacent : nodeToAdjacents.get(head)) {
-				nodeToInDegree.put(adjacent, nodeToInDegree.get(adjacent) - 1);
+      for (int adj : nodeToAdjs.get(head)) {
+        nodeToInDegree.put(adj, nodeToInDegree.get(adj) - 1);
+        if (nodeToInDegree.get(adj) == 0) {
+          queue.offer(adj);
+        }
+      }
+    }
 
-				if (nodeToInDegree.get(adjacent) == 0) {
-					queue.offer(adjacent);
-				}
-			}
-		}
-
-		if (result.size() != nodeToInDegree.size()) {
-			return null;
-		}
-
-		return result;
-	}
+    return (result.size() == nodeToInDegree.size()) ? result : null;
+  }
 }
