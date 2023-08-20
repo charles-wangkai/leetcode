5bff9a47c5218582de3066bf016f23bb71a455cc
Kai Wang <charles.wangkai@gmail.com>
2023-08-20 11:23:45 +0800

    Count Pairs Whose Sum is Less than Target

---
+++ count-pairs-whose-sum-is-less-than-target/Solution.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+class Solution {
+  public int countPairs(List<Integer> nums, int target) {
+    int result = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      for (int j = i + 1; j < nums.size(); ++j) {
+        if (nums.get(i) + nums.get(j) < target) {
+          ++result;
+        }
+      }
+    }
+
+    return result;
+  }
+}
