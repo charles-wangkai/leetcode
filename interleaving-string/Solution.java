c1c0f235dbbe9e09b7ae39a6a97fa306c56dc8a4
Kai Wang <charles.wangkai@gmail.com>
2023-08-25 08:40:39 +0800

    Interleaving String

--- interleaving-string/Solution.java
+++ interleaving-string/Solution.java
@@ -4,16 +4,16 @@ class Solution {
       return false;
     }
 
-    boolean[][] interleaves = new boolean[s1.length() + 1][s2.length() + 1];
+    boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
     for (int i = 0; i <= s1.length(); ++i) {
       for (int j = 0; j <= s2.length(); ++j) {
-        interleaves[i][j] =
+        dp[i][j] =
             (i == 0 && j == 0)
-                || (i != 0 && s3.charAt(i + j - 1) == s1.charAt(i - 1) && interleaves[i - 1][j])
-                || (j != 0 && s3.charAt(i + j - 1) == s2.charAt(j - 1) && interleaves[i][j - 1]);
+                || (i != 0 && s3.charAt(i + j - 1) == s1.charAt(i - 1) && dp[i - 1][j])
+                || (j != 0 && s3.charAt(i + j - 1) == s2.charAt(j - 1) && dp[i][j - 1]);
       }
     }
 
-    return interleaves[s1.length()][s2.length()];
+    return dp[s1.length()][s2.length()];
   }
 }
