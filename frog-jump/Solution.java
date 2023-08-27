fb54b49a323fd4018b0a30787390f102ca3e0fea
Kai Wang <charles.wangkai@gmail.com>
2023-08-27 09:18:14 +0800

    Frog Jump

--- frog-jump/Solution.java
+++ frog-jump/Solution.java
@@ -3,34 +3,28 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class Solution {
+class Solution {
   public boolean canCross(int[] stones) {
-    Map<Integer, Set<Integer>> stone2units = new HashMap<>();
+    Map<Integer, Set<Integer>> stoneToLastJumps = new HashMap<>();
     for (int stone : stones) {
-      stone2units.put(stone, new HashSet<>());
+      stoneToLastJumps.put(stone, new HashSet<>());
     }
-    stone2units.get(0).add(0);
+    stoneToLastJumps.get(0).add(0);
 
-    for (int i = 0; i < stones.length - 1; i++) {
-      for (int unit : stone2units.get(stones[i])) {
-        for (int delta = -1; delta <= 1; delta++) {
-          int nextUnit = unit + delta;
-
-          if (nextUnit <= 0) {
-            continue;
+    for (int i = 0; i < stones.length; ++i) {
+      for (int lastJump : stoneToLastJumps.get(stones[i])) {
+        for (int delta = -1; delta <= 1; ++delta) {
+          int jump = lastJump + delta;
+          if (jump > 0) {
+            int nextStone = stones[i] + jump;
+            if (stoneToLastJumps.containsKey(nextStone)) {
+              stoneToLastJumps.get(nextStone).add(jump);
+            }
           }
-
-          int nextStone = stones[i] + nextUnit;
-
-          if (!stone2units.containsKey(nextStone)) {
-            continue;
-          }
-
-          stone2units.get(nextStone).add(nextUnit);
         }
       }
     }
 
-    return !stone2units.get(stones[stones.length - 1]).isEmpty();
+    return !stoneToLastJumps.get(stones[stones.length - 1]).isEmpty();
   }
 }
