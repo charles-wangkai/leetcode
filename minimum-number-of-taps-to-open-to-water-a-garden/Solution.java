778c16f6cbd69c0ef6ccab9780c102b40731aaf8
Kai Wang <charles.wangkai@gmail.com>
2023-08-31 11:04:02 +0800

    Minimum Number of Taps to Open to Water a Garden

--- minimum-number-of-taps-to-open-to-water-a-garden/Solution.java
+++ minimum-number-of-taps-to-open-to-water-a-garden/Solution.java
@@ -1,46 +1,38 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int minTaps(int n, int[] ranges) {
-		Interval[] intervals = IntStream.range(0, ranges.length)
-				.mapToObj(i -> new Interval(i - ranges[i], i + ranges[i]))
-				.sorted((i1, i2) -> Integer.compare(i1.left, i2.left)).toArray(Interval[]::new);
+class Solution {
+  public int minTaps(int n, int[] ranges) {
+    Interval[] intervals =
+        IntStream.range(0, ranges.length)
+            .mapToObj(i -> new Interval(i - ranges[i], i + ranges[i]))
+            .sorted(Comparator.comparing(Interval::left))
+            .toArray(Interval[]::new);
 
-		int maxRight = 0;
-		int index = 0;
-		int result = 0;
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-		while (maxRight < n) {
-			while (index != intervals.length && intervals[index].left <= maxRight) {
-				pq.offer(intervals[index].right);
-				++index;
-			}
+    int maxRight = 0;
+    int nextMaxRight = 0;
+    int index = 0;
+    int result = 0;
+    while (maxRight < n) {
+      while (index != intervals.length && intervals[index].left() <= maxRight) {
+        nextMaxRight = Math.max(nextMaxRight, intervals[index].right());
+        ++index;
+      }
 
-			if (pq.isEmpty() || pq.peek() <= maxRight) {
-				break;
-			}
+      if (nextMaxRight <= maxRight) {
+        break;
+      }
 
-			++result;
-			maxRight = pq.poll();
-			pq.clear();
-		}
+      ++result;
+      maxRight = nextMaxRight;
+    }
 
-		if (maxRight < n) {
-			return -1;
-		}
+    if (maxRight < n) {
+      return -1;
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
 
-class Interval {
-	int left;
-	int right;
-
-	Interval(int left, int right) {
-		this.left = left;
-		this.right = right;
-	}
-}
+record Interval(int left, int right) {}
