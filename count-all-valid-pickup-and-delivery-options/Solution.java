5893974057793d0c8e84d0a3d8ee920d37c52173
Kai Wang <charles.wangkai@gmail.com>
2023-09-10 11:50:55 +0800

    Count All Valid Pickup and Delivery Options

--- count-all-valid-pickup-and-delivery-options/Solution.java
+++ count-all-valid-pickup-and-delivery-options/Solution.java
@@ -1,10 +1,12 @@
 import java.util.stream.IntStream;
 
 class Solution {
+  static final int MODULUS = 1_000_000_007;
+
   public int countOrders(int n) {
     return IntStream.rangeClosed(1, n)
         .map(i -> i * (2 * i - 1))
-        .reduce((x, y) -> (int) ((long) x * y % 1_000_000_007))
+        .reduce((x, y) -> Math.floorMod((long) x * y, MODULUS))
         .getAsInt();
   }
 }
