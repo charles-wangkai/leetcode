5c2ae5151aeb57a63b05df051e8e22c6d8415a82
Kai Wang <charles.wangkai@gmail.com>
2023-09-13 08:55:56 +0800

    Candy

--- candy/Solution.java
+++ candy/Solution.java
@@ -6,20 +6,13 @@ class Solution {
 
     int[] leftMins = new int[n];
     for (int i = 0; i < leftMins.length; ++i) {
-      if (i != 0 && ratings[i] > ratings[i - 1]) {
-        leftMins[i] = leftMins[i - 1] + 1;
-      } else {
-        leftMins[i] = 1;
-      }
+      leftMins[i] = (i == 0 || ratings[i] <= ratings[i - 1]) ? 1 : (leftMins[i - 1] + 1);
     }
 
     int[] rightMins = new int[n];
     for (int i = rightMins.length - 1; i >= 0; --i) {
-      if (i != rightMins.length - 1 && ratings[i] > ratings[i + 1]) {
-        rightMins[i] = rightMins[i + 1] + 1;
-      } else {
-        rightMins[i] = 1;
-      }
+      rightMins[i] =
+          (i == rightMins.length - 1 || ratings[i] <= ratings[i + 1]) ? 1 : (rightMins[i + 1] + 1);
     }
 
     return IntStream.range(0, n).map(i -> Math.max(leftMins[i], rightMins[i])).sum();
