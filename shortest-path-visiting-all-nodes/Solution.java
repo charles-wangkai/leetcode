3968485e1c497c97af696b129450cece2040ccf7
Kai Wang <charles.wangkai@gmail.com>
2023-09-17 08:30:32 +0800

    Shortest Path Visiting All Nodes

--- shortest-path-visiting-all-nodes/Solution.java
+++ shortest-path-visiting-all-nodes/Solution.java
@@ -7,42 +7,34 @@ class Solution {
   public int shortestPathLength(int[][] graph) {
     int n = graph.length;
 
-    int[][] steps = new int[1 << n][n];
-    for (int i = 0; i < steps.length; ++i) {
-      Arrays.fill(steps[i], Integer.MAX_VALUE);
+    int[][] dp = new int[1 << n][n];
+    for (int i = 0; i < dp.length; ++i) {
+      Arrays.fill(dp[i], Integer.MAX_VALUE);
     }
 
     Queue<State> queue = new ArrayDeque<>();
     for (int i = 0; i < n; ++i) {
       State state = new State(1 << i, i);
 
-      steps[state.mask][state.currentNode] = 0;
+      dp[state.mask()][state.currentNode()] = 0;
       queue.offer(state);
     }
 
     while (!queue.isEmpty()) {
       State state = queue.poll();
 
-      for (int nextNode : graph[state.currentNode]) {
-        int nextMask = state.mask | (1 << nextNode);
+      for (int nextNode : graph[state.currentNode()]) {
+        int nextMask = state.mask() | (1 << nextNode);
 
-        if (steps[nextMask][nextNode] == Integer.MAX_VALUE) {
-          steps[nextMask][nextNode] = steps[state.mask][state.currentNode] + 1;
+        if (dp[nextMask][nextNode] == Integer.MAX_VALUE) {
+          dp[nextMask][nextNode] = dp[state.mask()][state.currentNode()] + 1;
           queue.offer(new State(nextMask, nextNode));
         }
       }
     }
 
-    return IntStream.range(0, n).map(i -> steps[(1 << n) - 1][i]).min().getAsInt();
+    return IntStream.range(0, n).map(i -> dp[(1 << n) - 1][i]).min().getAsInt();
   }
 }
 
-class State {
-  int mask;
-  int currentNode;
-
-  State(int mask, int currentNode) {
-    this.mask = mask;
-    this.currentNode = currentNode;
-  }
-}
+record State(int mask, int currentNode) {}
