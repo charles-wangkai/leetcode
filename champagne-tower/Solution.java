543368e48f784a333c049f1e0519c77a9ba8fc5b
Kai Wang <charles.wangkai@gmail.com>
2023-09-24 08:48:58 +0800

    Champagne Tower

--- champagne-tower/Solution.java
+++ champagne-tower/Solution.java
@@ -3,7 +3,6 @@ class Solution {
     double[] row = {poured};
     while (row.length < query_row + 1) {
       double[] nextRow = new double[row.length + 1];
-
       for (int i = 0; i < row.length; ++i) {
         nextRow[i] += Math.max(0, row[i] - 1) / 2;
         nextRow[i + 1] += Math.max(0, row[i] - 1) / 2;
