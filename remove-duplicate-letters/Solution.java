7a09fbb03c35cb762baa0c7f346b4d57a42f19f2
Kai Wang <charles.wangkai@gmail.com>
2023-09-26 08:59:10 +0800

    Remove Duplicate Letters

--- remove-duplicate-letters/Solution.java
+++ remove-duplicate-letters/Solution.java
@@ -1,48 +1,49 @@
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
-import java.util.Queue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
   public String removeDuplicateLetters(String s) {
     List<LetterState> letterStates =
-        IntStream.range(0, 26)
-            .mapToObj(i -> new LetterState((char) (i + 'a')))
+        IntStream.rangeClosed('a', 'z')
+            .mapToObj(c -> new LetterState((char) c, new ArrayDeque<>()))
             .collect(Collectors.toList());
 
     for (int i = 0; i < s.length(); ++i) {
-      letterStates.get(s.charAt(i) - 'a').appendIndex(i);
+      letterStates.get(s.charAt(i) - 'a').indices().offerLast(i);
     }
 
-    letterStates.removeIf(letterState -> letterState.indices.isEmpty());
+    letterStates.removeIf(letterState -> letterState.indices().isEmpty());
 
     StringBuilder result = new StringBuilder();
     while (!letterStates.isEmpty()) {
       int minLastIndex =
-          letterStates.stream().mapToInt(letterState -> letterState.lastIndex).min().getAsInt();
+          letterStates.stream()
+              .mapToInt(letterState -> letterState.indices().peekLast())
+              .min()
+              .getAsInt();
 
       char selectedLetter = 0;
       int selectedIndex = -1;
       for (LetterState letterState : letterStates) {
-        if (letterState.indices.peek() <= minLastIndex) {
-          selectedLetter = letterState.letter;
-          selectedIndex = letterState.indices.peek();
+        if (letterState.indices().peekFirst() <= minLastIndex) {
+          selectedLetter = letterState.letter();
+          selectedIndex = letterState.indices().peekFirst();
 
           break;
         }
       }
 
       char selectedLetter_ = selectedLetter;
-      letterStates.removeIf(letterState -> letterState.letter == selectedLetter_);
+      letterStates.removeIf(letterState -> letterState.letter() == selectedLetter_);
 
-      int selectedIndex_ = selectedIndex;
-      letterStates.forEach(
-          letterState -> {
-            while (letterState.indices.peek() <= selectedIndex_) {
-              letterState.indices.poll();
-            }
-          });
+      for (LetterState letterState : letterStates) {
+        while (letterState.indices().peekFirst() <= selectedIndex) {
+          letterState.indices().pollFirst();
+        }
+      }
 
       result.append(selectedLetter);
     }
@@ -51,17 +52,4 @@ class Solution {
   }
 }
 
-class LetterState {
-  char letter;
-  Queue<Integer> indices = new ArrayDeque<>();
-  int lastIndex = -1;
-
-  LetterState(char letter) {
-    this.letter = letter;
-  }
-
-  void appendIndex(int index) {
-    indices.offer(index);
-    lastIndex = index;
-  }
-}
+record LetterState(char letter, Deque<Integer> indices) {}
