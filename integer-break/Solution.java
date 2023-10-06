5717c677d528d27a818f0e400eb818be68094d4d
Kai Wang <charles.wangkai@gmail.com>
2023-10-06 08:48:20 +0800

    Integer Break

--- integer-break/Solution.java
+++ integer-break/Solution.java
@@ -1,24 +1,23 @@
-public class Solution {
-	public int integerBreak(int n) {
-		if (n == 2) {
-			return 1;
-		} else if (n == 3) {
-			return 2;
-		}
+import java.util.stream.IntStream;
 
-		int num2 = 0;
-		while ((n - num2 * 2) % 3 != 0) {
-			num2++;
-		}
-		int num3 = (n - num2 * 2) / 3;
-		return pow(2, num2) * pow(3, num3);
-	}
+class Solution {
+  public int integerBreak(int n) {
+    if (n == 2) {
+      return 1;
+    }
+    if (n == 3) {
+      return 2;
+    }
 
-	int pow(int base, int exponent) {
-		int result = 1;
-		for (int i = 0; i < exponent; i++) {
-			result *= base;
-		}
-		return result;
-	}
+    int num2 = 0;
+    while ((n - num2 * 2) % 3 != 0) {
+      ++num2;
+    }
+
+    return pow(2, num2) * pow(3, (n - num2 * 2) / 3);
+  }
+
+  int pow(int base, int exponent) {
+    return IntStream.range(0, exponent).reduce(1, (acc, x) -> acc * base);
+  }
 }
