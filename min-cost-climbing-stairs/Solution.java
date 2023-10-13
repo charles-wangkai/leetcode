9c3185408b441a4f42bc5948e3ba8d65c57e4a6e
Kai Wang <charles.wangkai@gmail.com>
2023-10-13 09:19:26 +0800

    Min Cost Climbing Stairs

--- min-cost-climbing-stairs/Solution.java
+++ min-cost-climbing-stairs/Solution.java
@@ -2,13 +2,13 @@ class Solution {
   public int minCostClimbingStairs(int[] cost) {
     int prev = 0;
     int curr = 0;
-    for (int i = 2; i <= cost.length; ++i) {
-      int next = Math.min(prev + cost[i - 2], curr + cost[i - 1]);
+    for (int i = 0; i < cost.length; ++i) {
+      int next = Math.min(prev, curr) + cost[i];
 
       prev = curr;
       curr = next;
     }
 
-    return curr;
+    return Math.min(prev, curr);
   }
 }
