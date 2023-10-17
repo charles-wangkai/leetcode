2367e27fcf2aea6de9c274d5360b7d03ddf80100
Kai Wang <charles.wangkai@gmail.com>
2023-10-17 11:13:10 +0800

    Validate Binary Tree Nodes

--- validate-binary-tree-nodes/Solution.java
+++ validate-binary-tree-nodes/Solution.java
@@ -1,13 +1,31 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        int[] counts = new int[n];
-        IntStream.concat(Arrays.stream(leftChild), Arrays.stream(rightChild)).filter(child -> child != -1)
-                .forEach(child -> ++counts[child]);
+class Solution {
+  public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+    int[] counts = new int[n];
+    IntStream.concat(Arrays.stream(leftChild), Arrays.stream(rightChild))
+        .filter(child -> child != -1)
+        .forEach(child -> ++counts[child]);
 
-        return Arrays.stream(counts).filter(count -> count == 0).count() == 1
-                && Arrays.stream(counts).filter(count -> count == 1).count() == n - 1;
+    if (Arrays.stream(counts).filter(count -> count == 0).count() != 1
+        || Arrays.stream(counts).filter(count -> count == 1).count() != n - 1) {
+      return false;
     }
+
+    int root = IntStream.range(0, counts.length).filter(i -> counts[i] == 0).findAny().getAsInt();
+    boolean[] visited = new boolean[n];
+    search(leftChild, rightChild, visited, root);
+
+    return IntStream.range(0, visited.length).allMatch(i -> visited[i]);
+  }
+
+  void search(int[] leftChild, int[] rightChild, boolean[] visited, int node) {
+    visited[node] = true;
+    for (int child : new int[] {leftChild[node], rightChild[node]}) {
+      if (child != -1) {
+        search(leftChild, rightChild, visited, child);
+      }
+    }
+  }
 }
