d9723888aec96bef1dbfe2f64508a5df7275e15c
Kai Wang <charles.wangkai@gmail.com>
2023-10-25 08:40:07 +0800

    K-th Symbol in Grammar

--- k-th-symbol-in-grammar/Solution.java
+++ k-th-symbol-in-grammar/Solution.java
@@ -1,26 +1,17 @@
-public class Solution {
-	public int kthGrammar(int N, int K) {
-		return search(K);
-	}
+class Solution {
+  public int kthGrammar(int n, int k) {
+    return search(k);
+  }
 
-	int search(int K) {
-		if (K == 1) {
-			return 0;
-		}
+  int search(int k) {
+    if (k == 1) {
+      return 0;
+    }
 
-		int prev = search((K - 1) / 2 + 1);
-		if (prev == 0) {
-			if (K % 2 != 0) {
-				return 0;
-			} else {
-				return 1;
-			}
-		} else {
-			if (K % 2 != 0) {
-				return 1;
-			} else {
-				return 0;
-			}
-		}
-	}
+    if (search((k - 1) / 2 + 1) == 0) {
+      return (k % 2 == 1) ? 0 : 1;
+    }
+
+    return (k % 2 == 1) ? 1 : 0;
+  }
 }
