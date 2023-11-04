451ecfa0bf1a314f6c5a72cf53cd9382ef113db7
Kai Wang <charles.wangkai@gmail.com>
2023-11-04 15:46:34 +0800

    Last Moment Before All Ants Fall Out of a Plank

--- last-moment-before-all-ants-fall-out-of-a-plank/Solution.java
+++ last-moment-before-all-ants-fall-out-of-a-plank/Solution.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int getLastMoment(int n, int[] left, int[] right) {
-        return IntStream.concat(Arrays.stream(left), Arrays.stream(right).map(r -> n - r)).max().getAsInt();
-    }
+  public int getLastMoment(int n, int[] left, int[] right) {
+    return IntStream.concat(Arrays.stream(left), Arrays.stream(right).map(r -> n - r))
+        .max()
+        .getAsInt();
+  }
 }
