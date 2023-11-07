b504dfb681d8f39c4ea2fc306dc7e55fb5b35506
Kai Wang <charles.wangkai@gmail.com>
2023-11-07 09:07:51 +0800

    Eliminate Maximum Number of Monsters

--- eliminate-maximum-number-of-monsters/Solution.java
+++ eliminate-maximum-number-of-monsters/Solution.java
@@ -3,12 +3,7 @@ import java.util.stream.IntStream;
 class Solution {
   public int eliminateMaximum(int[] dist, int[] speed) {
     int[] times =
-        IntStream.range(0, dist.length)
-            .map(i -> (dist[i] - 1) / speed[i])
-            .boxed()
-            .sorted()
-            .mapToInt(x -> x)
-            .toArray();
+        IntStream.range(0, dist.length).map(i -> (dist[i] - 1) / speed[i]).sorted().toArray();
 
     int result = 0;
     for (int i = 0; i < times.length && i <= times[i]; ++i) {
