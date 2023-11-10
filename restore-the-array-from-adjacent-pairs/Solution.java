3f37d092515f6420497db402b7ca6d5d3d5388f1
Kai Wang <charles.wangkai@gmail.com>
2023-11-10 10:47:06 +0800

    Restore the Array From Adjacent Pairs

--- restore-the-array-from-adjacent-pairs/Solution.java
+++ restore-the-array-from-adjacent-pairs/Solution.java
@@ -9,8 +9,11 @@ class Solution {
   public int[] restoreArray(int[][] adjacentPairs) {
     Map<Integer, List<Integer>> valueToAdjs = new HashMap<>();
     for (int[] adjacentPair : adjacentPairs) {
-      addValueToAdjs(valueToAdjs, adjacentPair[0], adjacentPair[1]);
-      addValueToAdjs(valueToAdjs, adjacentPair[1], adjacentPair[0]);
+      valueToAdjs.putIfAbsent(adjacentPair[0], new ArrayList<>());
+      valueToAdjs.get(adjacentPair[0]).add(adjacentPair[1]);
+
+      valueToAdjs.putIfAbsent(adjacentPair[1], new ArrayList<>());
+      valueToAdjs.get(adjacentPair[1]).add(adjacentPair[0]);
     }
 
     int[] result = new int[adjacentPairs.length + 1];
@@ -19,24 +22,17 @@ class Solution {
             .filter(value -> valueToAdjs.get(value).size() == 1)
             .findAny()
             .get();
-    Set<Integer> used = new HashSet<>();
-    used.add(result[0]);
+    Set<Integer> seen = new HashSet<>();
+    seen.add(result[0]);
     for (int i = 1; i < result.length; ++i) {
       result[i] =
           valueToAdjs.get(result[i - 1]).stream()
-              .filter(adj -> !used.contains(adj))
+              .filter(adj -> !seen.contains(adj))
               .findAny()
               .get();
-      used.add(result[i]);
+      seen.add(result[i]);
     }
 
     return result;
-  }
-
-  void addValueToAdjs(Map<Integer, List<Integer>> valueToAdjs, int value, int adj) {
-    if (!valueToAdjs.containsKey(value)) {
-      valueToAdjs.put(value, new ArrayList<>());
-    }
-    valueToAdjs.get(value).add(adj);
   }
 }
