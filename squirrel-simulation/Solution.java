d0b41decc7eb925c646bfaa75ecdc69dee1ccc39
Kai Wang <charles.wangkai@gmail.com>
2023-11-10 10:59:51 +0800

    Squirrel Simulation

--- squirrel-simulation/Solution.java
+++ squirrel-simulation/Solution.java
@@ -1,19 +1,15 @@
-public class Solution {
-	public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
-		int minDist = Integer.MAX_VALUE;
-		for (int i = 0; i < nuts.length; i++) {
-			int dist = computeDistance(squirrel, nuts[i]) + computeDistance(nuts[i], tree);
-			for (int j = 0; j < nuts.length; j++) {
-				if (j != i) {
-					dist += computeDistance(tree, nuts[j]) * 2;
-				}
-			}
-			minDist = Math.min(minDist, dist);
-		}
-		return minDist;
-	}
+import java.util.Arrays;
 
-	int computeDistance(int[] pos1, int[] pos2) {
-		return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
-	}
+class Solution {
+  public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
+    return 2 * Arrays.stream(nuts).mapToInt(nut -> computeDistance(nut, tree)).sum()
+        + Arrays.stream(nuts)
+            .mapToInt(nut -> computeDistance(nut, squirrel) - computeDistance(nut, tree))
+            .min()
+            .getAsInt();
+  }
+
+  int computeDistance(int[] pos1, int[] pos2) {
+    return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
+  }
 }
