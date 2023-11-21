d0a670ceb34a7341a0f5d880281dd8a15d60366e
Kai Wang <charles.wangkai@gmail.com>
2023-11-21 23:15:21 +0800

    Count Nice Pairs in an Array

--- count-nice-pairs-in-an-array/Solution.java
+++ count-nice-pairs-in-an-array/Solution.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+  static final int MODULUS = 1_000_000_007;
+
   public int countNicePairs(int[] nums) {
     Map<Integer, Integer> diffToCount = new HashMap<>();
     for (int num : nums) {
@@ -11,10 +13,10 @@ class Solution {
 
     return (int)
         (diffToCount.values().stream().mapToLong(count -> count * (count - 1L) / 2).sum()
-            % 1_000_000_007);
+            % MODULUS);
   }
 
-  static int rev(int x) {
+  int rev(int x) {
     return Integer.parseInt(new StringBuilder(String.valueOf(x)).reverse().toString());
   }
 }
