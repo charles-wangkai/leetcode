379bfecf28309854e7d57094127e3dc4de5d2eac
Kai Wang <charles.wangkai@gmail.com>
2023-11-22 08:52:36 +0800

    Diagonal Traverse II

--- diagonal-traverse-ii/Solution.java
+++ diagonal-traverse-ii/Solution.java
@@ -1,25 +1,16 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        return IntStream.range(0, nums.size()).boxed()
-                .flatMap(r -> IntStream.range(0, nums.get(r).size())
-                        .mapToObj(c -> new Element(r, c, nums.get(r).get(c))))
-                .sorted((e1, e2) -> (e1.r + e1.c != e2.r + e2.c) ? Integer.compare(e1.r + e1.c, e2.r + e2.c)
-                        : Integer.compare(e1.c, e2.c))
-                .mapToInt(e -> e.value).toArray();
-    }
+  public int[] findDiagonalOrder(List<List<Integer>> nums) {
+    return IntStream.range(0, nums.size())
+        .boxed()
+        .flatMap(r -> IntStream.range(0, nums.get(r).size()).mapToObj(c -> new Point(r, c)))
+        .sorted(Comparator.<Point, Integer>comparing(p -> p.r() + p.c()).thenComparing(Point::c))
+        .mapToInt(p -> nums.get(p.r()).get(p.c()))
+        .toArray();
+  }
 }
 
-class Element {
-    int r;
-    int c;
-    int value;
-
-    Element(int r, int c, int value) {
-        this.r = r;
-        this.c = c;
-        this.value = value;
-    }
-}
+record Point(int r, int c) {}
