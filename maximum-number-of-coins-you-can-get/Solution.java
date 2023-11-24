6df6a5041c9a2908f857436fe3ca6b5b7ac0e781
Kai Wang <charles.wangkai@gmail.com>
2023-11-24 08:44:07 +0800

    Maximum Number of Coins You Can Get

--- maximum-number-of-coins-you-can-get/Solution.java
+++ maximum-number-of-coins-you-can-get/Solution.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 
 class Solution {
-    public int maxCoins(int[] piles) {
-        piles = Arrays.stream(piles).boxed().sorted().mapToInt(x -> x).toArray();
+  public int maxCoins(int[] piles) {
+    Arrays.sort(piles);
 
-        int result = 0;
-        for (int i = piles.length / 3; i < piles.length; i += 2) {
-            result += piles[i];
-        }
-
-        return result;
+    int result = 0;
+    for (int i = piles.length / 3; i < piles.length; i += 2) {
+      result += piles[i];
     }
+
+    return result;
+  }
 }
