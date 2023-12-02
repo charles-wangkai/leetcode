c1bd4c9437ff2c0d85d114ff49283188f3d87c31
Kai Wang <charles.wangkai@gmail.com>
2023-12-02 08:25:21 +0800

    Find Words That Can Be Formed by Characters

--- find-words-that-can-be-formed-by-characters/Solution.java
+++ find-words-that-can-be-formed-by-characters/Solution.java
@@ -2,25 +2,28 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int countCharacters(String[] words, String chars) {
-		Map<Character, Integer> letterToCount = buildLetterToCount(chars);
+class Solution {
+  public int countCharacters(String[] words, String chars) {
+    Map<Character, Integer> letterToCount = buildLetterToCount(chars);
 
-		return Arrays.stream(words).filter(word -> isCovered(buildLetterToCount(word), letterToCount))
-				.mapToInt(String::length).sum();
-	}
+    return Arrays.stream(words)
+        .filter(word -> isCovered(buildLetterToCount(word), letterToCount))
+        .mapToInt(String::length)
+        .sum();
+  }
 
-	static Map<Character, Integer> buildLetterToCount(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+  Map<Character, Integer> buildLetterToCount(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		return letterToCount;
-	}
+    return letterToCount;
+  }
 
-	static boolean isCovered(Map<Character, Integer> letterToCount1, Map<Character, Integer> letterToCount2) {
-		return letterToCount1.keySet().stream()
-				.allMatch(letter -> letterToCount1.get(letter) <= letterToCount2.getOrDefault(letter, 0));
-	}
+  boolean isCovered(
+      Map<Character, Integer> letterToCount1, Map<Character, Integer> letterToCount2) {
+    return letterToCount1.keySet().stream()
+        .allMatch(letter -> letterToCount1.get(letter) <= letterToCount2.getOrDefault(letter, 0));
+  }
 }
