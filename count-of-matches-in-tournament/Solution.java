428b2bd2a4ac7e227517c2d132679c51e945dada
Kai Wang <charles.wangkai@gmail.com>
2023-12-05 08:26:21 +0800

    Count of Matches in Tournament

--- count-of-matches-in-tournament/Solution.java
+++ count-of-matches-in-tournament/Solution.java
@@ -1,16 +1,5 @@
 class Solution {
   public int numberOfMatches(int n) {
-    int result = 0;
-    while (n != 1) {
-      if (n % 2 == 0) {
-        result += n / 2;
-        n /= 2;
-      } else {
-        result += (n - 1) / 2;
-        n = (n - 1) / 2 + 1;
-      }
-    }
-
-    return result;
+    return (n == 1) ? 0 : (n / 2 + numberOfMatches((n + 1) / 2));
   }
 }
