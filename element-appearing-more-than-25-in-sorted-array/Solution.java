98d17c511aefd8f6dd56b5abffa0197e66a29393
Kai Wang <charles.wangkai@gmail.com>
2023-12-11 08:14:00 +0800

    Element Appearing More Than 25% In Sorted Array

--- element-appearing-more-than-25-in-sorted-array/Solution.java
+++ element-appearing-more-than-25-in-sorted-array/Solution.java
@@ -1,21 +1,21 @@
 public class Solution {
-	public int findSpecialInteger(int[] arr) {
-		int maxCount = 0;
-		int valueWithMaxCount = -1;
-		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (i != 0 && arr[i] == arr[i - 1]) {
-				count++;
-			} else {
-				count = 1;
-			}
+  public int findSpecialInteger(int[] arr) {
+    int result = -1;
+    int maxCount = 0;
+    int count = 0;
+    for (int i = 0; i < arr.length; ++i) {
+      if (i != 0 && arr[i] == arr[i - 1]) {
+        ++count;
+      } else {
+        count = 1;
+      }
 
-			if (count > maxCount) {
-				maxCount = count;
-				valueWithMaxCount = arr[i];
-			}
-		}
+      if (count > maxCount) {
+        maxCount = count;
+        result = arr[i];
+      }
+    }
 
-		return valueWithMaxCount;
-	}
+    return result;
+  }
 }
