3f28d424133b6ca4208b97062cc477c77d4313e6
Kai Wang <charles.wangkai@gmail.com>
2023-12-13 13:35:55 +0800

    Special Positions in a Binary Matrix

--- special-positions-in-a-binary-matrix/Solution.java
+++ special-positions-in-a-binary-matrix/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int numSpecial(int[][] mat) {
-        int row = mat.length;
-        int col = mat[0].length;
+  public int numSpecial(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
 
-        int[] rowOneCounts = new int[row];
-        int[] colOneCounts = new int[col];
-        for (int r = 0; r < row; ++r) {
-            for (int c = 0; c < col; ++c) {
-                if (mat[r][c] == 1) {
-                    ++rowOneCounts[r];
-                    ++colOneCounts[c];
-                }
-            }
+    int[] rowOneCounts = new int[m];
+    int[] colOneCounts = new int[n];
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (mat[r][c] == 1) {
+          ++rowOneCounts[r];
+          ++colOneCounts[c];
         }
-
-        int result = 0;
-        for (int r = 0; r < row; ++r) {
-            for (int c = 0; c < col; ++c) {
-                if (mat[r][c] == 1 && rowOneCounts[r] == 1 && colOneCounts[c] == 1) {
-                    ++result;
-                }
-            }
-        }
-
-        return result;
+      }
     }
+
+    int result = 0;
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (mat[r][c] == 1 && rowOneCounts[r] == 1 && colOneCounts[c] == 1) {
+          ++result;
+        }
+      }
+    }
+
+    return result;
+  }
 }
