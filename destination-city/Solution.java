e991c8a3278fa1f28501011cdd05984d4e66394b
Kai Wang <charles.wangkai@gmail.com>
2023-12-15 08:04:48 +0800

    Destination City

--- destination-city/Solution.java
+++ destination-city/Solution.java
@@ -3,9 +3,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 class Solution {
-    public String destCity(List<List<String>> paths) {
-        Set<String> startCities = paths.stream().map(path -> path.get(0)).collect(Collectors.toSet());
+  public String destCity(List<List<String>> paths) {
+    Set<String> startCities = paths.stream().map(path -> path.get(0)).collect(Collectors.toSet());
 
-        return paths.stream().map(path -> path.get(1)).filter(city -> !startCities.contains(city)).findAny().get();
-    }
+    return paths.stream()
+        .map(path -> path.get(1))
+        .filter(city -> !startCities.contains(city))
+        .findAny()
+        .get();
+  }
 }
