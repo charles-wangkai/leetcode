b401542572e92c28322d580b416c7196efcd9eb7
Kai Wang <charles.wangkai@gmail.com>
2023-12-21 09:14:47 +0800

    Widest Vertical Area Between Two Points Containing No Points

--- widest-vertical-area-between-two-points-containing-no-points/Solution.java
+++ widest-vertical-area-between-two-points-containing-no-points/Solution.java
@@ -3,8 +3,7 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int maxWidthOfVerticalArea(int[][] points) {
-    int[] sortedXs =
-        Arrays.stream(points).map(point -> point[0]).sorted().mapToInt(x -> x).toArray();
+    int[] sortedXs = Arrays.stream(points).mapToInt(point -> point[0]).sorted().toArray();
 
     return IntStream.range(0, sortedXs.length - 1)
         .map(i -> sortedXs[i + 1] - sortedXs[i])
