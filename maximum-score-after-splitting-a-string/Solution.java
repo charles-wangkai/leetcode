98a6b17c6f07218b7332d74672adcfe01a904f4f
Kai Wang <charles.wangkai@gmail.com>
2023-12-22 09:02:26 +0800

    Maximum Score After Splitting a String

--- maximum-score-after-splitting-a-string/Solution.java
+++ maximum-score-after-splitting-a-string/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
   public int maxScore(String s) {
     int result = 0;
-    int oneCount = s.replace("0", "").length();
+    int oneCount = (int) s.chars().filter(c -> c == '1').count();
     int zeroCount = 0;
     for (int i = 0; i < s.length() - 1; ++i) {
       if (s.charAt(i) == '0') {
