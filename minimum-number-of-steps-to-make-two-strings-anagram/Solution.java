1d37a1298285e467a833a4e55c9e8e4008921e03
Kai Wang <charles.wangkai@gmail.com>
2024-01-13 08:26:54 +0800

    Minimum Number of Steps to Make Two Strings Anagram

--- minimum-number-of-steps-to-make-two-strings-anagram/Solution.java
+++ minimum-number-of-steps-to-make-two-strings-anagram/Solution.java
@@ -1,19 +1,21 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-    public int minSteps(String s, String t) {
-        int[] sCounts = buildCounts(s);
-        int[] tCounts = buildCounts(t);
+class Solution {
+  static final int ALPHABET_SIZE = 26;
 
-        return IntStream.range(0, sCounts.length).map(i -> Math.abs(sCounts[i] - tCounts[i])).sum() / 2;
+  public int minSteps(String s, String t) {
+    int[] sCounts = buildCounts(s);
+    int[] tCounts = buildCounts(t);
+
+    return IntStream.range(0, sCounts.length).map(i -> Math.abs(sCounts[i] - tCounts[i])).sum() / 2;
+  }
+
+  int[] buildCounts(String str) {
+    int[] counts = new int[ALPHABET_SIZE];
+    for (char c : str.toCharArray()) {
+      ++counts[c - 'a'];
     }
 
-    int[] buildCounts(String str) {
-        int[] counts = new int[26];
-        for (char ch : str.toCharArray()) {
-            ++counts[ch - 'a'];
-        }
-
-        return counts;
-    }
+    return counts;
+  }
 }
