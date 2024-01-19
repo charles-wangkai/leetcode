2809233e61c7e08c24dd6843b3831d1fd8faf13b
Kai Wang <charles.wangkai@gmail.com>
2024-01-19 12:56:24 +0800

    Minimum Falling Path Sum

--- minimum-falling-path-sum/Solution.java
+++ minimum-falling-path-sum/Solution.java
@@ -5,25 +5,25 @@ class Solution {
   public int minFallingPathSum(int[][] matrix) {
     int n = matrix.length;
 
-    int[] minSums = matrix[0];
+    int[] dp = matrix[0];
     for (int r = 1; r < n; ++r) {
-      int[] nextMinSums = new int[n];
+      int[] nextDp = new int[n];
       for (int c = 0; c < n; ++c) {
         int c_ = c;
-        int[] minSums_ = minSums;
-        nextMinSums[c] =
+        int[] dp_ = dp;
+        nextDp[c] =
             IntStream.rangeClosed(-1, 1)
                     .map(i -> c_ + i)
                     .filter(prevC -> prevC >= 0 && prevC < n)
-                    .map(prevC -> minSums_[prevC])
+                    .map(prevC -> dp_[prevC])
                     .min()
                     .getAsInt()
                 + matrix[r][c];
       }
 
-      minSums = nextMinSums;
+      dp = nextDp;
     }
 
-    return Arrays.stream(minSums).min().getAsInt();
+    return Arrays.stream(dp).min().getAsInt();
   }
 }
