cd4167110a1fc758418b124da3fd6df1a3242d93
Kai Wang <charles.wangkai@gmail.com>
2024-01-26 08:45:35 +0800

    Out of Boundary Paths

--- out-of-boundary-paths/Solution.java
+++ out-of-boundary-paths/Solution.java
@@ -4,34 +4,27 @@ class Solution {
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
   public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-    int[][] pathNums = new int[m][n];
-    for (int c = 0; c < n; ++c) {
-      pathNums[0][c] = addMod(pathNums[0][c], 1);
-      pathNums[m - 1][c] = addMod(pathNums[m - 1][c], 1);
-    }
-    for (int r = 0; r < m; ++r) {
-      pathNums[r][0] = addMod(pathNums[r][0], 1);
-      pathNums[r][n - 1] = addMod(pathNums[r][n - 1], 1);
-    }
+    int[][] dp = new int[m][n];
+    dp[startRow][startColumn] = 1;
 
     int result = 0;
     for (int i = 0; i < maxMove; ++i) {
-      result = addMod(result, pathNums[startRow][startColumn]);
-
-      int[][] nextPathNums = new int[m][n];
+      int[][] nextDp = new int[m][n];
       for (int r = 0; r < m; ++r) {
         for (int c = 0; c < n; ++c) {
           for (int j = 0; j < R_OFFSETS.length; ++j) {
             int adjR = r + R_OFFSETS[j];
             int adjC = c + C_OFFSETS[j];
             if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n) {
-              nextPathNums[r][c] = addMod(nextPathNums[r][c], pathNums[adjR][adjC]);
+              nextDp[adjR][adjC] = addMod(nextDp[adjR][adjC], dp[r][c]);
+            } else {
+              result = addMod(result, dp[r][c]);
             }
           }
         }
       }
 
-      pathNums = nextPathNums;
+      dp = nextDp;
     }
 
     return result;
