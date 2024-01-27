6c1ae93f7a35e7f790ae909c41d2d0c366b0b70f
Kai Wang <charles.wangkai@gmail.com>
2024-01-27 08:41:29 +0800

    K Inverse Pairs Array

--- k-inverse-pairs-array/Solution.java
+++ k-inverse-pairs-array/Solution.java
@@ -3,8 +3,8 @@ class Solution {
 
   public int kInversePairs(int n, int k) {
     int[][] dp = new int[n + 1][k + 1];
-    dp[1][0] = 1;
-    for (int i = 2; i <= n; ++i) {
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; ++i) {
       int sum = 0;
       for (int j = 0; j <= k; ++j) {
         sum = addMod(sum, dp[i - 1][j]);
