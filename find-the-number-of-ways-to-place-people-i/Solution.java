aa3a35952825f0fdd8451ba6180286be4e9c58b9
Kai Wang <charles.wangkai@gmail.com>
2024-02-04 18:53:42 +0800

    Find the Number of Ways to Place People I

--- find-the-number-of-ways-to-place-people-i/Solution.java
+++ find-the-number-of-ways-to-place-people-i/Solution.java
@@ -7,22 +7,21 @@ class Solution {
     int result = 0;
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
-        if (j != i) {
-          int i_ = i;
-          int j_ = j;
-          if (points[j][0] >= points[i][0]
-              && points[j][1] <= points[i][1]
-              && IntStream.range(0, n)
-                  .allMatch(
-                      k ->
-                          k == i_
-                              || k == j_
-                              || !(points[k][0] >= points[i_][0]
-                                  && points[k][0] <= points[j_][0]
-                                  && points[k][1] >= points[j_][1]
-                                  && points[k][1] <= points[i_][1]))) {
-            ++result;
-          }
+        int i_ = i;
+        int j_ = j;
+        if (j != i
+            && points[j][0] >= points[i][0]
+            && points[j][1] <= points[i][1]
+            && IntStream.range(0, n)
+                .allMatch(
+                    k ->
+                        k == i_
+                            || k == j_
+                            || !(points[k][0] >= points[i_][0]
+                                && points[k][0] <= points[j_][0]
+                                && points[k][1] >= points[j_][1]
+                                && points[k][1] <= points[i_][1]))) {
+          ++result;
         }
       }
     }
