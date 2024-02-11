447d17fa8b8b3b84c41013c22a9d9e75431825e8
Kai Wang <charles.wangkai@gmail.com>
2024-02-11 08:50:09 +0800

    Cherry Pickup II

--- cherry-pickup-ii/Solution.java
+++ cherry-pickup-ii/Solution.java
@@ -3,29 +3,29 @@ class Solution {
     int row = grid.length;
     int col = grid[0].length;
 
-    int[][][] cherrySums = new int[row][col][col];
+    int[][][] dp = new int[row][col][col];
     for (int r = 0; r < row; ++r) {
       for (int i = 0; i < col; ++i) {
         for (int j = 0; j < col; ++j) {
-          cherrySums[r][i][j] = Integer.MIN_VALUE;
+          dp[r][i][j] = Integer.MIN_VALUE;
         }
       }
     }
-    cherrySums[0][0][col - 1] = grid[0][0] + grid[0][col - 1];
+    dp[0][0][col - 1] = grid[0][0] + grid[0][col - 1];
 
     for (int r = 0; r < row - 1; ++r) {
       for (int i = 0; i < col; ++i) {
         for (int j = i; j < col; ++j) {
-          if (cherrySums[r][i][j] != Integer.MIN_VALUE) {
+          if (dp[r][i][j] != Integer.MIN_VALUE) {
             for (int offsetI = -1; offsetI <= 1; ++offsetI) {
               for (int offsetJ = -1; offsetJ <= 1; ++offsetJ) {
                 int nextI = i + offsetI;
                 int nextJ = j + offsetJ;
                 if (nextI >= 0 && nextI < col && nextJ >= 0 && nextJ < col && nextI <= nextJ) {
-                  cherrySums[r + 1][nextI][nextJ] =
+                  dp[r + 1][nextI][nextJ] =
                       Math.max(
-                          cherrySums[r + 1][nextI][nextJ],
-                          cherrySums[r][i][j]
+                          dp[r + 1][nextI][nextJ],
+                          dp[r][i][j]
                               + grid[r + 1][nextI]
                               + ((nextI == nextJ) ? 0 : grid[r + 1][nextJ]));
                 }
@@ -39,7 +39,7 @@ class Solution {
     int result = 0;
     for (int i = 0; i < col; ++i) {
       for (int j = i; j < col; ++j) {
-        result = Math.max(result, cherrySums[row - 1][i][j]);
+        result = Math.max(result, dp[row - 1][i][j]);
       }
     }
 
