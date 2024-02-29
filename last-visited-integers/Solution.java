1543d08a2540b7ff5e9b5290e20707471c4cb66c
Kai Wang <charles.wangkai@gmail.com>
2024-02-29 16:29:01 +0800

    Last Visited Integers

--- last-visited-integers/Solution.java
+++ last-visited-integers/Solution.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  public List<Integer> lastVisitedIntegers(List<String> words) {
+  public List<Integer> lastVisitedIntegers(int[] nums) {
     List<Integer> result = new ArrayList<>();
     List<Integer> values = new ArrayList<>();
     int prevCount = 0;
-    for (String word : words) {
-      if (word.equals("prev")) {
+    for (int num : nums) {
+      if (num == -1) {
         ++prevCount;
         result.add((prevCount <= values.size()) ? values.get(prevCount - 1) : -1);
       } else {
         prevCount = 0;
-        values.add(0, Integer.parseInt(word));
+        values.add(0, num);
       }
     }
 
