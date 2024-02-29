f98aeada9d002dfd5702c5cfa5625b731e6a3865
Kai Wang <charles.wangkai@gmail.com>
2024-02-29 16:18:50 +0800

    Longest Unequal Adjacent Groups Subsequence I

--- longest-unequal-adjacent-groups-subsequence-i/Solution.java
+++ longest-unequal-adjacent-groups-subsequence-i/Solution.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
-  public List<String> getWordsInLongestSubsequence(int n, String[] words, int[] groups) {
+  public List<String> getLongestSubsequence(String[] words, int[] groups) {
     return IntStream.range(0, groups.length)
         .filter(i -> i == 0 || groups[i] != groups[i - 1])
         .mapToObj(i -> words[i])
