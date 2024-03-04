5d70da04b89aea19f15b30b65b404cb91d0d70de
Kai Wang <charles.wangkai@gmail.com>
2024-03-04 09:07:21 +0800

    Task Scheduler

--- task-scheduler/Solution.java
+++ task-scheduler/Solution.java
@@ -1,35 +1,34 @@
 class Solution {
-	static final int ALPHABET_SIZE = 26;
+  static final int ALPHABET_SIZE = 26;
 
-	public int leastInterval(char[] tasks, int n) {
-		int[] counts = new int[ALPHABET_SIZE];
-		for (char task : tasks) {
-			++counts[task - 'A'];
-		}
+  public int leastInterval(char[] tasks, int n) {
+    int[] counts = new int[ALPHABET_SIZE];
+    for (char task : tasks) {
+      ++counts[task - 'A'];
+    }
 
-		int[] waits = new int[ALPHABET_SIZE];
+    int result = 0;
+    int[] waits = new int[ALPHABET_SIZE];
+    int rest = tasks.length;
+    while (rest != 0) {
+      int chosen = -1;
+      for (int i = 0; i < ALPHABET_SIZE; ++i) {
+        if (waits[i] != 0) {
+          --waits[i];
+        } else if (counts[i] != 0 && (chosen == -1 || counts[i] > counts[chosen])) {
+          chosen = i;
+        }
+      }
 
-		int interval = 0;
-		int remain = tasks.length;
-		while (remain != 0) {
-			int chosen = -1;
-			for (int i = 0; i < ALPHABET_SIZE; ++i) {
-				if (waits[i] != 0) {
-					--waits[i];
-				} else if (counts[i] != 0 && (chosen == -1 || counts[i] > counts[chosen])) {
-					chosen = i;
-				}
-			}
+      if (chosen != -1) {
+        --counts[chosen];
+        waits[chosen] = n;
+        --rest;
+      }
 
-			if (chosen != -1) {
-				--counts[chosen];
-				waits[chosen] = n;
-				--remain;
-			}
+      ++result;
+    }
 
-			++interval;
-		}
-
-		return interval;
-	}
+    return result;
+  }
 }
