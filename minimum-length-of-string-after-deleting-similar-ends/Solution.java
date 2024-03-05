5903c8383279be369f46a4b53180f5c3bf5c2202
Kai Wang <charles.wangkai@gmail.com>
2024-03-05 09:49:11 +0800

    Minimum Length of String After Deleting Similar Ends

--- minimum-length-of-string-after-deleting-similar-ends/Solution.java
+++ minimum-length-of-string-after-deleting-similar-ends/Solution.java
@@ -3,51 +3,22 @@ import java.util.Deque;
 
 class Solution {
   public int minimumLength(String s) {
-    Deque<Element> deque = new ArrayDeque<>();
-    int count = -1;
-    char prev = 0;
-    for (int i = 0; i <= s.length(); ++i) {
-      if (i != s.length() && s.charAt(i) == prev) {
-        ++count;
-      } else {
-        if (count != -1) {
-          deque.offerLast(new Element(prev, count));
-        }
+    Deque<Character> deque = new ArrayDeque<>();
+    for (char c : s.toCharArray()) {
+      deque.offerLast(c);
+    }
 
-        count = 1;
-        if (i != s.length()) {
-          prev = s.charAt(i);
-        }
+    while (deque.size() >= 2 && deque.peekFirst().equals(deque.peekLast())) {
+      char c = deque.peekFirst();
+
+      while (!deque.isEmpty() && deque.peekFirst() == c) {
+        deque.pollFirst();
+      }
+      while (!deque.isEmpty() && deque.peekLast() == c) {
+        deque.pollLast();
       }
     }
 
-    while (true) {
-      if (deque.size() == 1) {
-        if (deque.peekFirst().count >= 2) {
-          deque.pollFirst();
-        }
-
-        break;
-      }
-
-      if (deque.peekFirst().ch != deque.peekLast().ch) {
-        break;
-      }
-
-      deque.pollFirst();
-      deque.pollLast();
-    }
-
-    return deque.stream().mapToInt(e -> e.count).sum();
-  }
-}
-
-class Element {
-  char ch;
-  int count;
-
-  Element(char ch, int count) {
-    this.ch = ch;
-    this.count = count;
+    return deque.size();
   }
 }
