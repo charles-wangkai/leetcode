0029b76609fe8ab468c35e4641e5931ed0905dee
Kai Wang <charles.wangkai@gmail.com>
2024-03-10 08:27:46 +0800

    Intersection of Two Arrays

--- intersection-of-two-arrays/Solution.java
+++ intersection-of-two-arrays/Solution.java
@@ -1,29 +1,11 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class Solution {
+class Solution {
   public int[] intersection(int[] nums1, int[] nums2) {
-    Set<Integer> numSet1 = buildSet(nums1);
-    Set<Integer> numSet2 = buildSet(nums2);
+    Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
 
-    List<Integer> merged = new ArrayList<>();
-    for (int num1 : numSet1) {
-      if (numSet2.contains(num1)) {
-        merged.add(num1);
-      }
-    }
-
-    int[] result = new int[merged.size()];
-    for (int i = 0; i < merged.size(); i++) {
-      result[i] = merged.get(i);
-    }
-    return result;
-  }
-
-  Set<Integer> buildSet(int[] nums) {
-    return Arrays.stream(nums).collect(HashSet::new, Set::add, Set::addAll);
+    return Arrays.stream(nums2).filter(set1::contains).distinct().toArray();
   }
 }
