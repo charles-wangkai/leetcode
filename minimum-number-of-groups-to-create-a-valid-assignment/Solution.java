80113ddaa99ce7af6170d9f2beef2f4a6fe39951
Kai Wang <charles.wangkai@gmail.com>
2024-03-12 21:10:01 +0800

    Minimum Number of Groups to Create a Valid Assignment

--- minimum-number-of-groups-to-create-a-valid-assignment/Solution.java
+++ minimum-number-of-groups-to-create-a-valid-assignment/Solution.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-  public int minGroupsForValidAssignment(int[] nums) {
-    Map<Integer, Integer> valueToCount = new HashMap<>();
-    for (int num : nums) {
-      valueToCount.put(num, valueToCount.getOrDefault(num, 0) + 1);
+  public int minGroupsForValidAssignment(int[] balls) {
+    Map<Integer, Integer> ballToCount = new HashMap<>();
+    for (int ball : balls) {
+      ballToCount.put(ball, ballToCount.getOrDefault(ball, 0) + 1);
     }
 
-    int[] counts = valueToCount.values().stream().sorted().mapToInt(Integer::intValue).toArray();
+    int[] counts = ballToCount.values().stream().sorted().mapToInt(Integer::intValue).toArray();
     for (int size = counts[0]; ; --size) {
       int size_ = size;
       if (Arrays.stream(counts).allMatch(count -> count % size_ <= count / size_)) {
