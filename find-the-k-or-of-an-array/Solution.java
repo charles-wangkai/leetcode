53eae14e0dcee8289db7c32dd5fe87a1e98eff64
Kai Wang <charles.wangkai@gmail.com>
2024-03-15 14:15:24 +0800

    Find the K-or of an Array

--- find-the-k-or-of-an-array/Solution.java
+++ find-the-k-or-of-an-array/Solution.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
   static final int BIT_NUM = 31;
 
   public int findKOr(int[] nums, int k) {
-    int result = 0;
-    for (int i = 0; i < BIT_NUM; ++i) {
-      int i_ = i;
-      if (Arrays.stream(nums).filter(num -> ((num >> i_) & 1) == 1).count() >= k) {
-        result |= 1 << i;
-      }
-    }
-
-    return result;
+    return IntStream.range(0, BIT_NUM)
+        .map(
+            i ->
+                (Arrays.stream(nums).filter(num -> ((num >> i) & 1) == 1).count() >= k)
+                    ? (1 << i)
+                    : 0)
+        .sum();
   }
 }
