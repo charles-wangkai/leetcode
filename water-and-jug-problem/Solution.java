da099d619b8830fc194c8d9c7fd92d9fe08c2a96
Kai Wang <charles.wangkai@gmail.com>
2024-03-15 14:21:57 +0800

    Water and Jug Problem

--- water-and-jug-problem/Solution.java
+++ water-and-jug-problem/Solution.java
@@ -1,9 +1,9 @@
-public class Solution {
-	public boolean canMeasureWater(int x, int y, int z) {
-		return z <= x + y && z % gcd(x, y) == 0;
-	}
+class Solution {
+  public boolean canMeasureWater(int x, int y, int target) {
+    return target <= x + y && target % gcd(x, y) == 0;
+  }
 
-	private int gcd(int a, int b) {
-		return (b == 0) ? (a == 0 ? 1 : a) : gcd(b, a % b);
-	}
+  int gcd(int a, int b) {
+    return (b == 0) ? a : gcd(b, a % b);
+  }
 }
