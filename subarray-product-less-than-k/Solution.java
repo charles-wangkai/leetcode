803b35009084d38ccabae96094824ee860b43024
Kai Wang <charles.wangkai@gmail.com>
2024-03-27 08:27:06 +0800

    Subarray Product Less Than K

--- subarray-product-less-than-k/Solution.java
+++ subarray-product-less-than-k/Solution.java
@@ -1,26 +1,22 @@
-import java.util.Arrays;
-
 class Solution {
-	public int numSubarrayProductLessThanK(int[] nums, int k) {
-		if (k <= 1) {
-			return 0;
-		}
+  public int numSubarrayProductLessThanK(int[] nums, int k) {
+    int product = 1;
+    int endIndex = -1;
+    int result = 0;
+    for (int beginIndex = 0; beginIndex < nums.length; ++beginIndex) {
+      while (endIndex != nums.length - 1 && product * nums[endIndex + 1] < k) {
+        ++endIndex;
+        product *= nums[endIndex];
+      }
 
-		nums = Arrays.copyOf(nums, nums.length + 1);
-		nums[nums.length - 1] = k;
-		long product = 1;
-		int endIndex = -1;
-		int result = 0;
-		for (int beginIndex = 0; beginIndex < nums.length; ++beginIndex) {
-			while (product < k) {
-				++endIndex;
-				product *= nums[endIndex];
-			}
+      if (endIndex == beginIndex - 1) {
+        ++endIndex;
+      } else {
+        result += endIndex - beginIndex + 1;
+        product /= nums[beginIndex];
+      }
+    }
 
-			result += endIndex - beginIndex;
-			product /= nums[beginIndex];
-		}
-
-		return result;
-	}
+    return result;
+  }
 }
