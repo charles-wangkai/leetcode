250351d9c05a7a78503bf6d97eae388b40994731
Kai Wang <charles.wangkai@gmail.com>
2024-04-01 09:29:29 +0800

    Length of Last Word

--- length-of-last-word/Solution.java
+++ length-of-last-word/Solution.java
@@ -1,16 +1,5 @@
 class Solution {
-	public int lengthOfLastWord(String s) {
-		int lastLength = 0;
-		for (int i = s.length() - 1; i >= 0; --i) {
-			if (s.charAt(i) == ' ') {
-				if (lastLength != 0) {
-					break;
-				}
-			} else {
-				++lastLength;
-			}
-		}
-
-		return lastLength;
-	}
+  public int lengthOfLastWord(String s) {
+    return s.stripTrailing().length() - 1 - s.stripTrailing().lastIndexOf(' ');
+  }
 }
