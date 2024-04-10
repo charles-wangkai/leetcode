3d28b1b9c94f7319723a27d07218531172aa5051
Kai Wang <charles.wangkai@gmail.com>
2024-04-10 08:49:55 +0800

    Reveal Cards In Increasing Order

--- reveal-cards-in-increasing-order/Solution.java
+++ reveal-cards-in-increasing-order/Solution.java
@@ -1,25 +1,26 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
 
-public class Solution {
-	public int[] deckRevealedIncreasing(int[] deck) {
-		Arrays.sort(deck);
+class Solution {
+  public int[] deckRevealedIncreasing(int[] deck) {
+    Arrays.sort(deck);
 
-		int[] result = new int[deck.length];
-		Deque<Integer> indices = new LinkedList<>();
-		for (int i = 0; i < result.length; i++) {
-			indices.offerLast(i);
-		}
+    int[] result = new int[deck.length];
+    Deque<Integer> indices = new ArrayDeque<>();
+    for (int i = 0; i < result.length; ++i) {
+      indices.offerLast(i);
+    }
 
-		for (int i = 0; i < deck.length; i++) {
-			int index = indices.pollFirst();
-			result[index] = deck[i];
+    for (int i = 0; i < deck.length; ++i) {
+      int index = indices.pollFirst();
+      result[index] = deck[i];
 
-			if (i != deck.length - 1) {
-				indices.offerLast(indices.pollFirst());
-			}
-		}
-		return result;
-	}
+      if (i != deck.length - 1) {
+        indices.offerLast(indices.pollFirst());
+      }
+    }
+
+    return result;
+  }
 }
