a6aff3cad13ff72f4605d14dad27d1d6d7069871
Kai Wang <charles.wangkai@gmail.com>
2024-04-14 08:28:48 +0800

    Sum of Left Leaves

--- sum-of-left-leaves/Solution.java
+++ sum-of-left-leaves/Solution.java
@@ -18,24 +18,14 @@ class TreeNode {
 }
 
 class Solution {
-  enum Direction {
-    LEFT,
-    RIGHT
-  }
-
   public int sumOfLeftLeaves(TreeNode root) {
-    return search(root, null);
+    return search(root, false);
   }
 
-  int search(TreeNode node, Direction direction) {
-    if (node == null) {
-      return 0;
-    }
-
-    if (node.left == null && node.right == null && direction == Direction.LEFT) {
-      return node.val;
-    }
-
-    return search(node.left, Direction.LEFT) + search(node.right, Direction.RIGHT);
+  int search(TreeNode node, boolean left) {
+    return (node.left == null && node.right == null && left)
+        ? node.val
+        : (((node.left == null) ? 0 : search(node.left, true))
+            + ((node.right == null) ? 0 : search(node.right, false)));
   }
 }
