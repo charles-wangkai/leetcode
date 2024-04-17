3452f420783063bafeabdfc3c736bc8192f7d246
Kai Wang <charles.wangkai@gmail.com>
2024-04-17 08:34:59 +0800

    Smallest String Starting From Leaf

--- smallest-string-starting-from-leaf/Solution.java
+++ smallest-string-starting-from-leaf/Solution.java
@@ -4,17 +4,26 @@ class TreeNode {
   TreeNode left;
   TreeNode right;
 
-  TreeNode(int x) {
-    val = x;
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
   }
 }
 
-public class Solution {
+class Solution {
   String minPath;
 
   public String smallestFromLeaf(TreeNode root) {
     minPath = null;
     search(root, "");
+
     return minPath;
   }
 
