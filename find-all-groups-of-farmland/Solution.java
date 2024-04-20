f32970e6e101aff0d8703c818e997519c88a1eef
Kai Wang <charles.wangkai@gmail.com>
2024-04-20 08:33:05 +0800

    Find All Groups of Farmland

--- find-all-groups-of-farmland/Solution.java
+++ find-all-groups-of-farmland/Solution.java
@@ -2,53 +2,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  static final int[] R_OFFSETS = {-1, 0, 1, 0};
-  static final int[] C_OFFSETS = {0, 1, 0, -1};
-
   public int[][] findFarmland(int[][] land) {
     int m = land.length;
     int n = land[0].length;
 
     List<int[]> result = new ArrayList<>();
-    boolean[][] visited = new boolean[m][n];
-    for (int r = 0; r < m; ++r) {
-      for (int c = 0; c < n; ++c) {
-        if (!visited[r][c] && land[r][c] == 1) {
-          int[] farmland = {
-            Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE
-          };
-          search(farmland, land, visited, r, c);
+    for (int r1 = 0; r1 < m; ++r1) {
+      for (int c1 = 0; c1 < n; ++c1) {
+        if (land[r1][c1] == 1) {
+          int r2 = r1;
+          while (r2 != m - 1 && land[r2 + 1][c1] == 1) {
+            ++r2;
+          }
 
-          result.add(farmland);
+          int c2 = c1;
+          while (c2 != n - 1 && land[r2][c2 + 1] == 1) {
+            ++c2;
+          }
+
+          for (int r = r1; r <= r2; ++r) {
+            for (int c = c1; c <= c2; ++c) {
+              land[r][c] = 0;
+            }
+          }
+
+          result.add(new int[] {r1, c1, r2, c2});
         }
       }
     }
 
     return result.stream().toArray(int[][]::new);
-  }
-
-  void search(int[] farmland, int[][] land, boolean[][] visited, int r, int c) {
-    int m = land.length;
-    int n = land[0].length;
-
-    visited[r][c] = true;
-
-    farmland[0] = Math.min(farmland[0], r);
-    farmland[1] = Math.min(farmland[1], c);
-    farmland[2] = Math.max(farmland[2], r);
-    farmland[3] = Math.max(farmland[3], c);
-
-    for (int i = 0; i < R_OFFSETS.length; ++i) {
-      int adjR = r + R_OFFSETS[i];
-      int adjC = c + C_OFFSETS[i];
-      if (adjR >= 0
-          && adjR < m
-          && adjC >= 0
-          && adjC < n
-          && !visited[adjR][adjC]
-          && land[adjR][adjC] == 1) {
-        search(farmland, land, visited, adjR, adjC);
-      }
-    }
   }
 }
