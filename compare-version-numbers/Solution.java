a828eaff3d0aae6f4484673a829f7f4184a6ec8a
Kai Wang <charles.wangkai@gmail.com>
2024-05-03 09:22:30 +0800

    Compare Version Numbers

--- compare-version-numbers/Solution.java
+++ compare-version-numbers/Solution.java
@@ -3,21 +3,21 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int compareVersion(String version1, String version2) {
-    int[] numbers1 = convertToNumbers(version1);
-    int[] numbers2 = convertToNumbers(version2);
+    int[] values1 = convertToValues(version1);
+    int[] values2 = convertToValues(version2);
 
-    return IntStream.range(0, Math.max(numbers1.length, numbers2.length))
-        .map(i -> Integer.compare(getNumber(numbers1, i), getNumber(numbers2, i)))
+    return IntStream.range(0, Math.max(values1.length, values2.length))
+        .map(i -> Integer.compare(getValue(values1, i), getValue(values2, i)))
         .filter(x -> x != 0)
         .findFirst()
         .orElse(0);
   }
 
-  int[] convertToNumbers(String version) {
+  int[] convertToValues(String version) {
     return Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
   }
 
-  int getNumber(int[] numbers, int index) {
-    return (index < numbers.length) ? numbers[index] : 0;
+  int getValue(int[] values, int index) {
+    return (index < values.length) ? values[index] : 0;
   }
 }
