0bfc53748ec43eb92736269c13ca244fed573981
Kai Wang <charles.wangkai@gmail.com>
2024-05-10 08:29:30 +0800

    K-th Smallest Prime Fraction

--- k-th-smallest-prime-fraction/Solution.java
+++ k-th-smallest-prime-fraction/Solution.java
@@ -1,37 +1,25 @@
-import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
-public class Solution {
-  public int[] kthSmallestPrimeFraction(int[] A, int K) {
-    PriorityQueue<FractionIndices> pq =
-        new PriorityQueue<>(
-            (fi1, fi2) ->
-                Integer.compare(
-                    A[fi1.numeratorIndex] * A[fi2.denominatorIndex],
-                    A[fi2.numeratorIndex] * A[fi1.denominatorIndex]));
+class Solution {
+  public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+    Fraction fraction =
+        IntStream.range(0, arr.length)
+            .boxed()
+            .flatMap(
+                i ->
+                    IntStream.range(i + 1, arr.length)
+                        .boxed()
+                        .map(j -> new Fraction(arr[i], arr[j])))
+            .sorted(
+                (f1, f2) ->
+                    Integer.compare(
+                        f1.numerator() * f2.denominator(), f2.numerator() * f1.denominator()))
+            .skip(k - 1)
+            .findFirst()
+            .get();
 
-    for (int i = 0; i < A.length - 1; i++) {
-      pq.offer(new FractionIndices(i, A.length - 1));
-    }
-
-    for (int i = 0; i < K - 1; i++) {
-      FractionIndices head = pq.poll();
-
-      if (head.denominatorIndex - 1 > head.numeratorIndex) {
-        pq.offer(new FractionIndices(head.numeratorIndex, head.denominatorIndex - 1));
-      }
-    }
-
-    FractionIndices result = pq.peek();
-    return new int[] {A[result.numeratorIndex], A[result.denominatorIndex]};
+    return new int[] {fraction.numerator(), fraction.denominator()};
   }
 }
 
-class FractionIndices {
-  int numeratorIndex;
-  int denominatorIndex;
-
-  FractionIndices(int numeratorIndex, int denominatorIndex) {
-    this.numeratorIndex = numeratorIndex;
-    this.denominatorIndex = denominatorIndex;
-  }
-}
+record Fraction(int numerator, int denominator) {}
