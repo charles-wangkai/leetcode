3a84ad24b3753692a2bd04ae51b9743f666ac15b
Kai Wang <charles.wangkai@gmail.com>
2024-05-14 11:32:19 +0800

    Path with Maximum Gold

--- path-with-maximum-gold/Solution.java
+++ path-with-maximum-gold/Solution.java
@@ -1,43 +1,47 @@
 public class Solution {
-	static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-	static final int[] C_OFFSETS = { 0, 1, 0, -1 };
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
 
-	public int getMaximumGold(int[][] grid) {
-		int row = grid.length;
-		int col = grid[0].length;
+  int getMaximumGold(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-		boolean[][] visited = new boolean[row][col];
+    boolean[][] visited = new boolean[m][n];
 
-		int result = 0;
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				if (grid[r][c] != 0) {
-					result = Math.max(result, search(grid, visited, r, c));
-				}
-			}
-		}
+    int result = 0;
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (grid[r][c] != 0) {
+          result = Math.max(result, search(grid, visited, r, c));
+        }
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	static int search(int[][] grid, boolean[][] visited, int r, int c) {
-		int row = grid.length;
-		int col = grid[0].length;
+  static int search(int[][] grid, boolean[][] visited, int r, int c) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-		visited[r][c] = true;
+    visited[r][c] = true;
 
-		int maxSubResult = 0;
-		for (int i = 0; i < R_OFFSETS.length; i++) {
-			int adjR = r + R_OFFSETS[i];
-			int adjC = c + C_OFFSETS[i];
+    int maxSubResult = 0;
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int adjR = r + R_OFFSETS[i];
+      int adjC = c + C_OFFSETS[i];
+      if (adjR >= 0
+          && adjR < m
+          && adjC >= 0
+          && adjC < n
+          && grid[adjR][adjC] != 0
+          && !visited[adjR][adjC]) {
+        maxSubResult = Math.max(maxSubResult, search(grid, visited, adjR, adjC));
+      }
+    }
 
-			if (adjR >= 0 && adjR < row && adjC >= 0 && adjC < col && grid[adjR][adjC] != 0 && !visited[adjR][adjC]) {
-				maxSubResult = Math.max(maxSubResult, search(grid, visited, adjR, adjC));
-			}
-		}
+    visited[r][c] = false;
 
-		visited[r][c] = false;
-
-		return grid[r][c] + maxSubResult;
-	}
+    return grid[r][c] + maxSubResult;
+  }
 }
