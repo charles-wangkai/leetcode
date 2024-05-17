08ab75d799ee841faf17655d5ef02e9bd5ab0ff5
Kai Wang <charles.wangkai@gmail.com>
2024-05-17 08:49:58 +0800

    Delete Leaves With a Given Value

--- delete-leaves-with-a-given-value/Solution.java
+++ delete-leaves-with-a-given-value/Solution.java
@@ -1,27 +1,31 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode removeLeafNodes(TreeNode root, int target) {
-		if (root == null) {
-			return null;
-		}
+class Solution {
+  public TreeNode removeLeafNodes(TreeNode root, int target) {
+    if (root == null) {
+      return null;
+    }
 
-		root.left = removeLeafNodes(root.left, target);
-		root.right = removeLeafNodes(root.right, target);
+    root.left = removeLeafNodes(root.left, target);
+    root.right = removeLeafNodes(root.right, target);
 
-		if (root.left == null && root.right == null && root.val == target) {
-			return null;
-		}
-
-		return root;
-	}
+    return (root.left == null && root.right == null && root.val == target) ? null : root;
+  }
 }
