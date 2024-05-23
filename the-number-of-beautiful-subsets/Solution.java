aa6d0e7e46ab94066f8b647cab5433f8f3ee8e93
Kai Wang <charles.wangkai@gmail.com>
2024-05-23 15:22:26 +0800

    The Number of Beautiful Subsets

--- the-number-of-beautiful-subsets/Solution.java
+++ the-number-of-beautiful-subsets/Solution.java
@@ -1,29 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 class Solution {
   static final int LIMIT = 1000;
 
   public int beautifulSubsets(int[] nums, int k) {
-    return (int) IntStream.range(1, 1 << nums.length).filter(mask -> check(nums, k, mask)).count();
-  }
+    return (int)
+        IntStream.range(1, 1 << nums.length)
+            .filter(
+                mask -> {
+                  Set<Integer> seen = new HashSet<>();
+                  for (int i = 0; i < nums.length; ++i) {
+                    if (((mask >> i) & 1) == 1) {
+                      if (seen.contains(nums[i] - k) || seen.contains(nums[i] + k)) {
+                        return false;
+                      }
 
-  boolean check(int[] nums, int k, int mask) {
-    boolean[] invalids = new boolean[LIMIT + 1];
-    for (int i = 0; i < nums.length; ++i) {
-      if (((mask >> i) & 1) == 1) {
-        if (invalids[nums[i]]) {
-          return false;
-        }
+                      seen.add(nums[i]);
+                    }
+                  }
 
-        if (nums[i] >= k) {
-          invalids[nums[i] - k] = true;
-        }
-        if (nums[i] + k < invalids.length) {
-          invalids[nums[i] + k] = true;
-        }
-      }
-    }
-
-    return true;
+                  return true;
+                })
+            .count();
   }
 }
