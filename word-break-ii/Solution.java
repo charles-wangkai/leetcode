53746c40c93bd3817a095ea1c04aebbc57dd0f05
Kai Wang <charles.wangkai@gmail.com>
2024-05-25 08:59:12 +0800

    Word Break II

--- word-break-ii/Solution.java
+++ word-break-ii/Solution.java
@@ -1,46 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 class Solution {
-	public List<String> wordBreak(String s, List<String> wordDict) {
-		@SuppressWarnings("unchecked")
-		List<String>[] lastWordLists = new List[s.length()];
-		for (int i = 0; i < lastWordLists.length; ++i) {
-			lastWordLists[i] = new ArrayList<>();
-		}
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    Set<String> dictionary = Set.copyOf(wordDict);
 
-		for (int i = 0; i < s.length(); ++i) {
-			String prefix = s.substring(0, i + 1);
-
-			for (String word : wordDict) {
-				if (prefix.endsWith(word)
-						&& (word.length() == prefix.length() || !lastWordLists[i - word.length()].isEmpty())) {
-					lastWordLists[i].add(word);
-				}
-			}
-		}
-
-		List<String> sentences = new ArrayList<>();
-		search(sentences, lastWordLists, s.length(), new ArrayList<>());
-
-		return sentences;
-	}
-
-	void search(List<String> sentences, List<String>[] lastWordLists, int length, List<String> words) {
-		if (length == 0) {
-			List<String> sentence = new ArrayList<>(words);
-			Collections.reverse(sentence);
-
-			sentences.add(String.join(" ", sentence));
-
-			return;
-		}
-
-		for (String word : lastWordLists[length - 1]) {
-			words.add(word);
-			search(sentences, lastWordLists, length - word.length(), words);
-			words.remove(words.size() - 1);
-		}
-	}
+    return IntStream.range(0, 1 << (s.length() - 1))
+        .mapToObj(
+            mask ->
+                IntStream.concat(
+                        IntStream.range(0, s.length() - 1).filter(i -> ((mask >> i) & 1) == 1),
+                        IntStream.of(s.length() - 1))
+                    .toArray())
+        .map(
+            indices ->
+                IntStream.range(0, indices.length)
+                    .mapToObj(i -> s.substring((i == 0) ? 0 : (indices[i - 1] + 1), indices[i] + 1))
+                    .toArray(String[]::new))
+        .filter(words -> Arrays.stream(words).allMatch(dictionary::contains))
+        .map(words -> String.join(" ", words))
+        .toList();
+  }
 }
