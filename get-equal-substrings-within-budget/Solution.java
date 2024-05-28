c34842c59e2a6098f8f4dc458a63740859a97d69
Kai Wang <charles.wangkai@gmail.com>
2024-05-28 08:54:16 +0800

    Get Equal Substrings Within Budget

--- get-equal-substrings-within-budget/Solution.java
+++ get-equal-substrings-within-budget/Solution.java
@@ -1,24 +1,23 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int equalSubstring(String s, String t, int maxCost) {
-		int n = s.length();
-		int[] costs = IntStream.range(0, n).map(i -> Math.abs(s.charAt(i) - t.charAt(i))).toArray();
+class Solution {
+  public int equalSubstring(String s, String t, int maxCost) {
+    int[] costs =
+        IntStream.range(0, s.length()).map(i -> Math.abs(s.charAt(i) - t.charAt(i))).toArray();
 
-		int result = 0;
-		int beginIndex = 0;
-		int sum = 0;
-		for (int endIndex = 0; endIndex < n; endIndex++) {
-			sum += costs[endIndex];
+    int result = 0;
+    int beginIndex = 0;
+    int sum = 0;
+    for (int endIndex = 0; endIndex < s.length(); ++endIndex) {
+      sum += costs[endIndex];
+      while (sum > maxCost) {
+        sum -= costs[beginIndex];
+        ++beginIndex;
+      }
 
-			while (sum > maxCost) {
-				sum -= costs[beginIndex];
-				beginIndex++;
-			}
+      result = Math.max(result, endIndex - beginIndex + 1);
+    }
 
-			result = Math.max(result, endIndex - beginIndex + 1);
-		}
-
-		return result;
-	}
+    return result;
+  }
 }
