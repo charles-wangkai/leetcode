6330383ae59ddfd7021fe631e8b1858fde83595f
Kai Wang <charles.wangkai@gmail.com>
2024-05-29 09:40:01 +0800

    Number of Steps to Reduce a Number in Binary Representation to One

--- number-of-steps-to-reduce-a-number-in-binary-representation-to-one/Solution.java
+++ number-of-steps-to-reduce-a-number-in-binary-representation-to-one/Solution.java
@@ -1,18 +1,18 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int numSteps(String s) {
-        int result = 0;
-        int index = s.length() - 1;
-        while (s.charAt(index) == '0') {
-            ++result;
-            --index;
-        }
-
-        if (index != 0) {
-            result += index + (int) IntStream.range(0, index).filter(i -> s.charAt(i) == '0').count() + 2;
-        }
-
-        return result;
+  public int numSteps(String s) {
+    int result = 0;
+    int index = s.length() - 1;
+    while (s.charAt(index) == '0') {
+      ++result;
+      --index;
     }
+
+    if (index != 0) {
+      result += index + (int) IntStream.range(0, index).filter(i -> s.charAt(i) == '0').count() + 2;
+    }
+
+    return result;
+  }
 }
