feabbd7b44f6f75ea60a3937edb090ebdc961921
Kai Wang <charles.wangkai@gmail.com>
2024-05-31 08:20:22 +0800

    Single Number III

--- single-number-iii/Solution.java
+++ single-number-iii/Solution.java
@@ -2,21 +2,15 @@ import java.util.Arrays;
 
 class Solution {
   public int[] singleNumber(int[] nums) {
-    int xor = Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
+    int diffPos =
+        Integer.numberOfTrailingZeros(Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt());
 
-    int diffPos = 0;
-    while ((xor & (1 << diffPos)) == 0) {
-      ++diffPos;
-    }
-
-    return new int[] {
-      singleNumberForOne(nums, diffPos, false), singleNumberForOne(nums, diffPos, true)
-    };
+    return new int[] {singleNumberForOne(nums, diffPos, 0), singleNumberForOne(nums, diffPos, 1)};
   }
 
-  int singleNumberForOne(int[] nums, int pos, boolean setBit) {
+  int singleNumberForOne(int[] nums, int pos, int bit) {
     return Arrays.stream(nums)
-        .filter(num -> ((num & (1 << pos)) != 0) == setBit)
+        .filter(num -> ((num >> pos) & 1) == bit)
         .reduce((x, y) -> x ^ y)
         .getAsInt();
   }
