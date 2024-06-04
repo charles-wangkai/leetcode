b5fa92d277b1c073d0f22a54f72a9aa6def64d9d
Kai Wang <charles.wangkai@gmail.com>
2024-06-04 09:53:41 +0800

    Longest Palindrome

--- longest-palindrome/Solution.java
+++ longest-palindrome/Solution.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-	public int longestPalindrome(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+  public int longestPalindrome(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		return letterToCount.values().stream().mapToInt(count -> count / 2 * 2).sum()
-				+ (letterToCount.values().stream().anyMatch(count -> count % 2 != 0) ? 1 : 0);
-	}
+    return letterToCount.values().stream().mapToInt(count -> count / 2 * 2).sum()
+        + (letterToCount.values().stream().anyMatch(count -> count % 2 == 1) ? 1 : 0);
+  }
 }
