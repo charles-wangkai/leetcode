545ce20622379de1ac0490a8ccf811772da0c164
Kai Wang <charles.wangkai@gmail.com>
2024-06-05 08:43:35 +0800

    Find Common Characters

--- find-common-characters/Solution.java
+++ find-common-characters/Solution.java
@@ -1,33 +1,32 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public List<String> commonChars(String[] A) {
-		Map<Character, Integer> merged = buildLetterToCount(A[0]);
-		for (int i = 1; i < A.length; i++) {
-			Map<Character, Integer> letterToCount = buildLetterToCount(A[i]);
+class Solution {
+  public List<String> commonChars(String[] words) {
+    Map<Character, Integer> merged = buildLetterToCount(words[0]);
+    for (int i = 1; i < words.length; ++i) {
+      Map<Character, Integer> letterToCount = buildLetterToCount(words[i]);
 
-			for (char letter : merged.keySet()) {
-				merged.put(letter, Math.min(merged.get(letter), letterToCount.getOrDefault(letter, 0)));
-			}
-		}
+      for (char letter : merged.keySet()) {
+        merged.put(letter, Math.min(merged.get(letter), letterToCount.getOrDefault(letter, 0)));
+      }
+    }
 
-		List<String> result = new ArrayList<>();
-		for (char letter : merged.keySet()) {
-			for (int i = 0; i < merged.get(letter); i++) {
-				result.add(String.valueOf(letter));
-			}
-		}
-		return result;
-	}
+    return merged.keySet().stream()
+        .flatMap(
+            letter ->
+                IntStream.range(0, merged.get(letter)).mapToObj(i -> letter).map(String::valueOf))
+        .toList();
+  }
 
-	Map<Character, Integer> buildLetterToCount(String s) {
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : s.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
-		return letterToCount;
-	}
+  Map<Character, Integer> buildLetterToCount(String word) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : word.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
+
+    return letterToCount;
+  }
 }
