cbaa43496f727e6476d765a91b27c67bc447e8a3
Kai Wang <charles.wangkai@gmail.com>
2024-06-07 08:30:09 +0800

    Replace Words

--- replace-words/Solution.java
+++ replace-words/Solution.java
@@ -1,58 +1,47 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Solution {
-  public String replaceWords(List<String> dict, String sentence) {
-    Trie trie = new Trie();
-    dict.stream().forEach(word -> trie.add(word, 0));
-
-    StringBuilder result = new StringBuilder();
-    int wordIndex = 0;
-    for (int i = 0; i <= sentence.length(); i++) {
-      if (i == sentence.length() || sentence.charAt(i) == ' ') {
-        result.append(replaceWord(trie, sentence.substring(wordIndex, i)));
-
-        if (i < sentence.length()) {
-          result.append(" ");
-        }
-
-        wordIndex = i + 1;
-      }
+class Solution {
+  public String replaceWords(List<String> dictionary, String sentence) {
+    Node trie = new Node();
+    for (String word : dictionary) {
+      addTrie(trie, word);
     }
-    return result.toString();
+
+    return Arrays.stream(sentence.split(" "))
+        .map(word -> replaceWord(trie, word))
+        .collect(Collectors.joining(" "));
   }
 
-  String replaceWord(Trie trie, String word) {
-    for (int i = 0; i < word.length(); i++) {
+  void addTrie(Node trie, String word) {
+    for (int i = 0; i < word.length(); ++i) {
       char letter = word.charAt(i);
+      trie.letterToChild.putIfAbsent(letter, new Node());
+      trie = trie.letterToChild.get(letter);
+    }
+    trie.letterToChild.put(null, null);
+  }
 
-      if (!trie.letter2children.containsKey(letter)) {
+  String replaceWord(Node trie, String word) {
+    for (int i = 0; i < word.length(); ++i) {
+      char letter = word.charAt(i);
+      if (!trie.letterToChild.containsKey(letter)) {
         return word;
       }
 
-      trie = trie.letter2children.get(letter);
-
-      if (trie.letter2children.containsKey(null)) {
+      trie = trie.letterToChild.get(letter);
+      if (trie.letterToChild.containsKey(null)) {
         return word.substring(0, i + 1);
       }
     }
+
     return word;
   }
 }
 
-class Trie {
-  Map<Character, Trie> letter2children = new HashMap<>();
-
-  void add(String word, int index) {
-    if (index == word.length()) {
-      letter2children.put(null, null);
-    } else {
-      char letter = word.charAt(index);
-      if (!letter2children.containsKey(letter)) {
-        letter2children.put(letter, new Trie());
-      }
-      letter2children.get(letter).add(word, index + 1);
-    }
-  }
+class Node {
+  Map<Character, Node> letterToChild = new HashMap<>();
 }
