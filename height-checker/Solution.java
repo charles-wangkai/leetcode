d57ff7965d479446d1b6e582c23ea63996935c87
Kai Wang <charles.wangkai@gmail.com>
2024-06-10 08:59:32 +0800

    Height Checker

--- height-checker/Solution.java
+++ height-checker/Solution.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int heightChecker(int[] heights) {
-		int[] sortedHeights = Arrays.stream(heights).sorted().toArray();
-		return (int) IntStream.range(0, heights.length).filter(i -> heights[i] != sortedHeights[i]).count();
-	}
+class Solution {
+  public int heightChecker(int[] heights) {
+    int[] sorted = Arrays.stream(heights).sorted().toArray();
+
+    return (int) IntStream.range(0, heights.length).filter(i -> heights[i] != sorted[i]).count();
+  }
 }
