129c67186db93c8591e2227223a1b7d470c028ac
Kai Wang <charles.wangkai@gmail.com>
2024-06-18 09:52:21 +0800

    Most Profit Assigning Work

--- most-profit-assigning-work/Solution.java
+++ most-profit-assigning-work/Solution.java
@@ -1,54 +1,40 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 
-public class Solution {
-	public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-		Job[] jobs = new Job[difficulty.length];
-		for (int i = 0; i < jobs.length; i++) {
-			jobs[i] = new Job(difficulty[i], profit[i]);
-		}
+class Solution {
+  public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+    int[] sortedIndices =
+        IntStream.range(0, difficulty.length)
+            .boxed()
+            .sorted(Comparator.comparing(i -> difficulty[i]))
+            .mapToInt(Integer::intValue)
+            .toArray();
 
-		Arrays.sort(jobs,
-				(job1, job2) -> (job1.difficulty == job2.difficulty) ? Integer.compare(job2.profit, job1.profit)
-						: Integer.compare(job1.difficulty, job2.difficulty));
+    int[] maxProfits = new int[sortedIndices.length];
+    for (int i = 0; i < maxProfits.length; ++i) {
+      maxProfits[i] = Math.max((i == 0) ? -1 : maxProfits[i - 1], profit[sortedIndices[i]]);
+    }
 
-		int[] maxProfits = new int[jobs.length];
-		int maxProfit = -1;
-		for (int i = 0; i < maxProfits.length; i++) {
-			maxProfit = Math.max(maxProfit, jobs[i].profit);
-			maxProfits[i] = maxProfit;
-		}
+    return Arrays.stream(worker)
+        .map(limit -> findMaxProfit(difficulty, sortedIndices, maxProfits, limit))
+        .sum();
+  }
 
-		int result = 0;
-		for (int difficultLimit : worker) {
-			result += findMaxProfit(jobs, maxProfits, difficultLimit);
-		}
-		return result;
-	}
+  int findMaxProfit(int[] difficulty, int[] sortedIndices, int[] maxProfits, int limit) {
+    int result = 0;
+    int lower = 0;
+    int upper = sortedIndices.length - 1;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (difficulty[sortedIndices[middle]] <= limit) {
+        result = maxProfits[middle];
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
+    }
 
-	int findMaxProfit(Job[] jobs, int[] maxProfits, int difficultLimit) {
-		int result = 0;
-		int lower = 0;
-		int upper = jobs.length - 1;
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
-			if (jobs[middle].difficulty <= difficultLimit) {
-				result = maxProfits[middle];
-
-				lower = middle + 1;
-			} else {
-				upper = middle - 1;
-			}
-		}
-		return result;
-	}
-}
-
-class Job {
-	int difficulty;
-	int profit;
-
-	Job(int difficulty, int profit) {
-		this.difficulty = difficulty;
-		this.profit = profit;
-	}
+    return result;
+  }
 }
