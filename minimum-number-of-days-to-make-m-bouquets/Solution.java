c7e138b498efea53c65d4a03a067908f925df175
Kai Wang <charles.wangkai@gmail.com>
2024-06-19 09:06:21 +0800

    Minimum Number of Days to Make m Bouquets

--- minimum-number-of-days-to-make-m-bouquets/Solution.java
+++ minimum-number-of-days-to-make-m-bouquets/Solution.java
@@ -1,39 +1,38 @@
 import java.util.Arrays;
 
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int result = -1;
-        int lower = 1;
-        int upper = Arrays.stream(bloomDay).max().getAsInt();
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
-
-            if (check(bloomDay, m, k, middle)) {
-                result = middle;
-                upper = middle - 1;
-            } else {
-                lower = middle + 1;
-            }
-        }
-
-        return result;
+  public int minDays(int[] bloomDay, int m, int k) {
+    int result = -1;
+    int lower = 1;
+    int upper = Arrays.stream(bloomDay).max().getAsInt();
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(bloomDay, m, k, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
     }
 
-    boolean check(int[] bloomDay, int m, int k, int waitDay) {
-        int made = 0;
-        int count = 0;
-        for (int i = 0; i < bloomDay.length; ++i) {
-            if (bloomDay[i] <= waitDay) {
-                ++count;
-                if (count == k) {
-                    ++made;
-                    count = 0;
-                }
-            } else {
-                count = 0;
-            }
-        }
+    return result;
+  }
 
-        return made >= m;
+  boolean check(int[] bloomDay, int m, int k, int waitDay) {
+    int made = 0;
+    int count = 0;
+    for (int day : bloomDay) {
+      if (day <= waitDay) {
+        ++count;
+        if (count == k) {
+          ++made;
+          count = 0;
+        }
+      } else {
+        count = 0;
+      }
     }
+
+    return made >= m;
+  }
 }
