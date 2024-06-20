c3699e2bba8bb6c059b07e8e3f94aef8b6984b73
Kai Wang <charles.wangkai@gmail.com>
2024-06-20 09:11:51 +0800

    Magnetic Force Between Two Balls

--- magnetic-force-between-two-balls/Solution.java
+++ magnetic-force-between-two-balls/Solution.java
@@ -1,35 +1,35 @@
 import java.util.Arrays;
 
 class Solution {
-    public int maxDistance(int[] position, int m) {
-        position = Arrays.stream(position).boxed().sorted().mapToInt(x -> x).toArray();
+  public int maxDistance(int[] position, int m) {
+    Arrays.sort(position);
 
-        int result = -1;
-        int lower = 1;
-        int upper = position[position.length - 1] - position[0];
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
-            if (check(position, m, middle)) {
-                result = middle;
-                lower = middle + 1;
-            } else {
-                upper = middle - 1;
-            }
-        }
-
-        return result;
+    int result = -1;
+    int lower = 1;
+    int upper = position[position.length - 1] - position[0];
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(position, m, middle)) {
+        result = middle;
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
     }
 
-    boolean check(int[] position, int m, int distance) {
-        int count = 1;
-        int lastIndex = 0;
-        for (int i = 1; i < position.length; ++i) {
-            if (position[i] - position[lastIndex] >= distance) {
-                ++count;
-                lastIndex = i;
-            }
-        }
+    return result;
+  }
 
-        return count >= m;
+  boolean check(int[] position, int m, int distance) {
+    int count = 1;
+    int lastIndex = 0;
+    for (int i = 1; i < position.length; ++i) {
+      if (position[i] - position[lastIndex] >= distance) {
+        ++count;
+        lastIndex = i;
+      }
     }
+
+    return count >= m;
+  }
 }
