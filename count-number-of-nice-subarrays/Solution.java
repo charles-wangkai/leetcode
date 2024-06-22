950495d4c3d88cee3b7a78d98367a528dc8aadd9
Kai Wang <charles.wangkai@gmail.com>
2024-06-22 09:46:34 +0800

    Count Number of Nice Subarrays

--- count-number-of-nice-subarrays/Solution.java
+++ count-number-of-nice-subarrays/Solution.java
@@ -1,20 +1,19 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int numberOfSubarrays(int[] nums, int k) {
-		int[] oddIndices = IntStream.range(0, nums.length).filter(i -> nums[i] % 2 != 0).toArray();
+class Solution {
+  public int numberOfSubarrays(int[] nums, int k) {
+    int[] oddIndices = IntStream.range(0, nums.length).filter(i -> nums[i] % 2 == 1).toArray();
 
-		int result = 0;
-		for (int i = 0; i + k <= oddIndices.length; i++) {
-			result += computeDiff(nums.length, oddIndices, i - 1, i)
-					* computeDiff(nums.length, oddIndices, i + k - 1, i + k);
-		}
+    return IntStream.rangeClosed(0, oddIndices.length - k)
+        .map(
+            i ->
+                computeDiff(nums.length, oddIndices, i - 1, i)
+                    * computeDiff(nums.length, oddIndices, i + k - 1, i + k))
+        .sum();
+  }
 
-		return result;
-	}
-
-	int computeDiff(int length, int[] oddIndices, int leftIndex, int rightIndex) {
-		return (rightIndex == oddIndices.length ? length : oddIndices[rightIndex])
-				- (leftIndex == -1 ? -1 : oddIndices[leftIndex]);
-	}
+  int computeDiff(int length, int[] oddIndices, int leftIndex, int rightIndex) {
+    return ((rightIndex == oddIndices.length) ? length : oddIndices[rightIndex])
+        - ((leftIndex == -1) ? -1 : oddIndices[leftIndex]);
+  }
 }
