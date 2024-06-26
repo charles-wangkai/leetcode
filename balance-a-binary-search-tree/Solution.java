ee8cc273156be00147f68a156640067825ccac41
Kai Wang <charles.wangkai@gmail.com>
2024-06-26 14:08:59 +0800

    Balance a Binary Search Tree

--- balance-a-binary-search-tree/Solution.java
+++ balance-a-binary-search-tree/Solution.java
@@ -3,43 +3,49 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
-    }
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-    public TreeNode balanceBST(TreeNode root) {
-        List<Integer> values = new ArrayList<>();
-        inorder(values, root);
+  public TreeNode balanceBST(TreeNode root) {
+    List<Integer> values = new ArrayList<>();
+    search(values, root);
 
-        return buildBalancedBST(values, 0, values.size() - 1);
+    return buildBalancedBST(values, 0, values.size() - 1);
+  }
+
+  void search(List<Integer> values, TreeNode node) {
+    if (node != null) {
+      search(values, node.left);
+      values.add(node.val);
+      search(values, node.right);
+    }
+  }
+
+  TreeNode buildBalancedBST(List<Integer> values, int beginIndex, int endIndex) {
+    if (beginIndex > endIndex) {
+      return null;
     }
 
-    void inorder(List<Integer> values, TreeNode node) {
-        if (node == null) {
-            return;
-        }
+    int middleIndex = (beginIndex + endIndex) / 2;
 
-        inorder(values, node.left);
-        values.add(node.val);
-        inorder(values, node.right);
-    }
-
-    TreeNode buildBalancedBST(List<Integer> values, int beginIndex, int endIndex) {
-        if (beginIndex > endIndex) {
-            return null;
-        }
-
-        int middleIndex = (beginIndex + endIndex) / 2;
-        TreeNode node = new TreeNode(values.get(middleIndex));
-        node.left = buildBalancedBST(values, beginIndex, middleIndex - 1);
-        node.right = buildBalancedBST(values, middleIndex + 1, endIndex);
-
-        return node;
-    }
+    return new TreeNode(
+        values.get(middleIndex),
+        buildBalancedBST(values, beginIndex, middleIndex - 1),
+        buildBalancedBST(values, middleIndex + 1, endIndex));
+  }
 }
