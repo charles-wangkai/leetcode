ab312f12babd2dfe742834732ca7c514dcb9b3f1
Kai Wang <charles.wangkai@gmail.com>
2024-06-27 19:05:03 +0800

    Find Center of Star Graph

--- find-center-of-star-graph/Solution.java
+++ find-center-of-star-graph/Solution.java
@@ -2,12 +2,14 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int findCenter(int[][] edges) {
-    int[] degrees = new int[edges.length + 2];
+    int n = edges.length + 1;
+
+    int[] degrees = new int[n];
     for (int[] edge : edges) {
-      ++degrees[edge[0]];
-      ++degrees[edge[1]];
+      ++degrees[edge[0] - 1];
+      ++degrees[edge[1] - 1];
     }
 
-    return IntStream.range(1, degrees.length).filter(i -> degrees[i] != 1).findAny().getAsInt();
+    return IntStream.range(0, degrees.length).filter(i -> degrees[i] != 1).findAny().getAsInt() + 1;
   }
 }
