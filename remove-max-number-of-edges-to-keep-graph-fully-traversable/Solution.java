5a0d84f774dc46d0ff541b3e28f8ea5d7d115442
Kai Wang <charles.wangkai@gmail.com>
2024-06-30 08:22:43 +0800

    Remove Max Number of Edges to Keep Graph Fully Traversable

--- remove-max-number-of-edges-to-keep-graph-fully-traversable/Solution.java
+++ remove-max-number-of-edges-to-keep-graph-fully-traversable/Solution.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 class Solution {
   public int maxNumEdgesToRemove(int n, int[][] edges) {
-    @SuppressWarnings("unchecked")
-    List<int[]>[] typeToEdges = new List[4];
-    for (int i = 0; i < typeToEdges.length; ++i) {
-      typeToEdges[i] = new ArrayList<>();
+    Map<Integer, List<int[]>> typeToEdges = new HashMap<>();
+    for (int type = 1; type <= 3; ++type) {
+      typeToEdges.put(type, new ArrayList<>());
     }
     for (int[] edge : edges) {
-      typeToEdges[edge[0]].add(edge);
+      typeToEdges.get(edge[0]).add(edge);
     }
 
-    int removedCommonEdgeNum = computeRemovedCommonEdgeNum(n, typeToEdges[3]);
-    int removedEdgeNum1 = computeRemovedEdgeNum(n, typeToEdges[3], typeToEdges[1]);
-    int removedEdgeNum2 = computeRemovedEdgeNum(n, typeToEdges[3], typeToEdges[2]);
+    int removedCommonEdgeNum = computeRemovedCommonEdgeNum(n, typeToEdges.get(3));
+    int removedEdgeNum1 = computeRemovedEdgeNum(n, typeToEdges.get(3), typeToEdges.get(1));
+    int removedEdgeNum2 = computeRemovedEdgeNum(n, typeToEdges.get(3), typeToEdges.get(2));
 
     return (removedEdgeNum1 == -1 || removedEdgeNum2 == -1)
         ? -1
@@ -24,17 +25,17 @@ class Solution {
   }
 
   int computeRemovedCommonEdgeNum(int n, List<int[]> commonEdges) {
-    int[] parents = new int[n + 1];
+    int[] parents = new int[n];
     Arrays.fill(parents, -1);
 
     int result = 0;
     for (int[] edge : commonEdges) {
-      int root1 = findRoot(parents, edge[1]);
-      int root2 = findRoot(parents, edge[2]);
-      if (root1 != root2) {
-        parents[root2] = root1;
-      } else {
+      int root1 = findRoot(parents, edge[1] - 1);
+      int root2 = findRoot(parents, edge[2] - 1);
+      if (root2 == root1) {
         ++result;
+      } else {
+        parents[root2] = root1;
       }
     }
 
@@ -42,29 +43,29 @@ class Solution {
   }
 
   int computeRemovedEdgeNum(int n, List<int[]> commonEdges, List<int[]> onlyEdges) {
-    int[] parents = new int[n + 1];
+    int[] parents = new int[n];
     Arrays.fill(parents, -1);
 
     for (int[] edge : commonEdges) {
-      int root1 = findRoot(parents, edge[1]);
-      int root2 = findRoot(parents, edge[2]);
-      if (root1 != root2) {
+      int root1 = findRoot(parents, edge[1] - 1);
+      int root2 = findRoot(parents, edge[2] - 1);
+      if (root2 != root1) {
         parents[root2] = root1;
       }
     }
 
     int result = 0;
     for (int[] edge : onlyEdges) {
-      int root1 = findRoot(parents, edge[1]);
-      int root2 = findRoot(parents, edge[2]);
-      if (root1 != root2) {
-        parents[root2] = root1;
-      } else {
+      int root1 = findRoot(parents, edge[1] - 1);
+      int root2 = findRoot(parents, edge[2] - 1);
+      if (root2 == root1) {
         ++result;
+      } else {
+        parents[root2] = root1;
       }
     }
 
-    return (IntStream.rangeClosed(1, n).map(i -> findRoot(parents, i)).distinct().count() == 1)
+    return (IntStream.range(0, n).map(i -> findRoot(parents, i)).distinct().count() == 1)
         ? result
         : -1;
   }
