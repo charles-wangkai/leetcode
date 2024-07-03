28aaebae7610966294fc76993d0170298cf63b4f
Kai Wang <charles.wangkai@gmail.com>
2024-07-03 09:31:23 +0800

    Minimum Difference Between Largest and Smallest Value in Three Moves

--- minimum-difference-between-largest-and-smallest-value-in-three-moves/Solution.java
+++ minimum-difference-between-largest-and-smallest-value-in-three-moves/Solution.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int minDifference(int[] nums) {
-        if (nums.length <= 4) {
-            return 0;
-        }
-
-        nums = Arrays.stream(nums).boxed().sorted().mapToInt(x -> x).toArray();
-
-        int[] nums_ = nums;
-        return IntStream.rangeClosed(0, 3).map(i -> nums_[i + nums_.length - 4] - nums_[i]).min().getAsInt();
+  public int minDifference(int[] nums) {
+    if (nums.length <= 4) {
+      return 0;
     }
+
+    Arrays.sort(nums);
+
+    return IntStream.rangeClosed(0, 3)
+        .map(i -> nums[i + nums.length - 4] - nums[i])
+        .min()
+        .getAsInt();
+  }
 }
