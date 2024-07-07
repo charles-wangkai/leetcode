928c6213d12945810d6915fe4eb0f239071d40e3
Kai Wang <charles.wangkai@gmail.com>
2024-07-07 08:02:57 +0800

    Water Bottles

--- water-bottles/Solution.java
+++ water-bottles/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int result = numBottles;
-        while (numBottles >= numExchange) {
-            result += numBottles / numExchange;
-            numBottles = numBottles / numExchange + numBottles % numExchange;
-        }
-
-        return result;
+  public int numWaterBottles(int numBottles, int numExchange) {
+    int result = numBottles;
+    while (numBottles >= numExchange) {
+      result += numBottles / numExchange;
+      numBottles = numBottles / numExchange + numBottles % numExchange;
     }
+
+    return result;
+  }
 }
