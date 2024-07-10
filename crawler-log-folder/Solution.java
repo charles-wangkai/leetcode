818019b60dbf0137046528a2aefd6ceb7c4262b7
Kai Wang <charles.wangkai@gmail.com>
2024-07-10 09:00:34 +0800

    Crawler Log Folder

--- crawler-log-folder/Solution.java
+++ crawler-log-folder/Solution.java
@@ -1,14 +1,16 @@
 class Solution {
-    public int minOperations(String[] logs) {
-        int depth = 0;
-        for (String log : logs) {
-            if (log.equals("../")) {
-                depth = Math.max(0, depth - 1);
-            } else if (!log.equals("./")) {
-                ++depth;
-            }
+  public int minOperations(String[] logs) {
+    int depth = 0;
+    for (String log : logs) {
+      if (log.equals("../")) {
+        if (depth != 0) {
+          --depth;
         }
-
-        return depth;
+      } else if (!log.equals("./")) {
+        ++depth;
+      }
     }
+
+    return depth;
+  }
 }
