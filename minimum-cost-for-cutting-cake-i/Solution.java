69fb626f955bb629ec48bb9f0b26cf3ca785a886
Kai Wang <charles.wangkai@gmail.com>
2024-07-14 23:39:25 +0800

    Minimum Cost for Cutting Cake I

---
+++ minimum-cost-for-cutting-cake-i/Solution.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-cost-for-cutting-cake-ii/solutions/5473140/java-c-python-greedy/
+
+import java.util.Arrays;
+
+class Solution {
+  public int minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+    Arrays.sort(horizontalCut);
+    Arrays.sort(verticalCut);
+
+    int result = 0;
+    int horizontalIndex = horizontalCut.length - 1;
+    int horizontalFactor = 1;
+    int verticalIndex = verticalCut.length - 1;
+    int verticalFactor = 1;
+    while (horizontalIndex != -1 || verticalIndex != -1) {
+      if (verticalIndex == -1
+          || (horizontalIndex != -1
+              && horizontalCut[horizontalIndex] > verticalCut[verticalIndex])) {
+        result += horizontalCut[horizontalIndex] * horizontalFactor;
+        ++verticalFactor;
+        --horizontalIndex;
+      } else {
+        result += verticalCut[verticalIndex] * verticalFactor;
+        ++horizontalFactor;
+        --verticalIndex;
+      }
+    }
+
+    return result;
+  }
+}
