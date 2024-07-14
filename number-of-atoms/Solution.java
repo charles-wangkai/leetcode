7e1496ad54c4ece699b405f32f6bc695ff132333
Kai Wang <charles.wangkai@gmail.com>
2024-07-14 08:36:44 +0800

    Number of Atoms

--- number-of-atoms/Solution.java
+++ number-of-atoms/Solution.java
@@ -5,19 +5,20 @@ import java.util.stream.Stream;
 
 public class Solution {
   public String countOfAtoms(String formula) {
-    Map<String, Integer> element2count = parse(formula);
-    return element2count.keySet().stream()
+    Map<String, Integer> elementToCount = parse(formula);
+
+    return elementToCount.keySet().stream()
         .sorted()
         .map(
             element ->
-                element + (element2count.get(element) == 1 ? "" : element2count.get(element)))
+                element + ((elementToCount.get(element) == 1) ? "" : elementToCount.get(element)))
         .collect(Collectors.joining());
   }
 
   Map<String, Integer> parse(String s) {
-    Map<String, Integer> element2count = new HashMap<>();
+    Map<String, Integer> elementToCount = new HashMap<>();
     int index = 0;
-    while (index < s.length()) {
+    while (index != s.length()) {
       if (s.charAt(index) == '(') {
         int partLength = readPart(s, index);
 
@@ -28,12 +29,12 @@ public class Solution {
                 : Integer.parseInt(
                     s.substring(index + partLength, index + partLength + countLength));
 
-        element2count =
+        elementToCount =
             add(
-                element2count,
+                elementToCount,
                 multiply(parse(s.substring(index + 1, index + partLength - 1)), count));
 
-        index = index + partLength + countLength;
+        index += partLength + countLength;
       } else {
         int elementLength = readElement(s, index);
         String element = s.substring(index, index + elementLength);
@@ -44,57 +45,61 @@ public class Solution {
             (countLength == 0) ? 1 : Integer.parseInt(s.substring(index, index + countLength));
         index += countLength;
 
-        element2count.put(element, element2count.getOrDefault(element, 0) + count);
+        elementToCount.put(element, elementToCount.getOrDefault(element, 0) + count);
       }
     }
-    return element2count;
+
+    return elementToCount;
   }
 
   int readPart(String s, int index) {
+    ++index;
     int depth = 1;
-    index++;
     int length = 1;
     while (depth != 0) {
-      char ch = s.charAt(index);
-      if (ch == '(') {
-        depth++;
-      } else if (ch == ')') {
-        depth--;
+      char c = s.charAt(index);
+      if (c == '(') {
+        ++depth;
+      } else if (c == ')') {
+        --depth;
       }
 
-      index++;
-      length++;
+      ++index;
+      ++length;
     }
+
     return length;
   }
 
   int readNumber(String s, int index) {
     int length = 0;
-    while (index < s.length() && Character.isDigit(s.charAt(index))) {
-      index++;
-      length++;
+    while (index != s.length() && Character.isDigit(s.charAt(index))) {
+      ++index;
+      ++length;
     }
+
     return length;
   }
 
   int readElement(String s, int index) {
-    index++;
+    ++index;
     int length = 1;
-    while (index < s.length() && Character.isLowerCase(s.charAt(index))) {
-      index++;
-      length++;
+    while (index != s.length() && Character.isLowerCase(s.charAt(index))) {
+      ++index;
+      ++length;
     }
+
     return length;
   }
 
-  Map<String, Integer> multiply(Map<String, Integer> element2count, int factor) {
-    return element2count.entrySet().stream()
+  Map<String, Integer> multiply(Map<String, Integer> elementToCount, int factor) {
+    return elementToCount.entrySet().stream()
         .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue() * factor));
   }
 
   Map<String, Integer> add(
-      Map<String, Integer> element2count1, Map<String, Integer> element2count2) {
-    return Stream.concat(element2count1.entrySet().stream(), element2count2.entrySet().stream())
+      Map<String, Integer> elementToCount1, Map<String, Integer> elementToCount2) {
+    return Stream.concat(elementToCount1.entrySet().stream(), elementToCount2.entrySet().stream())
         .collect(
             Collectors.toMap(
                 Map.Entry::getKey, Map.Entry::getValue, (count1, count2) -> count1 + count2));
