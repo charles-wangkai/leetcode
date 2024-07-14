d289df4e1ecef22ef3e25826ca83488f316def1e
Kai Wang <charles.wangkai@gmail.com>
2024-07-14 21:54:28 +0800

    Delete Nodes From Linked List Present in Array

---
+++ delete-nodes-from-linked-list-present-in-array/Solution.java
@@ -0,0 +1,39 @@
+// Definition for singly-linked list.
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public ListNode modifiedList(int[] nums, ListNode head) {
+    Set<Integer> values = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+    ListNode tempHead = new ListNode(0, head);
+    ListNode p = tempHead;
+    while (p.next != null) {
+      if (values.contains(p.next.val)) {
+        p.next = p.next.next;
+      } else {
+        p = p.next;
+      }
+    }
+
+    return tempHead.next;
+  }
+}
