116a4296aa432d5ee1c63c8f02b2933374c9caf9
Kai Wang <charles.wangkai@gmail.com>
2024-07-18 09:30:33 +0800

    Number of Good Leaf Nodes Pairs

--- number-of-good-leaf-nodes-pairs/Solution.java
+++ number-of-good-leaf-nodes-pairs/Solution.java
@@ -1,61 +1,60 @@
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode() {
-    }
+  TreeNode() {}
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+  TreeNode(int val) {
+    this.val = val;
+  }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 class Solution {
-    int pairNum;
+  int pairNum;
 
-    public int countPairs(TreeNode root, int distance) {
-        if (distance == 1) {
-            return 0;
-        }
-
-        pairNum = 0;
-        search(distance, root);
-
-        return pairNum;
+  public int countPairs(TreeNode root, int distance) {
+    if (distance == 1) {
+      return 0;
     }
 
-    int[] search(int distance, TreeNode node) {
-        int[] leafCounts = new int[distance - 1];
+    pairNum = 0;
+    search(distance, root);
 
-        if (node != null) {
-            int[] leftSubResult = search(distance, node.left);
-            int[] rightSubResult = search(distance, node.right);
+    return pairNum;
+  }
 
-            for (int i = 0; i < leftSubResult.length; ++i) {
-                for (int j = 0; j < rightSubResult.length; ++j) {
-                    if (i + j + 2 <= distance) {
-                        pairNum += leftSubResult[i] * rightSubResult[j];
-                    }
-                }
-            }
+  int[] search(int distance, TreeNode node) {
+    int[] leafCounts = new int[distance - 1];
 
-            if (node.left == null && node.right == null) {
-                ++leafCounts[0];
-            }
+    if (node != null) {
+      int[] leftSubResult = search(distance, node.left);
+      int[] rightSubResult = search(distance, node.right);
 
-            for (int i = 1; i < leafCounts.length; ++i) {
-                leafCounts[i] = leftSubResult[i - 1] + rightSubResult[i - 1];
-            }
+      for (int i = 0; i < leftSubResult.length; ++i) {
+        for (int j = 0; j < rightSubResult.length; ++j) {
+          if ((i + 1) + (j + 1) <= distance) {
+            pairNum += leftSubResult[i] * rightSubResult[j];
+          }
         }
+      }
 
-        return leafCounts;
+      if (node.left == null && node.right == null) {
+        ++leafCounts[0];
+      }
+
+      for (int i = 1; i < leafCounts.length; ++i) {
+        leafCounts[i] = leftSubResult[i - 1] + rightSubResult[i - 1];
+      }
     }
+
+    return leafCounts;
+  }
 }
