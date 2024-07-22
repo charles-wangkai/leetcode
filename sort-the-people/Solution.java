75835abfdc7c03ea49885703f7423a1cd6eb85ce
Kai Wang <charles.wangkai@gmail.com>
2024-07-22 08:22:01 +0800

    Sort the People

--- sort-the-people/Solution.java
+++ sort-the-people/Solution.java
@@ -5,7 +5,7 @@ class Solution {
   public String[] sortPeople(String[] names, int[] heights) {
     return IntStream.range(0, names.length)
         .boxed()
-        .sorted(Comparator.comparing((Integer i) -> heights[i]).reversed())
+        .sorted(Comparator.<Integer, Integer>comparing(i -> heights[i]).reversed())
         .map(i -> names[i])
         .toArray(String[]::new);
   }
