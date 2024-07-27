ede6f6aae4d0cf5ea7968b2b6c70012e3f260843
Kai Wang <charles.wangkai@gmail.com>
2024-07-27 12:46:14 +0800

    Minimum Cost to Convert String I

--- minimum-cost-to-convert-string-i/Solution.java
+++ minimum-cost-to-convert-string-i/Solution.java
@@ -1,27 +1,27 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
   static final int ALPHABET_SIZE = 26;
 
   public long minimumCost(
       String source, String target, char[] original, char[] changed, int[] cost) {
     int[][] distances = buildDistances(original, changed, cost);
+    int[] costs =
+        IntStream.range(0, source.length())
+            .map(i -> distances[source.charAt(i) - 'a'][target.charAt(i) - 'a'])
+            .toArray();
 
-    long result = 0;
-    for (int i = 0; i < source.length(); ++i) {
-      if (distances[source.charAt(i) - 'a'][target.charAt(i) - 'a'] == Integer.MAX_VALUE) {
-        return -1;
-      }
-
-      result += distances[source.charAt(i) - 'a'][target.charAt(i) - 'a'];
-    }
-
-    return result;
+    return Arrays.stream(costs).anyMatch(c -> c == Integer.MAX_VALUE)
+        ? -1
+        : Arrays.stream(costs).asLongStream().sum();
   }
 
   int[][] buildDistances(char[] original, char[] changed, int[] cost) {
     int[][] distances = new int[ALPHABET_SIZE][ALPHABET_SIZE];
     for (int i = 0; i < ALPHABET_SIZE; ++i) {
       for (int j = 0; j < ALPHABET_SIZE; ++j) {
-        distances[i][j] = (i == j) ? 0 : Integer.MAX_VALUE;
+        distances[i][j] = (j == i) ? 0 : Integer.MAX_VALUE;
       }
     }
 
