e608b223b87633ba9a5f0b67bbda5fa0c97d15c5
Kai Wang <charles.wangkai@gmail.com>
2024-08-03 08:07:13 +0800

    Make Two Arrays Equal by Reversing Subarrays

--- make-two-arrays-equal-by-reversing-sub-arrays/Solution.java
+++ make-two-arrays-equal-by-reversing-sub-arrays/Solution.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
-public class Solution {
-    public boolean canBeEqual(int[] target, int[] arr) {
-        return Arrays.equals(Arrays.stream(target).sorted().toArray(), Arrays.stream(arr).sorted().toArray());
-    }
+class Solution {
+  public boolean canBeEqual(int[] target, int[] arr) {
+    return Arrays.equals(
+        Arrays.stream(target).sorted().toArray(), Arrays.stream(arr).sorted().toArray());
+  }
 }
