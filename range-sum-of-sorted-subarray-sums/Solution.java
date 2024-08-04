15a64369ea78fbd9e8c51b02cdc177b7df4ee389
Kai Wang <charles.wangkai@gmail.com>
2024-08-04 08:36:35 +0800

    Range Sum of Sorted Subarray Sums

--- range-sum-of-sorted-subarray-sums/Solution.java
+++ range-sum-of-sorted-subarray-sums/Solution.java
@@ -4,18 +4,19 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Integer> sums = new ArrayList<>();
-        for (int beginIndex = 0; beginIndex < nums.length; ++beginIndex) {
-            int sum = 0;
-            for (int endIndex = beginIndex; endIndex < nums.length; ++endIndex) {
-                sum += nums[endIndex];
-                sums.add(sum);
-            }
-        }
-
-        Collections.sort(sums);
-
-        return (int) (IntStream.rangeClosed(left - 1, right - 1).map(sums::get).asLongStream().sum() % 1_000_000_007);
+  public int rangeSum(int[] nums, int n, int left, int right) {
+    List<Integer> sums = new ArrayList<>();
+    for (int beginIndex = 0; beginIndex < nums.length; ++beginIndex) {
+      int sum = 0;
+      for (int endIndex = beginIndex; endIndex < nums.length; ++endIndex) {
+        sum += nums[endIndex];
+        sums.add(sum);
+      }
     }
+    Collections.sort(sums);
+
+    return (int)
+        (IntStream.rangeClosed(left - 1, right - 1).map(sums::get).asLongStream().sum()
+            % 1_000_000_007);
+  }
 }
