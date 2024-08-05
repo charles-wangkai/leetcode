4de8f0455f072d53f88f1d20a409abb7bd9ce752
Kai Wang <charles.wangkai@gmail.com>
2024-08-05 08:06:51 +0800

    Kth Distinct String in an Array

--- kth-distinct-string-in-an-array/Solution.java
+++ kth-distinct-string-in-an-array/Solution.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 class Solution {
   public String kthDistinct(String[] arr, int k) {
-    return IntStream.range(0, arr.length)
-        .filter(i -> Arrays.stream(arr).filter(s -> s.equals(arr[i])).count() == 1)
+    return Arrays.stream(arr)
+        .filter(s -> Arrays.stream(arr).filter(s::equals).count() == 1)
         .skip(k - 1)
-        .mapToObj(i -> arr[i])
         .findFirst()
         .orElse("");
   }
