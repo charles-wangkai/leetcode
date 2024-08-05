5cc49f2a69cc7bc9cf5c88af54da0ea4377c1dc3
Kai Wang <charles.wangkai@gmail.com>
2024-08-05 21:27:59 +0800

    Shortest Distance After Road Addition Queries I

---
+++ shortest-distance-after-road-addition-queries-i/Solution.java
@@ -0,0 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+  public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+    @SuppressWarnings("unchecked")
+    List<Integer>[] adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < n - 1; ++i) {
+      adjLists[i].add(i + 1);
+    }
+
+    int[] result = new int[queries.length];
+    for (int i = 0; i < result.length; ++i) {
+      adjLists[queries[i][0]].add(queries[i][1]);
+      result[i] = computeMinDistance(adjLists);
+    }
+
+    return result;
+  }
+
+  int computeMinDistance(List<Integer>[] adjLists) {
+    int n = adjLists.length;
+
+    int[] distances = new int[n];
+    Arrays.fill(distances, -1);
+    distances[0] = 0;
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(0);
+
+    while (!queue.isEmpty()) {
+      int head = queue.poll();
+      for (int adj : adjLists[head]) {
+        if (distances[adj] == -1) {
+          distances[adj] = distances[head] + 1;
+          queue.offer(adj);
+        }
+      }
+    }
+
+    return distances[n - 1];
+  }
+}
