ef8821d9eccf58583d097134f29814ac8e281fbe
Kai Wang <charles.wangkai@gmail.com>
2024-08-09 09:27:36 +0800

    Magic Squares In Grid

--- magic-squares-in-grid/Solution.java
+++ magic-squares-in-grid/Solution.java
@@ -1,30 +1,36 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int numMagicSquaresInside(int[][] grid) {
-		return IntStream.range(0, grid.length - 2)
-				.map(r -> (int) IntStream.range(0, grid[0].length - 2).filter(c -> isMagicSquare(grid, r, c)).count())
-				.sum();
-	}
+class Solution {
+  public int numMagicSquaresInside(int[][] grid) {
+    int result = 0;
+    for (int beginR = 0; beginR < grid.length - 2; ++beginR) {
+      for (int beginC = 0; beginC < grid[beginR].length - 2; ++beginC) {
+        if (isMagicSquare(grid, beginR, beginC)) {
+          ++result;
+        }
+      }
+    }
 
-	boolean isMagicSquare(int[][] grid, int r, int c) {
-		int[] numbers = { grid[r][c], grid[r][c + 1], grid[r][c + 2], grid[r + 1][c], grid[r + 1][c + 1],
-				grid[r + 1][c + 2], grid[r + 2][c], grid[r + 2][c + 1], grid[r + 2][c + 2] };
+    return result;
+  }
 
-		if (!(Arrays.stream(numbers).distinct().count() == 9 && Arrays.stream(numbers).min().getAsInt() == 1
-				&& Arrays.stream(numbers).max().getAsInt() == 9)) {
-			return false;
-		}
+  boolean isMagicSquare(int[][] grid, int beginR, int beginC) {
+    int[] values =
+        IntStream.range(beginR, beginR + 3)
+            .flatMap(r -> IntStream.range(beginC, beginC + 3).map(c -> grid[r][c]))
+            .toArray();
+    int sum = IntStream.range(0, 3).map(i -> values[i]).sum();
 
-		int sum = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
-
-		return grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2] == sum
-				&& grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2] == sum
-				&& grid[r][c] + grid[r + 1][c] + grid[r + 2][c] == sum
-				&& grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1] == sum
-				&& grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2] == sum
-				&& grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] == sum
-				&& grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] == sum;
-	}
+    return Arrays.stream(values).distinct().count() == 9
+        && Arrays.stream(values).min().getAsInt() == 1
+        && Arrays.stream(values).max().getAsInt() == 9
+        && IntStream.range(3, 6).map(i -> values[i]).sum() == sum
+        && IntStream.range(6, 9).map(i -> values[i]).sum() == sum
+        && IntStream.range(0, 3).map(i -> values[i * 3]).sum() == sum
+        && IntStream.range(0, 3).map(i -> values[i * 3 + 1]).sum() == sum
+        && IntStream.range(0, 3).map(i -> values[i * 3 + 2]).sum() == sum
+        && values[0] + values[4] + values[8] == sum
+        && values[2] + values[4] + values[6] == sum;
+  }
 }
