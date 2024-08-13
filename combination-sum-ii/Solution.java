85389cf34e582b11e7bad5a9c8931ab4e60dcb2f
Kai Wang <charles.wangkai@gmail.com>
2024-08-13 10:24:57 +0800

    Combination Sum II

--- combination-sum-ii/Solution.java
+++ combination-sum-ii/Solution.java
@@ -2,37 +2,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
-  public List<List<Integer>> combinationSum2(int[] num, int target) {
-    Arrays.sort(num);
+class Solution {
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+
     List<List<Integer>> solutions = new ArrayList<>();
-    search(solutions, num, target, new ArrayList<>(), 0);
+    search(solutions, candidates, target, new ArrayList<>(), 0);
+
     return solutions;
   }
 
   void search(
       List<List<Integer>> solutions,
-      int[] num,
-      int remain,
+      int[] candidates,
+      int rest,
       List<Integer> chosenIndices,
       int index) {
-    if (remain == 0) {
-      List<Integer> solution = new ArrayList<>();
-      for (int chosenIndex : chosenIndices) {
-        solution.add(num[chosenIndex]);
-      }
-      solutions.add(solution);
+    if (rest == 0) {
+      solutions.add(chosenIndices.stream().map(chosenIndex -> candidates[chosenIndex]).toList());
+
       return;
     }
-    if (index == num.length || remain < num[index]) {
+    if (index == candidates.length || candidates[index] > rest) {
       return;
     }
-    search(solutions, num, remain, chosenIndices, index + 1);
-    if (!(index > 0
-        && num[index] == num[index - 1]
+
+    search(solutions, candidates, rest, chosenIndices, index + 1);
+
+    if (!(index != 0
+        && candidates[index] == candidates[index - 1]
         && (chosenIndices.isEmpty() || chosenIndices.get(chosenIndices.size() - 1) != index - 1))) {
       chosenIndices.add(index);
-      search(solutions, num, remain - num[index], chosenIndices, index + 1);
+      search(solutions, candidates, rest - candidates[index], chosenIndices, index + 1);
       chosenIndices.remove(chosenIndices.size() - 1);
     }
   }
