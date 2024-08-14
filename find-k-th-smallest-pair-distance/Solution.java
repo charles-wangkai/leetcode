c25f110e3b51fe9d5dda21ed6a6614beb9e0fc6b
Kai Wang <charles.wangkai@gmail.com>
2024-08-14 20:56:59 +0800

    Find K-th Smallest Pair Distance

--- find-k-th-smallest-pair-distance/Solution.java
+++ find-k-th-smallest-pair-distance/Solution.java
@@ -1,35 +1,37 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int smallestDistancePair(int[] nums, int k) {
-		Arrays.sort(nums);
+class Solution {
+  public int smallestDistancePair(int[] nums, int k) {
+    Arrays.sort(nums);
 
-		int lower = IntStream.range(0, nums.length - 1).map(i -> nums[i + 1] - nums[i]).min().getAsInt();
-		int upper = nums[nums.length - 1] - nums[0];
-		int result = -1;
-		while (lower <= upper) {
-			int middle = (lower + upper) / 2;
+    int result = -1;
+    int lower =
+        IntStream.range(0, nums.length - 1).map(i -> nums[i + 1] - nums[i]).min().getAsInt();
+    int upper = nums[nums.length - 1] - nums[0];
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(nums, k, middle)) {
+        result = middle;
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
 
-			if (computePairNum(nums, middle) < k) {
-				result = middle;
-				lower = middle + 1;
-			} else {
-				upper = middle - 1;
-			}
-		}
-		return result;
-	}
+    return result;
+  }
 
-	int computePairNum(int[] nums, int distance) {
-		int pairNum = 0;
-		int endIndex = 0;
-		for (int i = 0; i < nums.length; i++) {
-			while (endIndex + 1 < nums.length && nums[endIndex + 1] - nums[i] < distance) {
-				endIndex++;
-			}
-			pairNum += endIndex - i;
-		}
-		return pairNum;
-	}
+  boolean check(int[] nums, int k, int distance) {
+    int pairNum = 0;
+    int endIndex = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      while (endIndex != nums.length - 1 && nums[endIndex + 1] - nums[i] <= distance) {
+        ++endIndex;
+      }
+      pairNum += endIndex - i;
+    }
+
+    return pairNum >= k;
+  }
 }
