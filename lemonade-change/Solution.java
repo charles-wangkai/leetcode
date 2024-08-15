a41fd7246eb24347210b217cc439bb72f5d71aff
Kai Wang <charles.wangkai@gmail.com>
2024-08-15 09:43:10 +0800

    Lemonade Change

--- lemonade-change/Solution.java
+++ lemonade-change/Solution.java
@@ -1,29 +1,27 @@
-public class Solution {
-	public boolean lemonadeChange(int[] bills) {
-		int count5 = 0;
-		int count10 = 0;
-		for (int bill : bills) {
-			if (bill == 5) {
-				count5++;
-			} else if (bill == 10) {
-				count10++;
+class Solution {
+  public boolean lemonadeChange(int[] bills) {
+    int count5 = 0;
+    int count10 = 0;
+    for (int bill : bills) {
+      if (bill == 5) {
+        ++count5;
+      } else if (bill == 10) {
+        if (count5 == 0) {
+          return false;
+        }
 
-				if (count5 >= 1) {
-					count5--;
-				} else {
-					return false;
-				}
-			} else if (bill == 20) {
-				if (count10 >= 1 && count5 >= 1) {
-					count10--;
-					count5--;
-				} else if (count5 >= 3) {
-					count5 -= 3;
-				} else {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+        ++count10;
+        --count5;
+      } else if (count10 != 0 && count5 != 0) {
+        --count10;
+        --count5;
+      } else if (count5 >= 3) {
+        count5 -= 3;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
