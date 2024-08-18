7b236808ac9fb381ae1e33ffd18302b2145ca496
Kai Wang <charles.wangkai@gmail.com>
2024-08-18 08:39:28 +0800

    Ugly Number II

--- ugly-number-ii/Solution.java
+++ ugly-number-ii/Solution.java
@@ -1,30 +1,23 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-	static final int[] FACTORS = { 2, 3, 5 };
+  static final int[] FACTORS = {2, 3, 5};
 
-	public int nthUglyNumber(int n) {
-		PriorityQueue<Element> pq = new PriorityQueue<>((e1, e2) -> Long.compare(e1.number, e2.number));
-		pq.offer(new Element(1, 0));
+  public int nthUglyNumber(int n) {
+    PriorityQueue<Element> pq = new PriorityQueue<>(Comparator.comparing(Element::value));
+    pq.offer(new Element(1, 0));
 
-		for (int i = 0; i < n - 1; i++) {
-			Element head = pq.poll();
+    for (int i = 0; i < n - 1; ++i) {
+      Element head = pq.poll();
 
-			for (int j = head.maxFactorIndex; j < FACTORS.length; ++j) {
-				pq.offer(new Element(head.number * FACTORS[j], j));
-			}
-		}
+      for (int j = head.beginFactorIndex(); j < FACTORS.length; ++j) {
+        pq.offer(new Element(head.value() * FACTORS[j], j));
+      }
+    }
 
-		return (int) pq.poll().number;
-	}
+    return (int) pq.poll().value();
+  }
 }
 
-class Element {
-	long number;
-	int maxFactorIndex;
-
-	Element(long number, int maxFactorIndex) {
-		this.number = number;
-		this.maxFactorIndex = maxFactorIndex;
-	}
-}
+record Element(long value, int beginFactorIndex) {}
