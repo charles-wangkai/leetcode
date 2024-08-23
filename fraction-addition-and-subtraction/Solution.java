11fda310554a8a1a5d316cb8a76e9c60a54f7add
Kai Wang <charles.wangkai@gmail.com>
2024-08-23 08:19:55 +0800

    Fraction Addition and Subtraction

--- fraction-addition-and-subtraction/Solution.java
+++ fraction-addition-and-subtraction/Solution.java
@@ -1,42 +1,42 @@
-public class Solution {
-	public String fractionAddition(String expression) {
-		Rational result = new Rational(0, 1);
+class Solution {
+  public String fractionAddition(String expression) {
+    Rational result = new Rational(0, 1);
 
-		int beginIndex = 0;
-		for (int i = 1; i <= expression.length(); i++) {
-			if (i == expression.length() || expression.charAt(i) == '+' || expression.charAt(i) == '-') {
-				String term = expression.substring(beginIndex, i);
+    int beginIndex = 0;
+    for (int i = 1; i <= expression.length(); ++i) {
+      if (i == expression.length() || expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+        String term = expression.substring(beginIndex, i);
+        int divisionIndex = term.indexOf('/');
+        int numerator = Integer.parseInt(term.substring(0, divisionIndex));
+        int denominator = Integer.parseInt(term.substring(divisionIndex + 1));
 
-				int divisionIndex = term.indexOf('/');
-				int numerator = Integer.parseInt(term.substring(0, divisionIndex));
-				int denominator = Integer.parseInt(term.substring(divisionIndex + 1));
+        result = result.add(new Rational(numerator, denominator));
 
-				result = Rational.add(result, new Rational(numerator, denominator));
+        beginIndex = i;
+      }
+    }
 
-				beginIndex = i;
-			}
-		}
-
-		return String.format("%d/%d", result.numerator, result.denominator);
-	}
+    return "%d/%d".formatted(result.numerator, result.denominator);
+  }
 }
 
 class Rational {
-	int numerator;
-	int denominator;
+  int numerator;
+  int denominator;
 
-	Rational(int numerator, int denominator) {
-		int g = gcd(Math.abs(numerator), denominator);
-		this.numerator = numerator / g;
-		this.denominator = denominator / g;
-	}
+  Rational(int numerator, int denominator) {
+    int g = gcd(Math.abs(numerator), denominator);
+    this.numerator = numerator / g;
+    this.denominator = denominator / g;
+  }
 
-	int gcd(int a, int b) {
-		return (b == 0) ? a : gcd(b, a % b);
-	}
+  int gcd(int x, int y) {
+    return (y == 0) ? x : gcd(y, x % y);
+  }
 
-	static Rational add(Rational r1, Rational r2) {
-		return new Rational(r1.numerator * r2.denominator + r2.numerator * r1.denominator,
-				r1.denominator * r2.denominator);
-	}
+  Rational add(Rational other) {
+    return new Rational(
+        numerator * other.denominator + other.numerator * denominator,
+        denominator * other.denominator);
+  }
 }
