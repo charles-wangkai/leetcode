bec926d8d9be1aeea9dfa28f6cbcd9ed50eb7202
Kai Wang <charles.wangkai@gmail.com>
2024-09-07 08:53:35 +0800

    Linked List in Binary Tree

--- linked-list-in-binary-tree/Solution.java
+++ linked-list-in-binary-tree/Solution.java
@@ -1,31 +1,49 @@
 // Definition for singly-linked list.
 class ListNode {
-    int val;
-    ListNode next;
+  int val;
+  ListNode next;
 
-    ListNode(int x) {
-        val = x;
-    }
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
 // Definition for a binary tree node.
 class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
-    }
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-    public boolean isSubPath(ListNode head, TreeNode root) {
-        return root != null && (check(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right));
-    }
+class Solution {
+  public boolean isSubPath(ListNode head, TreeNode root) {
+    return root != null
+        && (check(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right));
+  }
 
-    boolean check(ListNode head, TreeNode root) {
-        return head == null || (root != null && head.val == root.val
-                && (check(head.next, root.left) || check(head.next, root.right)));
-    }
+  boolean check(ListNode head, TreeNode root) {
+    return head == null
+        || (root != null
+            && head.val == root.val
+            && (check(head.next, root.left) || check(head.next, root.right)));
+  }
 }
