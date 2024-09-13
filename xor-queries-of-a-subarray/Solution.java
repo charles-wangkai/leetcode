6ee3cb0eace30b2d0e8dfdfb739b2ecaaa1446bf
Kai Wang <charles.wangkai@gmail.com>
2024-09-13 08:03:42 +0800

    XOR Queries of a Subarray

--- xor-queries-of-a-subarray/Solution.java
+++ xor-queries-of-a-subarray/Solution.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 
-public class Solution {
-	public int[] xorQueries(int[] arr, int[][] queries) {
-		int[] prefixXors = new int[arr.length + 1];
-		int prefixXor = 0;
-		for (int i = 1; i < prefixXors.length; ++i) {
-			prefixXor ^= arr[i - 1];
-			prefixXors[i] = prefixXor;
-		}
+class Solution {
+  public int[] xorQueries(int[] arr, int[][] queries) {
+    int[] prefixXors = new int[arr.length + 1];
+    for (int i = 1; i < prefixXors.length; ++i) {
+      prefixXors[i] = prefixXors[i - 1] ^ arr[i - 1];
+    }
 
-		return Arrays.stream(queries).mapToInt(query -> prefixXors[query[1] + 1] ^ prefixXors[query[0]]).toArray();
-	}
+    return Arrays.stream(queries)
+        .mapToInt(query -> prefixXors[query[1] + 1] ^ prefixXors[query[0]])
+        .toArray();
+  }
 }
