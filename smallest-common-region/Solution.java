3559c98d67b15833f8dc800b2ea5e06062b4c0ce
Kai Wang <charles.wangkai@gmail.com>
2024-09-15 22:14:27 +0800

    Smallest Common Region

--- smallest-common-region/Solution.java
+++ smallest-common-region/Solution.java
@@ -4,42 +4,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Solution {
-	public String findSmallestRegion(List<List<String>> regions, String region1, String region2) {
-		Map<String, String> regionToParent = new HashMap<>();
-		for (List<String> level : regions) {
-			String parent = level.get(0);
-			for (int i = 1; i < level.size(); i++) {
-				regionToParent.put(level.get(i), parent);
-			}
-		}
+class Solution {
+  public String findSmallestRegion(List<List<String>> regions, String region1, String region2) {
+    Map<String, String> regionToParent = new HashMap<>();
+    for (List<String> group : regions) {
+      for (int i = 1; i < group.size(); ++i) {
+        regionToParent.put(group.get(i), group.get(0));
+      }
+    }
 
-		List<String> path1 = findPath(regionToParent, region1);
-		List<String> path2 = findPath(regionToParent, region2);
+    List<String> path1 = findPath(regionToParent, region1);
+    List<String> path2 = findPath(regionToParent, region2);
 
-		int index = 0;
-		while (index + 1 < path1.size() && index + 1 < path2.size()
-				&& path1.get(index + 1).equals(path2.get(index + 1))) {
-			index++;
-		}
+    int index = 0;
+    while (index != path1.size() - 1
+        && index != path2.size() - 1
+        && path1.get(index + 1).equals(path2.get(index + 1))) {
+      ++index;
+    }
 
-		return path1.get(index);
-	}
+    return path1.get(index);
+  }
 
-	List<String> findPath(Map<String, String> regionToParent, String region) {
-		List<String> result = new ArrayList<>();
-		while (true) {
-			result.add(region);
+  List<String> findPath(Map<String, String> regionToParent, String region) {
+    List<String> result = new ArrayList<>();
+    while (true) {
+      result.add(region);
+      if (!regionToParent.containsKey(region)) {
+        break;
+      }
 
-			if (!regionToParent.containsKey(region)) {
-				break;
-			}
+      region = regionToParent.get(region);
+    }
+    Collections.reverse(result);
 
-			region = regionToParent.get(region);
-		}
-
-		Collections.reverse(result);
-
-		return result;
-	}
+    return result;
+  }
 }
