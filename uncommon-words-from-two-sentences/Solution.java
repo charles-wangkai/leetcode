fae1dfe449cdbed1f2b47c623c80bd1d8ea3aeb8
Kai Wang <charles.wangkai@gmail.com>
2024-09-17 08:05:47 +0800

    Uncommon Words from Two Sentences

--- uncommon-words-from-two-sentences/Solution.java
+++ uncommon-words-from-two-sentences/Solution.java
@@ -1,35 +1,15 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class Solution {
-	public String[] uncommonFromSentences(String A, String B) {
-		Map<String, Integer> wordToCountA = buildWordToCount(A);
-		Map<String, Integer> wordToCountB = buildWordToCount(B);
+class Solution {
+  public String[] uncommonFromSentences(String s1, String s2) {
+    Map<String, Integer> wordToCount = new HashMap<>();
+    for (String word : (s1 + " " + s2).split(" ")) {
+      wordToCount.put(word, wordToCount.getOrDefault(word, 0) + 1);
+    }
 
-		List<String> uncommons = new ArrayList<>();
-		searchUncommons(uncommons, wordToCountA, wordToCountB);
-		searchUncommons(uncommons, wordToCountB, wordToCountA);
-		return uncommons.toArray(new String[0]);
-	}
-
-	void searchUncommons(List<String> uncommons, Map<String, Integer> wordToCountThis,
-			Map<String, Integer> wordToCountOther) {
-		for (String word : wordToCountThis.keySet()) {
-			if (wordToCountThis.get(word) == 1 && !wordToCountOther.containsKey(word)) {
-				uncommons.add(word);
-			}
-		}
-	}
-
-	Map<String, Integer> buildWordToCount(String sentence) {
-		Map<String, Integer> wordToCount = new HashMap<>();
-		for (String word : sentence.split(" ")) {
-			if (!word.isEmpty()) {
-				wordToCount.put(word, wordToCount.getOrDefault(word, 0) + 1);
-			}
-		}
-		return wordToCount;
-	}
+    return wordToCount.keySet().stream()
+        .filter(word -> wordToCount.get(word) == 1)
+        .toArray(String[]::new);
+  }
 }
