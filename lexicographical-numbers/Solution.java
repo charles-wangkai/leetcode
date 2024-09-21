7c5315670a0d823e80fd572948cabfb8116201d7
Kai Wang <charles.wangkai@gmail.com>
2024-09-21 08:24:32 +0800

    Lexicographical Numbers

--- lexicographical-numbers/Solution.java
+++ lexicographical-numbers/Solution.java
@@ -1,25 +1,12 @@
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class Solution {
+class Solution {
   public List<Integer> lexicalOrder(int n) {
-    List<Integer> result = new ArrayList<>();
-    search(result, n, 0);
-    return result;
-  }
-
-  static void search(List<Integer> result, int n, int prevNumber) {
-    for (int digit = 0; digit < 10; digit++) {
-      int number = prevNumber * 10 + digit;
-      if (number == 0) {
-        continue;
-      }
-      if (number > n) {
-        break;
-      }
-
-      result.add(number);
-      search(result, n, number);
-    }
+    return IntStream.rangeClosed(1, n)
+        .boxed()
+        .sorted(Comparator.comparing(x -> String.valueOf(x)))
+        .toList();
   }
 }
