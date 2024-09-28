1bd4eac82f94c2ebc42822ec73db1ff5402d64a0
Kai Wang <charles.wangkai@gmail.com>
2024-09-28 22:24:17 +0800

    Design Circular Deque

--- design-circular-deque/MyCircularDeque.java
+++ design-circular-deque/MyCircularDeque.java
@@ -1,94 +1,71 @@
-public class MyCircularDeque {
-	private int[] data;
-	private int beginIndex;
-	private int itemNum;
+class MyCircularDeque {
+  private int[] values;
+  private int beginIndex;
+  private int size;
 
-	/** Initialize your data structure here. Set the size of the deque to be k. */
-	public MyCircularDeque(int k) {
-		data = new int[k];
-		beginIndex = 0;
-		itemNum = 0;
-	}
+  public MyCircularDeque(int k) {
+    values = new int[k];
+  }
 
-	/**
-	 * Adds an item at the front of Deque. Return true if the operation is
-	 * successful.
-	 */
-	public boolean insertFront(int value) {
-		if (isFull()) {
-			return false;
-		}
+  public boolean insertFront(int value) {
+    if (isFull()) {
+      return false;
+    }
 
-		beginIndex = (beginIndex - 1 + data.length) % data.length;
-		data[beginIndex] = value;
-		itemNum++;
+    beginIndex = Math.floorMod(beginIndex - 1, values.length);
+    values[beginIndex] = value;
+    ++size;
 
-		return true;
-	}
+    return true;
+  }
 
-	/**
-	 * Adds an item at the rear of Deque. Return true if the operation is
-	 * successful.
-	 */
-	public boolean insertLast(int value) {
-		if (isFull()) {
-			return false;
-		}
+  public boolean insertLast(int value) {
+    if (isFull()) {
+      return false;
+    }
 
-		data[(beginIndex + itemNum) % data.length] = value;
-		itemNum++;
+    values[(beginIndex + size) % values.length] = value;
+    ++size;
 
-		return true;
-	}
+    return true;
+  }
 
-	/**
-	 * Deletes an item from the front of Deque. Return true if the operation is
-	 * successful.
-	 */
-	public boolean deleteFront() {
-		if (isEmpty()) {
-			return false;
-		}
+  public boolean deleteFront() {
+    if (isEmpty()) {
+      return false;
+    }
 
-		beginIndex = (beginIndex + 1) % data.length;
-		itemNum--;
+    beginIndex = (beginIndex + 1) % values.length;
+    --size;
 
-		return true;
-	}
+    return true;
+  }
 
-	/**
-	 * Deletes an item from the rear of Deque. Return true if the operation is
-	 * successful.
-	 */
-	public boolean deleteLast() {
-		if (isEmpty()) {
-			return false;
-		}
+  public boolean deleteLast() {
+    if (isEmpty()) {
+      return false;
+    }
 
-		itemNum--;
+    --size;
 
-		return true;
-	}
+    return true;
+  }
 
-	/** Get the front item from the deque. */
-	public int getFront() {
-		return isEmpty() ? -1 : data[beginIndex];
-	}
+  public int getFront() {
+    return isEmpty() ? -1 : values[beginIndex];
+  }
 
-	/** Get the last item from the deque. */
-	public int getRear() {
-		return isEmpty() ? -1 : data[(beginIndex + itemNum - 1) % data.length];
-	}
+  public int getRear() {
+    return isEmpty() ? -1 : values[(beginIndex + size - 1) % values.length];
+  }
 
-	/** Checks whether the circular deque is empty or not. */
-	public boolean isEmpty() {
-		return itemNum == 0;
-	}
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-	/** Checks whether the circular deque is full or not. */
-	public boolean isFull() {
-		return itemNum == data.length;
-	}
+  public boolean isFull() {
+    return size == values.length;
+  }
 }
 
 // Your MyCircularDeque object will be instantiated and called as such:
