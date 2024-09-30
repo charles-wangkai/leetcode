1734bff2d09487aaec72cdce2850e47cde12dd77
Kai Wang <charles.wangkai@gmail.com>
2024-09-30 08:19:31 +0800

    Design a Stack With Increment Operation

--- design-a-stack-with-increment-operation/CustomStack.java
+++ design-a-stack-with-increment-operation/CustomStack.java
@@ -2,43 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 class CustomStack {
-    private int maxSize;
-    private List<Integer> values = new ArrayList<>();
-    private List<Integer> deltas = new ArrayList<>();
+  int maxSize;
+  List<Integer> values = new ArrayList<>();
+  List<Integer> deltas = new ArrayList<>();
 
-    public CustomStack(int maxSize) {
-        this.maxSize = maxSize;
+  public CustomStack(int maxSize) {
+    this.maxSize = maxSize;
+  }
+
+  public void push(int x) {
+    if (values.size() != maxSize) {
+      values.add(x);
+      deltas.add(0);
+    }
+  }
+
+  public int pop() {
+    if (values.isEmpty()) {
+      return -1;
     }
 
-    public void push(int x) {
-        if (values.size() == maxSize) {
-            return;
-        }
-
-        values.add(x);
-        deltas.add(0);
+    int value = values.removeLast();
+    int delta = deltas.removeLast();
+    if (!deltas.isEmpty()) {
+      deltas.set(deltas.size() - 1, deltas.get(deltas.size() - 1) + delta);
     }
 
-    public int pop() {
-        if (values.isEmpty()) {
-            return -1;
-        }
+    return value + delta;
+  }
 
-        int value = values.remove(values.size() - 1);
-        int delta = deltas.remove(deltas.size() - 1);
-        if (!deltas.isEmpty()) {
-            deltas.set(deltas.size() - 1, deltas.get(deltas.size() - 1) + delta);
-        }
-
-        return value + delta;
+  public void increment(int k, int val) {
+    if (!deltas.isEmpty()) {
+      int index = Math.min(deltas.size() - 1, k - 1);
+      deltas.set(index, deltas.get(index) + val);
     }
-
-    public void increment(int k, int val) {
-        if (!deltas.isEmpty()) {
-            int index = Math.min(deltas.size() - 1, k - 1);
-            deltas.set(index, deltas.get(index) + val);
-        }
-    }
+  }
 }
 
 // Your CustomStack object will be instantiated and called as such:
