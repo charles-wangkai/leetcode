469ba51afb5271de60d7a1f9d5a1ca93a8f44295
Kai Wang <charles.wangkai@gmail.com>
2024-10-01 08:07:34 +0800

    Largest Unique Number

--- largest-unique-number/Solution.java
+++ largest-unique-number/Solution.java
@@ -1,14 +1,17 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int largestUniqueNumber(int[] A) {
-		Map<Integer, Integer> valueToCount = new HashMap<>();
-		for (int value : A) {
-			valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
-		}
+class Solution {
+  public int largestUniqueNumber(int[] nums) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int value : nums) {
+      valueToCount.put(value, valueToCount.getOrDefault(value, 0) + 1);
+    }
 
-		return valueToCount.keySet().stream().filter(value -> valueToCount.get(value) == 1).mapToInt(x -> x).max()
-				.orElse(-1);
-	}
+    return valueToCount.keySet().stream()
+        .filter(value -> valueToCount.get(value) == 1)
+        .mapToInt(Integer::intValue)
+        .max()
+        .orElse(-1);
+  }
 }
