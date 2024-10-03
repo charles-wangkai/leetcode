36bc9f73940f04b6584a5d6a439f656f33c200ee
Kai Wang <charles.wangkai@gmail.com>
2024-10-03 08:46:38 +0800

    Make Sum Divisible by P

--- make-sum-divisible-by-p/Solution.java
+++ make-sum-divisible-by-p/Solution.java
@@ -3,26 +3,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public int minSubarray(int[] nums, int p) {
-        int targetRemainder = (int) (Arrays.stream(nums).asLongStream().sum() % p);
-        if (targetRemainder == 0) {
-            return 0;
-        }
-
-        int result = Integer.MAX_VALUE;
-        Map<Integer, Integer> remainderToLastIndex = new HashMap<>();
-        remainderToLastIndex.put(0, -1);
-        int remainder = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            remainder = (remainder + nums[i]) % p;
-            int otherRemainder = (remainder - targetRemainder + p) % p;
-            if (remainderToLastIndex.containsKey(otherRemainder)) {
-                result = Math.min(result, i - remainderToLastIndex.get(otherRemainder));
-            }
-
-            remainderToLastIndex.put(remainder, i);
-        }
-
-        return (result == nums.length) ? -1 : result;
+  public int minSubarray(int[] nums, int p) {
+    int targetRemainder = (int) (Arrays.stream(nums).asLongStream().sum() % p);
+    if (targetRemainder == 0) {
+      return 0;
     }
+
+    int result = Integer.MAX_VALUE;
+    Map<Integer, Integer> remainderToLastIndex = new HashMap<>();
+    remainderToLastIndex.put(0, -1);
+    int remainder = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      remainder = (remainder + nums[i]) % p;
+      int other = Math.floorMod(remainder - targetRemainder, p);
+      if (remainderToLastIndex.containsKey(other)) {
+        result = Math.min(result, i - remainderToLastIndex.get(other));
+      }
+
+      remainderToLastIndex.put(remainder, i);
+    }
+
+    return (result == nums.length) ? -1 : result;
+  }
 }
