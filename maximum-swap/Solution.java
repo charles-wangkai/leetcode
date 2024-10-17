1285e58f413c8322a22af5213cc96aa5673607b9
Kai Wang <charles.wangkai@gmail.com>
2024-10-17 14:32:58 +0800

    Maximum Swap

--- maximum-swap/Solution.java
+++ maximum-swap/Solution.java
@@ -1,20 +1,21 @@
-public class Solution {
-	public int maximumSwap(int num) {
-		char[] digits = String.valueOf(num).toCharArray();
-		int result = num;
-		for (int i = 0; i < digits.length; i++) {
-			for (int j = i + 1; j < digits.length; j++) {
-				swap(digits, i, j);
-				result = Math.max(result, Integer.parseInt(new String(digits)));
-				swap(digits, i, j);
-			}
-		}
-		return result;
-	}
+class Solution {
+  public int maximumSwap(int num) {
+    int result = num;
+    char[] digits = String.valueOf(num).toCharArray();
+    for (int i = 0; i < digits.length; ++i) {
+      for (int j = i + 1; j < digits.length; ++j) {
+        swap(digits, i, j);
+        result = Math.max(result, Integer.parseInt(new String(digits)));
+        swap(digits, i, j);
+      }
+    }
 
-	void swap(char[] digits, int index1, int index2) {
-		char temp = digits[index1];
-		digits[index1] = digits[index2];
-		digits[index2] = temp;
-	}
+    return result;
+  }
+
+  void swap(char[] digits, int index1, int index2) {
+    char temp = digits[index1];
+    digits[index1] = digits[index2];
+    digits[index2] = temp;
+  }
 }
