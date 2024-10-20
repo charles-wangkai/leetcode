7735147911d9c289d068778a6aa07e361a4613bd
Kai Wang <charles.wangkai@gmail.com>
2024-10-20 08:54:19 +0800

    Parsing A Boolean Expression

--- parsing-a-boolean-expression/Solution.java
+++ parsing-a-boolean-expression/Solution.java
@@ -1,39 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public boolean parseBoolExpr(String expression) {
-		char first = expression.charAt(0);
+class Solution {
+  public boolean parseBoolExpr(String expression) {
+    char symbol = expression.charAt(0);
+    if (symbol == 't') {
+      return true;
+    }
+    if (symbol == 'f') {
+      return false;
+    }
+    if (symbol == '!') {
+      return !parseBoolExpr(expression.substring(2, expression.length() - 1));
+    }
 
-		if (first == 't') {
-			return true;
-		} else if (first == 'f') {
-			return false;
-		} else if (first == '!') {
-			return !parseBoolExpr(expression.substring(2, expression.length() - 1));
-		}
+    List<Boolean> operands = new ArrayList<>();
+    int beginIndex = 2;
+    int depth = 0;
+    for (int i = 2; i <= expression.length() - 1; ++i) {
+      char c = expression.charAt(i);
+      if (i == expression.length() - 1 || (c == ',' && depth == 0)) {
+        operands.add(parseBoolExpr(expression.substring(beginIndex, i)));
+        beginIndex = i + 1;
+      } else if (c == '(') {
+        ++depth;
+      } else if (c == ')') {
+        --depth;
+      }
+    }
 
-		List<Boolean> parts = new ArrayList<>();
-		int beginIndex = 2;
-		int depth = 0;
-		for (int i = 2; i <= expression.length() - 1; i++) {
-			char ch = expression.charAt(i);
-
-			if (i == expression.length() - 1 || (ch == ',' && depth == 0)) {
-				parts.add(parseBoolExpr(expression.substring(beginIndex, i)));
-
-				beginIndex = i + 1;
-			} else if (ch == '(') {
-				depth++;
-			} else if (ch == ')') {
-				depth--;
-			}
-		}
-
-		if (first == '&') {
-			return parts.stream().allMatch(x -> x);
-		} else {
-			return parts.stream().anyMatch(x -> x);
-		}
-	}
+    return (symbol == '&')
+        ? operands.stream().allMatch(part -> part)
+        : operands.stream().anyMatch(part -> part);
+  }
 }
