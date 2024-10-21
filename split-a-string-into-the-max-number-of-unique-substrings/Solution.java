2949715820626579d333683903cf5bd2629fc099
Kai Wang <charles.wangkai@gmail.com>
2024-10-21 09:55:26 +0800

    Split a String Into the Max Number of Unique Substrings

--- split-a-string-into-the-max-number-of-unique-substrings/Solution.java
+++ split-a-string-into-the-max-number-of-unique-substrings/Solution.java
@@ -1,33 +1,32 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 class Solution {
-    public int maxUniqueSplit(String s) {
-        int result = -1;
-        for (int code = 1 << (s.length() - 1); code < (1 << s.length()); ++code) {
-            if (check(s, code)) {
-                result = Math.max(result, Integer.bitCount(code));
-            }
+  public int maxUniqueSplit(String s) {
+    return IntStream.range(0, 1 << (s.length() - 1))
+            .filter(mask -> check(s, mask))
+            .map(Integer::bitCount)
+            .max()
+            .getAsInt()
+        + 1;
+  }
+
+  boolean check(String s, int mask) {
+    Set<String> seen = new HashSet<>();
+    StringBuilder part = new StringBuilder();
+    for (int i = 0; i < s.length(); ++i) {
+      part.append(s.charAt(i));
+      if (i == s.length() - 1 || ((mask >> i) & 1) == 1) {
+        if (seen.contains(part.toString())) {
+          return false;
         }
 
-        return result;
+        seen.add(part.toString());
+        part = new StringBuilder();
+      }
     }
 
-    boolean check(String s, int code) {
-        Set<String> parts = new HashSet<>();
-        StringBuilder part = new StringBuilder();
-        for (int i = 0; i < s.length(); ++i) {
-            part.append(s.charAt(i));
-            if ((code & (1 << i)) != 0) {
-                if (parts.contains(part.toString())) {
-                    return false;
-                }
-
-                parts.add(part.toString());
-                part = new StringBuilder();
-            }
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
