3c9c35766d85243cd17be9c98ee5b1936c25126a
Kai Wang <charles.wangkai@gmail.com>
2024-10-24 08:56:14 +0800

    Flip Equivalent Binary Trees

--- flip-equivalent-binary-trees/Solution.java
+++ flip-equivalent-binary-trees/Solution.java
@@ -4,45 +4,28 @@ class TreeNode {
   TreeNode left;
   TreeNode right;
 
-  TreeNode(int x) {
-    val = x;
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
   }
 }
 
-public class Solution {
+class Solution {
   public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-    regulate(root1);
-    regulate(root2);
-
-    return isEqual(root1, root2);
-  }
-
-  static void regulate(TreeNode node) {
-    if (node == null) {
-      return;
+    if (root1 == null) {
+      return root2 == null;
     }
 
-    if (node.right != null && (node.left == null || node.left.val > node.right.val)) {
-      TreeNode temp = node.left;
-      node.left = node.right;
-      node.right = temp;
-    }
-
-    regulate(node.left);
-    regulate(node.right);
-  }
-
-  static boolean isEqual(TreeNode node1, TreeNode node2) {
-    if (node1 == null) {
-      return node2 == null;
-    } else {
-      if (node2 == null) {
-        return false;
-      } else {
-        return node1.val == node2.val
-            && isEqual(node1.left, node2.left)
-            && isEqual(node1.right, node2.right);
-      }
-    }
+    return root2 != null
+        && root1.val == root2.val
+        && ((flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
+            || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)));
   }
 }
