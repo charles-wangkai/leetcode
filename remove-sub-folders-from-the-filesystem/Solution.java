e2998d033a7d20dd4fa1461c612e2540c40cc7d3
Kai Wang <charles.wangkai@gmail.com>
2024-10-25 09:23:17 +0800

    Remove Sub-Folders from the Filesystem

--- remove-sub-folders-from-the-filesystem/Solution.java
+++ remove-sub-folders-from-the-filesystem/Solution.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
-	public List<String> removeSubfolders(String[] folder) {
-		Arrays.sort(folder);
+class Solution {
+  public List<String> removeSubfolders(String[] folder) {
+    Arrays.sort(folder);
 
-		List<String> result = new ArrayList<>();
-		for (String f : folder) {
-			if (result.isEmpty() || !(f + "/").startsWith(result.get(result.size() - 1) + "/")) {
-				result.add(f);
-			}
-		}
+    List<String> result = new ArrayList<>();
+    for (String f : folder) {
+      if (result.isEmpty() || !f.startsWith(result.getLast() + "/")) {
+        result.add(f);
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
