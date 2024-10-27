23616b315709d2c62ad7b993e7f7ecdbd987f4e5
Kai Wang <charles.wangkai@gmail.com>
2024-10-27 19:36:06 +0800

    Count Square Submatrices with All Ones

--- count-square-submatrices-with-all-ones/Solution.java
+++ count-square-submatrices-with-all-ones/Solution.java
@@ -1,27 +1,34 @@
-public class Solution {
-	public int countSquares(int[][] matrix) {
-		int m = matrix.length;
-		int n = matrix[0].length;
+class Solution {
+  public int countSquares(int[][] matrix) {
+    int m = matrix.length;
+    int n = matrix[0].length;
 
-		int[][] sums = new int[m + 1][n + 1];
-		for (int i = 1; i <= m; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + matrix[i - 1][j - 1];
-			}
-		}
+    int[][] prefixSums = new int[m + 1][n + 1];
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        prefixSums[i][j] =
+            prefixSums[i - 1][j]
+                + prefixSums[i][j - 1]
+                - prefixSums[i - 1][j - 1]
+                + matrix[i - 1][j - 1];
+      }
+    }
 
-		int result = 0;
-		for (int i = 1; i <= m; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				for (int size = 1; i + size - 1 <= m && j + size - 1 <= n; ++size) {
-					if (sums[i + size - 1][j + size - 1] - sums[i - 1][j + size - 1] - sums[i + size - 1][j - 1]
-							+ sums[i - 1][j - 1] == size * size) {
-						++result;
-					}
-				}
-			}
-		}
+    int result = 0;
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        for (int size = 1; i + size - 1 <= m && j + size - 1 <= n; ++size) {
+          if (prefixSums[i + size - 1][j + size - 1]
+                  - prefixSums[i - 1][j + size - 1]
+                  - prefixSums[i + size - 1][j - 1]
+                  + prefixSums[i - 1][j - 1]
+              == size * size) {
+            ++result;
+          }
+        }
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
