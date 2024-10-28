049bba261ed0b612ad74dcff401c49e761b80f96
Kai Wang <charles.wangkai@gmail.com>
2024-10-28 09:26:37 +0800

    Longest Square Streak in an Array

--- longest-square-streak-in-an-array/Solution.java
+++ longest-square-streak-in-an-array/Solution.java
@@ -1,14 +1,19 @@
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 class Solution {
   public int longestSquareStreak(int[] nums) {
     Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
 
     return Arrays.stream(nums)
-        .map(begin -> (int) IntStream.iterate(begin, x -> x * x).takeWhile(set::contains).count())
+        .map(
+            begin ->
+                (int)
+                    LongStream.iterate(begin, x -> x * x)
+                        .takeWhile(x -> x <= Integer.MAX_VALUE && set.contains((int) x))
+                        .count())
         .filter(x -> x >= 2)
         .max()
         .orElse(-1);
