3bfc77a3bce45b587b12e3809d12ba3bbb20ae65
Kai Wang <charles.wangkai@gmail.com>
2024-11-03 08:21:38 +0800

    Rotate String

--- rotate-string/Solution.java
+++ rotate-string/Solution.java
@@ -1,5 +1,5 @@
-public class Solution {
-	public boolean rotateString(String A, String B) {
-		return A.length() == B.length() && (A + A).contains(B);
-	}
+class Solution {
+  public boolean rotateString(String s, String goal) {
+    return s.length() == goal.length() && (s + s).contains(goal);
+  }
 }
