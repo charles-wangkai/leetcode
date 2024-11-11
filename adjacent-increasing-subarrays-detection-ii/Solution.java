a976983badd96f04d6a5ab03008268e349397b5c
Kai Wang <charles.wangkai@gmail.com>
2024-11-11 09:52:43 +0800

    Adjacent Increasing Subarrays Detection II

---
+++ adjacent-increasing-subarrays-detection-ii/Solution.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Solution {
+  public int maxIncreasingSubarrays(List<Integer> nums) {
+    int[] increasingLengths = new int[nums.size()];
+    for (int i = increasingLengths.length - 1; i >= 0; --i) {
+      increasingLengths[i] =
+          (i == increasingLengths.length - 1 || nums.get(i) >= nums.get(i + 1))
+              ? 1
+              : (increasingLengths[i + 1] + 1);
+    }
+
+    int result = -1;
+    int lower = 1;
+    int upper = nums.size() / 2;
+    while (lower <= upper) {
+      int middle = (lower + upper) / 2;
+      if (check(increasingLengths, middle)) {
+        result = middle;
+        lower = middle + 1;
+      } else {
+        upper = middle - 1;
+      }
+    }
+
+    return result;
+  }
+
+  boolean check(int[] increasingLengths, int k) {
+    return IntStream.rangeClosed(0, increasingLengths.length - 2 * k)
+        .anyMatch(i -> increasingLengths[i] >= k && increasingLengths[i + k] >= k);
+  }
+}
