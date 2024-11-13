7edbf8879619570a1746cbcb825c70ecc7bec89d
Kai Wang <charles.wangkai@gmail.com>
2024-11-13 09:22:30 +0800

    Count the Number of Fair Pairs

--- count-the-number-of-fair-pairs/Solution.java
+++ count-the-number-of-fair-pairs/Solution.java
@@ -5,47 +5,20 @@ class Solution {
     Arrays.sort(nums);
 
     long result = 0;
-    for (int num : nums) {
-      result +=
-          findUpperIndex(nums, upper - num)
-              - findLowerIndex(nums, lower - num)
-              + 1
-              - ((num * 2 >= lower && num * 2 <= upper) ? 1 : 0);
-    }
-    result /= 2;
-
-    return result;
-  }
-
-  int findLowerIndex(int[] nums, int target) {
-    int result = nums.length;
-    int lowerIndex = 0;
-    int upperIndex = nums.length - 1;
-    while (lowerIndex <= upperIndex) {
-      int middleIndex = (lowerIndex + upperIndex) / 2;
-      if (nums[middleIndex] >= target) {
-        result = middleIndex;
-        upperIndex = middleIndex - 1;
-      } else {
-        lowerIndex = middleIndex + 1;
+    int beginIndex = nums.length;
+    int endIndex = nums.length - 1;
+    for (int i = nums.length - 1; i >= 0; --i) {
+      while (beginIndex != nums.length && nums[beginIndex] < lower - nums[i]) {
+        ++beginIndex;
       }
-    }
-
-    return result;
-  }
-
-  int findUpperIndex(int[] nums, int target) {
-    int result = -1;
-    int lowerIndex = 0;
-    int upperIndex = nums.length - 1;
-    while (lowerIndex <= upperIndex) {
-      int middleIndex = (lowerIndex + upperIndex) / 2;
-      if (nums[middleIndex] <= target) {
-        result = middleIndex;
-        lowerIndex = middleIndex + 1;
-      } else {
-        upperIndex = middleIndex - 1;
+      while (endIndex + 1 != nums.length && nums[endIndex + 1] <= upper - nums[i]) {
+        ++endIndex;
       }
+
+      result += endIndex - beginIndex + 1;
+
+      --beginIndex;
+      --endIndex;
     }
 
     return result;
