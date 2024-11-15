51185bd98a2bf18b2ccf1189378abc3e46e2013a
Kai Wang <charles.wangkai@gmail.com>
2024-11-15 09:05:45 +0800

    Shortest Subarray to be Removed to Make Array Sorted

--- shortest-subarray-to-be-removed-to-make-array-sorted/Solution.java
+++ shortest-subarray-to-be-removed-to-make-array-sorted/Solution.java
@@ -1,31 +1,31 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int findLengthOfShortestSubarray(int[] arr) {
-        if (IntStream.range(0, arr.length - 1).allMatch(i -> arr[i] <= arr[i + 1])) {
-            return 0;
-        }
-
-        int leftEndIndex = 0;
-        while (arr[leftEndIndex] <= arr[leftEndIndex + 1]) {
-            ++leftEndIndex;
-        }
-
-        int rightBeginIndex = arr.length - 1;
-        while (arr[rightBeginIndex - 1] <= arr[rightBeginIndex]) {
-            --rightBeginIndex;
-        }
-
-        int result = rightBeginIndex;
-        int rightIndex = arr.length - 1;
-        for (int i = leftEndIndex; i >= 0; --i) {
-            while (rightIndex >= rightBeginIndex && arr[rightIndex] >= arr[i]) {
-                --rightIndex;
-            }
-
-            result = Math.min(result, rightIndex - i);
-        }
-
-        return result;
+  public int findLengthOfShortestSubarray(int[] arr) {
+    if (IntStream.range(0, arr.length - 1).allMatch(i -> arr[i] <= arr[i + 1])) {
+      return 0;
     }
+
+    int leftEndIndex = 0;
+    while (arr[leftEndIndex] <= arr[leftEndIndex + 1]) {
+      ++leftEndIndex;
+    }
+
+    int rightBeginIndex = arr.length - 1;
+    while (arr[rightBeginIndex - 1] <= arr[rightBeginIndex]) {
+      --rightBeginIndex;
+    }
+
+    int result = rightBeginIndex;
+    int rightIndex = arr.length - 1;
+    for (int i = leftEndIndex; i >= 0; --i) {
+      while (rightIndex >= rightBeginIndex && arr[rightIndex] >= arr[i]) {
+        --rightIndex;
+      }
+
+      result = Math.min(result, rightIndex - i);
+    }
+
+    return result;
+  }
 }
