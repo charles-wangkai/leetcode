601b7a935847e670d4d7449b4907a860698cb0ef
Kai Wang <charles.wangkai@gmail.com>
2024-11-18 08:54:14 +0800

    Defuse the Bomb

--- defuse-the-bomb/Solution.java
+++ defuse-the-bomb/Solution.java
@@ -4,17 +4,10 @@ class Solution {
   public int[] decrypt(int[] code, int k) {
     return IntStream.range(0, code.length)
         .map(
-            i -> {
-              if (k < 0) {
-                return IntStream.rangeClosed(k, -1)
-                    .map(j -> code[(i + j + code.length) % code.length])
-                    .sum();
-              } else if (k > 0) {
-                return IntStream.rangeClosed(1, k).map(j -> code[(i + j) % code.length]).sum();
-              } else {
-                return 0;
-              }
-            })
+            i ->
+                ((k >= 0) ? IntStream.rangeClosed(1, k) : IntStream.rangeClosed(k, -1))
+                    .map(j -> code[Math.floorMod(i + j, code.length)])
+                    .sum())
         .toArray();
   }
 }
