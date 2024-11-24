0d28b44b1288da7e8496754f6d03a9a8286504b1
Kai Wang <charles.wangkai@gmail.com>
2024-11-24 22:58:07 +0800

    Zero Array Transformation III

---
+++ zero-array-transformation-iii/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
+
+class Solution {
+  public int maxRemoval(int[] nums, int[][] queries) {
+    int[] sortedIndices =
+        IntStream.range(0, queries.length)
+            .boxed()
+            .sorted(Comparator.comparing(i -> queries[i][0]))
+            .mapToInt(Integer::intValue)
+            .toArray();
+
+    int chosenCount = 0;
+    PriorityQueue<Integer> chosenLastIndices = new PriorityQueue<>();
+    PriorityQueue<Integer> availableLastIndices = new PriorityQueue<>(Comparator.reverseOrder());
+    int index = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      while (index != sortedIndices.length && queries[sortedIndices[index]][0] == i) {
+        availableLastIndices.offer(queries[sortedIndices[index]][1]);
+        ++index;
+      }
+
+      while (!chosenLastIndices.isEmpty() && chosenLastIndices.peek() < i) {
+        chosenLastIndices.poll();
+      }
+
+      while (chosenLastIndices.size() < nums[i]) {
+        while (!availableLastIndices.isEmpty() && availableLastIndices.peek() < i) {
+          availableLastIndices.poll();
+        }
+
+        if (availableLastIndices.isEmpty()) {
+          return -1;
+        }
+
+        chosenLastIndices.offer(availableLastIndices.poll());
+        ++chosenCount;
+      }
+    }
+
+    return queries.length - chosenCount;
+  }
+}
