03c0b71f5bf25fe0c9a88be423d5e17159b72d92
Kai Wang <charles.wangkai@gmail.com>
2024-11-24 13:30:34 +0800

    Maximum Matrix Sum

--- maximum-matrix-sum/Solution.java
+++ maximum-matrix-sum/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
   public long maxMatrixSum(int[][] matrix) {
-    long sum = 0;
+    long absSum = 0;
     int negCount = 0;
     int minAbs = Integer.MAX_VALUE;
     for (int[] line : matrix) {
       for (int x : line) {
-        sum += Math.abs(x);
+        absSum += Math.abs(x);
         if (x < 0) {
           ++negCount;
         }
@@ -13,6 +13,6 @@ class Solution {
       }
     }
 
-    return sum - ((negCount % 2 == 0) ? 0 : minAbs) * 2;
+    return absSum - ((negCount % 2 == 0) ? 0 : minAbs) * 2;
   }
 }
