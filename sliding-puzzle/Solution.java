3593c39242865b9dd274fa37837dd4eb09b0a4f4
Kai Wang <charles.wangkai@gmail.com>
2024-11-25 09:27:28 +0800

    Sliding Puzzle

--- sliding-puzzle/Solution.java
+++ sliding-puzzle/Solution.java
@@ -1,71 +1,84 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-public class Solution {
+class Solution {
   public int slidingPuzzle(int[][] board) {
-    Map<String, Integer> state2step = new HashMap<>();
     String state = convertToState(board);
-    state2step.put(state, 0);
-    Queue<String> states = new LinkedList<>();
+
+    Map<String, Integer> stateToStep = new HashMap<>();
+    stateToStep.put(state, 0);
+
+    Queue<String> states = new ArrayDeque<>();
     states.offer(state);
+
     while (!states.isEmpty()) {
       String head = states.poll();
-      int step = state2step.get(head);
-
       if (head.equals("123450")) {
-        return step;
+        return stateToStep.get(head);
       }
 
-      for (String next : move(head)) {
-        if (!state2step.containsKey(next)) {
-          state2step.put(next, step + 1);
-          states.offer(next);
+      for (String nextState : move(head)) {
+        if (!stateToStep.containsKey(nextState)) {
+          stateToStep.put(nextState, stateToStep.get(head) + 1);
+          states.offer(nextState);
         }
       }
     }
+
     return -1;
   }
 
   List<String> move(String state) {
     List<String> nextStates = new ArrayList<>();
-    int index0 = state.indexOf('0');
-    int row0 = index0 / 3;
-    int col0 = index0 % 3;
+    int index = state.indexOf('0');
+    int r = index / 3;
+    int c = index % 3;
 
-    nextStates.add(swap(state, convertToIndex(0, col0), convertToIndex(1, col0)));
-    if (col0 > 0) {
-      nextStates.add(swap(state, convertToIndex(row0, col0), convertToIndex(row0, col0 - 1)));
+    nextStates.add(swap(state, convertToIndex(0, c), convertToIndex(1, c)));
+    if (c != 0) {
+      nextStates.add(swap(state, convertToIndex(r, c), convertToIndex(r, c - 1)));
     }
-    if (col0 < 2) {
-      nextStates.add(swap(state, convertToIndex(row0, col0), convertToIndex(row0, col0 + 1)));
+    if (c != 2) {
+      nextStates.add(swap(state, convertToIndex(r, c), convertToIndex(r, c + 1)));
     }
 
     return nextStates;
   }
 
-  int convertToIndex(int row, int col) {
-    return row * 3 + col;
+  int convertToIndex(int r, int c) {
+    return r * 3 + c;
   }
 
   String swap(String s, int index1, int index2) {
-    StringBuilder sb = new StringBuilder(s);
-    char temp = sb.charAt(index1);
-    sb.setCharAt(index1, sb.charAt(index2));
-    sb.setCharAt(index2, temp);
-    return sb.toString();
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < s.length(); ++i) {
+      int index;
+      if (i == index1) {
+        index = index2;
+      } else if (i == index2) {
+        index = index1;
+      } else {
+        index = i;
+      }
+
+      result.append(s.charAt(index));
+    }
+
+    return result.toString();
   }
 
   String convertToState(int[][] board) {
-    StringBuilder state = new StringBuilder();
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[0].length; j++) {
-        state.append(board[i][j]);
+    StringBuilder result = new StringBuilder();
+    for (int[] line : board) {
+      for (int x : line) {
+        result.append(x);
       }
     }
-    return state.toString();
+
+    return result.toString();
   }
 }
