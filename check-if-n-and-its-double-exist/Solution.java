2142b8878fde2a870c6648392fae1d94a7d7a8c8
Kai Wang <charles.wangkai@gmail.com>
2024-12-01 08:56:38 +0800

    Check If N and Its Double Exist

--- check-if-n-and-its-double-exist/Solution.java
+++ check-if-n-and-its-double-exist/Solution.java
@@ -1,13 +1,13 @@
-public class Solution {
-    public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i < arr.length; ++i) {
-            for (int j = i + 1; j < arr.length; ++j) {
-                if (arr[i] * 2 == arr[j] || arr[i] == arr[j] * 2) {
-                    return true;
-                }
-            }
+class Solution {
+  public boolean checkIfExist(int[] arr) {
+    for (int i = 0; i < arr.length; ++i) {
+      for (int j = 0; j < arr.length; ++j) {
+        if (j != i && arr[i] == arr[j] * 2) {
+          return true;
         }
-
-        return false;
+      }
     }
+
+    return false;
+  }
 }
