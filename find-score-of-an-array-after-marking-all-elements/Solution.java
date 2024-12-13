1897accbb5378a9fc6f855d4f19d4c0302ea2277
Kai Wang <charles.wangkai@gmail.com>
2024-12-13 08:15:14 +0800

    Find Score of an Array After Marking All Elements

--- find-score-of-an-array-after-marking-all-elements/Solution.java
+++ find-score-of-an-array-after-marking-all-elements/Solution.java
@@ -1,22 +1,22 @@
 import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 class Solution {
   public long findScore(int[] nums) {
     long result = 0;
     boolean[] marked = new boolean[nums.length];
-    PriorityQueue<Integer> pq =
-        new PriorityQueue<>(Comparator.comparing((Integer i) -> nums[i]).thenComparing(i -> i));
-    for (int i = 0; i < nums.length; ++i) {
-      pq.offer(i);
-    }
-    while (!pq.isEmpty()) {
-      int head = pq.poll();
-      if (!marked[head]) {
-        result += nums[head];
+    int[] sortedIndices =
+        IntStream.range(0, nums.length)
+            .boxed()
+            .sorted(Comparator.<Integer, Integer>comparing(i -> nums[i]).thenComparing(i -> i))
+            .mapToInt(Integer::intValue)
+            .toArray();
+    for (int index : sortedIndices) {
+      if (!marked[index]) {
+        result += nums[index];
 
         for (int i = -1; i <= 1; ++i) {
-          int next = head + i;
+          int next = index + i;
           if (next >= 0 && next < marked.length) {
             marked[next] = true;
           }
