f104a0025f83dace6eaf37c5c5b6b5ff64b9c564
Kai Wang <charles.wangkai@gmail.com>
2024-12-26 09:03:22 +0800

    Target Sum

--- target-sum/Solution.java
+++ target-sum/Solution.java
@@ -1,13 +1,14 @@
-public class Solution {
-	public int findTargetSumWays(int[] nums, int S) {
-		return search(nums, 0, S);
-	}
+class Solution {
+  public int findTargetSumWays(int[] nums, int target) {
+    return search(nums, target, 0);
+  }
 
-	int search(int[] nums, int index, int remain) {
-		if (index == nums.length) {
-			return (remain == 0) ? 1 : 0;
-		}
+  int search(int[] nums, int rest, int index) {
+    if (index == nums.length) {
+      return (rest == 0) ? 1 : 0;
+    }
 
-		return search(nums, index + 1, remain + nums[index]) + search(nums, index + 1, remain - nums[index]);
-	}
+    return search(nums, rest + nums[index], index + 1)
+        + search(nums, rest - nums[index], index + 1);
+  }
 }
