1d99d38082b6b945111f749d1628dd92df9603e0
Kai Wang <charles.wangkai@gmail.com>
2025-01-03 08:44:32 +0800

    Number of Ways to Split Array

--- number-of-ways-to-split-array/Solution.java
+++ number-of-ways-to-split-array/Solution.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 
 class Solution {
   public int waysToSplitArray(int[] nums) {
-    long total = Arrays.stream(nums).asLongStream().sum();
-
     int result = 0;
     long leftSum = 0;
+    long rightSum = Arrays.stream(nums).asLongStream().sum();
     for (int i = 0; i < nums.length - 1; ++i) {
       leftSum += nums[i];
-      long rightSum = total - leftSum;
+      rightSum -= nums[i];
       if (leftSum >= rightSum) {
         ++result;
       }
