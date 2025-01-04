c4043b0b0f2a2d159acb4544895b1a68d14f3e65
Kai Wang <charles.wangkai@gmail.com>
2025-01-04 09:25:14 +0800

    Unique Length-3 Palindromic Subsequences

--- unique-length-3-palindromic-subsequences/Solution.java
+++ unique-length-3-palindromic-subsequences/Solution.java
@@ -1,36 +1,25 @@
 class Solution {
-  static final int ALPHABET_SIZE = 26;
-
   public int countPalindromicSubsequence(String s) {
-    int[] leftCounts = new int[ALPHABET_SIZE];
-    int[] rightCounts = new int[ALPHABET_SIZE];
-    for (int i = 0; i < s.length(); ++i) {
-      ++rightCounts[s.charAt(i) - 'a'];
-    }
-
-    boolean[][] palindromes = new boolean[ALPHABET_SIZE][ALPHABET_SIZE];
-    for (int i = 0; i < s.length(); ++i) {
-      if (i != 0) {
-        ++leftCounts[s.charAt(i - 1) - 'a'];
-      }
-      --rightCounts[s.charAt(i) - 'a'];
-
-      for (int j = 0; j < ALPHABET_SIZE; ++j) {
-        if (leftCounts[j] != 0 && rightCounts[j] != 0) {
-          palindromes[j][s.charAt(i) - 'a'] = true;
-        }
-      }
-    }
-
     int result = 0;
-    for (int i = 0; i < palindromes.length; ++i) {
-      for (int j = 0; j < palindromes[i].length; ++j) {
-        if (palindromes[i][j]) {
+    for (char side = 'a'; side <= 'z'; ++side) {
+      for (char middle = 'a'; middle <= 'z'; ++middle) {
+        if (isSubsequence(s, String.format("%c%c%c", side, middle, side))) {
           ++result;
         }
       }
     }
 
     return result;
+  }
+
+  boolean isSubsequence(String s, String target) {
+    int index = 0;
+    for (char c : s.toCharArray()) {
+      if (index != target.length() && c == target.charAt(index)) {
+        ++index;
+      }
+    }
+
+    return index == target.length();
   }
 }
