432035109fa9daf490bd8b4aced024d39b1c6bf5
Kai Wang <charles.wangkai@gmail.com>
2025-01-10 08:47:45 +0800

    Word Subsets

--- word-subsets/Solution.java
+++ word-subsets/Solution.java
@@ -5,16 +5,16 @@ import java.util.Map;
 
 class Solution {
   public List<String> wordSubsets(String[] words1, String[] words2) {
-    Map<Character, Integer> letterToMinCount = new HashMap<>();
+    Map<Character, Integer> letterToMaxCount = new HashMap<>();
     for (String word2 : words2) {
       Map<Character, Integer> letterToCount2 = buildLetterToCount(word2);
       for (char letter : letterToCount2.keySet()) {
-        letterToMinCount.put(
-            letter, Math.max(letterToMinCount.getOrDefault(letter, 0), letterToCount2.get(letter)));
+        letterToMaxCount.put(
+            letter, Math.max(letterToMaxCount.getOrDefault(letter, 0), letterToCount2.get(letter)));
       }
     }
 
-    return Arrays.stream(words1).filter(word1 -> isUniversal(letterToMinCount, word1)).toList();
+    return Arrays.stream(words1).filter(word1 -> isUniversal(letterToMaxCount, word1)).toList();
   }
 
   Map<Character, Integer> buildLetterToCount(String word) {
@@ -26,10 +26,10 @@ class Solution {
     return letterToCount;
   }
 
-  boolean isUniversal(Map<Character, Integer> letterToMinCount, String word) {
+  boolean isUniversal(Map<Character, Integer> letterToMaxCount, String word) {
     Map<Character, Integer> letterToCount = buildLetterToCount(word);
 
-    return letterToMinCount.keySet().stream()
-        .allMatch(letter -> letterToCount.getOrDefault(letter, 0) >= letterToMinCount.get(letter));
+    return letterToMaxCount.keySet().stream()
+        .allMatch(letter -> letterToCount.getOrDefault(letter, 0) >= letterToMaxCount.get(letter));
   }
 }
