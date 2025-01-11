e032b17f02e57e080d80c604048eddd121cc6d9e
Kai Wang <charles.wangkai@gmail.com>
2025-01-11 08:45:35 +0800

    Construct K Palindrome Strings

--- construct-k-palindrome-strings/Solution.java
+++ construct-k-palindrome-strings/Solution.java
@@ -2,14 +2,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public boolean canConstruct(String s, int k) {
-        Map<Character, Integer> chToCount = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            chToCount.put(ch, chToCount.getOrDefault(ch, 0) + 1);
-        }
-
-        int oddCount = (int) chToCount.values().stream().filter(count -> count % 2 != 0).count();
-
-        return oddCount <= k && s.length() >= k;
+  public boolean canConstruct(String s, int k) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
     }
+
+    int oddCount = (int) letterToCount.values().stream().filter(count -> count % 2 == 1).count();
+
+    return k >= oddCount && k <= s.length();
+  }
 }
