f34ddb429eb0a4a9d04f470b9a5100a631363b09
Kai Wang <charles.wangkai@gmail.com>
2025-01-15 08:51:43 +0800

    Minimize XOR

--- minimize-xor/Solution.java
+++ minimize-xor/Solution.java
@@ -5,13 +5,13 @@ class Solution {
     int result = 0;
     int rest = Integer.bitCount(num2);
     for (int i = BIT_LIMIT; i >= 0; --i) {
-      if (rest != 0 && (num1 & (1 << i)) != 0) {
+      if (rest != 0 && ((num1 >> i) & 1) == 1) {
         result += 1 << i;
         --rest;
       }
     }
     for (int i = 0; i <= BIT_LIMIT; ++i) {
-      if (rest != 0 && (num1 & (1 << i)) == 0) {
+      if (rest != 0 && ((num1 >> i) & 1) == 0) {
         result += 1 << i;
         --rest;
       }
