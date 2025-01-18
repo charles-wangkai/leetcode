961a00b33f24df166fd1d6e4f9a69ef0a74ecbef
Kai Wang <charles.wangkai@gmail.com>
2025-01-18 08:47:13 +0800

    Minimum Cost to Make at Least One Valid Path in a Grid

--- minimum-cost-to-make-at-least-one-valid-path-in-a-grid/Solution.java
+++ minimum-cost-to-make-at-least-one-valid-path-in-a-grid/Solution.java
@@ -1,58 +1,48 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Deque;
 
-public class Solution {
-    static final int[] R_OFFSETS = { Integer.MIN_VALUE, 0, 0, 1, -1 };
-    static final int[] C_OFFSETS = { Integer.MIN_VALUE, 1, -1, 0, 0 };
+class Solution {
+  static final int[] R_OFFSETS = {Integer.MIN_VALUE, 0, 0, 1, -1};
+  static final int[] C_OFFSETS = {Integer.MIN_VALUE, 1, -1, 0, 0};
 
-    public int minCost(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+  public int minCost(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-        int[][] distances = new int[m][n];
-        for (int r = 0; r < m; ++r) {
-            Arrays.fill(distances[r], -1);
-        }
-        distances[0][0] = 0;
+    int[][] distances = new int[m][n];
+    for (int r = 0; r < m; ++r) {
+      Arrays.fill(distances[r], -1);
+    }
 
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0));
+    Deque<Element> deque = new ArrayDeque<>();
+    deque.offerFirst(new Element(0, 0, 0));
 
-        while (!queue.isEmpty()) {
-            Point head = queue.poll();
+    while (!deque.isEmpty()) {
+      Element head = deque.pollFirst();
+      if (distances[head.r()][head.c()] == -1) {
+        distances[head.r()][head.c()] = head.distance();
 
-            int nextR = head.r + R_OFFSETS[grid[head.r][head.c]];
-            int nextC = head.c + C_OFFSETS[grid[head.r][head.c]];
-            if (nextR >= 0 && nextR < m && nextC >= 0 && nextC < n
-                    && (distances[nextR][nextC] == -1 || distances[head.r][head.c] < distances[nextR][nextC])) {
-                distances[nextR][nextC] = distances[head.r][head.c];
-                queue.offer(new Point(nextR, nextC));
-            }
-
-            for (int i = 1; i < R_OFFSETS.length; ++i) {
-                if (i != grid[head.r][head.c]) {
-                    int adjR = head.r + R_OFFSETS[i];
-                    int adjC = head.c + C_OFFSETS[i];
-                    if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n
-                            && (distances[adjR][adjC] == -1 || distances[head.r][head.c] + 1 < distances[adjR][adjC])) {
-                        distances[adjR][adjC] = distances[head.r][head.c] + 1;
-                        queue.offer(new Point(adjR, adjC));
-                    }
-                }
-            }
+        int nextR = head.r() + R_OFFSETS[grid[head.r()][head.c()]];
+        int nextC = head.c() + C_OFFSETS[grid[head.r()][head.c()]];
+        if (nextR >= 0 && nextR < m && nextC >= 0 && nextC < n && distances[nextR][nextC] == -1) {
+          deque.offerFirst(new Element(nextR, nextC, head.distance()));
         }
 
-        return distances[m - 1][n - 1];
+        for (int i = 1; i < R_OFFSETS.length; ++i) {
+          if (i != grid[head.r()][head.c()]) {
+            int adjR = head.r() + R_OFFSETS[i];
+            int adjC = head.c() + C_OFFSETS[i];
+            if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && distances[adjR][adjC] == -1) {
+              deque.offerLast(new Element(adjR, adjC, head.distance() + 1));
+            }
+          }
+        }
+      }
     }
+
+    return distances[m - 1][n - 1];
+  }
 }
 
-class Point {
-    int r;
-    int c;
-
-    Point(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
+record Element(int r, int c, int distance) {}
