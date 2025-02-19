e9c548f055d830ec9b7b53555fb57a90bd287d6a
Kai Wang <charles.wangkai@gmail.com>
2025-02-19 08:44:03 +0800

    The k-th Lexicographical String of All Happy Strings of Length n

--- the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/Solution.java
+++ the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/Solution.java
@@ -2,27 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    static final char[] ALPHABET = { 'a', 'b', 'c' };
+  static final char[] LETTERS = {'a', 'b', 'c'};
 
-    public String getHappyString(int n, int k) {
-        List<String> happys = new ArrayList<>();
-        search(happys, new char[n], 0);
+  public String getHappyString(int n, int k) {
+    List<String> happys = new ArrayList<>();
+    search(happys, new char[n], 0);
 
-        return (happys.size() >= k) ? happys.get(k - 1) : "";
+    return (k <= happys.size()) ? happys.get(k - 1) : "";
+  }
+
+  void search(List<String> happys, char[] letters, int index) {
+    if (index == letters.length) {
+      happys.add(new String(letters));
+
+      return;
     }
 
-    void search(List<String> happys, char[] letters, int index) {
-        if (index == letters.length) {
-            happys.add(new String(letters));
-
-            return;
-        }
-
-        for (char letter : ALPHABET) {
-            if (index == 0 || letters[index - 1] != letter) {
-                letters[index] = letter;
-                search(happys, letters, index + 1);
-            }
-        }
+    for (char letter : LETTERS) {
+      if (index == 0 || letter != letters[index - 1]) {
+        letters[index] = letter;
+        search(happys, letters, index + 1);
+      }
     }
+  }
 }
