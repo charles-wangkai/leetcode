d45f58634ef1d66e9bffedc97fc4f36f0100d5ce
Kai Wang <charles.wangkai@gmail.com>
2025-02-23 09:07:51 +0800

    Construct Binary Tree from Preorder and Postorder Traversal

--- construct-binary-tree-from-preorder-and-postorder-traversal/Solution.java
+++ construct-binary-tree-from-preorder-and-postorder-traversal/Solution.java
@@ -1,42 +1,71 @@
 // Definition for a binary tree node.
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-public class Solution {
-	public TreeNode constructFromPrePost(int[] pre, int[] post) {
-		return build(pre, 0, pre.length - 1, post, 0, post.length - 1);
-	}
+class Solution {
+  public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+    return build(preorder, 0, preorder.length - 1, postorder, 0, postorder.length - 1);
+  }
 
-	TreeNode build(int[] pre, int preBeginIndex, int preEndIndex, int[] post, int postBeginIndex, int postEndIndex) {
-		if (preBeginIndex > preEndIndex) {
-			return null;
-		}
+  TreeNode build(
+      int[] preorder,
+      int preBeginIndex,
+      int preEndIndex,
+      int[] postorder,
+      int postBeginIndex,
+      int postEndIndex) {
+    if (preBeginIndex > preEndIndex) {
+      return null;
+    }
 
-		TreeNode node = new TreeNode(pre[preBeginIndex]);
-		if (preBeginIndex < preEndIndex) {
-			int leftPreBeginIndex = preBeginIndex + 1;
+    TreeNode node = new TreeNode(preorder[preBeginIndex]);
+    if (preBeginIndex < preEndIndex) {
+      int leftPreBeginIndex = preBeginIndex + 1;
 
-			int pos = search(post, postBeginIndex, pre[leftPreBeginIndex]);
-			int leftLength = pos - postBeginIndex + 1;
+      int pos = find(postorder, postBeginIndex, preorder[leftPreBeginIndex]);
+      int leftLength = pos - postBeginIndex + 1;
 
-			node.left = build(pre, leftPreBeginIndex, leftPreBeginIndex + leftLength - 1, post, postBeginIndex, pos);
-			node.right = build(pre, leftPreBeginIndex + leftLength, preEndIndex, post, pos + 1, postEndIndex - 1);
-		}
-		return node;
-	}
+      node.left =
+          build(
+              preorder,
+              leftPreBeginIndex,
+              leftPreBeginIndex + leftLength - 1,
+              postorder,
+              postBeginIndex,
+              pos);
+      node.right =
+          build(
+              preorder,
+              leftPreBeginIndex + leftLength,
+              preEndIndex,
+              postorder,
+              pos + 1,
+              postEndIndex - 1);
+    }
 
-	int search(int[] a, int beginIndex, int target) {
-		for (int i = beginIndex;; i++) {
-			if (a[i] == target) {
-				return i;
-			}
-		}
-	}
+    return node;
+  }
+
+  int find(int[] a, int beginIndex, int target) {
+    for (int i = beginIndex; ; ++i) {
+      if (a[i] == target) {
+        return i;
+      }
+    }
+  }
 }
