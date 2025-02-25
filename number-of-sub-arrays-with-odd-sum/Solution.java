69c0cee4bb202dbebbba5d45b2719f9eceba34c9
Kai Wang <charles.wangkai@gmail.com>
2025-02-25 08:35:46 +0800

    Number of Sub-arrays With Odd Sum

--- number-of-sub-arrays-with-odd-sum/Solution.java
+++ number-of-sub-arrays-with-odd-sum/Solution.java
@@ -1,26 +1,22 @@
 class Solution {
-    static final int MODULUS = 1_000_000_007;
+  static final int MODULUS = 1_000_000_007;
 
-    public int numOfSubarrays(int[] arr) {
-        int evenPrefixSumCount = 1;
-        int oddPrefixSumCount = 0;
-        int prefixSum = 0;
-        int result = 0;
-        for (int x : arr) {
-            prefixSum += x;
-            if (prefixSum % 2 == 0) {
-                result = addMod(result, oddPrefixSumCount);
-                ++evenPrefixSumCount;
-            } else {
-                result = addMod(result, evenPrefixSumCount);
-                ++oddPrefixSumCount;
-            }
-        }
+  public int numOfSubarrays(int[] arr) {
+    int[] dp = new int[2];
+    dp[0] = 1;
 
-        return result;
+    int result = 0;
+    int prefixSum = 0;
+    for (int x : arr) {
+      prefixSum = (prefixSum + x) % 2;
+      result = addMod(result, dp[1 - prefixSum]);
+      ++dp[prefixSum];
     }
 
-    static int addMod(int x, int y) {
-        return (x + y) % MODULUS;
-    }
+    return result;
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 }
