3e31405dd971967f25eebf7d9ce568041024053a
Kai Wang <charles.wangkai@gmail.com>
2025-03-11 08:39:03 +0800

    Number of Substrings Containing All Three Characters

--- number-of-substrings-containing-all-three-characters/Solution.java
+++ number-of-substrings-containing-all-three-characters/Solution.java
@@ -1,29 +1,29 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-    public int numberOfSubstrings(String s) {
-        int result = 0;
-        Map<Character, Integer> letterToCount = new HashMap<>();
-        int beginIndex = 0;
-        for (int endIndex = 0; endIndex < s.length(); ++endIndex) {
-            updateLetterToCount(letterToCount, s.charAt(endIndex), 1);
+class Solution {
+  public int numberOfSubstrings(String s) {
+    int result = 0;
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    int beginIndex = 0;
+    for (int endIndex = 0; endIndex < s.length(); ++endIndex) {
+      updateLetterToCount(letterToCount, s.charAt(endIndex), 1);
 
-            while (letterToCount.size() == 3 && letterToCount.get(s.charAt(beginIndex)) != 1) {
-                updateLetterToCount(letterToCount, s.charAt(beginIndex), -1);
-                ++beginIndex;
-            }
+      while (letterToCount.size() == 3 && letterToCount.get(s.charAt(beginIndex)) != 1) {
+        updateLetterToCount(letterToCount, s.charAt(beginIndex), -1);
+        ++beginIndex;
+      }
 
-            if (letterToCount.size() == 3) {
-                result += beginIndex + 1;
-            }
-        }
-
-        return result;
+      if (letterToCount.size() == 3) {
+        result += beginIndex + 1;
+      }
     }
 
-    void updateLetterToCount(Map<Character, Integer> letterToCount, char letter, int delta) {
-        letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + delta);
-        letterToCount.remove(letter, 0);
-    }
+    return result;
+  }
+
+  void updateLetterToCount(Map<Character, Integer> letterToCount, char letter, int delta) {
+    letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + delta);
+    letterToCount.remove(letter, 0);
+  }
 }
