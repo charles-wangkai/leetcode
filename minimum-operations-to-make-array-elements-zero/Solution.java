d226e1f9d97e5ddb3826a42b05b7eb73ab2b8812
Kai Wang <charles.wangkai@gmail.com>
2025-03-24 15:27:03 +0800

    Minimum Operations to Make Array Elements Zero

---
+++ minimum-operations-to-make-array-elements-zero/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  static final int LIMIT = 1_000_000_000;
+
+  static Map<Integer, Range> operationNumToRange;
+
+  static {
+    precompute();
+  }
+
+  static void precompute() {
+    operationNumToRange = new HashMap<>();
+    int min = 1;
+    int max = 3;
+    for (int operationNum = 1; ; ++operationNum) {
+      operationNumToRange.put(operationNum, new Range(min, max));
+
+      if (max >= LIMIT) {
+        break;
+      }
+
+      min = max + 1;
+      max = (max + 1) * 4 - 1;
+    }
+  }
+
+  public long minOperations(int[][] queries) {
+    return Arrays.stream(queries)
+        .mapToLong(query -> computeMinOperationNum(query[0], query[1]))
+        .sum();
+  }
+
+  long computeMinOperationNum(int left, int right) {
+    Map<Integer, Integer> operationNumToCount = new HashMap<>();
+    for (int operationNum : operationNumToRange.keySet()) {
+      int min = Math.max(left, operationNumToRange.get(operationNum).min());
+      int max = Math.min(right, operationNumToRange.get(operationNum).max());
+      if (min <= max) {
+        operationNumToCount.put(operationNum, max - min + 1);
+      }
+    }
+
+    return (operationNumToCount.keySet().stream()
+                .mapToLong(
+                    operationNum -> (long) operationNum * operationNumToCount.get(operationNum))
+                .sum()
+            + 1)
+        / 2;
+  }
+}
+
+record Range(int min, int max) {}
