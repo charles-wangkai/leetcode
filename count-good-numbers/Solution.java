32e47c9e43b59f94f76dab4a1fe205c50d1a984b
Kai Wang <charles.wangkai@gmail.com>
2025-04-13 08:48:33 +0800

    Count Good Numbers

--- count-good-numbers/Solution.java
+++ count-good-numbers/Solution.java
@@ -2,27 +2,19 @@ class Solution {
   static final int MODULUS = 1_000_000_007;
 
   public int countGoodNumbers(long n) {
-    long evenCount = (n + 1) / 2;
-    long oddCount = n - evenCount;
-
-    return multiplyMod(powMod(5, evenCount), powMod(4, oddCount));
+    return multiplyMod(powMod(5, (n + 1) / 2), powMod(4, n / 2));
   }
 
   int multiplyMod(int x, int y) {
-    return (int) ((long) x * y % MODULUS);
+    return Math.floorMod((long) x * y, MODULUS);
   }
 
   int powMod(int base, long exponent) {
-    int result = 1;
-    while (exponent != 0) {
-      if ((exponent & 1) != 0) {
-        result = multiplyMod(result, base);
-      }
-
-      base = multiplyMod(base, base);
-      exponent >>= 1;
+    if (exponent == 0) {
+      return 1;
     }
 
-    return result;
+    return multiplyMod(
+        powMod(multiplyMod(base, base), exponent / 2), (exponent % 2 == 0) ? 1 : base);
   }
 }
