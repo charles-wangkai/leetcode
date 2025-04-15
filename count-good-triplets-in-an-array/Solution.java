ca14026b7bcb0ae639b090aaee60b5cb8d8b067c
Kai Wang <charles.wangkai@gmail.com>
2025-04-15 08:35:28 +0800

    Count Good Triplets in an Array

--- count-good-triplets-in-an-array/Solution.java
+++ count-good-triplets-in-an-array/Solution.java
@@ -17,36 +17,35 @@ class Solution {
   int[] buildLeftSameNums(int[] nums1, int[] nums2) {
     int n = nums1.length;
 
-    int[] A = new int[Integer.highestOneBit(n) * 2 + 1];
+    int[] binaryIndexedTree = new int[Integer.highestOneBit(n) * 2 + 1];
 
     Map<Integer, Integer> valueToIndex =
         IntStream.range(0, n).boxed().collect(Collectors.toMap(i -> nums2[i], i -> i));
 
     int[] result = new int[n];
     for (int i = 0; i < result.length; ++i) {
-      result[i] = prefix_sum(A, valueToIndex.get(nums1[i]));
-      add(A, valueToIndex.get(nums1[i]), 1);
+      result[i] = query(binaryIndexedTree, valueToIndex.get(nums1[i]) + 1);
+      update(binaryIndexedTree, valueToIndex.get(nums1[i]) + 1, 1);
     }
 
     return result;
   }
 
-  int LSB(int i) {
-    return i & -i;
-  }
-
-  int prefix_sum(int[] A, int i) {
-    int sum = A[0];
-    for (; i != 0; i -= LSB(i)) sum += A[i];
-    return sum;
-  }
-
-  void add(int[] A, int i, int delta) {
-    if (i == 0) {
-      A[0] += delta;
-      return;
+  int query(int[] binaryIndexedTree, int index) {
+    int result = 0;
+    while (index != 0) {
+      result += binaryIndexedTree[index];
+      index -= index & -index;
     }
-    for (; i < A.length; i += LSB(i)) A[i] += delta;
+
+    return result;
+  }
+
+  void update(int[] binaryIndexedTree, int index, int delta) {
+    while (index < binaryIndexedTree.length) {
+      binaryIndexedTree[index] += delta;
+      index += index & -index;
+    }
   }
 
   int[] buildRightSameNums(int[] nums1, int[] nums2) {
