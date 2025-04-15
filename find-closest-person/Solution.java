2009f44fd57a22d2033a75db7650bc2f29845af6
Kai Wang <charles.wangkai@gmail.com>
2025-04-15 08:48:54 +0800

    Find Closest Person

---
+++ find-closest-person/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int findClosest(int x, int y, int z) {
+    int distance1 = Math.abs(x - z);
+    int distance2 = Math.abs(y - z);
+    if (distance1 < distance2) {
+      return 1;
+    } else if (distance1 > distance2) {
+      return 2;
+    }
+
+    return 0;
+  }
+}
