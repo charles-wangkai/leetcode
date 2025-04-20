ea6e9007a278b6f8f0b88c8ef4ff04f59d6176e7
Kai Wang <charles.wangkai@gmail.com>
2025-04-20 16:14:38 +0800

    Rabbits in Forest

--- rabbits-in-forest/Solution.java
+++ rabbits-in-forest/Solution.java
@@ -1,20 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
+class Solution {
   public int numRabbits(int[] answers) {
-    Map<Integer, Integer> size2count = new HashMap<>();
+    Map<Integer, Integer> sizeToCount = new HashMap<>();
     for (int answer : answers) {
       int size = answer + 1;
-      size2count.put(size, size2count.getOrDefault(size, 0) + 1);
+      sizeToCount.put(size, sizeToCount.getOrDefault(size, 0) + 1);
     }
 
-    return size2count.entrySet().stream()
-        .mapToInt(
-            entry ->
-                entry.getKey()
-                    * (entry.getValue() / entry.getKey()
-                        + (entry.getValue() % entry.getKey() == 0 ? 0 : 1)))
+    return sizeToCount.keySet().stream()
+        .mapToInt(size -> (sizeToCount.get(size) + size - 1) / size * size)
         .sum();
   }
 }
