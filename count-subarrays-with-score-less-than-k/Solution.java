9b59c0d1e38ced53f5363b7b6013e468b44f3782
Kai Wang <charles.wangkai@gmail.com>
2025-04-28 11:12:55 +0800

    Count Subarrays With Score Less Than K

--- count-subarrays-with-score-less-than-k/Solution.java
+++ count-subarrays-with-score-less-than-k/Solution.java
@@ -1,37 +1,20 @@
-import java.util.stream.IntStream;
-
 class Solution {
   public long countSubarrays(int[] nums, long k) {
-    long[] prefixSums = new long[nums.length];
-    for (int i = 0; i < prefixSums.length; ++i) {
-      prefixSums[i] = ((i == 0) ? 0 : prefixSums[i - 1]) + nums[i];
-    }
-
-    return IntStream.range(0, nums.length)
-        .map(i -> computeSubarrayNum(k, prefixSums, i))
-        .asLongStream()
-        .sum();
-  }
-
-  int computeSubarrayNum(long k, long[] prefixSums, int beginIndex) {
-    int index = beginIndex - 1;
-    int lowerIndex = beginIndex;
-    int upperIndex = prefixSums.length - 1;
-    while (lowerIndex <= upperIndex) {
-      int middleIndex = (lowerIndex + upperIndex) / 2;
-      if (computeRangeSum(prefixSums, beginIndex, middleIndex) * (middleIndex - beginIndex + 1)
-          < k) {
-        index = middleIndex;
-        lowerIndex = middleIndex + 1;
-      } else {
-        upperIndex = middleIndex - 1;
+    long result = 0;
+    int endIndex = -1;
+    long sum = 0;
+    for (int beginIndex = 0; beginIndex < nums.length; ++beginIndex) {
+      while (endIndex != nums.length - 1
+          && (sum + nums[endIndex + 1]) * (endIndex + 1 - beginIndex + 1) < k) {
+        ++endIndex;
+        sum += nums[endIndex];
       }
+
+      result += endIndex - beginIndex + 1;
+
+      sum -= nums[beginIndex];
     }
 
-    return index - beginIndex + 1;
-  }
-
-  long computeRangeSum(long[] prefixSums, int beginIndex, int endIndex) {
-    return prefixSums[endIndex] - ((beginIndex == 0) ? 0 : prefixSums[beginIndex - 1]);
+    return result;
   }
 }
