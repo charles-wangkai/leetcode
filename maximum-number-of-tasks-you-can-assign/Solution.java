4c5e3540c219415239e7df2b4606f25b942c5320
Kai Wang <charles.wangkai@gmail.com>
2025-05-01 09:01:02 +0800

    Maximum Number of Tasks You Can Assign

--- maximum-number-of-tasks-you-can-assign/Solution.java
+++ maximum-number-of-tasks-you-can-assign/Solution.java
@@ -4,15 +4,15 @@ import java.util.TreeMap;
 
 class Solution {
   public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
-    int[] sortedTasks = Arrays.stream(tasks).boxed().sorted().mapToInt(x -> x).toArray();
-    int[] sortedWorkers = Arrays.stream(workers).boxed().sorted().mapToInt(x -> x).toArray();
+    Arrays.sort(tasks);
+    Arrays.sort(workers);
 
     int result = 0;
     int lower = 1;
     int upper = Math.min(tasks.length, workers.length);
     while (lower <= upper) {
       int middle = (lower + upper) / 2;
-      if (check(sortedTasks, sortedWorkers, pills, strength, middle)) {
+      if (check(tasks, workers, pills, strength, middle)) {
         result = middle;
         lower = middle + 1;
       } else {
@@ -23,16 +23,16 @@ class Solution {
     return result;
   }
 
-  boolean check(int[] sortedTasks, int[] sortedWorkers, int pills, int strength, int matchedNum) {
+  boolean check(int[] tasks, int[] workers, int pills, int strength, int matchedNum) {
     NavigableMap<Integer, Integer> workerToCount = new TreeMap<>();
-    for (int i = sortedWorkers.length - 1; i >= sortedWorkers.length - matchedNum; --i) {
-      workerToCount.put(sortedWorkers[i], workerToCount.getOrDefault(sortedWorkers[i], 0) + 1);
+    for (int i = workers.length - 1; i >= workers.length - matchedNum; --i) {
+      workerToCount.put(workers[i], workerToCount.getOrDefault(workers[i], 0) + 1);
     }
 
     for (int i = matchedNum - 1; i >= 0; --i) {
-      Integer worker = workerToCount.ceilingKey(sortedTasks[i]);
+      Integer worker = workerToCount.ceilingKey(tasks[i]);
       if (worker == null) {
-        worker = workerToCount.ceilingKey(sortedTasks[i] - strength);
+        worker = workerToCount.ceilingKey(tasks[i] - strength);
         if (pills == 0 || worker == null) {
           return false;
         }
