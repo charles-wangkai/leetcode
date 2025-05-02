16f9ef96b233707869593931aa3b791cc2e62089
Kai Wang <charles.wangkai@gmail.com>
2025-05-02 16:29:36 +0800

    Push Dominoes

--- push-dominoes/Solution.java
+++ push-dominoes/Solution.java
@@ -3,57 +3,51 @@ import java.util.stream.IntStream;
 
 class Solution {
   public String pushDominoes(String dominoes) {
-    Side[] leftSides = new Side[dominoes.length()];
-    char state = 0;
-    int distance = 0;
-    for (int i = 0; i < leftSides.length; ++i) {
-      char c = dominoes.charAt(i);
-      if (c != '.') {
-        state = c;
-        distance = 0;
+    int[] rightDistances = new int[dominoes.length()];
+    int rightDistance = Integer.MAX_VALUE;
+    for (int i = 0; i < rightDistances.length; ++i) {
+      if (dominoes.charAt(i) == 'R') {
+        rightDistance = 0;
+      } else if (rightDistance != Integer.MAX_VALUE) {
+        ++rightDistance;
       }
-      leftSides[i] = new Side(state, distance);
 
-      ++distance;
+      rightDistances[i] = rightDistance;
+
+      if (dominoes.charAt(i) == 'L') {
+        rightDistance = Integer.MAX_VALUE;
+      }
     }
 
-    Side[] rightSides = new Side[dominoes.length()];
-    state = 0;
-    distance = 0;
-    for (int i = rightSides.length - 1; i >= 0; --i) {
-      char c = dominoes.charAt(i);
-      if (c != '.') {
-        state = c;
-        distance = 0;
+    int[] leftDistances = new int[dominoes.length()];
+    int leftDistance = Integer.MAX_VALUE;
+    for (int i = leftDistances.length - 1; i >= 0; --i) {
+      if (dominoes.charAt(i) == 'L') {
+        leftDistance = 0;
+      } else if (leftDistance != Integer.MAX_VALUE) {
+        ++leftDistance;
       }
-      rightSides[i] = new Side(state, distance);
 
-      ++distance;
+      leftDistances[i] = leftDistance;
+
+      if (dominoes.charAt(i) == 'R') {
+        leftDistance = Integer.MAX_VALUE;
+      }
     }
 
     return IntStream.range(0, dominoes.length())
-        .mapToObj(i -> computeFinalState(leftSides[i], rightSides[i]))
+        .mapToObj(
+            i -> {
+              if (rightDistances[i] < leftDistances[i]) {
+                return 'R';
+              }
+              if (rightDistances[i] > leftDistances[i]) {
+                return 'L';
+              }
+
+              return '.';
+            })
         .map(String::valueOf)
         .collect(Collectors.joining());
   }
-
-  char computeFinalState(Side leftSide, Side rightSide) {
-    if (leftSide.state() == 'R') {
-      if (rightSide.state() == 'L') {
-        if (leftSide.distance() < rightSide.distance()) {
-          return 'R';
-        } else if (leftSide.distance() > rightSide.distance()) {
-          return 'L';
-        } else {
-          return '.';
-        }
-      }
-
-      return 'R';
-    }
-
-    return (rightSide.state() == 'L') ? 'L' : '.';
-  }
 }
-
-record Side(char state, int distance) {}
