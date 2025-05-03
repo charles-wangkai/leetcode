3166787292463fbdf1825e2317bacd9c3cc93ef0
Kai Wang <charles.wangkai@gmail.com>
2025-05-03 08:29:02 +0800

    Minimum Domino Rotations For Equal Row

--- minimum-domino-rotations-for-equal-row/Solution.java
+++ minimum-domino-rotations-for-equal-row/Solution.java
@@ -1,26 +1,30 @@
-class Solution {
-  public int minDominoRotations(int[] A, int[] B) {
-    int result = Integer.MAX_VALUE;
-    for (int target = 1; target <= 6; ++target) {
-      result =
-          Math.min(result, Math.min(findRotationNum(A, B, target), findRotationNum(B, A, target)));
-    }
+import java.util.stream.IntStream;
 
-    return (result == Integer.MAX_VALUE) ? -1 : result;
+class Solution {
+  public int minDominoRotations(int[] tops, int[] bottoms) {
+    return IntStream.rangeClosed(1, 6)
+        .flatMap(
+            target ->
+                IntStream.of(
+                    computeRotationNum(tops, bottoms, target),
+                    computeRotationNum(bottoms, tops, target)))
+        .filter(target -> target != Integer.MAX_VALUE)
+        .min()
+        .orElse(-1);
   }
 
-  int findRotationNum(int[] primary, int[] secondary, int target) {
-    int rotationNum = 0;
+  int computeRotationNum(int[] primary, int[] secondary, int target) {
+    int result = 0;
     for (int i = 0; i < primary.length; ++i) {
       if (primary[i] != target) {
-        if (secondary[i] == target) {
-          ++rotationNum;
-        } else {
+        if (secondary[i] != target) {
           return Integer.MAX_VALUE;
         }
+
+        ++result;
       }
     }
 
-    return rotationNum;
+    return result;
   }
 }
