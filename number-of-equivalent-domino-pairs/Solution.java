e422e7f6ebc58755719ed96f5b63aa1ae4a0ce81
Kai Wang <charles.wangkai@gmail.com>
2025-05-04 14:13:54 +0800

    Number of Equivalent Domino Pairs

--- number-of-equivalent-domino-pairs/Solution.java
+++ number-of-equivalent-domino-pairs/Solution.java
@@ -2,19 +2,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public int numEquivDominoPairs(int[][] dominoes) {
-		Map<String, Integer> keyToCount = new HashMap<>();
-		for (int[] domino : dominoes) {
-			String key = buildKey(domino);
+class Solution {
+  public int numEquivDominoPairs(int[][] dominoes) {
+    Map<String, Integer> keyToCount = new HashMap<>();
+    for (int[] domino : dominoes) {
+      String key = generateKey(domino);
+      keyToCount.put(key, keyToCount.getOrDefault(key, 0) + 1);
+    }
 
-			keyToCount.put(key, keyToCount.getOrDefault(key, 0) + 1);
-		}
+    return keyToCount.values().stream().mapToInt(count -> count * (count - 1) / 2).sum();
+  }
 
-		return keyToCount.values().stream().mapToInt(count -> count * (count - 1) / 2).sum();
-	}
-
-	String buildKey(int[] domino) {
-		return Arrays.toString(Arrays.stream(domino).sorted().toArray());
-	}
+  String generateKey(int[] domino) {
+    return Arrays.toString(Arrays.stream(domino).sorted().toArray());
+  }
 }
