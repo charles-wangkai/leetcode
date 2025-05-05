d01e3cafcfd75a05b6c8246510ee50722ae3f042
Kai Wang <charles.wangkai@gmail.com>
2025-05-05 10:15:36 +0800

    Domino and Tromino Tiling

--- domino-and-tromino-tiling/Solution.java
+++ domino-and-tromino-tiling/Solution.java
@@ -2,18 +2,18 @@ class Solution {
   static final int MODULUS = 1_000_000_007;
 
   public int numTilings(int n) {
-    int[][] tilings = new int[n + 1][3];
-    tilings[0][0] = 1;
-    for (int i = 1; i < tilings.length; ++i) {
-      tilings[i][0] =
+    int[][] dp = new int[n + 1][3];
+    dp[0][0] = 1;
+    for (int i = 1; i < dp.length; ++i) {
+      dp[i][0] =
           addMod(
-              addMod(tilings[i - 1][0], (i >= 2) ? tilings[i - 2][0] : 0),
-              addMod(tilings[i - 1][1], tilings[i - 1][2]));
-      tilings[i][1] = addMod(tilings[i - 1][2], (i >= 2) ? tilings[i - 2][0] : 0);
-      tilings[i][2] = addMod(tilings[i - 1][1], (i >= 2) ? tilings[i - 2][0] : 0);
+              addMod(dp[i - 1][0], (i >= 2) ? dp[i - 2][0] : 0),
+              addMod(dp[i - 1][1], dp[i - 1][2]));
+      dp[i][1] = addMod(dp[i - 1][2], (i >= 2) ? dp[i - 2][0] : 0);
+      dp[i][2] = addMod(dp[i - 1][1], (i >= 2) ? dp[i - 2][0] : 0);
     }
 
-    return tilings[n][0];
+    return dp[n][0];
   }
 
   int addMod(int x, int y) {
