2d26b9d0536aed7e8dba925fe7f71f9d7b324f5b
Kai Wang <charles.wangkai@gmail.com>
2025-05-13 14:44:56 +0800

    Find Most Frequent Vowel and Consonant

---
+++ find-most-frequent-vowel-and-consonant/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  static final String VOWELS = "aeiou";
+
+  public int maxFreqSum(String s) {
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
+
+    return letterToCount.keySet().stream()
+            .filter(this::isVowel)
+            .mapToInt(letterToCount::get)
+            .max()
+            .orElse(0)
+        + letterToCount.keySet().stream()
+            .filter(letter -> !isVowel(letter))
+            .mapToInt(letterToCount::get)
+            .max()
+            .orElse(0);
+  }
+
+  boolean isVowel(char letter) {
+    return VOWELS.indexOf(letter) != -1;
+  }
+}
