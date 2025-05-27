13a809231897bf7fc5e266ad77b5c1c6f85cd8f5
Kai Wang <charles.wangkai@gmail.com>
2025-05-27 12:01:25 +0800

    Divisible and Non-divisible Sums Difference

--- divisible-and-non-divisible-sums-difference/Solution.java
+++ divisible-and-non-divisible-sums-difference/Solution.java
@@ -2,7 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int differenceOfSums(int n, int m) {
-    return IntStream.rangeClosed(1, n).filter(i -> i % m != 0).sum()
-        - IntStream.rangeClosed(1, n).filter(i -> i % m == 0).sum();
+    return IntStream.rangeClosed(1, n).map(i -> ((i % m == 0) ? -1 : 1) * i).sum();
   }
 }
