89c950a20e799379bf54423650812e185cb62de7
Kai Wang <charles.wangkai@gmail.com>
2025-05-30 08:44:07 +0800

    Find Closest Node to Given Two Nodes

--- find-closest-node-to-given-two-nodes/Solution.java
+++ find-closest-node-to-given-two-nodes/Solution.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 class Solution {
   public int closestMeetingNode(int[] edges, int node1, int node2) {
+    int n = edges.length;
+
     int[] distances1 = computeDistances(edges, node1);
     int[] distances2 = computeDistances(edges, node2);
 
     int minMax = Integer.MAX_VALUE;
     int result = -1;
-    for (int i = 0; i < edges.length; ++i) {
+    for (int i = 0; i < n; ++i) {
       int max = Math.max(distances1[i], distances2[i]);
       if (max < minMax) {
         minMax = max;
@@ -21,9 +23,13 @@ class Solution {
   int[] computeDistances(int[] edges, int node) {
     int[] result = new int[edges.length];
     Arrays.fill(result, Integer.MAX_VALUE);
-    for (int i = 0; node != -1 && result[node] == Integer.MAX_VALUE; ++i) {
-      result[node] = i;
+
+    int distance = 0;
+    while (node != -1 && result[node] == Integer.MAX_VALUE) {
+      result[node] = distance;
+
       node = edges[node];
+      ++distance;
     }
 
     return result;
