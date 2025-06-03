bc2919fd58b6bef1d9dabdd9561eef5894d84803
Kai Wang <charles.wangkai@gmail.com>
2025-06-03 08:28:59 +0800

    Maximum Candies You Can Get from Boxes

--- maximum-candies-you-can-get-from-boxes/Solution.java
+++ maximum-candies-you-can-get-from-boxes/Solution.java
@@ -1,40 +1,40 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Solution {
-	public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-		int n = status.length;
-		boolean[] processed = new boolean[n];
-		boolean[] found = new boolean[n];
-		boolean[] hasKeys = new boolean[n];
+class Solution {
+  public int maxCandies(
+      int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+    int n = status.length;
 
-		Queue<Integer> queue = new LinkedList<>();
-		for (int initialBox : initialBoxes) {
-			found[initialBox] = true;
-			queue.offer(initialBox);
-		}
+    boolean[] processed = new boolean[n];
+    boolean[] found = new boolean[n];
+    boolean[] hasKeys = new boolean[n];
 
-		int result = 0;
-		while (!queue.isEmpty()) {
-			int box = queue.poll();
-			if (processed[box] || !found[box] || (status[box] == 0 && !hasKeys[box])) {
-				continue;
-			}
+    Queue<Integer> queue = new ArrayDeque<>();
+    for (int initialBox : initialBoxes) {
+      found[initialBox] = true;
+      queue.offer(initialBox);
+    }
 
-			processed[box] = true;
-			result += candies[box];
+    int result = 0;
+    while (!queue.isEmpty()) {
+      int box = queue.poll();
+      if (!processed[box] && found[box] && (status[box] == 1 || hasKeys[box])) {
+        processed[box] = true;
+        result += candies[box];
 
-			for (int key : keys[box]) {
-				hasKeys[key] = true;
-				queue.offer(key);
-			}
+        for (int key : keys[box]) {
+          hasKeys[key] = true;
+          queue.offer(key);
+        }
 
-			for (int containedBox : containedBoxes[box]) {
-				found[containedBox] = true;
-				queue.offer(containedBox);
-			}
-		}
+        for (int containedBox : containedBoxes[box]) {
+          found[containedBox] = true;
+          queue.offer(containedBox);
+        }
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
