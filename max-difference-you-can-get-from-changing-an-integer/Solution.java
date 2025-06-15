4a069f9984e1377d24c9e11171f8de4b4321b23c
Kai Wang <charles.wangkai@gmail.com>
2025-06-15 09:30:53 +0800

    Max Difference You Can Get From Changing an Integer

--- max-difference-you-can-get-from-changing-an-integer/Solution.java
+++ max-difference-you-can-get-from-changing-an-integer/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int maxDiff(int num) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int x = 0; x <= 9; ++x) {
-            for (int y = 0; y <= 9; ++y) {
-                String replaced = String.valueOf(num).replaceAll(String.valueOf(x), String.valueOf(y));
-                if (replaced.charAt(0) != '0') {
-                    int value = Integer.parseInt(replaced);
+  public int maxDiff(int num) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int x = 0; x <= 9; ++x) {
+      for (int y = 0; y <= 9; ++y) {
+        String replaced = String.valueOf(num).replace((char) (x + '0'), (char) (y + '0'));
+        if (replaced.charAt(0) != '0') {
+          int value = Integer.parseInt(replaced);
 
-                    min = Math.min(min, value);
-                    max = Math.max(max, value);
-                }
-            }
+          min = Math.min(min, value);
+          max = Math.max(max, value);
         }
-
-        return max - min;
+      }
     }
+
+    return max - min;
+  }
 }
