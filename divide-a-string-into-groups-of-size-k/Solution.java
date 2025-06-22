afc7fd8f3e457e57c42202a0d61b3105d6d7948a
Kai Wang <charles.wangkai@gmail.com>
2025-06-22 09:05:23 +0800

    Divide a String Into Groups of Size k

--- divide-a-string-into-groups-of-size-k/Solution.java
+++ divide-a-string-into-groups-of-size-k/Solution.java
@@ -2,7 +2,7 @@ import java.util.stream.IntStream;
 
 class Solution {
   public String[] divideString(String s, int k, char fill) {
-    String filled = String.format("%s%s", s, String.valueOf(fill).repeat((k - s.length() % k) % k));
+    String filled = s + String.valueOf(fill).repeat((k - s.length() % k) % k);
 
     return IntStream.range(0, filled.length() / k)
         .mapToObj(i -> filled.substring(i * k, i * k + k))
