de58bc9718164d42c27b2710c6e38511e7dcdfc4
Kai Wang <charles.wangkai@gmail.com>
2025-06-26 08:55:30 +0800

    Longest Binary Subsequence Less Than or Equal to K

--- longest-binary-subsequence-less-than-or-equal-to-k/Solution.java
+++ longest-binary-subsequence-less-than-or-equal-to-k/Solution.java
@@ -8,17 +8,13 @@ class Solution {
     dp[0] = 0;
 
     for (char c : s.toCharArray()) {
-      int[] nextDp = dp.clone();
-      for (int i = nextDp.length - 1; i >= 1; --i) {
+      for (int i = dp.length - 1; i >= 1; --i) {
         if (dp[i - 1] != Integer.MAX_VALUE && dp[i - 1] * 2 + (c - '0') <= k) {
-          nextDp[i] = Math.min(nextDp[i], dp[i - 1] * 2 + (c - '0'));
+          dp[i] = Math.min(dp[i], dp[i - 1] * 2 + (c - '0'));
         }
       }
-
-      dp = nextDp;
     }
 
-    int[] dp_ = dp;
-    return IntStream.range(0, dp.length).filter(i -> dp_[i] != Integer.MAX_VALUE).max().getAsInt();
+    return IntStream.range(0, dp.length).filter(i -> dp[i] != Integer.MAX_VALUE).max().getAsInt();
   }
 }
