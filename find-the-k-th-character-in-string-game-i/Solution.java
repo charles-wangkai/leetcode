ce07aefe0ba1b6258d73f415eec80b0aebaf5803
Kai Wang <charles.wangkai@gmail.com>
2025-07-03 08:31:41 +0800

    Find the K-th Character in String Game I

--- find-the-k-th-character-in-string-game-i/Solution.java
+++ find-the-k-th-character-in-string-game-i/Solution.java
@@ -2,13 +2,13 @@ import java.util.stream.Collectors;
 
 class Solution {
   public char kthCharacter(int k) {
-    String word = "a";
+    StringBuilder word = new StringBuilder("a");
     while (word.length() < k) {
-      word +=
+      word.append(
           word.chars()
               .mapToObj(c -> (char) ((c == 'z') ? 'a' : (c + 1)))
               .map(String::valueOf)
-              .collect(Collectors.joining());
+              .collect(Collectors.joining()));
     }
 
     return word.charAt(k - 1);
