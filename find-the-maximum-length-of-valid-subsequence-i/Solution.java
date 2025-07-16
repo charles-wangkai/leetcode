b536d1a88def45313f4ebc79a03c0552f15cb14e
Kai Wang <charles.wangkai@gmail.com>
2025-07-16 08:23:18 +0800

    Find the Maximum Length of Valid Subsequence I

--- find-the-maximum-length-of-valid-subsequence-i/Solution.java
+++ find-the-maximum-length-of-valid-subsequence-i/Solution.java
@@ -3,19 +3,19 @@ import java.util.stream.IntStream;
 class Solution {
   public int maximumLength(int[] nums) {
     return IntStream.of(
-            computeLength(nums, false, false),
-            computeLength(nums, false, true),
-            computeLength(nums, true, false),
-            computeLength(nums, true, true))
+            computeLength(nums, 0, 0),
+            computeLength(nums, 0, 1),
+            computeLength(nums, 1, 0),
+            computeLength(nums, 1, 1))
         .max()
         .getAsInt();
   }
 
-  int computeLength(int[] nums, boolean firstEven, boolean secondEven) {
+  int computeLength(int[] nums, int firstRemainder, int secondRemainder) {
     int result = 0;
     for (int num : nums) {
-      if ((result % 2 == 0 && (num % 2 == 0) == firstEven)
-          || (result % 2 == 1 && (num % 2 == 0) == secondEven)) {
+      if ((result % 2 == 0 && num % 2 == firstRemainder)
+          || (result % 2 == 1 && num % 2 == secondRemainder)) {
         ++result;
       }
     }
