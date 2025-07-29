50d4a813d62b2a395fac70f1711ec5a95e454ef1
Kai Wang <charles.wangkai@gmail.com>
2025-07-29 08:40:55 +0800

    Smallest Subarrays With Maximum Bitwise OR

--- smallest-subarrays-with-maximum-bitwise-or/Solution.java
+++ smallest-subarrays-with-maximum-bitwise-or/Solution.java
@@ -5,14 +5,14 @@ class Solution {
   public int[] smallestSubarrays(int[] nums) {
     int[] result = new int[nums.length];
     Map<Integer, Integer> posToIndex = new HashMap<>();
-    for (int i = nums.length - 1; i >= 0; --i) {
+    for (int i = result.length - 1; i >= 0; --i) {
       for (int pos = 0; (1 << pos) <= nums[i]; ++pos) {
-        if ((nums[i] & (1 << pos)) != 0) {
+        if (((nums[i] >> pos) & 1) == 1) {
           posToIndex.put(pos, i);
         }
       }
 
-      result[i] = posToIndex.values().stream().mapToInt(x -> x).max().orElse(i) - i + 1;
+      result[i] = posToIndex.values().stream().mapToInt(Integer::intValue).max().orElse(i) - i + 1;
     }
 
     return result;
