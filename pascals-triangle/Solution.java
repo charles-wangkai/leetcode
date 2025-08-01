bd81d86ad3e3defe58b21f554f29a15acedce46d
Kai Wang <charles.wangkai@gmail.com>
2025-08-01 08:19:53 +0800

    Pascal's Triangle

--- pascals-triangle/Solution.java
+++ pascals-triangle/Solution.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 class Solution {
   public List<List<Integer>> generate(int numRows) {
-    List<List<Integer>> rows = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
     for (int i = 0; i < numRows; ++i) {
       List<Integer> row = new ArrayList<>();
       row.add(1);
-      if (!rows.isEmpty()) {
-        List<Integer> lastRow = rows.get(rows.size() - 1);
-        for (int j = 0; j < lastRow.size() - 1; ++j) {
-          row.add(lastRow.get(j) + lastRow.get(j + 1));
+      if (!result.isEmpty()) {
+        for (int j = 0; j < result.getLast().size() - 1; ++j) {
+          row.add(result.getLast().get(j) + result.getLast().get(j + 1));
         }
+
         row.add(1);
       }
-      rows.add(row);
+      result.add(row);
     }
 
-    return rows;
+    return result;
   }
 }
