bf4d320c60e3ecf2259aa0b3569a2cdcb044a9ea
Kai Wang <charles.wangkai@gmail.com>
2025-08-16 08:09:44 +0800

    Maximum 69 Number

--- maximum-69-number/Solution.java
+++ maximum-69-number/Solution.java
@@ -1,10 +1,5 @@
 class Solution {
   public int maximum69Number(int num) {
-    String s = String.valueOf(num);
-    int index = s.indexOf('6');
-
-    return (index >= 0)
-        ? Integer.parseInt(String.format("%s9%s", s.substring(0, index), s.substring(index + 1)))
-        : num;
+    return Integer.parseInt(String.valueOf(num).replaceFirst("6", "9"));
   }
 }
