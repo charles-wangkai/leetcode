cc82769fb610256ca0e044178671f92165fa6db8
Kai Wang <charles.wangkai@gmail.com>
2025-08-18 08:48:53 +0800

    24 Game

--- 24-game/Solution.java
+++ 24-game/Solution.java
@@ -1,98 +1,65 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class Solution {
+class Solution {
   static final Operation[] OPERATIONS = {
-    (x, y) ->
-        new Rational(
-            x.numerator * y.denominator + y.numerator * x.denominator,
-            x.denominator * y.denominator),
-    (x, y) ->
-        new Rational(
-            x.numerator * y.denominator - y.numerator * x.denominator,
-            x.denominator * y.denominator),
-    (x, y) -> new Rational(x.numerator * y.numerator, x.denominator * y.denominator),
-    (x, y) -> {
-      if (y.numerator == 0) {
-        throw new UnsupportedOperationException();
-      }
-      return new Rational(x.numerator * y.denominator, x.denominator * y.numerator);
-    }
+    (x, y) -> new Rational(x.numer() * y.denom() + y.numer() * x.denom(), x.denom() * y.denom()),
+    (x, y) -> new Rational(x.numer() * y.denom() - y.numer() * x.denom(), x.denom() * y.denom()),
+    (x, y) -> new Rational(x.numer() * y.numer(), x.denom() * y.denom()),
+    (x, y) -> (y.numer() == 0) ? null : new Rational(x.numer() * y.denom(), x.denom() * y.numer())
   };
 
-  public boolean judgePoint24(int[] nums) {
-    return search(Arrays.stream(nums).mapToObj(num -> new Rational(num, 1)).toList());
+  public boolean judgePoint24(int[] cards) {
+    return search(Arrays.stream(cards).mapToObj(num -> new Rational(num, 1)).toList());
   }
 
-  boolean search(List<Rational> numbers) {
-    if (numbers.size() == 1) {
-      return numbers.get(0).equals(new Rational(24, 1));
+  boolean search(List<Rational> values) {
+    if (values.size() == 1) {
+      return values.get(0).equals(new Rational(24, 1));
     }
 
-    for (int i = 0; i < numbers.size(); i++) {
-      for (int j = 0; j < numbers.size(); j++) {
-        if (j == i) {
-          continue;
-        }
+    for (int i = 0; i < values.size(); ++i) {
+      for (int j = 0; j < values.size(); ++j) {
+        if (j != i) {
+          for (Operation operation : OPERATIONS) {
+            Rational combined = operation.operate(values.get(i), values.get(j));
 
-        for (Operation operation : OPERATIONS) {
-          List<Rational> nextNumbers = new ArrayList<>();
-
-          try {
-            nextNumbers.add(operation.operate(numbers.get(i), numbers.get(j)));
-          } catch (UnsupportedOperationException e) {
-            continue;
-          }
-
-          int tempI = i;
-          int tempJ = j;
-          nextNumbers.addAll(
-              IntStream.range(0, numbers.size())
-                  .filter(k -> k != tempI && k != tempJ)
-                  .mapToObj(numbers::get)
-                  .toList());
-
-          if (search(nextNumbers)) {
-            return true;
+            int i_ = i;
+            int j_ = j;
+            if (combined != null
+                && search(
+                    Stream.concat(
+                            Stream.of(combined),
+                            IntStream.range(0, values.size())
+                                .filter(k -> k != i_ && k != j_)
+                                .mapToObj(values::get))
+                        .toList())) {
+              return true;
+            }
           }
         }
       }
     }
+
     return false;
   }
 }
 
-@FunctionalInterface
 interface Operation {
   Rational operate(Rational x, Rational y);
 }
 
-class Rational {
-  int numerator;
-  int denominator;
+record Rational(int numer, int denom) {
+  Rational(int numer, int denom) {
+    int g = gcd(numer, denom);
 
-  Rational(int numerator, int denominator) {
-    int g = gcd(numerator, denominator);
-
-    this.numerator = numerator / g;
-    this.denominator = denominator / g;
+    this.numer = numer / g;
+    this.denom = denom / g;
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    Rational other = (Rational) obj;
-    return numerator == other.numerator && denominator == other.denominator;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(numerator, denominator);
-  }
-
-  int gcd(int a, int b) {
-    return (b == 0) ? a : gcd(b, a % b);
+  int gcd(int x, int y) {
+    return (y == 0) ? x : gcd(y, x % y);
   }
 }
