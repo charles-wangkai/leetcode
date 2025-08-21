51789a9002ccb34e4ead03a3bca76ba3e949ef77
Kai Wang <charles.wangkai@gmail.com>
2025-08-21 08:38:41 +0800

    Count Submatrices With All Ones

--- count-submatrices-with-all-ones/Solution.java
+++ count-submatrices-with-all-ones/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
   public int numSubmat(int[][] mat) {
-    int row = mat.length;
-    int col = mat[0].length;
+    int m = mat.length;
+    int n = mat[0].length;
 
     int result = 0;
-    for (int beginR = 0; beginR < row; ++beginR) {
-      int[] heights = new int[col];
-      for (int endR = beginR; endR < row; ++endR) {
-        for (int c = 0; c < col; ++c) {
+    for (int beginR = 0; beginR < m; ++beginR) {
+      int[] heights = new int[n];
+      for (int endR = beginR; endR < m; ++endR) {
+        for (int c = 0; c < n; ++c) {
           if (mat[endR][c] == 1) {
             ++heights[c];
           }
         }
 
         int count = 0;
-        for (int c = 0; c <= col; ++c) {
-          if (c != col && heights[c] == endR - beginR + 1) {
+        for (int c = 0; c <= n; ++c) {
+          if (c != n && heights[c] == endR - beginR + 1) {
             ++count;
           } else {
-            result += count + count * (count - 1) / 2;
+            result += count * (count + 1) / 2;
             count = 0;
           }
         }
