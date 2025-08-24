fab5a3f59865b6d7365516a687af8533b5f2d668
Kai Wang <charles.wangkai@gmail.com>
2025-08-24 08:24:57 +0800

    Longest Subarray of 1's After Deleting One Element

--- longest-subarray-of-1s-after-deleting-one-element/Solution.java
+++ longest-subarray-of-1s-after-deleting-one-element/Solution.java
@@ -3,27 +3,14 @@ import java.util.stream.IntStream;
 class Solution {
   public int longestSubarray(int[] nums) {
     int[] leftOneNums = new int[nums.length];
-    int leftOneNum = 0;
     for (int i = 0; i < leftOneNums.length; ++i) {
-      leftOneNums[i] = leftOneNum;
-
-      if (nums[i] == 1) {
-        ++leftOneNum;
-      } else {
-        leftOneNum = 0;
-      }
+      leftOneNums[i] = (i != 0 && nums[i - 1] == 1) ? (leftOneNums[i - 1] + 1) : 0;
     }
 
     int[] rightOneNums = new int[nums.length];
-    int rightOneNum = 0;
     for (int i = rightOneNums.length - 1; i >= 0; --i) {
-      rightOneNums[i] = rightOneNum;
-
-      if (nums[i] == 1) {
-        ++rightOneNum;
-      } else {
-        rightOneNum = 0;
-      }
+      rightOneNums[i] =
+          (i != rightOneNums.length - 1 && nums[i + 1] == 1) ? (rightOneNums[i + 1] + 1) : 0;
     }
 
     return IntStream.range(0, nums.length)
