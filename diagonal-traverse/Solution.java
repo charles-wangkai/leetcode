12cb540c4ff7cb41aa279e052cf89a4bef50730a
Kai Wang <charles.wangkai@gmail.com>
2025-08-25 08:24:01 +0800

    Diagonal Traverse

--- diagonal-traverse/Solution.java
+++ diagonal-traverse/Solution.java
@@ -2,46 +2,36 @@ class Solution {
   static final int[] R_OFFSETS = {-1, 1};
   static final int[] C_OFFSETS = {1, -1};
 
-  public int[] findDiagonalOrder(int[][] matrix) {
-    int row = matrix.length;
-    if (row == 0) {
-      return new int[0];
-    }
-    int col = matrix[0].length;
-    if (col == 0) {
-      return new int[0];
-    }
+  public int[] findDiagonalOrder(int[][] mat) {
+    int m = mat.length;
+    int n = mat[0].length;
 
+    int[] result = new int[m * n];
     int r = 0;
     int c = 0;
     int direction = 0;
-    int[] result = new int[row * col];
     for (int i = 0; i < result.length; ++i) {
-      result[i] = matrix[r][c];
+      result[i] = mat[r][c];
 
       r += R_OFFSETS[direction];
       c += C_OFFSETS[direction];
-      if (c == col) {
+      if (c == n) {
         r += 2;
         --c;
-        direction = swapDirection(direction);
+        direction = 1 - direction;
       } else if (r < 0) {
         r = 0;
-        direction = swapDirection(direction);
-      } else if (r == row) {
+        direction = 1 - direction;
+      } else if (r == m) {
         --r;
         c += 2;
-        direction = swapDirection(direction);
+        direction = 1 - direction;
       } else if (c < 0) {
         c = 0;
-        direction = swapDirection(direction);
+        direction = 1 - direction;
       }
     }
 
     return result;
-  }
-
-  int swapDirection(int direction) {
-    return 1 - direction;
   }
 }
