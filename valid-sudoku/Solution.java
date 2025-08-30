0993516a6027d95c31b8972780ec44213d455e13
Kai Wang <charles.wangkai@gmail.com>
2025-08-30 08:51:12 +0800

    Valid Sudoku

--- valid-sudoku/Solution.java
+++ valid-sudoku/Solution.java
@@ -1,25 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 class Solution {
   public boolean isValidSudoku(char[][] board) {
-    return IntStream.range(0, 9).allMatch(r -> check(board, r, 0, 1, 9))
-        && IntStream.range(0, 9).allMatch(c -> check(board, 0, c, 9, 1))
+    return IntStream.range(0, 9).allMatch(beginR -> check(board, 1, 9, beginR, 0))
+        && IntStream.range(0, 9).allMatch(beginC -> check(board, 9, 1, 0, beginC))
         && IntStream.range(0, 3)
-            .allMatch(i -> IntStream.range(0, 3).allMatch(j -> check(board, i * 3, j * 3, 3, 3)));
+            .allMatch(i -> IntStream.range(0, 3).allMatch(j -> check(board, 3, 3, i * 3, j * 3)));
   }
 
-  boolean check(char[][] board, int r, int c, int row, int col) {
-    boolean[] seen = new boolean[10];
+  boolean check(char[][] board, int row, int col, int beginR, int beginC) {
+    Set<Character> seen = new HashSet<>();
     for (int dr = 0; dr < row; ++dr) {
       for (int dc = 0; dc < col; ++dc) {
-        char cell = board[r + dr][c + dc];
+        char cell = board[beginR + dr][beginC + dc];
         if (cell != '.') {
-          int digit = cell - '0';
-          if (seen[digit]) {
+          if (seen.contains(cell)) {
             return false;
           }
 
-          seen[digit] = true;
+          seen.add(cell);
         }
       }
     }
