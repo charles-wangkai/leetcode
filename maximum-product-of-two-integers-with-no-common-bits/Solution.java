5503fcac5c30fe229e8846c11eb3a6de7b782447
Kai Wang <charles.wangkai@gmail.com>
2025-08-31 23:12:19 +0800

    Maximum Product of Two Integers With No Common Bits

---
+++ maximum-product-of-two-integers-with-no-common-bits/Solution.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/maximum-product-of-two-integers-with-no-common-bits/solutions/7140200/bitmask-subset-dp/
+
+import java.util.Arrays;
+
+class Solution {
+  public long maxProduct(int[] nums) {
+    int bitNum = computeExponent(Arrays.stream(nums).max().getAsInt()) + 1;
+    int[] dp = new int[1 << bitNum];
+    for (int num : nums) {
+      dp[num] = num;
+    }
+
+    for (int b = 0; b < bitNum; ++b) {
+      for (int mask = 0; mask < 1 << bitNum; ++mask) {
+        if (((mask >> b) & 1) == 1) {
+          dp[mask] = Math.max(dp[mask], dp[mask - (1 << b)]);
+        }
+      }
+    }
+
+    return Arrays.stream(nums)
+        .mapToLong(x -> (long) x * dp[((1 << bitNum) - 1) ^ x])
+        .max()
+        .getAsLong();
+  }
+
+  int computeExponent(int x) {
+    return 31 - Integer.numberOfLeadingZeros(x);
+  }
+}
