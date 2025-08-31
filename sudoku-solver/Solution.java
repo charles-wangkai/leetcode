29de5af9fab0394d27d39f35c79807f804a801bd
Kai Wang <charles.wangkai@gmail.com>
2025-08-31 09:33:23 +0800

    Sudoku Solver

--- sudoku-solver/Solution.java
+++ sudoku-solver/Solution.java
@@ -1,69 +1,77 @@
 class Solution {
-  static final int CELL_SIZE = 9;
-  static final int BLOCK_SIZE = 3;
-
   public void solveSudoku(char[][] board) {
-    boolean[][] rows = new boolean[CELL_SIZE][10];
-    boolean[][] cols = new boolean[CELL_SIZE][10];
-    boolean[][][] blocks = new boolean[BLOCK_SIZE][BLOCK_SIZE][10];
-    for (int r = 0; r < CELL_SIZE; ++r) {
-      for (int c = 0; c < CELL_SIZE; ++c) {
+    boolean[][] rowSeen = new boolean[9][10];
+    boolean[][] colSeen = new boolean[9][10];
+    boolean[][][] blockSeen = new boolean[3][3][10];
+    for (int r = 0; r < 9; ++r) {
+      for (int c = 0; c < 9; ++c) {
         if (board[r][c] != '.') {
-          fill(board, rows, cols, blocks, r, c, board[r][c] - '0');
+          fill(board, rowSeen, colSeen, blockSeen, r, c, board[r][c] - '0');
         }
       }
     }
-    search(board, rows, cols, blocks, 0, 0);
+
+    search(board, rowSeen, colSeen, blockSeen, 0, 0);
   }
 
   boolean fill(
       char[][] board,
-      boolean[][] rows,
-      boolean[][] cols,
-      boolean[][][] blocks,
+      boolean[][] rowSeen,
+      boolean[][] colSeen,
+      boolean[][][] blockSeen,
       int r,
       int c,
       int digit) {
-    if (rows[r][digit] || cols[c][digit] || blocks[r / BLOCK_SIZE][c / BLOCK_SIZE][digit]) {
+    if (rowSeen[r][digit] || colSeen[c][digit] || blockSeen[r / 3][c / 3][digit]) {
       return false;
     }
 
     board[r][c] = (char) ('0' + digit);
-    rows[r][digit] = true;
-    cols[c][digit] = true;
-    blocks[r / BLOCK_SIZE][c / BLOCK_SIZE][digit] = true;
+    rowSeen[r][digit] = true;
+    colSeen[c][digit] = true;
+    blockSeen[r / 3][c / 3][digit] = true;
 
     return true;
   }
 
   void unfill(
-      char[][] board, boolean[][] rows, boolean[][] cols, boolean[][][] blocks, int r, int c) {
+      char[][] board,
+      boolean[][] rowSeen,
+      boolean[][] colSeen,
+      boolean[][][] blockSeen,
+      int r,
+      int c) {
     int digit = board[r][c] - '0';
     board[r][c] = '.';
-    rows[r][digit] = false;
-    cols[c][digit] = false;
-    blocks[r / BLOCK_SIZE][c / BLOCK_SIZE][digit] = false;
+    rowSeen[r][digit] = false;
+    colSeen[c][digit] = false;
+    blockSeen[r / 3][c / 3][digit] = false;
   }
 
   boolean search(
-      char[][] board, boolean[][] rows, boolean[][] cols, boolean[][][] blocks, int r, int c) {
-    if (r == CELL_SIZE) {
+      char[][] board,
+      boolean[][] rowSeen,
+      boolean[][] colSeen,
+      boolean[][][] blockSeen,
+      int r,
+      int c) {
+    if (r == 9) {
       return true;
     }
-    if (c == CELL_SIZE) {
-      return search(board, rows, cols, blocks, r + 1, 0);
+    if (c == 9) {
+      return search(board, rowSeen, colSeen, blockSeen, r + 1, 0);
     }
     if (board[r][c] != '.') {
-      return search(board, rows, cols, blocks, r, c + 1);
+      return search(board, rowSeen, colSeen, blockSeen, r, c + 1);
     }
 
     for (int digit = 1; digit <= 9; ++digit) {
-      if (fill(board, rows, cols, blocks, r, c, digit)) {
-        if (search(board, rows, cols, blocks, r, c + 1)) {
+      if (fill(board, rowSeen, colSeen, blockSeen, r, c, digit)) {
+        if (search(board, rowSeen, colSeen, blockSeen, r, c + 1)) {
           return true;
         }
 
-        unfill(board, rows, cols, blocks, r, c);
+        unfill(board, rowSeen, colSeen, blockSeen, r, c);
       }
     }
 
