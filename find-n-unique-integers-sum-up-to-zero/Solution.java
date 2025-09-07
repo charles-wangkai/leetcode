fdabde14e556b0ea389d3ba5e7982a37d45be73c
Kai Wang <charles.wangkai@gmail.com>
2025-09-07 11:31:30 +0800

    Find N Unique Integers Sum up to Zero

--- find-n-unique-integers-sum-up-to-zero/Solution.java
+++ find-n-unique-integers-sum-up-to-zero/Solution.java
@@ -1,13 +1,13 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int[] sumZero(int n) {
-		int[] result = new int[n];
-		for (int i = 0; i < result.length - 1; ++i) {
-			result[i] = i + 1;
-		}
-		result[result.length - 1] = -IntStream.range(0, result.length - 1).map(i -> result[i]).sum();
+class Solution {
+  public int[] sumZero(int n) {
+    int[] result = new int[n];
+    for (int i = 0; i < result.length - 1; ++i) {
+      result[i] = i + 1;
+    }
+    result[result.length - 1] = -IntStream.range(0, result.length - 1).map(i -> result[i]).sum();
 
-		return result;
-	}
+    return result;
+  }
 }
