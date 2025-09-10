a97f67ef41c79510ef28b8b7cf34baa1d57ef16c
Kai Wang <charles.wangkai@gmail.com>
2025-09-10 08:46:15 +0800

    Minimum Number of People to Teach

--- minimum-number-of-people-to-teach/Solution.java
+++ minimum-number-of-people-to-teach/Solution.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
   public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
@@ -11,30 +11,26 @@ class Solution {
         Arrays.stream(languages)
             .map(l -> Arrays.stream(l).boxed().collect(Collectors.toSet()))
             .toArray(Set[]::new);
-    Boolean[] communicated =
-        Arrays.stream(friendships)
-            .map(
-                friendship ->
-                    !Collections.disjoint(
-                        languageSets[friendship[0] - 1], languageSets[friendship[1] - 1]))
-            .toArray(Boolean[]::new);
 
-    int result = Integer.MAX_VALUE;
-    for (int i = 1; i <= n; ++i) {
-      Set<Integer> missingUsers = new HashSet<>();
-      for (int j = 0; j < friendships.length; ++j) {
-        if (!communicated[j]) {
-          for (int u : friendships[j]) {
-            if (!languageSets[u - 1].contains(i)) {
-              missingUsers.add(u);
-            }
-          }
-        }
-      }
+    int[] neededIndices =
+        IntStream.range(0, friendships.length)
+            .filter(
+                i ->
+                    Collections.disjoint(
+                        languageSets[friendships[i][0] - 1], languageSets[friendships[i][1] - 1]))
+            .toArray();
 
-      result = Math.min(result, missingUsers.size());
-    }
-
-    return result;
+    return IntStream.rangeClosed(1, n)
+        .map(
+            language ->
+                (int)
+                    Arrays.stream(neededIndices)
+                        .flatMap(
+                            index -> IntStream.of(friendships[index][0], friendships[index][1]))
+                        .filter(u -> !languageSets[u - 1].contains(language))
+                        .distinct()
+                        .count())
+        .min()
+        .getAsInt();
   }
 }
