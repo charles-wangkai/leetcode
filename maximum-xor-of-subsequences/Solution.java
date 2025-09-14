6c96204490a2878d70548b7708146def70b02aba
Kai Wang <charles.wangkai@gmail.com>
2025-09-14 17:38:35 +0800

    Maximum XOR of Subsequences

--- maximum-xor-of-subsequences/Solution.java
+++ maximum-xor-of-subsequences/Solution.java
@@ -1,25 +1,25 @@
 // https://github.com/charles-wangkai/codeforces/blob/master/acmsguru/275/Main.java
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 class Solution {
   static final int BIT_NUM = 30;
 
   public int maxXorSubsequences(int[] nums) {
     int result = 0;
     for (int b = BIT_NUM - 1; b >= 0; --b) {
-      for (int i = 0; i < nums.length; ++i) {
-        if (((nums[i] >> b) & 1) == 1) {
-          if (((result >> b) & 1) == 0) {
-            result ^= nums[i];
-          }
+      int b_ = b;
+      OptionalInt chosen = Arrays.stream(nums).filter(x -> ((x >> b_) & 1) == 1).findAny();
+      if (chosen.isPresent()) {
+        if (((result >> b) & 1) == 0) {
+          result ^= chosen.getAsInt();
+        }
 
-          int temp = nums[i];
-          for (int j = 0; j < nums.length; ++j) {
-            if (((nums[j] >> b) & 1) == 1) {
-              nums[j] ^= temp;
-            }
+        for (int i = 0; i < nums.length; ++i) {
+          if (((nums[i] >> b) & 1) == 1) {
+            nums[i] ^= chosen.getAsInt();
           }
-
-          break;
         }
       }
     }
