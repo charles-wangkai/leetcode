021341cb0d9cb3c108efb0b2d55a967778eb4324
Kai Wang <charles.wangkai@gmail.com>
2025-09-15 12:09:16 +0800

    Maximum Number of Words You Can Type

--- maximum-number-of-words-you-can-type/Solution.java
+++ maximum-number-of-words-you-can-type/Solution.java
@@ -1,15 +1,12 @@
 import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 class Solution {
   public int canBeTypedWords(String text, String brokenLetters) {
-    Set<Character> brokens =
-        brokenLetters.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet());
-
     return (int)
         Arrays.stream(text.split(" "))
-            .filter(word -> word.chars().allMatch(ch -> !brokens.contains((char) ch)))
+            .filter(
+                word ->
+                    word.chars().allMatch(c -> !brokenLetters.contains(String.valueOf((char) c))))
             .count();
   }
 }
