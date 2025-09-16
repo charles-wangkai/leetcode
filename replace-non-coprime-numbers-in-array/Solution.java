a612d676940cc658f480269a0fd7ece7d3215bfc
Kai Wang <charles.wangkai@gmail.com>
2025-09-16 08:23:25 +0800

    Replace Non-Coprime Numbers in Array

--- replace-non-coprime-numbers-in-array/Solution.java
+++ replace-non-coprime-numbers-in-array/Solution.java
@@ -6,10 +6,11 @@ class Solution {
     List<Integer> result = new ArrayList<>();
     for (int num : nums) {
       result.add(num);
+
       while (result.size() >= 2
           && gcd(result.get(result.size() - 1), result.get(result.size() - 2)) != 1) {
-        int x = result.remove(result.size() - 1);
-        int y = result.remove(result.size() - 1);
+        int x = result.removeLast();
+        int y = result.removeLast();
         result.add(lcm(x, y));
       }
     }
@@ -22,6 +23,6 @@ class Solution {
   }
 
   int lcm(int x, int y) {
-    return (int) ((long) x * y / gcd(x, y));
+    return x / gcd(x, y) * y;
   }
 }
