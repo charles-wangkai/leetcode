429c585e452e7c9c84c474012167e50592406cf9
Kai Wang <charles.wangkai@gmail.com>
2025-09-24 08:40:24 +0800

    Fraction to Recurring Decimal

--- fraction-to-recurring-decimal/Solution.java
+++ fraction-to-recurring-decimal/Solution.java
@@ -1,37 +1,43 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
+class Solution {
   public String fractionToDecimal(int numerator, int denominator) {
     StringBuilder result = new StringBuilder();
-    if ((long) numerator * denominator < 0) {
+
+    if (Integer.signum(numerator) * Integer.signum(denominator) == -1) {
       result.append("-");
     }
-    long numeratorLong = Math.abs((long) numerator);
-    long denominatorLong = Math.abs((long) denominator);
-    result.append(numeratorLong / denominatorLong);
-    numeratorLong %= denominatorLong;
-    if (numeratorLong != 0) {
-      Map<Long, Integer> remainder2index = new HashMap<>();
-      StringBuilder fraction = new StringBuilder();
-      for (int i = 1; ; i++) {
-        if (numeratorLong == 0) {
-          break;
-        }
-        if (remainder2index.containsKey(numeratorLong)) {
-          fraction.insert(remainder2index.get(numeratorLong) - 1, "(");
-          fraction.append(")");
-          break;
-        }
-        remainder2index.put(numeratorLong, i);
 
-        numeratorLong *= 10;
-        fraction.append(numeratorLong / denominatorLong);
-        numeratorLong %= denominatorLong;
+    long numer = Math.abs((long) numerator);
+    long denom = Math.abs((long) denominator);
+    result.append(numer / denom);
+    numer %= denom;
+
+    if (numer != 0) {
+      Map<Long, Integer> remainderToIndex = new HashMap<>();
+      StringBuilder fraction = new StringBuilder();
+      for (int i = 0; ; ++i) {
+        if (numer == 0) {
+          break;
+        }
+
+        if (remainderToIndex.containsKey(numer)) {
+          fraction.insert(remainderToIndex.get(numer), "(").append(")");
+
+          break;
+        }
+
+        remainderToIndex.put(numer, i);
+
+        numer *= 10;
+        fraction.append(numer / denom);
+        numer %= denom;
       }
-      result.append(".");
-      result.append(fraction);
+
+      result.append(".").append(fraction);
     }
+
     return result.toString();
   }
 }
