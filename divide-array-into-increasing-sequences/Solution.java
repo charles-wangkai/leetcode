1116884035f8215d461295c18caf6c6e47928db6
Kai Wang <charles.wangkai@gmail.com>
2025-09-29 09:22:39 +0800

    Divide Array Into Increasing Sequences

--- divide-array-into-increasing-sequences/Solution.java
+++ divide-array-into-increasing-sequences/Solution.java
@@ -1,13 +1,14 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public boolean canDivideIntoSubsequences(int[] nums, int K) {
-		Map<Integer, Integer> valueToCount = new HashMap<>();
-		for (int num : nums) {
-			valueToCount.put(num, valueToCount.getOrDefault(num, 0) + 1);
-		}
+class Solution {
+  public boolean canDivideIntoSubsequences(int[] nums, int k) {
+    Map<Integer, Integer> valueToCount = new HashMap<>();
+    for (int num : nums) {
+      valueToCount.put(num, valueToCount.getOrDefault(num, 0) + 1);
+    }
 
-		return valueToCount.values().stream().mapToInt(x -> x).max().getAsInt() * K <= nums.length;
-	}
+    return (long) valueToCount.values().stream().mapToInt(Integer::intValue).max().getAsInt() * k
+        <= nums.length;
+  }
 }
