266e6b77329b5340e8f26dee86f29948beac05d6
Kai Wang <charles.wangkai@gmail.com>
2025-09-30 08:30:11 +0800

    Find Triangular Sum of an Array

--- find-triangular-sum-of-an-array/Solution.java
+++ find-triangular-sum-of-an-array/Solution.java
@@ -2,11 +2,9 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int triangularSum(int[] nums) {
-    while (nums.length != 1) {
-      int[] nums_ = nums;
-      nums = IntStream.range(0, nums.length - 1).map(i -> (nums_[i] + nums_[i + 1]) % 10).toArray();
-    }
-
-    return nums[0];
+    return (nums.length == 1)
+        ? nums[0]
+        : triangularSum(
+            IntStream.range(0, nums.length - 1).map(i -> (nums[i] + nums[i + 1]) % 10).toArray());
   }
 }
