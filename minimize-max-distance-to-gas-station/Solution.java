8bc466b4a1e7174a6f57837e49479b9805fae08f
Kai Wang <charles.wangkai@gmail.com>
2025-10-08 08:54:10 +0800

    Minimize Max Distance to Gas Station

--- minimize-max-distance-to-gas-station/Solution.java
+++ minimize-max-distance-to-gas-station/Solution.java
@@ -1,21 +1,31 @@
-public class Solution {
-	public double minmaxGasDist(int[] stations, int K) {
-		double lower = 0;
-		double upper = 100000000;
-		while (upper - lower > 0.000001) {
-			double middle = (lower + upper) / 2;
+import java.util.stream.IntStream;
 
-			int count = 0;
-			for (int i = 0; i < stations.length - 1; i++) {
-				count += Math.max(0, Math.ceil((stations[i + 1] - stations[i]) / middle) - 1);
-			}
+class Solution {
+  static final int ITERATION_NUM = 100;
 
-			if (count <= K) {
-				upper = middle;
-			} else {
-				lower = middle;
-			}
-		}
-		return (lower + upper) / 2;
-	}
+  public double minmaxGasDist(int[] stations, int k) {
+    double lower = 0;
+    double upper =
+        IntStream.range(0, stations.length - 1)
+            .map(i -> stations[i + 1] - stations[i])
+            .max()
+            .getAsInt();
+    for (int i = 0; i < ITERATION_NUM; ++i) {
+      double middle = (lower + upper) / 2;
+      if (check(stations, k, middle)) {
+        upper = middle;
+      } else {
+        lower = middle;
+      }
+    }
+
+    return (lower + upper) / 2;
+  }
+
+  boolean check(int[] stations, int k, double distance) {
+    return IntStream.range(0, stations.length - 1)
+            .map(i -> Math.max(0, (int) Math.ceil((stations[i + 1] - stations[i]) / distance) - 1))
+            .sum()
+        <= k;
+  }
 }
