e78bfd01be16c391ec47d7e947d7c24face85cac
Kai Wang <charles.wangkai@gmail.com>
2025-10-09 08:56:35 +0800

    Find the Minimum Amount of Time to Brew Potions

--- find-the-minimum-amount-of-time-to-brew-potions/Solution.java
+++ find-the-minimum-amount-of-time-to-brew-potions/Solution.java
@@ -3,10 +3,10 @@ class Solution {
     long[] endTimes = new long[skill.length];
     for (int m : mana) {
       for (int i = 0; i < endTimes.length; ++i) {
-        endTimes[i] = Math.max((i == 0) ? 0 : endTimes[i - 1], endTimes[i]) + m * skill[i];
+        endTimes[i] = Math.max((i == 0) ? 0 : endTimes[i - 1], endTimes[i]) + skill[i] * m;
       }
       for (int i = endTimes.length - 2; i >= 0; --i) {
-        endTimes[i] = endTimes[i + 1] - m * skill[i + 1];
+        endTimes[i] = endTimes[i + 1] - skill[i + 1] * m;
       }
     }
 
