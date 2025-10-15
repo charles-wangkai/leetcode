6ea012f14ea26996a79c654120ab8120b39b6b1d
Kai Wang <charles.wangkai@gmail.com>
2025-10-15 15:00:31 +0800

    Minimum Cost Path with Alternating Directions I

---
+++ minimum-cost-path-with-alternating-directions-i/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int minCost(int m, int n) {
+    if (m == 1 && n == 1) {
+      return 1;
+    }
+    if ((m == 1 && n == 2) || (m == 2 && n == 1)) {
+      return 3;
+    }
+
+    return -1;
+  }
+}
