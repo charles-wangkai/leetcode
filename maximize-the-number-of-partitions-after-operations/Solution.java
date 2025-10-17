a08af8cec57c32f105ee5c3feaca8578c80066ac
Kai Wang <charles.wangkai@gmail.com>
2025-10-17 08:49:39 +0800

    Maximize the Number of Partitions After Operations

--- maximize-the-number-of-partitions-after-operations/Solution.java
+++ maximize-the-number-of-partitions-after-operations/Solution.java
@@ -4,42 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-  static final int ALPHABET_SIZE = 26;
-
   Map<State, Integer> cache;
 
   public int maxPartitionsAfterOperations(String s, int k) {
-    cache = new HashMap<>();
-
-    return search(s, k, 0, 0, false);
+    return search(new HashMap<>(), s, k, 0, 0, false);
   }
 
-  int search(String s, int k, int index, int mask, boolean changed) {
+  int search(Map<State, Integer> cache, String s, int k, int index, int mask, boolean changed) {
     if (index == s.length()) {
-      return 1;
+      return (mask == 0) ? 0 : 1;
     }
 
     State state = new State(index, mask, changed);
     if (!cache.containsKey(state)) {
-      int maxPartitionNum;
-      if (Integer.bitCount(mask) != k || ((mask >> (s.charAt(index) - 'a')) & 1) == 1) {
-        maxPartitionNum = search(s, k, index + 1, mask | (1 << (s.charAt(index) - 'a')), changed);
-      } else {
-        maxPartitionNum = 1 + search(s, k, index + 1, 1 << (s.charAt(index) - 'a'), changed);
-      }
+      int result =
+          (Integer.bitCount(mask) != k || ((mask >> (s.charAt(index) - 'a')) & 1) == 1)
+              ? search(cache, s, k, index + 1, mask | (1 << (s.charAt(index) - 'a')), changed)
+              : (1 + search(cache, s, k, index + 1, 1 << (s.charAt(index) - 'a'), changed));
 
       if (!changed) {
-        for (int i = 0; i < ALPHABET_SIZE; ++i) {
-          if (Integer.bitCount(mask) != k || ((mask >> i) & 1) == 1) {
-            maxPartitionNum =
-                Math.max(maxPartitionNum, search(s, k, index + 1, mask | (1 << i), true));
-          } else {
-            maxPartitionNum = Math.max(maxPartitionNum, 1 + search(s, k, index + 1, 1 << i, true));
-          }
+        for (int b = 0; b < 26; ++b) {
+          result =
+              Math.max(
+                  result,
+                  (Integer.bitCount(mask) != k || ((mask >> b) & 1) == 1)
+                      ? search(cache, s, k, index + 1, mask | (1 << b), true)
+                      : (1 + search(cache, s, k, index + 1, 1 << b, true)));
         }
       }
 
-      cache.put(state, maxPartitionNum);
+      cache.put(state, result);
     }
 
     return cache.get(state);
