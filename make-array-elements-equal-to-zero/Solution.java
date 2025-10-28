8c263266e43d47fe60d895e03cbec0782bbf47da
Kai Wang <charles.wangkai@gmail.com>
2025-10-28 08:32:30 +0800

    Make Array Elements Equal to Zero

--- make-array-elements-equal-to-zero/Solution.java
+++ make-array-elements-equal-to-zero/Solution.java
@@ -2,22 +2,20 @@ import java.util.Arrays;
 
 class Solution {
   public int countValidSelections(int[] nums) {
-    int total = Arrays.stream(nums).sum();
-
     int result = 0;
     int leftSum = 0;
+    int rightSum = Arrays.stream(nums).sum();
     for (int num : nums) {
       if (num == 0) {
-        int rightSum = total - leftSum;
         if (leftSum == rightSum) {
           result += 2;
-        }
-        if (Math.abs(leftSum - rightSum) == 1) {
+        } else if (Math.abs(leftSum - rightSum) == 1) {
           ++result;
         }
       }
 
       leftSum += num;
+      rightSum -= num;
     }
 
     return result;
