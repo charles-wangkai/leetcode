75b1cb6d5b9f4c9d6db5ff84af5ef1cf76833ba8
Kai Wang <charles.wangkai@gmail.com>
2025-10-30 08:34:01 +0800

    Minimum Number of Increments on Subarrays to Form a Target Array

--- minimum-number-of-increments-on-subarrays-to-form-a-target-array/Solution.java
+++ minimum-number-of-increments-on-subarrays-to-form-a-target-array/Solution.java
@@ -1,8 +1,9 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int minNumberOperations(int[] target) {
-        return IntStream.range(0, target.length)
-                .map(i -> Math.max(0, target[i] - ((i == target.length - 1) ? 0 : target[i + 1]))).sum();
-    }
+  public int minNumberOperations(int[] target) {
+    return IntStream.range(0, target.length)
+        .map(i -> Math.max(0, target[i] - ((i == 0) ? 0 : target[i - 1])))
+        .sum();
+  }
 }
