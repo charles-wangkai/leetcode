d8a3230abf0259eda774d2b7af785e36666dc10a
Kai Wang <charles.wangkai@gmail.com>
2025-11-11 10:35:47 +0800

    Ones and Zeroes

--- ones-and-zeroes/Solution.java
+++ ones-and-zeroes/Solution.java
@@ -5,22 +5,19 @@ class Solution {
       int num0 = (int) str.chars().filter(x -> x == '0').count();
       int num1 = str.length() - num0;
 
-      int[][] nextDp = new int[m + 1][n + 1];
-      for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= n; ++j) {
+      for (int i = m; i >= 0; --i) {
+        for (int j = n; j >= 0; --j) {
           if (i - 1 >= 0) {
-            nextDp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+            dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
           }
           if (j - 1 >= 0) {
-            nextDp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+            dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
           }
           if (i >= num0 && j >= num1) {
-            nextDp[i][j] = Math.max(dp[i][j], dp[i - num0][j - num1] + 1);
+            dp[i][j] = Math.max(dp[i][j], dp[i - num0][j - num1] + 1);
           }
         }
       }
-
-      dp = nextDp;
     }
 
     return dp[m][n];
