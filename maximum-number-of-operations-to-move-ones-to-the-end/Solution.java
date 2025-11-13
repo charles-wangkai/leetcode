22dce69432ce2f916fae85ada78785f7761d6766
Kai Wang <charles.wangkai@gmail.com>
2025-11-13 08:40:03 +0800

    Maximum Number of Operations to Move Ones to the End

--- maximum-number-of-operations-to-move-ones-to-the-end/Solution.java
+++ maximum-number-of-operations-to-move-ones-to-the-end/Solution.java
@@ -2,12 +2,15 @@ class Solution {
   public int maxOperations(String s) {
     int result = 0;
     int oneCount = 0;
-    for (int i = 0; i < s.length(); ++i) {
-      if (s.charAt(i) == '1') {
+    char prev = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '1') {
         ++oneCount;
-      } else if (i == s.length() - 1 || s.charAt(i + 1) == '1') {
+      } else if (prev == '1') {
         result += oneCount;
       }
+
+      prev = c;
     }
 
     return result;
