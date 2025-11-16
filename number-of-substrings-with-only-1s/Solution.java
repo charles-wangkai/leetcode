0a78c3112bd0e44a1ed3b100f00fbfa1412ba91b
Kai Wang <charles.wangkai@gmail.com>
2025-11-16 09:12:47 +0800

    Number of Substrings With Only 1s

--- number-of-substrings-with-only-1s/Solution.java
+++ number-of-substrings-with-only-1s/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int numSub(String s) {
-        long result = 0;
-        int count1 = 0;
-        for (int i = 0; i <= s.length(); ++i) {
-            if (i != s.length() && s.charAt(i) == '1') {
-                ++count1;
-            } else {
-                result += count1 + count1 * (count1 - 1L) / 2;
+  public int numSub(String s) {
+    long result = 0;
+    int count = 0;
+    for (int i = 0; i <= s.length(); ++i) {
+      if (i != s.length() && s.charAt(i) == '1') {
+        ++count;
+      } else {
+        result += count * (count + 1L) / 2;
 
-                count1 = 0;
-            }
-        }
-
-        return (int) (result % 1_000_000_007);
+        count = 0;
+      }
     }
+
+    return (int) (result % 1_000_000_007);
+  }
 }
