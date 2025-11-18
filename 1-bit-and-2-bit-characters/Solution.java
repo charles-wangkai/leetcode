989b6a4d247367f39b07c2250b166a286a77ac7c
Kai Wang <charles.wangkai@gmail.com>
2025-11-18 08:53:44 +0800

    1-bit and 2-bit Characters

--- 1-bit-and-2-bit-characters/Solution.java
+++ 1-bit-and-2-bit-characters/Solution.java
@@ -1,19 +1,15 @@
-public class Solution {
-	public boolean isOneBitCharacter(int[] bits) {
-		int index = 0;
-		while (true) {
-			if (index == bits.length - 1) {
-				return true;
-			}
-			if (index == bits.length) {
-				return false;
-			}
+class Solution {
+  public boolean isOneBitCharacter(int[] bits) {
+    int index = 0;
+    while (true) {
+      if (index == bits.length - 1) {
+        return true;
+      }
+      if (index == bits.length) {
+        return false;
+      }
 
-			if (bits[index] == 0) {
-				index++;
-			} else {
-				index += 2;
-			}
-		}
-	}
+      index += (bits[index] == 0) ? 1 : 2;
+    }
+  }
 }
