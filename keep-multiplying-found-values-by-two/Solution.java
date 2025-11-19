82e92e45ba7b397555384ca9734c1d11e0bede15
Kai Wang <charles.wangkai@gmail.com>
2025-11-19 08:35:44 +0800

    Keep Multiplying Found Values by Two

--- keep-multiplying-found-values-by-two/Solution.java
+++ keep-multiplying-found-values-by-two/Solution.java
@@ -1,16 +1,15 @@
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
   public int findFinalValue(int[] nums, int original) {
     Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
 
-    int result = original;
-    while (set.contains(result)) {
-      result *= 2;
-    }
-
-    return result;
+    return IntStream.iterate(original, x -> x * 2)
+        .filter(x -> !set.contains(x))
+        .findFirst()
+        .getAsInt();
   }
 }
