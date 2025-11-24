2a2c72cc4ba5b9a56d26aa4925937ac075bbd6f0
Kai Wang <charles.wangkai@gmail.com>
2025-11-24 08:38:17 +0800

    Binary Prefix Divisible By 5

--- binary-prefix-divisible-by-5/Solution.java
+++ binary-prefix-divisible-by-5/Solution.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-	public List<Boolean> prefixesDivBy5(int[] A) {
-		List<Boolean> result = new ArrayList<>();
-		int remainder = 0;
-		for (int digit : A) {
-			remainder = (remainder * 2 + digit) % 5;
+class Solution {
+  public List<Boolean> prefixesDivBy5(int[] nums) {
+    List<Boolean> result = new ArrayList<>();
+    int remainder = 0;
+    for (int digit : nums) {
+      remainder = (remainder * 2 + digit) % 5;
 
-			result.add(remainder == 0);
-		}
-		return result;
-	}
+      result.add(remainder == 0);
+    }
+
+    return result;
+  }
 }
