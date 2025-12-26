e88831e8f3f89dd6c1b9238bdf6e50c04bfe23a6
Kai Wang <charles.wangkai@gmail.com>
2025-12-26 08:24:58 +0800

    Minimum Penalty for a Shop

--- minimum-penalty-for-a-shop/Solution.java
+++ minimum-penalty-for-a-shop/Solution.java
@@ -1,17 +1,10 @@
 class Solution {
   public int bestClosingTime(String customers) {
-    int yCount = (int) customers.chars().filter(c -> c == 'Y').count();
-    int nCount = 0;
-    int minPenalty = yCount;
+    int penalty = (int) customers.chars().filter(c -> c == 'Y').count();
+    int minPenalty = penalty;
     int result = 0;
     for (int i = 0; i < customers.length(); ++i) {
-      if (customers.charAt(i) == 'Y') {
-        --yCount;
-      } else {
-        ++nCount;
-      }
-
-      int penalty = yCount + nCount;
+      penalty += (customers.charAt(i) == 'Y') ? -1 : 1;
       if (penalty < minPenalty) {
         minPenalty = penalty;
         result = i + 1;
