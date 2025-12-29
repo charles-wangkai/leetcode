02fe770a077c2ac3757301e20c31c5ecd7f9705e
Kai Wang <charles.wangkai@gmail.com>
2025-12-29 20:03:18 +0800

    Pyramid Transition Matrix

--- pyramid-transition-matrix/Solution.java
+++ pyramid-transition-matrix/Solution.java
@@ -5,24 +5,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class Solution {
+class Solution {
   public boolean pyramidTransition(String bottom, List<String> allowed) {
-    Map<String, List<Character>> base2tops = new HashMap<>();
-    for (String triple : allowed) {
-      String base = triple.substring(0, 2);
-      char top = triple.charAt(2);
+    Map<String, List<Character>> baseToTops = new HashMap<>();
+    for (String a : allowed) {
+      String base = a.substring(0, 2);
+      char top = a.charAt(2);
 
-      if (!base2tops.containsKey(base)) {
-        base2tops.put(base, new ArrayList<>());
-      }
-      base2tops.get(base).add(top);
+      baseToTops.putIfAbsent(base, new ArrayList<>());
+      baseToTops.get(base).add(top);
     }
 
-    return search(base2tops, new HashSet<>(), bottom, new StringBuilder());
+    return search(baseToTops, new HashSet<>(), bottom, new StringBuilder());
   }
 
   boolean search(
-      Map<String, List<Character>> base2tops,
+      Map<String, List<Character>> baseToTops,
       Set<String> visitedRows,
       String row,
       StringBuilder nextRow) {
@@ -31,25 +29,27 @@ public class Solution {
     }
 
     if (nextRow.length() == row.length() - 1) {
-      return search(base2tops, visitedRows, nextRow.toString(), new StringBuilder());
+      return search(baseToTops, visitedRows, nextRow.toString(), new StringBuilder());
     }
 
-    if (nextRow.length() == 0 && visitedRows.contains(row)) {
+    if (nextRow.isEmpty() && visitedRows.contains(row)) {
       return false;
     }
 
     String base = row.substring(nextRow.length(), nextRow.length() + 2);
-    if (base2tops.containsKey(base)) {
-      for (char top : base2tops.get(base)) {
+    if (baseToTops.containsKey(base)) {
+      for (char top : baseToTops.get(base)) {
         nextRow.append(top);
-        if (search(base2tops, visitedRows, row, nextRow)) {
+
+        if (search(baseToTops, visitedRows, row, nextRow)) {
           return true;
         }
+
         nextRow.deleteCharAt(nextRow.length() - 1);
       }
     }
 
-    if (nextRow.length() == 0) {
+    if (nextRow.isEmpty()) {
       visitedRows.add(row);
     }
 
