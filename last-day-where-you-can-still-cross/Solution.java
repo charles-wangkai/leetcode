5f04b70ff3b4ba77d3484f8fd43f3b4cc1fcd3f4
Kai Wang <charles.wangkai@gmail.com>
2025-12-31 08:43:33 +0800

    Last Day Where You Can Still Cross

--- last-day-where-you-can-still-cross/Solution.java
+++ last-day-where-you-can-still-cross/Solution.java
@@ -5,11 +5,22 @@ class Solution {
   static final int[] C_OFFSETS = {0, 1, 0, -1};
 
   public int latestDayToCross(int row, int col, int[][] cells) {
-    int[] minParents = new int[row * col];
-    Arrays.fill(minParents, -1);
-
-    int[] maxParents = new int[row * col];
-    Arrays.fill(maxParents, -1);
+    int[] parents = new int[row * col];
+    Arrays.fill(parents, -1);
+    for (int c = 0; c < col - 1; ++c) {
+      int root1 = findRoot(parents, c);
+      int root2 = findRoot(parents, c + 1);
+      if (root1 != root2) {
+        parents[root2] = root1;
+      }
+    }
+    for (int c = 0; c < col - 1; ++c) {
+      int root1 = findRoot(parents, (row - 1) * col + c);
+      int root2 = findRoot(parents, (row - 1) * col + c + 1);
+      if (root1 != root2) {
+        parents[root2] = root1;
+      }
+    }
 
     int[][] matrix = new int[row][col];
     for (int r = 0; r < matrix.length; ++r) {
@@ -21,52 +32,32 @@ class Solution {
       int c = cells[i][1] - 1;
 
       matrix[r][c] = 0;
+
       for (int j = 0; j < R_OFFSETS.length; ++j) {
         int adjR = r + R_OFFSETS[j];
         int adjC = c + C_OFFSETS[j];
         if (adjR >= 0 && adjR < row && adjC >= 0 && adjC < col && matrix[adjR][adjC] == 0) {
-          int minRoot1 = findRoot(minParents, r * col + c);
-          int minRoot2 = findRoot(minParents, adjR * col + adjC);
-          int maxRoot1 = findRoot(maxParents, r * col + c);
-          int maxRoot2 = findRoot(maxParents, adjR * col + adjC);
-          if (minRoot1 != minRoot2) {
-            if (minRoot1 > minRoot2) {
-              int temp = minRoot1;
-              minRoot1 = minRoot2;
-              minRoot2 = temp;
-            }
-            if (maxRoot1 > maxRoot2) {
-              int temp = maxRoot1;
-              maxRoot1 = maxRoot2;
-              maxRoot2 = temp;
-            }
-
-            if (minRoot1 / col == 0 && maxRoot2 / col == row - 1) {
-              return i;
-            }
-
-            minParents[minRoot2] = minRoot1;
-            maxParents[maxRoot1] = maxRoot2;
+          int root1 = findRoot(parents, r * col + c);
+          int root2 = findRoot(parents, adjR * col + adjC);
+          if (root1 != root2) {
+            parents[root2] = root1;
           }
         }
+      }
+
+      if (findRoot(parents, 0) == findRoot(parents, row * col - 1)) {
+        return i;
       }
     }
   }
 
-  static int findRoot(int[] parents, int node) {
-    int root = node;
-    while (parents[root] != -1) {
-      root = parents[root];
+  int findRoot(int[] parents, int node) {
+    if (parents[node] == -1) {
+      return node;
     }
 
-    int p = node;
-    while (p != root) {
-      int next = parents[p];
-      parents[p] = root;
+    parents[node] = findRoot(parents, parents[node]);
 
-      p = next;
-    }
-
-    return root;
+    return parents[node];
   }
 }
