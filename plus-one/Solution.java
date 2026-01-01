ac5e794c3518aeb5b2067af869ec0a65c1c67b5a
Kai Wang <charles.wangkai@gmail.com>
2026-01-01 09:44:18 +0800

    Plus One

--- plus-one/Solution.java
+++ plus-one/Solution.java
@@ -1,20 +1,15 @@
+import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
-	public int[] plusOne(int[] digits) {
-		if (Arrays.stream(digits).allMatch(digit -> digit == 9)) {
-			return IntStream.range(0, digits.length + 1).map(i -> (i == 0) ? 1 : 0).toArray();
-		}
-
-		int[] result = new int[digits.length];
-		int carry = 1;
-		for (int i = result.length - 1; i >= 0; --i) {
-			int sum = digits[i] + carry;
-			result[i] = sum % 10;
-			carry = sum / 10;
-		}
-
-		return result;
-	}
+  public int[] plusOne(int[] digits) {
+    return new BigInteger(
+            Arrays.stream(digits).mapToObj(String::valueOf).collect(Collectors.joining()))
+        .add(BigInteger.ONE)
+        .toString()
+        .chars()
+        .map(c -> c - '0')
+        .toArray();
+  }
 }
