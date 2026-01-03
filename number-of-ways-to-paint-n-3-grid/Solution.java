7eb6fadab5f8987af2e0440e80f07abd33109de3
Kai Wang <charles.wangkai@gmail.com>
2026-01-03 09:14:44 +0800

    Number of Ways to Paint N × 3 Grid

--- number-of-ways-to-paint-n-3-grid/Solution.java
+++ number-of-ways-to-paint-n-3-grid/Solution.java
@@ -7,48 +7,49 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
-    static final char[] COLORS = { 'R', 'Y', 'G' };
-    static final int MODULUS = 1_000_000_007;
+  static final char[] COLORS = {'R', 'Y', 'G'};
+  static final int MODULUS = 1_000_000_007;
 
-    public int numOfWays(int n) {
-        List<String> rows = new ArrayList<>();
-        for (char c1 : COLORS) {
-            for (char c2 : COLORS) {
-                if (c2 != c1) {
-                    for (char c3 : COLORS) {
-                        if (c3 != c2) {
-                            rows.add(String.format("%c%c%c", c1, c2, c3));
-                        }
-                    }
-                }
+  public int numOfWays(int n) {
+    List<String> rows = new ArrayList<>();
+    for (char c1 : COLORS) {
+      for (char c2 : COLORS) {
+        if (c2 != c1) {
+          for (char c3 : COLORS) {
+            if (c3 != c2) {
+              rows.add(String.format("%c%c%c", c1, c2, c3));
             }
+          }
+        }
+      }
+    }
+
+    Map<String, Integer> dp =
+        rows.stream().collect(Collectors.toMap(Function.identity(), row -> 1));
+    for (int i = 0; i < n - 1; ++i) {
+      Map<String, Integer> nextDp = new HashMap<>();
+      for (String row : rows) {
+        int wayNum = 0;
+        for (String lastRow : rows) {
+          if (check(lastRow, row)) {
+            wayNum = addMod(wayNum, dp.get(lastRow));
+          }
         }
 
-        Map<String, Integer> lastRowToWayNum = rows.stream().collect(Collectors.toMap(Function.identity(), x -> 1));
-        for (int i = 0; i < n - 1; ++i) {
-            Map<String, Integer> nextLastRowToWayNum = new HashMap<>();
-            for (String row : rows) {
-                int wayNum = 0;
-                for (String lastRow : rows) {
-                    if (check(lastRow, row)) {
-                        wayNum = addMod(wayNum, lastRowToWayNum.get(lastRow));
-                    }
-                }
+        nextDp.put(row, wayNum);
+      }
 
-                nextLastRowToWayNum.put(row, wayNum);
-            }
-
-            lastRowToWayNum = nextLastRowToWayNum;
-        }
-
-        return lastRowToWayNum.values().stream().mapToInt(x -> x).reduce(this::addMod).getAsInt();
+      dp = nextDp;
     }
 
-    boolean check(String lastRow, String row) {
-        return IntStream.range(0, lastRow.length()).allMatch(i -> lastRow.charAt(i) != row.charAt(i));
-    }
+    return dp.values().stream().mapToInt(Integer::intValue).reduce(this::addMod).getAsInt();
+  }
 
-    int addMod(int x, int y) {
-        return (x + y) % MODULUS;
-    }
+  boolean check(String row1, String row2) {
+    return IntStream.range(0, row1.length()).allMatch(i -> row1.charAt(i) != row2.charAt(i));
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, MODULUS);
+  }
 }
