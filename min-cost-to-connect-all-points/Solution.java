1c48eed2bfa54c5e20b4a25e3c79d89ca8e69127
Kai Wang <charles.wangkai@gmail.com>
2026-01-07 18:29:34 +0800

    Min Cost to Connect All Points

--- min-cost-to-connect-all-points/Solution.java
+++ min-cost-to-connect-all-points/Solution.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 class Solution {
   public int minCostConnectPoints(int[][] points) {
-    int[] parents = new int[points.length];
-    Arrays.fill(parents, -1);
-
     PriorityQueue<Element> pq = new PriorityQueue<>(Comparator.comparing(Element::cost));
     for (int i = 0; i < points.length; ++i) {
       for (int j = i + 1; j < points.length; ++j) {
@@ -19,29 +20,63 @@ class Solution {
     }
 
     int result = 0;
+    Dsu dsu = new Dsu(points.length);
     while (!pq.isEmpty()) {
       Element head = pq.poll();
 
-      int root1 = findRoot(parents, head.index1());
-      int root2 = findRoot(parents, head.index2());
-      if (root1 != root2) {
+      int leader1 = dsu.find(head.index1());
+      int leader2 = dsu.find(head.index2());
+      if (leader1 != leader2) {
         result += head.cost();
-        parents[root2] = root1;
+        dsu.union(leader1, leader2);
       }
     }
 
     return result;
   }
-
-  int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
-    }
-
-    parents[node] = findRoot(parents, parents[node]);
-
-    return parents[node];
-  }
 }
 
 record Element(int index1, int index2, int cost) {}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
+  }
+}
