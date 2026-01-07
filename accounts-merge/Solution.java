7862df2b2444ac351a1616230ad995c621ffdf88
Kai Wang <charles.wangkai@gmail.com>
2026-01-07 20:37:06 +0800

    Accounts Merge

--- accounts-merge/Solution.java
+++ accounts-merge/Solution.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class Solution {
@@ -13,53 +16,70 @@ class Solution {
       }
     }
 
-    Map<String, String> emailToParent = new HashMap<>();
+    String[] emails = emailToName.keySet().stream().toArray(String[]::new);
+    Map<String, Integer> emailToIndex =
+        IntStream.range(0, emails.length).boxed().collect(Collectors.toMap(i -> emails[i], i -> i));
+
+    Dsu dsu = new Dsu(emails.length);
+
     for (List<String> account : accounts) {
       for (int i = 1; i + 1 < account.size(); ++i) {
-        union(emailToParent, account.get(i), account.get(i + 1));
+        dsu.union(emailToIndex.get(account.get(i)), emailToIndex.get(account.get(i + 1)));
       }
     }
 
-    Map<String, List<String>> rootToEmails = new HashMap<>();
-    for (String email : emailToName.keySet()) {
-      String root = findRoot(emailToParent, email);
-      if (!rootToEmails.containsKey(root)) {
-        rootToEmails.put(root, new ArrayList<>());
-      }
-      rootToEmails.get(root).add(email);
-    }
+    Map<Integer, List<Integer>> leaderToGroup = dsu.buildLeaderToGroup();
 
-    return rootToEmails.keySet().stream()
+    return leaderToGroup.keySet().stream()
         .map(
-            root ->
+            leader ->
                 Stream.concat(
-                        Stream.of(emailToName.get(root)), rootToEmails.get(root).stream().sorted())
+                        Stream.of(emailToName.get(emails[leader])),
+                        leaderToGroup.get(leader).stream().map(index -> emails[index]).sorted())
                     .toList())
         .toList();
   }
+}
 
-  String findRoot(Map<String, String> emailToParent, String email) {
-    String root = email;
-    while (emailToParent.containsKey(root)) {
-      root = emailToParent.get(root);
-    }
+class Dsu {
+  int[] parentOrSizes;
 
-    String p = email;
-    while (!p.equals(root)) {
-      String next = emailToParent.get(p);
-      emailToParent.put(p, root);
-
-      p = next;
-    }
-
-    return root;
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
   }
 
-  void union(Map<String, String> emailToParent, String email1, String email2) {
-    String root1 = findRoot(emailToParent, email1);
-    String root2 = findRoot(emailToParent, email2);
-    if (!root1.equals(root2)) {
-      emailToParent.put(root2, root1);
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
