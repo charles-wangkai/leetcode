fa7120273c3b6ab53ba1446407fe658dd870bdaa
Kai Wang <charles.wangkai@gmail.com>
2026-01-07 15:23:43 +0800

    Count Connected Components in LCM Graph

--- count-connected-components-in-lcm-graph/Solution.java
+++ count-connected-components-in-lcm-graph/Solution.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 class Solution {
   public int countComponents(int[] nums, int threshold) {
@@ -15,29 +14,56 @@ class Solution {
       }
     }
 
-    int[] parents = new int[nums.length];
-    Arrays.fill(parents, -1);
+    Dsu dsu = new Dsu(nums.length);
     for (List<Integer> indices : multipleToIndices.values()) {
       for (int i = 0; i < indices.size() - 1; ++i) {
-        int root1 = findRoot(parents, indices.get(i));
-        int root2 = findRoot(parents, indices.get(i + 1));
-        if (root1 != root2) {
-          parents[root2] = root1;
-        }
+        dsu.union(indices.get(i), indices.get(i + 1));
       }
     }
 
-    return (int)
-        IntStream.range(0, parents.length).map(i -> findRoot(parents, i)).distinct().count();
+    return dsu.buildLeaderToGroup().size();
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
   }
 
-  int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
