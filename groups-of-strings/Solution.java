2e3f32f958d307a329117ccf13d9bb41e0d5a673
Kai Wang <charles.wangkai@gmail.com>
2026-01-07 16:51:33 +0800

    Groups of Strings

--- groups-of-strings/Solution.java
+++ groups-of-strings/Solution.java
@@ -1,78 +1,106 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
   static final int ALPHABET_SIZE = 26;
 
   public int[] groupStrings(String[] words) {
-    Map<Integer, Integer> keyToCount = new HashMap<>();
-    Map<Integer, Integer> keyToParent = new HashMap<>();
-    for (String word : words) {
-      int key = buildKey(word);
+    int[] keys = Arrays.stream(words).mapToInt(this::buildKey).distinct().toArray();
+    Map<Integer, Integer> keyToIndex =
+        IntStream.range(0, keys.length).boxed().collect(Collectors.toMap(i -> keys[i], i -> i));
 
-      keyToCount.put(key, keyToCount.getOrDefault(key, 0) + 1);
-      keyToParent.put(key, -1);
+    int[] counts = new int[keys.length];
+    for (String word : words) {
+      ++counts[keyToIndex.get(buildKey(word))];
     }
 
-    for (int key : keyToParent.keySet()) {
+    Dsu dsu = new Dsu(keys.length);
+    for (int key : keys) {
       for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        union(keyToParent, key, key | (1 << i));
+        union(dsu, keyToIndex, key, key | (1 << i));
       }
 
       for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        if ((key & (1 << i)) != 0) {
-          union(keyToParent, key, key - (1 << i));
+        if (((key >> i) & 1) == 1) {
+          union(dsu, keyToIndex, key, key - (1 << i));
         }
       }
 
       for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        if ((key & (1 << i)) != 0) {
+        if (((key >> i) & 1) == 1) {
           for (int j = 0; j < ALPHABET_SIZE; ++j) {
-            union(keyToParent, key, (key - (1 << i)) | (1 << j));
+            union(dsu, keyToIndex, key, (key - (1 << i)) | (1 << j));
           }
         }
       }
     }
 
-    Map<Integer, Integer> rootToCount = new HashMap<>();
-    for (int key : keyToParent.keySet()) {
-      int root = findRoot(keyToParent, key);
-      rootToCount.put(root, rootToCount.getOrDefault(root, 0) + keyToCount.get(key));
-    }
+    Map<Integer, List<Integer>> leaderToGroup = dsu.buildLeaderToGroup();
 
     return new int[] {
-      rootToCount.size(), rootToCount.values().stream().mapToInt(x -> x).max().getAsInt()
+      leaderToGroup.size(),
+      leaderToGroup.values().stream()
+          .mapToInt(group -> group.stream().mapToInt(node -> counts[node]).sum())
+          .max()
+          .getAsInt()
     };
   }
 
-  static void union(Map<Integer, Integer> keyToParent, int key1, int key2) {
-    if (keyToParent.containsKey(key2)) {
-      int root1 = findRoot(keyToParent, key1);
-      int root2 = findRoot(keyToParent, key2);
-      if (root1 != root2) {
-        keyToParent.put(root2, root1);
-      }
+  void union(Dsu dsu, Map<Integer, Integer> keyToIndex, int key1, int key2) {
+    if (keyToIndex.containsKey(key2)) {
+      dsu.union(keyToIndex.get(key1), keyToIndex.get(key2));
     }
-  }
-
-  static int findRoot(Map<Integer, Integer> keyToParent, int key) {
-    int root = key;
-    while (keyToParent.get(root) != -1) {
-      root = keyToParent.get(root);
-    }
-
-    int p = key;
-    while (p != root) {
-      int next = keyToParent.get(p);
-      keyToParent.put(p, root);
-
-      p = next;
-    }
-
-    return root;
   }
 
   int buildKey(String word) {
     return word.chars().map(c -> 1 << (c - 'a')).reduce(0, (x, y) -> x | y);
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
+    }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
