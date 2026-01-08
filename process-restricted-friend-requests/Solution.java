d990cf9fd3a09f69402624fd6d6267948708c834
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 09:29:04 +0800

    Process Restricted Friend Requests

--- process-restricted-friend-requests/Solution.java
+++ process-restricted-friend-requests/Solution.java
@@ -1,18 +1,20 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
   public boolean[] friendRequests(int n, int[][] restrictions, int[][] requests) {
-    int[] parents = new int[n];
-    Arrays.fill(parents, -1);
-
     boolean[] result = new boolean[requests.length];
+    Dsu dsu = new Dsu(n);
     for (int i = 0; i < result.length; ++i) {
-      int root1 = findRoot(parents, requests[i][0]);
-      int root2 = findRoot(parents, requests[i][1]);
-      if (root1 == root2) {
+      int leader1 = dsu.find(requests[i][0]);
+      int leader2 = dsu.find(requests[i][1]);
+      if (leader1 == leader2) {
         result[i] = true;
-      } else if (check(parents, restrictions, root1, root2)) {
-        parents[root2] = root1;
+      } else if (check(dsu, restrictions, leader1, leader2)) {
+        dsu.union(leader1, leader2);
         result[i] = true;
       }
     }
@@ -20,31 +22,57 @@ class Solution {
     return result;
   }
 
-  boolean check(int[] parents, int[][] restrictions, int root1, int root2) {
+  boolean check(Dsu dsu, int[][] restrictions, int leader1, int leader2) {
     return Arrays.stream(restrictions)
         .allMatch(
             restriction -> {
-              int r1 = findRoot(parents, restriction[0]);
-              int r2 = findRoot(parents, restriction[1]);
+              int l1 = dsu.find(restriction[0]);
+              int l2 = dsu.find(restriction[1]);
 
-              return !((r1 == root1 && r2 == root2) || (r1 == root2 && r2 == root1));
+              return !((l1 == leader1 && l2 == leader2) || (l1 == leader2 && l2 == leader1));
             });
   }
+}
 
-  int findRoot(int[] parents, int node) {
-    int root = node;
-    while (parents[root] != -1) {
-      root = parents[root];
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    int p = node;
-    while (p != root) {
-      int next = parents[p];
-      parents[p] = root;
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-      p = next;
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
     }
 
-    return root;
+    return leaderToGroup;
   }
 }
