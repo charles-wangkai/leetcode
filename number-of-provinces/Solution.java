2d3b950c052e348509fca1a1e9d12c29b5bfcf87
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 08:58:16 +0800

    Number of Provinces

--- number-of-provinces/Solution.java
+++ number-of-provinces/Solution.java
@@ -1,32 +1,63 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
   public int findCircleNum(int[][] isConnected) {
-    int[] parents = new int[isConnected.length];
-    Arrays.fill(parents, -1);
-
+    Dsu dsu = new Dsu(isConnected.length);
     for (int i = 0; i < isConnected.length; ++i) {
       for (int j = 0; j < i; ++j) {
         if (isConnected[i][j] == 1) {
-          int root1 = findRoot(parents, i);
-          int root2 = findRoot(parents, j);
-          if (root1 != root2) {
-            parents[root2] = root1;
-          }
+          dsu.union(i, j);
         }
       }
     }
 
-    return (int) Arrays.stream(parents).filter(parent -> parent == -1).count();
+    return dsu.buildLeaderToGroup().size();
+  }
+}
+
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
   }
 
-  int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
