f7c70ae0280afa25fc5252fdc946fa916020796d
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 18:23:02 +0800

    Sum of Beautiful Subsequences

--- sum-of-beautiful-subsequences/Solution.java
+++ sum-of-beautiful-subsequences/Solution.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/sum-of-beautiful-subsequences/solutions/7140297/python-sieve/
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,7 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
-  static final int MODULUS = 1_000_000_007;
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
 
   public int totalBeauty(int[] nums) {
     int maxValue = Arrays.stream(nums).max().getAsInt();
@@ -37,13 +38,13 @@ class Solution {
                 .boxed()
                 .collect(Collectors.toMap(indices::get, i -> i + 1));
 
-        int[] binaryIndexedTree = new int[Integer.highestOneBit(indices.size()) * 2 + 1];
+        FenwickTree fenwickTree = new FenwickTree(indices.size());
         for (int i = d; i <= maxValue; i += d) {
           for (int index : valueToIndices.getOrDefault(i, List.of()).reversed()) {
             int rank = indexToRank.get(index);
-            int addend = 1 + query(binaryIndexedTree, rank - 1);
-            f[d] = addMod(f[d], addend);
-            update(binaryIndexedTree, rank, addend);
+            int addend = 1 + fenwickTree.computePrefixSum(rank - 1);
+            f[d] = MOD_INT.addMod(f[d], addend);
+            fenwickTree.add(rank, addend);
           }
         }
       }
@@ -51,38 +52,75 @@ class Solution {
 
     for (int d = f.length - 1; d >= 1; --d) {
       for (int i = 2 * d; i < f.length; i += d) {
-        f[d] = addMod(f[d], -f[i]);
+        f[d] = MOD_INT.addMod(f[d], -f[i]);
       }
     }
 
     return IntStream.rangeClosed(1, maxValue)
-        .map(d -> multiplyMod(d, f[d]))
-        .reduce(this::addMod)
+        .map(d -> MOD_INT.multiplyMod(d, f[d]))
+        .reduce(MOD_INT::addMod)
         .getAsInt();
   }
+}
 
-  int query(int[] binaryIndexedTree, int index) {
+class FenwickTree {
+  int[] a;
+
+  FenwickTree(int size) {
+    a = new int[Integer.highestOneBit(size) * 2 + 1];
+  }
+
+  void add(int pos, int delta) {
+    while (pos < a.length) {
+      a[pos] = Solution.MOD_INT.addMod(a[pos], delta);
+      pos += pos & -pos;
+    }
+  }
+
+  int computePrefixSum(int pos) {
     int result = 0;
-    while (index != 0) {
-      result = addMod(result, binaryIndexedTree[index]);
-      index -= index & -index;
+    while (pos != 0) {
+      result = Solution.MOD_INT.addMod(result, a[pos]);
+      pos -= pos & -pos;
     }
 
     return result;
   }
+}
 
-  void update(int[] binaryIndexedTree, int index, int delta) {
-    while (index < binaryIndexedTree.length) {
-      binaryIndexedTree[index] = addMod(binaryIndexedTree[index], delta);
-      index += index & -index;
-    }
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return (int) (x % modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
   }
 
   int addMod(int x, int y) {
-    return Math.floorMod(x + y, MODULUS);
+    return Math.floorMod(x + y, modulus);
   }
 
   int multiplyMod(int x, int y) {
-    return Math.floorMod((long) x * y, MODULUS);
+    return Math.floorMod((long) x * y, modulus);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, int exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
