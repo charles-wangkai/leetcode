682531139ec01aaced9445c1725041a6c76cdd16
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 15:46:28 +0800

    Count Increasing Quadruplets

--- count-increasing-quadruplets/Solution.java
+++ count-increasing-quadruplets/Solution.java
@@ -9,13 +9,14 @@ class Solution {
     for (int k = 1; k < nums.length; ++k) {
       int[] rightSorted = IntStream.range(k + 1, nums.length).map(i -> nums[i]).sorted().toArray();
 
-      int[] A = new int[Integer.highestOneBit(nums.length) * 2 + 1];
+      FenwickTree fenwickTree = new FenwickTree(nums.length);
       for (int j = 0; j < k; ++j) {
         if (nums[j] > nums[k]) {
-          result += prefix_sum(A, nums[k]) * computeRightNum(rightSorted, nums[j]);
+          result +=
+              fenwickTree.computePrefixSum(nums[k] + 1) * computeRightNum(rightSorted, nums[j]);
         }
 
-        add(A, nums[j], 1);
+        fenwickTree.add(nums[j] + 1, 1);
       }
     }
 
@@ -25,22 +26,29 @@ class Solution {
   int computeRightNum(int[] rightSorted, int target) {
     return rightSorted.length - (-1 - Arrays.binarySearch(rightSorted, target));
   }
+}
 
-  int LSB(int i) {
-    return i & -i;
+class FenwickTree {
+  int[] a;
+
+  FenwickTree(int size) {
+    a = new int[Integer.highestOneBit(size) * 2 + 1];
   }
 
-  int prefix_sum(int[] A, int i) {
-    int sum = A[0];
-    for (; i != 0; i -= LSB(i)) sum += A[i];
-    return sum;
-  }
-
-  void add(int[] A, int i, int delta) {
-    if (i == 0) {
-      A[0] += delta;
-      return;
+  void add(int pos, int delta) {
+    while (pos < a.length) {
+      a[pos] += delta;
+      pos += pos & -pos;
     }
-    for (; i < A.length; i += LSB(i)) A[i] += delta;
+  }
+
+  int computePrefixSum(int pos) {
+    int result = 0;
+    while (pos != 0) {
+      result += a[pos];
+      pos -= pos & -pos;
+    }
+
+    return result;
   }
 }
