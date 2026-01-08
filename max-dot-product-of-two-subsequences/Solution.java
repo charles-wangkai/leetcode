7911ea65fabc26735e57c333fdc66b83f61d4120
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 08:39:49 +0800

    Max Dot Product of Two Subsequences

--- max-dot-product-of-two-subsequences/Solution.java
+++ max-dot-product-of-two-subsequences/Solution.java
@@ -6,14 +6,12 @@ class Solution {
     int[][] dp = new int[length1 + 1][length2 + 1];
     for (int i = 0; i <= length1; ++i) {
       for (int j = 0; j <= length2; ++j) {
-        if (i == 0 || j == 0) {
-          dp[i][j] = Integer.MIN_VALUE;
-        } else {
-          dp[i][j] =
-              Math.max(
-                  Math.max(dp[i - 1][j], dp[i][j - 1]),
-                  Math.max(0, dp[i - 1][j - 1]) + nums1[i - 1] * nums2[j - 1]);
-        }
+        dp[i][j] =
+            (i == 0 || j == 0)
+                ? Integer.MIN_VALUE
+                : Math.max(
+                    Math.max(dp[i - 1][j], dp[i][j - 1]),
+                    Math.max(0, dp[i - 1][j - 1]) + nums1[i - 1] * nums2[j - 1]);
       }
     }
 
