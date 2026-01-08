bfe923b2aba84d81296d1c192a322cd09409f951
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 17:09:06 +0800

    Number of Pairs Satisfying Inequality

--- number-of-pairs-satisfying-inequality/Solution.java
+++ number-of-pairs-satisfying-inequality/Solution.java
@@ -4,31 +4,38 @@ class Solution {
 
   public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
     long result = 0;
-    int[] A = new int[1 + Integer.highestOneBit(LIMIT) * 2];
+    FenwickTree fenwickTree = new FenwickTree(LIMIT);
     for (int i = 0; i < nums1.length; ++i) {
       int delta = nums1[i] - nums2[i];
-      result += prefix_sum(A, delta + diff + OFFSET);
-      add(A, delta + OFFSET, 1);
+      result += fenwickTree.computePrefixSum(delta + diff + OFFSET + 1);
+      fenwickTree.add(delta + OFFSET + 1, 1);
     }
 
     return result;
   }
+}
 
-  int prefix_sum(int[] A, int i) {
-    int sum = A[0];
-    for (; i != 0; i -= LSB(i)) sum += A[i];
-    return sum;
+class FenwickTree {
+  int[] a;
+
+  FenwickTree(int size) {
+    a = new int[Integer.highestOneBit(size) * 2 + 1];
   }
 
-  void add(int[] A, int i, int delta) {
-    if (i == 0) {
-      A[0] += delta;
-      return;
+  void add(int pos, int delta) {
+    while (pos < a.length) {
+      a[pos] += delta;
+      pos += pos & -pos;
     }
-    for (; i < A.length; i += LSB(i)) A[i] += delta;
   }
 
-  int LSB(int x) {
-    return x & -x;
+  int computePrefixSum(int pos) {
+    int result = 0;
+    while (pos != 0) {
+      result += a[pos];
+      pos -= pos & -pos;
+    }
+
+    return result;
   }
 }
