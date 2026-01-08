401f76ffb48af23993d714617dc7ac283b8f5aec
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 17:14:44 +0800

    Peaks in Array

--- peaks-in-array/Solution.java
+++ peaks-in-array/Solution.java
@@ -4,11 +4,11 @@ import java.util.List;
 class Solution {
   public List<Integer> countOfPeaks(int[] nums, int[][] queries) {
     boolean[] peaks = new boolean[nums.length];
-    int[] binaryIndexedTree = new int[Integer.highestOneBit(peaks.length) * 2 + 1];
+    FenwickTree fenwickTree = new FenwickTree(peaks.length);
     for (int i = 1; i < peaks.length - 1; ++i) {
       if (nums[i] > Math.max(nums[i - 1], nums[i + 1])) {
         peaks[i] = true;
-        update(binaryIndexedTree, i + 1, 1);
+        fenwickTree.add(i + 1, 1);
       }
     }
 
@@ -18,7 +18,7 @@ class Solution {
         int leftIndex = query[1];
         int rightIndex = query[2];
 
-        result.add(computeRangeSum(binaryIndexedTree, leftIndex + 2, rightIndex));
+        result.add(computeRangeSum(fenwickTree, leftIndex + 2, rightIndex));
       } else {
         int index = query[1];
         int value = query[2];
@@ -31,9 +31,9 @@ class Solution {
             boolean peak = nums[affected] > Math.max(nums[affected - 1], nums[affected + 1]);
             if (peak != peaks[affected]) {
               if (peak) {
-                update(binaryIndexedTree, affected + 1, 1);
+                fenwickTree.add(affected + 1, 1);
               } else {
-                update(binaryIndexedTree, affected + 1, -1);
+                fenwickTree.add(affected + 1, -1);
               }
 
               peaks[affected] = peak;
@@ -46,26 +46,34 @@ class Solution {
     return result;
   }
 
-  int computeRangeSum(int[] binaryIndexedTree, int beginIndex, int endIndex) {
+  int computeRangeSum(FenwickTree fenwickTree, int beginIndex, int endIndex) {
     return (beginIndex <= endIndex)
-        ? (query(binaryIndexedTree, endIndex) - query(binaryIndexedTree, beginIndex - 1))
+        ? (fenwickTree.computePrefixSum(endIndex) - fenwickTree.computePrefixSum(beginIndex - 1))
         : 0;
   }
+}
 
-  int query(int[] binaryIndexedTree, int index) {
+class FenwickTree {
+  int[] a;
+
+  FenwickTree(int size) {
+    a = new int[Integer.highestOneBit(size) * 2 + 1];
+  }
+
+  void add(int pos, int delta) {
+    while (pos < a.length) {
+      a[pos] += delta;
+      pos += pos & -pos;
+    }
+  }
+
+  int computePrefixSum(int pos) {
     int result = 0;
-    while (index != 0) {
-      result += binaryIndexedTree[index];
-      index -= index & -index;
+    while (pos != 0) {
+      result += a[pos];
+      pos -= pos & -pos;
     }
 
     return result;
-  }
-
-  void update(int[] binaryIndexedTree, int index, int delta) {
-    while (index < binaryIndexedTree.length) {
-      binaryIndexedTree[index] += delta;
-      index += index & -index;
-    }
   }
 }
