9962416efd6e7a41085df3dc4077e18dacecf15c
Kai Wang <charles.wangkai@gmail.com>
2026-01-08 09:21:20 +0800

    Power Grid Maintenance

--- power-grid-maintenance/Solution.java
+++ power-grid-maintenance/Solution.java
@@ -8,22 +8,16 @@ import java.util.TreeSet;
 
 class Solution {
   public int[] processQueries(int c, int[][] connections, int[][] queries) {
-    int[] parents = new int[c];
-    Arrays.fill(parents, -1);
-
+    Dsu dsu = new Dsu(c);
     for (int[] connection : connections) {
-      int root1 = findRoot(parents, connection[0] - 1);
-      int root2 = findRoot(parents, connection[1] - 1);
-      if (root1 != root2) {
-        parents[root2] = root1;
-      }
+      dsu.union(connection[0] - 1, connection[1] - 1);
     }
 
-    Map<Integer, SortedSet<Integer>> rootToNodes = new HashMap<>();
+    Map<Integer, SortedSet<Integer>> leaderToNodes = new HashMap<>();
     for (int node = 0; node < c; ++node) {
-      int root = findRoot(parents, node);
-      rootToNodes.putIfAbsent(root, new TreeSet<>());
-      rootToNodes.get(root).add(node);
+      int leader = dsu.find(node);
+      leaderToNodes.putIfAbsent(leader, new TreeSet<>());
+      leaderToNodes.get(leader).add(node);
     }
 
     List<Integer> result = new ArrayList<>();
@@ -31,25 +25,57 @@ class Solution {
       int type = query[0];
       int node = query[1] - 1;
 
+      SortedSet<Integer> nodes = leaderToNodes.get(dsu.find(node));
       if (type == 1) {
-        SortedSet<Integer> nodes = rootToNodes.get(findRoot(parents, node));
-
         result.add(nodes.isEmpty() ? -1 : ((nodes.contains(node) ? node : nodes.first()) + 1));
       } else {
-        rootToNodes.get(findRoot(parents, node)).remove(node);
+        nodes.remove(node);
       }
     }
 
     return result.stream().mapToInt(Integer::intValue).toArray();
   }
+}
 
-  int findRoot(int[] parents, int node) {
-    if (parents[node] == -1) {
-      return node;
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
 
-    parents[node] = findRoot(parents, parents[node]);
+    parentOrSizes[a] = find(parentOrSizes[a]);
 
-    return parents[node];
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
