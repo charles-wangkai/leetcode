f2c1ebf486bf0a9ade18df59b361d110a0d895a3
Kai Wang <charles.wangkai@gmail.com>
2026-01-09 14:23:27 +0800

    Maximum Sum of Subsequence With Non-adjacent Elements

--- maximum-sum-of-subsequence-with-non-adjacent-elements/Solution.java
+++ maximum-sum-of-subsequence-with-non-adjacent-elements/Solution.java
@@ -2,93 +2,80 @@ class Solution {
   static final int MODULUS = 1_000_000_007;
 
   public int maximumSumSubsequence(int[] nums, int[][] queries) {
-    Node segmentTree = buildNode(nums, 0, nums.length - 1);
-
     long result = 0;
+    SegTree segTree = new SegTree(nums);
     for (int[] query : queries) {
-      update(query[0], query[1], segmentTree);
-      result += segmentTree.maxSum11;
+      segTree.update(query[0], query[1]);
+      result += segTree.root.maxSum11;
     }
 
     return (int) (result % MODULUS);
   }
+}
 
-  void update(int index, int value, Node node) {
+class SegTree {
+  Node root;
+
+  SegTree(int[] values) {
+    root = buildNode(values, 0, values.length - 1);
+  }
+
+  private Node buildNode(int[] values, int beginIndex, int endIndex) {
+    Node node = new Node(beginIndex, endIndex);
+
+    if (beginIndex == endIndex) {
+      node.maxSum00 = 0;
+      node.maxSum01 = 0;
+      node.maxSum10 = 0;
+      node.maxSum11 = Math.max(0, values[beginIndex]);
+    } else {
+      int middleIndex = (beginIndex + endIndex) / 2;
+      node.left = buildNode(values, beginIndex, middleIndex);
+      node.right = buildNode(values, middleIndex + 1, endIndex);
+
+      node.pull();
+    }
+
+    return node;
+  }
+
+  void update(int index, int value) {
+    update(index, value, root);
+  }
+
+  private void update(int index, int value, Node node) {
     if (node.beginIndex <= index && node.endIndex >= index) {
       if (node.beginIndex == node.endIndex) {
         node.maxSum11 = Math.max(0, value);
       } else {
         update(index, value, node.left);
         update(index, value, node.right);
-        updateMaxSums(node);
+
+        node.pull();
       }
     }
   }
 
-  Node buildNode(int[] nums, int beginIndex, int endIndex) {
-    if (beginIndex == endIndex) {
-      return new Node(beginIndex, endIndex, 0, 0, 0, Math.max(0, nums[beginIndex]), null, null);
+  static class Node {
+    int beginIndex;
+    int endIndex;
+    long maxSum00;
+    long maxSum01;
+    long maxSum10;
+    long maxSum11;
+    Node left;
+    Node right;
+
+    Node(int beginIndex, int endIndex) {
+      this.beginIndex = beginIndex;
+      this.endIndex = endIndex;
     }
 
-    int middleIndex = (beginIndex + endIndex) / 2;
-
-    Node node =
-        new Node(
-            beginIndex,
-            endIndex,
-            -1,
-            -1,
-            -1,
-            -1,
-            buildNode(nums, beginIndex, middleIndex),
-            buildNode(nums, middleIndex + 1, endIndex));
-    updateMaxSums(node);
-
-    return node;
-  }
-
-  void updateMaxSums(Node node) {
-    node.maxSum00 =
-        Math.max(
-            node.left.maxSum00 + node.right.maxSum10, node.left.maxSum01 + node.right.maxSum00);
-    node.maxSum01 =
-        Math.max(
-            node.left.maxSum00 + node.right.maxSum11, node.left.maxSum01 + node.right.maxSum01);
-    node.maxSum10 =
-        Math.max(
-            node.left.maxSum10 + node.right.maxSum10, node.left.maxSum11 + node.right.maxSum00);
-    node.maxSum11 =
-        Math.max(
-            node.left.maxSum10 + node.right.maxSum11, node.left.maxSum11 + node.right.maxSum01);
-  }
-}
-
-class Node {
-  int beginIndex;
-  int endIndex;
-  long maxSum00;
-  long maxSum01;
-  long maxSum10;
-  long maxSum11;
-  Node left;
-  Node right;
-
-  Node(
-      int beginIndex,
-      int endIndex,
-      long maxSum00,
-      long maxSum01,
-      long maxSum10,
-      long maxSum11,
-      Node left,
-      Node right) {
-    this.beginIndex = beginIndex;
-    this.endIndex = endIndex;
-    this.maxSum00 = maxSum00;
-    this.maxSum01 = maxSum01;
-    this.maxSum10 = maxSum10;
-    this.maxSum11 = maxSum11;
-    this.left = left;
-    this.right = right;
+    void pull() {
+      maxSum00 = Math.max(left.maxSum00 + right.maxSum10, left.maxSum01 + right.maxSum00);
+      maxSum01 = Math.max(left.maxSum00 + right.maxSum11, left.maxSum01 + right.maxSum01);
+      maxSum10 = Math.max(left.maxSum10 + right.maxSum10, left.maxSum11 + right.maxSum00);
+      maxSum11 = Math.max(left.maxSum10 + right.maxSum11, left.maxSum11 + right.maxSum01);
+    }
   }
 }
