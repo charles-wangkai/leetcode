9f71a2f43bca35e0f75cfa7cd09c75a42fcc6529
Kai Wang <charles.wangkai@gmail.com>
2026-01-09 10:22:27 +0800

    Fruits Into Baskets III

--- fruits-into-baskets-iii/Solution.java
+++ fruits-into-baskets-iii/Solution.java
@@ -1,11 +1,10 @@
 class Solution {
   public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-    Node segmentTree = buildNode(baskets, 0, baskets.length - 1);
-
     int result = 0;
+    SegTree segTree = new SegTree(baskets);
     for (int fruit : fruits) {
-      if (segmentTree.maxValue >= fruit) {
-        update(findIndex(fruit, segmentTree), 0, segmentTree);
+      if (segTree.root.maxValue >= fruit) {
+        segTree.update(segTree.findIndex(fruit), 0);
       } else {
         ++result;
       }
@@ -13,8 +12,32 @@ class Solution {
 
     return result;
   }
+}
 
-  void update(int index, int value, Node node) {
+class SegTree {
+  Node root;
+
+  SegTree(int[] values) {
+    root = buildNode(values, 0, values.length - 1);
+  }
+
+  private Node buildNode(int[] values, int beginIndex, int endIndex) {
+    if (beginIndex == endIndex) {
+      return new Node(beginIndex, endIndex, values[beginIndex], null, null);
+    }
+
+    int middleIndex = (beginIndex + endIndex) / 2;
+    Node left = buildNode(values, beginIndex, middleIndex);
+    Node right = buildNode(values, middleIndex + 1, endIndex);
+
+    return new Node(beginIndex, endIndex, Math.max(left.maxValue, right.maxValue), left, right);
+  }
+
+  void update(int index, int value) {
+    update(index, value, root);
+  }
+
+  private void update(int index, int value, Node node) {
     if (node.beginIndex <= index && node.endIndex >= index) {
       if (node.beginIndex == node.endIndex) {
         node.maxValue = value;
@@ -27,7 +50,11 @@ class Solution {
     }
   }
 
-  int findIndex(int target, Node node) {
+  int findIndex(int target) {
+    return findIndex(target, root);
+  }
+
+  private int findIndex(int target, Node node) {
     if (node.beginIndex == node.endIndex) {
       return node.beginIndex;
     }
@@ -37,31 +64,19 @@ class Solution {
         : findIndex(target, node.right);
   }
 
-  Node buildNode(int[] values, int beginIndex, int endIndex) {
-    if (beginIndex == endIndex) {
-      return new Node(beginIndex, endIndex, values[beginIndex], null, null);
+  static class Node {
+    int beginIndex;
+    int endIndex;
+    int maxValue;
+    Node left;
+    Node right;
+
+    Node(int beginIndex, int endIndex, int maxValue, Node left, Node right) {
+      this.beginIndex = beginIndex;
+      this.endIndex = endIndex;
+      this.maxValue = maxValue;
+      this.left = left;
+      this.right = right;
     }
-
-    int middleIndex = (beginIndex + endIndex) / 2;
-    Node left = buildNode(values, beginIndex, middleIndex);
-    Node right = buildNode(values, middleIndex + 1, endIndex);
-
-    return new Node(beginIndex, endIndex, Math.max(left.maxValue, right.maxValue), left, right);
-  }
-}
-
-class Node {
-  int beginIndex;
-  int endIndex;
-  int maxValue;
-  Node left;
-  Node right;
-
-  Node(int beginIndex, int endIndex, int maxValue, Node left, Node right) {
-    this.beginIndex = beginIndex;
-    this.endIndex = endIndex;
-    this.maxValue = maxValue;
-    this.left = left;
-    this.right = right;
   }
 }
