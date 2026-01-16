6e72eeac00fa325c05f338574a8e2d00352cbdc6
Kai Wang <charles.wangkai@gmail.com>
2026-01-16 19:10:33 +0700

    Maximum Square Area by Removing Fences From a Field

--- maximum-square-area-by-removing-fences-from-a-field/Solution.java
+++ maximum-square-area-by-removing-fences-from-a-field/Solution.java
@@ -1,20 +1,19 @@
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 class Solution {
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
+
   public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
     Set<Integer> hSizes = buildSizes(m, hFences);
+    OptionalInt maxSize =
+        buildSizes(n, vFences).stream().filter(hSizes::contains).mapToInt(Integer::intValue).max();
 
-    long maxArea = -1;
-    for (int vSize : buildSizes(n, vFences)) {
-      if (hSizes.contains(vSize)) {
-        maxArea = Math.max(maxArea, (long) vSize * vSize);
-      }
-    }
-
-    return (maxArea == -1) ? -1 : (int) (maxArea % 1_000_000_007);
+    return maxSize.isPresent() ? MOD_INT.powMod(maxSize.getAsInt(), 2) : -1;
   }
 
   Set<Integer> buildSizes(int length, int[] fences) {
@@ -29,5 +28,42 @@ class Solution {
     }
 
     return result;
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return (int) (x % modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return Math.floorMod(x + y, modulus);
+  }
+
+  int multiplyMod(int x, int y) {
+    return Math.floorMod((long) x * y, modulus);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
