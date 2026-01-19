96964765793b5b7e05aa6f0e7ed5a02b97b3ed94
Kai Wang <charles.wangkai@gmail.com>
2026-01-19 09:43:16 +0800

    Maximum Side Length of a Square with Sum Less than or Equal to Threshold

--- maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/Solution.java
+++ maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/Solution.java
@@ -1,31 +1,36 @@
-public class Solution {
-	public int maxSideLength(int[][] mat, int threshold) {
-		int m = mat.length;
-		int n = mat[0].length;
+class Solution {
+  public int maxSideLength(int[][] mat, int threshold) {
+    int m = mat.length;
+    int n = mat[0].length;
 
-		int[][] prefixSums = new int[m + 1][n + 1];
-		for (int r = 1; r <= m; r++) {
-			for (int c = 1; c <= n; c++) {
-				prefixSums[r][c] = prefixSums[r - 1][c] + prefixSums[r][c - 1] - prefixSums[r - 1][c - 1]
-						+ mat[r - 1][c - 1];
-			}
-		}
+    int[][] prefixSums = new int[m + 1][n + 1];
+    for (int r = 1; r <= m; ++r) {
+      for (int c = 1; c <= n; ++c) {
+        prefixSums[r][c] =
+            prefixSums[r - 1][c]
+                + prefixSums[r][c - 1]
+                - prefixSums[r - 1][c - 1]
+                + mat[r - 1][c - 1];
+      }
+    }
 
-		for (int size = Math.min(m, n); size >= 1; size--) {
-			for (int r = 1; r + size - 1 <= m; r++) {
-				for (int c = 1; c + size - 1 <= n; c++) {
-					if (computeSquareSum(prefixSums, r, c, size) <= threshold) {
-						return size;
-					}
-				}
-			}
-		}
+    for (int side = Math.min(m, n); side >= 1; --side) {
+      for (int r = 1; r + side - 1 <= m; ++r) {
+        for (int c = 1; c + side - 1 <= n; ++c) {
+          if (computeRangeSum(prefixSums, r, c, side) <= threshold) {
+            return side;
+          }
+        }
+      }
+    }
 
-		return 0;
-	}
+    return 0;
+  }
 
-	static int computeSquareSum(int[][] prefixSums, int r, int c, int size) {
-		return prefixSums[r + size - 1][c + size - 1] - prefixSums[r - 1][c + size - 1]
-				- prefixSums[r + size - 1][c - 1] + prefixSums[r - 1][c - 1];
-	}
+  static int computeRangeSum(int[][] prefixSums, int r, int c, int side) {
+    return prefixSums[r + side - 1][c + side - 1]
+        - prefixSums[r - 1][c + side - 1]
+        - prefixSums[r + side - 1][c - 1]
+        + prefixSums[r - 1][c - 1];
+  }
 }
