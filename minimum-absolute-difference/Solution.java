66595e15ea2d67aac3e70f1119578267f15b9941
Kai Wang <charles.wangkai@gmail.com>
2026-01-26 09:12:18 +0800

    Minimum Absolute Difference

--- minimum-absolute-difference/Solution.java
+++ minimum-absolute-difference/Solution.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
   public List<List<Integer>> minimumAbsDifference(int[] arr) {
-    int[] sorted = Arrays.stream(arr).boxed().sorted().mapToInt(x -> x).toArray();
-    int minDiff =
-        IntStream.range(0, sorted.length - 1).map(i -> sorted[i + 1] - sorted[i]).min().getAsInt();
+    Arrays.sort(arr);
+    int minDiff = IntStream.range(0, arr.length - 1).map(i -> arr[i + 1] - arr[i]).min().getAsInt();
 
-    return IntStream.range(0, sorted.length - 1)
-        .filter(i -> sorted[i + 1] - sorted[i] == minDiff)
-        .mapToObj(i -> List.of(sorted[i], sorted[i + 1]))
-        .collect(Collectors.toList());
+    return IntStream.range(0, arr.length - 1)
+        .filter(i -> arr[i + 1] - arr[i] == minDiff)
+        .mapToObj(i -> List.of(arr[i], arr[i + 1]))
+        .toList();
   }
 }
