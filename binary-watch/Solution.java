80fe75a947da3698ffe36935068c77ad7d8ec920
Kai Wang <charles.wangkai@gmail.com>
2026-02-17 08:58:24 +0800

    Binary Watch

--- binary-watch/Solution.java
+++ binary-watch/Solution.java
@@ -1,28 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
-  public List<String> readBinaryWatch(int num) {
-    List<String> times = new ArrayList<>();
-    for (int hour = 0; hour <= 11; hour++) {
-      int hourOneCount = countOne(hour);
-      for (int minute = 0; minute <= 59; minute++) {
-        if (hourOneCount + countOne(minute) == num) {
-          times.add(String.format("%d:%02d", hour, minute));
+class Solution {
+  public List<String> readBinaryWatch(int turnedOn) {
+    List<String> result = new ArrayList<>();
+    for (int hour = 0; hour <= 11; ++hour) {
+      for (int minute = 0; minute <= 59; ++minute) {
+        if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+          result.add("%d:%02d".formatted(hour, minute));
         }
       }
     }
-    return times;
-  }
 
-  int countOne(int n) {
-    int result = 0;
-    while (n != 0) {
-      if (n % 2 != 0) {
-        result++;
-      }
-      n /= 2;
-    }
     return result;
   }
 }
