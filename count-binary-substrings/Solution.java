983392507cdc8b98f0e1792b9e47aa3e4175de2c
Kai Wang <charles.wangkai@gmail.com>
2026-02-19 09:13:58 +0800

    Count Binary Substrings

--- count-binary-substrings/Solution.java
+++ count-binary-substrings/Solution.java
@@ -3,12 +3,12 @@ class Solution {
     int result = 0;
     int prevCount = 0;
     int currCount = 0;
-    char target = '0';
-    for (char current : s.toCharArray()) {
-      if (current != target) {
+    char curr = '0';
+    for (char c : s.toCharArray()) {
+      if (c != curr) {
         prevCount = currCount;
         currCount = 0;
-        target = (char) ('0' + '1' - target);
+        curr = (char) ('0' + '1' - curr);
       }
 
       ++currCount;
