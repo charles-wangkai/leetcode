86b75bb4c00a66e3bad017edbba4b32ee1e4e4fd
Kai Wang <charles.wangkai@gmail.com>
2026-02-21 08:56:13 +0800

    Prime Number of Set Bits in Binary Representation

--- prime-number-of-set-bits-in-binary-representation/Solution.java
+++ prime-number-of-set-bits-in-binary-representation/Solution.java
@@ -1,24 +1,22 @@
 import java.util.stream.IntStream;
 
-public class Solution {
-	public int countPrimeSetBits(int L, int R) {
-		return (int) IntStream.rangeClosed(L, R).filter(this::isValid).count();
-	}
+class Solution {
+  public int countPrimeSetBits(int left, int right) {
+    return (int)
+        IntStream.rangeClosed(left, right).filter(x -> isPrime(Integer.bitCount(x))).count();
+  }
 
-	boolean isValid(int n) {
-		return isPrime((int) Integer.toBinaryString(n).chars().filter(digit -> digit == '1').count());
-	}
+  boolean isPrime(int n) {
+    if (n < 2) {
+      return false;
+    }
 
-	boolean isPrime(int n) {
-		if (n < 2) {
-			return false;
-		}
+    for (int i = 2; i * i <= n; ++i) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
 
-		for (int i = 2; i * i <= n; i++) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+    return true;
+  }
 }
