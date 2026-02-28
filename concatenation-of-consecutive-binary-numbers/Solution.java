f149fbf92480a82638f647607a0a878348576f31
Kai Wang <charles.wangkai@gmail.com>
2026-02-28 08:25:18 +0800

    Concatenation of Consecutive Binary Numbers

--- concatenation-of-consecutive-binary-numbers/Solution.java
+++ concatenation-of-consecutive-binary-numbers/Solution.java
@@ -1,19 +1,50 @@
+import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 class Solution {
-  static final int MODULUS = 1_000_000_007;
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
 
   public int concatenatedBinary(int n) {
     return IntStream.rangeClosed(1, n)
-        .reduce((result, i) -> addMod(multiplyMod(result, (Integer.highestOneBit(i) * 2)), i))
-        .getAsInt();
+        .reduce(
+            0,
+            (acc, x) -> MOD_INT.addMod(MOD_INT.multiplyMod(acc, Integer.highestOneBit(x) * 2), x));
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
   }
 
   int addMod(int x, int y) {
-    return Math.floorMod(x + y, MODULUS);
+    return mod(x + y);
   }
 
   int multiplyMod(int x, int y) {
-    return Math.floorMod((long) x * y, MODULUS);
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
