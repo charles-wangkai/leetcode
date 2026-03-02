f64de648b00c0f556ce8ae8dcad1cc27b5c18929
Kai Wang <charles.wangkai@gmail.com>
2026-03-02 09:02:14 +0800

    Minimum Swaps to Arrange a Binary Grid

--- minimum-swaps-to-arrange-a-binary-grid/Solution.java
+++ minimum-swaps-to-arrange-a-binary-grid/Solution.java
@@ -1,45 +1,41 @@
 import java.util.Arrays;
 
 class Solution {
-    public int minSwaps(int[][] grid) {
-        int n = grid.length;
+  public int minSwaps(int[][] grid) {
+    int n = grid.length;
 
-        int[] trailingZeroNums = Arrays.stream(grid).mapToInt(this::computeTrailingZeroNum).toArray();
+    int[] trailingZeroNums = Arrays.stream(grid).mapToInt(this::computeTrailingZeroNum).toArray();
 
-        int result = 0;
-        for (int i = 0; i < n; ++i) {
-            int lowerTrailingZeroNum = n - 1 - i;
+    int result = 0;
+    for (int r = 0; r < n; ++r) {
+      int lowerTrailingZeroNum = n - 1 - r;
 
-            int goodIndex = i;
-            while (goodIndex != n && trailingZeroNums[goodIndex] < lowerTrailingZeroNum) {
-                ++goodIndex;
-            }
-            if (goodIndex == n) {
-                return -1;
-            }
+      int goodIndex = r;
+      while (goodIndex != n && trailingZeroNums[goodIndex] < lowerTrailingZeroNum) {
+        ++goodIndex;
+      }
+      if (goodIndex == n) {
+        return -1;
+      }
 
-            result += goodIndex - i;
+      result += goodIndex - r;
 
-            int temp = trailingZeroNums[goodIndex];
-            for (int j = goodIndex; j >= i + 1; --j) {
-                trailingZeroNums[j] = trailingZeroNums[j - 1];
-            }
-            trailingZeroNums[i] = temp;
-        }
-
-        return result;
+      int temp = trailingZeroNums[goodIndex];
+      for (int j = goodIndex; j >= r + 1; --j) {
+        trailingZeroNums[j] = trailingZeroNums[j - 1];
+      }
+      trailingZeroNums[r] = temp;
     }
 
-    int computeTrailingZeroNum(int[] row) {
-        int result = 0;
-        for (int i = row.length - 1; i >= 0; --i) {
-            if (row[i] != 0) {
-                break;
-            }
+    return result;
+  }
 
-            ++result;
-        }
-
-        return result;
+  int computeTrailingZeroNum(int[] line) {
+    int result = 0;
+    for (int c = line.length - 1; c >= 0 && line[c] == 0; --c) {
+      ++result;
     }
+
+    return result;
+  }
 }
