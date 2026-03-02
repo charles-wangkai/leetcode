64e52b5a38b338f1400982f3d91900ef1a1c99cb
Kai Wang <charles.wangkai@gmail.com>
2026-03-02 13:15:21 +0800

    Palindromic Path Queries in a Tree

--- palindromic-path-queries-in-a-tree/Solution.java
+++ palindromic-path-queries-in-a-tree/Solution.java
@@ -4,36 +4,12 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
-  int time;
-
   public List<Boolean> palindromePath(int n, int[][] edges, String s, String[] queries) {
-    @SuppressWarnings("unchecked")
-    List<Integer>[] adjLists = new List[n];
-    for (int i = 0; i < adjLists.length; ++i) {
-      adjLists[i] = new ArrayList<>();
-    }
-    for (int[] edge : edges) {
-      adjLists[edge[0]].add(edge[1]);
-      adjLists[edge[1]].add(edge[0]);
-    }
-
-    FenwickTree[] fenwickTrees = new FenwickTree[26];
-    for (int i = 0; i < fenwickTrees.length; ++i) {
-      fenwickTrees[i] = new FenwickTree(2 * n);
-    }
-
-    int[] heights = new int[n];
-    int[][] parents = new int[n][Integer.toBinaryString(n).length()];
-    int[] inTimes = new int[n];
-    int[] outTimes = new int[n];
-    time = 1;
-    search(heights, parents, inTimes, outTimes, fenwickTrees, s, adjLists, 0, -1, 0);
-
-    for (int i = 1; i < parents[0].length; ++i) {
-      for (int node = 0; node < n; ++node) {
-        parents[node][i] = (parents[node][i - 1] == -1) ? -1 : parents[parents[node][i - 1]][i - 1];
-      }
-    }
+    Tree tree =
+        new Tree(
+            Arrays.stream(edges).mapToInt(edge -> edge[0]).toArray(),
+            Arrays.stream(edges).mapToInt(edge -> edge[1]).toArray(),
+            s);
 
     List<Boolean> result = new ArrayList<>();
     char[] letters = s.toCharArray();
@@ -43,25 +19,25 @@ class Solution {
         int u = Integer.parseInt(fields[1]);
         char c = fields[2].charAt(0);
 
-        fenwickTrees[letters[u] - 'a'].add(inTimes[u], -1);
-        fenwickTrees[letters[u] - 'a'].add(outTimes[u], 1);
+        tree.fenwickTrees[letters[u] - 'a'].add(tree.inTimes[u], -1);
+        tree.fenwickTrees[letters[u] - 'a'].add(tree.outTimes[u], 1);
 
         letters[u] = c;
 
-        fenwickTrees[letters[u] - 'a'].add(inTimes[u], 1);
-        fenwickTrees[letters[u] - 'a'].add(outTimes[u], -1);
+        tree.fenwickTrees[letters[u] - 'a'].add(tree.inTimes[u], 1);
+        tree.fenwickTrees[letters[u] - 'a'].add(tree.outTimes[u], -1);
       } else {
         int u = Integer.parseInt(fields[1]);
         int v = Integer.parseInt(fields[2]);
 
-        int lca = findLca(heights, parents, u, v);
+        int lca = tree.findLca(u, v);
         int[] counts =
             IntStream.range(0, 26)
                 .map(
                     i ->
-                        fenwickTrees[i].computePrefixSum(inTimes[u])
-                            + fenwickTrees[i].computePrefixSum(inTimes[v])
-                            - fenwickTrees[i].computePrefixSum(inTimes[lca]) * 2
+                        tree.fenwickTrees[i].computePrefixSum(tree.inTimes[u])
+                            + tree.fenwickTrees[i].computePrefixSum(tree.inTimes[v])
+                            - tree.fenwickTrees[i].computePrefixSum(tree.inTimes[lca]) * 2
                             + ((letters[lca] - 'a' == i) ? 1 : 0))
                 .toArray();
 
@@ -70,38 +46,6 @@ class Solution {
     }
 
     return result;
-  }
-
-  void search(
-      int[] heights,
-      int[][] parents,
-      int[] inTimes,
-      int[] outTimes,
-      FenwickTree[] fenwickTrees,
-      String s,
-      List<Integer>[] adjLists,
-      int height,
-      int parent,
-      int node) {
-    heights[node] = height;
-    parents[node][0] = parent;
-
-    inTimes[node] = time;
-    ++time;
-
-    fenwickTrees[s.charAt(node) - 'a'].add(inTimes[node], 1);
-
-    for (int adj : adjLists[node]) {
-      if (adj != parent) {
-        search(
-            heights, parents, inTimes, outTimes, fenwickTrees, s, adjLists, height + 1, node, adj);
-      }
-    }
-
-    outTimes[node] = time;
-    ++time;
-
-    fenwickTrees[s.charAt(node) - 'a'].add(outTimes[node], -1);
   }
 
   int findLca(int[] heights, int[][] parents, int node1, int node2) {
@@ -160,5 +104,102 @@ class FenwickTree {
     }
 
     return result;
+  }
+}
+
+class Tree {
+  int n;
+  int[] u;
+  int[] v;
+  String s;
+  List<Integer>[] edgeLists;
+  int[] depths;
+  int[][] ancestors;
+  FenwickTree[] fenwickTrees;
+  int[] inTimes;
+  int[] outTimes;
+  int time = 1;
+
+  @SuppressWarnings("unchecked")
+  Tree(int[] u, int[] v, String s) {
+    n = u.length + 1;
+
+    this.u = u;
+    this.v = v;
+    this.s = s;
+
+    edgeLists = new List[n];
+    for (int i = 0; i < edgeLists.length; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < u.length; ++i) {
+      edgeLists[u[i]].add(i);
+      edgeLists[v[i]].add(i);
+    }
+
+    depths = new int[n];
+    ancestors = new int[n][Integer.toBinaryString(n).length()];
+
+    fenwickTrees = new FenwickTree[26];
+    for (int i = 0; i < fenwickTrees.length; ++i) {
+      fenwickTrees[i] = new FenwickTree(2 * n);
+    }
+
+    inTimes = new int[n];
+    outTimes = new int[n];
+
+    init(0, -1, 0);
+  }
+
+  private void init(int depth, int parent, int node) {
+    depths[node] = depth;
+
+    ancestors[node][0] = parent;
+    for (int i = 1; i < ancestors[node].length; ++i) {
+      ancestors[node][i] =
+          (ancestors[node][i - 1] == -1) ? -1 : ancestors[ancestors[node][i - 1]][i - 1];
+    }
+
+    inTimes[node] = time;
+    ++time;
+
+    fenwickTrees[s.charAt(node) - 'a'].add(inTimes[node], 1);
+
+    for (int edge : edgeLists[node]) {
+      int adj = (node == u[edge]) ? v[edge] : u[edge];
+      if (adj != parent) {
+        init(depth + 1, node, adj);
+      }
+    }
+
+    outTimes[node] = time;
+    ++time;
+
+    fenwickTrees[s.charAt(node) - 'a'].add(outTimes[node], -1);
+  }
+
+  int findLca(int node1, int node2) {
+    if (depths[node1] < depths[node2]) {
+      return findLca(node2, node1);
+    }
+
+    for (int i = ancestors[node1].length - 1; i >= 0; --i) {
+      if (ancestors[node1][i] != -1 && depths[ancestors[node1][i]] >= depths[node2]) {
+        node1 = ancestors[node1][i];
+      }
+    }
+
+    if (node1 == node2) {
+      return node1;
+    }
+
+    for (int i = ancestors[node1].length - 1; i >= 0; --i) {
+      if (ancestors[node1][i] != ancestors[node2][i]) {
+        node1 = ancestors[node1][i];
+        node2 = ancestors[node2][i];
+      }
+    }
+
+    return ancestors[node1][0];
   }
 }
