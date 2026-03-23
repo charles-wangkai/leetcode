527a16d3a0680eb112df47141ec55bf535a0dc9b
Kai Wang <charles.wangkai@gmail.com>
2026-03-23 08:37:58 +0800

    Maximum Non Negative Product in a Matrix

--- maximum-non-negative-product-in-a-matrix/Solution.java
+++ maximum-non-negative-product-in-a-matrix/Solution.java
@@ -1,35 +1,49 @@
 class Solution {
-    public int maxProductPath(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
+  public int maxProductPath(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-        long[][] minProducts = new long[row][col];
-        long[][] maxProducts = new long[row][col];
-        for (int r = 0; r < row; ++r) {
-            for (int c = 0; c < col; ++c) {
-                if (r == 0 && c == 0) {
-                    minProducts[0][0] = grid[0][0];
-                    maxProducts[0][0] = grid[0][0];
-                } else {
-                    minProducts[r][c] = Long.MAX_VALUE;
-                    maxProducts[r][c] = Long.MIN_VALUE;
+    long[][] minProducts = new long[m][n];
+    long[][] maxProducts = new long[m][n];
+    for (int r = 0; r < m; ++r) {
+      for (int c = 0; c < n; ++c) {
+        if (r == 0 && c == 0) {
+          minProducts[0][0] = grid[0][0];
+          maxProducts[0][0] = grid[0][0];
+        } else {
+          minProducts[r][c] = Long.MAX_VALUE;
+          maxProducts[r][c] = Long.MIN_VALUE;
 
-                    if (r != 0) {
-                        minProducts[r][c] = Math.min(minProducts[r][c],
-                                Math.min(minProducts[r - 1][c] * grid[r][c], maxProducts[r - 1][c] * grid[r][c]));
-                        maxProducts[r][c] = Math.max(maxProducts[r][c],
-                                Math.max(minProducts[r - 1][c] * grid[r][c], maxProducts[r - 1][c] * grid[r][c]));
-                    }
-                    if (c != 0) {
-                        minProducts[r][c] = Math.min(minProducts[r][c],
-                                Math.min(minProducts[r][c - 1] * grid[r][c], maxProducts[r][c - 1] * grid[r][c]));
-                        maxProducts[r][c] = Math.max(maxProducts[r][c],
-                                Math.max(minProducts[r][c - 1] * grid[r][c], maxProducts[r][c - 1] * grid[r][c]));
-                    }
-                }
-            }
+          if (r != 0) {
+            update(minProducts, maxProducts, grid, r, c, -1, 0);
+          }
+          if (c != 0) {
+            update(minProducts, maxProducts, grid, r, c, 0, -1);
+          }
         }
-
-        return (maxProducts[row - 1][col - 1] >= 0) ? (int) (maxProducts[row - 1][col - 1] % 1_000_000_007) : -1;
+      }
     }
+
+    return (maxProducts[m - 1][n - 1] >= 0)
+        ? (int) (maxProducts[m - 1][n - 1] % 1_000_000_007)
+        : -1;
+  }
+
+  void update(
+      long[][] minProducts,
+      long[][] maxProducts,
+      int[][] grid,
+      int r,
+      int c,
+      int offsetR,
+      int offsetC) {
+    int prevR = r + offsetR;
+    int prevC = c + offsetC;
+
+    long product1 = minProducts[prevR][prevC] * grid[r][c];
+    long product2 = maxProducts[prevR][prevC] * grid[r][c];
+
+    minProducts[r][c] = Math.min(minProducts[r][c], Math.min(product1, product2));
+    maxProducts[r][c] = Math.max(maxProducts[r][c], Math.max(product1, product2));
+  }
 }
