9b0db821a7c2d13f96378dd692dcb2ccb592452d
Kai Wang <charles.wangkai@gmail.com>
2026-04-08 08:44:38 +0800

    XOR After Range Multiplication Queries I

--- xor-after-range-multiplication-queries-i/Solution.java
+++ xor-after-range-multiplication-queries-i/Solution.java
@@ -1,19 +1,53 @@
+import java.math.BigInteger;
 import java.util.Arrays;
 
 class Solution {
-  static final int MODULUS = 1_000_000_007;
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
 
   public int xorAfterQueries(int[] nums, int[][] queries) {
     for (int[] query : queries) {
       for (int i = query[0]; i <= query[1]; i += query[2]) {
-        nums[i] = multiplyMod(nums[i], query[3]);
+        nums[i] = MOD_INT.multiplyMod(nums[i], query[3]);
       }
     }
 
-    return Arrays.stream(nums).reduce((acc, x) -> acc ^ x).getAsInt();
+    return Arrays.stream(nums).reduce(0, (acc, x) -> acc ^ x);
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
   }
 
   int multiplyMod(int x, int y) {
-    return Math.floorMod((long) x * y, MODULUS);
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
