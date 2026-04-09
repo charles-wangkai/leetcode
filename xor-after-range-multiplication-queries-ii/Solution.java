5cf3f418e3b24d32642cfd829952663f98ecebc0
Kai Wang <charles.wangkai@gmail.com>
2026-04-09 09:54:44 +0800

    XOR After Range Multiplication Queries II

--- xor-after-range-multiplication-queries-ii/Solution.java
+++ xor-after-range-multiplication-queries-ii/Solution.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-  static final int MODULUS = 1_000_000_007;
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
 
   public int xorAfterQueries(int[] nums, int[][] queries) {
     int threshold = (int) Math.ceil(Math.sqrt(nums.length));
@@ -28,18 +28,18 @@ class Solution {
 
         int[] events = stepToEvents.get(k);
 
-        events[l] = multiplyMod(events[l], v);
+        events[l] = MOD_INT.multiplyMod(events[l], v);
 
         int r2 = r + Math.floorMod(-(r - l), k);
         if (r2 == r) {
           r2 += k;
         }
         if (r2 < events.length) {
-          events[r2] = multiplyMod(events[r2], inverseMod(v));
+          events[r2] = MOD_INT.multiplyMod(events[r2], MOD_INT.modInv(v));
         }
       } else {
         for (int i = l; i <= r; i += k) {
-          nums[i] = multiplyMod(nums[i], v);
+          nums[i] = MOD_INT.multiplyMod(nums[i], v);
         }
       }
     }
@@ -50,20 +50,49 @@ class Solution {
       for (int remainder = 0; remainder < step; ++remainder) {
         int factor = 1;
         for (int i = remainder; i < events.length; i += step) {
-          factor = multiplyMod(factor, events[i]);
-          nums[i] = multiplyMod(nums[i], factor);
+          factor = MOD_INT.multiplyMod(factor, events[i]);
+          nums[i] = MOD_INT.multiplyMod(nums[i], factor);
         }
       }
     }
 
-    return Arrays.stream(nums).reduce((acc, x) -> acc ^ x).getAsInt();
+    return Arrays.stream(nums).reduce(0, (acc, x) -> acc ^ x);
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
   }
 
   int multiplyMod(int x, int y) {
-    return Math.floorMod((long) x * y, MODULUS);
+    return mod((long) x * y);
   }
 
-  int inverseMod(int x) {
-    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(MODULUS)).intValue();
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
