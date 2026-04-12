b91bea1d9413ec46a7bfc087fab56182930bd576
Kai Wang <charles.wangkai@gmail.com>
2026-04-12 08:38:46 +0800

    Minimum Distance to Type a Word Using Two Fingers

--- minimum-distance-to-type-a-word-using-two-fingers/Solution.java
+++ minimum-distance-to-type-a-word-using-two-fingers/Solution.java
@@ -1,91 +1,49 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-public class Solution {
-	static final int COLUMN_SIZE = 6;
+class Solution {
+  static final int COLUMN_NUM = 6;
 
-	public int minimumDistance(String word) {
-		Map<State, Integer> stateToDistance = new HashMap<>();
-		stateToDistance.put(new State((char) 0, (char) 0), 0);
+  public int minimumDistance(String word) {
+    Map<State, Integer> dp = new HashMap<>();
+    for (char left = 'A'; left <= 'Z'; ++left) {
+      for (char right = 'A'; right <= 'Z'; ++right) {
+        dp.put(new State(left, right), 0);
+      }
+    }
 
-		for (char current : word.toCharArray()) {
-			List<State> currStates = new ArrayList<>();
-			for (char ch = 'A'; ch <= 'Z'; ++ch) {
-				currStates.add(new State(ch, current));
-				currStates.add(new State((char) 0, current));
+    for (char letter : word.toCharArray()) {
+      Map<State, Integer> nextDp = new HashMap<>();
+      for (State state : dp.keySet()) {
+        for (State nextState :
+            new State[] {new State(letter, state.right()), new State(state.left(), letter)}) {
+          nextDp.put(
+              nextState,
+              Math.min(
+                  nextDp.getOrDefault(nextState, Integer.MAX_VALUE),
+                  dp.get(state) + computeStateDistance(state, nextState)));
+        }
+      }
 
-				currStates.add(new State(current, ch));
-				currStates.add(new State(current, (char) 0));
-			}
+      dp = nextDp;
+    }
 
-			Map<State, Integer> nextStateToDistance = new HashMap<>();
-			for (State currState : currStates) {
-				for (State prevState : stateToDistance.keySet()) {
-					if (canSwitchState(prevState, currState)) {
-						updateMap(nextStateToDistance, currState,
-								stateToDistance.get(prevState) + computeStateDistance(prevState, currState));
-					}
-				}
-			}
+    return dp.values().stream().mapToInt(Integer::intValue).min().getAsInt();
+  }
 
-			stateToDistance = nextStateToDistance;
-		}
+  int computeStateDistance(State state1, State state2) {
+    return computeLetterDistance(state1.left(), state2.left())
+        + computeLetterDistance(state1.right(), state2.right());
+  }
 
-		return stateToDistance.values().stream().mapToInt(x -> x).min().getAsInt();
-	}
+  int toIndex(char letter) {
+    return letter - 'A';
+  }
 
-	boolean canSwitchState(State prevState, State currState) {
-		return !(prevState.left == 0 && prevState.right == 0 && currState.left != 0 && currState.right != 0)
-				&& canSwitchLetter(prevState.left, currState.left) && canSwitchLetter(prevState.right, currState.right);
-	}
-
-	boolean canSwitchLetter(char prevLetter, char currLetter) {
-		return currLetter != 0 || prevLetter == 0;
-	}
-
-	int toIndex(char letter) {
-		return letter - 'A';
-	}
-
-	int computeStateDistance(State prevState, State currState) {
-		return computeLetterDistance(prevState.left, currState.left)
-				+ computeLetterDistance(prevState.right, currState.right);
-	}
-
-	int computeLetterDistance(char prevLetter, char currLetter) {
-		if (prevLetter == 0) {
-			return 0;
-		}
-
-		return Math.abs(toIndex(prevLetter) / COLUMN_SIZE - toIndex(currLetter) / COLUMN_SIZE)
-				+ Math.abs(toIndex(prevLetter) % COLUMN_SIZE - toIndex(currLetter) % COLUMN_SIZE);
-	}
-
-	void updateMap(Map<State, Integer> stateToDistance, State state, int distance) {
-		stateToDistance.put(state, Math.min(stateToDistance.getOrDefault(state, Integer.MAX_VALUE), distance));
-	}
+  int computeLetterDistance(char letter1, char letter2) {
+    return Math.abs(toIndex(letter1) / COLUMN_NUM - toIndex(letter2) / COLUMN_NUM)
+        + Math.abs(toIndex(letter1) % COLUMN_NUM - toIndex(letter2) % COLUMN_NUM);
+  }
 }
 
-class State {
-	char left;
-	char right;
-
-	State(char left, char right) {
-		this.left = left;
-		this.right = right;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(left, right);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		State other = (State) obj;
-		return left == other.left && right == other.right;
-	}
-}
+record State(char left, char right) {}
