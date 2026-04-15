6909cfcf04a2d85df4ea8f09835e92388f3c5e61
Kai Wang <charles.wangkai@gmail.com>
2026-04-15 08:34:41 +0800

    Shortest Distance to Target String in a Circular Array

--- shortest-distance-to-target-string-in-a-circular-array/Solution.java
+++ shortest-distance-to-target-string-in-a-circular-array/Solution.java
@@ -1,7 +1,7 @@
 import java.util.stream.IntStream;
 
 class Solution {
-  public int closetTarget(String[] words, String target, int startIndex) {
+  public int closestTarget(String[] words, String target, int startIndex) {
     return IntStream.range(0, words.length)
         .filter(i -> words[i].equals(target))
         .flatMap(
