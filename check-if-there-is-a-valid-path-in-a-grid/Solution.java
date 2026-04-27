0fe8db271bf7a87b2527a1832f170f014dfe19f0
Kai Wang <charles.wangkai@gmail.com>
2026-04-27 08:57:40 +0800

    Check if There is a Valid Path in a Grid

--- check-if-there-is-a-valid-path-in-a-grid/Solution.java
+++ check-if-there-is-a-valid-path-in-a-grid/Solution.java
@@ -1,39 +1,46 @@
-import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
-    static final int[] R_OFFSETS = { -1, 0, 1, 0 };
-    static final int[] C_OFFSETS = { 0, 1, 0, -1 };
-    static final int[][] CURRENT_VALID_STREETS = { { 2, 5, 6 }, { 1, 4, 6 }, { 2, 3, 4 }, { 1, 3, 5 } };
-    static final int[][] ADJ_VALID_STREETS = { { 2, 3, 4 }, { 1, 3, 5 }, { 2, 5, 6 }, { 1, 4, 6 } };
+  static final int[] R_OFFSETS = {-1, 0, 1, 0};
+  static final int[] C_OFFSETS = {0, 1, 0, -1};
+  static final List<Set<Integer>> CURRENT_VALID_STREETS =
+      List.of(Set.of(2, 5, 6), Set.of(1, 4, 6), Set.of(2, 3, 4), Set.of(1, 3, 5));
+  static final List<Set<Integer>> ADJ_VALID_STREETS =
+      List.of(Set.of(2, 3, 4), Set.of(1, 3, 5), Set.of(2, 5, 6), Set.of(1, 4, 6));
 
-    public boolean hasValidPath(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+  public boolean hasValidPath(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-        return search(grid, new boolean[m][n], 0, 0);
+    return search(grid, new boolean[m][n], 0, 0);
+  }
+
+  boolean search(int[][] grid, boolean[][] visited, int r, int c) {
+    int m = grid.length;
+    int n = grid[0].length;
+
+    if (r == m - 1 && c == n - 1) {
+      return true;
     }
 
-    boolean search(int[][] grid, boolean[][] visited, int r, int c) {
-        int m = grid.length;
-        int n = grid[0].length;
+    visited[r][c] = true;
 
-        visited[r][c] = true;
-        if (r == m - 1 && c == n - 1) {
-            return true;
-        }
-
-        for (int i = 0; i < R_OFFSETS.length; ++i) {
-            int adjR = r + R_OFFSETS[i];
-            int adjC = c + C_OFFSETS[i];
-
-            if (adjR >= 0 && adjR < m && adjC >= 0 && adjC < n && !visited[adjR][adjC]
-                    && Arrays.stream(CURRENT_VALID_STREETS[i]).anyMatch(x -> x == grid[r][c])
-                    && Arrays.stream(ADJ_VALID_STREETS[i]).anyMatch(x -> x == grid[adjR][adjC])
-                    && search(grid, visited, adjR, adjC)) {
-                return true;
-            }
-        }
-
-        return false;
+    for (int i = 0; i < R_OFFSETS.length; ++i) {
+      int adjR = r + R_OFFSETS[i];
+      int adjC = c + C_OFFSETS[i];
+      if (adjR >= 0
+          && adjR < m
+          && adjC >= 0
+          && adjC < n
+          && !visited[adjR][adjC]
+          && CURRENT_VALID_STREETS.get(i).contains(grid[r][c])
+          && ADJ_VALID_STREETS.get(i).contains(grid[adjR][adjC])
+          && search(grid, visited, adjR, adjC)) {
+        return true;
+      }
     }
+
+    return false;
+  }
 }
