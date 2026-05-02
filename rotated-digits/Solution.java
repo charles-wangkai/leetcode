471fe282bc9a10f9cad7ed2c7cbe17cf7b93d318
Kai Wang <charles.wangkai@gmail.com>
2026-05-02 09:13:51 +0800

    Rotated Digits

--- rotated-digits/Solution.java
+++ rotated-digits/Solution.java
@@ -1,34 +1,34 @@
-import java.util.HashMap;
+import static java.util.Map.entry;
+
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public class Solution {
-  static final Map<Integer, Integer> DIGIT_TO_ROTATED = new HashMap<>();
-
-  static {
-    DIGIT_TO_ROTATED.put(0, 0);
-    DIGIT_TO_ROTATED.put(1, 1);
-    DIGIT_TO_ROTATED.put(8, 8);
-    DIGIT_TO_ROTATED.put(2, 5);
-    DIGIT_TO_ROTATED.put(5, 2);
-    DIGIT_TO_ROTATED.put(6, 9);
-    DIGIT_TO_ROTATED.put(9, 6);
-  }
+class Solution {
+  static final Map<Integer, Integer> DIGIT_TO_ROTATED =
+      Map.ofEntries(
+          entry(0, 0),
+          entry(1, 1),
+          entry(8, 8),
+          entry(2, 5),
+          entry(5, 2),
+          entry(6, 9),
+          entry(9, 6));
 
   public int rotatedDigits(int N) {
     return (int) IntStream.rangeClosed(1, N).filter(this::isGood).count();
   }
 
   boolean isGood(int n) {
-    String s = String.valueOf(n);
     StringBuilder rotated = new StringBuilder();
-    for (char ch : s.toCharArray()) {
-      int digit = ch - '0';
+    for (char c : String.valueOf(n).toCharArray()) {
+      int digit = c - '0';
       if (!DIGIT_TO_ROTATED.containsKey(digit)) {
         return false;
       }
+
       rotated.append(DIGIT_TO_ROTATED.get(digit));
     }
+
     return Integer.parseInt(rotated.toString()) != n;
   }
 }
