6228d8da94e804a2ee561c7cf95cae9bd4d68c07
Kai Wang <charles.wangkai@gmail.com>
2026-05-24 19:40:30 +0800

    Jump Game V

--- jump-game-v/Solution.java
+++ jump-game-v/Solution.java
@@ -1,33 +1,38 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
-public class Solution {
-	static final int[] OFFSETS = { -1, 1 };
+class Solution {
+  static final int[] DIRECTIONS = {-1, 1};
 
-	public int maxJumps(int[] arr, int d) {
-		int[] indices = IntStream.range(0, arr.length).boxed().sorted((i1, i2) -> Integer.compare(arr[i1], arr[i2]))
-				.mapToInt(x -> x).toArray();
+  public int maxJumps(int[] arr, int d) {
+    int[] indices =
+        IntStream.range(0, arr.length)
+            .boxed()
+            .sorted(Comparator.comparing(i -> arr[i]))
+            .mapToInt(Integer::intValue)
+            .toArray();
 
-		int[] visitNums = new int[arr.length];
-		Arrays.fill(visitNums, 1);
+    int[] dp = new int[arr.length];
+    Arrays.fill(dp, 1);
 
-		for (int index : indices) {
-			for (int offset : OFFSETS) {
-				int max = arr[index];
-				for (int i = 1; i <= d; ++i) {
-					int nextIndex = index + offset * i;
-					if (nextIndex < 0 || nextIndex >= arr.length) {
-						break;
-					}
+    for (int index : indices) {
+      for (int direction : DIRECTIONS) {
+        int maxValue = arr[index];
+        for (int i = 1; i <= d; ++i) {
+          int nextIndex = index + direction * i;
+          if (nextIndex < 0 || nextIndex >= arr.length) {
+            break;
+          }
 
-					if (arr[nextIndex] > max) {
-						visitNums[nextIndex] = Math.max(visitNums[nextIndex], visitNums[index] + 1);
-						max = arr[nextIndex];
-					}
-				}
-			}
-		}
+          if (arr[nextIndex] > maxValue) {
+            dp[nextIndex] = Math.max(dp[nextIndex], dp[index] + 1);
+            maxValue = arr[nextIndex];
+          }
+        }
+      }
+    }
 
-		return Arrays.stream(visitNums).max().getAsInt();
-	}
+    return Arrays.stream(dp).max().getAsInt();
+  }
 }
