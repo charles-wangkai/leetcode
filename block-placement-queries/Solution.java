058d0d0d32f93ef387b14e773f22298d6f0a98d3
Kai Wang <charles.wangkai@gmail.com>
2026-05-30 14:02:56 +0800

    Block Placement Queries

--- block-placement-queries/Solution.java
+++ block-placement-queries/Solution.java
@@ -4,23 +4,68 @@ import java.util.List;
 
 class Solution {
   public List<Boolean> getResults(int[][] queries) {
+    SegTree segTree =
+        new SegTree(Arrays.stream(queries).mapToInt(query -> query[1]).max().getAsInt());
+
     List<Boolean> result = new ArrayList<>();
-    Node segmentTree =
-        buildNode(0, Arrays.stream(queries).mapToInt(query -> query[1]).max().getAsInt());
     for (int[] query : queries) {
       if (query[0] == 1) {
-        update(query[1], segmentTree);
+        segTree.update(query[1]);
       } else {
-        result.add(findMaxSize(segmentTree, query[1]) >= query[2]);
+        result.add(segTree.query(query[1]) >= query[2]);
       }
     }
 
     return result;
   }
+}
 
-  int findMaxSize(Node segmentTree, int x) {
+class SegTree {
+  Node root;
+
+  SegTree(int maxIndex) {
+    root = buildNode(0, maxIndex);
+  }
+
+  private Node buildNode(int beginIndex, int endIndex) {
+    Node node = new Node(beginIndex, endIndex);
+
+    if (beginIndex == endIndex) {
+      node.maxSize = 0;
+      node.minObstacle = -1;
+      node.maxObstacle = -1;
+    } else {
+      int middleIndex = (beginIndex + endIndex) / 2;
+      node.left = buildNode(beginIndex, middleIndex);
+      node.right = buildNode(middleIndex + 1, endIndex);
+
+      node.pull();
+    }
+
+    return node;
+  }
+
+  void update(int x) {
+    update(x, root);
+  }
+
+  private void update(int x, Node node) {
+    if (node.beginIndex <= x && node.endIndex >= x) {
+      if (node.beginIndex == node.endIndex) {
+        node.minObstacle = x;
+        node.maxObstacle = x;
+      } else {
+        update(x, node.left);
+        update(x, node.right);
+
+        node.pull();
+      }
+    }
+  }
+
+  int query(int x) {
     List<Node> contained = new ArrayList<>();
-    search(contained, x, segmentTree);
+    search(contained, x, root);
 
     int result = -1;
     int lastObstacle = 0;
@@ -37,78 +82,50 @@ class Solution {
     return result;
   }
 
-  void search(List<Node> contained, int x, Node node) {
-    if (node.end <= x) {
+  private void search(List<Node> contained, int x, Node node) {
+    if (node.endIndex <= x) {
       contained.add(node);
-    } else if (node.begin <= x) {
+    } else if (node.beginIndex <= x) {
       search(contained, x, node.left);
       search(contained, x, node.right);
     }
   }
 
-  void update(int x, Node node) {
-    if (node.begin <= x && node.end >= x) {
-      if (node.begin == node.end) {
-        node.minObstacle = x;
-        node.maxObstacle = x;
-      } else {
-        update(x, node.left);
-        update(x, node.right);
+  static class Node {
+    int beginIndex;
+    int endIndex;
+    int maxSize;
+    int minObstacle;
+    int maxObstacle;
+    Node left;
+    Node right;
 
-        if (node.left.minObstacle == -1) {
-          node.maxSize =
-              Math.max(
-                  Math.max(node.left.maxSize, node.right.maxSize),
-                  node.right.minObstacle - node.left.begin);
-          node.minObstacle = node.right.minObstacle;
-          node.maxObstacle = node.right.maxObstacle;
-        } else if (node.right.minObstacle == -1) {
-          node.maxSize =
-              Math.max(
-                  Math.max(node.left.maxSize, node.right.maxSize),
-                  node.right.end - node.left.maxObstacle);
-          node.minObstacle = node.left.minObstacle;
-          node.maxObstacle = node.left.maxObstacle;
-        } else {
-          node.maxSize =
-              Math.max(
-                  Math.max(node.left.maxSize, node.right.maxSize),
-                  node.right.minObstacle - node.left.maxObstacle);
-          node.minObstacle = node.left.minObstacle;
-          node.maxObstacle = node.right.maxObstacle;
-        }
+    Node(int beginIndex, int endIndex) {
+      this.beginIndex = beginIndex;
+      this.endIndex = endIndex;
+    }
+
+    void pull() {
+      if (left.minObstacle == -1) {
+        maxSize =
+            Math.max(
+                Math.max(left.maxSize, right.maxSize),
+                (right.minObstacle == -1)
+                    ? Integer.MIN_VALUE
+                    : (right.minObstacle - left.beginIndex));
+        minObstacle = right.minObstacle;
+        maxObstacle = right.maxObstacle;
+      } else if (right.minObstacle == -1) {
+        maxSize =
+            Math.max(Math.max(left.maxSize, right.maxSize), right.endIndex - left.maxObstacle);
+        minObstacle = left.minObstacle;
+        maxObstacle = left.maxObstacle;
+      } else {
+        maxSize =
+            Math.max(Math.max(left.maxSize, right.maxSize), right.minObstacle - left.maxObstacle);
+        minObstacle = left.minObstacle;
+        maxObstacle = right.maxObstacle;
       }
     }
-  }
-
-  Node buildNode(int begin, int end) {
-    if (begin == end) {
-      return new Node(begin, end, 0, -1, -1, null, null);
-    }
-
-    int middle = (begin + end) / 2;
-
-    return new Node(
-        begin, end, end - begin, -1, -1, buildNode(begin, middle), buildNode(middle + 1, end));
-  }
-}
-
-class Node {
-  int begin;
-  int end;
-  int maxSize;
-  int minObstacle;
-  int maxObstacle;
-  Node left;
-  Node right;
-
-  Node(int begin, int end, int maxSize, int minObstacle, int maxObstacle, Node left, Node right) {
-    this.begin = begin;
-    this.end = end;
-    this.maxSize = maxSize;
-    this.minObstacle = minObstacle;
-    this.maxObstacle = maxObstacle;
-    this.left = left;
-    this.right = right;
   }
 }
