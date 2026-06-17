05fe25194879e6869eb3dd1fd2e4a5364a88fadf
Kai Wang <charles.wangkai@gmail.com>
2026-06-17 08:39:20 +0800

    Process String with Special Operations II

--- process-string-with-special-operations-ii/Solution.java
+++ process-string-with-special-operations-ii/Solution.java
@@ -10,29 +10,24 @@ class Solution {
         ++length;
       }
     }
-
-    return search(s, s.length(), length, k);
-  }
-
-  char search(String s, int processed, long length, long k) {
     if (k >= length) {
       return '.';
     }
 
-    char c = s.charAt(processed - 1);
-    if (c == '*') {
-      return search(s, processed - 1, length + 1, k);
+    for (int i = s.length() - 1; ; --i) {
+      char c = s.charAt(i);
+      if (c == '*') {
+        ++length;
+      } else if (c == '#') {
+        length /= 2;
+        k %= length;
+      } else if (c == '%') {
+        k = length - 1 - k;
+      } else if (k == length - 1) {
+        return c;
+      } else {
+        --length;
+      }
     }
-    if (c == '#') {
-      return search(s, processed - 1, length / 2, k % (length / 2));
-    }
-    if (c == '%') {
-      return search(s, processed - 1, length, length - 1 - k);
-    }
-    if (k == length - 1) {
-      return c;
-    }
-
-    return search(s, processed - 1, length - 1, k);
   }
 }
