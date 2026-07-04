29a32d1c142b1e4e0f8fde7c351219a5e4a492f2
Kai Wang <charles.wangkai@gmail.com>
2026-07-04 17:23:55 +0800

    Minimum Score of a Path Between Two Cities

--- minimum-score-of-a-path-between-two-cities/Solution.java
+++ minimum-score-of-a-path-between-two-cities/Solution.java
@@ -1,35 +1,63 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
   public int minScore(int n, int[][] roads) {
-    @SuppressWarnings("unchecked")
-    List<Integer>[] adjLists = new List[n];
-    for (int i = 0; i < adjLists.length; ++i) {
-      adjLists[i] = new ArrayList<>();
-    }
+    Dsu dsu = new Dsu(n);
     for (int[] road : roads) {
-      adjLists[road[0] - 1].add(road[1] - 1);
-      adjLists[road[1] - 1].add(road[0] - 1);
+      dsu.union(road[0] - 1, road[1] - 1);
     }
-
-    boolean[] visited = new boolean[n];
-    search(adjLists, visited, 0);
 
     return Arrays.stream(roads)
-        .filter(road -> visited[road[0] - 1] && visited[road[1] - 1])
+        .filter(road -> dsu.find(road[0] - 1) == dsu.find(0))
         .mapToInt(road -> road[2])
         .min()
         .getAsInt();
   }
+}
 
-  void search(List<Integer>[] adjLists, boolean[] visited, int node) {
-    visited[node] = true;
-    for (int adj : adjLists[node]) {
-      if (!visited[adj]) {
-        search(adjLists, visited, adj);
-      }
+class Dsu {
+  int[] parentOrSizes;
+
+  Dsu(int n) {
+    parentOrSizes = new int[n];
+    Arrays.fill(parentOrSizes, -1);
+  }
+
+  int find(int a) {
+    if (parentOrSizes[a] < 0) {
+      return a;
     }
+
+    parentOrSizes[a] = find(parentOrSizes[a]);
+
+    return parentOrSizes[a];
+  }
+
+  void union(int a, int b) {
+    int aLeader = find(a);
+    int bLeader = find(b);
+    if (aLeader != bLeader) {
+      parentOrSizes[aLeader] += parentOrSizes[bLeader];
+      parentOrSizes[bLeader] = aLeader;
+    }
+  }
+
+  int getSize(int a) {
+    return -parentOrSizes[find(a)];
+  }
+
+  Map<Integer, List<Integer>> buildLeaderToGroup() {
+    Map<Integer, List<Integer>> leaderToGroup = new HashMap<>();
+    for (int i = 0; i < parentOrSizes.length; ++i) {
+      int leader = find(i);
+      leaderToGroup.putIfAbsent(leader, new ArrayList<>());
+      leaderToGroup.get(leader).add(i);
+    }
+
+    return leaderToGroup;
   }
 }
