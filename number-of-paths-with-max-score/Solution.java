1e0f009fe616f617a0b43278b1fd9e7ef975c3b9
Kai Wang <charles.wangkai@gmail.com>
2026-07-05 17:06:27 +0800

    Number of Paths with Max Score

--- number-of-paths-with-max-score/Solution.java
+++ number-of-paths-with-max-score/Solution.java
@@ -1,68 +1,96 @@
+import java.math.BigInteger;
 import java.util.List;
 
-public class Solution {
-	static final int MODULUS = 1_000_000_007;
-	static final int[] R_OFFSETS = { 0, 1, 1 };
-	static final int[] C_OFFSETS = { 1, 0, 1 };
+class Solution {
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
+  static final int[] R_OFFSETS = {0, 1, 1};
+  static final int[] C_OFFSETS = {1, 0, 1};
 
-	public int[] pathsWithMaxScore(List<String> board) {
-		int size = board.size();
+  public int[] pathsWithMaxScore(List<String> board) {
+    int size = board.size();
 
-		Element[][] elements = new Element[size][size];
-		for (int r = size - 1; r >= 0; --r) {
-			for (int c = size - 1; c >= 0; --c) {
-				char ch = board.get(r).charAt(c);
+    Element[][] elements = new Element[size][size];
+    for (int r = size - 1; r >= 0; --r) {
+      for (int c = size - 1; c >= 0; --c) {
+        char square = board.get(r).charAt(c);
 
-				int maxSum;
-				int pathNum;
-				if (ch == 'S') {
-					maxSum = 0;
-					pathNum = 1;
-				} else {
-					maxSum = -1;
-					pathNum = 0;
+        int maxSum;
+        int pathNum;
+        if (square == 'S') {
+          maxSum = 0;
+          pathNum = 1;
+        } else {
+          maxSum = -1;
+          pathNum = 0;
 
-					if (ch != 'X') {
-						for (int i = 0; i < R_OFFSETS.length; ++i) {
-							int adjR = r + R_OFFSETS[i];
-							int adjC = c + C_OFFSETS[i];
-							if (adjR < size && adjC < size) {
-								maxSum = Math.max(maxSum, elements[adjR][adjC].maxSum);
-							}
-						}
+          if (square != 'X') {
+            for (int i = 0; i < R_OFFSETS.length; ++i) {
+              int adjR = r + R_OFFSETS[i];
+              int adjC = c + C_OFFSETS[i];
+              if (adjR < size && adjC < size) {
+                maxSum = Math.max(maxSum, elements[adjR][adjC].maxSum());
+              }
+            }
 
-						for (int i = 0; i < R_OFFSETS.length; ++i) {
-							int adjR = r + R_OFFSETS[i];
-							int adjC = c + C_OFFSETS[i];
-							if (adjR < size && adjC < size && elements[adjR][adjC].maxSum == maxSum) {
-								pathNum = addMod(pathNum, elements[adjR][adjC].pathNum);
-							}
-						}
+            for (int i = 0; i < R_OFFSETS.length; ++i) {
+              int adjR = r + R_OFFSETS[i];
+              int adjC = c + C_OFFSETS[i];
+              if (adjR < size && adjC < size && elements[adjR][adjC].maxSum() == maxSum) {
+                pathNum = MOD_INT.addMod(pathNum, elements[adjR][adjC].pathNum());
+              }
+            }
 
-						if (maxSum != -1) {
-							maxSum += (ch >= '1' && ch <= '9') ? (ch - '0') : 0;
-						}
-					}
-				}
+            if (maxSum != -1 && Character.isDigit(square)) {
+              maxSum += square - '0';
+            }
+          }
+        }
 
-				elements[r][c] = new Element(maxSum, pathNum);
-			}
-		}
+        elements[r][c] = new Element(maxSum, pathNum);
+      }
+    }
 
-		return new int[] { Math.max(0, elements[0][0].maxSum), elements[0][0].pathNum };
-	}
-
-	int addMod(int x, int y) {
-		return (x + y) % MODULUS;
-	}
+    return (elements[0][0].maxSum() == -1)
+        ? new int[] {0, 0}
+        : new int[] {elements[0][0].maxSum(), elements[0][0].pathNum()};
+  }
 }
 
-class Element {
-	int maxSum;
-	int pathNum;
+record Element(int maxSum, int pathNum) {}
 
-	Element(int maxSum, int pathNum) {
-		this.maxSum = maxSum;
-		this.pathNum = pathNum;
-	}
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
+  }
+
+  int addMod(int x, int y) {
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
+  }
 }
