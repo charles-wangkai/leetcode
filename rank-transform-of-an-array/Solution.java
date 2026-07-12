5c148d6b760ad728107dfb98ed44b1fff40d0c2a
Kai Wang <charles.wangkai@gmail.com>
2026-07-12 08:06:32 +0800

    Rank Transform of an Array

--- rank-transform-of-an-array/Solution.java
+++ rank-transform-of-an-array/Solution.java
@@ -1,24 +1,16 @@
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
   public int[] arrayRankTransform(int[] arr) {
-    int[] sortedIndices =
-        IntStream.range(0, arr.length)
+    int[] sortedValue = Arrays.stream(arr).sorted().distinct().toArray();
+    Map<Integer, Integer> valueToIndex =
+        IntStream.range(0, sortedValue.length)
             .boxed()
-            .sorted(Comparator.comparing(i -> arr[i]))
-            .mapToInt(Integer::intValue)
-            .toArray();
+            .collect(Collectors.toMap(i -> sortedValue[i], i -> i));
 
-    int[] result = new int[arr.length];
-    for (int i = 0; i < sortedIndices.length; ++i) {
-      result[sortedIndices[i]] =
-          (i == 0)
-              ? 1
-              : (result[sortedIndices[i - 1]]
-                  + ((arr[sortedIndices[i]] == arr[sortedIndices[i - 1]]) ? 0 : 1));
-    }
-
-    return result;
+    return Arrays.stream(arr).map(value -> valueToIndex.get(value) + 1).toArray();
   }
 }
