dc3a8973e75d3fd7e7b97d151e6c85867fc4779c
Kai Wang <charles.wangkai@gmail.com>
2026-07-13 13:39:14 +0800

    Sequential Digits

--- sequential-digits/Solution.java
+++ sequential-digits/Solution.java
@@ -1,19 +1,18 @@
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 class Solution {
   static final String DIGITS = "123456789";
-  static final int[] SEQUENTIALS =
-      IntStream.range(0, DIGITS.length())
-          .flatMap(
-              i ->
-                  IntStream.range(i + 1, DIGITS.length())
-                      .map(j -> Integer.parseInt(DIGITS.substring(i, j + 1))))
-          .sorted()
-          .toArray();
 
   public List<Integer> sequentialDigits(int low, int high) {
-    return Arrays.stream(SEQUENTIALS).filter(x -> x >= low && x <= high).boxed().toList();
+    return IntStream.range(0, DIGITS.length())
+        .flatMap(
+            beginIndex ->
+                IntStream.range(beginIndex, DIGITS.length())
+                    .map(endIndex -> Integer.parseInt(DIGITS.substring(beginIndex, endIndex + 1))))
+        .filter(x -> x >= low && x <= high)
+        .sorted()
+        .boxed()
+        .toList();
   }
 }
