3f04f22824e507a2fb91d7452e33d9fd6717e5e9
Kai Wang <charles.wangkai@gmail.com>
2026-07-19 21:03:42 +0800

    Smallest Subsequence of Distinct Characters

--- smallest-subsequence-of-distinct-characters/Solution.java
+++ smallest-subsequence-of-distinct-characters/Solution.java
@@ -1,32 +1,36 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
-	public String smallestSubsequence(String text) {
-		if (text.isEmpty()) {
-			return "";
-		}
+class Solution {
+  public String smallestSubsequence(String s) {
+    if (s.isEmpty()) {
+      return "";
+    }
 
-		Map<Character, Integer> letterToCount = new HashMap<>();
-		for (char letter : text.toCharArray()) {
-			letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
-		}
+    Map<Character, Integer> letterToCount = new HashMap<>();
+    for (char letter : s.toCharArray()) {
+      letterToCount.put(letter, letterToCount.getOrDefault(letter, 0) + 1);
+    }
 
-		int selectedIndex = 0;
-		for (int i = 0;; i++) {
-			char letter = text.charAt(i);
-			if (letter < text.charAt(selectedIndex)) {
-				selectedIndex = i;
-			}
+    int selectedIndex = 0;
+    for (int i = 0; ; ++i) {
+      char letter = s.charAt(i);
+      if (letter < s.charAt(selectedIndex)) {
+        selectedIndex = i;
+      }
 
-			if (letterToCount.get(letter) == 1) {
-				break;
-			}
+      if (letterToCount.get(letter) == 1) {
+        break;
+      }
 
-			letterToCount.put(letter, letterToCount.get(letter) - 1);
-		}
+      letterToCount.put(letter, letterToCount.get(letter) - 1);
+    }
 
-		return String.format("%c%s", text.charAt(selectedIndex), smallestSubsequence(
-				text.substring(selectedIndex + 1).replaceAll(String.valueOf(text.charAt(selectedIndex)), "")));
-	}
+    return "%c%s"
+        .formatted(
+            s.charAt(selectedIndex),
+            smallestSubsequence(
+                s.substring(selectedIndex + 1)
+                    .replace(String.valueOf(s.charAt(selectedIndex)), "")));
+  }
 }
