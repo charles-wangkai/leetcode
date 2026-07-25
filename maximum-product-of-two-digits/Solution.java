cd8d7292899810425c6e85c925b6ec5dc5929104
Kai Wang <charles.wangkai@gmail.com>
2026-07-25 08:12:47 +0800

    Maximum Product of Two Digits

--- maximum-product-of-two-digits/Solution.java
+++ maximum-product-of-two-digits/Solution.java
@@ -1,14 +1,14 @@
-import java.util.Comparator;
-
 class Solution {
   public int maxProduct(int n) {
-    return String.valueOf(n)
-        .chars()
-        .boxed()
-        .sorted(Comparator.reverseOrder())
-        .mapToInt(c -> c - '0')
-        .limit(2)
-        .reduce((acc, x) -> acc * x)
-        .getAsInt();
+    int[] digits = String.valueOf(n).chars().map(c -> c - '0').toArray();
+
+    int result = -1;
+    for (int i = 0; i < digits.length; ++i) {
+      for (int j = i + 1; j < digits.length; ++j) {
+        result = Math.max(result, digits[i] * digits[j]);
+      }
+    }
+
+    return result;
   }
 }
