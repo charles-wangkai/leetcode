eca46158d0c9bc453e310d7344545736fa583e7f
Kai Wang <charles.wangkai@gmail.com>
2026-07-26 08:21:29 +0800

    Maximum Product of Three Numbers

--- maximum-product-of-three-numbers/Solution.java
+++ maximum-product-of-three-numbers/Solution.java
@@ -1,51 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-public class Solution {
+class Solution {
   public int maximumProduct(int[] nums) {
+    Arrays.sort(nums);
+
+    int[] candidates =
+        IntStream.range(0, nums.length)
+            .filter(i -> i < 3 || i >= nums.length - 3)
+            .map(i -> nums[i])
+            .toArray();
+
     int result = Integer.MIN_VALUE;
-
-    List<Integer> negatives = new ArrayList<>();
-    List<Integer> positives = new ArrayList<>();
-    for (int num : nums) {
-      if (num == 0) {
-        result = Math.max(result, 0);
-      } else if (num < 0) {
-        negatives.add(num);
-      } else {
-        positives.add(num);
+    for (int i = 0; i < candidates.length; ++i) {
+      for (int j = i + 1; j < candidates.length; ++j) {
+        for (int k = j + 1; k < candidates.length; ++k) {
+          result = Math.max(result, candidates[i] * candidates[j] * candidates[k]);
+        }
       }
-    }
-
-    Collections.sort(negatives);
-    Collections.sort(positives);
-
-    if (positives.size() >= 3) {
-      result =
-          Math.max(
-              result,
-              positives.get(positives.size() - 3)
-                  * positives.get(positives.size() - 2)
-                  * positives.get(positives.size() - 1));
-    }
-    if (!negatives.isEmpty() && positives.size() >= 2) {
-      result =
-          Math.max(
-              result, negatives.get(negatives.size() - 1) * positives.get(0) * positives.get(1));
-    }
-    if (negatives.size() >= 2 && !positives.isEmpty()) {
-      result =
-          Math.max(
-              result, negatives.get(0) * negatives.get(1) * positives.get(positives.size() - 1));
-    }
-    if (negatives.size() >= 3) {
-      result =
-          Math.max(
-              result,
-              negatives.get(negatives.size() - 3)
-                  * negatives.get(negatives.size() - 2)
-                  * negatives.get(negatives.size() - 1));
     }
 
     return result;
