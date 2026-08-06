84b79ce0354a5625df0b487450a10bbc38546692
Kai Wang <charles.wangkai@gmail.com>
2026-08-06 08:31:39 +0800

    Smallest Divisible Digit Product I

--- smallest-divisible-digit-product-i/Solution.java
+++ smallest-divisible-digit-product-i/Solution.java
@@ -1,12 +1,12 @@
+import java.util.stream.IntStream;
+
 class Solution {
   public int smallestNumber(int n, int t) {
-    int result = n;
-    while (String.valueOf(result).chars().map(c -> c - '0').reduce((acc, x) -> acc * x).getAsInt()
-            % t
-        != 0) {
-      ++result;
-    }
-
-    return result;
+    return IntStream.iterate(n, x -> x + 1)
+        .filter(
+            x ->
+                String.valueOf(x).chars().map(c -> c - '0').reduce(1, (acc, e) -> acc * e) % t == 0)
+        .findFirst()
+        .getAsInt();
   }
 }
