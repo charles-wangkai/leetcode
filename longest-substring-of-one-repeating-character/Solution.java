d62512c1e1ba14abaabb7f17a7efef1c7016bbfb
Kai Wang <charles.wangkai@gmail.com>
2026-08-13 13:11:15 +0800

    Longest Substring of One Repeating Character

--- longest-substring-of-one-repeating-character/Solution.java
+++ longest-substring-of-one-repeating-character/Solution.java
@@ -1,7 +1,6 @@
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.NavigableSet;
-import java.util.Objects;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -9,7 +8,7 @@ import java.util.TreeSet;
 
 class Solution {
   public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
-    NavigableSet<Range> ranges = new TreeSet<>(Comparator.comparing(range -> range.left));
+    NavigableSet<Range> ranges = new TreeSet<>(Comparator.comparing(Range::left));
     SortedMap<Integer, Set<Range>> lengthToRanges = new TreeMap<>();
 
     int left = 0;
@@ -28,25 +27,27 @@ class Solution {
       char newLetter = queryCharacters.charAt(i);
 
       Range range = ranges.floor(new Range(queryIndices[i], -1, (char) 0));
-      if (range.letter != newLetter) {
+      if (range.letter() != newLetter) {
         ranges.remove(range);
         removeFromMap(lengthToRanges, range);
 
-        if (range.left != queryIndices[i]) {
-          Range lRange = new Range(range.left, queryIndices[i] - 1, range.letter);
+        if (range.left() != queryIndices[i]) {
+          Range lRange = new Range(range.left(), queryIndices[i] - 1, range.letter());
           ranges.add(lRange);
           addToMap(lengthToRanges, lRange);
         }
-        if (range.right != queryIndices[i]) {
-          Range rRange = new Range(queryIndices[i] + 1, range.right, range.letter);
+        if (range.right() != queryIndices[i]) {
+          Range rRange = new Range(queryIndices[i] + 1, range.right(), range.letter());
           ranges.add(rRange);
           addToMap(lengthToRanges, rRange);
         }
 
         int newLeft = queryIndices[i];
         Range leftRange = ranges.lower(range);
-        if (leftRange != null && leftRange.right == newLeft - 1 && leftRange.letter == newLetter) {
-          newLeft = leftRange.left;
+        if (leftRange != null
+            && leftRange.right() == queryIndices[i] - 1
+            && leftRange.letter() == newLetter) {
+          newLeft = leftRange.left();
 
           ranges.remove(leftRange);
           removeFromMap(lengthToRanges, leftRange);
@@ -55,9 +56,9 @@ class Solution {
         int newRight = queryIndices[i];
         Range rightRange = ranges.higher(range);
         if (rightRange != null
-            && rightRange.left == newRight + 1
-            && rightRange.letter == newLetter) {
-          newRight = rightRange.right;
+            && rightRange.left() == queryIndices[i] + 1
+            && rightRange.letter() == newLetter) {
+          newRight = rightRange.right();
 
           ranges.remove(rightRange);
           removeFromMap(lengthToRanges, rightRange);
@@ -75,42 +76,18 @@ class Solution {
   }
 
   void addToMap(SortedMap<Integer, Set<Range>> lengthToRanges, Range range) {
-    int length = range.getLength();
-    lengthToRanges.putIfAbsent(length, new HashSet<>());
-    lengthToRanges.get(length).add(range);
+    lengthToRanges.putIfAbsent(range.getLength(), new HashSet<>());
+    lengthToRanges.get(range.getLength()).add(range);
   }
 
   void removeFromMap(SortedMap<Integer, Set<Range>> lengthToRanges, Range range) {
-    int length = range.getLength();
-    lengthToRanges.get(length).remove(range);
-    lengthToRanges.remove(length, Set.of());
+    lengthToRanges.get(range.getLength()).remove(range);
+    lengthToRanges.remove(range.getLength(), Set.of());
   }
 }
 
-class Range {
-  int left;
-  int right;
-  char letter;
-
-  Range(int left, int right, char letter) {
-    this.left = left;
-    this.right = right;
-    this.letter = letter;
-  }
-
+record Range(int left, int right, char letter) {
   int getLength() {
     return right - left + 1;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(left, right, letter);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    Range other = (Range) obj;
-
-    return left == other.left && right == other.right && letter == other.letter;
   }
 }
