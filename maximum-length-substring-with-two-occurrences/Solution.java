305a88fe648d12d22c882a8e6be4f918fed411dd
Kai Wang <charles.wangkai@gmail.com>
2026-08-14 08:10:15 +0800

    Maximum Length Substring With Two Occurrences

--- maximum-length-substring-with-two-occurrences/Solution.java
+++ maximum-length-substring-with-two-occurrences/Solution.java
@@ -1,15 +1,15 @@
-import java.util.stream.IntStream;
-
 class Solution {
   public int maximumLengthSubstring(String s) {
     int result = 0;
     for (int beginIndex = 0; beginIndex < s.length(); ++beginIndex) {
+      int[] counts = new int[26];
       for (int endIndex = beginIndex; endIndex < s.length(); ++endIndex) {
-        String substr = s.substring(beginIndex, endIndex + 1);
-        if (IntStream.rangeClosed('a', 'z')
-            .allMatch(c -> substr.chars().filter(ch -> ch == c).count() <= 2)) {
-          result = Math.max(result, endIndex - beginIndex + 1);
+        ++counts[s.charAt(endIndex) - 'a'];
+        if (counts[s.charAt(endIndex) - 'a'] == 3) {
+          break;
         }
+
+        result = Math.max(result, endIndex - beginIndex + 1);
       }
     }
 
