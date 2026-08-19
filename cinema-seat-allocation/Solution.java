0ddbc92fa159d1544d38fcd5b091d32494aec741
Kai Wang <charles.wangkai@gmail.com>
2026-08-19 13:03:50 +0800

    Cinema Seat Allocation

--- cinema-seat-allocation/Solution.java
+++ cinema-seat-allocation/Solution.java
@@ -5,28 +5,32 @@ import java.util.Set;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, Set<Integer>> rowToColumns = new HashMap<>();
-        for (int[] seat : reservedSeats) {
-            if (!rowToColumns.containsKey(seat[0])) {
-                rowToColumns.put(seat[0], new HashSet<>());
-            }
-
-            rowToColumns.get(seat[0]).add(seat[1]);
-        }
-
-        return (n - rowToColumns.size()) * 2 + rowToColumns.values().stream().mapToInt(columns -> {
-            if (isAvailable(columns, 2, 9)) {
-                return 2;
-            } else if (isAvailable(columns, 2, 5) || isAvailable(columns, 6, 9) || isAvailable(columns, 4, 7)) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }).sum();
+  public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+    Map<Integer, Set<Integer>> rowToReserved = new HashMap<>();
+    for (int[] seat : reservedSeats) {
+      rowToReserved.putIfAbsent(seat[0], new HashSet<>());
+      rowToReserved.get(seat[0]).add(seat[1]);
     }
 
-    boolean isAvailable(Set<Integer> columns, int begin, int end) {
-        return IntStream.rangeClosed(begin, end).allMatch(x -> !columns.contains(x));
-    }
+    return (n - rowToReserved.size()) * 2
+        + rowToReserved.values().stream()
+            .mapToInt(
+                columns -> {
+                  if (isAvailable(columns, 2, 9)) {
+                    return 2;
+                  }
+                  if (isAvailable(columns, 2, 5)
+                      || isAvailable(columns, 4, 7)
+                      || isAvailable(columns, 6, 9)) {
+                    return 1;
+                  }
+
+                  return 0;
+                })
+            .sum();
+  }
+
+  boolean isAvailable(Set<Integer> reserved, int begin, int end) {
+    return IntStream.rangeClosed(begin, end).allMatch(x -> !reserved.contains(x));
+  }
 }
