0aeedf1d8b55430e10ed08a0bde519997b1f42f3
Kai Wang <charles.wangkai@gmail.com>
2026-08-27 09:16:02 +0800

    Lexicographically Smallest Permutation Greater Than Target

--- lexicographically-smallest-permutation-greater-than-target/Solution.java
+++ lexicographically-smallest-permutation-greater-than-target/Solution.java
@@ -9,17 +9,16 @@ class Solution {
       ++counts[c - 'a'];
     }
 
-    String result = null;
+    String result = "";
     for (int i = 0; i < target.length(); ++i) {
-      int i_ = i;
       OptionalInt greater =
-          IntStream.range(0, counts.length)
-              .filter(j -> j > target.charAt(i_) - 'a' && counts[j] != 0)
+          IntStream.range(target.charAt(i) - 'a' + 1, counts.length)
+              .filter(j -> counts[j] != 0)
               .findFirst();
       if (greater.isPresent()) {
         --counts[greater.getAsInt()];
 
-        String candidate =
+        result =
             "%s%c%s"
                 .formatted(
                     target.substring(0, i),
@@ -27,9 +26,6 @@ class Solution {
                     IntStream.range(0, counts.length)
                         .mapToObj(j -> String.valueOf((char) ('a' + j)).repeat(counts[j]))
                         .collect(Collectors.joining()));
-        if (result == null || candidate.compareTo(result) < 0) {
-          result = candidate;
-        }
 
         ++counts[greater.getAsInt()];
       }
@@ -41,6 +37,6 @@ class Solution {
       --counts[target.charAt(i) - 'a'];
     }
 
-    return (result == null) ? "" : result;
+    return result;
   }
 }
