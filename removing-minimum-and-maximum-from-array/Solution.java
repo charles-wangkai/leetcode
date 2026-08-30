86210a3ae85e03a46b9372b5b1e0164aa8fb962c
Kai Wang <charles.wangkai@gmail.com>
2026-08-30 09:20:27 +0800

    Removing Minimum and Maximum From Array

--- removing-minimum-and-maximum-from-array/Solution.java
+++ removing-minimum-and-maximum-from-array/Solution.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
 class Solution {
@@ -7,16 +7,13 @@ class Solution {
       return 1;
     }
 
-    int minValue = Arrays.stream(nums).min().getAsInt();
-    int minIndex =
-        IntStream.range(0, nums.length).filter(i -> nums[i] == minValue).findAny().getAsInt();
+    int indexForMinValue =
+        IntStream.range(0, nums.length).boxed().min(Comparator.comparing(i -> nums[i])).get();
+    int indexForMaxValue =
+        IntStream.range(0, nums.length).boxed().max(Comparator.comparing(i -> nums[i])).get();
 
-    int maxValue = Arrays.stream(nums).max().getAsInt();
-    int maxIndex =
-        IntStream.range(0, nums.length).filter(i -> nums[i] == maxValue).findAny().getAsInt();
-
-    int leftIndex = Math.min(minIndex, maxIndex);
-    int rightIndex = Math.max(minIndex, maxIndex);
+    int leftIndex = Math.min(indexForMinValue, indexForMaxValue);
+    int rightIndex = Math.max(indexForMinValue, indexForMaxValue);
 
     return Math.min(
         Math.min(rightIndex + 1, nums.length - leftIndex),
