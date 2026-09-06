d85de2d6d9e529a044b84f261cf6af39d5bcd9fc
Kai Wang <charles.wangkai@gmail.com>
2026-09-06 22:40:19 +0800

    Distinct Subsequences

--- distinct-subsequences/Solution.java
+++ distinct-subsequences/Solution.java
@@ -1,22 +1,16 @@
 class Solution {
   public int numDistinct(String s, String t) {
-    int sLength = s.length();
-    int tLength = t.length();
-    int[][] wayNums = new int[sLength + 1][tLength + 1];
-    for (int i = 0; i <= sLength; ++i) {
-      for (int j = 0; j <= tLength; ++j) {
-        if (j == 0) {
-          wayNums[i][j] = 1;
-        } else if (i == 0) {
-          wayNums[i][j] = 0;
-        } else {
-          wayNums[i][j] =
-              wayNums[i - 1][j]
-                  + ((s.charAt(i - 1) == t.charAt(j - 1)) ? wayNums[i - 1][j - 1] : 0);
+    int[] dp = new int[t.length() + 1];
+    dp[0] = 1;
+
+    for (char c : s.toCharArray()) {
+      for (int i = t.length(); i >= 1; --i) {
+        if (c == t.charAt(i - 1)) {
+          dp[i] += dp[i - 1];
         }
       }
     }
 
-    return wayNums[sLength][tLength];
+    return dp[t.length()];
   }
 }
