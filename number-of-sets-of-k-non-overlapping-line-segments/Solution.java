0b002a40c1bb4c72629a91899c7b67252a636bb8
Kai Wang <charles.wangkai@gmail.com>
2026-09-16 09:31:22 +0800

    Number of Sets of K Non-Overlapping Line Segments

--- number-of-sets-of-k-non-overlapping-line-segments/Solution.java
+++ number-of-sets-of-k-non-overlapping-line-segments/Solution.java
@@ -1,23 +1,64 @@
+import java.math.BigInteger;
+
 class Solution {
-  static final int MODULUS = 1_000_000_007;
+  static final ModInt MOD_INT = new ModInt(1_000_000_007);
 
   public int numberOfSets(int n, int k) {
-    int[][][] wayNums = new int[n + 1][k + 1][3];
-    wayNums[0][0][0] = 1;
+    int[][] dp = new int[k + 1][2];
+    dp[0][0] = 1;
 
-    for (int i = 1; i <= n; ++i) {
-      for (int j = 0; j <= k; ++j) {
-        wayNums[i][j][0] =
-            (j == 0) ? wayNums[i - 1][j][0] : addMod(wayNums[i - 1][j][0], wayNums[i][j - 1][2]);
-        wayNums[i][j][1] = wayNums[i][j][0];
-        wayNums[i][j][2] = addMod(wayNums[i - 1][j][1], wayNums[i - 1][j][2]);
+    for (int i = 0; i < n - 1; ++i) {
+      int[][] nextDp = new int[k + 1][2];
+      for (int segmentNum = 0; segmentNum <= k; ++segmentNum) {
+        nextDp[segmentNum][0] = MOD_INT.addMod(dp[segmentNum][0], dp[segmentNum][1]);
+
+        if (segmentNum != 0) {
+          nextDp[segmentNum][1] =
+              MOD_INT.addMod(
+                  MOD_INT.addMod(dp[segmentNum - 1][0], dp[segmentNum - 1][1]), dp[segmentNum][1]);
+        }
       }
+
+      dp = nextDp;
     }
 
-    return wayNums[n][k][0];
+    return MOD_INT.addMod(dp[k][0], dp[k][1]);
+  }
+}
+
+class ModInt {
+  int modulus;
+
+  ModInt(int modulus) {
+    this.modulus = modulus;
+  }
+
+  int mod(long x) {
+    return Math.floorMod(x, modulus);
+  }
+
+  int modInv(int x) {
+    return BigInteger.valueOf(x).modInverse(BigInteger.valueOf(modulus)).intValue();
   }
 
   int addMod(int x, int y) {
-    return (x + y) % MODULUS;
+    return mod(x + y);
+  }
+
+  int multiplyMod(int x, int y) {
+    return mod((long) x * y);
+  }
+
+  int divideMod(int x, int y) {
+    return multiplyMod(x, modInv(y));
+  }
+
+  int powMod(int base, long exponent) {
+    if (exponent == 0) {
+      return 1;
+    }
+
+    return multiplyMod(
+        (exponent % 2 == 0) ? 1 : base, powMod(multiplyMod(base, base), exponent / 2));
   }
 }
