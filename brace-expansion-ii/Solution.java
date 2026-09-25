acf8f13167ec09450f1d5d47e7df18ee5b0da825
Kai Wang <charles.wangkai@gmail.com>
2026-09-25 08:41:03 +0800

    Brace Expansion II

--- brace-expansion-ii/Solution.java
+++ brace-expansion-ii/Solution.java
@@ -2,22 +2,21 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class Solution {
+class Solution {
   public List<String> braceExpansionII(String expression) {
     return expand(expression).stream().sorted().toList();
   }
 
   Set<String> expand(String expression) {
     if (expression.length() == 1) {
-      return Stream.of(expression).collect(Collectors.toSet());
-    } else if (isUnion(expression)) {
-      return union(expression);
-    } else {
-      return concat(expression);
+      return Set.of(expression);
     }
+    if (isUnion(expression)) {
+      return union(expression);
+    }
+
+    return concat(expression);
   }
 
   Set<String> concat(String expression) {
@@ -25,36 +24,36 @@ public class Solution {
 
     int beginIndex = -1;
     int depth = 0;
-    for (int i = 0; i < expression.length(); i++) {
-      char ch = expression.charAt(i);
+    for (int i = 0; i < expression.length(); ++i) {
+      char c = expression.charAt(i);
 
-      if (ch == '{') {
+      if (c == '{') {
         if (depth == 0) {
           beginIndex = i;
         }
 
-        depth++;
-      } else if (ch == '}') {
-        depth--;
+        ++depth;
+      } else if (c == '}') {
+        --depth;
 
         if (depth == 0) {
           partsList.add(expand(expression.substring(beginIndex, i + 1)));
         }
-      } else {
-        if (depth == 0) {
-          partsList.add(expand(expression.substring(i, i + 1)));
-        }
+      } else if (depth == 0) {
+        partsList.add(expand(expression.substring(i, i + 1)));
       }
     }
 
-    Set<String> result = new HashSet<>();
-    search(result, partsList, 0, new StringBuilder());
-    return result;
+    Set<String> concatenated = new HashSet<>();
+    search(concatenated, partsList, 0, new StringBuilder());
+
+    return concatenated;
   }
 
-  void search(Set<String> result, List<Set<String>> partsList, int index, StringBuilder word) {
+  void search(
+      Set<String> concatenated, List<Set<String>> partsList, int index, StringBuilder word) {
     if (index == partsList.size()) {
-      result.add(word.toString());
+      concatenated.add(word.toString());
 
       return;
     }
@@ -62,7 +61,7 @@ public class Solution {
     for (String part : partsList.get(index)) {
       word.append(part);
 
-      search(result, partsList, index + 1, word);
+      search(concatenated, partsList, index + 1, word);
 
       word.delete(word.length() - part.length(), word.length());
     }
@@ -73,15 +72,15 @@ public class Solution {
 
     int beginIndex = 1;
     int depth = 0;
-    for (int i = 1; i <= expression.length() - 1; i++) {
+    for (int i = 1; i <= expression.length() - 1; ++i) {
       if (i == expression.length() - 1 || (depth == 0 && expression.charAt(i) == ',')) {
         result.addAll(expand(expression.substring(beginIndex, i)));
 
         beginIndex = i + 1;
       } else if (expression.charAt(i) == '{') {
-        depth++;
+        ++depth;
       } else if (expression.charAt(i) == '}') {
-        depth--;
+        --depth;
       }
     }
 
@@ -94,13 +93,13 @@ public class Solution {
     }
 
     int depth = 0;
-    for (int i = 0; ; i++) {
-      char ch = expression.charAt(i);
+    for (int i = 0; ; ++i) {
+      char c = expression.charAt(i);
 
-      if (ch == '{') {
-        depth++;
-      } else if (ch == '}') {
-        depth--;
+      if (c == '{') {
+        ++depth;
+      } else if (c == '}') {
+        --depth;
 
         if (depth == 0) {
           return i == expression.length() - 1;
